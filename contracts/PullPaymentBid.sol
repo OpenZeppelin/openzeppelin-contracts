4ad2e0fb2d21db3d395a22bee07c9ce6c08726ad
Manuel Araoz <manuelaraoz@gmail.com>
2016-08-10 15:36:10 -0300

    remove tabs

--- contracts/PullPaymentBid.sol
+++ contracts/PullPaymentBid.sol
@@ -1,14 +1,14 @@
 import './PullPaymentCapable.sol';
 
 contract PullPaymentBid is PullPaymentCapable {
-	address public highestBidder;
+  address public highestBidder;
   uint public highestBid;
   
   function bid() external {
     if (msg.value <= highestBid) throw;
     
     if (highestBidder != 0) {
-			asyncSend(highestBidder, highestBid);
+      asyncSend(highestBidder, highestBid);
     }
     highestBidder = msg.sender;
     highestBid = msg.value;
