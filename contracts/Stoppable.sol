c543282f40401b69f769815d8bf7da5c651cfe74
Manuel Araoz <manuelaraoz@gmail.com>
2016-08-12 11:33:56 -0300

    stoppable style

--- contracts/Stoppable.sol
+++ contracts/Stoppable.sol
@@ -11,16 +11,12 @@ contract Stoppable {
   modifier onlyInEmergency { if (stopped) _ }
 
   function Stoppable(address _curator) {
-    if (_curator == 0) {
-      throw;
-    }
+    if (_curator == 0) throw;
     curator = _curator;
   }
 
   function emergencyStop() external {
-    if (msg.sender != curator) {
-      throw;
-    }
+    if (msg.sender != curator) throw;
     stopped = true;
   }
 
