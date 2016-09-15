50713039697311f3f932b043b75a74afc04e2eec
Manuel Araoz <manuelaraoz@gmail.com>
2016-09-15 15:22:30 -0700

    new less trivial ownable test

--- test/ownable.js
+++ test/ownable.js
@@ -6,6 +6,18 @@ contract('Ownable', function(accounts) {
       assert.isTrue(owner != 0);
     })
     .then(done)
+  });
 
+  it("changes owner after transfer", function(done) {
+    var ownable = Ownable.deployed();
+    var other = '0xe682569efa3752a07fdc09885007c47beee803a7';
+    return ownable.transfer(other)
+    .then(function() {
+      return ownable.owner();
+    })
+    .then(function(owner) {
+      assert.isTrue(owner === other);
+    })
+    .then(done)
   });
 });
