fa828f25ab77180d7cab22d9adfe6081ef6894ab
adklempner <adklempner@gmail.com>
2016-09-20 22:52:16 -0700

    Return a bool to indicate whether PullPaymentBid.bid() is successful.

--- contracts/examples/PullPaymentBid.sol
+++ contracts/examples/PullPaymentBid.sol
@@ -3,14 +3,15 @@ import '../PullPaymentCapable.sol';
 contract PullPaymentBid is PullPaymentCapable {
   address public highestBidder;
   uint public highestBid;
-  
-  function bid() external {
-    if (msg.value <= highestBid) throw;
-    
+
+  function bid() external returns (bool) {
+    if (msg.value <= highestBid) return false;
+
     if (highestBidder != 0) {
       asyncSend(highestBidder, highestBid);
     }
     highestBidder = msg.sender;
     highestBid = msg.value;
+    return true;
   }
 }
