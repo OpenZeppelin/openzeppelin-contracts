b1a49c91a2082ccbc32a823f9fa307838d6146ec
adklempner <adklempner@gmail.com>
2016-09-20 20:52:56 -0700

    Make use of before() hook, declare ownable only once, access accounts array once per test.

--- test/ownable.js
+++ test/ownable.js
@@ -1,6 +1,12 @@
 contract('Ownable', function(accounts) {
+
+  var ownable;
+
+  before(function() {
+    ownable = Ownable.deployed();
+  });
+
   it("should have an owner", function(done) {
-    var ownable = Ownable.deployed();
     return ownable.owner()
     .then(function(owner) {
       assert.isTrue(owner != 0);
@@ -9,8 +15,8 @@ contract('Ownable', function(accounts) {
   });
 
   it("changes owner after transfer", function(done) {
-    var ownable = Ownable.deployed();
     var other = accounts[1];
+
     return ownable.transfer(other)
     .then(function() {
       return ownable.owner();
@@ -21,10 +27,10 @@ contract('Ownable', function(accounts) {
     .then(done)
   });
 
-  it("should prevent non-owners from transfering" ,function(done) {
-    var ownable = Ownable.deployed();
+  it("should prevent non-owners from transfering", function(done) {
     var other = accounts[2];
-    return ownable.transfer(other, {from: accounts[2]})
+
+    return ownable.transfer(other, {from: other})
     .then(function() {
       return ownable.owner();
     })
