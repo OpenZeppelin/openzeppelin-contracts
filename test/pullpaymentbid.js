002bb9a86f0cbc05f9b4d776c3e1ea04327d29ab
adklempner <adklempner@gmail.com>
2016-09-20 22:36:55 -0700

    Test that PullPaymentBid.bid() replaces the current bid and bidder when a higher one is made.

--- test/pullpaymentbid.js
+++ test/pullpaymentbid.js
@@ -29,4 +29,27 @@ contract('PullPaymentBid', function(accounts) {
     .then(done);
   });
 
+  it("replaces first bid and bidder with higher ones", function(done) {
+    var bidAmount = 2;
+    var bidderAddress = accounts[2];
+
+    return pullPaymentBid.bid({
+      from: bidderAddress,
+      value: bidAmount
+    })
+    .then(function() {
+      return pullPaymentBid.highestBid()
+      .then(function(bid) {
+        assert.isTrue(bid.c[0] === bidAmount);
+      });
+    })
+    .then(function() {
+      return pullPaymentBid.highestBidder()
+      .then(function(bidder) {
+        assert.isTrue(bidder === bidderAddress);
+      });
+    })
+    .then(done);
+  });
+  
 });
