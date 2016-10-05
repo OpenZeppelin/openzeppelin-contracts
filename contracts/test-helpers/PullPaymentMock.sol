503faa051d7eb27c444958fecb499b25b00ece05
Manuel Araoz <manuelaraoz@gmail.com>
2016-10-05 17:08:40 -0300

    rename PullPaymentCapable=>PullPayment

---
+++ contracts/test-helpers/PullPaymentMock.sol
@@ -0,0 +1,9 @@
+import '../PullPayment.sol';
+
+// mock class using PullPayment
+contract PullPaymentMock is PullPayment {
+  // test helper function to call asyncSend
+  function callSend(address dest, uint amount) external {
+    asyncSend(dest, amount);
+  }
+}
