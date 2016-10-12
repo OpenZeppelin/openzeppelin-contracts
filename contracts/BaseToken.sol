f8c486ea1b729d979697c8490665f23ab25caddd
Manuel Araoz <manuelaraoz@gmail.com>
2016-10-12 20:07:15 -0300

    style changes to base token

--- contracts/BaseToken.sol
+++ contracts/BaseToken.sol
@@ -7,48 +7,50 @@ import './ERC20.sol';
  * ERC 20 token
  *
  * https://github.com/ethereum/EIPs/issues/20
- * Based on original code by FirstBlood:
+ * Based on code by FirstBlood:
  * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
 contract BaseToken is ERC20 {
 
-    function transfer(address _to, uint256 _value) returns (bool success) {
-        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
-    }
+  mapping(address => uint256) balances;
+  mapping (address => mapping (address => uint256)) allowed;
+  uint256 public totalSupply;
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
+  function transfer(address _to, uint256 _value) returns (bool success) {
+    if (balances[msg.sender] >= _value &&
+        balances[_to] + _value > balances[_to]) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      Transfer(msg.sender, _to, _value);
+      return true;
+    } else { return false; }
+  }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    if (balances[_from] >= _value &&
+        allowed[_from][msg.sender] >= _value &&
+        balances[_to] + _value > balances[_to]) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
+      allowed[_from][msg.sender] -= _value;
+      Transfer(_from, _to, _value);
+      return true;
+    } else { return false; }
+  }
 
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
 
-    mapping(address => uint256) balances;
+  function approve(address _spender, uint256 _value) returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
 
-    mapping (address => mapping (address => uint256)) allowed;
-
-    uint256 public totalSupply;
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
 
 }
