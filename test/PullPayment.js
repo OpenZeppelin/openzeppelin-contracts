a9d030acb4ba2328d089590e65462b17a794303d
Michael J. Curry <currymj@fastmail.fm>
2016-10-12 14:04:18 -0400

    fix names, tests passing

--- test/PullPayment.js
+++ test/PullPayment.js
@@ -1,8 +1,8 @@
-contract('PullPaymentCapable', function(accounts) {
+contract('PullPaymentExample', function(accounts) {
 
   it("can't call asyncSend externally", function(done) {
     var ppc;
-    return PullPaymentCapableExample.new()
+    return PullPaymentExample.new()
       .then(function(ppc) {
         assert.isUndefined(ppc.asyncSend);
       })
@@ -12,7 +12,7 @@ contract('PullPaymentCapable', function(accounts) {
   it("can record an async payment correctly", function(done) {
     var ppce;
     var AMOUNT = 100;
-    return PullPaymentCapableExample.new()
+    return PullPaymentExample.new()
       .then(function(_ppce) {
         ppce = _ppce;
         ppce.callSend(accounts[0], AMOUNT)
