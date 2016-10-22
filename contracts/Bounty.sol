7bdcba320e30d3dad42516def187e957007a95c5
Makoto Inoue <inouemak@googlemail.com>
2016-10-22 22:42:21 +0100

    WIP: add createFromAddress

--- contracts/Bounty.sol
+++ contracts/Bounty.sol
@@ -12,7 +12,32 @@ contract Target {
   function checkInvariant() returns(bool);
 }
 
-contract Bounty is PullPayment {
+contract BytecodeDeployer {
+    function createFromAddress(address _addr){
+      return createFromBytecode(msg.value, getByteCode(_addr)
+    }
+
+    // From http://solidity.readthedocs.io/en/latest/control-structures.html#opcodes
+    function getByteCode(address _addr) returns (bytes o_code) {
+      assembly {
+        let size := extcodesize(_addr)
+        o_code := mload(0x40)
+        mstore(0x40, add(o_code, and(add(add(size, 0x20), 0x1f), not(0x1f))))
+        mstore(o_code, size)
+        extcodecopy(_addr, add(o_code, 0x20), 0, size)
+      }
+    }
+
+    // From https://gist.github.com/axic/fe2640e03fddea3f710d3856c20a579e
+    function createFromBytecode(uint value, bytes bytecode) returns (address result) {
+      assembly {
+          let size := mload(bytecode)
+          result := create(value, add(bytecode, 32), size)
+      }
+    }
+}
+
+contract Bounty is PullPayment, BytecodeDeployer {
   Target target;
   bool public claimed;
   mapping(address => address) public researchers;
@@ -22,7 +47,7 @@ contract Bounty is PullPayment {
   }
 
   function createTarget(address targetAddress) returns(Target) {
-    target = Target(targetAddress);
+    target = createFromAddress(targetAddress);
     researchers[target] = msg.sender;
     return target;
   }
