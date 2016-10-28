949f25b244228e87aa9c121b22bcd026d316ff1f
Makoto Inoue <inouemak@googlemail.com>
2016-10-28 21:55:16 +0100

    Deploy mock contracts on test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,10 @@ module.exports = function(deployer) {
   deployer.deploy(ProofOfExistence);
   deployer.deploy(Ownable);
   deployer.deploy(LimitFunds);
-
   if(deployer.network == 'test'){
+    deployer.deploy(SecureTargetMock);
     deployer.deploy(SecureTargetFactory);
+    deployer.deploy(InsecureTargetMock);
     deployer.deploy(InsecureTargetFactory);
   };
 };
