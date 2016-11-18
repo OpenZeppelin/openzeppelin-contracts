7bc35f6f7889802b746cbd1fc8b1fe81272d39cc
Manuel Araoz <manuelaraoz@gmail.com>
2016-11-18 14:13:28 -0300

    polish BasicToken

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -11,14 +11,13 @@ contract BasicToken is ERC20Basic, SafeMath {
 
   mapping(address => uint) balances;
 
-  function transfer(address _to, uint _value) returns (bool success) {
+  function transfer(address _to, uint _value) {
     if (balances[msg.sender] < _value) {
       throw;
     } 
     balances[msg.sender] = safeSub(balances[msg.sender], _value);
     balances[_to] = safeAdd(balances[_to], _value);
     Transfer(msg.sender, _to, _value);
-    return true;
   }
 
   function balanceOf(address _owner) constant returns (uint balance) {
