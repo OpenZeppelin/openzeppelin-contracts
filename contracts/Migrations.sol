794596254bb52399d23647802e4ab6776bc66e25
Arseniy Klempner <adklempner@gmail.com>
2016-11-20 19:48:30 -0800

    Inherit Ownable in Migrations and Killable. Fix naming convention.

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,22 +1,13 @@
 pragma solidity ^0.4.4;
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+contract Migrations is Ownable {
+  uint public lastCompletedMigration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
+  function setCompleted(uint completed) onlyOwner {
+    lastCompletedMigration = completed;
   }
 
-  function Migrations() {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint completed) restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+  function upgrade(address newAddress) onlyOwner {
+    Migrations upgraded = Migrations(newAddress);
+    upgraded.setCompleted(lastCompletedMigration);
   }
 }
