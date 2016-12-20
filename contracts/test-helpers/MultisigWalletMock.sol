813c1ff0cd990a63519993d71aa8dfcec77cf131
Arseniy Klempner <adklempner@gmail.com>
2016-12-20 13:12:45 -0800

    Add payable to MultisigWalletMock()

--- contracts/test-helpers/MultisigWalletMock.sol
+++ contracts/test-helpers/MultisigWalletMock.sol
@@ -5,7 +5,7 @@ contract MultisigWalletMock is MultisigWallet {
   uint public totalSpending;
 
   function MultisigWalletMock(address[] _owners, uint _required, uint _daylimit)
-    MultisigWallet(_owners, _required, _daylimit) { }
+    MultisigWallet(_owners, _required, _daylimit) payable { }
 
   function changeOwner(address _from, address _to) external { }
 
