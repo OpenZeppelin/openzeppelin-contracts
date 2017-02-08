70fbc7fe27941a1204fe0650f389fadff8feb215
Esteban Ordano <eordano@gmail.com>
2017-02-07 19:45:32 -0500

    BasicToken.sol: Remove useless check
    The condition this commit removes is already implemented by the call to `safeSub`

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -14,9 +14,6 @@ contract BasicToken is ERC20Basic, SafeMath {
   mapping(address => uint) balances;
 
   function transfer(address _to, uint _value) {
-    if (balances[msg.sender] < _value) {
-      throw;
-    } 
     balances[msg.sender] = safeSub(balances[msg.sender], _value);
     balances[_to] = safeAdd(balances[_to], _value);
     Transfer(msg.sender, _to, _value);
