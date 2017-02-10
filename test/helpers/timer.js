ee56abcc8a981300b0f33c017cb1c67d65d40db0
Manuel Araoz <manuelaraoz@gmail.com>
2017-02-10 18:35:05 -0300

    remove console.logs from timer

--- test/helpers/timer.js
+++ test/helpers/timer.js
@@ -1,6 +1,5 @@
 // timer for tests specific to testrpc
 module.exports = s => {
-  console.log('timer to', s);
   return new Promise((resolve, reject) => {
     web3.currentProvider.sendAsync({
       jsonrpc: '2.0', 
@@ -8,7 +7,6 @@ module.exports = s => {
       params: [s], // 60 seaconds, may need to be hex, I forget
       id: new Date().getTime() // Id of the request; anything works, really
     }, function(err) {
-      console.log('resolved to', err);
       if (err) return reject(err);
       resolve();
     });
