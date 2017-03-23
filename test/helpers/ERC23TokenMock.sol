94d3c447b788c09b6c79cbe8de6caf126af7a593
Remco Bloemen <remco-git@xn--2-umb.com>
2017-03-23 15:22:48 +0000

    Add HasNoTokens

---
+++ test/helpers/ERC23TokenMock.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.8;
+
+
+import '../../contracts/token/BasicToken.sol';
+
+
+contract ERC23ContractInterface {
+  function tokenFallback(address _from, uint _value, bytes _data) external;
+}
+
+contract ERC23TokenMock is BasicToken {
+
+  function ERC23TokenMock(address initialAccount, uint initialBalance) {
+    balances[initialAccount] = initialBalance;
+    totalSupply = initialBalance;
+  }
+
+  // ERC23 compatible transfer function (except the name)
+  function transferERC23(address _to, uint _value, bytes _data)
+    returns (bool success)
+  {
+    transfer(_to, _value);
+    bool is_contract = false;
+    assembly {
+      is_contract := not(iszero(extcodesize(_to)))
+    }
+    if(is_contract) {
+      ERC23ContractInterface receiver = ERC23ContractInterface(_to);
+      receiver.tokenFallback(msg.sender, _value, _data);
+    }
+    return true;
+  }
+}
--- test/helpers/expectThrow.js
+++ test/helpers/expectThrow.js
@@ -5,7 +5,15 @@ export default async promise => {
     // TODO: Check jump destination to destinguish between a throw
     //       and an actual invalid jump.
     const invalidJump = error.message.search('invalid JUMP') >= 0;
-    assert(invalidJump, "Expected throw, got '" + error + "' instead");
+    // TODO: When we contract A calls contract B, and B throws, instead
+    //       of an 'invalid jump', we get an 'out of gas' error. How do
+    //       we distinguish this from an actual out of gas event? (The
+    //       testrpc log actually show an 'invalid jump' event.)
+    const outOfGas = error.message.search('out of gas') >= 0;
+    assert(
+      invalidJump || outOfGas,
+      "Expected throw, got '" + error + "' instead",
+    );
     return;
   }
   assert.fail('Expected throw not received');
