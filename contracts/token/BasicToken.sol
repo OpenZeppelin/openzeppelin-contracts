5d75264f0f5a552ec994266cd8691fadfa422252
Jerome de Tychey <jdetychey@gmail.com>
2017-04-11 17:25:03 +0200

    Fix for bigger payloads
    as suggested by izqui in case a function calls transfer under the hood

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -17,7 +17,7 @@ contract BasicToken is ERC20Basic, SafeMath {
  * Fix for the ERC20 short address attack  
  */
   modifier onlyPayloadSize(uint size) {
-     assert(msg.data.length == size + 4);
+     assert(msg.data.length >= size + 4);
      _;
   }
 
