e1cf60248756a30cfd2fe74be1120d868b514425
Mikko Ohtamaa <mikko@opensourcehacker.com>
2017-05-17 01:41:43 +0300

    Fix variable naming.

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -31,10 +31,10 @@ contract StandardToken is BasicToken, ERC20 {
   function approve(address _spender, uint _value) {
 
     // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender,0)` if it is not
+    //  allowance to zero by calling `approve(_spender, 0)` if it is not
     //  already 0 to mitigate the race condition described here:
     //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    if ((_amount!=0) && (allowed[msg.sender][_spender] !=0)) throw;
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) throw;
 
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
