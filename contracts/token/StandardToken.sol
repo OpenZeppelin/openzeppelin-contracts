b206a4c470fbeafa3e754d7e5d3d2c2f68ff9944
pipaman <pablo.yabo@gmail.com>
2017-05-17 16:28:47 -0300

    Update StandardToken.sol
    Variable mistake

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -59,7 +59,7 @@ contract StandardToken is BasicToken, ERC20 {
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
         } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_addedValue);
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
         }
         Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
       return true;
