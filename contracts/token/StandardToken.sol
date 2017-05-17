b935881fec9bc26e6045e9f58f5f3247bb2281ed
pipaman <pablo.yabo@gmail.com>
2017-05-17 15:25:36 -0300

    Update StandardToken.sol
    * Fixed double spend attack on approve
    * Fixed Short Address Attack for transferFrom

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -16,7 +16,17 @@ contract StandardToken is BasicToken, ERC20 {
 
   mapping (address => mapping (address => uint)) allowed;
 
-  function transferFrom(address _from, address _to, uint _value) {
+  /*
+   * Fix for the ERC20 short address attack  
+   */
+  modifier onlyPayloadSize(uint size) {
+     if(msg.data.length < size + 4) {
+       throw;
+     }
+     _;
+  }
+  
+  function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32) {
     var _allowance = allowed[_from][msg.sender];
 
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
@@ -29,10 +39,40 @@ contract StandardToken is BasicToken, ERC20 {
   }
 
   function approve(address _spender, uint _value) {
+    // prevent double spend attack
+    if ((_value != 0) && (allowed[msg.sender][_spender] != 0)) {
+       return false;
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
   }
 
+  /*
+   * approve should be called when allowed[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until 
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   */
+  function increaseApproval (address _spender, uint _addedValue) 
+    onlyPayloadSize(2 * 32)
+    returns (bool success) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+  function decreaseApproval (address _spender, uint _subtractedValue) 
+    onlyPayloadSize(2 * 32)
+    returns (bool success) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_addedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+      return true;
+  }
+    
   function allowance(address _owner, address _spender) constant returns (uint remaining) {
     return allowed[_owner][_spender];
   }
