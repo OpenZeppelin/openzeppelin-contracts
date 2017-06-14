403e7336a9e00f77b944e7a6ab4dcd9ad4e520db
Matt <mbranton@gmail.com>
2017-06-14 16:31:12 -0400

    fixed typo

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -36,7 +36,7 @@ contract StandardToken is BasicToken, ERC20 {
   }
 
   /**
-   * @dev Aprove the passed address to spend the specified amount of tokens on beahlf of msg.sender.
+   * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
