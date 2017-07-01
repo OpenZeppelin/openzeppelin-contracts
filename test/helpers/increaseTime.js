b3ca0c73b34ca99baf16941d3e2a83142f892309
Francisco Giordano <frangio.1@gmail.com>
2017-06-30 21:25:59 -0300

    add a TokenTimelock contract

---
+++ test/helpers/increaseTime.js
@@ -0,0 +1,23 @@
+// Increases testrpc time by the passed duration (a moment.js instance)
+export default function increaseTime(duration) {
+  const id = Date.now()
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration.asSeconds()],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1)
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id+1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res)
+      })
+    })
+  })
+}
