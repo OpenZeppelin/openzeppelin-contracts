ba383a6d204bba3e910d011e29b7ab4c5bb4fadf
Francisco Giordano <frangio.1@gmail.com>
2017-07-02 13:01:55 -0300

    refactor max and min out of SafeMath

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -29,21 +29,4 @@ library SafeMath {
     assert(c >= a);
     return c;
   }
-
-  function max64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a >= b ? a : b;
-  }
-
-  function min64(uint64 a, uint64 b) internal constant returns (uint64) {
-    return a < b ? a : b;
-  }
-
-  function max256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a >= b ? a : b;
-  }
-
-  function min256(uint256 a, uint256 b) internal constant returns (uint256) {
-    return a < b ? a : b;
-  }
-
 }
