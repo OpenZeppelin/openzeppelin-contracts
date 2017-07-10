5afbdf00dba439078e696bddbce7daccb8ce43e0
cgewecke <christophergewecke@gmail.com>
2017-07-10 15:45:12 -0700

    Fix coverage

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    norpc: true,
+    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+    skipFiles: ['lifecycle/Migrations.sol']
+}
