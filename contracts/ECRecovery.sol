a3dd9fb0a0297e4a513aac2a3fff9dc2ac60334d
Augusto <augusto.lemble@gmail.com>
2017-07-14 10:10:06 -0300

    Removed safeRecover, using ecrecover method instead, recovering signature from entire hash

--- contracts/ECRecovery.sol
+++ contracts/ECRecovery.sol
@@ -10,76 +10,35 @@ pragma solidity ^0.4.11;
 library ECRecovery {
 
   /**
-   * @dev Duplicate Solidity's ecrecover, but catching the CALL return value
-   * @param hash bytes32 messahe hash from which the signature will be recovered
-   * @param v uint8 signature version
-   * @param r bytes32 signature r value
-   * @param s bytes32 signature s value
-   */
-  function safeRecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns (bool, address) {
-    // We do our own memory management here. Solidity uses memory offset
-    // 0x40 to store the current end of memory. We write past it (as
-    // writes are memory extensions), but don't update the offset so
-    // Solidity will reuse it. The memory used here is only needed for
-    // this context.
-
-    bool ret;
-    address addr;
-
-    assembly {
-      let size := mload(0x40)
-      mstore(size, hash)
-      mstore(add(size, 32), v)
-      mstore(add(size, 64), r)
-      mstore(add(size, 96), s)
-
-      // NOTE: we can reuse the request memory because we deal with
-      //       the return code
-      ret := call(3000, 1, 0, size, 128, size, 32)
-      addr := mload(size)
-    }
-
-    return (ret, addr);
-  }
-
-  /**
    * @dev Recover signer address from a message by using his signature
-   * @param hash bytes32 messahe hash from which the signature will be recovered
-   * @param sig bytes signature
+   * @param hash bytes32 message hash from which the signature will be recovered
+   * @param sig bytes signature, the siganture is generated using web3.eth.sign()
    */
   function recover(bytes32 hash, bytes sig) constant returns (address) {
     bytes32 r;
     bytes32 s;
     uint8 v;
 
+    //Check the signature length
     if (sig.length != 65)
       return (address(0));
 
+    // Divide the signature in r, s and v variables
     assembly {
       r := mload(add(sig, 32))
       s := mload(add(sig, 64))
       v := byte(0, mload(add(sig, 96)))
     }
 
-    // albeit non-transactional signatures are not specified by the YP, one would expect it
-    // to match the YP range of [27, 28]
-    //
-    // geth uses [0, 1] and some clients have followed. This might change, see:
-    //  https://github.com/ethereum/go-ethereum/issues/2053
+    // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
     if (v < 27)
      v += 27;
 
+    // If the version is correct return the signer address
     if (v != 27 && v != 28)
       return (address(0));
-
-    bool ret;
-    address addr;
-    (ret, addr) = safeRecover(hash, v, r, s);
-
-    if (!ret)
-      return address(0);
     else
-      return addr;
+      return ecrecover(hash, v, r, s);
   }
 
 }
--- docs/source/ecrecovery.rst
+++ docs/source/ecrecovery.rst
@@ -1,14 +1,9 @@
 ECReovery
 =============================================
 
-Recover the signer address of messages using elliptic curve signatures.
-
-safeRecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal returns (bool, address)
-"""""""""""""""""""""""""""""""""""""""""""""""""
-
 Returns the signer of the the hash using the signature divided in v, r, and s values.
 
 recover(bytes32 hash, bytes sig) internal returns (address)
 """""""""""""""""""""""""""""""""""""""""""""""""
 
-Returns the signer of the the hash using the signature that provides the web3.sign() method.
+Returns the signer of the the hash using the signature that provides the web3.eth.sign() method.
