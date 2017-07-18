4f444279661e7c2e6ff7c442c3d13ae45d33b892
AugustoL <augusto.lemble@gmail.com>
2017-07-17 22:47:00 -0300

    Added function helper, few changes on ECRecover lib

--- contracts/ECRecovery.sol
+++ contracts/ECRecovery.sol
@@ -20,8 +20,9 @@ library ECRecovery {
     uint8 v;
 
     //Check the signature length
-    if (sig.length != 65)
+    if (sig.length != 65) {
       return (address(0));
+    }
 
     // Divide the signature in r, s and v variables
     assembly {
@@ -31,14 +32,16 @@ library ECRecovery {
     }
 
     // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-    if (v < 27)
-     v += 27;
+    if (v < 27) {
+      v += 27;
+    }
 
     // If the version is correct return the signer address
-    if (v != 27 && v != 28)
+    if (v != 27 && v != 28) {
       return (address(0));
-    else
+    } else {
       return ecrecover(hash, v, r, s);
+    }
   }
 
 }
---
+++ test/helpers/hashMessage.js
@@ -0,0 +1,8 @@
+import utils from 'ethereumjs-util';
+
+// Hash and add same prefix to the hash that testrpc use.
+module.exports = function(message) {
+  const messageHex = new Buffer(utils.sha3(message).toString('hex'), 'hex');
+  const prefix = utils.toBuffer('\u0019Ethereum Signed Message:\n' + messageHex.length.toString());
+  return utils.bufferToHex( utils.sha3(Buffer.concat([prefix, messageHex])) );
+};
