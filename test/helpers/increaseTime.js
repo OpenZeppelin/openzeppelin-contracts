99e0f5b5cbe967884cae4aff71f1dfc891053229
Jakub Wojciechowski <jakub@alice.si>
2017-08-13 12:48:31 +0200

    Refactor time manipulation in TokenTimelock tests

--- test/helpers/increaseTime.js
+++ test/helpers/increaseTime.js
@@ -43,5 +43,7 @@ export const duration = {
   minutes: function(val) { return val * this.seconds(60) },
   hours:   function(val) { return val * this.minutes(60) },
   days:    function(val) { return val * this.hours(24) },
-  weeks:   function(val) { return val * this.days(7) }
+  weeks:   function(val) { return val * this.days(7) },
+  months:  function(val) { return val * this.days(30)},
+  years:   function(val) { return val * this.days(365)} 
 };
