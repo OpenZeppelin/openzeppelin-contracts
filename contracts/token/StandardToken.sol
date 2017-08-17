9333fec5f1e5cf0ad60f760df3a1020b60d529f8
Yaron Velner <yaron172@yahoo.com>
2017-08-17 21:56:11 +0300

    fix to issue 375

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -29,8 +29,8 @@ contract StandardToken is ERC20, BasicToken {
     // Check is not needed because sub(_allowance, _value) will already throw if this condition is not met
     // require (_value <= _allowance);
 
-    balances[_to] = balances[_to].add(_value);
     balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
     allowed[_from][msg.sender] = _allowance.sub(_value);
     Transfer(_from, _to, _value);
     return true;
