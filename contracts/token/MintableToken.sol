b12db57f34017a00154982917f239d8e4dd9ae5e
Petr Korolev <sky4winder+github@gmail.com>
2017-08-18 15:25:15 +0300

    Fixed typo
    recieve -> receive

--- contracts/token/MintableToken.sol
+++ contracts/token/MintableToken.sol
@@ -27,7 +27,7 @@ contract MintableToken is StandardToken, Ownable {
 
   /**
    * @dev Function to mint tokens
-   * @param _to The address that will recieve the minted tokens.
+   * @param _to The address that will receive the minted tokens.
    * @param _amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
