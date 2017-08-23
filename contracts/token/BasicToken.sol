7bc34e1a9e9c067c00dbff9234c319324610d08d
Pavel Rubin <pash7ka@gmail.com>
2017-08-24 01:46:44 +0300

    Check transfer destination is not a zero address. Fix issue #400

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -20,7 +20,6 @@ contract BasicToken is ERC20Basic {
   * @param _value The amount to be transferred.
   */
   function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
     require(_value <= balances[msg.sender]);
 
     // SafeMath.sub will throw if there is not enough balance.
