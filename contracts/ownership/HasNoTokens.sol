61fcaaf4442b6c5f9f7cee1bc81d2c48d12dedb1
Pavel Rubin <pash7ka@gmail.com>
2017-08-28 11:57:45 +0300

    Fix for solidity compiler warnings, issue #367. Crazy and temporary solution, but it's working.

--- contracts/ownership/HasNoTokens.sol
+++ contracts/ownership/HasNoTokens.sol
@@ -18,7 +18,8 @@ contract HasNoTokens is CanReclaimToken {
   * param data_ Bytes The data passed from the caller.
   */
   function tokenFallback(address from_, uint256 value_, bytes data_) external {
-    revert();
+    // revert();    
+    if(true || from_ == 0 || value_ == 0 || data_.length == 0 ) revert(); // Temporary fix for solidity compiler warning
   }
 
 }
