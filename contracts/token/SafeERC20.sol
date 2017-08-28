7b463769a5d88fb1e312b10ce5bf810d39853289
Francisco Giordano <frangio.1@gmail.com>
2017-08-28 17:34:25 -0300

    explain how to use SafeERC20

--- contracts/token/SafeERC20.sol
+++ contracts/token/SafeERC20.sol
@@ -6,6 +6,8 @@ import './ERC20.sol';
 /**
  * @title SafeERC20
  * @dev Wrappers around ERC20 operations that throw on failure.
+ * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
+ * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
  */
 library SafeERC20 {
   function safeTransfer(ERC20Basic token, address to, uint256 value) internal {
