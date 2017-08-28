0376f2a6f2ba1b5daab3fd259bc38529b1699448
Pavel Rubin <pash7ka@gmail.com>
2017-08-28 15:37:45 +0300

    Revert unnmerged commits

--- contracts/ownership/HasNoTokens.sol
+++ contracts/ownership/HasNoTokens.sol
@@ -13,11 +13,11 @@ contract HasNoTokens is CanReclaimToken {
 
  /**
   * @dev Reject all ERC23 compatible tokens
-  * param from_ address The address that is transferring the tokens
-  * param value_ uint256 the amount of the specified token
-  * param data_ Bytes The data passed from the caller.
+  * @param from_ address The address that is transferring the tokens
+  * @param value_ uint256 the amount of the specified token
+  * @param data_ Bytes The data passed from the caller.
   */
-  function tokenFallback(address /*from_*/, uint256 /*value_*/, bytes /*data_*/) external {
+  function tokenFallback(address from_, uint256 value_, bytes data_) external {
     revert();
   }
 
