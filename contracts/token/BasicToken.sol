aad25205cdc54243ccfe82a1bd35c08904d2a152
Francisco Giordano <frangio.1@gmail.com>
2017-08-29 11:50:02 -0300

    add comment making explicit that SafeMath.sub can throw

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -22,6 +22,7 @@ contract BasicToken is ERC20Basic {
   function transfer(address _to, uint256 _value) returns (bool) {
     require(_to != address(0));
 
+    // SafeMath.sub will throw if there is not enough balance.
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
