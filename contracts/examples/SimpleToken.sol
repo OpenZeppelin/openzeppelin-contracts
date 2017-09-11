61f62eb3c7343e2eeda3498659949fd7f8e5ecd7
Santiago Palladino <spalladino@gmail.com>
2017-09-11 12:13:19 -0300

    Fix INITIAL_SUPPLY in SimpleToken example
    Use token decimals

--- contracts/examples/SimpleToken.sol
+++ contracts/examples/SimpleToken.sol
@@ -16,7 +16,7 @@ contract SimpleToken is StandardToken {
   string public constant symbol = "SIM";
   uint8 public constant decimals = 18;
 
-  uint256 public constant INITIAL_SUPPLY = 10000;
+  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
 
   /**
    * @dev Contructor that gives msg.sender all of existing tokens.
