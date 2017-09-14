d2f8761d9e5c4c14b4375529e7fb2489ba6a6cde
jakub-wojciechowski <jakub@alice.si>
2017-09-14 22:04:34 +0200

    Make approve method compliant with ERC20

--- contracts/token/ERC20.sol
+++ contracts/token/ERC20.sol
@@ -11,6 +11,6 @@ import './ERC20Basic.sol';
 contract ERC20 is ERC20Basic {
   function allowance(address owner, address spender) constant returns (uint256);
   function transferFrom(address from, address to, uint256 value) returns (bool);
-  function approve(address spender, uint256 value) returns (bool);
+  function approve(address spender, uint256 value) returns (bool success);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -43,14 +43,7 @@ contract StandardToken is ERC20, BasicToken {
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
-  function approve(address _spender, uint256 _value) returns (bool) {
-
-    // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender, 0)` if it is not
-    //  already 0 to mitigate the race condition described here:
-    //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-
+  function approve(address _spender, uint256 _value) returns (bool success) {
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
     return true;
