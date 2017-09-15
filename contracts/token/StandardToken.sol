83918cad4b9f5bea6dcb3c0c18f3295b2484c825
SylTi <sylvain.tissier@gmail.com>
2017-09-15 19:49:25 +0200

    change approve() to conform to ERC20. Fix #438

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -49,7 +49,7 @@ contract StandardToken is ERC20, BasicToken {
     //  allowance to zero by calling `approve(_spender, 0)` if it is not
     //  already 0 to mitigate the race condition described here:
     //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+    // require((_value == 0) || (allowed[msg.sender][_spender] == 0));
 
     allowed[msg.sender][_spender] = _value;
     Approval(msg.sender, _spender, _value);
