79c3626a134e4c9d94e46bbb70d1b476e9a5182a
jakub-wojciechowski <jakub@alice.si>
2017-09-15 12:28:29 +0200

    Warning in approve method documentation.

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -40,6 +40,11 @@ contract StandardToken is ERC20, BasicToken {
 
   /**
    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
