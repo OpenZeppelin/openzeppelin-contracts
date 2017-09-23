e70dd187342f83a4c447a950dfbdb0f1ca32ef35
Dan Phifer <dan.phifer@gmail.com>
2017-09-23 11:57:29 +0800

    Moving business logic preconditions to the beginning of the function.

--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -21,6 +21,7 @@ contract BasicToken is ERC20Basic {
   */
   function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
+    require(_value <= balances[msg.sender]);
 
     // SafeMath.sub will throw if there is not enough balance.
     balances[msg.sender] = balances[msg.sender].sub(_value);
--- contracts/token/BurnableToken.sol
+++ contracts/token/BurnableToken.sol
@@ -16,6 +16,9 @@ contract BurnableToken is StandardToken {
      */
     function burn(uint256 _value) public {
         require(_value > 0);
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
 
         address burner = msg.sender;
         balances[burner] = balances[burner].sub(_value);
