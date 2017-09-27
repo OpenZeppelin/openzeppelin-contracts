d14f1643dbc79147355758c609dc661b5979abeb
nedodn <odennate@gmail.com>
2017-09-27 14:15:12 -0500

    Update ERC20Basic.sol: Issue #434
    Changed totalSupply into a function rather than a state variable as per Issue #434

--- contracts/token/ERC20Basic.sol
+++ contracts/token/ERC20Basic.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.4.11;
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
 contract ERC20Basic {
-  uint256 public totalSupply;
+  function totalSupply() constant returns (uint256 result);
   function balanceOf(address who) public constant returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
