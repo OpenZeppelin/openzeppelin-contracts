7e4d5b58daaf3d12512e3c037f7d38ef85978d33
nedodn <odennate@gmail.com>
2017-09-27 17:40:14 -0500

    Update StandardToken.sol: Issue #248
    Added check to ensure you cannot send to the token contract. Issue #248

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -24,7 +24,7 @@ contract StandardToken is ERC20, BasicToken {
    * @param _value uint256 the amount of tokens to be transferred
    */
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
+    require(_to != address(0) && _to != address(this));
     require(_value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
 
