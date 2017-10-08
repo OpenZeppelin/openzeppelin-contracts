790833e5d4481ca43deb75019b5cc4917bbbcea2
Dmitry Dudin <nedudi@gmail.com>
2017-10-08 04:26:21 +0300

    Update SimpleToken.sol

--- contracts/examples/SimpleToken.sol
+++ contracts/examples/SimpleToken.sol
@@ -14,7 +14,7 @@ contract SimpleToken is StandardToken {
 
   string public constant name = "SimpleToken";
   string public constant symbol = "SIM";
-  uint256 public constant decimals = 18;
+  uint8 public constant decimals = 18;
 
   uint256 public constant INITIAL_SUPPLY = 10000;
 
