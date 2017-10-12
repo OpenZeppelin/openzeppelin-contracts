f785ffa6a995b36e543155d2d4da710ef5de1220
Patricio Palladino <email@patriciopalladino.com>
2017-10-12 14:55:15 -0300

    Add a parameter to Crowdsale#forwardFunds.
    A parameter was added in order to have way to control how much is forwarded
    to the wallet.

--- contracts/crowdsale/Crowdsale.sol
+++ contracts/crowdsale/Crowdsale.sol
@@ -84,13 +84,13 @@ contract Crowdsale {
     token.mint(beneficiary, tokens);
     TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
 
-    forwardFunds();
+    forwardFunds(weiAmount);
   }
 
   // send ether to the fund collection wallet
   // override to create custom fund forwarding mechanisms
-  function forwardFunds() internal {
-    wallet.transfer(msg.value);
+  function forwardFunds(uint256 weiAmount) internal {
+    wallet.transfer(weiAmount);
   }
 
   // @return true if the transaction can buy tokens
--- contracts/crowdsale/RefundableCrowdsale.sol
+++ contracts/crowdsale/RefundableCrowdsale.sol
@@ -30,8 +30,8 @@ contract RefundableCrowdsale is FinalizableCrowdsale {
   // We're overriding the fund forwarding from Crowdsale.
   // In addition to sending the funds, we want to call
   // the RefundVault deposit function
-  function forwardFunds() internal {
-    vault.deposit.value(msg.value)(msg.sender);
+  function forwardFunds(uint256 weiAmount) internal {
+    vault.deposit.value(weiAmount)(msg.sender);
   }
 
   // if crowdsale is unsuccessful, investors can claim refunds here
