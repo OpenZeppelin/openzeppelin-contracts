595bad26f518da93860ff0d88bbae719d15ff9ca
Yondon Fu <yondon.fu@gmail.com>
2017-10-30 10:59:41 -0400

    Check for revert in expectThrow

--- test/helpers/expectThrow.js
+++ test/helpers/expectThrow.js
@@ -10,8 +10,13 @@ export default async promise => {
     //       we distinguish this from an actual out of gas event? (The
     //       testrpc log actually show an 'invalid jump' event.)
     const outOfGas = error.message.search('out of gas') >= 0;
+    // Check for revert opcode. As of the Byzantium hardfork, contracts
+    // that use the 'require' guard or call 'revert' will use the revert opcode to stop exection upon error,
+    // rollback contract state changes during the transaction and return gas to the sender.
+    const revert = error.message.search('revert') >= 0;
+
     assert(
-      invalidOpcode || outOfGas,
+      invalidOpcode || outOfGas || revert,
       "Expected throw, got '" + error + "' instead",
     );
     return;
