4c0d0fe022e31b832de0d0cf9d04f14b113c86ab
Brandon Gilchrist <BrandonGilchrist@users.noreply.github.com>
2017-11-28 21:43:20 -0800

    Updated math check in StandardToken
    Removed redundant math check in the decreaseApproval function in favor of using SafeMath.

--- contracts/token/StandardToken.sol
+++ contracts/token/StandardToken.sol
@@ -75,11 +75,7 @@ contract StandardToken is ERC20, BasicToken {
 
   function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
     uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-    }
+    allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
     Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
     return true;
   }
