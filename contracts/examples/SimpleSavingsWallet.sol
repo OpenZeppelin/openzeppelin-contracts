560a855dc7e5066cc21360bd214dfb393add23e7
zava <azavalla@dc.uba.ar>
2017-11-28 23:04:39 -0300

    further example documentation

--- contracts/examples/SimpleSavingsWallet.sol
+++ contracts/examples/SimpleSavingsWallet.sol
@@ -6,6 +6,11 @@ import "../ownership/Inheritable.sol";
 /**
  * @title SimpleSavingsWallet
  * @dev Simplest form of savings wallet that can be inherited if owner dies.
+ * In this example, we take a very simple savings wallet providing two operations
+ * (to send and receive funds) and extend its capabilities by making it Inheritable.
+ * The account that creates the contract is set as owner, who has the authority to
+ * choose an heir account. Heir account can reclaim the contract ownership in the
+ * case that the owner dies.
  */
 contract SimpleSavingsWallet is Inheritable {
 
