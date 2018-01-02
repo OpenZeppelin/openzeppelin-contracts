326df13bf39c188657de88a16ea8ffdd9ad014c1
AugustoL <augusto.lemble@gmail.com>
2018-01-02 14:42:22 -0300

    Add ECRecoveryMock contract

--- test/ECRecovery.test.js
+++ test/ECRecovery.test.js
@@ -1,11 +1,15 @@
-var ECRecovery = artifacts.require('../contracts/ECRecovery.sol');
+var ECRecoveryMock = artifacts.require('../contracts/mocks/ECRecoveryMock.sol');
+var ECRecoveryLib = artifacts.require('../contracts/ECRecovery.sol');
+
 var hashMessage = require('./helpers/hashMessage.js');
 
 contract('ECRecovery', function (accounts) {
   let ecrecovery;
 
   before(async function () {
-    ecrecovery = await ECRecovery.new();
+    const ecRecoveryLib = await ECRecoveryLib.new();
+    ECRecoveryMock.link('ECRecovery', ecRecoveryLib.address);
+    ecrecovery = await ECRecoveryMock.new();
   });
 
   it('recover v0', async function () {
@@ -14,7 +18,8 @@ contract('ECRecovery', function (accounts) {
     let message = '0x7dbaf558b0a1a5dc7a67202117ab143c1d8605a983e4a743bc06fcc03162dc0d'; // web3.sha3('OpenZeppelin')
     // eslint-disable-next-line max-len
     let signature = '0x5d99b6f7f6d1f73d1a26497f2b1c89b24c0993913f86e9a2d02cd69887d9c94f3c880358579d811b21dd1b7fd9bb01c1d81d10e69f0384e675c32b39643be89200';
-    assert.equal(signer, await ecrecovery.recover(message, signature));
+    await ecrecovery.recover(message, signature);
+    assert.equal(signer, await ecrecovery.addrRecovered());
   });
 
   it('recover v1', async function () {
@@ -23,7 +28,8 @@ contract('ECRecovery', function (accounts) {
     let message = '0x7dbaf558b0a1a5dc7a67202117ab143c1d8605a983e4a743bc06fcc03162dc0d'; // web3.sha3('OpenZeppelin')
     // eslint-disable-next-line max-len
     let signature = '0x331fe75a821c982f9127538858900d87d3ec1f9f737338ad67cad133fa48feff48e6fa0c18abc62e42820f05943e47af3e9fbe306ce74d64094bdf1691ee53e001';
-    assert.equal(signer, await ecrecovery.recover(message, signature));
+    await ecrecovery.recover(message, signature);
+    assert.equal(signer, await ecrecovery.addrRecovered());
   });
 
   it('recover using web3.eth.sign()', async function () {
@@ -31,7 +37,8 @@ contract('ECRecovery', function (accounts) {
     const signature = web3.eth.sign(web3.eth.accounts[0], web3.sha3('OpenZeppelin'));
 
     // Recover the signer address form the generated message and signature.
-    assert.equal(web3.eth.accounts[0], await ecrecovery.recover(hashMessage('OpenZeppelin'), signature));
+    await ecrecovery.recover(hashMessage('OpenZeppelin'), signature);
+    assert.equal(web3.eth.accounts[0], await ecrecovery.addrRecovered());
   });
 
   it('recover using web3.eth.sign() should return wrong signer', async function () {
@@ -39,7 +46,8 @@ contract('ECRecovery', function (accounts) {
     const signature = web3.eth.sign(web3.eth.accounts[0], web3.sha3('OpenZeppelin'));
 
     // Recover the signer address form the generated message and wrong signature.
-    assert.notEqual(web3.eth.accounts[0], await ecrecovery.recover(hashMessage('Test'), signature));
+    await ecrecovery.recover(hashMessage('Test'), signature);
+    assert.notEqual(web3.eth.accounts[0], await ecrecovery.addrRecovered());
   });
 
   it('recover should fail when a wrong hash is sent', async function () {
@@ -47,8 +55,7 @@ contract('ECRecovery', function (accounts) {
     let signature = web3.eth.sign(web3.eth.accounts[0], web3.sha3('OpenZeppelin'));
 
     // Recover the signer address form the generated message and wrong signature.
-    assert.equal('0x0000000000000000000000000000000000000000',
-      await ecrecovery.recover(hashMessage('OpenZeppelin').substring(2), signature)
-    );
+    await ecrecovery.recover(hashMessage('OpenZeppelin').substring(2), signature);
+    assert.equal('0x0000000000000000000000000000000000000000', await ecrecovery.addrRecovered());
   });
 });
