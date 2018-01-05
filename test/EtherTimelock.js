511c9d9b267b4781fd46082e0e64a963e3b48be6
nedodn <odennate@gmail.com>
2018-01-04 18:28:06 -0600

    Update EtherTimelock.js

--- test/EtherTimelock.js
+++ test/EtherTimelock.js
@@ -1,51 +1,50 @@
-const BigNumber = web3.BigNumber
+import latestTime from './helpers/latestTime';
+import { increaseTimeTo, duration } from './helpers/increaseTime';
+import { advanceBlock } from './helpers/advanceToBlock';
+import ether from './helpers/ether';
+
+const BigNumber = web3.BigNumber;
 
 require('chai')
   .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(BigNumber))
-  .should()
+  .should();
 
-
-import latestTime from './helpers/latestTime'
-import {increaseTimeTo, duration} from './helpers/increaseTime'
-import ether from './helpers/ether'
-
-const EtherTimelock = artifacts.require('EtherTimelock')
+const EtherTimelock = artifacts.require('EtherTimelock');
 
 contract('EtherTimelock', function ([_, owner, beneficiary]) {
 
-  const FUNDS = ether(1)
+  const FUNDS = ether(1);
 
   beforeEach(async function () {
-    this.releaseTime = latestTime() + duration.years(1)
-    this.timelock = await EtherTimelock.new(beneficiary, this.releaseTime)
+    this.releaseTime = latestTime() + duration.years(1);
+    this.timelock = await EtherTimelock.new(beneficiary, this.releaseTime);
 
-    await this.timelock.sendTransaction({value: FUNDS, from: owner})
-  })
+    await this.timelock.sendTransaction({value: FUNDS, from: owner});
+  });
 
   it('cannot be released before time limit', async function () {
-    await this.timelock.release().should.be.rejected
-  })
+    await this.timelock.release().should.be.rejected;
+  });
 
   it('cannot be released just before time limit', async function () {
-    await increaseTimeTo(this.releaseTime - duration.seconds(3))
-    await this.timelock.release().should.be.rejected
-  })
+    await increaseTimeTo(this.releaseTime - duration.seconds(3));
+    await this.timelock.release().should.be.rejected;
+  });
 
   it('can be released just after limit', async function () {
-    await increaseTimeTo(this.releaseTime + duration.seconds(1))
-    await this.timelock.release().should.be.fulfilled
-  })
+    await increaseTimeTo(this.releaseTime + duration.seconds(1));
+    await this.timelock.release().should.be.fulfilled;
+  });
 
   it('can be released after time limit', async function () {
-    await increaseTimeTo(this.releaseTime + duration.years(1))
-    await this.timelock.release().should.be.fulfilled
-  })
+    await increaseTimeTo(this.releaseTime + duration.years(1));
+    await this.timelock.release().should.be.fulfilled;
+  });
 
   it('cannot be released with zero amount', async function () {
-    await increaseTimeTo(this.releaseTime + duration.years(1))
-    await this.timelock.release().should.be.fulfilled
-    await this.timelock.release().should.be.rejected
-  })
-
-})
+    await increaseTimeTo(this.releaseTime + duration.years(1));
+    await this.timelock.release().should.be.fulfilled;
+    await this.timelock.release().should.be.rejected;
+  });
+});
