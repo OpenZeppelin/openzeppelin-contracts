5b50e99a0df4c197e77f9d195fd0fee785f433ba
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-01-16 15:58:09 -0300

    ERC721 required functionality tests

---
+++ contracts/mocks/ERC721TokenMock.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.18;
+
+import '../token/ERC721Token.sol';
+
+/**
+ * @title ERC721TokenMock
+ * This mock just provides a public mint and burn functions for testing purposes.
+ */
+contract ERC721TokenMock is ERC721Token {
+  function ERC721TokenMock() ERC721Token() public { }
+
+  function publicMint(address _to, uint256 _tokenId) public {
+    super.mint(_to, _tokenId);
+  }
+
+  function publicBurn(uint256 _tokenId) public {
+    super.burn(_tokenId);
+  }
+}
