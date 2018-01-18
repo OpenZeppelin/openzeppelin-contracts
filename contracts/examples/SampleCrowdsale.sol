70fd5e562224a5a9e921acb07db59980f1c4f824
Santiago Palladino <spalladino@gmail.com>
2018-01-18 17:41:51 -0300

    Move token creation outside of crowdsale contract
    Fixes #358

--- contracts/examples/SampleCrowdsale.sol
+++ contracts/examples/SampleCrowdsale.sol
@@ -32,19 +32,14 @@ contract SampleCrowdsaleToken is MintableToken {
  */
 contract SampleCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
-  function SampleCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet) public
+  function SampleCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, MintableToken _token) public
     CappedCrowdsale(_cap)
     FinalizableCrowdsale()
     RefundableCrowdsale(_goal)
-    Crowdsale(_startTime, _endTime, _rate, _wallet)
+    Crowdsale(_startTime, _endTime, _rate, _wallet, _token)
   {
     //As goal needs to be met for a successful crowdsale
     //the value needs to less or equal than a cap which is limit for accepted funds
     require(_goal <= _cap);
   }
-
-  function createTokenContract() internal returns (MintableToken) {
-    return new SampleCrowdsaleToken();
-  }
-
 }
