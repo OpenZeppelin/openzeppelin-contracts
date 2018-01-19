9cc55ef2a55581a359659eb3bb63e15b2cb7c4a7
JU HYEONG PARK <dkdkajej@gmail.com>
2018-01-20 03:45:48 +0900

    use short circuit (#654)

--- contracts/crowdsale/CappedCrowdsale.sol
+++ contracts/crowdsale/CappedCrowdsale.sol
@@ -22,14 +22,14 @@ contract CappedCrowdsale is Crowdsale {
   // @return true if crowdsale event has ended
   function hasEnded() public view returns (bool) {
     bool capReached = weiRaised >= cap;
-    return super.hasEnded() || capReached;
+    return capReached || super.hasEnded();
   }
 
   // overriding Crowdsale#validPurchase to add extra cap logic
   // @return true if investors can buy at the moment
   function validPurchase() internal view returns (bool) {
     bool withinCap = weiRaised.add(msg.value) <= cap;
-    return super.validPurchase() && withinCap;
+    return withinCap && super.validPurchase();
   }
 
 }
