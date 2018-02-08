8a3cfb9649b6759d4771b0c5e20aef8517788074
Alejo Salles <alejosalles@gmail.com>
2018-02-08 15:39:07 -0300

    added Whitelisted implementation and test, passed.

---
+++ contracts/mocks/WhitelistedCrowdsaleImpl.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.18;
+
+//import "../examples/SimpleToken.sol";
+import "../token/ERC20/ERC20.sol";
+import "../crowdsale-refactor/validation/WhitelistedCrowdsale.sol";
+
+
+contract WhitelistedCrowdsaleImpl is WhitelistedCrowdsale {
+
+  function WhitelistedCrowdsaleImpl (
+    uint256 _rate,
+    address _wallet,
+    ERC20 _token
+  ) public
+    Crowdsale(_rate, _wallet, _token)
+    Ownable() //Do I need to call it anyway?!
+  {
+  }
+
+}
---
+++ test/crowdsale-refactor/WhitelistedCrowdsale.js
@@ -0,0 +1,41 @@
+import ether from '../helpers/ether';
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should();
+
+const WhitelistedCrowdsale = artifacts.require('WhitelistedCrowdsaleImpl');
+const SimpleToken = artifacts.require('SimpleToken');
+
+contract('WhitelistedCrowdsale', function([_, wallet, authorized, unauthorized]) {
+
+  var crowdsale;
+  var property;
+  const rate = 1;
+  const value = ether(42);
+  const capital = ether(10000);
+
+  beforeEach(async function() {
+
+    this.token = await SimpleToken.new();
+    this.crowdsale = await WhitelistedCrowdsale.new(rate, wallet, this.token.address);
+    await this.token.transfer(this.crowdsale.address, capital);
+    await this.crowdsale.addToWhitelist(authorized);
+  });
+
+
+  describe('accepting payments', function () {
+
+    it('should accept payments to whitelisted (from whichever buyers)', async function () {
+      await this.crowdsale.buyTokens(authorized, { value: value, from: authorized }).should.be.fulfilled;
+      await this.crowdsale.buyTokens(authorized, { value: value, from: unauthorized }).should.be.fulfilled;
+    });
+
+    it('should reject payments to whitelisted (with whichever buyers)', async function () {
+      await this.crowdsale.send(value).should.be.rejected; // send() goes from _ (accounts[0])
+      await this.crowdsale.buyTokens(unauthorized, { value: value, from: unauthorized }).should.be.rejected;
+      await this.crowdsale.buyTokens(unauthorized, { value: value, from: authorized }).should.be.rejected;
+    });
+
+  });
+});
