d6553fb1dfaea03cdf4b1bc6a3645655c2dc478d
Bruno Barbieri <brunobar79@gmail.com>
2018-02-13 03:02:31 -0500

    update SampleCrowdsale

--- contracts/examples/SampleCrowdsale.sol
+++ contracts/examples/SampleCrowdsale.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.18;
 
 import "../crowdsale/CappedCrowdsale.sol";
 import "../crowdsale/RefundableCrowdsale.sol";
+import "../crowdsale/RefundVault.sol";
 import "../token/ERC20/MintableToken.sol";
 
 
@@ -32,10 +33,10 @@ contract SampleCrowdsaleToken is MintableToken {
  */
 contract SampleCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
-  function SampleCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, MintableToken _token) public
+  function SampleCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, MintableToken _token, RefundVault _vault) public
     CappedCrowdsale(_cap)
     FinalizableCrowdsale()
-    RefundableCrowdsale(_goal)
+    RefundableCrowdsale(_goal, _vault)
     Crowdsale(_startTime, _endTime, _rate, _wallet, _token)
   {
     //As goal needs to be met for a successful crowdsale
