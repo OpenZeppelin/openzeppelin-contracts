af3e1b005f06141c28535a73f821e981d6a4582c
Cédric Walter <cedric.walter@gmail.com>
2018-02-15 22:00:10 +0100

    add overflow check for add and sub int256

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -7,61 +7,71 @@ pragma solidity ^0.4.18;
  */
 library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    int256 constant INT256_MIN = int256((uint256(1) << 255));
+
+    int256 constant INT256_MAX = int256(~((uint256(1) << 255)));
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
-
-  /**
-  * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
-
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
-
-  function sub(int256 a, int256 b) internal pure returns (int256) {
-    return a - b;
-  }
-
-  function add(int256 a, int256 b) internal pure returns (int256) {
-    return a + b;
-  }
-
-  function mul(int256 a, int256 b) internal pure returns (int256) {
-    if (a == 0) {
-      return 0;
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
     }
-    int256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
+
+    /**
+    * @dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+    function sub(int256 a, int256 b) internal pure returns (int256) {
+        assert(!(a > 0 && b > INT256_MIN - a));  // underflow
+        assert(!(a < 0 && b < INT256_MAX - a));  // overflow
+
+        return a - b;
+    }
+
+    function add(int256 a, int256 b) internal pure returns (int256) {
+        assert(!(a > 0 && b > INT256_MAX - a));  // overflow
+        assert(!(a < 0 && b < INT256_MIN - a));  // underflow
+
+        return a + b;
+    }
+
+    function mul(int256 a, int256 b) internal pure returns (int256) {
+        if (a == 0) {
+            return 0;
+        }
+        int256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
 
 
 }
