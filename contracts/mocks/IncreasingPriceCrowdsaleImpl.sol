51c0954e27dcff64cbfe5c4f82bb7484bbf8a0a8
Alejo Salles <alejosalles@gmail.com>
2018-02-16 15:50:13 -0300

    fixed indentation in IncreasingPriceCrowdsaleImpl.sol for linter

--- contracts/mocks/IncreasingPriceCrowdsaleImpl.sol
+++ contracts/mocks/IncreasingPriceCrowdsaleImpl.sol
@@ -3,20 +3,21 @@ pragma solidity ^0.4.18;
 import "../crowdsale/price/IncreasingPriceCrowdsale.sol";
 import "../math/SafeMath.sol";
 
+
 contract IncreasingPriceCrowdsaleImpl is IncreasingPriceCrowdsale {
 
   function IncreasingPriceCrowdsaleImpl (
-      uint256 _startTime,
-      uint256 _endTime,
-      address _wallet,
-      ERC20 _token,
-      uint256 _initialRate,
-      uint256 _finalRate
-    ) public
-      Crowdsale(_initialRate, _wallet, _token)
-      TimedCrowdsale(_startTime, _endTime)
-      IncreasingPriceCrowdsale(_initialRate, _finalRate)
-    {
-    }
+    uint256 _startTime,
+    uint256 _endTime,
+    address _wallet,
+    ERC20 _token,
+    uint256 _initialRate,
+    uint256 _finalRate
+  ) public
+    Crowdsale(_initialRate, _wallet, _token)
+    TimedCrowdsale(_startTime, _endTime)
+    IncreasingPriceCrowdsale(_initialRate, _finalRate)
+  {
+  }
 
 }
