cd15b4174ecf50e889636ba43175cad752cfc3d8
Alejo Salles <alejosalles@gmail.com>
2018-02-17 00:41:02 -0300

    fixed ignoring token.mint return value in MintedCrowdsale.sol

--- contracts/crowdsale/emission/MintedCrowdsale.sol
+++ contracts/crowdsale/emission/MintedCrowdsale.sol
@@ -6,6 +6,6 @@ import "../../token/ERC20/MintableToken.sol";
 contract MintedCrowdsale is Crowdsale {
 
   function _emitTokens(address _beneficiary, uint256 _tokenAmount) internal {
-    MintableToken(token).mint(_beneficiary, _tokenAmount);
+    require(MintableToken(token).mint(_beneficiary, _tokenAmount));
   }
 }
