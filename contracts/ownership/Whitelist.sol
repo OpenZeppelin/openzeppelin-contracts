39853694ee02732c5d9fdf1190b3806c5838c92c
evgeny <evgeny@ampostech.com>
2018-02-19 14:48:53 +0700

    Add functions that allow adding and removing arrays of addresses to the whitelist

--- contracts/ownership/Whitelist.sol
+++ contracts/ownership/Whitelist.sol
@@ -26,27 +26,56 @@ contract Whitelist is Ownable {
   /**
    * @dev add an address to the whitelist
    * @param addr address
+   * @return true if the address was added to the whitelist, false if the address was already in the whitelist 
    */
-  function addToWhitelist(address addr) onlyOwner public returns(bool) {
-    if (whitelist[addr]) {
-      return false; 
+  function addAddressToWhitelist(address addr) onlyOwner public returns(bool success) {
+    if (!whitelist[addr]) {
+      whitelist[addr] = true;
+      WhitelistedAddressAdded(addr);
+      success = true; 
     }
-    whitelist[addr] = true;
-    WhitelistedAddressAdded(addr);
-    return true;
+  }
+
+  /**
+   * @dev add addresses to the whitelist
+   * @param addrs addresses
+   * @return true if at least one address was added to the whitelist, 
+   * false if all addresses were already in the whitelist  
+   */
+  function addAddressesToWhitelist(address[] addrs) onlyOwner public returns(bool success) {
+    for(uint256 i = 0; i < addrs.length; i++) {
+      if (addAddressToWhitelist(addrs[i])) {
+        success = true;
+      }
+    }
   }
 
   /**
    * @dev remove an address from the whitelist
    * @param addr address
+   * @return true if the address was removed from the whitelist, 
+   * false if the address wasn't in the whitelist in the first place 
    */
-  function removeFromWhitelist(address addr) onlyOwner public returns(bool) {
-    if (!whitelist[addr]) {
-      return false;
+  function removeAddressFromWhitelist(address addr) onlyOwner public returns(bool success) {
+    if (whitelist[addr]) {
+      whitelist[addr] = false;
+      WhitelistedAddressRemoved(addr);
+      success = true;
     }
-    whitelist[addr] = false;
-    WhitelistedAddressRemoved(addr);
-    return true;
+  }
+
+  /**
+   * @dev remove addresses from the whitelist
+   * @param addrs addresses
+   * @return true if at least one address was removed from the whitelist, 
+   * false if all addresses weren't in the whitelist in the first place
+   */
+  function removeAddressesFromWhitelist(address[] addrs) onlyOwner public returns(bool success) {
+    for(uint256 i = 0; i < addrs.length; i++) {
+      if (removeAddressFromWhitelist(addrs[i])) {
+        success = true;
+      }
+    }
   }
 
 }
--- test/ownership/Whitelist.test.js
+++ test/ownership/Whitelist.test.js
@@ -1,4 +1,5 @@
 import expectThrow from '../helpers/expectThrow';
+import expectEvent from '../helpers/expectEvent';
 
 const WhitelistMock = artifacts.require('WhitelistMock');
 
@@ -11,44 +12,85 @@ contract('Whitelist', function (accounts) {
 
   const [
     owner,
-    whitelisted,
+    whitelistedAddress1,
+    whitelistedAddress2,
     anyone,
   ] = accounts;
+
+  const whitelistedAddresses = [whitelistedAddress1, whitelistedAddress2];
 
   before(async function () {
     mock = await WhitelistMock.new();
   });
 
   context('in normal conditions', () => {
-    it('should add to whitelist', async function () {
-      await mock.addToWhitelist(whitelisted, { from: owner });
-      const isWhitelisted = await mock.whitelist(whitelisted);
+    it('should add address to the whitelist', async function () {
+      expectEvent.inTransaction(
+        mock.addAddressToWhitelist(whitelistedAddress1, { from: owner }),
+        'WhitelistedAddressAdded'
+      );
+      const isWhitelisted = await mock.whitelist(whitelistedAddress1);
       isWhitelisted.should.be.equal(true);
     });
 
-    it('should remove from whitelist', async function () {
-      await mock.removeFromWhitelist(whitelisted, { from: owner });
-      let isWhitelisted = await mock.whitelist(whitelisted);
+    it('should add addresses to the whitelist', async function () {
+      expectEvent.inTransaction(
+        mock.addAddressesToWhitelist(whitelistedAddresses, { from: owner }),
+        'WhitelistedAddressAdded'
+      );
+      for (let addr of whitelistedAddresses) {
+        const isWhitelisted = await mock.whitelist(addr);
+        isWhitelisted.should.be.equal(true);
+      }
+    });
+
+    it('should not announce WhitelistedAddressAdded event if address is already in the whitelist', async function () {
+      const { logs } = await mock.addAddressToWhitelist(whitelistedAddress1, { from: owner });
+      logs.length.should.be.equal(0);
+    });
+
+    it('should remove address from the whitelist', async function () {
+      expectEvent.inTransaction(
+        mock.removeAddressFromWhitelist(whitelistedAddress1, { from: owner }),
+        'WhitelistedAddressRemoved'
+      );
+      let isWhitelisted = await mock.whitelist(whitelistedAddress1);
       isWhitelisted.should.be.equal(false);
     });
+
+    it('should remove addresses from the the whitelist', async function () {
+      expectEvent.inTransaction(
+        mock.removeAddressesFromWhitelist(whitelistedAddresses, { from: owner }),
+        'WhitelistedAddressRemoved'
+      );
+      for (let addr of whitelistedAddresses) {
+        const isWhitelisted = await mock.whitelist(addr);
+        isWhitelisted.should.be.equal(false);
+      }
+    });
+
+    it('should not announce WhitelistedAddressRemoved event if address is not in the whitelist', async function () {
+      const { logs } = await mock.removeAddressFromWhitelist(whitelistedAddress1, { from: owner });
+      logs.length.should.be.equal(0);
+    });
     
-    it('should allow whitelisted to call #onlyWhitelistedCanDoThis', async () => {
-      await mock.addToWhitelist(whitelisted, { from: owner });
-      await mock.onlyWhitelistedCanDoThis({ from: whitelisted })
+    it('should allow whitelisted address to call #onlyWhitelistedCanDoThis', async () => {
+      await mock.addAddressToWhitelist(whitelistedAddress1, { from: owner });
+      await mock.onlyWhitelistedCanDoThis({ from: whitelistedAddress1 })
         .should.be.fulfilled;
     });
   });
 
   context('in adversarial conditions', () => {
-    it('should not allow "anyone" to add to whitelist', async () => {
+    it('should not allow "anyone" to add to the whitelist', async () => {
       await expectThrow(
-        mock.addToWhitelist(whitelisted, { from: anyone })
+        mock.addAddressToWhitelist(whitelistedAddress1, { from: anyone })
       );
     });
     
-    it('should not allow "anyone" to remove from whitelist', async () => {
+    it('should not allow "anyone" to remove from the whitelist', async () => {
       await expectThrow(
-        mock.removeFromWhitelist(whitelisted, { from: anyone })
+        mock.removeAddressFromWhitelist(whitelistedAddress1, { from: anyone })
       );
     });
     
