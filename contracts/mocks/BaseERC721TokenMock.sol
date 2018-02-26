1192e684c29282613a827266f7a7d2437f2179ba
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-02-26 17:50:27 -0300

    Rename current ERC721 implementation to BaseERC721

--- contracts/mocks/BaseERC721TokenMock.sol
+++ contracts/mocks/BaseERC721TokenMock.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 
-import "../token/ERC721/ERC721Token.sol";
+import "../token/ERC721/BaseERC721Token.sol";
 
 /**
- * @title ERC721TokenMock
+ * @title BaseERC721TokenMock
  * This mock just provides a public mint and burn functions for testing purposes.
  */
-contract ERC721TokenMock is ERC721Token {
-  function ERC721TokenMock() ERC721Token() public { }
+contract BaseERC721TokenMock is BaseERC721Token {
+  function BaseERC721TokenMock() BaseERC721Token() public { }
 
   function mint(address _to, uint256 _tokenId) public {
     super._mint(_to, _tokenId);
--- contracts/token/ERC721/BaseERC721.sol
+++ contracts/token/ERC721/BaseERC721.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.18;
 
 /**
- * @title ERC721 interface
- * @dev see https://github.com/ethereum/eips/issues/721
+ * @title Base ERC721 interface
+ * @dev see https://github.com/ethereum/eips/issues/721 and https://github.com/ethereum/EIPs/pull/841
  */
-contract ERC721 {
+contract BaseERC721 {
   event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
   event Approval(address indexed _owner, address indexed _approved, uint256 _tokenId);
 
