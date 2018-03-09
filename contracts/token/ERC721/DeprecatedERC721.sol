6fbe771c276f83e734256908877c8f9aaa2e1bfc
Santiago Palladino <spalladino@gmail.com>
2018-03-09 15:40:48 -0300

    Remove deprecated implementation
    We only want to keep the interface, for interacting with already deployed contracts

--- contracts/token/ERC721/DeprecatedERC721.sol
+++ contracts/token/ERC721/DeprecatedERC721.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.18;
 
 import "./ERC721.sol";
 
+/**
+ * @title ERC-721 methods shipped in OpenZeppelin v1.7.0, removed in the latest version of the standard
+ * @dev Only use this interface for compatibility with previously deployed contracts
+ * @dev Use ERC721 for interacting with new contracts which are standard-compliant
+ */
 contract DeprecatedERC721 is ERC721 {
   function takeOwnership(uint256 _tokenId) public;
   function transfer(address _to, uint256 _tokenId) public;
