626742e5afb6bda38ebd82ce4d966ea0f97170fc
Santiago Palladino <spalladino@gmail.com>
2018-03-20 13:32:48 -0300

    Add notice to isContract helper on constract constructors

--- contracts/AddressUtils.sol
+++ contracts/AddressUtils.sol
@@ -7,6 +7,8 @@ library AddressUtils {
 
   /**
    * Returns whether there is code in the target address
+   * @dev This function will return false if invoked during the constructor of a contract,
+   *  as the code is not actually created until after the constructor finishes.
    * @param addr address address to check
    * @return whether there is code in the target address
    */
