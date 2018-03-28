021c55dc1f1aa1b8f938fd2a34d1548fa20caf59
Michal Stefanow <mstefanow@gmail.com>
2018-03-27 23:17:32 -0400

    Removing obsolete comment - explicit check in place
    Back in a day we were relying on `SafeMath` doing the job and throwing an error - see the commit - https://github.com/OpenZeppelin/zeppelin-solidity/commit/aad25205cdc54243ccfe82a1bd35c08904d2a152
    
    Now there is explicit check in place `require(_value <= balances[msg.sender]);` - no longer need for comment

--- contracts/token/ERC20/BasicToken.sol
+++ contracts/token/ERC20/BasicToken.sol
@@ -32,7 +32,6 @@ contract BasicToken is ERC20Basic {
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
 
-    // SafeMath.sub will throw if there is not enough balance.
     balances[msg.sender] = balances[msg.sender].sub(_value);
     balances[_to] = balances[_to].add(_value);
     Transfer(msg.sender, _to, _value);
