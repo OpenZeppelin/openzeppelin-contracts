e6be68d252acf745239b2bd1367aa93021f7e0c5
Jy-Liu <junyouliu9@gmail.com>
2018-03-28 10:03:07 +0800

    fix potential overflow on decimals

--- contracts/token/ERC20/DetailedERC20.sol
+++ contracts/token/ERC20/DetailedERC20.sol
@@ -6,9 +6,9 @@ import "./ERC20.sol";
 contract DetailedERC20 is ERC20 {
   string public name;
   string public symbol;
-  uint8 public decimals;
+  uint256 public decimals;
 
-  function DetailedERC20(string _name, string _symbol, uint8 _decimals) public {
+  function DetailedERC20(string _name, string _symbol, uint256 _decimals) public {
     name = _name;
     symbol = _symbol;
     decimals = _decimals;
