44e46ef94f8ae24a39a9691c6db71cc496e09c95
DumasV <dumasv.dev@gmail.com>
2018-04-03 10:11:23 +0200

    fix: update event emission warning
    - Fix event emission warnings for solidity 4.21 after truffle has been
    updated to use this version

--- contracts/ownership/Ownable.sol
+++ contracts/ownership/Ownable.sol
@@ -35,7 +35,7 @@ contract Ownable {
    */
   function transferOwnership(address newOwner) public onlyOwner {
     require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
+    emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
 
