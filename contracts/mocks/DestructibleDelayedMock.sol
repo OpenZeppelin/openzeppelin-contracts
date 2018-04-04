c742e12fcf748273f83c6406cf25154ae732330c
PhABC <ph.castonguay@gmail.com>
2018-04-04 14:18:38 -0400

    call constructor of DestructibleDelay

--- contracts/mocks/DestructibleDelayedMock.sol
+++ contracts/mocks/DestructibleDelayedMock.sol
@@ -5,9 +5,11 @@ import "../lifecycle/DestructibleDelayed.sol";
 // mock class using BasicToken
 contract DestructibleDelayedMock is DestructibleDelayed {
 
-  //Test assumes 2 weeks delay
-  uint256 public SELFDESTRUCTION_DELAY = 2 weeks; 
-
-  function DestructibleDelayedMock() public payable {}
+  function DestructibleDelayedMock(uint256 _destructionDelay) 
+    public 
+    payable 
+    DestructibleDelayed(_destructionDelay)
+  {
+  }
 
  }
