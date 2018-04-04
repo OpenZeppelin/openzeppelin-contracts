a66c5ff390d2cef39d37fc34fe3f965bb248bb61
PhABC <ph.castonguay@gmail.com>
2018-04-04 14:18:54 -0400

    Lint fixes and constructor input

--- test/lifecycle/DestructibleDelayed.test.js
+++ test/lifecycle/DestructibleDelayed.test.js
@@ -1,7 +1,8 @@
-const BigNumber = web3.BigNumber;
 
 import expectEvent from '../helpers/expectEvent';
 import expectThrow from '../helpers/expectThrow';
+
+const BigNumber = web3.BigNumber;
 
 var DestructibleDelayedMock = artifacts.require('DestructibleDelayedMock');
 require('../helpers/transactionMined.js');
@@ -11,21 +12,21 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-const MAXVALUE = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+const twoWeeks = 1209600;
 
 contract('DestructibleDelayed', function ([owner, anyone]) {
   let destructibleDelayed;
 
   describe('Destruction requests', function () {
-
     beforeEach(async function () {
-      destructibleDelayed = await DestructibleDelayedMock.new({ from: owner, value: web3.toWei('10', 'ether') });
+      destructibleDelayed = await DestructibleDelayedMock.new(
+        twoWeeks, { from: owner, value: web3.toWei('10', 'ether') }
+      );
     });
 
     it('owner should be able to create a destroyRequest', async function () {
-
       await expectEvent.inTransaction(
-        destructibleDelayed.destroyRequest({ from:owner }),
+        destructibleDelayed.destroyRequest({ from: owner }),
         'SelfDestructionRequest'
       );
 
@@ -35,24 +36,22 @@ contract('DestructibleDelayed', function ([owner, anyone]) {
     });
 
     it('anyone should not be able to create a destroy request', async function () {
-
-      await expectThrow( 
-        destructibleDelayed.destroyRequest({ from:anyone })
+      await expectThrow(
+        destructibleDelayed.destroyRequest({ from: anyone })
       );
     });
-
     
     describe('once destruction requested,', function () {
       var block;
       var tx;
 
       beforeEach(async function () {
-        tx = await destructibleDelayed.destroyRequest({ from:owner });
-        block = await web3.eth.getBlock(tx['receipt']['blockNumber']);
+        tx = await destructibleDelayed.destroyRequest({ from: owner });
+        block = await web3.eth.getBlock(tx.receipt.blockNumber);
       });
 
       it('destruction time should be set to block.timestamp + destruction_delay', async function () {
-        const destructionTime  = await destructibleDelayed.destructionTime();
+        const destructionTime = await destructibleDelayed.destructionTime();
         const destructionDelay = await destructibleDelayed.SELFDESTRUCTION_DELAY();
 
         destructionTime.should.be.bignumber.equal(
@@ -62,13 +61,13 @@ contract('DestructibleDelayed', function ([owner, anyone]) {
 
       it('should not allow a new #destroyRequest call', async function () {
         await expectThrow(
-          destructibleDelayed.destroyRequest({ from:owner })
+          destructibleDelayed.destroyRequest({ from: owner })
         );
       });
 
       it('destroyRequest can be cancelled by owner', async function () {
         await expectEvent.inTransaction(
-          destructibleDelayed.cancelDestroyRequest({ from:owner }),
+          destructibleDelayed.cancelDestroyRequest({ from: owner }),
           'SelfDestructionRequestCancelled'
         );
 
@@ -78,35 +77,33 @@ contract('DestructibleDelayed', function ([owner, anyone]) {
       });
 
       it('anyone should not be able to cancel a destroy request', async function () {
-
-        await expectThrow( 
-          destructibleDelayed.cancelDestroyRequest({ from:anyone })
+        await expectThrow(
+          destructibleDelayed.cancelDestroyRequest({ from: anyone })
         );
       });
 
       it('should not allow to call #destroy before delay is passed', async function () {
         await expectThrow(
-          destructibleDelayed.destroyRequest({ from:owner })
+          destructibleDelayed.destroyRequest({ from: owner })
         );
       });
 
       describe('once destructionTime is passed', function () {
-
-        beforeEach(async function () { 
+        beforeEach(async function () {
           await web3.currentProvider.send({
-            jsonrpc: "2.0", 
-            method: "evm_increaseTime", 
-            params: [1209601], // 2 weeks + 1 second 
-            id: 0
+            jsonrpc: '2.0',
+            method: 'evm_increaseTime',
+            params: [1209601], // 2 weeks + 1 second
+            id: 0,
           });
         });
 
         it('should allow #destroy after delay is passed', async function () {
-          await destructibleDelayed.destroy(owner, { from:owner }).should.be.fulfilled;
+          await destructibleDelayed.destroy(owner, { from: owner }).should.be.fulfilled;
         });
 
         it('should not allow anyone to call #destroy after delay is passed', async function () {
-          await destructibleDelayed.destroy(anyone, { from:anyone }).should.be.rejected;
+          await destructibleDelayed.destroy(anyone, { from: anyone }).should.be.rejected;
         });
 
         it('should send balance to owner after destruction', async function () {
@@ -124,7 +121,6 @@ contract('DestructibleDelayed', function ([owner, anyone]) {
 
           assert.isTrue(newBalance.greaterThan(initBalance));
         });
-
       });
     });
   });
