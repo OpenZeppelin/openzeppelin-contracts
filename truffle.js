45b06c6bf2a4a2e577086cccda88d59aaeb8984d
Francisco Giordano <frangio.1@gmail.com>
2018-04-05 13:06:18 -0300

    fix pr review comments

--- truffle.js
+++ truffle.js
@@ -21,7 +21,7 @@ module.exports = {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // eslint-disable-line camelcase
+      network_id: '*', // eslint-disable-line camelcase
     },
     ropsten: {
       provider: ropstenProvider,
@@ -34,14 +34,9 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
-    "ganache-cli": {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*' // eslint-disable-line camelcase
-    },
     ganache: {
       host: 'localhost',
-      port: 7545,
+      port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
   },
