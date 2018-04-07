a1d71fa546767e0c2d10f4e73fde1cc88a3e89fd
HU Yao-Chieh <yhuag@connect.ust.hk>
2018-04-08 07:41:41 +0800

    Update Grantable.sol to meet lint requirements

--- contracts/ownership/Grantable.sol
+++ contracts/ownership/Grantable.sol
@@ -1,47 +1,48 @@
 pragma solidity ^0.4.13;
 
+
 /**
  * @title Grantable
  * @dev The Grantable contract has a grants mapping, and provides basic grants control
  * functions, this simplifies the implementation of "access granting".
  */
 contract Grantable {
-    mapping(address => bool) public grants;
-    
-    event AccessGranted(address indexed grantee);
-    event AccessDeprivation(address indexed grantee);
-    
-    /**
-    * @dev The Grantable constructor assign grants to the sender account
-    */
-    function Grantable() public {
-        grants[msg.sender] = true;
-        AccessGranted(msg.sender);
-    }
-    
-    /**
-    * @dev Throws if called by any account without grants.
-    */
-    modifier onlyGranted(){
-        require(grants[msg.sender]);
-        _;
-    }
-    
-    /**
-    * @dev Allows only the account granted with access to assgin grants to new grantee.
-    * @param _grantee The address to grant access to.
-    */
-    function assignGrants(address _grantee) public onlyGranted {
-        grants[_grantee] = true;
-        AccessGranted(_grantee);
-    }
-    
-    /**
-    * @dev Allows only the account granted with access to deprive grants from a grantee.
-    * @param _grantee The address to deprive grants to.
-    */
-    function depriveGrants(address _grantee) public onlyGranted {
-        grants[_grantee] = false;
-        AccessDeprivation(_grantee);
-    }
+  mapping(address => bool) public grants;
+  
+  event AccessGranted(address indexed grantee);
+  event AccessDeprivation(address indexed grantee);
+  
+  /**
+  * @dev The Grantable constructor assign grants to the sender account
+  */
+  function Grantable() public {
+    grants[msg.sender] = true;
+    AccessGranted(msg.sender);
+  }
+  
+  /**
+  * @dev Throws if called by any account without grants.
+  */
+  modifier onlyGranted(){
+    require(grants[msg.sender]);
+    _;
+  }
+  
+  /**
+  * @dev Allows only the account granted with access to assgin grants to new grantee.
+  * @param _grantee The address to grant access to.
+  */
+  function assignGrants(address _grantee) public onlyGranted {
+    grants[_grantee] = true;
+    AccessGranted(_grantee);
+  }
+  
+  /**
+  * @dev Allows only the account granted with access to deprive grants from a grantee.
+  * @param _grantee The address to deprive grants to.
+  */
+  function depriveGrants(address _grantee) public onlyGranted {
+    grants[_grantee] = false;
+    AccessDeprivation(_grantee);
+  }
 }
