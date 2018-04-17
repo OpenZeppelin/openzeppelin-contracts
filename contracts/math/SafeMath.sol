c63b203c1d1800c9a8b5f51f0b444187fdc6c185
DeltaBalances <30391324+DeltaBalances@users.noreply.github.com>
2018-04-18 00:16:30 +0200

    Small SafeMath.sol gas improvements (add & mul). (#894)

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -10,11 +10,11 @@ library SafeMath {
   /**
   * @dev Multiplies two numbers, throws on overflow.
   */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
     if (a == 0) {
       return 0;
     }
-    uint256 c = a * b;
+    c = a * b;
     assert(c / a == b);
     return c;
   }
@@ -40,8 +40,8 @@ library SafeMath {
   /**
   * @dev Adds two numbers, throws on overflow.
   */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
     assert(c >= a);
     return c;
   }
