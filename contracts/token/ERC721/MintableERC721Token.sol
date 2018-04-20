2eac2a79b76abd31df612d1760f498a840fb43e4
Francisco Giordano <frangio.1@gmail.com>
2018-04-20 11:21:53 -0300

    add mintable erc721 token

---
+++ contracts/token/ERC721/MintableERC721Token.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.21;
+
+import "./ERC721Token.sol";
+import "../../ownership/Ownable.sol";
+
+/**
+ * @title Mintable ERC721 Token
+ */
+contract MintableERC721Token is Migratable, Ownable, ERC721Token {
+  function initialize(address _sender, string _name, string _symbol) isInitializer("MintableERC721Token", "0") {
+    Ownable.initialize(_sender);
+    ERC721Token.initialize(_sender, _name, _symbol);
+  }
+
+  function mint(address _to, uint256 _tokenId) onlyOwner public {
+    _mint(_to, _tokenId);
+  }
+}
