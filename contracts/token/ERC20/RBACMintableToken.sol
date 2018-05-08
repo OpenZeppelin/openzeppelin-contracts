180d9dd3bd40e2553fb70d05e3158b4e3f002544
vittominacori <vittoriominacori@gmail.com>
2018-05-08 14:28:30 +0200

    made code shared between mintable and rbacmintable

--- contracts/token/ERC20/RBACMintableToken.sol
+++ contracts/token/ERC20/RBACMintableToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.21;
 
-import "./StandardToken.sol";
+import "./MintableToken.sol";
 import "../../ownership/rbac/RBACWithAdmin.sol";
 
 
@@ -9,54 +9,19 @@ import "../../ownership/rbac/RBACWithAdmin.sol";
  * @author Vittorio Minacori (@vittominacori)
  * @dev Simple ERC20 Mintable Token, with RBAC minter permissions
  */
-contract RBACMintableToken is StandardToken, RBACWithAdmin {
-  event Mint(address indexed to, uint256 amount);
-  event MintFinished();
-
+contract RBACMintableToken is MintableToken, RBACWithAdmin {
   /**
    * A constant role name for indicating minters.
    */
   string public constant ROLE_MINTER = "minter";
 
-  bool public mintingFinished = false;
-
-
-  modifier canMint() {
-    require(!mintingFinished);
-    _;
-  }
-
-  /**
-   * @dev modifier to scope access to minters
-   * // reverts
-   */
-  modifier onlyMinter()
-  {
+  modifier hasMintPermission() {
     checkRole(msg.sender, ROLE_MINTER);
     _;
   }
 
-  /**
-   * @dev Function to mint tokens
-   * @param _to The address that will receive the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) onlyMinter canMint public returns (bool) {
-    totalSupply_ = totalSupply_.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    emit Mint(_to, _amount);
-    emit Transfer(address(0), _to, _amount);
-    return true;
-  }
-
-  /**
-   * @dev Function to stop minting new tokens.
-   * @return True if the operation was successful.
-   */
-  function finishMinting() onlyAdmin canMint public returns (bool) {
-    mintingFinished = true;
-    emit MintFinished();
-    return true;
+  modifier hasFinishMintingPermission() {
+    checkRole(msg.sender, ROLE_ADMIN);
+    _;
   }
 }
