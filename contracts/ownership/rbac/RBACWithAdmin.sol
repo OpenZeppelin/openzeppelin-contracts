c118ea86db4f746be33e24d35056cedc4910d6df
Matt Condon <mattgcondon@gmail.com>
2018-05-08 16:57:34 -0700

    fix: add comment about rbacwithadmin being rather powerful

--- contracts/ownership/rbac/RBACWithAdmin.sol
+++ contracts/ownership/rbac/RBACWithAdmin.sol
@@ -8,6 +8,12 @@ import "./RBAC.sol";
  * @author Matt Condon (@Shrugs)
  * @dev It's recommended that you define constants in the contract,
  * @dev like ROLE_ADMIN below, to avoid typos.
+ * @dev
+ * @dev NOTE: RBACWithAdmin is probably too expansive and powerful for your
+ * @dev  application; and admin is actually able to change any address to any role
+ * @dev  which is a very large API surface. It's recommended that you follow a strategy
+ * @dev  of strictly defining the abilities of your roles
+ * @dev  and the API-surface of your contract.
  */
 contract RBACWithAdmin is RBAC {
   /**
