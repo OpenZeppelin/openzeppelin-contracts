5b988361300a79dd9ec9a3d7f6eb42a145284e31
John Shutt <john.d.shutt@gmail.com>
2018-05-08 11:56:55 -0700

    update all contracts to solidity ^0.4.23

--- contracts/crowdsale/emission/MintedCrowdsale.sol
+++ contracts/crowdsale/emission/MintedCrowdsale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import "../Crowdsale.sol";
 import "../../token/ERC20/MintableToken.sol";
@@ -7,7 +7,7 @@ import "../../token/ERC20/MintableToken.sol";
 /**
  * @title MintedCrowdsale
  * @dev Extension of Crowdsale contract whose tokens are minted in each purchase.
- * Token ownership should be transferred to MintedCrowdsale for minting. 
+ * Token ownership should be transferred to MintedCrowdsale for minting.
  */
 contract MintedCrowdsale is Crowdsale {
 
--- contracts/token/ERC721/ERC721BasicToken.sol
+++ contracts/token/ERC721/ERC721BasicToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.23;
 
 import "./ERC721Basic.sol";
 import "./ERC721Receiver.sol";
