579446d5d99655028fb707309dbd4b6d6a47f09e
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-05-19 01:02:12 -0300

    Fixed MintableToken's initialize not being called.

--- contracts/token/ERC20/DetailedMintableToken.sol
+++ contracts/token/ERC20/DetailedMintableToken.sol
@@ -7,6 +7,6 @@ import "./MintableToken.sol";
 contract DetailedMintableToken is Migratable, DetailedERC20, MintableToken {
   function initialize(address _sender, string _name, string _symbol, uint8 _decimals) isInitializer("DetailedMintableToken", "1.9.0-beta") {
     DetailedERC20.initialize(_name, _symbol, _decimals);
-    Ownable.initialize(_sender);
+    MintableToken.initialize(_sender);
   }
 }
