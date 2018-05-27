00391fc483176cbcc31bcd6639267c9a177d0bb0
vittominacori <vittoriominacori@gmail.com>
2018-05-27 17:34:23 +0200

    added burnable test in example

--- test/examples/SimpleERC721Token.test.js
+++ test/examples/SimpleERC721Token.test.js
@@ -2,6 +2,7 @@ import shouldBehaveLikeERC721BasicToken from '../token/ERC721/ERC721BasicToken.b
 import shouldBehaveLikeERC721Token from '../token/ERC721/ERC721Token.behaviour';
 import shouldBehaveLikeMintableERC721Token from '../token/ERC721/ERC721Mint.behaviour';
 import shouldBehaveLikeBurnableERC721Token from '../token/ERC721/ERC721Burn.behaviour';
+import assertRevert from '../helpers/assertRevert';
 
 const BigNumber = web3.BigNumber;
 const SimpleERC721Token = artifacts.require('SimpleERC721Token.sol');
@@ -26,4 +27,19 @@ contract('SimpleERC721Token', function (accounts) {
   shouldBehaveLikeERC721Token(accounts);
   shouldBehaveLikeMintableERC721Token([creator, beneficiary]);
   shouldBehaveLikeBurnableERC721Token([creator, beneficiary, anotherAccount]);
+
+  context('like a BurnableERC721Token', function () {
+    const minter = creator;
+    const tokenId = 1;
+
+    beforeEach(async function () {
+      await this.token.mint(beneficiary, tokenId, { from: minter });
+    });
+
+    describe('when sender is not token owner', function () {
+      it('reverts', async function () {
+        await assertRevert(this.token.burn(tokenId, { from: anotherAccount }));
+      });
+    });
+  });
 });
