286e542b839493d7b3c834bafcac7fa82d3ee719
B1nj0y <idegorepl@gmail.com>
2018-05-27 23:13:51 +0800

    add check to second parameter of mul function of SafeMath

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -11,7 +11,7 @@ library SafeMath {
   * @dev Multiplies two numbers, throws on overflow.
   */
   function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
+    if (a == 0 || b == 0) {
       return 0;
     }
     c = a * b;
