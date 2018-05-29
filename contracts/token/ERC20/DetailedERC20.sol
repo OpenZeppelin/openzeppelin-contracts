e19b94821df1da3a8caa76d71c0f0b225300f65f
vittominacori <vittoriominacori@gmail.com>
2018-05-29 19:23:01 +0200

    updated comment to be more clear

--- contracts/token/ERC20/DetailedERC20.sol
+++ contracts/token/ERC20/DetailedERC20.sol
@@ -5,10 +5,9 @@ import "./ERC20.sol";
 
 /**
  * @title DetailedERC20 token
- * @dev If you are building a UI, be careful on decimals amount your users can insert
- * @dev mostly if you want to use token with a Crowdsale
- * @dev Remember that on Ethereum the smaller unit is wei.
- * @dev Issue: * https://github.com/OpenZeppelin/openzeppelin-solidity/issues/953
+ * @dev The decimals are only for visualization purposes.
+ * All the operations are done using the smallest and indivisible token unit,
+ * just as on Ethereum all the operations are done in wei.
  */
 contract DetailedERC20 is ERC20 {
   string public name;
