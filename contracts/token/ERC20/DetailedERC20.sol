91f04f250828bc2bd70a15226b0ba0151ef3b17b
vittominacori <vittoriominacori@gmail.com>
2018-05-29 18:49:24 +0200

    add a comment on detailed erc20 token #953

--- contracts/token/ERC20/DetailedERC20.sol
+++ contracts/token/ERC20/DetailedERC20.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.23;
 import "./ERC20.sol";
 
 
+/**
+ * @title DetailedERC20 token
+ * @dev If you are building a UI, be careful on decimals amount your users can insert
+ * @dev mostly if you want to use token with a Crowdsale
+ * @dev Remember that on Ethereum the smaller unit is wei.
+ * @dev Issue: * https://github.com/OpenZeppelin/openzeppelin-solidity/issues/953
+ */
 contract DetailedERC20 is ERC20 {
   string public name;
   string public symbol;
