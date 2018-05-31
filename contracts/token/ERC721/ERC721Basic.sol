be7d1d732ee30e965f5aaf313314473d3c5f2de1
Matt Condon <mattgcondon@gmail.com>
2018-05-30 17:04:54 -0700

    feat: implement ERC721's ERC165

--- contracts/token/ERC721/ERC721Basic.sol
+++ contracts/token/ERC721/ERC721Basic.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.23;
 
+import "../../SupportsInterfaceWithLookup.sol";
+
 
 /**
  * @title ERC721 Non-Fungible Token Standard basic interface
  * @dev see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
-contract ERC721Basic {
+contract ERC721Basic is SupportsInterfaceWithLookup {
   event Transfer(
     address indexed _from,
     address indexed _to,
@@ -21,6 +23,34 @@ contract ERC721Basic {
     address indexed _operator,
     bool _approved
   );
+
+  bytes4 constant InterfaceId_ERC721 = 0x80ac58cd;
+  /*
+   * 0x80ac58cd ===
+   *   bytes4(keccak256('balanceOf(address)')) ^
+   *   bytes4(keccak256('ownerOf(uint256)')) ^
+   *   bytes4(keccak256('approve(address,uint256)')) ^
+   *   bytes4(keccak256('getApproved(uint256)')) ^
+   *   bytes4(keccak256('setApprovalForAll(address,bool)')) ^
+   *   bytes4(keccak256('isApprovedForAll(address,address)')) ^
+   *   bytes4(keccak256('transferFrom(address,address,uint256)')) ^
+   *   bytes4(keccak256('safeTransferFrom(address,address,uint256)')) ^
+   *   bytes4(keccak256('safeTransferFrom(address,address,uint256,bytes)'))
+   */
+
+  bytes4 public constant InterfaceId_ERC721_Exists = 0x4f558e79;
+  /*
+   * 0x4f558e79 ===
+   *   bytes4(keccak256('exists(uint256)'))
+   */
+
+  constructor ()
+    public
+  {
+    // register the supported interfaces to conform to ERC721 via ERC165
+    _registerInterface(InterfaceId_ERC721);
+    _registerInterface(InterfaceId_ERC721_Exists);
+  }
 
   function balanceOf(address _owner) public view returns (uint256 _balance);
   function ownerOf(uint256 _tokenId) public view returns (address _owner);
