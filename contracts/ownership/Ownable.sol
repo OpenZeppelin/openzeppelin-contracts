9c061edc532a249111318c0432e52ad23107e2f2
Leo Arias <yo@elopio.net>
2018-06-08 05:14:08 +0000

    Apply the suggestion by @shrugs

--- contracts/ownership/Ownable.sol
+++ contracts/ownership/Ownable.sol
@@ -43,8 +43,8 @@ contract Ownable {
   /**
    * @dev Allows the current owner to relinquish control of the contract.
    * @notice Renouncing to ownership will leave the contract without an owner.
-   * It will not be possible to call anymore the functions with the onlyOwner
-   * modifier.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
    */
   function renounceOwnership() public onlyOwner {
     emit OwnershipRenounced(owner);
