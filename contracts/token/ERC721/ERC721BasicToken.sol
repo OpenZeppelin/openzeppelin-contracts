259b9da3e6081c3c14f449cad83004d64c0b9cb7
Matt Condon <mattgcondon@gmail.com>
2018-06-08 14:50:12 -0700

    add 165 to 721 (#972)
    * make _tokenId indexed in Transfer and Approval events
    
    via: https://github.com/ethereum/EIPs/pull/1124/files
    
    * fix: make name() and symbol() external instead of public
    
    * feat: implement ERC721's ERC165
    
    * feat: erc165 tests
    
    * fix: don't use chai-as-promised in direct await
    
    * fix: reorganize to /introspection
    
    * feat: abstract all erc165 tests to a behavior
    
    * feat: disallow registering 0xffffffff

--- contracts/token/ERC721/ERC721BasicToken.sol
+++ contracts/token/ERC721/ERC721BasicToken.sol
@@ -16,7 +16,7 @@ contract ERC721BasicToken is ERC721Basic {
 
   // Equals to `bytes4(keccak256("onERC721Received(address,uint256,bytes)"))`
   // which can be also obtained as `ERC721Receiver(0).onERC721Received.selector`
-  bytes4 constant ERC721_RECEIVED = 0xf0b9e5ba;
+  bytes4 private constant ERC721_RECEIVED = 0xf0b9e5ba;
 
   // Mapping from token ID to owner
   mapping (uint256 => address) internal tokenOwner;
