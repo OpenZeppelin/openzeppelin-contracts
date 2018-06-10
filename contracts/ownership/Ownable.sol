72792a724db85e64f3a5cfbc8347bda5c33080c2
Leo Arias <leo@zeppelin.solutions>
2018-06-10 13:39:54 -0600

    ownable: add a notice on renounceOwnership (#937)
    * ownable: add a notice on renounceOwnership
    
    * Apply the suggestion by @shrugs

--- contracts/ownership/Ownable.sol
+++ contracts/ownership/Ownable.sol
@@ -35,6 +35,9 @@ contract Ownable {
 
   /**
    * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
    */
   function renounceOwnership() public onlyOwner {
     emit OwnershipRenounced(owner);
