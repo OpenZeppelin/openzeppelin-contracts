2f3b47d877276fb1a17a1e2e3c7ec118b1344e80
Andrei Karpushonak <karpushonak@gmail.com>
2018-06-11 13:05:55 +0200

    Increased code coverage #16

--- test/oracle/Oracle.test.js
+++ test/oracle/Oracle.test.js
@@ -11,19 +11,52 @@ const Oracle = artifacts.require('Oracle');
 
 contract('Oracle', function ([owner, oracle, other]) {
   const amount = web3.toWei(1.0, 'ether');
+  const amountOfUpdates = 3;
+  // 24h
+  const minFrequencyInSeconds = 60 * 60 * 24;
+  // 25h
+  const maxFrequencyInSeconds = 60 * 60 * 24 + 60 * 60;
 
   beforeEach(async function () {
-    const amountOfUpdates = 3;
-    // 24h
-    const minFrequencyInSeconds = 60 * 60 * 24;
-    // 25h
-    const maxFrequencyInSeconds = 60 * 60 * 24 + 60 * 60;
-
     this.contract = await Oracle.new(oracle, amountOfUpdates, minFrequencyInSeconds, maxFrequencyInSeconds, amount);
   });
 
-  it('should reject activation if not sufficiently funded', async function () {
+  it('should reject contruction if amount of update is wrong', async function () {
+    const wrongAmountOfUpdates = 0;
+    await Oracle.new(oracle, wrongAmountOfUpdates, minFrequencyInSeconds, maxFrequencyInSeconds, amount).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject contruction if min frequency is wrong', async function () {
+    const wrongMinFrequency = 0;
+    await Oracle.new(oracle, amountOfUpdates, wrongMinFrequency, maxFrequencyInSeconds, amount).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject contruction if max frequency is wrong', async function () {
+    const wrongMaxFrequency = 0;
+    await Oracle.new(oracle, amountOfUpdates, minFrequencyInSeconds, wrongMaxFrequency, amount).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject contruction if reward is wrong', async function () {
+    const wrongReward = -1;
+    await Oracle.new(oracle, amountOfUpdates, minFrequencyInSeconds, maxFrequencyInSeconds, wrongReward).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject contruction if max frequency is less than a minimum', async function () {
+    const minFrequency = 10;
+    const maxFrequency = 1;
+    await Oracle.new(oracle, amountOfUpdates, minFrequency, maxFrequency, amount).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject canceling the reward if contract was not activated', async function () {
+    await this.contract.cancelReward({ from: oracle }).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject activation if not sufficiently funded during the activation', async function () {
     await this.contract.activate().should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject claiming the reward if contract was not activated', async function () {
+    await this.contract.claimReward({ from: oracle }).should.be.rejectedWith(EVMThrow);
   });
 
   it('should accept activation if sufficiently funded', async function () {
@@ -45,6 +78,23 @@ contract('Oracle', function ([owner, oracle, other]) {
 
     const balance = web3.eth.getBalance(this.contract.address);
     balance.should.be.bignumber.equal(amount);
+  });
+
+  it('should reject updating the data if updated too frequently', async function () {
+    const valueOne = 1;
+
+    await web3.eth.sendTransaction({ from: owner, to: this.contract.address, value: amount });
+    await this.contract.activate({ from: owner });
+    await this.contract.addOracleData(valueOne, { from: oracle }).should.be.rejectedWith(EVMThrow);
+  });
+
+  it('should reject updating the data if updated too late', async function () {
+    const valueOne = 1;
+
+    await web3.eth.sendTransaction({ from: owner, to: this.contract.address, value: amount });
+    await this.contract.activate({ from: owner });
+    increaseTime(2 * 60 * 60 * 24 + 1);
+    await this.contract.addOracleData(valueOne, { from: oracle }).should.be.rejectedWith(EVMThrow);
   });
 
   it('should accept updating the data only by the oracle and according to the frequency rules', async function () {
