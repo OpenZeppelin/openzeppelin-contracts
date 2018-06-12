260899f5239473af2f109d695c86c617b7697d1b
Alex Beregszaszi <alex@rtfs.hu>
2018-06-12 20:16:29 +0100

    Do not use implicit type conversion for address literals
    Uses the explicit address(0) for the zero address

--- contracts/examples/SimpleSavingsWallet.sol
+++ contracts/examples/SimpleSavingsWallet.sol
@@ -32,7 +32,7 @@ contract SimpleSavingsWallet is Heritable {
    * @dev wallet can send funds
    */
   function sendTo(address payee, uint256 amount) public onlyOwner {
-    require(payee != 0 && payee != address(this));
+    require(payee != address(0) && payee != address(this));
     require(amount > 0);
     payee.transfer(amount);
     emit Sent(payee, amount, address(this).balance);
