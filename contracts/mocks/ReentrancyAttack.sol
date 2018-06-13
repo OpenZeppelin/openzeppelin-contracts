eee5976312fc1dcde1a06cfee0aeaa14e0c556d0
chriseth <chris@ethereum.org>
2018-06-13 18:12:42 +0200

    Update the use of call() with selector only.

--- contracts/mocks/ReentrancyAttack.sol
+++ contracts/mocks/ReentrancyAttack.sol
@@ -5,7 +5,7 @@ contract ReentrancyAttack {
 
   function callSender(bytes4 data) public {
     // solium-disable-next-line security/no-low-level-calls
-    require(msg.sender.call(data));
+    require(msg.sender.call(abi.encodeWithSelector(data)));
   }
 
 }
