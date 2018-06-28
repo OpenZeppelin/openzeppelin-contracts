0ba583eed5c2a554fc822c4737682bf6a9dab30f
Angelica Fulcher <angelica.fulcher@validitylabs.org>
2018-06-28 12:18:18 +0200

    Fix indentation

--- contracts/token/ERC20/MultiSendToken.sol
+++ contracts/token/ERC20/MultiSendToken.sol
@@ -8,18 +8,18 @@ import "./StandardToken.sol";
  */
 contract MultiSendToken is StandardToken {
 
-    /**
-     * @dev Transfers specific amounts of tokens to specified addresses.
-     * @param _beneficiaries The addresses to transfer to.
-     * @param _values The amount of tokens to be transferred per address.
-     */
-    function multiSend(address[] _beneficiaries, uint256[] _values) public {
-        require(_beneficiaries.length == _values.length);
+  /**
+   * @dev Transfers specific amounts of tokens to specified addresses.
+   * @param _beneficiaries The addresses to transfer to.
+   * @param _values The amount of tokens to be transferred per address.
+   */
+  function multiSend(address[] _beneficiaries, uint256[] _values) public {
+    require(_beneficiaries.length == _values.length);
 
-        uint256 length = _beneficiaries.length;
+    uint256 length = _beneficiaries.length;
 
-        for (uint256 i = 0; i < length; i++) {
-            transfer(_beneficiaries[i], _values[i]);
-        }
+    for (uint256 i = 0; i < length; i++) {
+      transfer(_beneficiaries[i], _values[i]);
     }
+  }
 }
