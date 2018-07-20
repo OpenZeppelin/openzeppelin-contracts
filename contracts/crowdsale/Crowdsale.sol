45d69439134b0f0791fe7a231b83b945387b86c4
Leo Arias <leo@zeppelin.solutions>
2018-07-19 18:48:20 -0600

    Fix typos (#1082)

--- contracts/crowdsale/Crowdsale.sol
+++ contracts/crowdsale/Crowdsale.sol
@@ -12,7 +12,7 @@ import "../token/ERC20/SafeERC20.sol";
  * such functionality in its most fundamental form and can be extended to provide additional
  * functionality and/or custom behavior.
  * The external interface represents the basic interface for purchasing tokens, and conform
- * the base architecture for crowdsales. They are *not* intended to be modified / overriden.
+ * the base architecture for crowdsales. They are *not* intended to be modified / overridden.
  * The internal interface conforms the extensible and modifiable surface of crowdsales. Override
  * the methods to add functionality. Consider using 'super' where appropriate to concatenate
  * behavior.
