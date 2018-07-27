c1b69667243d42c80bb34c71dd0d7be67e2d8985
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-07-27 11:40:39 +0200

    Improve MigratableERC20 inline documentation

--- contracts/token/ERC20/MigratableERC20.sol
+++ contracts/token/ERC20/MigratableERC20.sol
@@ -6,12 +6,20 @@ import "./SafeERC20.sol";
 
 /**
  * @title MigratableERC20
- * @dev This strategy carries out an optional migration of the token balances. This migration is performed and paid for
- * @dev by the token holders. The new token contract starts with no initial supply and no balances. The only way to
- * @dev "mint" the new tokens is for users to "turn in" their old ones. This is done by first approving the amount they
- * @dev want to migrate via `ERC20.approve(newTokenAddress, amountToMigrate)` and then calling a function of the new
- * @dev token called `migrateTokens`. The old tokens are sent to a burn address, and the holder receives an equal amount
- * @dev in the new contract.
+ * @dev This contract provides a logic to migrate an ERC20 token from one contract to another.
+ * The strategy provided carries out an optional migration of the token balances. This migration is performed and paid
+ * for by the token holders. The new token contract starts with no initial supply and no balances. The only way to
+ * "mint" the new tokens is for users to "turn in" their old ones. This is done by first approving the amount they
+ * want to migrate via `ERC20.approve(newTokenAddress, amountToMigrate)` and then calling a function of the new
+ * token called `migrateTokens`. The old tokens are sent to a burn address, and the holder receives an equal amount
+ * in the new contract.
+ * This contract does not force any custom minting logic. Thus, it has to be used in combination with a contract that
+ * provides one. For instance, you can see MigratedERC20Mock.sol as an example.
+ *
+ * Although this contract can be used in many different scenarios, the main motivation was to provide a way of how an
+ * ERC20 token can be migrated to an upgradeable version of it using ZeppelinOS. To read more about how this can be
+ * done using this implementation, please follow the official documentation site of ZeppelinOS:
+ * https://docs.zeppelinos.org/docs/erc20_onboarding.html
  */
 contract MigratableERC20 {
   using SafeERC20 for ERC20;
@@ -40,7 +48,7 @@ contract MigratableERC20 {
   }
 
   /**
-   * @dev Migrates the a given amount of old-token balance to the new token contract
+   * @dev Migrates a given amount of old-token balance to the new token contract
    * @dev This function will burn a given amount of tokens from the old contract and mint the same amount in the new one
    * @param _amount uint256 representing the amount of tokens to be migrated
    */
