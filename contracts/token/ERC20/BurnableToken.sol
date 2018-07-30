10d297ef7f96539362fbf56b179b902796c91850
Doug Crescenzi <doug@upstate.agency>
2018-07-30 07:35:24 -0400

    Fixed linting warning in BurnableToken

--- contracts/token/ERC20/BurnableToken.sol
+++ contracts/token/ERC20/BurnableToken.sol
@@ -19,17 +19,6 @@ contract BurnableToken is StandardToken {
     _burn(msg.sender, _value);
   }
 
-  function _burn(address _who, uint256 _value) internal {
-    require(_value <= balances[_who]);
-    // no need to require value <= totalSupply, since that would imply the
-    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
-
-    balances[_who] = balances[_who].sub(_value);
-    totalSupply_ = totalSupply_.sub(_value);
-    emit Burn(_who, _value);
-    emit Transfer(_who, address(0), _value);
-  }
-
   /**
    * @dev Burns a specific amount of tokens from the target address and decrements allowance
    * @param _from address The address which you want to send tokens from
@@ -41,5 +30,16 @@ contract BurnableToken is StandardToken {
     // this function needs to emit an event with the updated approval.
     allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
     _burn(_from, _value);
+  }
+
+  function _burn(address _who, uint256 _value) internal {
+    require(_value <= balances[_who]);
+    // no need to require value <= totalSupply, since that would imply the
+    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+    balances[_who] = balances[_who].sub(_value);
+    totalSupply_ = totalSupply_.sub(_value);
+    emit Burn(_who, _value);
+    emit Transfer(_who, address(0), _value);
   }
 }
