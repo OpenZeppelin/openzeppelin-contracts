8ac9ed28539f2f8471b4cfddb936f9beb9144a8a
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-08-02 18:30:23 -0300

    Added back burnFrom tests.

--- test/token/ERC20/BurnableToken.behaviour.js
+++ test/token/ERC20/BurnableToken.behaviour.js
@@ -8,8 +8,8 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-function shouldBehaveLikeBurnableToken (owner, initialBalance) {
-  describe('as a basic burnable token', function () {
+function shouldBehaveLikeBurnableToken (owner, initialBalance, [burner]) {
+  describe('burn', function () {
     describe('when the given amount is not greater than balance of the sender', function () {
       const amount = 100;
 
@@ -41,6 +41,57 @@ function shouldBehaveLikeBurnableToken (owner, initialBalance) {
 
       it('reverts', async function () {
         await assertRevert(this.token.burn(amount, { from: owner }));
+      });
+    });
+  });
+
+  describe('burnFrom', function () {
+    describe('on success', function () {
+      const amount = 100;
+
+      beforeEach(async function () {
+        await this.token.approve(burner, 300, { from: owner });
+        const { logs } = await this.token.burnFrom(owner, amount, { from: burner });
+        this.logs = logs;
+      });
+
+      it('burns the requested amount', async function () {
+        const balance = await this.token.balanceOf(owner);
+        balance.should.be.bignumber.equal(initialBalance - amount);
+      });
+
+      it('decrements allowance', async function () {
+        const allowance = await this.token.allowance(owner, burner);
+        allowance.should.be.bignumber.equal(200);
+      });
+
+      it('emits a burn event', async function () {
+        const event = await inLogs(this.logs, 'Burn');
+        event.args.burner.should.eq(owner);
+        event.args.value.should.be.bignumber.equal(amount);
+      });
+
+      it('emits a transfer event', async function () {
+        const event = await inLogs(this.logs, 'Transfer');
+        event.args.from.should.eq(owner);
+        event.args.to.should.eq(ZERO_ADDRESS);
+        event.args.value.should.be.bignumber.equal(amount);
+      });
+    });
+
+    describe('when the given amount is greater than the balance of the sender', function () {
+      const amount = initialBalance + 1;
+      it('reverts', async function () {
+        await this.token.approve(burner, amount, { from: owner });
+        await assertRevert(this.token.burnFrom(owner, amount, { from: burner }));
+      });
+    });
+
+    describe('when the given amount is greater than the allowance', function () {
+      const amount = 100;
+      it('reverts', async function () {
+        await this.token.approve(burner, amount - 1, { from: owner });
+        await assertRevert(this.token.burnFrom(owner, amount, { from: burner }));
       });
     });
   });
