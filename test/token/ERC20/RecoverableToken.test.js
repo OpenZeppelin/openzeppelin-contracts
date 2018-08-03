720556fdce99d40137820f889292543613c854da
jules goullée <julesgoullee@gmail.com>
2018-08-03 09:33:46 +0530

    remove chai-as-promised

--- test/token/ERC20/RecoverableToken.test.js
+++ test/token/ERC20/RecoverableToken.test.js
@@ -3,7 +3,6 @@ const RecoverableTokenMock = artifacts.require('RecoverableTokenMock');
 const BigNumber = web3.BigNumber;
 
 require('chai')
-  .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
