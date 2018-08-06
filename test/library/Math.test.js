519951da330fbbfacc4d2fd43e46cd1bf20969ef
Francisco Giordano <frangio.1@gmail.com>
2018-08-06 15:04:01 -0300

    enhance Math coverage

--- test/library/Math.test.js
+++ test/library/Math.test.js
@@ -1,22 +1,34 @@
 const MathMock = artifacts.require('MathMock');
 
 contract('Math', function () {
+  const min = 1234;
+  const max = 5678;
+
   beforeEach(async function () {
     this.math = await MathMock.new();
   });
 
-  it('returns max correctly', async function () {
-    const a = 5678;
-    const b = 1234;
-    const result = await this.math.max(a, b);
-    assert.equal(result, a);
+  describe('max', function () {
+    it('is correctly detected in first argument position', async function () {
+      const result = await this.math.max(max, min);
+      assert.equal(result, max);
+    });
+
+    it('is correctly detected in second argument position', async function () {
+      const result = await this.math.max(min, max);
+      assert.equal(result, max);
+    });
   });
 
-  it('returns min correctly', async function () {
-    const a = 5678;
-    const b = 1234;
-    const result = await this.math.min(a, b);
+  describe('min', function () {
+    it('is correctly detected in first argument position', async function () {
+      const result = await this.math.min(min, max);
+      assert.equal(result, min);
+    });
 
-    assert.equal(result, b);
+    it('is correctly detected in second argument position', async function () {
+      const result = await this.math.min(max, min);
+      assert.equal(result, min);
+    });
   });
 });
