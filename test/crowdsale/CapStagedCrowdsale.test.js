20fe8ba90f83401cd6ee44a86a5c3ba9f000226d
tadejo <tadej@trendnet.si>
2018-08-08 00:34:03 +0200

    edited lint errors

--- test/crowdsale/CapStagedCrowdsale.test.js
+++ test/crowdsale/CapStagedCrowdsale.test.js
@@ -1,4 +1,3 @@
-const { ether } = require('../helpers/ether');
 const CapStagedCrowdsale = artifacts.require('CapStagedCrowdsale');
 const BigNumber = web3.BigNumber;
 const CapStagedCrowdsaleToken = artifacts.require('CapStagedCrowdsaleToken');
@@ -9,15 +8,15 @@ require('chai')
 
 contract('CapStagedCrowdsale', function ([_, creator, wallet, investor]) {
   const _rate = new BigNumber(1000);
-  const _value =  web3.toWei('10', 'ether');//  10ether
+  const _value = web3.toWei('10', 'ether');//  10ether
   const stage1Limit = web3.toWei('10', 'ether');//  10ether
   const stage1Rate = new BigNumber(4000);
   const stage2Limit = web3.toWei('20', 'ether');//  10ether //  20ether
   const stage2Rate = new BigNumber(3000);
   const stage3Limit = web3.toWei('30', 'ether');//  10ether//  30ether
   const stage3Rate = new BigNumber(2000);
-  var stLimits = [stage1Limit, stage2Limit, stage3Limit];
-  var stRates = [stage1Rate, stage2Rate, stage3Rate];
+  const stLimits = [stage1Limit, stage2Limit, stage3Limit];
+  const stRates = [stage1Rate, stage2Rate, stage3Rate];
   beforeEach(async function () {
     this._token = await CapStagedCrowdsaleToken.new();
     this._csale = await CapStagedCrowdsale.new(_rate, wallet, this._token.address, stLimits, stRates);
@@ -36,7 +35,7 @@ contract('CapStagedCrowdsale', function ([_, creator, wallet, investor]) {
   });
   describe('should accept payments in stage 2 (rate 3000)', function () {
     it('should accept payments in stage 2 (rate 3000)', async function () {
-      await this._csale.buyTokens(investor, { from: investor, value: _value*2 }).should.be.fulfilled;
+      await this._csale.buyTokens(investor, { from: investor, value: _value * 2 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
       assert(rt.should.be.bignumber.eq(3000));
       const wi = await this._csale.weiRaised();
@@ -45,7 +44,7 @@ contract('CapStagedCrowdsale', function ([_, creator, wallet, investor]) {
   });
   describe('should accept payments in stage 3 (rate 2000)', function () {
     it('should accept payments in stage 3 (rate 2000)', async function () {
-      await this._csale.buyTokens(investor, { from: investor, value: _value*3 }).should.be.fulfilled;
+      await this._csale.buyTokens(investor, { from: investor, value: _value * 3 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
       assert(rt.should.be.bignumber.eq(2000));
       const wi = await this._csale.weiRaised();
@@ -54,12 +53,12 @@ contract('CapStagedCrowdsale', function ([_, creator, wallet, investor]) {
   });
   describe('should reject payments in stage 3 (rate 2000)', function () {
     it('should reject payments over last stage limit (stage 3, rate 2000)', async function () {
-      await this._csale.buyTokens(investor, { from: investor, value: _value*3 }).should.be.fulfilled;
+      await this._csale.buyTokens(investor, { from: investor, value: _value * 3 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
       const wi = await this._csale.weiRaised();
       assert(rt.should.be.bignumber.eq(2000));
       assert(wi.should.be.bignumber.eq(30000000000000000000));
-      await this._csale.buyTokens(investor, { from: investor, value: _value*1 }).should.be.rejected;
+      await this._csale.buyTokens(investor, { from: investor, value: _value * 1 }).should.be.rejected;
     });
   });
 });
