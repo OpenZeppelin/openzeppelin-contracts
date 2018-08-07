5e87cf3b69bf1ee92c8243c95025373153a7e462
tadejo <tadej@trendnet.si>
2018-08-08 00:56:44 +0200

    edited lint errors

--- truffle.js
+++ truffle.js
@@ -3,9 +3,9 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     test: {
-       network_id: "*",
-       host: "127.0.0.1",
-       port: 8545 // the port that Ganache-cli exposes
-     }
+      network_id: "*",
+      host: "127.0.0.1",
+      port: 8545 // the port that Ganache-cli exposes
+    }
   }
 };
