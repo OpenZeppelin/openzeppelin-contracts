e9e4058f0dbc7c8c8aa7fff7fcd3e9cfeea8e0db
tadejo <tadej@trendnet.si>
2018-08-07 23:58:08 +0200

    implemented review changes

--- contracts/crowdsale/price/CapStagedCrowdsale.sol
+++ contracts/crowdsale/price/CapStagedCrowdsale.sol
@@ -12,26 +12,23 @@ import "../../ownership/Ownable.sol";
   */
 contract CapStagedCrowdsale is Crowdsale, Ownable {
   using SafeMath for uint256;
-  using SafeMath for uint;
-  bool isActive = false;
 
   struct Stage {
-    uint256 stageLimit;
-    uint256 stageRate;
+    uint256 limit;
+    uint256 rate;
   }
 
   Stage[] public stages;
 
   /**
-   * @dev Reverts if crowdsale started - weiRaised is more than 0 or isActive is set to 1 with setCrowdsaleState function
+   * @dev Reverts if crowdsale started - weiRaised is more than 0
    */
-  modifier crowdsaleStarted() {
-    require((weiRaised == 0) && (isActive == false));
+  modifier beforeCrowdsaleStart() {
+    require(weiRaised == 0);
     _;
   }
 
   /**
-   * @dev Constructor, takes iniital rate, wallet for funds, token address, array od stage limits in wei and stage rates
    * @param _rate Number of tokens a buyer gets per wei - default parameter for crowdsale contract
    * @param _wallet Address where funds should be transferred
    * @param _token ERC20 token address
@@ -49,6 +46,7 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
     Crowdsale(_rate, _wallet, _token)
     public
   {
+    require(_stageLimits.length == _stageRates.length);
     for (uint256 i = 0; i < _stageLimits.length; i++) {
       addStage(_stageLimits[i], _stageRates[i]);
     }
@@ -56,11 +54,13 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
 
   /**
     * @dev Function for adding stages. Stages must be inserted from first to last. Stages can only be added until weiRaised is 0
-    * @param _stLimit - Stage limit in wei, _stRate - stage rate
+    * @param _limit - Stage limit in wei, _rate - stage rate
     */
-  function addStage(uint256 _stLimit, uint256 _stRate) onlyOwner crowdsaleStarted public {
-    Stage memory stage = Stage(_stLimit, _stRate);
-    stages.push(stage);
+  function addStage(uint256 _limit, uint256 _rate) beforeCrowdsaleStart internal {
+    stages.push(Stage({
+      limit: _limit,
+      rate: _rate
+    }));
   }
 
   /**
@@ -69,18 +69,19 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
     */
   function getRate() public view returns(uint256) {
     bool isSet = false;
-    for (uint x = 0; x < stages.length; x++) {
-      if (isSet == false) {
-        if (stages[x].stageLimit >= weiRaised) {
-          isSet = true;
-          return stages[x].stageRate;
+    if (stages.length > 0){
+      for (uint x = 0; x < stages.length; x++) {
+        if (isSet == false) {
+          if (stages[x].limit >= weiRaised) {
+            isSet = true;
+            return stages[x].rate;
+          }
         }
       }
     }
-  }
-
-  function setCrowdsaleState(bool _state) onlyOwner crowdsaleStarted public {
-    isActive = _state;
+    else {
+      return rate;
+    }
   }
 
   /**
@@ -89,7 +90,6 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
     * @return The number of tokens _weiAmount wei will buy at present time
     */
   function _getTokenAmount(uint256 _weiAmount) internal view returns(uint256) {
-    require(stages[(stages.length).sub(1)].stageLimit > weiRaised);
     uint256 currentRate = getRate();
     return currentRate.mul(_weiAmount);
   }
