c0612d2d0d5c9bd33e5a11980cd38c6444eeb3ca
Tadej <tadej@trendnet.si>
2018-08-08 02:25:09 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,11 +1,2 @@
 require('babel-register');
 require('babel-polyfill');
-module.exports = {
-  networks: {
-    test: {
-      network_id: "*",
-      host: "127.0.0.1",
-      port: 8545 // the port that Ganache-cli exposes
-    }
-  }
-};
