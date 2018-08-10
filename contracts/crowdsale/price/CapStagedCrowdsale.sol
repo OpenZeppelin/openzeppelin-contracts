af60b214dc360a1299ba59774a446250059e7387
tadejo <tadej@trendnet.si>
2018-08-10 17:09:25 +0200

    edited changes from review

--- contracts/crowdsale/price/CapStagedCrowdsale.sol
+++ contracts/crowdsale/price/CapStagedCrowdsale.sol
@@ -10,7 +10,7 @@ import "../../ownership/Ownable.sol";
   * @dev Extension of Crowdsale contract that changes the price of tokens regarding of raised ETH. Each stage of crowdsale is defined in StageLimit (cap limit for the stage) and stageRate (stage rate)
   * Stages must be inserted from first to last with increasing stageLimit.
   */
-contract CapStagedCrowdsale is Crowdsale, Ownable {
+contract CapStagedCrowdsale is Crowdsale {
   using SafeMath for uint256;
 
   struct Stage {
@@ -21,15 +21,7 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
   Stage[] public stages;
 
   /**
-   * @dev Reverts if crowdsale started - weiRaised is more than 0
-   */
-  modifier beforeCrowdsaleStart() {
-    require(weiRaised == 0);
-    _;
-  }
-
-  /**
-   * @param _rate Number of tokens a buyer gets per wei - default parameter for crowdsale contract
+   * @param _initialRate Number of tokens a buyer gets per wei - default parameter for crowdsale contract
    * @param _wallet Address where funds should be transferred
    * @param _token ERC20 token address
    * @param _stageLimits Array of stage limits in wei
@@ -37,30 +29,23 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
    */
   constructor
   (
-    uint256 _rate,
+    uint256 _initialRate,
     address _wallet,
     ERC20 _token,
     uint256[] _stageLimits,
     uint256[] _stageRates
   )
-    Crowdsale(_rate, _wallet, _token)
+    Crowdsale(_initialRate, _wallet, _token)
     public
   {
     require(_stageLimits.length == _stageRates.length);
+    require(_stageLimits.length > 0);
     for (uint256 i = 0; i < _stageLimits.length; i++) {
-      addStage(_stageLimits[i], _stageRates[i]);
+      stages.push(Stage({
+        limit: _stageLimits[i],
+        rate: _stageRates[i]
+      }));
     }
-  }
-
-  /**
-    * @dev Function for adding stages. Stages must be inserted from first to last. Stages can only be added until weiRaised is 0
-    * @param _limit - Stage limit in wei, _rate - stage rate
-    */
-  function addStage(uint256 _limit, uint256 _rate) internal beforeCrowdsaleStart {
-    stages.push(Stage({
-      limit: _limit,
-      rate: _rate
-    }));
   }
 
   /**
@@ -68,20 +53,16 @@ contract CapStagedCrowdsale is Crowdsale, Ownable {
     * @return uint256 Current rate of the stage
     */
   function getRate() public view returns(uint256) {
-    bool isSet = false;
     if (stages.length > 0) {
       for (uint x = 0; x < stages.length; x++) {
-        if (isSet == false) {
-          if (stages[x].limit >= weiRaised) {
-            isSet = true;
-            return stages[x].rate;
-          }
+        if (stages[x].limit >= weiRaised) {
+          return stages[x].rate;
         }
       }
     } else {
       return rate;
       }
-  }
+    }
 
   /**
     * @dev Overrides parent method taking into account variable rate.
--- test/crowdsale/CapStagedCrowdsale.test.js
+++ test/crowdsale/CapStagedCrowdsale.test.js
@@ -1,6 +1,6 @@
 const CapStagedCrowdsale = artifacts.require('CapStagedCrowdsale');
 const BigNumber = web3.BigNumber;
-const CapStagedCrowdsaleToken = artifacts.require('CapStagedCrowdsaleToken');
+const SimpleToken = artifacts.require('SimpleToken');
 require('chai')
   .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(BigNumber))
@@ -10,53 +10,54 @@ contract('CapStagedCrowdsale', function ([_, creator, wallet, investor]) {
   const _rate = new BigNumber(1000);
   const _value = web3.toWei('10', 'ether');//  10ether
   const stage1Limit = web3.toWei('10', 'ether');//  10ether
-  const stage1Rate = new BigNumber(4000);
+  const stage1Rate = new BigNumber(40);
   const stage2Limit = web3.toWei('20', 'ether');//  10ether //  20ether
-  const stage2Rate = new BigNumber(3000);
+  const stage2Rate = new BigNumber(30);
   const stage3Limit = web3.toWei('30', 'ether');//  10ether//  30ether
-  const stage3Rate = new BigNumber(2000);
+  const stage3Rate = new BigNumber(20);
   const stLimits = [stage1Limit, stage2Limit, stage3Limit];
   const stRates = [stage1Rate, stage2Rate, stage3Rate];
+  const tokenSupply = new BigNumber(1e+22);
   beforeEach(async function () {
-    this._token = await CapStagedCrowdsaleToken.new();
+    this._token = await SimpleToken.new();
     this._csale = await CapStagedCrowdsale.new(_rate, wallet, this._token.address, stLimits, stRates);
-    await this._token.transfer(this._csale.address, 1000000000000000000000000);
-    await this._token.transferOwnership(this._csale.address);
+    await this._token.transfer(this._csale.address, tokenSupply);
+
   });
 
-  describe('should accept payments in stage 1 (rate 4000)', function () {
-    it('should accept payments in stage 1 (rate 4000)', async function () {
+  describe('should accept payments in stage 1 (rate 40)', function () {
+    it('should accept payments in stage 1 (rate 40)', async function () {
       await this._csale.buyTokens(investor, { from: investor, value: _value }).should.be.fulfilled;
       const rt = await this._csale.getRate();
-      assert(rt.should.be.bignumber.eq(4000));
+      assert(rt.should.be.bignumber.eq(40));
       const wi = await this._csale.weiRaised();
       assert(wi.should.be.bignumber.eq(10000000000000000000));
     });
   });
-  describe('should accept payments in stage 2 (rate 3000)', function () {
-    it('should accept payments in stage 2 (rate 3000)', async function () {
+  describe('should accept payments in stage 2 (rate 30)', function () {
+    it('should accept payments in stage 2 (rate 30)', async function () {
       await this._csale.buyTokens(investor, { from: investor, value: _value * 2 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
-      assert(rt.should.be.bignumber.eq(3000));
+      assert(rt.should.be.bignumber.eq(30));
       const wi = await this._csale.weiRaised();
       assert(wi.should.be.bignumber.eq(20000000000000000000));
     });
   });
-  describe('should accept payments in stage 3 (rate 2000)', function () {
-    it('should accept payments in stage 3 (rate 2000)', async function () {
+  describe('should accept payments in stage 3 (rate 20)', function () {
+    it('should accept payments in stage 3 (rate 20)', async function () {
       await this._csale.buyTokens(investor, { from: investor, value: _value * 3 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
-      assert(rt.should.be.bignumber.eq(2000));
+      assert(rt.should.be.bignumber.eq(20));
       const wi = await this._csale.weiRaised();
       assert(wi.should.be.bignumber.eq(30000000000000000000));
     });
   });
-  describe('should reject payments in stage 3 (rate 2000)', function () {
-    it('should reject payments over last stage limit (stage 3, rate 2000)', async function () {
+  describe('should reject payments in stage 3 (rate 20)', function () {
+    it('should reject payments over last stage limit (stage 3, rate 20)', async function () {
       await this._csale.buyTokens(investor, { from: investor, value: _value * 3 }).should.be.fulfilled;
       const rt = await this._csale.getRate();
       const wi = await this._csale.weiRaised();
-      assert(rt.should.be.bignumber.eq(2000));
+      assert(rt.should.be.bignumber.eq(20));
       assert(wi.should.be.bignumber.eq(30000000000000000000));
       await this._csale.buyTokens(investor, { from: investor, value: _value * 1 }).should.be.rejected;
     });
