a2e710386933d3002062888b35aae8ac0401a7b3
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-08-10 17:34:34 -0300

    Improved SafeMath inline comments.

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -25,7 +25,7 @@ library SafeMath {
   }
 
   /**
-  * @dev Integer division of two numbers, truncating the quotient.
+  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
   */
   function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
     require(_b > 0); // Solidity only automatically asserts when dividing by 0
