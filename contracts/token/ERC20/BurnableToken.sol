78f4b9e9e73d354e9bf299abbdca8c37cb2fa95f
Francisco Giordano <frangio.1@gmail.com>
2018-08-15 16:02:40 -0300

    add inline docs to BurnableToken._burn

--- contracts/token/ERC20/BurnableToken.sol
+++ contracts/token/ERC20/BurnableToken.sol
@@ -28,6 +28,10 @@ contract BurnableToken is StandardToken {
     _burnFrom(_from, _value);
   }
 
+  /**
+   * @dev Overrides StandardToken._burn in order for burn and burnFrom to emit
+   * an additional Burn event.
+   */
   function _burn(address _who, uint256 _value) internal {
     super._burn(_who, _value);
     emit Burn(_who, _value);
