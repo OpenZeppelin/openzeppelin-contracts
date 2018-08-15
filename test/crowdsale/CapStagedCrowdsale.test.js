59ff99f29d7e1a1cb10ce2cdc97a1ca6c953791c
tadejo <tadej@trendnet.si>
2018-08-16 01:59:05 +0200

    corrected lint errors

--- test/crowdsale/CapStagedCrowdsale.test.js
+++ test/crowdsale/CapStagedCrowdsale.test.js
@@ -28,49 +28,44 @@ contract('CapStagedCrowdsale', function ([_, owner, wallet, investor]) {
   const capital = web3.toWei(3, 'ether');
   const tokenSupply = web3.toWei(160, 'ether');
   describe('sending stages in correct order', function () {
-  beforeEach(async function () {
-    this.token = await SimpleToken.new({ from: owner });
-    this.crowdsale = await CapStagedCrowdsale.new(_rate, wallet, this.token.address, capital, limits, rates, { from: owner });
-    await this.token.transfer(this.crowdsale.address, tokenSupply, { from: owner });
-  });
-  context('stage 1', function () {
-    it('should accept payments in stage 1 (rate 40)', async function () {
-      await this.crowdsale.buyTokens(investor, { from: investor, value: _value });
-      const rt = await this.crowdsale.getRate();
-      const wi = await this.crowdsale.weiRaised();
-      assert(wi.should.be.bignumber.eq(web3.toWei(1, 'ether')));
+    beforeEach(async function () {
+      this.token = await SimpleToken.new({ from: owner });
+      this.crowdsale = await CapStagedCrowdsale.new(_rate, wallet, this.token.address, capital, limits, rates, { from: owner });
+      await this.token.transfer(this.crowdsale.address, tokenSupply, { from: owner });
+    });
+    context('stage 1', function () {
+      it('should accept payments in stage 1 (rate 40)', async function () {
+        await this.crowdsale.buyTokens(investor, { from: investor, value: _value });
+        const wi = await this.crowdsale.weiRaised();
+        assert(wi.should.be.bignumber.eq(web3.toWei(1, 'ether')));
+      });
+    });
+    context('stage 2', function () {
+      it('should accept payments in stage 2 (rate 30)', async function () {
+        await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 2 });
+        const wi = await this.crowdsale.weiRaised();
+        assert(wi.should.be.bignumber.eq(web3.toWei(2, 'ether')));
+      });
+    });
+    context('stage 3', function () {
+      it('should accept payments in stage 3 (rate 20)', async function () {
+        await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 3 });
+        const wi = await this.crowdsale.weiRaised();
+        assert(wi.should.be.bignumber.eq(web3.toWei(3, 'ether')));
+      });
+    });
+    context('over last stage limit', function () {
+      it('should reject payments over last stage limit (stage 3, rate 20)', async function () {
+        await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 3 });
+        const wi = await this.crowdsale.weiRaised();
+        assert(wi.should.be.bignumber.eq(web3.toWei(3, 'ether')));
+        await expectThrow(
+        this.crowdsale.buyTokens(investor, { from: investor, value: _value * 1 }),
+        EVMRevert,
+        );
+      });
     });
   });
-  context('stage 2', function () {
-    it('should accept payments in stage 2 (rate 30)', async function () {
-      await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 2 });
-      const rt = await this.crowdsale.getRate();
-      const wi = await this.crowdsale.weiRaised();
-      assert(wi.should.be.bignumber.eq(web3.toWei(2, 'ether')));
-    });
-  });
-  context('stage 3', function () {
-    it('should accept payments in stage 3 (rate 20)', async function () {
-      await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 3 });
-      const rt = await this.crowdsale.getRate();
-      const wi = await this.crowdsale.weiRaised();
-      assert(wi.should.be.bignumber.eq(web3.toWei(3, 'ether')));
-    });
-  });
-  context('over last stage limit', function () {
-    it('should reject payments over last stage limit (stage 3, rate 20)', async function () {
-      await this.crowdsale.buyTokens(investor, { from: investor, value: _value * 3 });
-      const rt = await this.crowdsale.getRate();
-      const wi = await this.crowdsale.weiRaised();
-      const cp = await this.crowdsale.cap();
-      assert(wi.should.be.bignumber.eq(web3.toWei(3, 'ether')));
-      await expectThrow(
-      this.crowdsale.buyTokens(investor, { from: investor, value: _value * 1 }),
-      EVMRevert,
-      );
-    });
-  });
-});
   describe('sending stages in wrong order', function () {
     it('shoud be reverted', async function () {
       this.token = await SimpleToken.new({ from: owner });
