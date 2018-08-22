472127bb0e19e066302fac96d051495d47b2bd9c
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-08-22 20:03:08 -0300

    Removed RBACMintableToken from the MintedCrowdsale tests.

--- test/crowdsale/MintedCrowdsale.test.js
+++ test/crowdsale/MintedCrowdsale.test.js
@@ -5,7 +5,6 @@ const BigNumber = web3.BigNumber;
 
 const MintedCrowdsale = artifacts.require('MintedCrowdsaleImpl');
 const MintableToken = artifacts.require('MintableToken');
-const RBACMintableToken = artifacts.require('RBACMintableToken');
 
 contract('MintedCrowdsale', function ([_, investor, wallet, purchaser]) {
   const rate = new BigNumber(1000);
@@ -20,22 +19,6 @@ contract('MintedCrowdsale', function ([_, investor, wallet, purchaser]) {
 
     it('should be token owner', async function () {
       (await this.token.owner()).should.eq(this.crowdsale.address);
-    });
-
-    shouldBehaveLikeMintedCrowdsale([_, investor, wallet, purchaser], rate, value);
-  });
-
-  describe('using RBACMintableToken', function () {
-    const ROLE_MINTER = 'minter';
-
-    beforeEach(async function () {
-      this.token = await RBACMintableToken.new();
-      this.crowdsale = await MintedCrowdsale.new(rate, wallet, this.token.address);
-      await this.token.addMinter(this.crowdsale.address);
-    });
-
-    it('should have minter role on token', async function () {
-      (await this.token.hasRole(this.crowdsale.address, ROLE_MINTER)).should.be.true;
     });
 
     shouldBehaveLikeMintedCrowdsale([_, investor, wallet, purchaser], rate, value);
