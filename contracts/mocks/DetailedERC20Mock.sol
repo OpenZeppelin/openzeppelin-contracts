50c43fef64b2697a6ad7a20862294c47037664c5
Francisco Giordano <frangio.1@gmail.com>
2018-08-29 13:23:28 -0300

    move DetailedERC20.sol to ERC20Detailed.sol, likewise for related files

--- contracts/mocks/DetailedERC20Mock.sol
+++ contracts/mocks/DetailedERC20Mock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "../token/ERC20/ERC20.sol";
-import "../token/ERC20/DetailedERC20.sol";
+import "../token/ERC20/ERC20Detailed.sol";
 
 
 contract ERC20DetailedMock is ERC20, ERC20Detailed {
