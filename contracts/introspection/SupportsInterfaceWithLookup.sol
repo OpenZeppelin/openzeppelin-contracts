81ea4a8a0d84a5437f69c6f981dd7dd231a3d12a
Francisco Giordano <frangio.1@gmail.com>
2018-08-29 14:52:38 -0300

    move ERC165.sol to IERC165.sol

--- contracts/introspection/SupportsInterfaceWithLookup.sol
+++ contracts/introspection/SupportsInterfaceWithLookup.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "./ERC165.sol";
+import "./IERC165.sol";
 
 
 /**
--- contracts/mocks/ERC165/ERC165InterfacesSupported.sol
+++ contracts/mocks/ERC165/ERC165InterfacesSupported.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "../../introspection/ERC165.sol";
+import "../../introspection/IERC165.sol";
 
 
 /**
