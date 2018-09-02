3d69c023d7a75e3b4d59127a23e1e0f31674af66
Tal Ater <tal@talater.com>
2018-09-02 15:25:37 +0300

    Broke function definition to multiple lines
    Comply with new max line length

--- contracts/crowdsale/PausableCrowdsale.sol
+++ contracts/crowdsale/PausableCrowdsale.sol
@@ -15,7 +15,10 @@ contract PausableCrowdsale is Pausable, Crowdsale {
    * @param _beneficiary Address performing the token purchase
    * @param _weiAmount Value in wei involved in the purchase
    */
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal whenNotPaused {
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount)
+    internal
+    whenNotPaused
+  {
     return super._preValidatePurchase(_beneficiary, _weiAmount);
   }
 }
