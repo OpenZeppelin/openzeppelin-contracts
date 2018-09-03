3a2c242b150589f36aeae0b9dde619ffa39dee50
Leo Arias <yo@elopio.net>
2018-09-03 03:13:48 +0000

    add the underscore

--- contracts/lifecycle/Pausable.sol
+++ contracts/lifecycle/Pausable.sol
@@ -12,21 +12,21 @@ contract Pausable is Ownable {
   event Paused();
   event Unpaused();
 
-  bool private paused = false;
+  bool private paused_ = false;
 
 
   /**
    * @return true if the contract is paused, false otherwise.
    */
   function isPaused() public view returns(bool) {
-    return paused;
+    return paused_;
   }
 
   /**
    * @dev Modifier to make a function callable only when the contract is not paused.
    */
   modifier whenNotPaused() {
-    require(!paused);
+    require(!paused_);
     _;
   }
 
@@ -34,7 +34,7 @@ contract Pausable is Ownable {
    * @dev Modifier to make a function callable only when the contract is paused.
    */
   modifier whenPaused() {
-    require(paused);
+    require(paused_);
     _;
   }
 
@@ -42,7 +42,7 @@ contract Pausable is Ownable {
    * @dev called by the owner to pause, triggers stopped state
    */
   function pause() public onlyOwner whenNotPaused {
-    paused = true;
+    paused_ = true;
     emit Paused();
   }
 
@@ -50,7 +50,7 @@ contract Pausable is Ownable {
    * @dev called by the owner to unpause, returns to normal state
    */
   function unpause() public onlyOwner whenPaused {
-    paused = false;
+    paused_ = false;
     emit Unpaused();
   }
 }
