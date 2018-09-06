58178b5f572d2c3f87fe4d043ea04f48b91a93ae
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-09-06 11:10:27 -0300

    Added PauserRole.

--- test/lifecycle/Pausable.test.js
+++ test/lifecycle/Pausable.test.js
@@ -8,58 +8,95 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('Pausable', function () {
+contract('Pausable', function ([_, pauser, anyone]) {
   beforeEach(async function () {
-    this.Pausable = await PausableMock.new();
+    this.Pausable = await PausableMock.new({ from: pauser });
   });
 
-  it('can perform normal process in non-pause', async function () {
-    (await this.Pausable.count()).should.be.bignumber.equal(0);
+  context('when unapused', function () {
+    beforeEach(async function () {
+      (await this.Pausable.paused()).should.equal(false);
+    });
 
-    await this.Pausable.normalProcess();
-    (await this.Pausable.count()).should.be.bignumber.equal(1);
-  });
+    it('can perform normal process in non-pause', async function () {
+      (await this.Pausable.count()).should.be.bignumber.equal(0);
 
-  it('can not perform normal process in pause', async function () {
-    await this.Pausable.pause();
-    (await this.Pausable.count()).should.be.bignumber.equal(0);
+      await this.Pausable.normalProcess({ from: anyone });
+      (await this.Pausable.count()).should.be.bignumber.equal(1);
+    });
 
-    await assertRevert(this.Pausable.normalProcess());
-    (await this.Pausable.count()).should.be.bignumber.equal(0);
-  });
+    it('cannot take drastic measure in non-pause', async function () {
+      await assertRevert(this.Pausable.drasticMeasure({ from: anyone }));
+      (await this.Pausable.drasticMeasureTaken()).should.equal(false);
+    });
 
-  it('can not take drastic measure in non-pause', async function () {
-    await assertRevert(this.Pausable.drasticMeasure());
-    (await this.Pausable.drasticMeasureTaken()).should.equal(false);
-  });
+    describe('pausing', function () {
+      it('is pausable by the pauser', async function () {
+        await this.Pausable.pause({ from: pauser });
+        (await this.Pausable.paused()).should.equal(true);
+      });
 
-  it('can take a drastic measure in a pause', async function () {
-    await this.Pausable.pause();
-    await this.Pausable.drasticMeasure();
-    (await this.Pausable.drasticMeasureTaken()).should.equal(true);
-  });
+      it('reverts when pausing from non-pauser', async function () {
+        await assertRevert(this.Pausable.pause({ from: anyone }));
+      });
 
-  it('should resume allowing normal process after pause is over', async function () {
-    await this.Pausable.pause();
-    await this.Pausable.unpause();
-    await this.Pausable.normalProcess();
-    (await this.Pausable.count()).should.be.bignumber.equal(1);
-  });
+      context('when paused', function () {
+        beforeEach(async function () {
+          ({ logs: this.logs } = await this.Pausable.pause({ from: pauser }));
+        });
 
-  it('should prevent drastic measure after pause is over', async function () {
-    await this.Pausable.pause();
-    await this.Pausable.unpause();
+        it('emits a Paused event', function () {
+          expectEvent.inLogs(this.logs, 'Paused');
+        });
 
-    await assertRevert(this.Pausable.drasticMeasure());
+        it('cannot perform normal process in pause', async function () {
+          await assertRevert(this.Pausable.normalProcess({ from: anyone }));
+        });
 
-    (await this.Pausable.drasticMeasureTaken()).should.equal(false);
-  });
+        it('can take a drastic measure in a pause', async function () {
+          await this.Pausable.drasticMeasure({ from: anyone });
+          (await this.Pausable.drasticMeasureTaken()).should.equal(true);
+        });
 
-  it('should log Pause and Unpause events appropriately', async function () {
-    const setPauseLogs = (await this.Pausable.pause()).logs;
-    expectEvent.inLogs(setPauseLogs, 'Paused');
+        it('reverts when re-pausing', async function () {
+          await assertRevert(this.Pausable.pause({ from: pauser }));
+        });
 
-    const setUnPauseLogs = (await this.Pausable.unpause()).logs;
-    expectEvent.inLogs(setUnPauseLogs, 'Unpaused');
+        describe('unpausing', function () {
+          it('is unpausable by the pauser', async function () {
+            await this.Pausable.unpause({ from: pauser });
+            (await this.Pausable.paused()).should.equal(false);
+          });
+
+          it('reverts when unpausing from non-pauser', async function () {
+            await assertRevert(this.Pausable.unpause({ from: anyone }));
+          });
+
+          context('when paused', function () {
+            beforeEach(async function () {
+              ({ logs: this.logs } = await this.Pausable.unpause({ from: pauser }));
+            });
+
+            it('emits an Unpaused event', function () {
+              expectEvent.inLogs(this.logs, 'Unpaused');
+            });
+
+            it('should resume allowing normal process', async function () {
+              (await this.Pausable.count()).should.be.bignumber.equal(0);
+              await this.Pausable.normalProcess({ from: anyone });
+              (await this.Pausable.count()).should.be.bignumber.equal(1);
+            });
+
+            it('should prevent drastic measure', async function () {
+              await assertRevert(this.Pausable.drasticMeasure({ from: anyone }));
+            });
+
+            it('reverts when re-unpausing', async function () {
+              await assertRevert(this.Pausable.unpause({ from: pauser }));
+            });
+          });
+        });
+      });
+    });
   });
 });
--- test/token/ERC721/ERC721Pausable.test.js
+++ test/token/ERC721/ERC721Pausable.test.js
@@ -8,29 +8,29 @@ require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('ERC721Pausable', function ([_, owner, recipient, operator, ...otherAccounts]) {
+contract('ERC721Pausable', function ([_, pauser, recipient, operator, ...otherAccounts]) {
   beforeEach(async function () {
-    this.token = await ERC721Pausable.new({ from: owner });
+    this.token = await ERC721Pausable.new({ from: pauser });
   });
 
   context('when token is paused', function () {
     beforeEach(async function () {
-      await this.token.pause({ from: owner });
+      await this.token.pause({ from: pauser });
     });
 
-    shouldBehaveLikeERC721PausedToken(owner, [...otherAccounts]);
+    shouldBehaveLikeERC721PausedToken(pauser, [...otherAccounts]);
   });
 
   context('when token is not paused yet', function () {
-    shouldBehaveLikeERC721Basic([owner, ...otherAccounts]);
+    shouldBehaveLikeERC721Basic([pauser, ...otherAccounts]);
   });
 
   context('when token is paused and then unpaused', function () {
     beforeEach(async function () {
-      await this.token.pause({ from: owner });
-      await this.token.unpause({ from: owner });
+      await this.token.pause({ from: pauser });
+      await this.token.unpause({ from: pauser });
     });
 
-    shouldBehaveLikeERC721Basic([owner, ...otherAccounts]);
+    shouldBehaveLikeERC721Basic([pauser, ...otherAccounts]);
   });
 });
