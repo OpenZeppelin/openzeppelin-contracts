936d5e475936527842d7fdc61be981b7818b5d64
Mudit <guptamudit@ymail.com>
2018-09-07 08:59:06 +0530

    Added ERC1132

---
+++ contracts/drafts/ERC1132/LockableToken.sol
@@ -0,0 +1,213 @@
+pragma solidity 0.4.24;
+
+import "../../token/ERC20/ERC20.sol";
+import "./ERC1132.sol";
+
+
+contract LockableToken is ERC20, ERC1132 {
+	/**
+	 * @dev Error messages for require statements
+	 */
+	string internal constant ALREADY_LOCKED = "Tokens already locked";
+    string internal constant NOT_LOCKED = "No tokens locked";
+    string internal constant AMOUNT_ZERO = "Amount can not be 0";
+
+	/**
+	 * @dev constructor to mint initial tokens
+	 */
+	constructor(uint256 _supply) public {
+        _mint(msg.sender, _supply);
+    }
+
+    /**
+     * @dev Locks a specified amount of tokens against an address,
+     *      for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be locked
+     * @param _time Lock time in seconds
+     */
+    function lock(bytes32 _reason, uint256 _amount, uint256 _time)
+        public
+        returns (bool)
+    {
+        uint256 validUntil = now.add(_time); //solhint-disable-line
+
+        // If tokens are already locked, then functions extendLock or
+        // increaseLockAmount should be used to make any changes
+        require(tokensLocked(msg.sender, _reason) == 0, ALREADY_LOCKED);
+        require(_amount != 0, AMOUNT_ZERO);
+
+        if (locked[msg.sender][_reason].amount == 0)
+            lockReason[msg.sender].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason] = lockToken(_amount, validUntil, false);
+
+        emit TokensLocked(msg.sender, _reason, _amount, validUntil);
+        return true;
+    }
+    
+    /**
+     * @dev Transfers and Locks a specified amount of tokens,
+     *      for a specified reason and time
+     * @param _to adress to which tokens are to be transfered
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be transfered and locked
+     * @param _time Lock time in seconds
+     */
+    function transferWithLock(address _to, bytes32 _reason, uint256 _amount, uint256 _time)
+        public
+        returns (bool)
+    {
+        uint256 validUntil = now.add(_time); //solhint-disable-line
+
+        require(tokensLocked(_to, _reason) == 0, ALREADY_LOCKED);
+        require(_amount != 0, AMOUNT_ZERO);
+
+        if (locked[_to][_reason].amount == 0)
+            lockReason[_to].push(_reason);
+
+        transfer(address(this), _amount);
+
+        locked[_to][_reason] = lockToken(_amount, validUntil, false);
+        
+        emit TokensLocked(_to, _reason, _amount, validUntil);
+        return true;
+    }
+
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     */
+    function tokensLocked(address _of, bytes32 _reason)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (!locked[_of][_reason].claimed)
+            amount = locked[_of][_reason].amount;
+    }
+    
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason at a specific time
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     * @param _time The timestamp to query the lock tokens for
+     */
+    function tokensLockedAtTime(address _of, bytes32 _reason, uint256 _time)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity > _time)
+            amount = locked[_of][_reason].amount;
+    }
+
+    /**
+     * @dev Returns total tokens held by an address (locked + transferable)
+     * @param _of The address to query the total balance of
+     */
+    function totalBalanceOf(address _of)
+        public
+        view
+        returns (uint256 amount)
+    {
+    	amount = balanceOf(_of);
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            amount = amount.add(tokensLocked(_of, lockReason[_of][i]));
+        }   
+    }    
+    
+    /**
+     * @dev Extends lock for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _time Lock extension time in seconds
+     */
+    function extendLock(bytes32 _reason, uint256 _time)
+        public
+        returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, NOT_LOCKED);
+
+        locked[msg.sender][_reason].validity = locked[msg.sender][_reason].validity.add(_time);
+
+        emit TokensLocked(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+    
+    /**
+     * @dev Increase number of tokens locked for a specified reason
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be increased
+     */
+    function increaseLockAmount(bytes32 _reason, uint256 _amount)
+        public
+        returns (bool)
+    {
+        require(tokensLocked(msg.sender, _reason) > 0, NOT_LOCKED);
+        transfer(address(this), _amount);
+
+        locked[msg.sender][_reason].amount = locked[msg.sender][_reason].amount.add(_amount);
+
+        emit TokensLocked(msg.sender, _reason, locked[msg.sender][_reason].amount, locked[msg.sender][_reason].validity);
+        return true;
+    }
+
+    /**
+     * @dev Returns unlockable tokens for a specified address for a specified reason
+     * @param _of The address to query the the unlockable token count of
+     * @param _reason The reason to query the unlockable tokens for
+     */
+    function tokensUnlockable(address _of, bytes32 _reason)
+        public
+        view
+        returns (uint256 amount)
+    {
+        if (locked[_of][_reason].validity <= now && !locked[_of][_reason].claimed) //solhint-disable-line
+            amount = locked[_of][_reason].amount;
+    }
+
+    /**
+     * @dev Unlocks the unlockable tokens of a specified address
+     * @param _of Address of user, claiming back unlockable tokens
+     */
+    function unlock(address _of)
+        public
+        returns (uint256 unlockableTokens)
+    {
+        uint256 lockedTokens;
+
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            lockedTokens = tokensUnlockable(_of, lockReason[_of][i]);
+            if (lockedTokens > 0) {
+                unlockableTokens = unlockableTokens.add(lockedTokens);
+                locked[_of][lockReason[_of][i]].claimed = true;
+                emit TokensUnlocked(_of, lockReason[_of][i], lockedTokens);
+            }
+        }  
+
+        if (unlockableTokens > 0)
+        	this.transfer(_of, unlockableTokens);
+    }
+
+    /**
+     * @dev Gets the unlockable tokens of a specified address
+     * @param _of The address to query the the unlockable token count of
+     */
+    function getUnlockableTokens(address _of)
+        public
+        view
+        returns (uint256 unlockableTokens)
+    {
+        for (uint256 i = 0; i < lockReason[_of].length; i++) {
+            unlockableTokens = unlockableTokens.add(tokensUnlockable(_of, lockReason[_of][i]));
+        }  
+    }
+}
---
+++ test/drafts/ERC1132/LockableToken.js
@@ -0,0 +1,210 @@
+const LockableToken = artifacts.require('LockableToken');
+const { assertRevert } = require('../../helpers/assertRevert');
+
+const supply = 1000;
+const lockReason = 'GOV';
+const lockReason2 = 'CLAIM';
+const lockReason3 = 'VESTED';
+const lockReason4 = 'GB';
+const lockReason5 = 'MG';
+const lockReason6 = 'OZ';
+const lockedAmount = 200;
+const lockPeriod = 1000;
+let blockNumber = web3.eth.blockNumber;
+const lockTimestamp = web3.eth.getBlock(blockNumber).timestamp;
+const approveAmount = 10;
+const nullAddress = 0x0000000000000000000000000000000000000000;
+const increaseTime = function(duration) {
+  web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: lockTimestamp
+    },
+    (err, resp) => {
+      if (!err) {
+        web3.currentProvider.send({
+          jsonrpc: '2.0',
+          method: 'evm_mine',
+          params: [],
+          id: lockTimestamp + 1
+        });
+      }
+    }
+  );
+};
+let token;
+
+contract('LockableToken', ([owner, receiver, spender]) => {
+
+  before(async () => {
+    token = await LockableToken.new(supply);
+  });
+
+  it('can be created', () => {
+    assert.ok(token);
+  });
+
+  it('has the right balance for the contract owner', async () => {
+    const balance = await token.balanceOf(owner);
+    const totalBalance = await token.totalBalanceOf(owner);
+    const totalSupply = await token.totalSupply();
+    assert.equal(balance.toNumber(), supply);
+    assert.equal(totalBalance.toNumber(), supply);
+    assert.equal(totalSupply.toNumber(), supply);
+  });
+
+  it('has the right total balance for the contract owner', async () => {
+    const balance = await token.totalBalanceOf(owner);
+    assert.equal(balance.toNumber(), supply);
+  });
+
+  it('reduces locked tokens from transferable balance', async () => {
+    const origBalance = await token.balanceOf(owner);
+    blockNumber = await web3.eth.blockNumber;
+    const newLockTimestamp = await web3.eth.getBlock(blockNumber).timestamp;
+    await token.lock(lockReason, lockedAmount, lockPeriod);
+    const balance = await token.balanceOf(owner);
+    const totalBalance = await token.totalBalanceOf(owner);
+    assert.equal(balance.toNumber(), origBalance.toNumber() - lockedAmount);
+    assert.equal(totalBalance.toNumber(), origBalance.toNumber());
+    let actualLockedAmount = await token.tokensLocked(owner, lockReason);
+    assert.equal(lockedAmount, actualLockedAmount.toNumber());
+    actualLockedAmount = await token.tokensLockedAtTime(
+      owner,
+      lockReason,
+      newLockTimestamp + lockPeriod + 1
+    );
+    assert.equal(0, actualLockedAmount.toNumber());
+
+    const transferAmount = 1;
+    const {
+      logs
+    } = await token.transfer(receiver, transferAmount, {
+      from: owner
+    });
+    const newSenderBalance = await token.balanceOf(owner);
+    const newReceiverBalance = await token.balanceOf(receiver);
+    assert.equal(newReceiverBalance.toNumber(), transferAmount);
+    assert.equal(newSenderBalance.toNumber(), balance - transferAmount);
+    assert.equal(logs.length, 1);
+    assert.equal(logs[0].event, 'Transfer');
+    assert.equal(logs[0].args.from, owner);
+    assert.equal(logs[0].args.to, receiver);
+    assert(logs[0].args.value.eq(transferAmount));
+  });
+
+  it('reverts locking more tokens via lock function', async () => {
+    const balance = await token.balanceOf(owner);
+    await assertRevert(token.lock(lockReason, balance, lockPeriod));
+  });
+
+  it('can extend lock period for an existing lock', async () => {
+    await token.tokensLocked(owner, lockReason);
+    const lockValidityOrig = await token.locked(owner, lockReason);
+    await token.extendLock(lockReason, lockPeriod);
+    const lockValidityExtended = await token.locked(owner, lockReason);
+    assert.equal(
+      lockValidityExtended[1].toNumber(),
+      lockValidityOrig[1].toNumber() + lockPeriod
+    );
+    await assertRevert(token.extendLock(lockReason2, lockPeriod));
+    await assertRevert(token.increaseLockAmount(lockReason2, lockPeriod));
+  });
+
+  it('can increase the number of tokens locked', async () => {
+    const actualLockedAmount = await token.tokensLocked(owner, lockReason);
+    await token.increaseLockAmount(lockReason, lockedAmount);
+    const increasedLockAmount = await token.tokensLocked(owner, lockReason);
+    assert.equal(
+      increasedLockAmount.toNumber(),
+      actualLockedAmount.toNumber() + lockedAmount
+    );
+  });
+
+  it('can unLockTokens', async () => {
+    const lockValidityExtended = await token.locked(owner, lockReason);
+    const balance = await token.balanceOf(owner);
+    const tokensLocked = await token.tokensLockedAtTime(
+      owner,
+      lockReason,
+      lockTimestamp
+    );
+    await increaseTime(
+      lockValidityExtended[1].toNumber() + 60 - lockTimestamp
+    );
+    unlockableToken = await token.getUnlockableTokens(owner);
+    assert.equal(unlockableToken.toNumber(), tokensLocked.toNumber());
+    await token.unlock(owner);
+    unlockableToken = await token.getUnlockableTokens(owner);
+    assert.equal(unlockableToken.toNumber(), 0);
+    const newBalance = await token.balanceOf(owner);
+    assert.equal(
+      newBalance.toNumber(),
+      balance.toNumber() + tokensLocked.toNumber()
+    );
+    await token.unlock(owner);
+    const newNewBalance = await token.balanceOf(owner);
+    assert.equal(newBalance.toNumber(), newNewBalance.toNumber());
+  });
+
+  it('should allow to lock token again', async () => {
+    await token.lock(lockReason4, 1, 0);
+    await token.unlock(owner);
+    await token.lock(lockReason4, 1, 100000);
+    const actualLockedAmount = await token.tokensLocked(owner, lockReason4);
+    assert.equal(actualLockedAmount.toNumber(), 1);
+  });
+
+  it('can transferWithLock', async () => {
+    const ownerBalance = (await token.balanceOf(owner)).toNumber();
+    const receiverBalance = (await token.balanceOf(receiver)).toNumber();
+    await token.transferWithLock(receiver, lockReason3, ownerBalance - 1, 0);
+    await assertRevert(
+      token.transferWithLock(receiver, lockReason3, ownerBalance, lockPeriod)
+    );
+    const locked = await token.locked(receiver, lockReason3);
+    assert.equal((await token.balanceOf(owner)).toNumber(), 1);
+    assert.equal(
+      (await token.balanceOf(receiver)).toNumber(),
+      receiverBalance
+    );
+    assert.equal(locked[0].toNumber(), ownerBalance - 1);
+  });
+
+  it('should not allow 0 lock amount', async () => {
+    await assertRevert(token.lock(lockReason5, 0, lockTimestamp));
+    await assertRevert(
+      token.transferWithLock(receiver, lockReason5, 0, lockPeriod)
+    );
+  });
+
+  it('should show 0 lock amount for unknown reasons', async () => {
+    const actualLockedAmount = await token.tokensLocked(owner, lockReason6);
+    assert.equal(actualLockedAmount.toNumber(), 0);
+  });
+
+  it('should not allow to increase lock amount by more than balance', async () => {
+    await assertRevert(
+      token.increaseLockAmount(
+        lockReason,
+        (await token.balanceOf(owner)).toNumber() + 1
+      )
+    );
+  });
+
+  it('should not allow to transfer and lock more than balance', async () => {
+    const ownerBalance = (await token.balanceOf(owner)).toNumber();
+    await assertRevert(
+      token.transferWithLock(receiver, lockReason6, ownerBalance + 1, lockPeriod)
+    );
+  });
+
+  it('should allow transfer with lock again after claiming', async () => {
+    await token.unlock(receiver);
+    const oldlLockedAmount = await token.tokensLocked(receiver, lockReason3);
+    await token.transferWithLock(receiver, lockReason3, 1, 10000);
+    const newLockedAmount = await token.tokensLocked(receiver, lockReason3);
+    assert.equal(newLockedAmount.toNumber(), oldlLockedAmount.toNumber() + 1);
+  });
+});
