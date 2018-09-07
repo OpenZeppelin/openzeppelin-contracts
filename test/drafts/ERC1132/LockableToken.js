3be110f7f43f22b6e2dffcbace18c74097da0125
Mudit <guptamudit@ymail.com>
2018-09-07 09:21:28 +0530

    JS linting

--- test/drafts/ERC1132/LockableToken.js
+++ test/drafts/ERC1132/LockableToken.js
@@ -16,22 +16,21 @@ let token;
 
 const increaseTime = function (duration) {
   web3.currentProvider.sendAsync({
-      jsonrpc: '2.0',
-      method: 'evm_increaseTime',
-      params: [duration],
-      id: lockTimestamp
-    },
-    (err, resp) => {
-      if (!err) {
-        web3.currentProvider.send({
-          jsonrpc: '2.0',
-          method: 'evm_mine',
-          params: [],
-          id: lockTimestamp + 1
-        });
-      }
+    jsonrpc: '2.0',
+    method: 'evm_increaseTime',
+    params: [duration],
+    id: lockTimestamp
+  },
+  (err, resp) => {
+    if (!err) {
+      web3.currentProvider.send({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        params: [],
+        id: lockTimestamp + 1
+      });
     }
-  );
+  });
 };
 
 contract('LockableToken', ([owner, receiver, spender]) => {
