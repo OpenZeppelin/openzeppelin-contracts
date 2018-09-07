1b335fe91a5c65322b37a0eedb6f552692aa79f6
Francisco Giordano <frangio.1@gmail.com>
2018-09-07 10:37:25 -0300

    test beginMigration

--- test/drafts/MigratorERC20.test.js
+++ test/drafts/MigratorERC20.test.js
@@ -19,97 +19,123 @@ contract('ERC20Migrator', function ([_, owner, recipient, anotherAccount]) {
     this.legacyToken = await ERC20Mock.new(owner, totalSupply);
     this.migrator = await ERC20Migrator.new(this.legacyToken.address);
     this.newToken = await ERC20Mintable.new();
-    await this.newToken.addMinter(this.migrator.address);
-    await this.migrator.beginMigration(this.newToken.address);
   });
 
-  describe('migrateAll', function () {
-    const baseAmount = totalSupply;
-
-    describe('when the approved balance is higher or equal to the owned balance', function () {
-      const amount = baseAmount;
-
-      beforeEach('approving the whole balance to the new contract', async function () {
-        await this.legacyToken.approve(this.migrator.address, amount, { from: owner });
-      });
-
-      beforeEach('migrating token', async function () {
-        const tx = await this.migrator.migrateAll(owner);
-        this.logs = tx.receipt.logs;
-      });
-
-      it('mints the same balance of the new token', async function () {
-        const currentBalance = await this.newToken.balanceOf(owner);
-        currentBalance.should.be.bignumber.equal(amount);
-      });
-
-      it('burns a given amount of old tokens', async function () {
-        const currentBurnedBalance = await this.legacyToken.balanceOf(this.migrator.address);
-        currentBurnedBalance.should.be.bignumber.equal(amount);
-
-        const currentLegacyTokenBalance = await this.legacyToken.balanceOf(owner);
-        currentLegacyTokenBalance.should.be.bignumber.equal(0);
-      });
-
-      it('updates the total supply', async function () {
-        const currentSupply = await this.newToken.totalSupply();
-        currentSupply.should.be.bignumber.equal(amount);
-      });
+  describe('beginMigration', function () {
+    it('rejects zero address for token', async function () {
+      await assertRevert(this.migrator.beginMigration(ZERO_ADDRESS));
     });
 
-    describe('when the approved balance is lower than the owned balance', function () {
-      const amount = baseAmount - 1;
+    it('reverts if not a minter of the token', async function () {
+      await assertRevert(this.migrator.beginMigration(this.newToken.address));
+    });
 
-      beforeEach('approving part of the balance to the new contract', async function () {
-        await this.legacyToken.approve(this.migrator.address, amount, { from: owner });
-        await this.migrator.migrateAll(owner);
-      });
+    it('succeeds if it is a minter of the token', async function () {
+      await this.newToken.addMinter(this.migrator.address);
+      await this.migrator.beginMigration(this.newToken.address);
+    });
 
-      it('migrates only approved amount', async function () {
-        const currentBalance = await this.newToken.balanceOf(owner);
-        currentBalance.should.be.bignumber.equal(amount);
-      });
+    it('reverts the second time it is called', async function () {
+      await this.newToken.addMinter(this.migrator.address);
+      await this.migrator.beginMigration(this.newToken.address);
+      await assertRevert(this.migrator.beginMigration(this.newToken.address));
     });
   });
 
-  describe('migrate', function () {
-    const baseAmount = 50
-
-    beforeEach('approving tokens to the new contract', async function () {
-      await this.legacyToken.approve(this.migrator.address, baseAmount, { from: owner });
+  describe('once migration began', function () {
+    beforeEach('', async function () {
+      await this.newToken.addMinter(this.migrator.address);
+      await this.migrator.beginMigration(this.newToken.address);
     });
 
-    describe('when the amount is lower or equal to the one approved', function () {
-      const amount = baseAmount;
+    describe('migrateAll', function () {
+      const baseAmount = totalSupply;
 
-      beforeEach('migrate token', async function () {
-        ({ logs: this.logs } = await this.migrator.migrate(owner, amount));
+      describe('when the approved balance is higher or equal to the owned balance', function () {
+        const amount = baseAmount;
+
+        beforeEach('approving the whole balance to the new contract', async function () {
+          await this.legacyToken.approve(this.migrator.address, amount, { from: owner });
+        });
+
+        beforeEach('migrating token', async function () {
+          const tx = await this.migrator.migrateAll(owner);
+          this.logs = tx.receipt.logs;
+        });
+
+        it('mints the same balance of the new token', async function () {
+          const currentBalance = await this.newToken.balanceOf(owner);
+          currentBalance.should.be.bignumber.equal(amount);
+        });
+
+        it('burns a given amount of old tokens', async function () {
+          const currentBurnedBalance = await this.legacyToken.balanceOf(this.migrator.address);
+          currentBurnedBalance.should.be.bignumber.equal(amount);
+
+          const currentLegacyTokenBalance = await this.legacyToken.balanceOf(owner);
+          currentLegacyTokenBalance.should.be.bignumber.equal(0);
+        });
+
+        it('updates the total supply', async function () {
+          const currentSupply = await this.newToken.totalSupply();
+          currentSupply.should.be.bignumber.equal(amount);
+        });
       });
 
-      it('mints that amount of the new token', async function () {
-        const currentBalance = await this.newToken.balanceOf(owner);
-        currentBalance.should.be.bignumber.equal(amount);
-      });
+      describe('when the approved balance is lower than the owned balance', function () {
+        const amount = baseAmount - 1;
 
-      it('burns a given amount of old tokens', async function () {
-        const currentBurnedBalance = await this.legacyToken.balanceOf(this.migrator.address);
-        currentBurnedBalance.should.be.bignumber.equal(amount);
+        beforeEach('approving part of the balance to the new contract', async function () {
+          await this.legacyToken.approve(this.migrator.address, amount, { from: owner });
+          await this.migrator.migrateAll(owner);
+        });
 
-        const currentLegacyTokenBalance = await this.legacyToken.balanceOf(owner);
-        currentLegacyTokenBalance.should.be.bignumber.equal(totalSupply - amount);
-      });
-
-      it('updates the total supply', async function () {
-        const currentSupply = await this.newToken.totalSupply();
-        currentSupply.should.be.bignumber.equal(amount);
+        it('migrates only approved amount', async function () {
+          const currentBalance = await this.newToken.balanceOf(owner);
+          currentBalance.should.be.bignumber.equal(amount);
+        });
       });
     });
 
-    describe('when the given amount is higher than the one approved', function () {
-      const amount = baseAmount + 1;
+    describe('migrate', function () {
+      const baseAmount = 50
 
-      it('reverts', async function () {
-        await assertRevert(this.migrator.migrate(owner, amount));
+      beforeEach('approving tokens to the new contract', async function () {
+        await this.legacyToken.approve(this.migrator.address, baseAmount, { from: owner });
+      });
+
+      describe('when the amount is lower or equal to the one approved', function () {
+        const amount = baseAmount;
+
+        beforeEach('migrate token', async function () {
+          ({ logs: this.logs } = await this.migrator.migrate(owner, amount));
+        });
+
+        it('mints that amount of the new token', async function () {
+          const currentBalance = await this.newToken.balanceOf(owner);
+          currentBalance.should.be.bignumber.equal(amount);
+        });
+
+        it('burns a given amount of old tokens', async function () {
+          const currentBurnedBalance = await this.legacyToken.balanceOf(this.migrator.address);
+          currentBurnedBalance.should.be.bignumber.equal(amount);
+
+          const currentLegacyTokenBalance = await this.legacyToken.balanceOf(owner);
+          currentLegacyTokenBalance.should.be.bignumber.equal(totalSupply - amount);
+        });
+
+        it('updates the total supply', async function () {
+          const currentSupply = await this.newToken.totalSupply();
+          currentSupply.should.be.bignumber.equal(amount);
+        });
+      });
+
+      describe('when the given amount is higher than the one approved', function () {
+        const amount = baseAmount + 1;
+
+        it('reverts', async function () {
+          await assertRevert(this.migrator.migrate(owner, amount));
+        });
       });
     });
   });
