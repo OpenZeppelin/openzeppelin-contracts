7132fe33a2ad1104c103b103ddb7d2cd61f988ab
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-09-07 14:12:40 -0300

    Fixed variable shadowing.

--- contracts/token/ERC721/IERC721Metadata.sol
+++ contracts/token/ERC721/IERC721Metadata.sol
@@ -8,7 +8,7 @@ import "./IERC721.sol";
  * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
 contract IERC721Metadata is IERC721 {
-  function name() external view returns (string name);
-  function symbol() external view returns (string symbol);
+  function name() external view returns (string);
+  function symbol() external view returns (string);
   function tokenURI(uint256 tokenId) public view returns (string);
 }
