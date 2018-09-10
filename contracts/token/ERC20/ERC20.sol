fee83f460b107990d2f834ccb4815980df39e27c
Jerome de Tychey <jdetychey@gmail.com>
2018-09-10 11:57:00 +0200

    Update ERC20.sol
    correct typo in doc

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -29,7 +29,7 @@ contract ERC20 is IERC20 {
 
   /**
   * @dev Gets the balance of the specified address.
-  * @param owner The address to query the the balance of.
+  * @param owner The address to query the balance of.
   * @return An uint256 representing the amount owned by the passed address.
   */
   function balanceOf(address owner) public view returns (uint256) {
