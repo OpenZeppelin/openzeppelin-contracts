7825caa1fdf8bec80ad43f4da15cf5ee4c9a8e3f
Jakub Bogacz <bogacz.jakub3@gmail.com>
2018-09-12 17:49:00 +0200

    Add unit test specific to Address utils (#1251) (#1316)

---
+++ contracts/mocks/AddressImpl.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.24;
+
+import "../utils/Address.sol";
+
+
+contract AddressImpl {
+  function isContract(address account)
+    external
+    view
+    returns (bool)
+  {
+    return Address.isContract(account); 
+  }
+  
+}
