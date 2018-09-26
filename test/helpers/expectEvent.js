947de54ceea3abe4783ac2e417b6d9bfb93d708e
Jakub Bogacz <bogacz.jakub3@gmail.com>
2018-09-26 22:04:41 +0200

    Add BigNumber support to expectEvent/inLogs (#1026) (#1338)
    * Add BigNumber support to expectEvent/inLogs (#1026)
    
    * switched direct logs array check to expectEvent method in AllowanceCrowdsale.test.js
    
    * Refactor expectEvent.inLogs function to use simple value number check
    
    * Introduced should.be.bignumber method to compare BigNumber values
    
    * Destructure transaction object to extract logs field

--- test/helpers/expectEvent.js
+++ test/helpers/expectEvent.js
@@ -1,30 +1,38 @@
-const should = require('chai').should();
+const BigNumber = web3.BigNumber;
+const should = require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
 
 function inLogs (logs, eventName, eventArgs = {}) {
   const event = logs.find(function (e) {
     if (e.event === eventName) {
-      let matches = true;
-
       for (const [k, v] of Object.entries(eventArgs)) {
-        if (e.args[k] !== v) {
-          matches = false;
-        }
+        contains(e.args, k, v);
       }
-
-      if (matches) {
-        return true;
-      }
+      return true;
     }
   });
-
   should.exist(event);
-
   return event;
 }
 
 async function inTransaction (tx, eventName, eventArgs = {}) {
   const { logs } = await tx;
   return inLogs(logs, eventName, eventArgs);
+}
+
+function contains (args, key, value) {
+  if (isBigNumber(args[key])) {
+    args[key].should.be.bignumber.equal(value);
+  } else {
+    args[key].should.be.equal(value);
+  }
+}
+
+function isBigNumber (object) {
+  return object.isBigNumber ||
+    object instanceof BigNumber ||
+    (object.constructor && object.constructor.name === 'BigNumber');
 }
 
 module.exports = {
