d7b7644e682bde3bcbd936e317bc7d795ed92f44
Francisco Giordano <frangio.1@gmail.com>
2018-09-26 18:30:45 -0300

    convert ERC1046 to initializers

--- contracts/drafts/ERC1046/TokenMetadata.sol
+++ contracts/drafts/ERC1046/TokenMetadata.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
 
+import "../../Initializable.sol";
 import "../../token/ERC20/IERC20.sol";
 
 
@@ -9,16 +10,17 @@ import "../../token/ERC20/IERC20.sol";
  * @dev tokenURI must respond with a URI that implements https://eips.ethereum.org/EIPS/eip-1047
  * @dev TODO - update https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC721/IERC721.sol#L17 when 1046 is finalized
  */
-contract ERC20TokenMetadata is IERC20 {
+contract ERC20TokenMetadata is Initializable, IERC20 {
   function tokenURI() external view returns (string);
 }
 
 
-contract ERC20WithMetadata is ERC20TokenMetadata {
+contract ERC20WithMetadata is Initializable, ERC20TokenMetadata {
   string private _tokenURI = "";
 
-  constructor(string tokenURI)
+  function initialize(string tokenURI)
     public
+    initializer
   {
     _tokenURI = tokenURI;
   }
