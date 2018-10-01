2bd2a2c6a4a15f7b4b06d3f9f8ed59dc6d9b335c
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-10-01 09:24:07 -0300

    Fixed advanceToBlock tests.

--- test/helpers/test/advanceToBlock.test.js
+++ test/helpers/test/advanceToBlock.test.js
@@ -1,5 +1,10 @@
 const advanceToBlock = require('../advanceToBlock');
 
+const BigNumber = web3.BigNumber;
+require('chai')
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
 describe('advanceToBlock', function () {
   beforeEach(function () {
     this.startingBlock = web3.eth.blockNumber;
