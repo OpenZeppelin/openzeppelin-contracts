10a5864fdf291525d3c8b40f0f3928425a2970f6
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-10-10 20:55:10 -0300

    First test helpers tests (#1369)
    * Removed unused advanceToBlock.
    
    * Added advanceBlock tests.
    
    * Fixed advanceToBlock tests.
    
    * Added single argument tests.
    
    * Finished inLogs tests.
    
    * Fixed linter errors.
    
    * Fixed linter errors.
    
    (cherry picked from commit 9f822906b8e8e052bae7947d4c362fbabdd2c3ba)

--- test/helpers/advanceToBlock.js
+++ test/helpers/advanceToBlock.js
@@ -10,18 +10,6 @@ function advanceBlock () {
   });
 }
 
-// Advances the block number so that the last mined block is `number`.
-async function advanceToBlock (number) {
-  if (web3.eth.blockNumber > number) {
-    throw Error(`block number ${number} is in the past (current is ${web3.eth.blockNumber})`);
-  }
-
-  while (web3.eth.blockNumber < number) {
-    await advanceBlock();
-  }
-}
-
 module.exports = {
   advanceBlock,
-  advanceToBlock,
 };
