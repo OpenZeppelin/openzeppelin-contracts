76e3d74e2deb3222ad67620475807794d59c8db8
Aniket-Engg <aniketengg.225@gmail.com>
2018-10-17 18:34:50 +0530

    fixes #1413

--- contracts/ownership/Secondary.sol
+++ contracts/ownership/Secondary.sol
@@ -7,11 +7,16 @@ pragma solidity ^0.4.24;
 contract Secondary {
   address private _primary;
 
+  event PrimarinessTransferred(
+    address indexed recipient
+  );
+
   /**
    * @dev Sets the primary account to the one that is creating the Secondary contract.
    */
   constructor() public {
     _primary = msg.sender;
+    emit PrimarinessTransferred(_primary);
   }
 
   /**
@@ -22,13 +27,20 @@ contract Secondary {
     _;
   }
 
+  /**
+   * @return the address of the primary.
+   */
   function primary() public view returns (address) {
     return _primary;
   }
-
+  
+  /**
+   * @dev Transfers primariness to a recipient.
+   * @param recipient The address to transfer primariness. 
+   */
   function transferPrimary(address recipient) public onlyPrimary {
     require(recipient != address(0));
-
     _primary = recipient;
+    emit PrimarinessTransferred(_primary);
   }
 }
