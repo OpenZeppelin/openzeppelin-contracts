96d6103e0b70c5a09005bc77cf5bb9310fb90ac3
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-10-18 10:47:35 -0300

    Made some inherit-only contracts internal. (#1433)
    * Made some inherit-only contracts internal.
    
    * Added OwnableMock.

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -11,7 +11,7 @@ contract ReentrancyGuard {
   /// @dev counter to allow mutex lock with only one SSTORE operation
   uint256 private _guardCounter;
 
-  constructor() public {
+  constructor() internal {
     // The counter starts at one to prevent changing it from zero to a non-zero
     // value, which is a more expensive operation.
     _guardCounter = 1;
