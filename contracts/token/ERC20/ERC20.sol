940999e80435228a902cee78c5616ab770083e53
alant <alan.htang@gmail.com>
2018-10-21 10:27:53 -0700

    fix ERC20.sol#L174 and ERC20.sol#L187 should be casted to an address type.

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -171,7 +171,7 @@ contract ERC20 is IERC20 {
    * @param value The amount that will be created.
    */
   function _mint(address account, uint256 value) internal {
-    require(account != 0);
+    require(account != address(0));
     _totalSupply = _totalSupply.add(value);
     _balances[account] = _balances[account].add(value);
     emit Transfer(address(0), account, value);
@@ -184,7 +184,7 @@ contract ERC20 is IERC20 {
    * @param value The amount that will be burnt.
    */
   function _burn(address account, uint256 value) internal {
-    require(account != 0);
+    require(account != address(0));
     require(value <= _balances[account]);
 
     _totalSupply = _totalSupply.sub(value);
