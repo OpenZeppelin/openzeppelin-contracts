71dd49014230d500c7e2823a854db75d8147a6d1
wbt <wbt@users.noreply.github.com>
2018-10-25 17:18:38 -0400

    Add constant MAX_UINT32
    For testing SafeMath32.

--- test/helpers/constants.js
+++ test/helpers/constants.js
@@ -3,4 +3,5 @@ const BigNumber = web3.BigNumber;
 module.exports = {
   ZERO_ADDRESS: '0x0000000000000000000000000000000000000000',
   MAX_UINT256: new BigNumber(2).pow(256).minus(1),
+  MAX_UINT32: new BigNumber(2).pow(32).minus(1),
 };
