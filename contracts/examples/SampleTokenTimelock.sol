b7f1ba91ffad8c03e8a44c9c77273b36dd4cc192
Aniket-Engg <aniketengg.225@gmail.com>
2018-11-02 15:41:35 +0530

    fixes #706

---
+++ contracts/examples/SampleTokenTimelock.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.24;
+
+import "../token/ERC20/ERC20Mintable.sol";
+import "../token/ERC20/ERC20Detailed.sol";
+import "../token/ERC20/TokenTimelock.sol";
+
+/**
+ * @title SampleTimelockToken
+ * @dev Very simple ERC20 Token that can be minted.
+ * It is meant to be used in a tokentimelock contract.
+ */
+contract SampleTimelockToken is ERC20Mintable, ERC20Detailed {
+  constructor() public ERC20Detailed("Sample Timelock Token", "SLT", 18) {}
+}
+
+
+/**
+ * @title SampleTokenTimelock
+ * @dev This is an example of a token lock for certain time.
+ */
+
+contract SampleTokenTimelock is TokenTimelock{
+
+  constructor(
+    ERC20Mintable token,
+    address beneficiary,
+    uint256 releaseTime
+  ) 
+  public
+  TokenTimelock(token, beneficiary, releaseTime){}
+
+}
