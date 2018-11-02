83bc045a56d891726ab3a4cbdd6be35e0023e72c
William Entriken <github.com@phor.net>
2018-11-02 06:16:35 -0400

    Remove extraneous quantity check, fixes #1454 (#1455)

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -197,8 +197,6 @@ contract ERC20 is IERC20 {
    * @param value The amount that will be burnt.
    */
   function _burnFrom(address account, uint256 value) internal {
-    require(value <= _allowed[account][msg.sender]);
-
     // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
     // this function needs to emit an event with the updated approval.
     _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(
