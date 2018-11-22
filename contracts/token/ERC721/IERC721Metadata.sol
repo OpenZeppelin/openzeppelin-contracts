870ebf93895ce3fc3a7db0ffdc1f94c2daf7a95f
Roderik van der Veer <roderik@settlemint.com>
2018-11-22 10:05:00 +0300

    fix: add memory to the string return types

--- contracts/token/ERC721/IERC721Metadata.sol
+++ contracts/token/ERC721/IERC721Metadata.sol
@@ -7,7 +7,7 @@ import "./IERC721.sol";
  * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
 contract IERC721Metadata is IERC721 {
-  function name() external view returns (string);
-  function symbol() external view returns (string);
-  function tokenURI(uint256 tokenId) public view returns (string);
+  function name() external view returns (string memory);
+  function symbol() external view returns (string memory);
+  function tokenURI(uint256 tokenId) public view returns (string memory);
 }
