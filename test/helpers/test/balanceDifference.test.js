f58fc834998a1c03a1d00c86cca8dba20b1fc693
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-11-23 18:30:28 -0300

    Added balanceDifference tests.

--- test/helpers/test/balanceDifference.test.js
+++ test/helpers/test/balanceDifference.test.js
@@ -9,12 +9,14 @@ require('chai')
 
 contract('balanceDifference', function ([sender, receiver]) {
   it('returns balance increments', async function () {
-    (await balanceDifference(receiver, () => sendEther(sender, receiver, ether(1))))
-      .should.be.bignumber.equal(ether(1));
+    (await balanceDifference(receiver, () =>
+      sendEther(sender, receiver, ether(1)))
+    ).should.be.bignumber.equal(ether(1));
   });
 
   it('returns balance decrements', async function () {
-    (await balanceDifference(sender, () => sendEther(sender, receiver, ether(1))))
-      .should.be.bignumber.equal(ether(-1));
+    (await balanceDifference(sender, () =>
+      sendEther(sender, receiver, ether(1)))
+    ).should.be.bignumber.equal(ether(-1));
   });
 });
