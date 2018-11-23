3ca8b20d05ce73f0ca475644a529b11c0116a65a
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-11-23 10:40:07 -0300

    Moved role constructor tests to public role behavior.

--- test/access/roles/CapperRole.test.js
+++ test/access/roles/CapperRole.test.js
@@ -1,14 +1,9 @@
 const { shouldBehaveLikePublicRole } = require('../../access/roles/PublicRole.behavior');
 const CapperRoleMock = artifacts.require('CapperRoleMock');
-const expectEvent = require('../../helpers/expectEvent');
 
 contract('CapperRole', function ([_, capper, otherCapper, ...otherAccounts]) {
   beforeEach(async function () {
     this.contract = await CapperRoleMock.new({ from: capper });
-    await expectEvent.inConstruction(this.contract, 'CapperAdded', {
-      account: capper,
-    });
-
     await this.contract.addCapper(otherCapper, { from: capper });
   });
 
--- test/access/roles/SignerRole.test.js
+++ test/access/roles/SignerRole.test.js
@@ -1,14 +1,9 @@
 const { shouldBehaveLikePublicRole } = require('../../access/roles/PublicRole.behavior');
 const SignerRoleMock = artifacts.require('SignerRoleMock');
-const expectEvent = require('../../helpers/expectEvent');
 
 contract('SignerRole', function ([_, signer, otherSigner, ...otherAccounts]) {
   beforeEach(async function () {
     this.contract = await SignerRoleMock.new({ from: signer });
-    await expectEvent.inConstruction(this.contract, 'SignerAdded', {
-      account: signer,
-    });
-
     await this.contract.addSigner(otherSigner, { from: signer });
   });
 
