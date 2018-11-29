f277761159de8965ef1460c38f6e1861ae1b48e4
Francisco Giordano <frangio.1@gmail.com>
2018-11-29 11:37:30 -0300

    remove unused networks from truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,3 @@
-require('dotenv').config();
-
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-const providerWithMnemonic = (mnemonic, rpcEndpoint) =>
-  new HDWalletProvider(mnemonic, rpcEndpoint);
-
-const infuraProvider = network => providerWithMnemonic(
-  process.env.MNEMONIC || '',
-  `https://${network}.infura.io/${process.env.INFURA_API_KEY}`
-);
-
 module.exports = {
   networks: {
     development: {
@@ -17,21 +5,12 @@ module.exports = {
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
-    ropsten: {
-      provider: () => infuraProvider('ropsten'),
-      network_id: 3, // eslint-disable-line camelcase
-    },
     coverage: {
       host: 'localhost',
       network_id: '*', // eslint-disable-line camelcase
       port: 8555,
       gas: 0xfffffffffff,
       gasPrice: 0x01,
-    },
-    ganache: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // eslint-disable-line camelcase
     },
   },
 };
