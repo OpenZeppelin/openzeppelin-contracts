195496d0ec7890a3123ffca8ec270422a0601691
Nicolás Venturo <nicolas.venturo@gmail.com>
2018-12-05 17:44:03 -0300

    Fixed unnecessary dependency of RefundableCrowdsaleImpl on ERC20Mintable.

--- contracts/mocks/RefundableCrowdsaleImpl.sol
+++ contracts/mocks/RefundableCrowdsaleImpl.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "../token/ERC20/ERC20Mintable.sol";
+import "../token/ERC20/IERC20.sol";
 import "../crowdsale/distribution/RefundableCrowdsale.sol";
 
 contract RefundableCrowdsaleImpl is RefundableCrowdsale {
@@ -9,7 +9,7 @@ contract RefundableCrowdsaleImpl is RefundableCrowdsale {
         uint256 closingTime,
         uint256 rate,
         address wallet,
-        ERC20Mintable token,
+        IERC20 token,
         uint256 goal
     )
         public
