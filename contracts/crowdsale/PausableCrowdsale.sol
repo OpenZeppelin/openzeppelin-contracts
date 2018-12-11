f35c7e1766ae6631c274a97bb70693818073c900
Francisco Giordano <frangio.1@gmail.com>
2018-12-11 18:51:18 -0300

    improve documentation

--- contracts/crowdsale/PausableCrowdsale.sol
+++ contracts/crowdsale/PausableCrowdsale.sol
@@ -6,12 +6,13 @@ import "../lifecycle/Pausable.sol";
 
 /**
  * @title PausableCrowdsale
- * @dev Extension of Crowdsale contract that can be paused and unpaused by owner
+ * @dev Extension of Crowdsale contract that can be paused and unpaused by the pauser role.
  */
 contract PausableCrowdsale is Pausable, Crowdsale {
 
     /**
      * @dev Validation of an incoming purchase. Use require statements to revert state when conditions are not met. Use super to concatenate validations.
+     * Adds the validation that the crowdsale must not be paused.
      * @param _beneficiary Address performing the token purchase
      * @param _weiAmount Value in wei involved in the purchase
      */
