268182e7af397de66688b9d0c61c900e50d018c6
Francisco Giordano <frangio.1@gmail.com>
2018-12-12 18:25:37 -0300

    fix transaction senders to be the anyone account

--- test/crowdsale/PausableCrowdsale.test.js
+++ test/crowdsale/PausableCrowdsale.test.js
@@ -9,35 +9,41 @@ contract('PausableCrowdsale', function ([_, pauser, wallet, anyone]) {
   const rate = 1;
   const value = 1;
 
-  const from = pauser;
-
   beforeEach(async function () {
+    const from = pauser;
+
     this.token = await SimpleToken.new({ from });
     this.crowdsale = await PausableCrowdsale.new(rate, wallet, this.token.address, { from });
     await this.token.transfer(this.crowdsale.address, 2 * value, { from });
   });
 
   it('purchases work', async function () {
+    const from = anyone;
+
     await this.crowdsale.sendTransaction({ from, value });
     await this.crowdsale.buyTokens(from, { from, value });
   });
 
   context('after pause', function () {
     beforeEach(async function () {
-      await this.crowdsale.pause({ from });
+      await this.crowdsale.pause({ from: pauser });
     });
 
     it('purchases do not work', async function () {
+      const from = anyone;
+
       await shouldFail.reverting(this.crowdsale.sendTransaction({ from, value }));
       await shouldFail.reverting(this.crowdsale.buyTokens(from, { from, value }));
     });
 
     context('after unpause', function () {
       beforeEach(async function () {
-        await this.crowdsale.unpause({ from });
+        await this.crowdsale.unpause({ from: pauser });
       });
 
       it('purchases work', async function () {
+        const from = anyone;
+
         await this.crowdsale.sendTransaction({ from, value });
         await this.crowdsale.buyTokens(from, { from, value });
       });
