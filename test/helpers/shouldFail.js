17d80dac7322b145cd73766f8203dbcd67e0abc4
Remon Nashid <remon.sherin@gmail.com>
2018-12-13 20:56:08 -0500

    An attempt to deduct ganache version

--- test/helpers/shouldFail.js
+++ test/helpers/shouldFail.js
@@ -1,4 +1,5 @@
 const { should } = require('./setup');
+const Failer = artifacts.require('Failer');
 
 async function shouldFailWithMessage (promise, message) {
   try {
@@ -29,8 +30,29 @@ async function shouldFail (promise) {
   await shouldFailWithMessage(promise);
 }
 
+async function withMessage (promise, message) {
+  // First, let's find out if current version of Ganache (or testrpc-sc)
+  // supports revert reason by reverting deliberately and then catching and inspecting
+  // thrown error.
+  try {
+    const failer = await Failer.new();
+    await failer.failWithRevertVocally();
+  } catch (error) {
+    if (error.message.includes('Doomed to fail')) {
+      // Current Ganache supports reason string. Proceed with revert reason
+      // assertion.
+      return shouldFailWithMessage(promise, message);
+    } else if (error.message.includes('revert')) {
+      // Current version does NOT support reason string. Fall back to regular
+      // revert assertion.
+      return reverting(promise);
+    }
+  }
+  should.fail('This line should be unreachable');
+}
+
 shouldFail.reverting = reverting;
-shouldFail.reverting.withMessage = shouldFailWithMessage;
+shouldFail.reverting.withMessage = withMessage;
 shouldFail.throwing = throwing;
 shouldFail.outOfGas = outOfGas;
 
