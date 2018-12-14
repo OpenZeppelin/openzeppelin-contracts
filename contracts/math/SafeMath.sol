550e95ecd6b54f30d7b98e4495b2b9c0584e00c0
Francisco Giordano <frangio.1@gmail.com>
2018-12-14 12:07:32 -0300

    signed safe math

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -5,8 +5,10 @@ pragma solidity ^0.4.24;
  * @dev Math operations with safety checks that revert on error
  */
 library SafeMath {
+  int256 constant private INT256_MIN = -2^255;
+
     /**
-    * @dev Multiplies two numbers, reverts on overflow.
+    * @dev Multiplies two unsigned integers, reverts on overflow.
     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
@@ -23,7 +25,24 @@ library SafeMath {
     }
 
     /**
-    * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
+    * @dev Multiplies two signed integers, reverts on overflow.
+    */
+    function mul(int256 a, int256 b) internal pure returns (int256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        int256 c = a * b;
+        require(c / a == b && (a != -1 || b != INT256_MIN));
+
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // Solidity only automatically asserts when dividing by 0
@@ -35,7 +54,18 @@ library SafeMath {
     }
 
     /**
-    * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+    * @dev Integer division of two signed integers truncating the quotient, reverts on division by zero.
+    */
+    function div(int256 a, int256 b) internal pure returns (int256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b != 0 && b != -1 && a != INT256_MIN);
+        int256 c = a / b;
+
+        return c;
+    }
+
+    /**
+    * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b <= a);
@@ -45,7 +75,17 @@ library SafeMath {
     }
 
     /**
-    * @dev Adds two numbers, reverts on overflow.
+    * @dev Subtracts two signed integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a - b;
+        require((b >= 0 && c <= a) || (b < 0 && c > a));
+
+        return c;
+    }
+
+    /**
+    * @dev Adds two unsigned integers, reverts on overflow.
     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
@@ -55,7 +95,17 @@ library SafeMath {
     }
 
     /**
-    * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
+    * @dev Adds two signed integers, reverts on overflow.
+    */
+    function add(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a + b;
+        require((b >= 0 && c >= a) || (b < 0 && c < a));
+
+        return c;
+    }
+
+    /**
+    * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
     * reverts when dividing by zero.
     */
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
--- contracts/mocks/SafeMathMock.sol
+++ contracts/mocks/SafeMathMock.sol
@@ -1,25 +1,44 @@
 pragma solidity ^0.4.24;
 
+
 import "../math/SafeMath.sol";
 
+
 contract SafeMathMock {
-    function mul(uint256 a, uint256 b) public pure returns (uint256) {
-        return SafeMath.mul(a, b);
-    }
 
-    function div(uint256 a, uint256 b) public pure returns (uint256) {
-        return SafeMath.div(a, b);
-    }
+  function mulUints(uint256 a, uint256 b) public pure returns (uint256) {
+    return SafeMath.mul(a, b);
+  }
 
-    function sub(uint256 a, uint256 b) public pure returns (uint256) {
-        return SafeMath.sub(a, b);
-    }
+  function mulInts(int256 a, int256 b) public pure returns (int256) {
+    return SafeMath.mul(a, b);
+  }
 
-    function add(uint256 a, uint256 b) public pure returns (uint256) {
-        return SafeMath.add(a, b);
-    }
+  function divUints(uint256 a, uint256 b) public pure returns (uint256) {
+    return SafeMath.div(a, b);
+  }
 
-    function mod(uint256 a, uint256 b) public pure returns (uint256) {
-        return SafeMath.mod(a, b);
-    }
+  function divInts(int256 a, int256 b) public pure returns (int256) {
+    return SafeMath.div(a, b);
+  }
+
+  function subUints(uint256 a, uint256 b) public pure returns (uint256) {
+    return SafeMath.sub(a, b);
+  }
+
+  function subInts(int256 a, int256 b) public pure returns (int256) {
+    return SafeMath.sub(a, b);
+  }
+
+  function addUints(uint256 a, uint256 b) public pure returns (uint256) {
+    return SafeMath.add(a, b);
+  }
+
+  function addInts(int256 a, int256 b) public pure returns (int256) {
+    return SafeMath.add(a, b);
+  }
+
+  function modUints(uint256 a, uint256 b) public pure returns (uint256) {
+    return SafeMath.mod(a, b);
+  }
 }
