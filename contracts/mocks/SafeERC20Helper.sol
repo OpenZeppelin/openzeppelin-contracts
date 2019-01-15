893cba6cfe865d775352b130fc58d7ec196f00f8
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-01-15 15:23:18 -0300

    Add dummy state variable to SafeERC20Helper.

--- contracts/mocks/SafeERC20Helper.sol
+++ contracts/mocks/SafeERC20Helper.sol
@@ -6,19 +6,27 @@ import "../token/ERC20/SafeERC20.sol";
 contract ERC20FailingMock {
     uint256 private _allowance;
 
+    // IERC20's functions are not pure, but these mock implementations are: to prevent Solidity from issuing warnings,
+    // we write to a dummy state variable.
+    uint256 private _dummy;
+
     function transfer(address, uint256) public returns (bool) {
+        _dummy = 0;
         return false;
     }
 
     function transferFrom(address, address, uint256) public returns (bool) {
+        _dummy = 0;
         return false;
     }
 
     function approve(address, uint256) public returns (bool) {
+        _dummy = 0;
         return false;
     }
 
     function allowance(address, address) public view returns (uint256) {
+        require(_dummy == 0);
         return 0;
     }
 }
@@ -26,15 +34,22 @@ contract ERC20FailingMock {
 contract ERC20SucceedingMock {
     uint256 private _allowance;
 
+    // IERC20's functions are not pure, but these mock implementations are: to prevent Solidity from issuing warnings,
+    // we write to a dummy state variable.
+    uint256 private _dummy;
+
     function transfer(address, uint256) public returns (bool) {
+        _dummy = 0;
         return true;
     }
 
     function transferFrom(address, address, uint256) public returns (bool) {
+        _dummy = 0;
         return true;
     }
 
     function approve(address, uint256) public returns (bool) {
+        _dummy = 0;
         return true;
     }
 
