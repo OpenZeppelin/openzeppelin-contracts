11570189d5891dbe33d8180aa3989a8f58d9d7ef
Brendan Chou <brendanchou@msn.com>
2019-02-08 11:49:56 -0800

    Use assembly in SafeMath

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -9,49 +9,46 @@ library SafeMath {
      * @dev Multiplies two unsigned integers, reverts on overflow.
      */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
+        assembly {
+            switch a
+            case 0 {
+                r := 0
+            }
+            default {
+                r := mul(a, b)
+                if not(eq(div(r, a), b)) { revert(0, 0) }
+            }
         }
-
-        uint256 c = a * b;
-        require(c / a == b);
-
-        return c;
     }
 
     /**
      * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
      */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
+        assembly {
+            if iszero(b) { revert(0, 0) }
+            r := div(a, b)
+        }
     }
 
     /**
      * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
      */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        uint256 c = a - b;
-
-        return c;
+        assembly {
+            if gt(a, b) { revert(0, 0) }
+            r := sub(a, b)
+        }
     }
 
     /**
      * @dev Adds two unsigned integers, reverts on overflow.
      */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-
-        return c;
+        assembly {
+            r := add(a,b)
+            if lt(r, a) { revert(0, 0) }
+        }
     }
 
     /**
@@ -59,7 +56,9 @@ library SafeMath {
      * reverts when dividing by zero.
      */
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b != 0);
-        return a % b;
+        assembly {
+            if iszero(b) { revert(0, 0) }
+            r := mod(a, b)
+        }
     }
 }
