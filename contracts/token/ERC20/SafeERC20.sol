7e6919d6e997143b7b3443bbb4b8966fd58772ca
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-02-28 16:37:32 -0300

    Replace assembly for abi.decode.

--- contracts/token/ERC20/SafeERC20.sol
+++ contracts/token/ERC20/SafeERC20.sol
@@ -16,11 +16,11 @@ library SafeERC20 {
     using SafeMath for uint256;
 
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        callAndAssertSuccess(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
 
     function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        callAndAssertSuccess(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
 
     function safeApprove(IERC20 token, address spender, uint256 value) internal {
@@ -28,55 +28,35 @@ library SafeERC20 {
         // or when resetting it to zero. To increase and decrease it, use
         // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
         require((value == 0) || (token.allowance(address(this), spender) == 0));
-        callAndAssertSuccess(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
 
     function safeIncreaseAllowance(IERC20 token, address spender, uint256 value) internal {
         uint256 newAllowance = token.allowance(address(this), spender).add(value);
-        callAndAssertSuccess(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
     }
 
     function safeDecreaseAllowance(IERC20 token, address spender, uint256 value) internal {
         uint256 newAllowance = token.allowance(address(this), spender).sub(value);
-        callAndAssertSuccess(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, newAllowance));
     }
 
     /**
-     * @dev Performs a function call on a token, asserting its success, which is defined by a) the call succeeding,
-     * and b) the return data being either empty or true.
-     * @param token The token targeted byt he call.
+     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
+     * on the return value: the return value is optional (but if data is returned, it must equal true).
+     * @param token The token targeted by the call.
      * @param data The call data (encoded using abi.encode or one of its variants).
      */
-    function callAndAssertSuccess(IERC20 token, bytes memory data) private {
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
         // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
         // we're implementing it ourselves.
 
         // solhint-disable-next-line avoid-low-level-calls
-        (bool success,) = address(token).call(data);
+        (bool success, bytes memory returndata) = address(token).call(data);
         require(success);
-        
-        require(getBooleanReturnValue());
-    }
 
-    /**
-     * @dev Parses the return value of the last contract call and attempts to return a boolean value out of it.
-     */
-    function getBooleanReturnValue() private pure returns (bool result) {
-        // We need to use inline assembly here, since it is the only way we can utilize the returndatasize opcode.
-
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            switch returndatasize()
-            case 0 { // No return value - assume success
-                result := 1
-            }
-            case 32 { // Standard ERC20 - read the returned 32 byte value
-                returndatacopy(0, 0, 32)
-                result := mload(0)
-            }
-            default { // Other return sizes are unsupported
-                revert(0, 0)
-            }
+        if (returndata.length > 0) {
+            require(abi.decode(returndata, (bool)));
         }
     }
 }
