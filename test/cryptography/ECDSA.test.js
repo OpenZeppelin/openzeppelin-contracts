8edafa9d854df1cc4800fef262e301293128347d
tbocek <tom@tomp2p.net>
2019-03-04 20:02:29 +0100

    better test description for testing the version 0, which returns a zero address

--- test/cryptography/ECDSA.test.js
+++ test/cryptography/ECDSA.test.js
@@ -18,8 +18,8 @@ contract('ECDSA', function ([_, anyone]) {
       // eslint-disable-next-line max-len
       const signatureWithoutVersion = '0x5d99b6f7f6d1f73d1a26497f2b1c89b24c0993913f86e9a2d02cd69887d9c94f3c880358579d811b21dd1b7fd9bb01c1d81d10e69f0384e675c32b39643be892';
 
-      context('with 00 as version value (wrong)', function () {
-        it('works', async function () {
+      context('with 00 as version value', function () {
+        it('returns 0', async function () {
           const version = '00';
           const signature = signatureWithoutVersion + version;
           (await this.ecdsa.recover(TEST_MESSAGE, signature)).should.equal(
