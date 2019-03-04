4abfaf18e24324135bc572a69ab8455c06c5289f
tbocek <tom@tomp2p.net>
2019-03-04 20:23:04 +0100

    Check that the conversion from 0/1 to 27/28 only happens if its 0/1

--- test/helpers/sign.js
+++ test/helpers/sign.js
@@ -10,10 +10,13 @@ function toEthSignedMessageHash (messageHex) {
 }
 
 function fixSignature (signature) {
-  //in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
-  //signature malleability
-  //https://github.com/ethereum/go-ethereum/blob/master/internal/ethapi/api.go#L447
-  const v = parseInt(signature.slice(130, 132), 16) + 27;
+  // in geth its always 27/28, in ganache its 0/1. Change to 27/28 to prevent
+  // signature malleability if version is 0/1
+  // see https://github.com/ethereum/go-ethereum/blob/v1.8.23/internal/ethapi/api.go#L465
+  let v = parseInt(signature.slice(130, 132), 16);
+  if (v < 27) {
+    v += 27;
+  }
   const vHex = v.toString(16);
   return signature.slice(0, 130) + vHex;
 }
