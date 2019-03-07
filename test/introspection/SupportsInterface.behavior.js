359dd71d59eea1bee5cc8b3f3c1f01cf44d535a4
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2019-03-07 14:16:19 +1100

    Compute function signatures in ERC165 interfaces
    The ERC165 tests currently precompute some known interface ids.
    This commit extracts the interfaces into a separate object and
    precomputes the individual function signatures.
    
    This will be useful to identify contracts that support an interface
    but do not implement all of the corresponding functions.

--- test/introspection/SupportsInterface.behavior.js
+++ test/introspection/SupportsInterface.behavior.js
@@ -1,10 +1,10 @@
 const { makeInterfaceId } = require('openzeppelin-test-helpers');
 
-const INTERFACE_IDS = {
-  ERC165: makeInterfaceId([
+const INTERFACES = {
+  ERC165: [
     'supportsInterface(bytes4)',
-  ]),
-  ERC721: makeInterfaceId([
+  ],
+  ERC721: [
     'balanceOf(address)',
     'ownerOf(uint256)',
     'approve(address,uint256)',
@@ -14,21 +14,31 @@ const INTERFACE_IDS = {
     'transferFrom(address,address,uint256)',
     'safeTransferFrom(address,address,uint256)',
     'safeTransferFrom(address,address,uint256,bytes)',
-  ]),
-  ERC721Enumerable: makeInterfaceId([
+  ],
+  ERC721Enumerable: [
     'totalSupply()',
     'tokenOfOwnerByIndex(address,uint256)',
     'tokenByIndex(uint256)',
-  ]),
-  ERC721Metadata: makeInterfaceId([
+  ],
+  ERC721Metadata: [
     'name()',
     'symbol()',
     'tokenURI(uint256)',
-  ]),
-  ERC721Exists: makeInterfaceId([
+  ],
+  ERC721Exists: [
     'exists(uint256)',
-  ]),
+  ],
 };
+
+const INTERFACE_IDS = {};
+const FN_SIGNATURES = {};
+for (const k of Object.getOwnPropertyNames(INTERFACES)) {
+  INTERFACE_IDS[k] = makeInterfaceId(INTERFACES[k]);
+  for (const fnName of INTERFACES[k]) {
+    // the interface id of a single function is equivalent to its function signature
+    FN_SIGNATURES[fnName] = makeInterfaceId([fnName]);
+  }
+}
 
 function shouldSupportInterfaces (interfaces = []) {
   describe('ERC165\'s supportsInterface(bytes4)', function () {
