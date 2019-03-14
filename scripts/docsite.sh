43798a543c82b3f22f5e92ae799aabace4f52328
Francisco Giordano <frangio.1@gmail.com>
2019-03-14 18:04:32 -0300

    install and set up solidity-docgen

--- scripts/docsite.sh
+++ scripts/docsite.sh
@@ -9,10 +9,14 @@ docsite="${DOCSITE:-https://github.com/OpenZeppelin/openzeppelin-docsite.git}"
 if [ -d docsite ]; then
   cd docsite
   git pull --ff-only "$docsite"
+  cd ..
 else
   git clone "$docsite" docsite
-  cd docsite
 fi
+
+npm run docgen
+
+cd docsite
 
 npm install
 
