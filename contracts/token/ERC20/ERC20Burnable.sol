41eb342c37417b12a96d37535106ddcd15e8d43b
skyge <18844062006@163.com>
2019-03-20 18:25:41 +0800

    Add a return value in the contract ERC20Burnable.

--- contracts/token/ERC20/ERC20Burnable.sol
+++ contracts/token/ERC20/ERC20Burnable.sol
@@ -10,17 +10,21 @@ contract ERC20Burnable is ERC20 {
     /**
      * @dev Burns a specific amount of tokens.
      * @param value The amount of token to be burned.
+     * @return A boolean that indicates if the operation was successful.
      */
-    function burn(uint256 value) public {
+    function burn(uint256 value) public returns (bool) {
         _burn(msg.sender, value);
+        return true;
     }
 
     /**
      * @dev Burns a specific amount of tokens from the target address and decrements allowance
      * @param from address The account whose tokens will be burned.
      * @param value uint256 The amount of token to be burned.
+     * @return A boolean that indicates if the operation was successful.
      */
-    function burnFrom(address from, uint256 value) public {
+    function burnFrom(address from, uint256 value) public returns (bool) {
         _burnFrom(from, value);
+        return true;
     }
 }
