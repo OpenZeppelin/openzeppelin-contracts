5c0ee8acc7008dfa3121a5aa5bc237bbca0bb3f6
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-03-27 18:16:33 -0300

    Add first operator send tests.

--- test/drafts/ERC777/ERC777.test.js
+++ test/drafts/ERC777/ERC777.test.js
@@ -8,6 +8,7 @@ contract.only('ERC777', function ([_, registryFunder, initialHolder, defaultOper
   const name = 'ERC777Test';
   const symbol = '777T';
   const data = web3.utils.sha3('OZ777TestData');
+  const operatorData = web3.utils.sha3('OZ777TestOperatorData');
 
   const defaultOperators = [defaultOperatorA, defaultOperatorB];
 
@@ -24,6 +25,29 @@ contract.only('ERC777', function ([_, registryFunder, initialHolder, defaultOper
         amount,
         data,
         operatorData: null,
+      });
+
+      const finalFromBalance = await this.token.balanceOf(from);
+      const finalToBalance = await this.token.balanceOf(to);
+
+      finalToBalance.sub(initialToBalance).should.be.bignumber.equal(amount);
+      finalFromBalance.sub(initialFromBalance).should.be.bignumber.equal(amount.neg());
+    });
+  }
+
+  function assertOperatorSendSuccess(from, operator, to, amount, data, operatorData) {
+    it(`operator can send an amount of ${amount}`, async function () {
+      const initialFromBalance = await this.token.balanceOf(from);
+      const initialToBalance = await this.token.balanceOf(to);
+
+      const { logs } = await this.token.operatorSend(from, to, amount, data, operatorData, { from: operator });
+      expectEvent.inLogs(logs, 'Sent', {
+        operator,
+        from,
+        to,
+        amount,
+        data,
+        operatorData,
       });
 
       const finalFromBalance = await this.token.balanceOf(from);
@@ -98,26 +122,60 @@ contract.only('ERC777', function ([_, registryFunder, initialHolder, defaultOper
       context('when the sender has no tokens', async function () {
         const from = anyone;
 
-        assertSendSuccess(from, initialHolder, new BN('0'), data);
+        describe('direct send', function () {
+          assertSendSuccess(from, initialHolder, new BN('0'), data);
 
-        it('reverts when sending a non-zero amount', async function () {
-          await shouldFail.reverting(this.token.send(initialHolder, new BN('1'), data, { from }));
+          it('reverts when sending a non-zero amount', async function () {
+            await shouldFail.reverting(this.token.send(initialHolder, new BN('1'), data, { from }));
+          });
+        });
+
+        describe('operator send', function () {
+          assertOperatorSendSuccess(from, defaultOperatorA, initialHolder, new BN('0'), data, operatorData);
+
+          it('reverts when sending a non-zero amount', async function () {
+            await shouldFail.reverting(
+              this.token.operatorSend(from, initialHolder, new BN('1'), data, operatorData, { from: operator })
+            );
+          });
         });
       });
 
       context('when the sender has tokens', async function () {
         const from = initialHolder;
 
-        assertSendSuccess(from, anyone, new BN('0'), data);
-        assertSendSuccess(from, anyone, new BN('1'), data);
+        describe('direct send', function () {
+          assertSendSuccess(from, anyone, new BN('0'), data);
+          assertSendSuccess(from, anyone, new BN('1'), data);
 
-        it('reverts when sending more than the balance', async function () {
-          const balance = await this.token.balanceOf(from);
-          await shouldFail.reverting(this.token.send(anyone, balance.addn(1), data, { from }));
+          it('reverts when sending more than the balance', async function () {
+            const balance = await this.token.balanceOf(from);
+            await shouldFail.reverting(this.token.send(anyone, balance.addn(1), data, { from }));
+          });
+
+          it('reverts when sending to the zero address', async function () {
+            await shouldFail.reverting(this.token.send(ZERO_ADDRESS, new BN('1'), data, { from }));
+          });
         });
 
-        it('reverts when sending to the zero address', async function () {
-          await shouldFail.reverting(this.token.send(ZERO_ADDRESS, new BN('1'), data, { from }));
+        describe('operator send', function () {
+          assertOperatorSendSuccess(from, defaultOperatorA, anyone, new BN('0'), data, operatorData);
+          assertOperatorSendSuccess(from, defaultOperatorB, anyone, new BN('1'), data, operatorData);
+
+          it('reverts when sending more than the balance', async function () {
+            const balance = await this.token.balanceOf(from);
+            await shouldFail.reverting(
+              this.token.operatorSend(from, anyone, balance.addn(1), data, operatorData, { from: defaultOperatorA })
+            );
+          });
+
+          it('reverts when sending to the zero address', async function () {
+            await shouldFail.reverting(
+              this.token.operatorSend(
+                from, ZERO_ADDRESS, new BN('1'), data, operatorData, { from: defaultOperatorA }
+              )
+            );
+          });
         });
       });
     });
