807ab22bbba9314ae3c2db5c06e82f812f2e842d
Francisco Giordano <frangio.1@gmail.com>
2019-03-31 00:25:05 -0300

    add docsite as dependency and adjust script

--- scripts/docsite.sh
+++ scripts/docsite.sh
@@ -1,26 +1,8 @@
 #!/usr/bin/env bash
 
+# usage: npm run docsite [build|start]
+
 set -o errexit
 
-cmd="$1"
-
-docsite_repo="${DOCSITE_REPO:-https://github.com/OpenZeppelin/openzeppelin-docsite.git}"
-
-docs_path="$(realpath docs)"
-
-if [ -d docsite ]; then
-  cd docsite
-  git checkout next
-  git pull --ff-only "$docsite_repo" next
-  cd ..
-else
-  git clone --branch next "$docsite_repo" docsite
-fi
-
 npm run docgen
-
-cd docsite
-
-npm install
-
-env DOCS_PATH="$docs_path" npm run "$cmd"
+npx openzeppelin-docsite-run "$1"
