9fc10f5b7f3632e4bf226dab5e188f27dc46d5e2
prince <sinhaprince013@gmail.com>
2019-04-10 23:10:28 +0530

    removed unnecessary require

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -68,7 +68,6 @@ contract ERC721 is ERC165, IERC721 {
      */
     function ownerOf(uint256 tokenId) public view returns (address) {
         address owner = _tokenOwner[tokenId];
-        require(owner != address(0));
         return owner;
     }
 
