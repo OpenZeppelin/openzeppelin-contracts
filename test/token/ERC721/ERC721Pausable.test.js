4f8461cb823c9840e41ed2e27c00c2bc7b6757c3
Shei <shei@Sheis-MacBook.local>
2019-04-11 17:14:23 -0700

    removing unused variables

--- test/token/ERC721/ERC721Pausable.test.js
+++ test/token/ERC721/ERC721Pausable.test.js
@@ -6,10 +6,7 @@ const { shouldBehaveLikePublicRole } = require('../../behaviors/access/roles/Pub
 const ERC721PausableMock = artifacts.require('ERC721PausableMock.sol');
 
 contract('ERC721Pausable', function ([
-  _,
   creator,
-  owner,
-  operator,
   otherPauser,
   ...accounts
 ]) {
