9486ebd0267109157713ea98672257c3628d16bc
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-04-16 19:09:15 -0300

    Fianal update of revert reason strings.

--- contracts/token/ERC20/SafeERC20.sol
+++ contracts/token/ERC20/SafeERC20.sol
@@ -30,7 +30,9 @@ library SafeERC20 {
         // or when resetting it to zero. To increase and decrease it, use
         // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
         // solhint-disable-next-line max-line-length
-        require((value == 0) || (token.allowance(address(this), spender) == 0), "SafeERC20: (value == 0) || (token.allowance(address(this), spender) == 0)");
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
         callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
 
@@ -59,15 +61,15 @@ library SafeERC20 {
         //  2. The call itself is made, and success asserted
         //  3. The return value is decoded, which in turn checks the size of the returned data.
         // solhint-disable-next-line max-line-length
-        require(address(token).isContract(), "SafeERC20: token address is not a contract address");
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
 
         // solhint-disable-next-line avoid-low-level-calls
         (bool success, bytes memory returndata) = address(token).call(data);
-        require(success, "SafeERC20: the previous low-level call returned false");
+        require(success, "SafeERC20: low-level call failed");
 
         if (returndata.length > 0) { // Return data is optional
             // solhint-disable-next-line max-line-length
-            require(abi.decode(returndata, (bool)), "SafeERC20: abi.decode(returndata, (bool))");
+            require(abi.decode(returndata, (bool)), "SafeERC20: unknown low-level call return data");
         }
     }
 }
