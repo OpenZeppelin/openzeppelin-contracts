1567f30d0553ad6559b5956ee8235aaaab3cecef
Francisco Giordano <frangio.1@gmail.com>
2019-04-23 18:58:37 -0300

    add solhint ignore directive

--- contracts/crowdsale/distribution/PostDeliveryCrowdsale.sol
+++ contracts/crowdsale/distribution/PostDeliveryCrowdsale.sol
@@ -57,6 +57,7 @@ contract PostDeliveryCrowdsale is TimedCrowdsale {
  * @dev Similar to an Escrow for tokens, this contract allows its primary account to spend its tokens as it sees fit.
  * This contract is an internal helper for PostDeliveryCrowdsale, and should not be used outside of this context.
  */
+// solhint-disable-next-line contract-name-camelcase
 contract __unstable__TokenVault is Secondary {
     function transfer(IERC20 token, address to, uint256 amount) public onlyPrimary {
         token.transfer(to, amount);
