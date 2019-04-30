381f1cd600cfbe63fcc6d6578b3702febf6c6d2a
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-04-30 16:18:20 -0300

    Improve documentation.

--- contracts/drafts/ERC777/ERC777.sol
+++ contracts/drafts/ERC777/ERC777.sol
@@ -90,11 +90,28 @@ contract ERC777 is IERC777, ERC20Detailed {
         _send(msg.sender, from, to, amount, data, operatorData);
     }
 
+    /**
+     * @dev Transfer token to a specified address.
+     * Required for ERC20 compatiblity. Note that transferring tokens this way may result in locked tokens (i.e. tokens
+     * can be sent to a contract that does not implement the ERC777TokensRecipient interface).
+     * @param to The address to transfer to.
+     * @param value The amount to be transferred.
+     */
     function transfer(address to, uint256 value) external returns (bool) {
         _send(msg.sender, msg.sender, to, value, "", "");
         return true;
     }
 
+    /**
+     * @dev Transfer tokens from one address to another.
+     * Note that while this function emits an Approval event, this is not required as per the specification,
+     * and other compliant implementations may not emit the event.
+     * Required for ERC20 compatiblity. Note that transferring tokens this way may result in locked tokens (i.e. tokens
+     * can be sent to a contract that does not implement the ERC777TokensRecipient interface).
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     */
     function transferFrom(address from, address to, uint256 value) external returns (bool) {
         _send(msg.sender, from, to, value, "", "");
         _approve(from, msg.sender, _allowances[from][msg.sender].sub(value));
@@ -154,6 +171,16 @@ contract ERC777 is IERC777, ERC20Detailed {
         emit RevokedOperator(operator, msg.sender);
     }
 
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * Required for ERC20 compatilibity.
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
     function approve(address spender, uint256 value) external returns (bool) {
         _approve(msg.sender, spender, value);
         return true;
@@ -209,6 +236,13 @@ contract ERC777 is IERC777, ERC20Detailed {
             _operators[tokenHolder][operator];
     }
 
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * Required for ERC20 compatibility.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
     function allowance(address owner, address spender) public view returns (uint256) {
         return _allowances[owner][spender];
     }
