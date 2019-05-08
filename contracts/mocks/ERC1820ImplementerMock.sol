1292b6abab424aee491e4d9ce2cd1c5a5022b0cf
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-05-08 16:11:00 -0300

    Move ERC1820 and ERC777 out of drafts (#1742)
    * Moved ERC1820 related contracts out of drafts and into introspection.
    
    * Moved ERC777 related contracts out of drafts and into token.
    
    (cherry picked from commit c794c9661722ad44a93fe163b3fcc624eacd4361)

--- contracts/mocks/ERC1820ImplementerMock.sol
+++ contracts/mocks/ERC1820ImplementerMock.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "../drafts/ERC1820Implementer.sol";
+import "../introspection/ERC1820Implementer.sol";
 
 contract ERC1820ImplementerMock is ERC1820Implementer {
     function registerInterfaceForAddress(bytes32 interfaceHash, address account) public {
