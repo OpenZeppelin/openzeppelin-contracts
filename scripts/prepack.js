46ed5df1eaa4b2cf22d514d00f22ab622d2a8169
Francisco Giordano <frangio.1@gmail.com>
2019-05-14 13:18:49 -0300

    make prepack use pkg.files

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -4,25 +4,38 @@
 
 const fs = require('fs');
 const path = require('path');
-const proc = require('child_process');
+const process = require('process');
+const cp = require('child_process');
+const match = require('micromatch');
 
 function exec(cmd, ...args) {
-  proc.execFileSync(cmd, args, { stdio: 'inherit' });
+  cp.execFileSync(cmd, args, { stdio: 'inherit' });
+}
+
+function readJSON(path) {
+  return JSON.parse(fs.readFileSync(path));
 }
 
 exec('npm', 'run', 'compile');
 
-const ignoredPaths = [
-  path.resolve('contracts/mocks'),
-  path.resolve('contracts/examples'),
-];
+const pkgFiles = readJSON('package.json').files;
+
+// Get only negated patterns.
+const ignorePatterns = pkgFiles
+  .filter(pat => pat.startsWith('!'))
+// Add **/* to ignore all files contained in the directories.
+  .flatMap(pat => [pat, path.join(pat, '**/*')])
+// Remove the negation part. Makes micromatch usage more intuitive.
+  .map(pat => pat.slice(1));
 
 const artifactsDir = 'build/contracts';
 
 for (const artifact of fs.readdirSync(artifactsDir)) {
   const fullArtifactPath = path.join(artifactsDir, artifact);
-  const { sourcePath } = JSON.parse(fs.readFileSync(fullArtifactPath));
-  const ignore = ignoredPaths.find(p => sourcePath.startsWith(p));
+  const { sourcePath: fullSourcePath } = readJSON(fullArtifactPath);
+  const sourcePath = path.relative('.', fullSourcePath);
+
+  const ignore = match.any(sourcePath, ignorePatterns);
 
   if (ignore) {
     fs.unlinkSync(fullArtifactPath);
