8a747938c6e1d0294709fd4a67c0108cffca69bf
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-05-15 13:34:43 -0300

    Add test for transferFrom zero address to behavior.

--- test/token/ERC20/ERC20.behavior.js
+++ test/token/ERC20/ERC20.behavior.js
@@ -73,101 +73,116 @@ function shouldBehaveLikeERC20 (errorPrefix, initialSupply, initialHolder, recip
   describe('transfer from', function () {
     const spender = recipient;
 
-    describe('when the recipient is not the zero address', function () {
-      const to = anotherAccount;
+    describe('when the token owner is not the zero address', function () {
+      const tokenOwner = initialHolder;
 
-      describe('when the spender has enough approved balance', function () {
-        beforeEach(async function () {
-          await this.token.approve(spender, initialSupply, { from: initialHolder });
-        });
+      describe('when the recipient is not the zero address', function () {
+        const to = anotherAccount;
 
-        describe('when the initial holder has enough balance', function () {
-          const amount = initialSupply;
-
-          it('transfers the requested amount', async function () {
-            await this.token.transferFrom(initialHolder, to, amount, { from: spender });
-
-            (await this.token.balanceOf(initialHolder)).should.be.bignumber.equal('0');
-
-            (await this.token.balanceOf(to)).should.be.bignumber.equal(amount);
+        describe('when the spender has enough approved balance', function () {
+          beforeEach(async function () {
+            await this.token.approve(spender, initialSupply, { from: initialHolder });
           });
 
-          it('decreases the spender allowance', async function () {
-            await this.token.transferFrom(initialHolder, to, amount, { from: spender });
+          describe('when the token owner has enough balance', function () {
+            const amount = initialSupply;
 
-            (await this.token.allowance(initialHolder, spender)).should.be.bignumber.equal('0');
-          });
+            it('transfers the requested amount', async function () {
+              await this.token.transferFrom(tokenOwner, to, amount, { from: spender });
 
-          it('emits a transfer event', async function () {
-            const { logs } = await this.token.transferFrom(initialHolder, to, amount, { from: spender });
+              (await this.token.balanceOf(tokenOwner)).should.be.bignumber.equal('0');
 
-            expectEvent.inLogs(logs, 'Transfer', {
-              from: initialHolder,
-              to: to,
-              value: amount,
+              (await this.token.balanceOf(to)).should.be.bignumber.equal(amount);
+            });
+
+            it('decreases the spender allowance', async function () {
+              await this.token.transferFrom(tokenOwner, to, amount, { from: spender });
+
+              (await this.token.allowance(tokenOwner, spender)).should.be.bignumber.equal('0');
+            });
+
+            it('emits a transfer event', async function () {
+              const { logs } = await this.token.transferFrom(tokenOwner, to, amount, { from: spender });
+
+              expectEvent.inLogs(logs, 'Transfer', {
+                from: tokenOwner,
+                to: to,
+                value: amount,
+              });
+            });
+
+            it('emits an approval event', async function () {
+              const { logs } = await this.token.transferFrom(tokenOwner, to, amount, { from: spender });
+
+              expectEvent.inLogs(logs, 'Approval', {
+                owner: tokenOwner,
+                spender: spender,
+                value: await this.token.allowance(tokenOwner, spender),
+              });
             });
           });
 
-          it('emits an approval event', async function () {
-            const { logs } = await this.token.transferFrom(initialHolder, to, amount, { from: spender });
+          describe('when the token owner does not have enough balance', function () {
+            const amount = initialSupply.addn(1);
 
-            expectEvent.inLogs(logs, 'Approval', {
-              owner: initialHolder,
-              spender: spender,
-              value: await this.token.allowance(initialHolder, spender),
+            it('reverts', async function () {
+              await shouldFail.reverting.withMessage(this.token.transferFrom(
+                tokenOwner, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
+              );
             });
           });
         });
 
-        describe('when the initial holder does not have enough balance', function () {
-          const amount = initialSupply.addn(1);
+        describe('when the spender does not have enough approved balance', function () {
+          beforeEach(async function () {
+            await this.token.approve(spender, initialSupply.subn(1), { from: tokenOwner });
+          });
 
-          it('reverts', async function () {
-            await shouldFail.reverting.withMessage(this.token.transferFrom(
-              initialHolder, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
-            );
+          describe('when the token owner has enough balance', function () {
+            const amount = initialSupply;
+
+            it('reverts', async function () {
+              await shouldFail.reverting.withMessage(this.token.transferFrom(
+                tokenOwner, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
+              );
+            });
+          });
+
+          describe('when the token owner does not have enough balance', function () {
+            const amount = initialSupply.addn(1);
+
+            it('reverts', async function () {
+              await shouldFail.reverting.withMessage(this.token.transferFrom(
+                tokenOwner, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
+              );
+            });
           });
         });
       });
 
-      describe('when the spender does not have enough approved balance', function () {
+      describe('when the recipient is the zero address', function () {
+        const amount = initialSupply;
+        const to = ZERO_ADDRESS;
+
         beforeEach(async function () {
-          await this.token.approve(spender, initialSupply.subn(1), { from: initialHolder });
+          await this.token.approve(spender, amount, { from: tokenOwner });
         });
 
-        describe('when the initial holder has enough balance', function () {
-          const amount = initialSupply;
-
-          it('reverts', async function () {
-            await shouldFail.reverting.withMessage(this.token.transferFrom(
-              initialHolder, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
-            );
-          });
-        });
-
-        describe('when the initial holder does not have enough balance', function () {
-          const amount = initialSupply.addn(1);
-
-          it('reverts', async function () {
-            await shouldFail.reverting.withMessage(this.token.transferFrom(
-              initialHolder, to, amount, { from: spender }), 'SafeMath: subtraction overflow'
-            );
-          });
+        it('reverts', async function () {
+          await shouldFail.reverting.withMessage(this.token.transferFrom(
+            tokenOwner, to, amount, { from: spender }), `${errorPrefix}: transfer to the zero address`
+          );
         });
       });
     });
 
-    describe('when the recipient is the zero address', function () {
-      const amount = initialSupply;
-      const to = ZERO_ADDRESS;
-
-      beforeEach(async function () {
-        await this.token.approve(spender, amount, { from: initialHolder });
-      });
+    describe('when the token owner is the zero address', function () {
+      const amount = 0;
+      const to = recipient;
 
       it('reverts', async function () {
         await shouldFail.reverting.withMessage(this.token.transferFrom(
-          initialHolder, to, amount, { from: spender }), `${errorPrefix}: transfer to the zero address`
+          ZERO_ADDRESS, to, amount, { from: to }), `${errorPrefix}: transfer from the zero address`
         );
       });
     });
