ae7f8d700679106dfd1d4aa1b15706f3a3440fef
prashant.prabhakar <prashant.prabhakar@sofocle.com>
2019-05-18 16:37:48 +0530

    added check to restrain from approving to self

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -171,7 +171,7 @@ contract ERC20 is IERC20 {
     function _approve(address owner, address spender, uint256 value) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
-
+        require(spender != msg.sender, "ERC20: approval to current owner");
         _allowances[owner][spender] = value;
         emit Approval(owner, spender, value);
     }
