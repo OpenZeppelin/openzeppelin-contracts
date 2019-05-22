036dd9bd6ec834a2882e0f2e5d1209eaf4296979
Francisco Giordano <frangio.1@gmail.com>
2019-05-22 18:35:40 -0300

    Change prepack npm script to prepare (take 2) (#1755)
    * update truffle to include bugfix
    
    * change prepack script to prepare
    
    * add npx in compile script
    
    * fix for older node
    
    * rename script file to prepare

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,7 +4,4 @@ if [ "$SOLC_NIGHTLY" = true ]; then
   docker pull ethereum/solc:nightly
 fi
 
-# Necessary to avoid an error in Truffle
-rm -rf build/contracts
-
-truffle compile
+npx truffle compile
