906b18f144b8a9a0f4661070cfb95f8dbdf68ae5
Francisco Giordano <frangio.1@gmail.com>
2019-06-06 11:54:19 -0300

    add updated zos files except for mainnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -12,6 +12,35 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01,
     },
+
+    mainnet: {
+      host: 'localhost',
+      port: 8565,
+      network_id: '1',
+      gasPrice: 2000000000,
+    },
+
+    ropsten: {
+      host: 'localhost',
+      port: 8565,
+      network_id: '3',
+      gasPrice: 2000000000,
+    },
+
+    rinkeby: {
+      host: 'localhost',
+      port: 8565,
+      network_id: '4',
+      gasPrice: 0,
+    },
+
+    kovan: {
+      host: 'localhost',
+      port: 8565,
+      network_id: '42',
+      gasPrice: 0,
+    },
+
   },
 
   compilers: {
