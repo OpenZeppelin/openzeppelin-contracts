98469901f57fc62608cd07ede3b2f9da15ecaa58
Philippe Castonguay <ph.castonguay@gmail.com>
2019-06-18 19:36:45 -0400

    Fix

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -19,7 +19,7 @@ library Address {
         // construction, since the code is only stored at the end of the
         // constructor execution.
 
-            zbytes32 codehash;
+        bytes32 codehash;
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         // solhint-disable-next-line no-inline-assembly
         assembly { codehash := extcodehash(account) }
