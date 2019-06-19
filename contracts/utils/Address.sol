72364d86ffea64a20377f71553d21f6fe939066d
Philippe Castonguay <ph.castonguay@gmail.com>
2019-06-19 11:22:12 -0400

    Add explainer

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -18,6 +18,10 @@ library Address {
         // This method relies in extcodesize, which returns 0 for contracts in
         // construction, since the code is only stored at the end of the
         // constructor execution.
+        
+        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
+        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
+        // for accounts without code, i.e. `keccak256('')`
 
         bytes32 codehash;
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
