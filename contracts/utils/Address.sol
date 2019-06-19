c6f7fbe1c844bf0dcbf26c481a6d836404c694ca
Philippe Castonguay <ph.castonguay@gmail.com>
2019-06-19 11:22:27 -0400

    Update Address.sol

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -22,7 +22,6 @@ library Address {
         // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
         // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
         // for accounts without code, i.e. `keccak256('')`
-
         bytes32 codehash;
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         // solhint-disable-next-line no-inline-assembly
