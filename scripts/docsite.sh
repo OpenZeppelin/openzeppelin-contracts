c5c0e22c8963971381ba8e68198cf3ef0131ae3e
Francisco Giordano <frangio.1@gmail.com>
2019-07-17 17:33:51 -0300

    fix docsite build script

--- scripts/docsite.sh
+++ scripts/docsite.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# usage: npm run docsite
+if [ "$1" != "build" -a "$1" != "start" ];then
+  echo "usage: npm run docsite (build|start)" >&2
+  exit 1
+fi
 
 set -o errexit
 
@@ -8,9 +11,16 @@ if [ ! -d openzeppelin-docs ]; then
   git clone https://github.com/frangio/openzeppelin-docs.git
 fi
 
-git -C openzeppelin-docs pull
+git -C openzeppelin-docs pull -q
 
-npx concurrently \
-  'nodemon --delay 1 -e "*" -w contracts -w "docs/*.hbs" -x npm run prepare-docs' \
-  'cd docs; env DISABLE_PREPARE_DOCS= nodemon --delay 1 -e adoc,yml ../openzeppelin-docs/build-local.js' \
-  'http-server -c-1 openzeppelin-docs/build/site'
+if [ "$1" = "build" ]; then
+  npm run prepare-docs
+  cd docs
+  env DISABLE_PREPARE_DOCS= node ../openzeppelin-docs/build-local.js
+
+elif [ "$1" = "start" ]; then
+  npx concurrently \
+    'nodemon --delay 1 -e "*" -w contracts -w "docs/*.hbs" -x npm run prepare-docs' \
+    'cd docs; env DISABLE_PREPARE_DOCS= nodemon --delay 1 -e adoc,yml ../openzeppelin-docs/build-local.js' \
+    'http-server -c-1 openzeppelin-docs/build/site'
+fi
