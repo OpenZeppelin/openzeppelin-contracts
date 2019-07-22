9f78dc0e9546f77846d147034574482283171bcb
CallMeGwei <34364155+CallMeGwei@users.noreply.github.com>
2019-07-22 18:50:47 -0500

    remove non-documentation changes from branch

--- contracts/mocks/OwnableCautiouMock.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-import "../ownership/OwnableCautious.sol";
-
-contract OwnableCautiousMock is OwnableCautious {
-    function onlyOwnerMock() public view onlyOwner {
-        // solhint-disable-previous-line no-empty-blocks
-    }
-}
