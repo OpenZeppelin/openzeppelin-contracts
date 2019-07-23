90831c897019433f92ae814b22c3105f1464f048
Francisco Giordano <frangio.1@gmail.com>
2019-07-23 15:54:12 -0300

    Squash merge of branch docs-v2.x into master

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -4,10 +4,4 @@ OUTDIR=docs/modules/api/pages/
 
 rm -rf "$OUTDIR"
 solidity-docgen -t docs -o "$OUTDIR" -e contracts/mocks,contracts/examples
-
-gen-nav() {
-  echo '.API'
-  find "$OUTDIR" -type f | sed -Ee "s:$OUTDIR(.+):* xref\:\1[]:"
-}
-
-gen-nav > "$OUTDIR/../nav.adoc"
+node scripts/gen-nav.js "$OUTDIR" > "$OUTDIR/../nav.adoc"
