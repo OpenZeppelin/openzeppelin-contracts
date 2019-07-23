50d596a1e57fb0c43aa9ffe811a7d7fdc22ba88d
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-07-23 17:09:05 -0300

    Add RelayHub deployment to tests

--- test/drafts/meta-tx/GSNContext.test.js
+++ test/drafts/meta-tx/GSNContext.test.js
@@ -1,11 +1,11 @@
-require('openzeppelin-test-helpers');
+const { singletons } = require('openzeppelin-test-helpers');
 
 const GSNContextMock = artifacts.require('GSNContextMock');
 const ContextMockCaller = artifacts.require('ContextMockCaller');
 
 const { shouldBehaveLikeRegularContext } = require('./Context.behavior');
 
-contract('GSNContext', function ([_, sender, rhub]) {
+contract('GSNContext', function ([_, deployer, sender, rhub]) {
   beforeEach(async function () {
     this.context = await GSNContextMock.new(rhub);
     this.caller = await ContextMockCaller.new();
@@ -13,5 +13,11 @@ contract('GSNContext', function ([_, sender, rhub]) {
 
   context('when called directly', function () {
     shouldBehaveLikeRegularContext(sender);
+  });
+
+  context('when receiving a relayed call', function () {
+    beforeEach(async function () {
+      this.relayHub = await singletons.RelayHub(deployer);
+    });
   });
 });
