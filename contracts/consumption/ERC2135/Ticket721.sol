bb4be3b8b9ee7cf328bc84901275218601c158fe
Zainan Victor Zhou <zzn@zzn.im>
2019-07-22 21:46:19 -0700

    Fix lint and doc

--- contracts/consumption/ERC2135/Ticket721.sol
+++ contracts/consumption/ERC2135/Ticket721.sol
@@ -10,27 +10,28 @@ import "../../token/ERC721/ERC721Mintable.sol";
  */
 contract Ticket721 is IERC2135, ERC721Mintable {
 
-  mapping(uint256 => bool) private tickets;
+    mapping(uint256 => bool) private tickets;
 
-  function issueTicket(address holder, uint256 ticketId) public {
-    require (!tickets[ticketId]); // ticket needs to be not issued yet;
-    require (isMinter(msg.sender), "Only minter can issue ticket.");
-    _mint(holder, ticketId);
-    tickets[ticketId] = true;
-    emit OnTicketIssued(ticketId);
-  }
+    function issueTicket(address holder, uint256 ticketId) public {
+        require(!tickets[ticketId]);
+        // ticket needs to be not issued yet;
+        require(isMinter(msg.sender), "Only minter can issue ticket.");
+        _mint(holder, ticketId);
+        tickets[ticketId] = true;
+        emit OnTicketIssued(ticketId);
+    }
 
-  function consume(uint256 ticketId) public returns (bool success) {
-    require (tickets[ticketId], "Ticket needs to be consumable.");
-    require (msg.sender == ownerOf(ticketId), "Ticket should be held by tx sender to be consumed.");
-    tickets[ticketId] = false;
-    emit OnConsumption(ticketId);
-    return true;
-  }
+    function consume(uint256 ticketId) public returns (bool success) {
+        require(tickets[ticketId], "Ticket needs to be consumable.");
+        require(msg.sender == ownerOf(ticketId), "Ticket should be held by tx sender to be consumed.");
+        tickets[ticketId] = false;
+        emit OnConsumption(ticketId);
+        return true;
+    }
 
-  function isConsumable(uint256 ticketId) public view returns (bool consumable) {
-    return tickets[ticketId];
-  }
+    function isConsumable(uint256 ticketId) public view returns (bool consumable) {
+        return tickets[ticketId];
+    }
 
-  event OnTicketIssued(uint256 indexed ticketId);
+    event OnTicketIssued(uint256 indexed ticketId);
 }
