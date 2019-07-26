33856238e9776eb5e13a01ef8a67ee995843c891
Francisco Giordano <frangio.1@gmail.com>
2019-07-26 13:29:24 -0300

    add @openzeppelin/contracts as subpackage with release automation

---
+++ scripts/prepare-contracts-package.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# cd to the root of the repo
+cd "$(git rev-parse --show-toplevel)"
+
+# used to avoid re-compilation during publishing of both packages
+if [[ ! -v ALREADY_COMPILED ]]; then
+  npm run prepare
+fi
+
+cp README.md contracts/
+cp -Tr build/contracts contracts/build
