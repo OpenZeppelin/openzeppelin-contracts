da9952ad2513a98d12edd31fb24d356f4b182942
Francisco Giordano <frangio.1@gmail.com>
2019-07-26 15:25:42 -0300

    use a saner version script

--- scripts/release/version.sh
+++ scripts/release/version.sh
@@ -3,13 +3,4 @@
 set -o errexit
 
 scripts/release/update-changelog-release-date.js
-scripts/release/update-ethpm-version.js
-
-cd contracts
-
-# forward the arguments to npm version by taking
-# them from the npm_config_argv env variable
-args="$(node -pe 'JSON.parse(process.argv[1]).cooked.slice(1).join(" ")' "$npm_config_argv")"
-
-npm version --no-git-tag-version $args > /dev/null
-git add package.json
+scripts/release/synchronize-versions.js
