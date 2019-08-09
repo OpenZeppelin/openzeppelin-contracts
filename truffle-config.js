08056660642cd1d9e1455992ef287f1cd895b08d
Francisco Giordano <frangio.1@gmail.com>
2019-08-09 20:11:59 -0300

    Revert "set up gsn provider for coverage too"
    This reverts commit 8a7b5be5f942002710cba148f249cb888ee8e373.

--- truffle-config.js
+++ truffle-config.js
@@ -12,28 +12,24 @@ const solcNightly = {
 
 const useSolcNightly = process.env.SOLC_NIGHTLY === 'true';
 
-function getProvider (url, options) {
-  return new GSNDevProvider('http://localhost:8545', Object.assign({
-    txfee: 70,
-    useGSN: false,
-    // The last two accounts defined in test.sh
-    ownerAddress: '0x26be9c03ca7f61ad3d716253ee1edcae22734698',
-    relayerAddress: '0xdc5fd04802ea70f6e27aec12d56716624c98e749',
-  }, options));
-}
-
 module.exports = {
   networks: {
     development: {
-      provider: getProvider('http://localhost:8545'),
+      provider: new GSNDevProvider('http://localhost:8545', {
+        txfee: 70,
+        useGSN: false,
+        // The last two accounts defined in test.sh
+        ownerAddress: '0x26be9c03ca7f61ad3d716253ee1edcae22734698',
+        relayerAddress: '0xdc5fd04802ea70f6e27aec12d56716624c98e749',
+      }),
       network_id: '*', // eslint-disable-line camelcase
     },
     coverage: {
-      provider: getProvider('http://localhost:8555', {
-        gas: 0xfffffffffff,
-        gasPrice: 0x01,
-      }),
+      host: 'localhost',
       network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
     },
   },
 
