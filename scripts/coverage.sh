eaed1c468bc560ace7fb51b4b4373d9009d1d806
Francisco Giordano <frangio.1@gmail.com>
2019-08-12 17:46:41 +0200

    enable test run to fail in coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -o errexit -o pipefail
 
-SOLIDITY_COVERAGE=true scripts/test.sh
+log() {
+  echo "$*" >&2
+}
+
+SOLIDITY_COVERAGE=true scripts/test.sh || log "Test run failed"
 
 if [ "$CI" = true ]; then
   curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"
