e2aced1eb4b25da578eab11452140a4805c390e8
Francisco Giordano <frangio.1@gmail.com>
2019-08-16 15:29:31 +0200

    add content to gsn api ref

--- contracts/GSN/GSNRecipient.sol
+++ contracts/GSN/GSNRecipient.sol
@@ -11,10 +11,18 @@ import "./IRelayHub.sol";
  * must do so themselves.
  */
 contract GSNRecipient is IRelayRecipient, GSNContext, GSNBouncerBase {
+    /**
+     * @dev Returns the RelayHub address for this recipient contract.
+     */
     function getHubAddr() public view returns (address) {
         return _relayHub;
     }
 
+    /**
+     * @dev This function returns the version string of the RelayHub for which
+     * this recipient implementation was built. It's not currently used, but
+     * may be used by tooling.
+     */
     // This function is view for future-proofing, it may require reading from
     // storage in the future.
     function relayHubVersion() public view returns (string memory) {
@@ -22,6 +30,11 @@ contract GSNRecipient is IRelayRecipient, GSNContext, GSNBouncerBase {
         return "1.0.0";
     }
 
+    /**
+     * @dev Triggers a withdraw of the recipient's deposits in RelayHub. Can
+     * be used by derived contracts to expose the functionality in an external
+     * interface.
+     */
     function _withdrawDeposits(uint256 amount, address payable payee) internal {
         IRelayHub(_relayHub).withdraw(amount, payee);
     }
