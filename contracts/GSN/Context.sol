25b82b0db8717dfc26c8c3db40e2aa37ee1cb71e
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-08-22 03:19:26 -0400

    Update Context.sol (#1892)
    Fix typos in instructions
    
    (cherry picked from commit 2836b066c9f9450376dba95c629637547428140a)

--- contracts/GSN/Context.sol
+++ contracts/GSN/Context.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 /*
  * @dev Provides information about the current execution context, including the
  * sender of the transaction and its data. While these are generally available
- * via msg.sender and msg.data, they not should not be accessed in such a direct
+ * via msg.sender and msg.data, they should not be accessed in such a direct
  * manner, since when dealing with GSN meta-transactions the account sending and
  * paying for execution may not be the actual sender (as far as an application
  * is concerned).
@@ -12,7 +12,7 @@ pragma solidity ^0.5.0;
  */
 contract Context {
     // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, with should be used via inheritance.
+    // an instance of this contract, which should be used via inheritance.
     constructor () internal { }
     // solhint-disable-previous-line no-empty-blocks
 
