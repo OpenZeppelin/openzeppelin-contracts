b33b906b6e0ce641b26182589973e723e5131edd
Ajitesh Mishra <48247702+AjiteshBD@users.noreply.github.com>
2019-09-11 22:41:31 +0530

    Update Counter.sol (#1917)
    * Update Counter.sol
    
    Added a comment in increment to make this more visible to not use SafeMath here.
    
    * Update Counters.sol

--- contracts/drafts/Counters.sol
+++ contracts/drafts/Counters.sol
@@ -28,6 +28,7 @@ library Counters {
     }
 
     function increment(Counter storage counter) internal {
+        // The {SafeMath} overflow check can be skipped here, see the comment at the top
         counter._value += 1;
     }
 
