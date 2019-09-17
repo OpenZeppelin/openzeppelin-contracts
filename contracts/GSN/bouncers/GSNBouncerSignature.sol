61265e0150044abf9d72d42da68c4696a6d80544
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-17 11:03:21 -0400

    GSNSignatureBoucer does not accept zero address

--- contracts/GSN/bouncers/GSNBouncerSignature.sol
+++ contracts/GSN/bouncers/GSNBouncerSignature.sol
@@ -22,6 +22,7 @@ contract GSNBouncerSignature is GSNBouncerBase {
      * @dev Sets the trusted signer that is going to be producing signatures to approve relayed calls.
      */
     constructor(address trustedSigner) public {
+        require(trustedSigner != address(0), "Trusted Signer can not be a Zero Address");
         _trustedSigner = trustedSigner;
     }
 
--- test/GSN/GSNBouncerSignature.test.js
+++ test/GSN/GSNBouncerSignature.test.js
@@ -1,74 +1,125 @@
-const { expectEvent } = require('openzeppelin-test-helpers');
-const gsn = require('@openzeppelin/gsn-helpers');
-const { fixSignature } = require('../helpers/sign');
-const { utils: { toBN } } = require('web3');
+const { expectEvent, expectRevert } = require("openzeppelin-test-helpers");
+const gsn = require("@openzeppelin/gsn-helpers");
+const { fixSignature } = require("../helpers/sign");
+const {
+  utils: { toBN }
+} = require("web3");
 
-const GSNBouncerSignatureMock = artifacts.require('GSNBouncerSignatureMock');
+const GSNBouncerSignatureMock = artifacts.require("GSNBouncerSignatureMock");
 
-contract('GSNBouncerSignature', function ([_, signer, other]) {
-  beforeEach(async function () {
+contract("GSNBouncerSignature", function([_, signer, other]) {
+  beforeEach(async function() {
     this.recipient = await GSNBouncerSignatureMock.new(signer);
   });
 
-  context('when called directly', function () {
-    it('mock function can be called', async function () {
+  context("when called directly", function() {
+    it("mock function can be called", async function() {
       const { logs } = await this.recipient.mockFunction();
-      expectEvent.inLogs(logs, 'MockFunctionCalled');
+      expectEvent.inLogs(logs, "MockFunctionCalled");
     });
   });
 
-  context('when relay-called', function () {
-    beforeEach(async function () {
+  context("when constructor is called with a zero address", function() {
+    it("fails when constructor called with a zero address", async function() {
+      await expectRevert(
+        GSNBouncerSignatureMock.new(
+          "0x0000000000000000000000000000000000000000"
+        ),
+        "Trusted Signer can not be a Zero Address"
+      );
+    });
+  });
+
+  context("when relay-called", function() {
+    beforeEach(async function() {
       await gsn.fundRecipient(web3, { recipient: this.recipient.address });
     });
 
-    it('rejects unsigned relay requests', async function () {
-      await gsn.expectError(this.recipient.mockFunction({ value: 0, useGSN: true }));
+    it("rejects unsigned relay requests", async function() {
+      await gsn.expectError(
+        this.recipient.mockFunction({ value: 0, useGSN: true })
+      );
     });
 
-    it('rejects relay requests where some parameters are signed', async function () {
-      const approveFunction = async (data) =>
+    it("rejects relay requests where some parameters are signed", async function() {
+      const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
             web3.utils.soliditySha3(
               // the nonce is not signed
               // eslint-disable-next-line max-len
-              data.relayerAddress, data.from, data.encodedFunctionCall, toBN(data.txFee), toBN(data.gasPrice), toBN(data.gas)
-            ), signer
+              data.relayerAddress,
+              data.from,
+              data.encodedFunctionCall,
+              toBN(data.txFee),
+              toBN(data.gasPrice),
+              toBN(data.gas)
+            ),
+            signer
           )
         );
 
-      await gsn.expectError(this.recipient.mockFunction({ value: 0, useGSN: true, approveFunction }));
+      await gsn.expectError(
+        this.recipient.mockFunction({ value: 0, useGSN: true, approveFunction })
+      );
     });
 
-    it('accepts relay requests where all parameters are signed', async function () {
-      const approveFunction = async (data) =>
+    it("accepts relay requests where all parameters are signed", async function() {
+      const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
             web3.utils.soliditySha3(
               // eslint-disable-next-line max-len
-              data.relayerAddress, data.from, data.encodedFunctionCall, toBN(data.txFee), toBN(data.gasPrice), toBN(data.gas), toBN(data.nonce), data.relayHubAddress, data.to
-            ), signer
+              data.relayerAddress,
+              data.from,
+              data.encodedFunctionCall,
+              toBN(data.txFee),
+              toBN(data.gasPrice),
+              toBN(data.gas),
+              toBN(data.nonce),
+              data.relayHubAddress,
+              data.to
+            ),
+            signer
           )
         );
 
-      const { tx } = await this.recipient.mockFunction({ value: 0, useGSN: true, approveFunction });
+      const { tx } = await this.recipient.mockFunction({
+        value: 0,
+        useGSN: true,
+        approveFunction
+      });
 
-      await expectEvent.inTransaction(tx, GSNBouncerSignatureMock, 'MockFunctionCalled');
+      await expectEvent.inTransaction(
+        tx,
+        GSNBouncerSignatureMock,
+        "MockFunctionCalled"
+      );
     });
 
-    it('rejects relay requests where all parameters are signed by an invalid signer', async function () {
-      const approveFunction = async (data) =>
+    it("rejects relay requests where all parameters are signed by an invalid signer", async function() {
+      const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
             web3.utils.soliditySha3(
               // eslint-disable-next-line max-len
-              data.relayerAddress, data.from, data.encodedFunctionCall, toBN(data.txFee), toBN(data.gasPrice), toBN(data.gas), toBN(data.nonce), data.relayHubAddress, data.to
-            ), other
+              data.relayerAddress,
+              data.from,
+              data.encodedFunctionCall,
+              toBN(data.txFee),
+              toBN(data.gasPrice),
+              toBN(data.gas),
+              toBN(data.nonce),
+              data.relayHubAddress,
+              data.to
+            ),
+            other
           )
         );
 
-      await gsn.expectError(this.recipient.mockFunction({ value: 0, useGSN: true, approveFunction }));
+      await gsn.expectError(
+        this.recipient.mockFunction({ value: 0, useGSN: true, approveFunction })
+      );
     });
   });
 });
