11002cf36493542d5e12e1b84dd3594908f5b689
crazyrabbitLTC <dennison@dennisonbertram.com>
2019-09-17 11:06:57 -0400

    Linting code.

--- test/GSN/GSNBouncerSignature.test.js
+++ test/GSN/GSNBouncerSignature.test.js
@@ -1,47 +1,47 @@
-const { expectEvent, expectRevert } = require("openzeppelin-test-helpers");
-const gsn = require("@openzeppelin/gsn-helpers");
-const { fixSignature } = require("../helpers/sign");
+const { expectEvent, expectRevert } = require('openzeppelin-test-helpers');
+const gsn = require('@openzeppelin/gsn-helpers');
+const { fixSignature } = require('../helpers/sign');
 const {
-  utils: { toBN }
-} = require("web3");
+  utils: { toBN },
+} = require('web3');
 
-const GSNBouncerSignatureMock = artifacts.require("GSNBouncerSignatureMock");
+const GSNBouncerSignatureMock = artifacts.require('GSNBouncerSignatureMock');
 
-contract("GSNBouncerSignature", function([_, signer, other]) {
-  beforeEach(async function() {
+contract('GSNBouncerSignature', function ([_, signer, other]) {
+  beforeEach(async function () {
     this.recipient = await GSNBouncerSignatureMock.new(signer);
   });
 
-  context("when called directly", function() {
-    it("mock function can be called", async function() {
+  context('when called directly', function () {
+    it('mock function can be called', async function () {
       const { logs } = await this.recipient.mockFunction();
-      expectEvent.inLogs(logs, "MockFunctionCalled");
+      expectEvent.inLogs(logs, 'MockFunctionCalled');
     });
   });
 
-  context("when constructor is called with a zero address", function() {
-    it("fails when constructor called with a zero address", async function() {
+  context('when constructor is called with a zero address', function () {
+    it('fails when constructor called with a zero address', async function () {
       await expectRevert(
         GSNBouncerSignatureMock.new(
-          "0x0000000000000000000000000000000000000000"
+          '0x0000000000000000000000000000000000000000'
         ),
-        "Trusted Signer can not be a Zero Address"
+        'Trusted Signer can not be a Zero Address'
       );
     });
   });
 
-  context("when relay-called", function() {
-    beforeEach(async function() {
+  context('when relay-called', function () {
+    beforeEach(async function () {
       await gsn.fundRecipient(web3, { recipient: this.recipient.address });
     });
 
-    it("rejects unsigned relay requests", async function() {
+    it('rejects unsigned relay requests', async function () {
       await gsn.expectError(
         this.recipient.mockFunction({ value: 0, useGSN: true })
       );
     });
 
-    it("rejects relay requests where some parameters are signed", async function() {
+    it('rejects relay requests where some parameters are signed', async function () {
       const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
@@ -64,7 +64,7 @@ contract("GSNBouncerSignature", function([_, signer, other]) {
       );
     });
 
-    it("accepts relay requests where all parameters are signed", async function() {
+    it('accepts relay requests where all parameters are signed', async function () {
       const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
@@ -87,17 +87,17 @@ contract("GSNBouncerSignature", function([_, signer, other]) {
       const { tx } = await this.recipient.mockFunction({
         value: 0,
         useGSN: true,
-        approveFunction
+        approveFunction,
       });
 
       await expectEvent.inTransaction(
         tx,
         GSNBouncerSignatureMock,
-        "MockFunctionCalled"
+        'MockFunctionCalled'
       );
     });
 
-    it("rejects relay requests where all parameters are signed by an invalid signer", async function() {
+    it('rejects relay requests where all parameters are signed by an invalid signer', async function () {
       const approveFunction = async data =>
         fixSignature(
           await web3.eth.sign(
