e613a4eb012020a7a4c5200c076c9203885a7146
Benjamin Smith <bh2smith@users.noreply.github.com>
2019-10-07 16:47:52 -0400

    Update test/utils/SafeCast.test.js

--- test/utils/SafeCast.test.js
+++ test/utils/SafeCast.test.js
@@ -33,7 +33,10 @@ contract('SafeCast', async () => {
       });
 
       it(`reverts when downcasting 2^${bits} + 1 (${maxValue.addn(2)})`, async function () {
-        await expectRevert(this.safeCast[`toUint${bits}`](maxValue.addn(2)), `SafeCast: value doesn't fit in ${bits} bits`);
+        await expectRevert(
+          this.safeCast[`toUint${bits}`](maxValue.addn(2)),
+          `SafeCast: value doesn't fit in ${bits} bits`
+        );
       });
     });
   }
