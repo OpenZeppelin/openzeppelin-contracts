7ed0134de91bd9a54d022fc7a1f26db63fee2f70
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-12 15:58:32 -0300

    Add GSN setup

--- .test-env.js
+++ .test-env.js
@@ -1,5 +1,20 @@
+const { GSNDevProvider } = require('@openzeppelin/gsn-provider');
+
 module.exports = {
   accounts: {
     ether: 1e6,
+  },
+
+  gasLimit: 8e6,
+
+  setupProvider: (baseProvider) => {
+    const { accounts } = require('@openzeppelin/test-env');
+
+    return new GSNDevProvider(baseProvider, {
+      txfee: 70,
+      useGSN: false,
+      ownerAddress: accounts[8],
+      relayerAddress: accounts[9],
+    });
   },
 };
