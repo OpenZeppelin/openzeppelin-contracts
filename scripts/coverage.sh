3743a3f221ce1c922da7843d4e4d1658cb148c1c
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-14 20:17:21 -0300

    Setup coverage using test-env

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,8 +6,11 @@ log() {
   echo "$*" >&2
 }
 
-SOLIDITY_COVERAGE=true scripts/test.sh || log "Test run failed"
+ln -s coverageEnv/allFiredEvents allFiredEvents
+OZ_TEST_ENV_COVERAGE=true npx solidity-coverage || log "Test run failed"
 
 if [ "$CI" = true ]; then
   curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"
 fi
+
+rm -f allFiredEvents
