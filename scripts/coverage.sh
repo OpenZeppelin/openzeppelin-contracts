32b6ff712ab074c6a2881ac922fd694bfe3f7822
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-11-20 13:13:23 -0300

    Add comment on coverage setup

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,7 +6,10 @@ log() {
   echo "$*" >&2
 }
 
+# The allFiredEvents file is created inside coverageEnv, but solidity-coverage
+# expects it to be at the top level. We create a symlink to fix this
 ln -s coverageEnv/allFiredEvents allFiredEvents
+
 OZ_TEST_ENV_COVERAGE=true npx solidity-coverage || log "Test run failed"
 
 if [ "$CI" = true ]; then
