b88b644d00565d030dfcf1e84641521a46ed2dfe
Francisco Giordano <frangio.1@gmail.com>
2020-01-20 15:48:41 -0300

    add note suggesting reentrancy post

--- contracts/payment/PullPayment.sol
+++ contracts/payment/PullPayment.sol
@@ -12,6 +12,10 @@ import "./escrow/Escrow.sol";
  * Ether, security-wise. It prevents recipients from blocking execution, and
  * eliminates reentrancy concerns.
  *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ *
  * To use, derive from the `PullPayment` contract, and use {_asyncTransfer}
  * instead of Solidity's `transfer` function. Payees can query their due
  * payments with {payments}, and retrieve them with {withdrawPayments}.
