a716b80858fac2bc90bcdeef72ebbc4a1a6e764a
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-02-03 18:24:06 -0300

    Remove roles

--- contracts/mocks/WhitelistedRoleMock.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-
-import "../access/roles/WhitelistedRole.sol";
-
-contract WhitelistedRoleMock is WhitelistedRole {
-    function onlyWhitelistedMock() public view onlyWhitelisted { }
-}
