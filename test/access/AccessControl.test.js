4732b68d4514759a6e992d4553a9e1f698500041
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-03-06 18:16:20 -0300

    Cleanup tests

--- test/access/AccessControl.test.js
+++ test/access/AccessControl.test.js
@@ -7,22 +7,22 @@ const { expect } = require('chai');
 const AccessControlMock = contract.fromArtifact('AccessControlMock');
 
 describe('AccessControl', function () {
-  const [ defaultAdmin, authorized, other ] = accounts;
+  const [ admin, authorized, otherAuthorized, other ] = accounts;
 
   const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
-  const OTHER_ROLE = web3.utils.soliditySha3('OTHER_ROLE');
+  const ROLE = web3.utils.soliditySha3('ROLE');
 
   beforeEach(async function () {
-    this.accessControl = await AccessControlMock.new({ from: defaultAdmin });
+    this.accessControl = await AccessControlMock.new({ from: admin });
   });
 
   describe('default admin', function () {
     it('deployer has default admin role', async function () {
-      expect(await this.accessControl.hasRole(DEFAULT_ADMIN_ROLE, defaultAdmin)).to.equal(true);
+      expect(await this.accessControl.hasRole(DEFAULT_ADMIN_ROLE, admin)).to.equal(true);
     });
 
     it('other roles\'s admin is the default admin role', async function () {
-      expect(await this.accessControl.getRoleAdmin(OTHER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
+      expect(await this.accessControl.getRoleAdmin(ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
     });
 
     it('default admin role\'s admin is itself', async function () {
@@ -32,92 +32,92 @@ describe('AccessControl', function () {
 
   describe('granting', function () {
     it('admin can grant role to other accounts', async function () {
-      const receipt = await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE });
+      const receipt = await this.accessControl.grantRole(ROLE, authorized, { from: admin });
+      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: ROLE });
 
-      expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(true);
+      expect(await this.accessControl.hasRole(ROLE, authorized)).to.equal(true);
     });
 
     it('non-admin cannot grant role to other accounts', async function () {
       await expectRevert(
-        this.accessControl.grantRole(OTHER_ROLE, authorized, { from: other }),
+        this.accessControl.grantRole(ROLE, authorized, { from: other }),
         'AccessControl: sender must be an admin to grant'
       );
     });
 
     it('accounts can be granted a role multiple times', async function () {
-      await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-      const receipt = await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE });
+      await this.accessControl.grantRole(ROLE, authorized, { from: admin });
+      const receipt = await this.accessControl.grantRole(ROLE, authorized, { from: admin });
+      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: ROLE });
     });
   });
 
   describe('revoking', function () {
     it('roles that are not had can be revoked', async function () {
-      expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
+      expect(await this.accessControl.hasRole(ROLE, authorized)).to.equal(false);
 
-      const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+      const receipt = await this.accessControl.revokeRole(ROLE, authorized, { from: admin });
+      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
     });
 
     context('with granted role', function () {
       beforeEach(async function () {
-        await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+        await this.accessControl.grantRole(ROLE, authorized, { from: admin });
       });
 
       it('admin can revoke role', async function () {
-        const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+        const receipt = await this.accessControl.revokeRole(ROLE, authorized, { from: admin });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
 
-        expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
+        expect(await this.accessControl.hasRole(ROLE, authorized)).to.equal(false);
       });
 
       it('non-admin cannot revoke role', async function () {
         await expectRevert(
-          this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: other }),
+          this.accessControl.revokeRole(ROLE, authorized, { from: other }),
           'AccessControl: sender must be an admin to revoke'
         );
       });
 
       it('a role can be revoked multiple times', async function () {
-        await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+        await this.accessControl.revokeRole(ROLE, authorized, { from: admin });
 
-        const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+        const receipt = await this.accessControl.revokeRole(ROLE, authorized, { from: admin });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
       });
     });
   });
 
   describe('renouncing', function () {
     it('roles that are not had can be renounced', async function () {
-      const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
-      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+      const receipt = await this.accessControl.renounceRole(ROLE, authorized, { from: authorized });
+      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
     });
 
     context('with granted role', function () {
       beforeEach(async function () {
-        await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+        await this.accessControl.grantRole(ROLE, authorized, { from: admin });
       });
 
       it('bearer can renounce role', async function () {
-        const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+        const receipt = await this.accessControl.renounceRole(ROLE, authorized, { from: authorized });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
 
-        expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
+        expect(await this.accessControl.hasRole(ROLE, authorized)).to.equal(false);
       });
 
       it('only the sender can renounce their roles', async function () {
         await expectRevert(
-          this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: defaultAdmin }),
+          this.accessControl.renounceRole(ROLE, authorized, { from: admin }),
           'AccessControl: can only renounce roles for self'
         );
       });
 
       it('a role can be renounced multiple times', async function () {
-        await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
+        await this.accessControl.renounceRole(ROLE, authorized, { from: authorized });
 
-        const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
+        const receipt = await this.accessControl.renounceRole(ROLE, authorized, { from: authorized });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: ROLE });
       });
     });
   });
