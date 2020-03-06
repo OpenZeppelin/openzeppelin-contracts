6754f6376ca7d807e8cf54d297c54846ce41da89
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-03-06 18:06:11 -0300

    Add documentation

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -2,74 +2,175 @@ pragma solidity ^0.6.0;
 
 import "../utils/EnumerableSet.sol";
 
+/**
+ * @dev Contract module that allows children to implement role-based access
+ * control mechanisms.
+ *
+ * Roles are referred to by their `bytes32` identifier. These should be exposed
+ * in the external API and be unique. The best way to achieve this is by
+ * using `public constant` hash digests:
+ *
+ * ```
+ * bytes32 public constant MY_ROLE = keccak256("MY_ROLE");
+ * ```
+ *
+ * Roles can be used to represent a set of permissions. To restrict access to a
+ * function call, use {hasRole}:
+ *
+ * ```
+ * function foo() public {
+ *     require(hasRole(MY_ROLE, msg.sender));
+ *     ...
+ * }
+ * ```
+ *
+ * Roles can be granted and revoked programatically by calling the `internal`
+ * {_grantRole} and {_revokeRole} functions.
+ *
+ * This can also be achieved dynamically via the `external` {grantRole} and
+ * {revokeRole} functions. Each role has an associated admin role, and only
+ * accounts that have a role's admin role can call {grantRole} and {revokeRoke}.
+ *
+ * By default, the admin role for all roles is `DEFAULT_ADMIN_ROLE`, which means
+ * that only accounts with this role will be able to grant or revoke other
+ * roles. More complex role relationships can be created by using
+ * {_setRoleAdmin}.
+ */
 abstract contract AccessControl {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     struct Role {
         EnumerableSet.AddressSet members;
-        bytes32 admin;
+        bytes32 admin; // This role's admin role
     }
 
     mapping (bytes32 => Role) private _roles;
 
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
+    /**
+     * @dev Emitted when `account` is granted `roleId` role.
+     */
     event RoleGranted(bytes32 indexed roleId, address indexed account);
+
+    /**
+     * @dev Emitted when `account` is revoked the `roleId` role.
+     */
     event RoleRevoked(bytes32 indexed roleId, address indexed account);
 
-    // Returns true if an account has a role
+    /**
+     * @dev Returns `true` if `account` has the `roleId` role.
+     */
     function hasRole(bytes32 roleId, address account) public view returns (bool) {
         return _roles[roleId].members.contains(account);
     }
 
-    // Returns the number of accounts with a role
+    /**
+     * @dev Returns the number of accounts that have the `roleId` role. Can be
+     * used together with {getRoleMember} to enumerate all bearers of a role.
+     */
     function getRoleMembersCount(bytes32 roleId) public view returns (uint256) {
         return _roles[roleId].members.length();
     }
 
-    // Returns an account with a role at index
+    /**
+     * @dev Returns one of the accounts that has the `roleId` role. `index` must
+     * be a value between 0 and {getRoleMembersCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleMember} and {getRoleMembersCount}, make sure
+     * you perform all queries on the same block. See the following
+     * https://forum.openzeppelin.com/t/iterating-over-elements-on-enumerableset-in-openzeppelin-contracts/2296[forum post]
+     * for more information.
+     */
     function getRoleMember(bytes32 roleId, uint256 index) public view returns (address) {
         return _roles[roleId].members.get(index);
     }
 
-    // Returns a role's admin role
+    /**
+     * @dev Returns the role identifier for the admin role that controls
+     * `roleId` role. See {grantRole} and {revokeRole}.
+     *
+     * To change a role's admin, use {_setRoleAdmin}.
+     */
     function getRoleAdmin(bytes32 roleId) external view returns (bytes32) {
         return _roles[roleId].admin;
     }
 
-    // Gives a role to an account. Caller must have its admin role
+    /**
+     * @dev Grants the `roleId` role to `account`.
+     *
+     * Calls {_grantRole} internally.
+     *
+     * Requirements:
+     *
+     * - the caller must have `roleId`'s admin role.
+     */
     function grantRole(bytes32 roleId, address account) external virtual {
         require(hasRole(_roles[roleId].admin, msg.sender), "AccessControl: sender must be an admin to grant");
 
         _grantRole(roleId, account);
     }
 
-    // Revokes a role from an account. Caller must have its admin role
+    /**
+     * @dev Revokes the `roleId` role from `account`.
+     *
+     * Calls {_revokeRole} internally.
+     *
+     * Requirements:
+     *
+     * - the caller must have `roleId`'s admin role.
+     */
     function revokeRole(bytes32 roleId, address account) external virtual {
         require(hasRole(_roles[roleId].admin, msg.sender), "AccessControl: sender must be an admin to revoke");
 
         _revokeRole(roleId, account);
     }
 
-    // Renounces a role. Caller must be `account`
+    /**
+     * @dev Revokes a role from calling account.
+     *
+     * Roles are often managed via {grantRole} and {revokeRole}: this function's
+     * purpose is to provide a mechanism for accounts to lose their privileges
+     * if they are compromised (such as when a trusted device is misplaced).
+     *
+     * Requirements:
+     *
+     * - the caller must be `account`.
+     */
     function renounceRole(bytes32 roleId, address account) external virtual {
         require(account == msg.sender, "AccessControl: can only renounce roles for self");
 
         _revokeRole(roleId, account);
     }
 
+    /**
+     * @dev Grants the `roleId` role to `account`.
+     *
+     * Emits a {RoleGranted} event.
+     */
     function _grantRole(bytes32 roleId, address account) internal virtual {
         _roles[roleId].members.add(account);
 
         emit RoleGranted(roleId, account);
     }
 
+    /**
+     * @dev Revokes the `roleId` role from `account`.
+     *
+     * Emits a {RoleRevoked} event.
+     */
     function _revokeRole(bytes32 roleId, address account) internal virtual {
         _roles[roleId].members.remove(account);
 
         emit RoleRevoked(roleId, account);
     }
 
+    /**
+     * @dev Sets `adminRoleId` as `roleId` role's admin role.
+     */
     function _setRoleAdmin(bytes32 roleId, bytes32 adminRoleId) internal virtual {
         _roles[roleId].admin = adminRoleId;
     }
--- test/access/AccessControl.test.js
+++ test/access/AccessControl.test.js
@@ -9,8 +9,8 @@ const AccessControlMock = contract.fromArtifact('AccessControlMock');
 describe('AccessControl', function () {
   const [ defaultAdmin, authorized, other ] = accounts;
 
-  const DEFAULT_ADMIN_ROLE_ID = '0x0000000000000000000000000000000000000000000000000000000000000000';
-  const OTHER_ROLE_ID = web3.utils.soliditySha3('OTHER_ROLE_ID');
+  const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
+  const OTHER_ROLE = web3.utils.soliditySha3('OTHER_ROLE');
 
   beforeEach(async function () {
     this.accessControl = await AccessControlMock.new({ from: defaultAdmin });
@@ -18,106 +18,106 @@ describe('AccessControl', function () {
 
   describe('default admin', function () {
     it('deployer has default admin role', async function () {
-      expect(await this.accessControl.hasRole(DEFAULT_ADMIN_ROLE_ID, defaultAdmin)).to.equal(true);
+      expect(await this.accessControl.hasRole(DEFAULT_ADMIN_ROLE, defaultAdmin)).to.equal(true);
     });
 
     it('other roles\'s admin is the default admin role', async function () {
-      expect(await this.accessControl.getRoleAdmin(OTHER_ROLE_ID)).to.equal(DEFAULT_ADMIN_ROLE_ID);
+      expect(await this.accessControl.getRoleAdmin(OTHER_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
     });
 
     it('default admin role\'s admin is itself', async function () {
-      expect(await this.accessControl.getRoleAdmin(DEFAULT_ADMIN_ROLE_ID)).to.equal(DEFAULT_ADMIN_ROLE_ID);
+      expect(await this.accessControl.getRoleAdmin(DEFAULT_ADMIN_ROLE)).to.equal(DEFAULT_ADMIN_ROLE);
     });
   });
 
   describe('granting', function () {
     it('admin can grant role to other accounts', async function () {
-      const receipt = await this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE_ID });
+      const receipt = await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE });
 
-      expect(await this.accessControl.hasRole(OTHER_ROLE_ID, authorized)).to.equal(true);
+      expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(true);
     });
 
     it('non-admin cannot grant role to other accounts', async function () {
       await expectRevert(
-        this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: other }),
+        this.accessControl.grantRole(OTHER_ROLE, authorized, { from: other }),
         'AccessControl: sender must be an admin to grant'
       );
     });
 
     it('accounts can be granted a role multiple times', async function () {
-      await this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-      const receipt = await this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE_ID });
+      await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+      const receipt = await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+      expectEvent(receipt, 'RoleGranted', { account: authorized, roleId: OTHER_ROLE });
     });
   });
 
   describe('revoking', function () {
     it('roles that are not had can be revoked', async function () {
-      expect(await this.accessControl.hasRole(OTHER_ROLE_ID, authorized)).to.equal(false);
+      expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
 
-      const receipt = await this.accessControl.revokeRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+      const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
     });
 
     context('with granted role', function () {
       beforeEach(async function () {
-        await this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
+        await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
       });
 
       it('admin can revoke role', async function () {
-        const receipt = await this.accessControl.revokeRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+        const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
 
-        expect(await this.accessControl.hasRole(OTHER_ROLE_ID, authorized)).to.equal(false);
+        expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
       });
 
       it('non-admin cannot revoke role', async function () {
         await expectRevert(
-          this.accessControl.revokeRole(OTHER_ROLE_ID, authorized, { from: other }),
+          this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: other }),
           'AccessControl: sender must be an admin to revoke'
         );
       });
 
       it('a role can be revoked multiple times', async function () {
-        await this.accessControl.revokeRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
+        await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
 
-        const receipt = await this.accessControl.revokeRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+        const receipt = await this.accessControl.revokeRole(OTHER_ROLE, authorized, { from: defaultAdmin });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
       });
     });
   });
 
   describe('renouncing', function () {
     it('roles that are not had can be renounced', async function () {
-      const receipt = await this.accessControl.renounceRole(OTHER_ROLE_ID, authorized, { from: authorized });
-      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+      const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
+      expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
     });
 
     context('with granted role', function () {
       beforeEach(async function () {
-        await this.accessControl.grantRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin });
+        await this.accessControl.grantRole(OTHER_ROLE, authorized, { from: defaultAdmin });
       });
 
       it('bearer can renounce role', async function () {
-        const receipt = await this.accessControl.renounceRole(OTHER_ROLE_ID, authorized, { from: authorized });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+        const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
 
-        expect(await this.accessControl.hasRole(OTHER_ROLE_ID, authorized)).to.equal(false);
+        expect(await this.accessControl.hasRole(OTHER_ROLE, authorized)).to.equal(false);
       });
 
       it('only the sender can renounce their roles', async function () {
         await expectRevert(
-          this.accessControl.renounceRole(OTHER_ROLE_ID, authorized, { from: defaultAdmin }),
+          this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: defaultAdmin }),
           'AccessControl: can only renounce roles for self'
         );
       });
 
       it('a role can be renounced multiple times', async function () {
-        await this.accessControl.renounceRole(OTHER_ROLE_ID, authorized, { from: authorized });
+        await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
 
-        const receipt = await this.accessControl.renounceRole(OTHER_ROLE_ID, authorized, { from: authorized });
-        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE_ID });
+        const receipt = await this.accessControl.renounceRole(OTHER_ROLE, authorized, { from: authorized });
+        expectEvent(receipt, 'RoleRevoked', { account: authorized, roleId: OTHER_ROLE });
       });
     });
   });
