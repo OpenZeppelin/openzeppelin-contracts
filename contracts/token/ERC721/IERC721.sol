c25628db771b085d7dd85eba2dde2222d0f4fb5a
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-03-09 17:11:55 -0300

    Bump minimum compiler version for IERC721

--- contracts/token/ERC721/IERC721.sol
+++ contracts/token/ERC721/IERC721.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.2;
 
 import "../../introspection/IERC165.sol";
 
--- contracts/token/ERC721/IERC721Enumerable.sol
+++ contracts/token/ERC721/IERC721Enumerable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.2;
 
 import "./IERC721.sol";
 
