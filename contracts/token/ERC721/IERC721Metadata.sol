465afbd7a63345f533843f0a8090b5ab1c78d5d8
GuillaumeCz <115c7a5fac@pm.me>
2020-04-01 23:40:30 +0200

    Add documentation to the IERC721Metadata contract

--- contracts/token/ERC721/IERC721Metadata.sol
+++ contracts/token/ERC721/IERC721Metadata.sol
@@ -7,7 +7,19 @@ import "./IERC721.sol";
  * @dev See https://eips.ethereum.org/EIPS/eip-721
  */
 interface IERC721Metadata is IERC721 {
+
+    /**
+     * @dev Gets the NFT name.
+     */
     function name() external view returns (string memory);
+
+    /**
+     * @dev Gets the NFT symbol.
+     */
     function symbol() external view returns (string memory);
+
+    /**
+     * @dev Gets the Uniform Resource Identifier (URI) for a given NFT (`tokenId`).
+     */
     function tokenURI(uint256 tokenId) external view returns (string memory);
 }
