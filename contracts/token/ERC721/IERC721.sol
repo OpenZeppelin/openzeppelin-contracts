f86ed0eee034bc7d830fe0338ebdc60a002cd7b8
GuillaumeCz <115c7a5fac@pm.me>
2020-04-01 23:40:10 +0200

    Add documentation to the IERC721 contract

--- contracts/token/ERC721/IERC721.sol
+++ contracts/token/ERC721/IERC721.sol
@@ -24,8 +24,6 @@ interface IERC721 is IERC165 {
      * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
      * another (`to`).
      *
-     *
-     *
      * Requirements:
      * - `from`, `to` cannot be zero.
      * - `tokenId` must be owned by `from`.
@@ -33,6 +31,7 @@ interface IERC721 is IERC165 {
      * NFT by either {approve} or {setApprovalForAll}.
      */
     function safeTransferFrom(address from, address to, uint256 tokenId) external;
+
     /**
      * @dev Transfers a specific NFT (`tokenId`) from one account (`from`) to
      * another (`to`).
@@ -42,12 +41,37 @@ interface IERC721 is IERC165 {
      * either {approve} or {setApprovalForAll}.
      */
     function transferFrom(address from, address to, uint256 tokenId) external;
+
+    /**
+     * @dev Approves the transfer of a specific NFT (`tokenId`) to an account (`to`)
+     *
+     * Requirements:
+     * - The caller must own the token or be an approved operator
+     */
     function approve(address to, uint256 tokenId) external;
+
+    /**
+     * @dev Gets the approved account for a specific NFT (`tokenId`)
+     */
     function getApproved(uint256 tokenId) external view returns (address operator);
 
+    /**
+     * @dev Enable or disable (`_approved`) an account (`operator`) to manage all of the assets of the sender (`msg.sender`).
+     *
+     * Emits an {ApprovalForAll} event.
+     */
     function setApprovalForAll(address operator, bool _approved) external;
+
+    /**
+     * @dev Queries if an account (`operator`) is allowed to manage all of the assets of another account (`owner`)
+     */
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 
-
+    /**
+     * @dev Safely transfers a specific NFT (`tokenId`) from one account (`from`) to another (`to`)
+     *
+     * Requirements:
+     *  - If the caller is not `from`, it must be approved to move this NFT by etheir {approve} or {setApprovalForAll}
+     */
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
 }
