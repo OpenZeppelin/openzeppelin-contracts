f867450fa9fc03cf29e73083bab0989b8730deb8
GuillaumeCz <115c7a5fac@pm.me>
2020-04-02 23:51:24 +0200

    Improves IERC721Enumerable documentation

--- contracts/token/ERC721/IERC721Enumerable.sol
+++ contracts/token/ERC721/IERC721Enumerable.sol
@@ -9,17 +9,17 @@ import "./IERC721.sol";
 interface IERC721Enumerable is IERC721 {
 
     /**
-     * @dev Gets the total amount of NFT stored by the contract.
+     * @dev Returns the total amount of tokens stored by the contract.
      */
     function totalSupply() external view returns (uint256);
 
     /**
-     * @dev Gets a token ID owned by an account (`owner`) at a given index (`index`) of its token list.
+     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
      */
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
 
     /**
-     * @dev Gets a token ID at a given index (`index`) of all the NFT of the tokens stored by the contract.
+     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
      */
     function tokenByIndex(uint256 index) external view returns (uint256);
 }
