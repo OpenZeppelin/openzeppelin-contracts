154bfc16a287b26685761caf3a2f11977b2453d3
GuillaumeCz <115c7a5fac@pm.me>
2020-04-02 23:49:37 +0200

    Improves IERC721Metadata documentation

--- contracts/token/ERC721/IERC721Metadata.sol
+++ contracts/token/ERC721/IERC721Metadata.sol
@@ -9,17 +9,17 @@ import "./IERC721.sol";
 interface IERC721Metadata is IERC721 {
 
     /**
-     * @dev Gets the NFT name.
+     * @dev Returns the token collection name.
      */
     function name() external view returns (string memory);
 
     /**
-     * @dev Gets the NFT symbol.
+     * @dev Returns the token collection symbol.
      */
     function symbol() external view returns (string memory);
 
     /**
-     * @dev Gets the Uniform Resource Identifier (URI) for a given NFT (`tokenId`).
+     * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
      */
     function tokenURI(uint256 tokenId) external view returns (string memory);
 }
