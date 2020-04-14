f9c0f2ab6f4a863c9af27188aa0ed904136b3fe9
Francisco Giordano <frangio.1@gmail.com>
2020-04-14 18:19:58 -0300

    Update contracts/token/ERC20/ERC20Snapshot.sol
    Co-Authored-By: Nicolás Venturo <nicolas.venturo@gmail.com>

--- contracts/token/ERC20/ERC20Snapshot.sol
+++ contracts/token/ERC20/ERC20Snapshot.sol
@@ -60,13 +60,21 @@ abstract contract ERC20Snapshot is ERC20 {
      *
      * Emits a {Snapshot} event that contains the same id.
      *
-     * {_snapshot} is internal so that you can decide if and how to expose it externally. If it is exposed, it can be
-     * guarded by something like {AccessControl} or open to the public. The latter is required for implementing certain
-     * trust minimization mechanisms such as forking, but you must consider that it can potentially be used by attackers in
-     * two ways. First, it can be used to increase the cost of retrieval of values from snapshots, although it will grow
+     * {_snapshot} is `internal`: you must decide how to expose it externally. This can be done both by
+     * guarding it with a system such as {AccessControl}, or by leaving it open to the public. 
+     *
+     * [NOTE]
+     * ====
+     * While an open way of calling {_snapshot} is required for certain minimization mechanisms
+     * such as forking, you must consider that it can potentially be used by attackers in
+     * two ways. 
+     *
+     * First, it can be used to increase the cost of retrieval of values from snapshots, although it will grow
      * logarithmically thus rendering this attack ineffective in the long term. Second, it can be used to target specific
-     * accounts and increase the cost of ERC20 transfers for them, in the ways specified in the Gas Costs secion above.
+     * accounts and increase the cost of ERC20 transfers for them, in the ways specified in the Gas Costs section above.
+     *
      * We haven't measured the actual numbers; if this is something you're interested in please reach out to us.
+     * ====
      */
     function _snapshot() internal virtual returns (uint256) {
         _currentSnapshotId.increment();
