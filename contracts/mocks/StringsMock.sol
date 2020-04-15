51cb947e7061165f5940676150e45e96babefadb
Francisco Giordano <frangio.1@gmail.com>
2020-04-15 17:33:17 -0300

    fix conflict with js toString method

--- contracts/mocks/StringsMock.sol
+++ contracts/mocks/StringsMock.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.0;
 import "../utils/Strings.sol";
 
 contract StringsMock {
-    function toString(uint256 value) public pure returns (string memory) {
+    function fromUint256(uint256 value) public pure returns (string memory) {
         return Strings.toString(value);
     }
 }
