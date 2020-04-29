0596b0deb6270b841057278f993eee9f489587b5
Brendan Chou <brendanchou@msn.com>
2020-04-29 14:57:52 -0700

    Update SafeERC20.sol

--- contracts/token/ERC20/SafeERC20.sol
+++ contracts/token/ERC20/SafeERC20.sol
@@ -25,14 +25,12 @@ library SafeERC20 {
         _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
 
+    /**
+     * @dev safeApprove should only be called when setting an initial allowance,
+     * or when resetting it to zero. To increase and decrease it, use
+     * 'safeIncreaseAllowance' and 'safeDecreaseAllowance'.
+     */
     function safeApprove(IERC20 token, address spender, uint256 value) internal {
-        // safeApprove should only be called when setting an initial allowance,
-        // or when resetting it to zero. To increase and decrease it, use
-        // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
-        // solhint-disable-next-line max-line-length
-        require((value == 0) || (token.allowance(address(this), spender) == 0),
-            "SafeERC20: approve from non-zero to non-zero allowance"
-        );
         _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
 
