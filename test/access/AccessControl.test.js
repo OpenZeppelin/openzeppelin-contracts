82f3265e38902e285bedcfc368fc3e342530f141
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-05-06 17:18:36 -0300

    Update AccessControl.test.js

--- test/access/AccessControl.test.js
+++ test/access/AccessControl.test.js
@@ -143,10 +143,12 @@ describe('AccessControl', function () {
   describe('setting role admin', function () {
     beforeEach(async function () {
       const receipt = await this.accessControl.setRoleAdmin(ROLE, OTHER_ROLE);
-      expectEvent(receipt,
-        'RoleAdminChanged',
-        { role: ROLE, previousAdminRole: DEFAULT_ADMIN_ROLE, newAdminRole: OTHER_ROLE }
-      );
+      expectEvent(receipt, 'RoleAdminChanged', {
+        role: ROLE, 
+        previousAdminRole: DEFAULT_ADMIN_ROLE, 
+        newAdminRole: OTHER_ROLE 
+      });
+      
       await this.accessControl.grantRole(OTHER_ROLE, otherAdmin, { from: admin });
     });
 
