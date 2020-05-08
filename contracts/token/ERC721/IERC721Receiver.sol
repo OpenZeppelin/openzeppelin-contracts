5a7ea89835fcd24444c9979d7008e7a4dae4fe24
GuillaumeCz <115c7a5fac@pm.me>
2020-05-08 10:37:51 +0200

    Add IERC721Receiver & ERC721Holder documentations

--- contracts/token/ERC721/IERC721Receiver.sol
+++ contracts/token/ERC721/IERC721Receiver.sol
@@ -6,19 +6,19 @@ pragma solidity ^0.6.0;
  * from ERC721 asset contracts.
  */
 abstract contract IERC721Receiver {
+
     /**
-     * @notice Handle the receipt of an NFT
-     * @dev The ERC721 smart contract calls this function on the recipient
-     * after a {IERC721-safeTransferFrom}. This function MUST return the function selector,
-     * otherwise the caller will revert the transaction. The selector to be
-     * returned can be obtained as `this.onERC721Received.selector`. This
-     * function MAY throw to revert and reject the transfer.
-     * Note: the ERC721 contract address is always the message sender.
-     * @param operator The address which called `safeTransferFrom` function
-     * @param from The address which previously owned the token
-     * @param tokenId The NFT identifier which is being transferred
-     * @param data Additional data with no specified format
-     * @return bytes4 `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+     * @dev Returns the function selector on the recipient after `operator` safely transfers ({IERC721-safeTransferFrom}) `tokenId` from `from` to this contract.
+     *
+     * The selector to be returned can be obtained as `this.onERC721Received.selector`.
+     *
+     * This function may throw to revert and reject the transfer.
+     *
+     * The ERC721 contract address is always the message sender.
+     *
+     * Requirements:
+     *
+     * - This function must return the function selector, otherwise the caller will revert the transaction.
      */
     function onERC721Received(address operator, address from, uint256 tokenId, bytes memory data)
     public virtual returns (bytes4);
