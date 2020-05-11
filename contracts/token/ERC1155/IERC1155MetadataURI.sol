77db2a1bd07c5a93524cb00e755c71e7f9000f5c
Robert Kaiser <kairo@kairo.at>
2020-05-11 17:52:08 +0200

    Update contracts/token/ERC1155/IERC1155MetadataURI.sol
    Starting on Solidity v0.6.2, interfaces can now inherit. \o/
    
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- contracts/token/ERC1155/IERC1155MetadataURI.sol
+++ contracts/token/ERC1155/IERC1155MetadataURI.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.2;
 
 import "./IERC1155.sol";
 
@@ -6,6 +6,6 @@ import "./IERC1155.sol";
  * @title ERC-1155 Multi Token Standard basic interface, optional metadata URI extension
  * @dev See https://eips.ethereum.org/EIPS/eip-1155
  */
-abstract contract IERC1155MetadataURI is IERC1155 {
+interface IERC1155MetadataURI is IERC1155 {
     function uri(uint256 id) external view virtual returns (string memory);
 }
