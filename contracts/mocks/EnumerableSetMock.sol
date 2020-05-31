de387f10b76e0a42033cb01ce1841595756aa9ce
Julian R <56316686+julianmrodri@users.noreply.github.com>
2020-05-31 12:57:33 -0300

    feat: adding tests for EnumerableSet.UintSet

--- contracts/mocks/EnumerableSetMock.sol
+++ contracts/mocks/EnumerableSetMock.sol
@@ -34,3 +34,34 @@ contract EnumerableAddressSetMock {
         return _set.at(index);
     }
 }
+
+// UintSet
+contract EnumerableUintSetMock {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    event OperationResult(bool result);
+
+    EnumerableSet.UintSet private _set;
+
+    function contains(uint256 value) public view returns (bool) {
+        return _set.contains(value);
+    }
+
+    function add(uint256 value) public {
+        bool result = _set.add(value);
+        emit OperationResult(result);
+    }
+
+    function remove(uint256 value) public {
+        bool result = _set.remove(value);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _set.length();
+    }
+
+    function at(uint256 index) public view returns (uint256) {
+        return _set.at(index);
+    }
+}
--- test/utils/EnumerableSet.test.js
+++ test/utils/EnumerableSet.test.js
@@ -1,8 +1,9 @@
 const { accounts, contract } = require('@openzeppelin/test-environment');
-const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 
 const EnumerableAddressSetMock = contract.fromArtifact('EnumerableAddressSetMock');
+const EnumerableUintSetMock = contract.fromArtifact('EnumerableUintSetMock');
 
 describe('EnumerableSet', function () {
   // AddressSet
@@ -116,6 +117,125 @@ describe('EnumerableSet', function () {
       await expectMembersMatch(this.set, [accountA, accountC]);
 
       expect(await this.set.contains(accountB)).to.equal(false);
+    });
+  });
+
+  // UintSet
+  describe('EnumerableUintSet', function () {
+    const uintA = new BN('1234');
+    const uintB = new BN('5678');
+    const uintC = new BN('9101112');
+
+    beforeEach(async function () {
+      this.set = await EnumerableUintSetMock.new();
+    });
+
+    async function expectMembersMatch (set, values) {
+      await Promise.all(values.map(async uint =>
+        expect(await set.contains(uint)).to.equal(true)
+      ));
+
+      expect(await set.length()).to.bignumber.equal(values.length.toString());
+
+      // To compare values we convert BNs to strings to workaround Chai
+      // limitations when dealing with nested arrays
+      expect(await Promise.all([...Array(values.length).keys()].map(async (index) => {
+        const entry = await set.at(index);
+        return entry.toString();
+      }))).to.have.same.members(values.map(v => v.toString()));
+    }
+
+    it('starts empty', async function () {
+      expect(await this.set.contains(uintA)).to.equal(false);
+
+      await expectMembersMatch(this.set, []);
+    });
+
+    it('adds a value', async function () {
+      const receipt = await this.set.add(uintA);
+      expectEvent(receipt, 'OperationResult', { result: true });
+
+      await expectMembersMatch(this.set, [uintA]);
+    });
+
+    it('adds several values', async function () {
+      await this.set.add(uintA);
+      await this.set.add(uintB);
+
+      await expectMembersMatch(this.set, [uintA, uintB]);
+      expect(await this.set.contains(uintC)).to.equal(false);
+    });
+
+    it('returns false when adding values already in the set', async function () {
+      await this.set.add(uintA);
+
+      const receipt = (await this.set.add(uintA));
+      expectEvent(receipt, 'OperationResult', { result: false });
+
+      await expectMembersMatch(this.set, [uintA]);
+    });
+
+    it('reverts when retrieving non-existent elements', async function () {
+      await expectRevert(this.set.at(0), 'EnumerableSet: index out of bounds');
+    });
+
+    it('removes added values', async function () {
+      await this.set.add(uintA);
+
+      const receipt = await this.set.remove(uintA);
+      expectEvent(receipt, 'OperationResult', { result: true });
+
+      expect(await this.set.contains(uintA)).to.equal(false);
+      await expectMembersMatch(this.set, []);
+    });
+
+    it('returns false when removing values not in the set', async function () {
+      const receipt = await this.set.remove(uintA);
+      expectEvent(receipt, 'OperationResult', { result: false });
+
+      expect(await this.set.contains(uintA)).to.equal(false);
+    });
+
+    it('adds and removes multiple values', async function () {
+      // []
+
+      await this.set.add(uintA);
+      await this.set.add(uintC);
+
+      // [A, C]
+
+      await this.set.remove(uintA);
+      await this.set.remove(uintB);
+
+      // [C]
+
+      await this.set.add(uintB);
+
+      // [C, B]
+
+      await this.set.add(uintA);
+      await this.set.remove(uintC);
+
+      // [A, B]
+
+      await this.set.add(uintA);
+      await this.set.add(uintB);
+
+      // [A, B]
+
+      await this.set.add(uintC);
+      await this.set.remove(uintA);
+
+      // [B, C]
+
+      await this.set.add(uintA);
+      await this.set.remove(uintB);
+
+      // [A, C]
+
+      await expectMembersMatch(this.set, [uintA, uintC]);
+
+      expect(await this.set.contains(uintB)).to.equal(false);
     });
   });
 });
