4925671ad503623fd2b8f24a59b2ff5ee27804fa
Nicolás Venturo <nicolas.venturo@gmail.com>
2020-06-04 15:46:22 -0300

    Add ERC1155PresetMinterPauser.sol

--- contracts/token/ERC1155/ERC1155Burnable.sol
+++ contracts/token/ERC1155/ERC1155Burnable.sol
@@ -9,12 +9,6 @@ import "./ERC1155.sol";
  * own tokens and those that they have been approved to use.
  */
 contract ERC1155Burnable is ERC1155 {
-    /**
-     * @dev Internal function to burn an amount of a token with the given ID
-     * @param account Account which owns the token to be burnt
-     * @param id ID of the token to be burnt
-     * @param value Amount of the token to be burnt
-     */
     function burn(address account, uint256 id, uint256 value) public virtual {
         require(
             account == _msgSender() || isApprovedForAll(account, _msgSender()),
@@ -24,12 +18,6 @@ contract ERC1155Burnable is ERC1155 {
         _burn(account, id, value);
     }
 
-    /**
-     * @dev Internal function to batch burn an amounts of tokens with the given IDs
-     * @param account Account which owns the token to be burnt
-     * @param ids IDs of the tokens to be burnt
-     * @param values Amounts of the tokens to be burnt
-     */
     function burnBatch(address account, uint256[] memory ids, uint256[] memory values) public virtual {
         require(
             account == _msgSender() || isApprovedForAll(account, _msgSender()),
