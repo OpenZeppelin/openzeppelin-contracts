24b20259d2b11b4805e6a1223eaa64b919a83c42
X5 Engine <12363020+x5engine@users.noreply.github.com>
2020-06-05 04:53:13 +0100

    fix weird syntax
    this won't compile under 0.6.0
    
    (bool success, ) = recipient.call{ value: amount }("");
    so fixed it

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -53,8 +53,8 @@ library Address {
     function sendValue(address payable recipient, uint256 amount) internal {
         require(address(this).balance >= amount, "Address: insufficient balance");
 
-        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
-        (bool success, ) = recipient.call{ value: amount }("");
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value    
+        (bool success, ) = recipient.call.value(amount)("");
         require(success, "Address: unable to send value, recipient may have reverted");
     }
 }
