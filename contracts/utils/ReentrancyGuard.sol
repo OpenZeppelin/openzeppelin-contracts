2452feff1e2f7c5a4fdaee1f36983c7dacd69292
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-06-28 11:27:34 -0400

    Update ReentrancyGuard.sol
    add placeholder initialization "0" to play nice with ^0.6.0 compiles

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -30,7 +30,7 @@ contract ReentrancyGuard {
     // amount. Since refunds are capped to a percentage of the total
     // transaction's gas, it is best to keep them low in cases like this one, to
     // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _NOT_ENTERED;
+    uint256 private constant _NOT_ENTERED = 0;
     uint256 private constant _ENTERED = 1;
 
     uint256 private _status;
