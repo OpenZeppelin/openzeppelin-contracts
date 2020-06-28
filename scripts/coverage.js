5e24852d9a8ab65fd7824357499cd96a6b541080
Igor Yalovoy <to@ylv.io>
2020-06-29 01:36:10 +0300

    Update runCoverage testCommand argument

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -3,7 +3,7 @@
 const { execSync } = require('child_process');
 const { runCoverage } = require('@openzeppelin/test-environment');
 
-runCoverage(['mocks'], 'npm run compile', './node_modules/.bin/mocha --exit --timeout 10000 --recursive');
+runCoverage(['mocks'], 'npm run compile', './node_modules/.bin/mocha --exit --timeout 10000 --recursive'.split(' '));
 
 if (process.env.CI) {
   execSync('curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"');
