1829d1a518cbc0081ece32735f91ed71fecb2de0
Igor Yalovoy <to@ylv.io>
2020-06-29 00:14:14 +0300

    Add codecov.io support

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+const { execSync } = require('child_process');
 const { runCoverage } = require('@openzeppelin/test-environment');
 
 runCoverage(['mocks'], 'npm run compile', './node_modules/.bin/mocha --exit --timeout 10000 --recursive');
+
+if (process.env.CI) {
+  execSync('curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"');
+}
