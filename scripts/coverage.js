14f3de5d85ce1e41a0bc576662683be72d2b890b
Igor Yalovoy <to@ylv.io>
2020-06-30 12:37:39 -0300

    Update scripts/coverage.js
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -6,5 +6,5 @@ const { runCoverage } = require('@openzeppelin/test-environment');
 runCoverage(['mocks'], 'npm run compile', './node_modules/.bin/mocha --exit --timeout 10000 --recursive'.split(' '));
 
 if (process.env.CI) {
-  execSync('curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"');
+  execSync('curl -s https://codecov.io/bash | bash -s -- -C "$CIRCLE_SHA1"', { stdio: 'inherit' });
 }
