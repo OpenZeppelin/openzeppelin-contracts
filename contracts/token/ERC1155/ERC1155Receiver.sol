fa28df7984766756865c92ca8d2f3595e569c60a
elenadimitrova <elena@arenabg.com>
2020-07-29 18:41:19 +0300

    Remove public constructor declaration from abstract contracts

--- contracts/token/ERC1155/ERC1155Receiver.sol
+++ contracts/token/ERC1155/ERC1155Receiver.sol
@@ -9,7 +9,7 @@ import "../../introspection/ERC165.sol";
  * @dev _Available since v3.1._
  */
 abstract contract ERC1155Receiver is ERC165, IERC1155Receiver {
-    constructor() public {
+    constructor() {
         _registerInterface(
             ERC1155Receiver(0).onERC1155Received.selector ^
             ERC1155Receiver(0).onERC1155BatchReceived.selector
