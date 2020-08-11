f29331cd0469aec19a40a1af2fa6c66a8cc0267e
David Mihal <david@dmihal.com>
2020-08-11 14:14:47 -0300

    Prevent transfer to the token contract

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -209,6 +209,7 @@ contract ERC20 is Context, IERC20 {
     function _transfer(address sender, address recipient, uint256 amount) internal virtual {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(recipient != address(this), "ERC20: transfer to the token contract");
 
         _beforeTokenTransfer(sender, recipient, amount);
 
