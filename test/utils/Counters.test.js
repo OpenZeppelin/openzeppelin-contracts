cf26e93df8130ef0fa0439b2d2e8934f40162c61
Paolo Dibitonto <p.dibitonto@almaviva.it>
2020-08-21 12:08:26 +0200

    reviewed Counters test descriptions

--- test/utils/Counters.test.js
+++ test/utils/Counters.test.js
@@ -9,23 +9,26 @@ describe('Counters', function () {
   beforeEach(async function () {
     this.counter = await CountersImpl.new();
   });
-
-  it('starts at zero', async function () {
-    expect(await this.counter.current()).to.be.bignumber.equal('0');
+  context('once deployed', function () {
+    it('starts at zero', async function () {
+      expect(await this.counter.current()).to.be.bignumber.equal('0');
+    });
   });
 
   describe('increment', function () {
-    it('increments the current value by one', async function () {
-      await this.counter.increment();
-      expect(await this.counter.current()).to.be.bignumber.equal('1');
-    });
+    context('starting from 0', function () {
+      it('increments the current value by one', async function () {
+        await this.counter.increment();
+        expect(await this.counter.current()).to.be.bignumber.equal('1');
+      });
 
-    it('can be called multiple times', async function () {
-      await this.counter.increment();
-      await this.counter.increment();
-      await this.counter.increment();
+      it('can be called multiple times', async function () {
+        await this.counter.increment();
+        await this.counter.increment();
+        await this.counter.increment();
 
-      expect(await this.counter.current()).to.be.bignumber.equal('3');
+        expect(await this.counter.current()).to.be.bignumber.equal('3');
+      });
     });
   });
 
@@ -34,28 +37,30 @@ describe('Counters', function () {
       await this.counter.increment();
       expect(await this.counter.current()).to.be.bignumber.equal('1');
     });
+    context('starting from 1', function () {
+      it('decrements the current value by one', async function () {
+        await this.counter.decrement();
+        expect(await this.counter.current()).to.be.bignumber.equal('0');
+      });
 
-    it('decrements the current value by one', async function () {
-      await this.counter.decrement();
-      expect(await this.counter.current()).to.be.bignumber.equal('0');
+      it('reverts if the current value is 0', async function () {
+        await this.counter.decrement();
+        await expectRevert(this.counter.decrement(), 'SafeMath: subtraction overflow');
+      });
     });
+    context('after incremented to 3', function () {
+      it('can be called multiple times', async function () {
+        await this.counter.increment();
+        await this.counter.increment();
 
-    it('reverts if the current value is 0', async function () {
-      await this.counter.decrement();
-      await expectRevert(this.counter.decrement(), 'SafeMath: subtraction overflow');
-    });
+        expect(await this.counter.current()).to.be.bignumber.equal('3');
 
-    it('can be called multiple times', async function () {
-      await this.counter.increment();
-      await this.counter.increment();
+        await this.counter.decrement();
+        await this.counter.decrement();
+        await this.counter.decrement();
 
-      expect(await this.counter.current()).to.be.bignumber.equal('3');
-
-      await this.counter.decrement();
-      await this.counter.decrement();
-      await this.counter.decrement();
-
-      expect(await this.counter.current()).to.be.bignumber.equal('0');
+        expect(await this.counter.current()).to.be.bignumber.equal('0');
+      });
     });
   });
 });
