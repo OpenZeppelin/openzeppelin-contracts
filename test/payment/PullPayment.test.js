7888185a31e6c14e87655a817c481385c87a27c4
Paolo Dibitonto <p.dibitonto@almaviva.it>
2020-08-21 15:55:52 +0200

    reviewed PullPayment test description

--- test/payment/PullPayment.test.js
+++ test/payment/PullPayment.test.js
@@ -15,35 +15,39 @@ describe('PullPayment', function () {
     this.contract = await PullPaymentMock.new({ value: amount });
   });
 
-  it('can record an async payment correctly', async function () {
-    await this.contract.callTransfer(payee1, 100, { from: payer });
-    expect(await this.contract.payments(payee1)).to.be.bignumber.equal('100');
+  describe('payments', function () {
+    it('can record an async payment correctly', async function () {
+      await this.contract.callTransfer(payee1, 100, { from: payer });
+      expect(await this.contract.payments(payee1)).to.be.bignumber.equal('100');
+    });
+
+    it('can add multiple balances on one account', async function () {
+      await this.contract.callTransfer(payee1, 200, { from: payer });
+      await this.contract.callTransfer(payee1, 300, { from: payer });
+      expect(await this.contract.payments(payee1)).to.be.bignumber.equal('500');
+    });
+
+    it('can add balances on multiple accounts', async function () {
+      await this.contract.callTransfer(payee1, 200, { from: payer });
+      await this.contract.callTransfer(payee2, 300, { from: payer });
+
+      expect(await this.contract.payments(payee1)).to.be.bignumber.equal('200');
+
+      expect(await this.contract.payments(payee2)).to.be.bignumber.equal('300');
+    });
   });
 
-  it('can add multiple balances on one account', async function () {
-    await this.contract.callTransfer(payee1, 200, { from: payer });
-    await this.contract.callTransfer(payee1, 300, { from: payer });
-    expect(await this.contract.payments(payee1)).to.be.bignumber.equal('500');
-  });
+  describe('withdrawPayments', function () {
+    it('can withdraw payment', async function () {
+      const balanceTracker = await balance.tracker(payee1);
 
-  it('can add balances on multiple accounts', async function () {
-    await this.contract.callTransfer(payee1, 200, { from: payer });
-    await this.contract.callTransfer(payee2, 300, { from: payer });
+      await this.contract.callTransfer(payee1, amount, { from: payer });
+      expect(await this.contract.payments(payee1)).to.be.bignumber.equal(amount);
 
-    expect(await this.contract.payments(payee1)).to.be.bignumber.equal('200');
+      await this.contract.withdrawPayments(payee1);
 
-    expect(await this.contract.payments(payee2)).to.be.bignumber.equal('300');
-  });
-
-  it('can withdraw payment', async function () {
-    const balanceTracker = await balance.tracker(payee1);
-
-    await this.contract.callTransfer(payee1, amount, { from: payer });
-    expect(await this.contract.payments(payee1)).to.be.bignumber.equal(amount);
-
-    await this.contract.withdrawPayments(payee1);
-
-    expect(await balanceTracker.delta()).to.be.bignumber.equal(amount);
-    expect(await this.contract.payments(payee1)).to.be.bignumber.equal('0');
+      expect(await balanceTracker.delta()).to.be.bignumber.equal(amount);
+      expect(await this.contract.payments(payee1)).to.be.bignumber.equal('0');
+    });
   });
 });
