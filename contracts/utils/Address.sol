f29e2ebbef700f6d64d82273c919ea8fd91a72cf
Anton Bukov <k06aaa@gmail.com>
2020-08-24 22:00:34 +0300

    Update contracts/utils/Address.sol
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -117,7 +117,7 @@ library Address {
     }
 
     function functionStaticCall(address target, bytes memory data, string memory errorMessage) internal view returns (bytes memory) {
-        require(isContract(target), "Address: call to non-contract");
+        require(isContract(target), "Address: static call to non-contract");
 
         // solhint-disable-next-line avoid-low-level-calls
         (bool success, bytes memory returndata) = target.staticcall(data);
