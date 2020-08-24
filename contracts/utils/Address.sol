5b03a8d5e5823af60de81f13f35deb0048e1f4d6
Anton Bukov <k06aaa@gmail.com>
2020-08-24 22:00:42 +0300

    Update contracts/utils/Address.sol
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -140,7 +140,7 @@ library Address {
     }
 
     function functionDelegateCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
-        require(isContract(target), "Address: call to non-contract");
+        require(isContract(target), "Address: delegate call to non-contract");
 
         // solhint-disable-next-line avoid-low-level-calls
         (bool success, bytes memory returndata) = target.delegatecall(data);
