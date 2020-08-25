61a65f6c45b90bbf0d4eb8cb734c6c8488429f59
Paolo Dibitonto <p.dibitonto@almaviva.it>
2020-08-25 10:46:59 +0200

    removed 'once deployed' context as unnecessary from ReentrancyGuard.test.js

--- test/utils/ReentrancyGuard.test.js
+++ test/utils/ReentrancyGuard.test.js
@@ -11,26 +11,25 @@ describe('ReentrancyGuard', function () {
     this.reentrancyMock = await ReentrancyMock.new();
     expect(await this.reentrancyMock.counter()).to.be.bignumber.equal('0');
   });
-  context('once deployed', function () {
-    it('does not allow remote callback', async function () {
-      const attacker = await ReentrancyAttack.new();
-      await expectRevert(
-        this.reentrancyMock.countAndCall(attacker.address), 'ReentrancyAttack: failed call');
-    });
 
-    // The following are more side-effects than intended behavior:
-    // I put them here as documentation, and to monitor any changes
-    // in the side-effects.
-    it('does not allow local recursion', async function () {
-      await expectRevert(
-        this.reentrancyMock.countLocalRecursive(10), 'ReentrancyGuard: reentrant call'
-      );
-    });
+  it('does not allow remote callback', async function () {
+    const attacker = await ReentrancyAttack.new();
+    await expectRevert(
+      this.reentrancyMock.countAndCall(attacker.address), 'ReentrancyAttack: failed call');
+  });
 
-    it('does not allow indirect local recursion', async function () {
-      await expectRevert(
-        this.reentrancyMock.countThisRecursive(10), 'ReentrancyMock: failed call'
-      );
-    });
+  // The following are more side-effects than intended behavior:
+  // I put them here as documentation, and to monitor any changes
+  // in the side-effects.
+  it('does not allow local recursion', async function () {
+    await expectRevert(
+      this.reentrancyMock.countLocalRecursive(10), 'ReentrancyGuard: reentrant call'
+    );
+  });
+
+  it('does not allow indirect local recursion', async function () {
+    await expectRevert(
+      this.reentrancyMock.countThisRecursive(10), 'ReentrancyMock: failed call'
+    );
   });
 });
