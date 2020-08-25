e20b3893a337186d460ba7eb13bc21854343b583
Francisco Giordano <frangio.1@gmail.com>
2020-08-25 15:19:39 -0300

    indent with 4 spaces

--- contracts/proxy/Initializable.sol
+++ contracts/proxy/Initializable.sol
@@ -17,46 +17,46 @@ pragma solidity >=0.4.24 <0.7.0;
  */
 contract Initializable {
 
-  /**
-   * @dev Indicates that the contract has been initialized.
-   */
-  bool private _initialized;
+    /**
+     * @dev Indicates that the contract has been initialized.
+     */
+    bool private _initialized;
 
-  /**
-   * @dev Indicates that the contract is in the process of being initialized.
-   */
-  bool private _initializing;
+    /**
+     * @dev Indicates that the contract is in the process of being initialized.
+     */
+    bool private _initializing;
 
-  /**
-   * @dev Modifier to use in the initializer function of a contract.
-   */
-  modifier initializer() {
-    require(_initializing || _isConstructor() || !_initialized, "Initializable: contract is already initialized");
+    /**
+     * @dev Modifier to use in the initializer function of a contract.
+     */
+    modifier initializer() {
+        require(_initializing || _isConstructor() || !_initialized, "Initializable: contract is already initialized");
 
-    bool isTopLevelCall = !_initializing;
-    if (isTopLevelCall) {
-      _initializing = true;
-      _initialized = true;
+        bool isTopLevelCall = !_initializing;
+        if (isTopLevelCall) {
+            _initializing = true;
+            _initialized = true;
+        }
+
+        _;
+
+        if (isTopLevelCall) {
+            _initializing = false;
+        }
     }
 
-    _;
-
-    if (isTopLevelCall) {
-      _initializing = false;
+    /// @dev Returns true if and only if the function is running in the constructor
+    function _isConstructor() private view returns (bool) {
+        // extcodesize checks the size of the code stored in an address, and
+        // address returns the current address. Since the code is still not
+        // deployed when running a constructor, any checks on its code size will
+        // yield zero, making it an effective way to detect if a contract is
+        // under construction or not.
+        address self = address(this);
+        uint256 cs;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { cs := extcodesize(self) }
+        return cs == 0;
     }
-  }
-
-  /// @dev Returns true if and only if the function is running in the constructor
-  function _isConstructor() private view returns (bool) {
-    // extcodesize checks the size of the code stored in an address, and
-    // address returns the current address. Since the code is still not
-    // deployed when running a constructor, any checks on its code size will
-    // yield zero, making it an effective way to detect if a contract is
-    // under construction or not.
-    address self = address(this);
-    uint256 cs;
-    // solhint-disable-next-line no-inline-assembly
-    assembly { cs := extcodesize(self) }
-    return cs == 0;
-  }
 }
--- contracts/proxy/Proxy.sol
+++ contracts/proxy/Proxy.sol
@@ -10,69 +10,69 @@ pragma solidity ^0.6.0;
  * returned by the abstract _implementation() internal function.
  */
 abstract contract Proxy {
-  /**
-   * @dev Fallback function.
-   * Implemented entirely in `_fallback`.
-   */
-  fallback () payable external {
-    _fallback();
-  }
-
-  /**
-   * @dev Receive function.
-   * Implemented entirely in `_fallback`.
-   */
-  receive () payable external {
-    _fallback();
-  }
-
-  /**
-   * @return The Address of the implementation.
-   */
-  function _implementation() internal virtual view returns (address);
-
-  /**
-   * @dev Delegates execution to an implementation contract.
-   * This is a low level function that doesn't return to its internal call site.
-   * It will return to the external caller whatever the implementation returns.
-   * @param implementation Address to delegate.
-   */
-  function _delegate(address implementation) internal {
-    // solhint-disable-next-line no-inline-assembly
-    assembly {
-      // Copy msg.data. We take full control of memory in this inline assembly
-      // block because it will not return to Solidity code. We overwrite the
-      // Solidity scratch pad at memory position 0.
-      calldatacopy(0, 0, calldatasize())
-
-      // Call the implementation.
-      // out and outsize are 0 because we don't know the size yet.
-      let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
-
-      // Copy the returned data.
-      returndatacopy(0, 0, returndatasize())
-
-      switch result
-      // delegatecall returns 0 on error.
-      case 0 { revert(0, returndatasize()) }
-      default { return(0, returndatasize()) }
+    /**
+     * @dev Fallback function.
+     * Implemented entirely in `_fallback`.
+     */
+    fallback () payable external {
+        _fallback();
     }
-  }
 
-  /**
-   * @dev Function that is run as the first thing in the fallback function.
-   * Can be redefined in derived contracts to add functionality.
-   * Redefinitions must call super._willFallback().
-   */
-  function _willFallback() internal virtual {
-  }
+    /**
+     * @dev Receive function.
+     * Implemented entirely in `_fallback`.
+     */
+    receive () payable external {
+        _fallback();
+    }
 
-  /**
-   * @dev fallback implementation.
-   * Extracted to enable manual triggering.
-   */
-  function _fallback() internal {
-    _willFallback();
-    _delegate(_implementation());
-  }
+    /**
+     * @return The Address of the implementation.
+     */
+    function _implementation() internal virtual view returns (address);
+
+    /**
+     * @dev Delegates execution to an implementation contract.
+     * This is a low level function that doesn't return to its internal call site.
+     * It will return to the external caller whatever the implementation returns.
+     * @param implementation Address to delegate.
+     */
+    function _delegate(address implementation) internal {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+            let result := delegatecall(gas(), implementation, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+
+    /**
+     * @dev Function that is run as the first thing in the fallback function.
+     * Can be redefined in derived contracts to add functionality.
+     * Redefinitions must call super._willFallback().
+     */
+    function _willFallback() internal virtual {
+    }
+
+    /**
+     * @dev fallback implementation.
+     * Extracted to enable manual triggering.
+     */
+    function _fallback() internal {
+        _willFallback();
+        _delegate(_implementation());
+    }
 }
