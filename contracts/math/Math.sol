6d87432d4b9dafb040ea6e29f8a4a1e6ef5240ba
HowJMay <vulxj0j8j8@gmail.com>
2020-09-01 10:39:12 +0800

    refactor: Accelerate 'average' calculation
    Using bitwise operation could be faster and consume less gas.
    When turning on optimization, now running 'average()' costs only 91.1%
    gas than it did, and 94.4% gas on execution deploying this contract
    than it did.
    
    The difference would be much greater when optimization is off.

--- contracts/math/Math.sol
+++ contracts/math/Math.sol
@@ -26,6 +26,6 @@ library Math {
      */
     function average(uint256 a, uint256 b) internal pure returns (uint256) {
         // (a + b) / 2 can overflow, so we distribute
-        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+        return (a >> 1) + (b >> 1) + ((a & 1) + (b & 1) >> 1);
     }
 }
