ccb61148b4f946ad9a8bb8ccfe36c49b13983242
psykeeper <69228311+psykeeper@users.noreply.github.com>
2020-10-19 18:06:43 -0400

    Update ReentrancyGuard contract to abstract

--- contracts/utils/ReentrancyGuard.sol
+++ contracts/utils/ReentrancyGuard.sol
@@ -18,7 +18,7 @@ pragma solidity ^0.7.0;
  * to protect against it, check out our blog post
  * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
  */
-contract ReentrancyGuard {
+abstract contract ReentrancyGuard {
     // Booleans are more expensive than uint256 or any type that takes up a full
     // word because each write operation emits an extra SLOAD to first read the
     // slot's contents, replace the bits taken up by the boolean, and then write
