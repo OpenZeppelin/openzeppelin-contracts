1fbcecc67782e1ecf2dee228944250d0d9a5304c
ngongoclam <63093433+ngongoclam@users.noreply.github.com>
2020-10-19 16:04:53 +0700

    Update ReentrancyGuard.test.js

--- test/utils/ReentrancyGuard.test.js
+++ test/utils/ReentrancyGuard.test.js
@@ -1,35 +1,33 @@
-const { contract } = require('@openzeppelin/test-environment');
-const { expectRevert } = require('@openzeppelin/test-helpers');
+const { shouldFail } = require('openzeppelin-test-helpers');
 
-const { expect } = require('chai');
+const ReentrancyMock = artifacts.require('ReentrancyMock');
+const ReentrancyAttack = artifacts.require('ReentrancyAttack');
 
-const ReentrancyMock = contract.fromArtifact('ReentrancyMock');
-const ReentrancyAttack = contract.fromArtifact('ReentrancyAttack');
-
-describe('ReentrancyGuard', function () {
+contract('ReentrancyGuard', function () {
   beforeEach(async function () {
     this.reentrancyMock = await ReentrancyMock.new();
-    expect(await this.reentrancyMock.counter()).to.be.bignumber.equal('0');
+    (await this.reentrancyMock.counter()).should.be.bignumber.equal('0');
   });
 
-  it('does not allow remote callback', async function () {
+  it('should not allow remote callback', async function () {
     const attacker = await ReentrancyAttack.new();
-    await expectRevert(
-      this.reentrancyMock.countAndCall(attacker.address), 'ReentrancyAttack: failed call');
+    await shouldFail.reverting.withMessage(
+      this.reentrancyMock.countAndCall(attacker.address), 'ReentrancyGuard: reentrant call');
   });
 
   // The following are more side-effects than intended behavior:
   // I put them here as documentation, and to monitor any changes
   // in the side-effects.
-  it('does not allow local recursion', async function () {
-    await expectRevert(
-      this.reentrancyMock.countLocalRecursive(10), 'ReentrancyGuard: reentrant call',
+
+  it('should not allow local recursion', async function () {
+    await shouldFail.reverting.withMessage(
+      this.reentrancyMock.countLocalRecursive(10), 'ReentrancyGuard: reentrant call'
     );
   });
 
-  it('does not allow indirect local recursion', async function () {
-    await expectRevert(
-      this.reentrancyMock.countThisRecursive(10), 'ReentrancyMock: failed call',
+  it('should not allow indirect local recursion', async function () {
+    await shouldFail.reverting.withMessage(
+      this.reentrancyMock.countThisRecursive(10), 'ReentrancyMock: failed call'
     );
   });
 });
