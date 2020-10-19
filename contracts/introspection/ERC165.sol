0397e155b909ee5fa6ab1490a65b4c174eb52701
psykeeper <69228311+psykeeper@users.noreply.github.com>
2020-10-19 18:01:25 -0400

    Update ERC165 contract to abstract

--- contracts/introspection/ERC165.sol
+++ contracts/introspection/ERC165.sol
@@ -10,7 +10,7 @@ import "./IERC165.sol";
  * Contracts may inherit from this and call {_registerInterface} to declare
  * their support of an interface.
  */
-contract ERC165 is IERC165 {
+abstract contract ERC165 is IERC165 {
     /*
      * bytes4(keccak256('supportsInterface(bytes4)')) == 0x01ffc9a7
      */
