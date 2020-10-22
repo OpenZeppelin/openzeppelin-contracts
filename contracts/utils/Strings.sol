9eedc5e2ec76e92eb0b7e9b36c742898a71bd2d7
Anton Bukov <k06aaa@gmail.com>
2020-10-22 16:21:17 +0300

    Add Strings.toHex

--- contracts/utils/Strings.sol
+++ contracts/utils/Strings.sol
@@ -31,4 +31,32 @@ library Strings {
         }
         return string(buffer);
     }
+
+    function toHex(bool flag) internal pure returns(string memory) {
+        return toHex(abi.encodePacked(flag));
+    }
+
+    function toHex(address account) internal pure returns(string memory) {
+        return toHex(abi.encodePacked(account));
+    }
+
+    function toHex(uint256 value) internal pure returns(string memory) {
+        return toHex(abi.encodePacked(value));
+    }
+
+    function toHex(bytes32 value) internal pure returns(string memory) {
+        return toHex(abi.encodePacked(value));
+    }
+
+    function toHex(bytes memory data) internal pure returns(string memory) {
+        bytes16 alphabet = 0x30313233343536373839616263646566;
+
+        bytes memory str = new bytes(data.length * 2);
+        for (uint i = 0; i < data.length; i++) {
+            str[i*2] = alphabet[uint(uint8(data[i] >> 4))];
+            str[i*2 + 1] = alphabet[uint(uint8(data[i] & 0x0f))];
+        }
+
+        return string(str);
+    }
 }
