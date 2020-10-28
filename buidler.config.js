fabec09de85e3ed6c9c49310a7d9eef7b5a786d7
Francisco Giordano <frangio.1@gmail.com>
2020-10-28 14:52:11 -0300

    add comment explaining buidler config snippet

--- buidler.config.js
+++ buidler.config.js
@@ -4,6 +4,9 @@ usePlugin('@nomiclabs/buidler-truffle5');
 extendEnvironment(hre => {
   const { contract } = hre;
   hre.contract = function (name, body) {
+    // remove the default account from the accounts list used in tests, in order
+    // to protect tests against accidentally passing due to the contract
+    // deployer being used subsequently as function caller
     contract(name, accounts => body(accounts.slice(1)));
   };
 });
