1e78adc28c33c47f5f647faed1efa5da26467c52
Francisco Giordano <frangio.1@gmail.com>
2020-11-06 14:12:33 -0300

    Refactor buidler config

--- buidler.config.js
+++ buidler.config.js
@@ -1,15 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 usePlugin('solidity-coverage');
 usePlugin('@nomiclabs/buidler-truffle5');
 
-extendEnvironment(env => {
-  const { contract } = env;
-  env.contract = function (name, body) {
-    // remove the default account from the accounts list used in tests, in order
-    // to protect tests against accidentally passing due to the contract
-    // deployer being used subsequently as function caller
-    contract(name, accounts => body(accounts.slice(1)));
-  };
-});
+for (const f of fs.readdirSync(path.join(__dirname, 'buidler'))) {
+  require(path.join(__dirname, 'buidler', f));
+}
 
 module.exports = {
   networks: {
