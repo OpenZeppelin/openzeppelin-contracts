3dc6dfb37165a56b5c79320b3e9adf970393a5e9
hrkrshnn <webmail.hari@gmail.com>
2020-11-10 13:19:35 +0100

    Introduce explicit conversions between addresses an numbers.
    This is valid on <0.8.0, but will be required on >=0.8.0.
    
    Co-authored-by: cameel <cameel2@gmail.com>

--- contracts/utils/Create2.sol
+++ contracts/utils/Create2.sol
@@ -54,6 +54,6 @@ library Create2 {
         bytes32 _data = keccak256(
             abi.encodePacked(bytes1(0xff), deployer, salt, bytecodeHash)
         );
-        return address(uint256(_data));
+        return address(uint160(uint256(_data)));
     }
 }
