951e946e71850f17267ada197d405bffd28b95dc
Francisco Giordano <frangio.1@gmail.com>
2020-11-26 13:03:01 -0300

    Add a beacon proxy contract (#2411)
    Co-authored-by: Santiago Palladino <spalladino@gmail.com>

---
+++ contracts/mocks/BadBeacon.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "../proxy/IBeacon.sol";
+
+contract BadBeaconNoImpl {
+}
+
+contract BadBeaconNotContract is IBeacon {
+    function implementation() external view override returns (address) {
+        return address(0x1);
+    }
+}
