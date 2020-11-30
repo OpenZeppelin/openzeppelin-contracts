d0e12116354b7ee66694db6cd2de2cefb551663b
Francisco Giordano <frangio.1@gmail.com>
2020-11-30 17:27:50 -0300

    add docs

--- contracts/cryptography/EIP712.sol
+++ contracts/cryptography/EIP712.sol
@@ -2,6 +2,17 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
+/**
+ * @dev https://eips.ethereum.org/EIPS/eip-712[EIP 712] is a standard for hashing and signing of typed structured data.
+ *
+ * The encoding specified in the EIP is very generic, and such a generic implementation in Solidity is not feasible,
+ * thus this contract does not implement the encoding itself. Protocols need to implement the type-specific encoding
+ * they need in their contracts using a combination of `abi.encode` and `keccak256`.
+ *
+ * This contract only implements the EIP 712 domain separator that is used as part of the encoding scheme. The
+ * implementation contained here was designed to be as efficient as possible while still properly updating the chain id
+ * to protect against replay attacks on an eventual fork of the chain.
+ */
 abstract contract EIP712 {
     /* solhint-disable var-name-mixedcase */
     // Cache the domain separator as an immutable value, but also store the chain id that it corresponds to, in order to
@@ -15,6 +26,18 @@ abstract contract EIP712 {
     bytes32 private immutable _TYPE_HASH_SALT;
     /* solhint-enable var-name-mixedcase */
 
+    /**
+     * @dev Initializes the domain separator and parameter caches.
+     *
+     * The meaning of `name` and `version` is specified in
+     * https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator[EIP 712]:
+     *
+     * - `name`: the user readable name of the signing domain, i.e. the name of the DApp or the protocol.
+     * - `version`: the current major version of the signing domain.
+     *
+     * NOTE: These parameters cannot be changed except through a xref:learn::upgrading-smart-contracts.adoc[smart
+     * contract upgrade].
+     */
     constructor(string memory name, string memory version) internal {
         bytes32 hashedName = keccak256(bytes(name));
         bytes32 hashedVersion = keccak256(bytes(version));
@@ -27,6 +50,9 @@ abstract contract EIP712 {
         _TYPE_HASH_SALT = keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract,bytes32 salt)");
     }
 
+    /**
+     * @dev Returns the domain separator for the current chain.
+     */
     function _domainSeparator() internal view returns (bytes32) {
         if (_getChainId() == _CACHED_CHAIN_ID) {
             return _CACHED_DOMAIN_SEPARATOR;
@@ -35,6 +61,12 @@ abstract contract EIP712 {
         }
     }
 
+    /**
+     * @dev Returns the domain separator for the current chain, containing a salt as defined in 
+     * https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator[EIP 712]:
+     *
+     * - `salt`: a disambiguating salt for the protocol.
+     */
     function _domainSeparator(bytes32 salt) internal view returns (bytes32) {
         return keccak256(
             abi.encode(
