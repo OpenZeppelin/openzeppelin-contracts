fa65bf89beac7821ab7aff86aefc1d93e7c7bbfa
Francisco Giordano <frangio.1@gmail.com>
2020-11-30 16:31:42 -0300

    Remove inheritance that conflicts with upgradeable transpiler

--- contracts/mocks/BadBeacon.sol
+++ contracts/mocks/BadBeacon.sol
@@ -2,13 +2,11 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import "../proxy/IBeacon.sol";
-
 contract BadBeaconNoImpl {
 }
 
-contract BadBeaconNotContract is IBeacon {
-    function implementation() external view override returns (address) {
+contract BadBeaconNotContract {
+    function implementation() external view returns (address) {
         return address(0x1);
     }
 }
