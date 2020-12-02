d31df32e0e47491d9d20b5d031bb1cd87af0e5de
ashwinYardi <ashwin.yar@live.com>
2020-12-02 08:09:51 +0530

    remove unsued describe,context from tests and add tests to establish total supply behaviour

--- test/presets/ERC20PresetFixedSupply.test.js
+++ test/presets/ERC20PresetFixedSupply.test.js
@@ -11,23 +11,32 @@ contract('ERC20PresetFixedSupply', function (accounts) {
   const name = 'PresetFixedSupply';
   const symbol = 'PFS';
 
-  const intialSupply = new BN('50000');
+  const initialSupply = new BN('50000');
+  const amount = new BN('10000');
 
-  beforeEach(async function () {
-    this.token = await ERC20PresetFixedSupply.new(name, symbol, intialSupply, owner, { from: deployer });
+  before(async function () {
+    this.token = await ERC20PresetFixedSupply.new(name, symbol, initialSupply, owner, { from: deployer });
   });
 
   it('deployer has the balance equal to initial supply', async function () {
-    expect(await this.token.balanceOf(owner)).to.be.bignumber.equal(intialSupply);
+    expect(await this.token.balanceOf(owner)).to.be.bignumber.equal(initialSupply);
+  });
+
+  it('total supply is equal to initial supply', async function () {
+    expect(await this.token.totalSupply()).to.be.bignumber.equal(initialSupply);
   });
 
   describe('burning', function () {
     it('holders can burn their tokens', async function () {
-      const amount = new BN('10000');
-      const remainingBalance = intialSupply.sub(amount);
+      const remainingBalance = initialSupply.sub(amount);
       const receipt = await this.token.burn(amount, { from: owner });
       expectEvent(receipt, 'Transfer', { from: owner, to: ZERO_ADDRESS, value: amount });
       expect(await this.token.balanceOf(owner)).to.be.bignumber.equal(remainingBalance);
+    });
+
+    it('decrements totalSupply', async function () {
+      const expectedSupply = initialSupply.sub(amount);
+      expect(await this.token.totalSupply()).to.be.bignumber.equal(expectedSupply);
     });
   });
 });
--- test/presets/ERC777PresetFixedSupply.test.js
+++ test/presets/ERC777PresetFixedSupply.test.js
@@ -13,45 +13,33 @@ contract('ERC777PresetFixedSupply', function (accounts) {
 
   const defaultOperators = [defaultOperatorA, defaultOperatorB];
 
-  context('with default operators', function () {
-    beforeEach(async function () {
-      this.token = await ERC777PresetFixedSupply.new(name, symbol, initialSupply, owner, defaultOperators);
-    });
+  beforeEach(async function () {
+    this.token = await ERC777PresetFixedSupply.new(name, symbol, initialSupply, owner, defaultOperators);
+  });
 
-    describe('token is created', function () {
-      it('returns the name', async function () {
-        expect(await this.token.name()).to.equal(name);
-      });
+  it('returns the name', async function () {
+    expect(await this.token.name()).to.equal(name);
+  });
 
-      it('returns the symbol', async function () {
-        expect(await this.token.symbol()).to.equal(symbol);
-      });
+  it('returns the symbol', async function () {
+    expect(await this.token.symbol()).to.equal(symbol);
+  });
 
-      it('returns a granularity of 1', async function () {
-        expect(await this.token.granularity()).to.be.bignumber.equal('1');
-      });
+  it('returns the default operators', async function () {
+    expect(await this.token.defaultOperators()).to.deep.equal(defaultOperators);
+  });
 
-      it('returns the default operators', async function () {
-        expect(await this.token.defaultOperators()).to.deep.equal(defaultOperators);
-      });
+  it('default operators are operators for all accounts', async function () {
+    for (const operator of defaultOperators) {
+      expect(await this.token.isOperatorFor(operator, anyone)).to.equal(true);
+    }
+  });
 
-      it('default operators are operators for all accounts', async function () {
-        for (const operator of defaultOperators) {
-          expect(await this.token.isOperatorFor(operator, anyone)).to.equal(true);
-        }
-      });
+  it('returns the total supply equal to initial supply', async function () {
+    expect(await this.token.totalSupply()).to.be.bignumber.equal(initialSupply);
+  });
 
-      it('returns the total supply equal to initial supply', async function () {
-        expect(await this.token.totalSupply()).to.be.bignumber.equal(initialSupply);
-      });
-
-      it('returns the balance of owner equal to initial supply', async function () {
-        expect(await this.token.balanceOf(owner)).to.be.bignumber.equal(initialSupply);
-      });
-
-      it('returns 18 when decimals is called', async function () {
-        expect(await this.token.decimals()).to.be.bignumber.equal('18');
-      });
-    });
+  it('returns the balance of owner equal to initial supply', async function () {
+    expect(await this.token.balanceOf(owner)).to.be.bignumber.equal(initialSupply);
   });
 });
