9c789b0516599551d4b887c1588d1c0e95466b7d
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-01-08 10:45:23 +0100

    silence inline assembly warning in mock

--- contracts/mocks/SafeMathMock.sol
+++ contracts/mocks/SafeMathMock.sol
@@ -27,36 +27,46 @@ contract SafeMathMock {
 
     function mulMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := mload(0x40) }
         for (uint256 i = 0; i < length; ++i) { SafeMath.mul(1, 1); }
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := sub(mload(0x40), mem) }
     }
 
     function divMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := mload(0x40) }
         for (uint256 i = 0; i < length; ++i) { SafeMath.div(1, 1); }
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := sub(mload(0x40), mem) }
     }
 
     function subMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := mload(0x40) }
         for (uint256 i = 0; i < length; ++i) { SafeMath.sub(1, 1); }
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := sub(mload(0x40), mem) }
     }
 
     function addMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := mload(0x40) }
         for (uint256 i = 0; i < length; ++i) { SafeMath.add(1, 1); }
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := sub(mload(0x40), mem) }
     }
 
     function modMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := mload(0x40) }
         for (uint256 i = 0; i < length; ++i) { SafeMath.mod(1, 1); }
+        // solhint-disable-next-line no-inline-assembly
         assembly { mem := sub(mload(0x40), mem) }
     }
 
