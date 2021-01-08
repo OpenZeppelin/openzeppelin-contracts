17e3e4c2fc3272c87e0d9cf5c2dcf5f7656998dd
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-01-08 17:52:12 +0100

    adding an internal _setOwner to Ownable

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -52,8 +52,7 @@ abstract contract Ownable is Context {
      * thereby removing any functionality that is only available to the owner.
      */
     function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
+        _setOwner(address(0));
     }
 
     /**
@@ -62,6 +61,14 @@ abstract contract Ownable is Context {
      */
     function transferOwnership(address newOwner) public virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    /**
+     * @dev Internal function that forces an ownership change. Can be used to
+     * implement custom ownership management logic in childs contracts.
+     */
+    function _setOwner(address newOwner) internal virtual {
         emit OwnershipTransferred(_owner, newOwner);
         _owner = newOwner;
     }
