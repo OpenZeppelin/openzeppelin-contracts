68e3724a395f07f0952df34d837434ff26a34339
Akarsh <akarshb02@gmail.com>
2021-01-15 16:32:43 +0530

    Update Ownable.sol

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -23,7 +23,7 @@ abstract contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor () internal {
+    constructor () public {
         address msgSender = _msgSender();
         _owner = msgSender;
         emit OwnershipTransferred(address(0), msgSender);
