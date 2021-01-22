7e18c3b9d6015fcb6a9f1b7bf8aa6fe56cb74047
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-01-22 11:22:30 +0100

    address comments on proxy/TransparentUpgradeableProxy

--- contracts/proxy/TransparentUpgradeableProxy.sol
+++ contracts/proxy/TransparentUpgradeableProxy.sol
@@ -67,7 +67,7 @@ contract TransparentUpgradeableProxy is UpgradeableProxy {
      * https://eth.wiki/json-rpc/API#eth_getstorageat[`eth_getStorageAt`] RPC call.
      * `0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103`
      */
-    function admin() external virtual ifAdmin returns (address admin_) {
+    function admin() external ifAdmin returns (address admin_) {
         admin_ = _admin();
     }
 
@@ -80,7 +80,7 @@ contract TransparentUpgradeableProxy is UpgradeableProxy {
      * https://eth.wiki/json-rpc/API#eth_getstorageat[`eth_getStorageAt`] RPC call.
      * `0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc`
      */
-    function implementation() external virtual ifAdmin returns (address implementation_) {
+    function implementation() external ifAdmin returns (address implementation_) {
         implementation_ = _implementation();
     }
 
