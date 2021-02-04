33098290e5219d0a17cda0a76a397b31d753fbe5
Hadrien Croubois <hc@iex.ec>
2021-02-04 19:29:49 +0100

    Update contracts/utils/Strings.sol
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- contracts/utils/Strings.sol
+++ contracts/utils/Strings.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
  * @dev String operations.
  */
 library Strings {
-    bytes16 private constant alphabet = 0x30313233343536373839616263646566;
+    bytes16 private constant alphabet = "0123456789abcdef";
 
     /**
      * @dev Converts a `uint256` to its ASCII `string` representation.
