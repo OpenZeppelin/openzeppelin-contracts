0283326ecf7bc0c3ba8f4a1a3a30a60b7081de67
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-04 20:58:52 +0100

    change revert message

--- contracts/utils/Counters.sol
+++ contracts/utils/Counters.sol
@@ -30,7 +30,7 @@ library Counters {
 
     function decrement(Counter storage counter) internal {
         uint256 value = counter._value;
-        require(value > 0, "Counter: decrement underflow");
+        require(value > 0, "Counter: decrement overflow");
         unchecked {
             counter._value = value - 1;
         }
