592e8c3dabd6252129d559a39468e43ba5fe8589
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-12 19:21:11 +0100

    add a ERC165Storage implementation

--- contracts/mocks/ERC165Mock.sol
+++ contracts/mocks/ERC165Mock.sol
@@ -2,31 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "../introspection/IERC165.sol";
+import "../introspection/ERC165Storage.sol";
 
-contract ERC165Mock is IERC165 {
-    /**
-     * @dev A mapping of interface id to whether or not it's supported.
-     */
-    mapping(bytes4 => bool) private _supportedInterfaces;
-
-    /**
-     * @dev A contract implementing SupportsInterfaceWithLookup
-     * implement ERC165 itself.
-     */
-    constructor () {
-        registerInterface(type(IERC165).interfaceId);
-    }
-
-    /**
-     * @dev Implement supportsInterface(bytes4) using a lookup table.
-     */
-    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
-        return _supportedInterfaces[interfaceId];
-    }
-
+contract ERC165Mock is ERC165Storage {
     function registerInterface(bytes4 interfaceId) public {
-        require(interfaceId != 0xffffffff, "ERC165: invalid interface id");
-        _supportedInterfaces[interfaceId] = true;
+        _registerInterface(interfaceId);
     }
 }
