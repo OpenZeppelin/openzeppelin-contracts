da584116d6b9ab30e759fcbf3cdb102674cc194e
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-17 22:57:50 +0100

    address review comments

--- contracts/utils/Context.sol
+++ contracts/utils/Context.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
  * @dev Provides information about the current execution context, including the
  * sender of the transaction and its data. While these are generally available
  * via msg.sender and msg.data, they should not be accessed in such a direct
- * manner, since when dealing with GSN meta-transactions the account sending and
+ * manner, since when dealing with meta-transactions the account sending and
  * paying for execution may not be the actual sender (as far as an application
  * is concerned).
  *
