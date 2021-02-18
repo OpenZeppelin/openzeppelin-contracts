cd6a1dd768e83d51f16a1d1ad2196365a7392d8f
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-18 21:31:35 +0100

    wave 2.2 of reorg

--- contracts/token/ERC20/utils/TokenTimelock.sol
+++ contracts/token/ERC20/utils/TokenTimelock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/utils/SafeERC20.sol";
+import "./SafeERC20.sol";
 
 /**
  * @dev A token holder contract that will allow a beneficiary to extract the
