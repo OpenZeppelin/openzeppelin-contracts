e8fd5db88c553541cc2742f7a15c7e4ce6619288
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-19 19:05:02 +0100

    fix npm pack

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 # cross platform `mkdir -p`
 node -e 'fs.mkdirSync("build/contracts", { recursive: true })'
 
-cp artifacts/*.json build/contracts
+cp artifacts/**/*.json build/contracts
 
 node scripts/remove-ignored-artifacts.js
