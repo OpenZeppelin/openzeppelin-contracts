0d71a30c821386b15f5af4aa664f990682cca293
Francisco Giordano <frangio.1@gmail.com>
2021-02-18 22:57:14 -0300

    remove dependencies from rewrite-imports

--- scripts/rewrite-imports.js
+++ scripts/rewrite-imports.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const glob = require('glob');
+const { promises: fs } = require('fs');
 const path = require('path');
 
 const versions = [
@@ -7,7 +6,7 @@ const versions = [
   '@openzeppelin/contracts-upgradeable',
 ];
 
-const updates = {
+const pathUpdates = {
   'access/AccessControl.sol': undefined,
   'access/Ownable.sol': undefined,
   'access/TimelockController.sol': 'governance/TimelockController.sol',
@@ -42,110 +41,122 @@ const updates = {
   'presets/ERC721PresetMinterPauserAutoId.sol': 'token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol',
   'presets/ERC777PresetFixedSupply.sol': 'token/ERC777/presets/ERC777PresetFixedSupply.sol',
   'proxy/BeaconProxy.sol': 'proxy/beacon/BeaconProxy.sol',
-  'proxy/Clones.sol': undefined,
+  // 'proxy/Clones.sol': undefined,
   'proxy/IBeacon.sol': 'proxy/beacon/IBeacon.sol',
   'proxy/Initializable.sol': 'utils/Initializable.sol',
   'proxy/ProxyAdmin.sol': 'proxy/transparent/ProxyAdmin.sol',
-  'proxy/Proxy.sol': undefined,
+  // 'proxy/Proxy.sol': undefined,
   'proxy/TransparentUpgradeableProxy.sol': 'proxy/transparent/TransparentUpgradeableProxy.sol',
   'proxy/UpgradeableBeacon.sol': 'proxy/beacon/UpgradeableBeacon.sol',
-  'proxy/UpgradeableProxy.sol': undefined,
+  // 'proxy/UpgradeableProxy.sol': undefined,
   'token/ERC1155/ERC1155Burnable.sol': 'token/ERC1155/extensions/ERC1155Burnable.sol',
   'token/ERC1155/ERC1155Holder.sol': 'token/ERC1155/utils/ERC1155Holder.sol',
   'token/ERC1155/ERC1155Pausable.sol': 'token/ERC1155/extensions/ERC1155Pausable.sol',
   'token/ERC1155/ERC1155Receiver.sol': 'token/ERC1155/utils/ERC1155Receiver.sol',
-  'token/ERC1155/ERC1155.sol': undefined,
+  // 'token/ERC1155/ERC1155.sol': undefined,
   'token/ERC1155/IERC1155MetadataURI.sol': 'token/ERC1155/extensions/IERC1155MetadataURI.sol',
-  'token/ERC1155/IERC1155Receiver.sol': undefined,
-  'token/ERC1155/IERC1155.sol': undefined,
+  // 'token/ERC1155/IERC1155Receiver.sol': undefined,
+  // 'token/ERC1155/IERC1155.sol': undefined,
   'token/ERC20/ERC20Burnable.sol': 'token/ERC20/extensions/ERC20Burnable.sol',
   'token/ERC20/ERC20Capped.sol': 'token/ERC20/extensions/ERC20Capped.sol',
   'token/ERC20/ERC20Pausable.sol': 'token/ERC20/extensions/ERC20Pausable.sol',
   'token/ERC20/ERC20Snapshot.sol': 'token/ERC20/extensions/ERC20Snapshot.sol',
-  'token/ERC20/ERC20.sol': undefined,
-  'token/ERC20/IERC20.sol': undefined,
+  // 'token/ERC20/ERC20.sol': undefined,
+  // 'token/ERC20/IERC20.sol': undefined,
   'token/ERC20/SafeERC20.sol': 'token/ERC20/utils/SafeERC20.sol',
   'token/ERC20/TokenTimelock.sol': 'token/ERC20/utils//TokenTimelock.sol',
   'token/ERC721/ERC721Burnable.sol': 'token/ERC721/extensions/ERC721Burnable.sol',
   'token/ERC721/ERC721Holder.sol': 'token/ERC721/utils/ERC721Holder.sol',
   'token/ERC721/ERC721Pausable.sol': 'token/ERC721/extensions/ERC721Pausable.sol',
-  'token/ERC721/ERC721.sol': undefined,
+  // 'token/ERC721/ERC721.sol': undefined,
   'token/ERC721/IERC721Enumerable.sol': 'token/ERC721/extensions/IERC721Enumerable.sol',
   'token/ERC721/IERC721Metadata.sol': 'token/ERC721/extensions/IERC721Metadata.sol',
-  'token/ERC721/IERC721Receiver.sol': undefined,
-  'token/ERC721/IERC721.sol': undefined,
-  'token/ERC777/ERC777.sol': undefined,
-  'token/ERC777/IERC777Recipient.sol': undefined,
-  'token/ERC777/IERC777Sender.sol': undefined,
-  'token/ERC777/IERC777.sol': undefined,
-  'utils/Address.sol': undefined,
-  'utils/Arrays.sol': undefined,
-  'utils/Context.sol': undefined,
-  'utils/Counters.sol': undefined,
-  'utils/Create2.sol': undefined,
+  // 'token/ERC721/IERC721Receiver.sol': undefined,
+  // 'token/ERC721/IERC721.sol': undefined,
+  // 'token/ERC777/ERC777.sol': undefined,
+  // 'token/ERC777/IERC777Recipient.sol': undefined,
+  // 'token/ERC777/IERC777Sender.sol': undefined,
+  // 'token/ERC777/IERC777.sol': undefined,
+  // 'utils/Address.sol': undefined,
+  // 'utils/Arrays.sol': undefined,
+  // 'utils/Context.sol': undefined,
+  // 'utils/Counters.sol': undefined,
+  // 'utils/Create2.sol': undefined,
   'utils/EnumerableMap.sol': 'utils/structs/EnumerableMap.sol',
   'utils/EnumerableSet.sol': 'utils/structs/EnumerableSet.sol',
   'utils/Pausable.sol': 'security/Pausable.sol',
   'utils/ReentrancyGuard.sol': 'security/ReentrancyGuard.sol',
   'utils/SafeCast.sol': 'utils/math/SafeCast.sol',
-  'utils/Strings.sol': undefined,
+  // 'utils/Strings.sol': undefined,
 };
 
-const globAsync = (pattern) =>
-  new Promise((resolve, reject) => {
-    glob(pattern, (error, files) => error ? reject(error) : resolve(files));
-  });
+async function main () {
+  const files = await listFilesRecursively('contracts');
+  const solidityFiles = files.filter(f => f.match(/\.sol$/));
 
-const getPathsAsync = (patterns) =>
-  Promise.all(patterns.map(globAsync)).then(paths => [].concat.apply([], paths));
+  const updatedFiles = [];
 
-const replaceAsync = (file, from, to, encoding = 'utf-8') =>
-  new Promise((resolve, reject) => {
-    fs.readFile(file, encoding, (error, contents) => {
-      if (error) {
-        return reject(error);
-      }
-
-      const newContents = from.reduce((contents, item, i) => contents.replace(item, to[i]), contents);
-
-      if (contents === newContents) {
-        return resolve({ file, hasChanged: false });
-      }
-
-      fs.writeFile(file, newContents, encoding, error => error ? reject(error) : resolve({ file, hasChanged: true }));
-    });
-  });
-
-const replaceInFiles = (files, from, to) =>
-  getPathsAsync(files)
-    .then(paths => Promise.all(paths.map(file => replaceAsync(file, from, to))))
-    .then(results => {
-      return results;
-    })
-    .catch(error => {
-      throw error;
-    });
-
-(async () => {
-  const results = await replaceInFiles(
-    /* files */
-    (process.argv.length > 2 ? process.argv.slice(2) : [ 'contracts' ])
-      .map(file => fs.statSync(file).isDirectory() ? path.join(file, '**', '*.sol') : file),
-    /* from */
-    Object.entries(updates)
-      .filter(([ from, to ]) => to)
-      .flatMap(([ from, to ]) => versions.map(version => path.join(version, from))),
-    /* to */
-    Object.entries(updates)
-      .filter(([ from, to ]) => to)
-      .flatMap(([ from, to ]) => versions.map(version => path.join(version, to))),
-  );
-
-  const changed = results.filter(result => result.hasChanged);
-  if (changed.length) {
-    console.log(`${changed.length} file(s) were changed`);
-    changed.forEach(result => console.log('-', result.file));
-  } else {
-    console.log('No files were changed');
+  for (const file of solidityFiles) {
+    const updated = await updateFile(file, updateImportPaths);
+    if (updated) {
+      updatedFiles.push(file);
+    }
   }
-})().catch(console.error);
+
+  if (updatedFiles.length > 0) {
+    console.log(`${updatedFiles.length} file(s) were updated`);
+    for (const c of updatedFiles) {
+      console.log('-', c);
+    }
+  } else {
+    console.log('No files were updated');
+  }
+}
+
+async function listFilesRecursively (dir) {
+  const queue = [dir];
+  const files = [];
+
+  while (queue.length > 0) {
+    const top = queue.shift();
+    for (const e of await fs.readdir(top, { withFileTypes: true })) {
+      const p = path.join(top, e.name);
+      if (e.isDirectory()) {
+        queue.push(p);
+      } else if (e.isFile()) {
+        files.push(p);
+      }
+    }
+  }
+
+  return files;
+}
+
+async function updateFile (file, update) {
+  const content = await fs.readFile(file, 'utf8');
+  const updatedContent = update(content);
+  if (updatedContent !== content) {
+    await fs.writeFile(file, updatedContent);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateImportPaths (source) {
+  for (const [oldPath, newPath] of Object.entries(pathUpdates)) {
+    for (const ver of versions) {
+      source = source.replace(
+        ver + '/' + oldPath,
+        ver + '/' + newPath,
+      );
+    }
+  }
+
+  return source;
+}
+
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
