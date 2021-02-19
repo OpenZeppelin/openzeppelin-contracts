c3178ff942f9f487b9fda2c648aa19e633560adb
Francisco Giordano <frangio.1@gmail.com>
2021-02-19 18:17:42 -0300

    Add message to caution that SafeMath.sol is for Solidity 0.8

--- contracts/math/SafeMath.sol
+++ contracts/math/SafeMath.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
+
 /**
  * @dev Wrappers over Solidity's arithmetic operations.
  */
