d1fc5a2de319c13e184c1a17f7424c9dd793ad97
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-19 14:51:16 +0100

    fix merging error

--- contracts/mocks/ERC165StorageMock.sol
+++ contracts/mocks/ERC165StorageMock.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../introspection/ERC165Storage.sol";
+import "../utils/introspection/ERC165Storage.sol";
 
 contract ERC165StorageMock is ERC165Storage {
     function registerInterface(bytes4 interfaceId) public {
