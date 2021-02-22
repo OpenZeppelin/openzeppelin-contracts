65f79d0b104bb7ec753c314820df7107c0772872
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-23 00:30:23 +0100

    rebase onto master

--- contracts/proxy/draft-vtable/VTableProxy.sol
+++ contracts/proxy/draft-vtable/VTableProxy.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "./VTable.sol";
 import "./modules/VTableUpdateModule.sol";
-import "../../proxy/Proxy.sol";
+import "../Proxy.sol";
 import "../../utils/Context.sol";
 
 /**
