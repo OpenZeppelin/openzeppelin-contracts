53d5d51341477406cbee3722c210649ce15faf23
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-04 17:14:00 +0100

    address comments from PR

--- contracts/token/ERC20/extensions/IERC20Metadata.sol
+++ contracts/token/ERC20/extensions/IERC20Metadata.sol
@@ -9,17 +9,17 @@ import "../IERC20.sol";
  */
 interface IERC20Metadata is IERC20 {
     /**
-     * @dev Returns the name of the tokens.
+     * @dev Returns the name of the token.
      */
     function name() external view returns (string memory);
 
     /**
-     * @dev Returns the symbol of the tokens.
+     * @dev Returns the symbol of the token.
      */
     function symbol() external view returns (string memory);
 
     /**
-     * @dev Returns the decimals places of the tokens.
+     * @dev Returns the decimals places of the token.
      */
     function decimals() external view returns (uint8);
 }
