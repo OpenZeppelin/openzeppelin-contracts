628a6976a54e07f72afac2e0000c23e33cd5d502
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-16 21:48:44 +0100

    remove safemath in ERC1155Supply

--- contracts/token/ERC1155/extensions/ERC1155Supply.sol
+++ contracts/token/ERC1155/extensions/ERC1155Supply.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.0;
 
 import "../ERC1155.sol";
-import "../../../utils/math/SafeMath.sol";
 
 /**
  * @dev ERC1155 token with totalSupply (per id).
@@ -14,8 +13,6 @@ import "../../../utils/math/SafeMath.sol";
  * same id are not going to be minted
  */
 abstract contract ERC1155Supply is ERC1155 {
-    using SafeMath for uint256;
-
     mapping (uint256 => uint256) private _totalSupply;
 
     /**
@@ -34,25 +31,25 @@ abstract contract ERC1155Supply is ERC1155 {
 
     function _mint(address account, uint256 id, uint256 amount, bytes memory data) internal virtual override {
         super._mint(account, id, amount, data);
-        _totalSupply[id] = _totalSupply[id].add(amount);
+        _totalSupply[id] += amount;
     }
 
     function _mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override {
         super._mintBatch(to, ids, amounts, data);
         for (uint256 i = 0; i < ids.length; ++i) {
-            _totalSupply[ids[i]] = _totalSupply[ids[i]].add(amounts[i]);
+            _totalSupply[ids[i]] += amounts[i];
         }
     }
 
     function _burn(address account, uint256 id, uint256 amount) internal virtual override {
         super._burn(account, id, amount);
-        _totalSupply[id] = _totalSupply[id].sub(amount);
+        _totalSupply[id] -= amount;
     }
 
     function _burnBatch(address account, uint256[] memory ids, uint256[] memory amounts) internal virtual override {
         super._burnBatch(account, ids, amounts);
         for (uint256 i = 0; i < ids.length; ++i) {
-            _totalSupply[ids[i]] = _totalSupply[ids[i]].sub(amounts[i]);
+            _totalSupply[ids[i]] -= amounts[i];
         }
     }
 }
