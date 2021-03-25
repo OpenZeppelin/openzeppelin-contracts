874d7b2c0b06bf404d95a41de7b0deaf2e55a4e8
Martín Triay <martriay@gmail.com>
2021-03-24 23:46:44 -0300

    add documentation

--- contracts/utils/BatchCall.sol
+++ contracts/utils/BatchCall.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.0;
 import "./Address.sol";
 
 /*
- * @dev Provides a way to batch together multiple function calls in a single external call.
+ * @dev Provides a function to batch together multiple calls in a single external call.
  */
 abstract contract BatchCall {
-   function batchCall(bytes[] calldata data) external returns(bytes[] memory results) {
+    /**
+    * @dev Receives and executes a batch of function calls on this contract.
+    */
+    function batchCall(bytes[] calldata data) external returns(bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint i = 0; i < data.length; i++) {
             results[i] = Address.functionDelegateCall(address(this), data[i]);
