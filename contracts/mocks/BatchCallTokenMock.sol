01c2a032c2e69c5cb429d1d713b684c46787baa9
Martín Triay <martriay@gmail.com>
2021-03-26 20:11:37 -0300

    go back to existing naming across files *

--- contracts/mocks/BatchCallTokenMock.sol
+++ contracts/mocks/BatchCallTokenMock.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-import "../utils/BatchCall.sol";
+import "../utils/Multicall.sol";
 import "./ERC20Mock.sol";
 
-contract BatchCallTokenMock is ERC20Mock, BatchCall {
-    constructor (uint256 initialBalance) ERC20Mock("BatchCallToken", "BCT", msg.sender, initialBalance) {}
+contract MulticallTokenMock is ERC20Mock, Multicall {
+    constructor (uint256 initialBalance) ERC20Mock("MulticallToken", "BCT", msg.sender, initialBalance) {}
 }
--- test/utils/BatchCall.test.js
+++ test/utils/BatchCall.test.js
@@ -1,31 +1,31 @@
 const { BN, expectRevert } = require('@openzeppelin/test-helpers');
-const BatchCallTokenMock = artifacts.require('BatchCallTokenMock');
+const MulticallTokenMock = artifacts.require('MulticallTokenMock');
 
-contract('BatchCallToken', function (accounts) {
+contract('MulticallToken', function (accounts) {
   const [deployer, alice, bob] = accounts;
   const amount = 12000;
 
   beforeEach(async function () {
-    this.batchCallToken = await BatchCallTokenMock.new(new BN(amount), { from: deployer });
+    this.multicallToken = await MulticallTokenMock.new(new BN(amount), { from: deployer });
   });
 
   it('batches function calls', async function () {
-    expect(await this.batchCallToken.balanceOf(alice)).to.be.bignumber.equal(new BN('0'));
-    expect(await this.batchCallToken.balanceOf(bob)).to.be.bignumber.equal(new BN('0'));
+    expect(await this.multicallToken.balanceOf(alice)).to.be.bignumber.equal(new BN('0'));
+    expect(await this.multicallToken.balanceOf(bob)).to.be.bignumber.equal(new BN('0'));
 
-    await this.batchCallToken.batchCall([
-      this.batchCallToken.contract.methods.transfer(alice, amount / 2).encodeABI(),
-      this.batchCallToken.contract.methods.transfer(bob, amount / 3).encodeABI(),
+    await this.multicallToken.Multicall([
+      this.multicallToken.contract.methods.transfer(alice, amount / 2).encodeABI(),
+      this.multicallToken.contract.methods.transfer(bob, amount / 3).encodeABI(),
     ], { from: deployer });
 
-    expect(await this.batchCallToken.balanceOf(alice)).to.be.bignumber.equal(new BN(amount / 2));
-    expect(await this.batchCallToken.balanceOf(bob)).to.be.bignumber.equal(new BN(amount / 3));
+    expect(await this.multicallToken.balanceOf(alice)).to.be.bignumber.equal(new BN(amount / 2));
+    expect(await this.multicallToken.balanceOf(bob)).to.be.bignumber.equal(new BN(amount / 3));
   });
 
   it('bubbles up revert reasons', async function () {
-    const call = this.batchCallToken.batchCall([
-      this.batchCallToken.contract.methods.transfer(alice, amount).encodeABI(),
-      this.batchCallToken.contract.methods.transfer(bob, amount).encodeABI(),
+    const call = this.multicallToken.Multicall([
+      this.multicallToken.contract.methods.transfer(alice, amount).encodeABI(),
+      this.multicallToken.contract.methods.transfer(bob, amount).encodeABI(),
     ], { from: deployer });
 
     await expectRevert(call, 'ERC20: transfer amount exceeds balance');
