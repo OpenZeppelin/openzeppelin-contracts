c27b7f2fafd36233bf07d1f0ff91433a47cc6bae
Martín Triay <martriay@gmail.com>
2021-03-29 21:15:00 -0300

    add 'return values' and 'sequential reverts' tests

--- test/utils/Multicall.test.js
+++ test/utils/Multicall.test.js
@@ -22,6 +22,30 @@ contract('MulticallToken', function (accounts) {
     expect(await this.multicallToken.balanceOf(bob)).to.be.bignumber.equal(new BN(amount / 3));
   });
 
+  it('returns an array with the result of each call', async function () {
+    const MulticallTest = artifacts.require('MulticallTest');
+    const multicallTest = await MulticallTest.new({ from: deployer });
+    await this.multicallToken.transfer(multicallTest.address, amount, { from: deployer });
+    expect(await this.multicallToken.balanceOf(multicallTest.address)).to.be.bignumber.equal(new BN(amount));
+
+    const recipients = [alice, bob];
+    const amounts = [amount / 2, amount / 3].map(n => new BN(n));
+
+    await multicallTest.testReturnValues(this.multicallToken.address, recipients, amounts);
+  });
+
+  it('reverts previous calls', async function () {
+    expect(await this.multicallToken.balanceOf(alice)).to.be.bignumber.equal(new BN('0'));
+
+    const call = this.multicallToken.multicall([
+      this.multicallToken.contract.methods.transfer(alice, amount).encodeABI(),
+      this.multicallToken.contract.methods.transfer(bob, amount).encodeABI(),
+    ], { from: deployer });
+
+    await expectRevert(call, 'ERC20: transfer amount exceeds balance');
+    expect(await this.multicallToken.balanceOf(alice)).to.be.bignumber.equal(new BN('0'));
+  });
+
   it('bubbles up revert reasons', async function () {
     const call = this.multicallToken.multicall([
       this.multicallToken.contract.methods.transfer(alice, amount).encodeABI(),
