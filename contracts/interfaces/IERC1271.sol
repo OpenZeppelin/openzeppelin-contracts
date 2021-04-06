48431a7834f17ca77ce118792e9f91f01dddb3b2
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-04-06 22:10:43 +0200

    document SignatureChecker & IERC1271

--- contracts/interfaces/IERC1271.sol
+++ contracts/interfaces/IERC1271.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.0;
 
+/**
+ * @dev Interface of the ERC1271 standard signature validation method for
+ * contracts as defined in https://eips.ethereum.org/EIPS/eip-1271[ERC-1271].
+ */
 interface IERC1271 {
   /**
    * @dev Should return whether the signature provided is valid for the provided data
--- contracts/utils/cryptography/SignatureChecker.sol
+++ contracts/utils/cryptography/SignatureChecker.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 import "./ECDSA.sol";
 import "../Address.sol";
 import "../../interfaces/IERC1271.sol";
 
+/**
+ * @dev Signature verification helper: Provide a single mechanism to verify both private-key (EOA) ECDSA signature and
+ * ERC1271 contract sigantures. Using this instead of ECDSA.recover in your contract will make them compatible with
+ * smart contract wallets such as Argent and Gnosis.
+ *
+ * Note: unlike ECDSA signatures, contract signature's are revocable, and the outcome of this function can thus change
+ * through time. It could return true at block N and false at block N+1 (or the opposite).
+ */
 library SignatureChecker {
     function isValidSignatureNow(address signer, bytes32 hash, bytes memory signature) internal view returns (bool) {
         if (Address.isContract(signer)) {
