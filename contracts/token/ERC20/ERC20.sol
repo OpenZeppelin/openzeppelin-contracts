718a32b448853240a5a94e816c54c094fe1cbfae
Behrouz <behrouz_ayati@yahoo.com>
2021-04-07 19:06:23 +0430

    Improve transferFrom security
    This function updates allowance data structure after calling `_transfer` function. This can easily lead to re-entrance vulnerabilities, when the user overrides `_transfer` method. We should finish all internal work (ie. state changes) first, and only then calling the (possibly) external function.

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -146,11 +146,11 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * `amount`.
      */
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
-
         uint256 currentAllowance = _allowances[sender][_msgSender()];
         require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
         _approve(sender, _msgSender(), currentAllowance - amount);
+        
+        _transfer(sender, recipient, amount);
 
         return true;
     }
