45ad38e96e31a14684eca503f3bccf6af315ffa3
Francisco Giordano <frangio.1@gmail.com>
2021-04-09 20:07:43 -0300

    reformat StorageSlot

--- contracts/utils/StorageSlot.sol
+++ contracts/utils/StorageSlot.sol
@@ -3,13 +3,43 @@
 pragma solidity ^0.8.0;
 
 library StorageSlot {
-    struct AddressSlot { address value; }
-    struct BooleanSlot { bool value; }
-    struct Bytes32Slot { bytes32 value; }
-    struct Uint256Slot { uint256 value; }
+    struct AddressSlot {
+        address value;
+    }
 
-    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) { assembly { r.slot := slot } }
-    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) { assembly { r.slot := slot } }
-    function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) { assembly { r.slot := slot } }
-    function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) { assembly { r.slot := slot } }
+    struct BooleanSlot {
+        bool value;
+    }
+
+    struct Bytes32Slot {
+        bytes32 value;
+    }
+
+    struct Uint256Slot {
+        uint256 value;
+    }
+
+    function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
 }
