7c77eda5e5f8bff1a31e67fc2110c1da6fc3553b
Francisco Giordano <frangio.1@gmail.com>
2021-04-15 18:55:10 -0300

    add docs for each function in StorageSlot

--- contracts/utils/StorageSlot.sol
+++ contracts/utils/StorageSlot.sol
@@ -8,6 +8,8 @@ pragma solidity ^0.8.0;
  * Storage slots are often used to avoid storage conflict when dealing with upgradeable contracts.
  * This library helps with reading and writing to such slots without the need for inline assembly.
  *
+ * The functions in this library return Slot structs that contain a `value` member that can be used to read or write.
+ *
  * Example usage to set ERC1967 implementation slot:
  * ```
  * contract ERC1967 {
@@ -43,24 +45,36 @@ library StorageSlot {
         uint256 value;
     }
 
+    /**
+     * @dev Returns an `AddressSlot` with member `value` located at `slot`.
+     */
     function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
         assembly {
             r.slot := slot
         }
     }
 
+    /**
+     * @dev Returns an `BooleanSlot` with member `value` located at `slot`.
+     */
     function getBooleanSlot(bytes32 slot) internal pure returns (BooleanSlot storage r) {
         assembly {
             r.slot := slot
         }
     }
 
+    /**
+     * @dev Returns an `Bytes32Slot` with member `value` located at `slot`.
+     */
     function getBytes32Slot(bytes32 slot) internal pure returns (Bytes32Slot storage r) {
         assembly {
             r.slot := slot
         }
     }
 
+    /**
+     * @dev Returns an `Uint256Slot` with member `value` located at `slot`.
+     */
     function getUint256Slot(bytes32 slot) internal pure returns (Uint256Slot storage r) {
         assembly {
             r.slot := slot
