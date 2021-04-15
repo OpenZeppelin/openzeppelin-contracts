5f54669bf9153eac1cd3ac19e14b71e14c9745a8
Francisco Giordano <frangio.1@gmail.com>
2021-04-15 19:22:35 -0300

    use name of return variable

--- contracts/proxy/transparent/TransparentUpgradeableProxy.sol
+++ contracts/proxy/transparent/TransparentUpgradeableProxy.sol
@@ -69,7 +69,7 @@ contract TransparentUpgradeableProxy is ERC1967Proxy {
      * `0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc`
      */
     function implementation() external ifAdmin returns (address implementation_) {
-        return _implementation();
+        implementation_ = _implementation();
     }
 
     /**
