165e6f19489786c9c6abfcc9bdc8f2815d807935
William Morriss <wjmelements@gmail.com>
2021-04-20 12:51:26 -0700

    EnumerableSet: Remove Boundary Check in _at (#2606)
    * remove boundary check
    
    * fix tests for EnumerableSet "index out of bound"
    
    * Changelog
    
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- contracts/utils/structs/EnumerableSet.sol
+++ contracts/utils/structs/EnumerableSet.sol
@@ -127,7 +127,6 @@ library EnumerableSet {
     * - `index` must be strictly less than {length}.
     */
     function _at(Set storage set, uint256 index) private view returns (bytes32) {
-        require(set._values.length > index, "EnumerableSet: index out of bounds");
         return set._values[index];
     }
 
