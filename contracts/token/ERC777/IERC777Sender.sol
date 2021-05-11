cc7fd876b488c94a7dc3e6ac014dac55bcda0265
iskanderandrews <iskander.s.andrews@gmail.com>
2021-05-11 07:17:40 +0200

    contracts/token/ERC777/IERC777Sender.sol: Fix indentation in the IERC777Sender interface SolcDoc

--- contracts/token/ERC777/IERC777Sender.sol
+++ contracts/token/ERC777/IERC777Sender.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.0;
  *
  * {IERC777} Token holders can be notified of operations performed on their
  * tokens by having a contract implement this interface (contract holders can be
- *  their own implementer) and registering it on the
+ * their own implementer) and registering it on the
  * https://eips.ethereum.org/EIPS/eip-1820[ERC1820 global registry].
  *
  * See {IERC1820Registry} and {ERC1820Implementer}.
