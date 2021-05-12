861f07fcefbb785667097c22427668d72035898a
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-12 12:52:36 +0200

    document batchInBlock

--- test/token/ERC20/extensions/draft-ERC20Votes.test.js
+++ test/token/ERC20/extensions/draft-ERC20Votes.test.js
@@ -28,21 +28,26 @@ async function countPendingTransactions() {
 }
 
 async function batchInBlock (txs) {
-  await network.provider.send('evm_setAutomine', [false]);
-
   try {
+    // disable auto-mining
+    await network.provider.send('evm_setAutomine', [false]);
+    // send all transactions
     const promises = txs.map(fn => fn());
-    do {
+    // wait for node to have all pending transactions
+    while (txs.length > await countPendingTransactions()) {
       await queue();
-    } while (txs.length > await countPendingTransactions());
+    }
+    // mine one block
     await network.provider.send('evm_mine');
+    // fetch receipts
     const receipts = await Promise.all(promises);
-
+    // Sanity check, all tx should be in the same block
     const minedBlocks = new Set(receipts.map(({ receipt }) => receipt.blockNumber));
     expect(minedBlocks.size).to.equal(1);
 
     return receipts;
   } finally {
+    // enable auto-mining
     await network.provider.send('evm_setAutomine', [true]);
   }
 }
