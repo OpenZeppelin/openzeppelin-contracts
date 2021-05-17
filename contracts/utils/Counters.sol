bbe472f06ca75621d2eb11655a5ff035741f4085
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-17 19:04:19 +0200

    Add changelog entry and fix documentation

--- contracts/utils/Counters.sol
+++ contracts/utils/Counters.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 /**
  * @title Counters
  * @author Matt Condon (@shrugs)
- * @dev Provides counters that can only be incremented or decremented by one. This can be used e.g. to track the number
+ * @dev Provides counters that can only be incremented, decremented or reset. This can be used e.g. to track the number
  * of elements in a mapping, issuing ERC721 ids, or counting request ids.
  *
  * Include with `using Counters for Counters.Counter;`
