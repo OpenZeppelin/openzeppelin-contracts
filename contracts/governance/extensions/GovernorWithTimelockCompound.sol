8f6844187a69beccf5035a5dc290669852cce479
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-20 22:30:18 +0200

    fix the signature issue with comp timelock

--- contracts/governance/extensions/GovernorWithTimelockCompound.sol
+++ contracts/governance/extensions/GovernorWithTimelockCompound.sol
@@ -2,16 +2,15 @@
 
 pragma solidity ^0.8.0;
 
+import "./IGovernorWithTimelock.sol";
 import "./ICompTimelock.sol";
 import "../Governor.sol";
 import "../TimelockController.sol";
 
-// cannot inherit `IGovernorWithTimelock` since we are not implementing the same interface.
-abstract contract GovernorWithTimelockCompound is Governor {
+abstract contract GovernorWithTimelockCompound is IGovernorWithTimelock, Governor {
     ICompTimelock private _timelock;
     mapping(uint256 => uint256) _proposalETA;
 
-    event ProposalQueued(uint256 indexed proposalId, uint256 eta);
     /**
      * @dev Emitted when the minimum delay for future operations is modified.
      */
@@ -32,18 +31,17 @@ abstract contract GovernorWithTimelockCompound is Governor {
         _timelock = ICompTimelock(payable(newTimelock));
     }
 
-    function timelock() public virtual returns (address) {
+    function timelock() public virtual override returns (address) {
         return address(_timelock);
     }
 
     function queue(
         address[] memory targets,
         uint256[] memory values,
-        string[] memory signature,
         bytes[] memory calldatas,
         bytes32 salt
     )
-    public virtual returns (uint256 proposalId)
+    public virtual override returns (uint256 proposalId)
     {
         // _call is overriden have no action (while keeping the checks)
         proposalId = _execute(targets, values, calldatas, salt);
@@ -52,12 +50,10 @@ abstract contract GovernorWithTimelockCompound is Governor {
         _proposalETA[proposalId] = eta;
 
         for (uint256 i = 0; i < targets.length; ++i) {
-            //TODO verify that bytes4(keccak256(bytes(signature))) matches the first 4 bytes of calldatas[i]
-            //TODO don't pass the first 4 bytes of calldatas[i]
             _timelock.queueTransaction(
                 targets[i],
                 values[i],
-                signature[i],
+                "",
                 calldatas[i],
                 eta
             );
@@ -67,24 +63,12 @@ abstract contract GovernorWithTimelockCompound is Governor {
     }
 
     function execute(
-        address[] memory /*targets*/,
-        uint256[] memory /*values*/,
-        bytes[] memory /*calldatas*/,
-        bytes32 /*salt*/
-    )
-    public payable virtual override returns (uint256)
-    {
-        revert("function-is-disabled");
-    }
-
-    function execute(
         address[] memory targets,
         uint256[] memory values,
-        string[] memory signature,
         bytes[] memory calldatas,
         bytes32 salt
     )
-    public payable virtual returns (uint256 proposalId)
+    public payable virtual override returns (uint256 proposalId)
     {
         proposalId = hashProposal(targets, values, calldatas, salt);
         Address.sendValue(payable(_timelock), msg.value);
@@ -92,12 +76,10 @@ abstract contract GovernorWithTimelockCompound is Governor {
         uint256 eta = _proposalETA[proposalId];
         if (eta > 0) {
             for (uint256 i = 0; i < targets.length; ++i) {
-                //TODO verify that bytes4(keccak256(bytes(signature))) matches the first 4 bytes of calldatas[i]
-                //TODO don't pass the first 4 bytes of calldatas[i]
                 _timelock.executeTransaction(
                     targets[i],
                     values[i],
-                    signature[i],
+                    "",
                     calldatas[i],
                     eta
                 );
@@ -110,23 +92,20 @@ abstract contract GovernorWithTimelockCompound is Governor {
     function _cancel(
         address[] memory targets,
         uint256[] memory values,
-        string[] memory signature,
         bytes[] memory calldatas,
         bytes32 salt
     )
-    internal virtual returns (uint256 proposalId)
+    internal virtual override returns (uint256 proposalId)
     {
         proposalId = super._cancel(targets, values, calldatas, salt);
 
         uint256 eta = _proposalETA[proposalId];
         if (eta > 0) {
             for (uint256 i = 0; i < targets.length; ++i) {
-                //TODO verify that bytes4(keccak256(bytes(signature))) matches the first 4 bytes of calldatas[i]
-                //TODO don't pass the first 4 bytes of calldatas[i]
                 _timelock.cancelTransaction(
                     targets[i],
                     values[i],
-                    signature[i],
+                    "",
                     calldatas[i],
                     eta
                 );
@@ -143,6 +122,6 @@ abstract contract GovernorWithTimelockCompound is Governor {
     )
     internal virtual override
     {
-        // don't do anything here, we don't have the signatures :/
+        // don't do anything here to avoid sload of `eta`
     }
 }
