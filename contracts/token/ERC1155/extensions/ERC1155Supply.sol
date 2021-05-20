9be203156ba8e7d67cd45e2763d39812dc04e87f
Francisco Giordano <frangio.1@gmail.com>
2021-05-20 18:39:23 -0300

    docs

--- contracts/token/ERC1155/extensions/ERC1155Supply.sol
+++ contracts/token/ERC1155/extensions/ERC1155Supply.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.0;
 import "../ERC1155.sol";
 
 /**
- * @dev ERC1155 token with totalSupply (per id).
+ * @dev Extension of ERC1155 that adds tracking of total supply per id.
  *
  * Useful for scenarios where Fungible and Non-fungible tokens have to be
  * clearly identified. Note: While a totalSupply of 1 might mean the
- * corresponding is an NFT, there is no guaranties that no other token with the
- * same id are not going to be minted
+ * corresponding is an NFT, there is no guarantees that no other token with the
+ * same id are not going to be minted.
  */
 abstract contract ERC1155Supply is ERC1155 {
     mapping (uint256 => uint256) private _totalSupply;
@@ -29,11 +29,17 @@ abstract contract ERC1155Supply is ERC1155 {
         return ERC1155Supply.totalSupply(id) > 0;
     }
 
+    /**
+     * @dev See {ERC1155-_mint}.
+     */
     function _mint(address account, uint256 id, uint256 amount, bytes memory data) internal virtual override {
         super._mint(account, id, amount, data);
         _totalSupply[id] += amount;
     }
 
+    /**
+     * @dev See {ERC1155-_mintBatch}.
+     */
     function _mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override {
         super._mintBatch(to, ids, amounts, data);
         for (uint256 i = 0; i < ids.length; ++i) {
@@ -41,11 +47,17 @@ abstract contract ERC1155Supply is ERC1155 {
         }
     }
 
+    /**
+     * @dev See {ERC1155-_burn}.
+     */
     function _burn(address account, uint256 id, uint256 amount) internal virtual override {
         super._burn(account, id, amount);
         _totalSupply[id] -= amount;
     }
 
+    /**
+     * @dev See {ERC1155-_burnBatch}.
+     */
     function _burnBatch(address account, uint256[] memory ids, uint256[] memory amounts) internal virtual override {
         super._burnBatch(account, ids, amounts);
         for (uint256 i = 0; i < ids.length; ++i) {
