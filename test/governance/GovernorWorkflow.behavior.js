ae7edba7089e55bb4e342784b21a506930be5de6
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-25 17:42:34 +0200

    fix lint

--- test/governance/GovernorWorkflow.behavior.js
+++ test/governance/GovernorWorkflow.behavior.js
@@ -10,6 +10,14 @@ async function getReceiptOrReason (promise, reason = undefined) {
   }
 }
 
+function tryGet (obj, path = '') {
+  try {
+    return path.split('.').reduce((o, k) => o[k], obj);
+  } catch (_) {
+    return undefined;
+  }
+}
+
 function runGovernorWorkflow () {
   beforeEach(async () => {
     this.receipts = {};
@@ -18,7 +26,7 @@ function runGovernorWorkflow () {
 
   it('run', async () => {
     // transfer tokens
-    if (this.settings?.voters) {
+    if (tryGet(this.settings, 'voters')) {
       for (const voter of this.settings.voters) {
         if (voter.weight) {
           await this.token.transfer(voter.address, voter.weight, { from: this.settings.tokenHolder });
@@ -27,18 +35,18 @@ function runGovernorWorkflow () {
     }
 
     // propose
-    if (this.settings?.steps?.propose?.enable != false) {
+    if (tryGet(this.settings, 'steps.propose.enable') !== false) {
       this.receipts.propose = await getReceiptOrReason(
         this.governor.propose(...this.settings.proposal),
-        this.settings?.steps?.propose?.reason,
+        tryGet(this.settings, 'steps.propose.reason'),
       );
-      if (this.settings?.steps?.propose?.delay) {
-        await time.increase(this.settings?.steps?.propose?.delay);
+      if (tryGet(this.settings, 'steps.propose.delay')) {
+        await time.increase(tryGet(this.settings, 'steps.propose.delay'));
       }
     }
 
     // vote
-    if (this.settings?.voters) {
+    if (tryGet(this.settings, 'voters')) {
       this.receipts.castVote = [];
       for (const voter of this.settings.voters) {
         if (!voter.signature) {
@@ -49,7 +57,7 @@ function runGovernorWorkflow () {
             ),
           );
         } else {
-          const { signature, v, r, s } = await voter.signature({ proposalId: this.id, support: voter.support });
+          const { v, r, s } = await voter.signature({ proposalId: this.id, support: voter.support });
           this.receipts.castVote.push(
             await getReceiptOrReason(
               this.governor.castVoteBySig(this.id, voter.support, v, r, s),
@@ -57,43 +65,39 @@ function runGovernorWorkflow () {
             ),
           );
         }
-        if (voter?.delay) {
-          await time.increase(voter?.delay);
+        if (tryGet(voter, 'delay')) {
+          await time.increase(tryGet(voter, 'delay'));
         }
       }
     }
 
     // fast forward
     ({ deadline: this.deadline } = await this.governor.viewProposal(this.id));
-    if (this.settings?.steps?.wait?.enable != false) {
+    if (tryGet(this.settings, 'steps.wait.enable') !== false) {
       await time.increaseTo(this.deadline.addn(1));
     }
 
     // queue (off by default)
-    if (this.settings?.steps?.queue?.enable == true) {
+    if (tryGet(this.settings, 'steps.queue.enable') === true) {
       this.receipts.queue = await getReceiptOrReason(
         this.governor.queue(...this.settings.proposal.slice(0, -1)),
-        this.settings?.steps?.queue?.reason,
+        tryGet(this.settings, 'steps.queue.reason'),
       );
-      if (this.settings?.steps?.queue?.delay) {
-        await time.increase(this.settings?.steps?.queue?.delay);
+      if (tryGet(this.settings, 'steps.queue.delay')) {
+        await time.increase(tryGet(this.settings, 'steps.queue.delay'));
       }
     }
 
     // execute
-    if (this.settings?.steps?.execute?.enable != false) {
+    if (tryGet(this.settings, 'steps.execute.enable') !== false) {
       this.receipts.execute = await getReceiptOrReason(
         this.governor.execute(...this.settings.proposal.slice(0, -1)),
-        this.settings?.steps?.execute?.reason,
+        tryGet(this.settings, 'steps.execute.reason'),
       );
-      if (this.settings?.steps?.execute?.delay) {
-        await time.increase(this.settings?.steps?.execute?.delay);
+      if (tryGet(this.settings, 'steps.execute.delay')) {
+        await time.increase(tryGet(this.settings, 'steps.execute.delay'));
       }
     }
-  });
-
-  afterEach(async () => {
-    if(this.settings.after) { throw new Error('GOT AFTER'); }
   });
 }
 
