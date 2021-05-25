a8469138080dbcee3f06b0bb40cb7a4f551580a4
William Entriken <github.com@phor.net>
2021-05-24 22:28:43 -0400

    Cite draft EIP as DRAFT

--- contracts/token/ERC20/extensions/draft-ERC20Permit.sol
+++ contracts/token/ERC20/extensions/draft-ERC20Permit.sol
@@ -10,7 +10,7 @@ import "../../../utils/Counters.sol";
 
 /**
  * @dev Implementation of the ERC20 Permit extension allowing approvals to be made via signatures, as defined in
- * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
+ * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612 DRAFT].
  *
  * Adds the {permit} method, which can be used to change an account's ERC20 allowance (see {IERC20-allowance}) by
  * presenting a message signed by the account. By not relying on `{IERC20-approve}`, the token holder account doesn't
