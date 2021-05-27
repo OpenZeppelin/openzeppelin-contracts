6c000d369089f839f470525a02bdf5f0bfb31abd
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-27 23:25:45 +0200

    mark erc20wrapper as non draft and update documentation

--- contracts/mocks/ERC20VotesMock.sol
+++ contracts/mocks/ERC20VotesMock.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-
 import "../token/ERC20/extensions/ERC20Votes.sol";
 
 contract ERC20VotesMock is ERC20Votes {
--- contracts/mocks/ERC20WrapperMock.sol
+++ contracts/mocks/ERC20WrapperMock.sol
@@ -2,9 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC20/extensions/draft-ERC20Wrapper.sol";
+import "../token/ERC20/extensions/ERC20Wrapper.sol";
 
-// mock class using ERC20Wrapper
 contract ERC20WrapperMock is ERC20Wrapper {
     constructor(IERC20 _underlyingToken, string memory name, string memory symbol)
     ERC20(name, symbol)
