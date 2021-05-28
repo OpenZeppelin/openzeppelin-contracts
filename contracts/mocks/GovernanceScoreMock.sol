1d71b99a1c953760b007c2ceecb00822c84e46fa
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-05-28 19:39:21 +0200

    coverage testing of ScoreVoting

---
+++ contracts/mocks/GovernanceScoreMock.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../token/ERC20/extensions/IComp.sol";
+import "../governance/Governor.sol";
+import "../governance/extensions/ScoreVoting.sol";
+import "../governance/extensions/WithVoteToken.sol";
+
+contract GovernanceScoreMock is Governor, ScoreVoting, WithVoteToken {
+    constructor(string memory name_, string memory version_, IComp token_)
+    EIP712(name_, version_)
+    WithVoteToken(token_)
+    {
+    }
+
+    receive() external payable {}
+
+    function votingDuration() public pure override returns (uint256) { return 7 days; } // FOR TESTING ONLY
+    function quorum(uint256)  public pure override returns (uint256) { return 1;      }
+
+    function cancel(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        bytes32 salt
+    ) public returns (uint256 proposalId) {
+        return _cancel(targets, values, calldatas, salt);
+    }
+}
