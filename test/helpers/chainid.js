d3851a80ca0decc916d677ef5479277207703464
Francisco Giordano <frangio.1@gmail.com>
2021-06-10 15:34:36 -0300

    lint

--- test/helpers/chainid.js
+++ test/helpers/chainid.js
@@ -1,5 +1,7 @@
-async function getChainId() {
-  const chainIdHex = await hre.network.provider.send('eth_chainId', [])
+const hre = require('hardhat');
+
+async function getChainId () {
+  const chainIdHex = await hre.network.provider.send('eth_chainId', []);
   return new hre.web3.utils.BN(chainIdHex.replace(/^0x/, ''), 'hex');
 }
 
