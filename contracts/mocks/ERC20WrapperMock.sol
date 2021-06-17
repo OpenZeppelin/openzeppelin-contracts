832ab3eee48238f1bbc0f2e73730d60bc3c69e26
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-06-17 14:42:32 +0200

    fix lint

--- contracts/mocks/ERC20WrapperMock.sol
+++ contracts/mocks/ERC20WrapperMock.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.0;
 import "../token/ERC20/extensions/ERC20Wrapper.sol";
 
 contract ERC20WrapperMock is ERC20Wrapper {
-    constructor(IERC20 _underlyingToken, string memory name, string memory symbol)
-    ERC20(name, symbol)
-    ERC20Wrapper(_underlyingToken)
-    {}
+    constructor(
+        IERC20 _underlyingToken,
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) ERC20Wrapper(_underlyingToken) {}
 
     function recover(address account) public returns (uint256) {
         return _recover(account);
--- contracts/token/ERC20/extensions/ERC20Wrapper.sol
+++ contracts/token/ERC20/extensions/ERC20Wrapper.sol
@@ -15,7 +15,7 @@ import "../utils/SafeERC20.sol";
  * _Available since v4.2._
  */
 abstract contract ERC20Wrapper is ERC20 {
-    IERC20 immutable public underlying;
+    IERC20 public immutable underlying;
 
     constructor(IERC20 underlyingToken) {
         underlying = underlyingToken;
