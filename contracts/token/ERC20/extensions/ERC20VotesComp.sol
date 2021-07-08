385db5b0d5bd417b09053396b0f29780cfb254c8
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-07-08 16:21:12 +0200

    fix two minor typos

--- contracts/token/ERC20/extensions/ERC20VotesComp.sol
+++ contracts/token/ERC20/extensions/ERC20VotesComp.sol
@@ -12,7 +12,7 @@ import "./ERC20Votes.sol";
  * with Governor Alpha or Bravo) and if you are sure the supply cap of 2^96^ is enough for you. Otherwise, use the
  * {ERC20Votes} variant of this module.
  *
- * This extensions keeps a history (checkpoints) of each account's vote power. Vote power can be delegated either
+ * This extension keeps a history (checkpoints) of each account's vote power. Vote power can be delegated either
  * by calling the {delegate} function directly, or by providing a signature to be used with {delegateBySig}. Voting
  * power can be queried through the public accessors {getCurrentVotes} and {getPriorVotes}.
  *
