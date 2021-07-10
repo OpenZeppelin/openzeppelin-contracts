6d97f0919547df11be9443b54af2d90631eaa733
rotcivegaf <victorfage@gmail.com>
2021-07-10 21:28:12 +0200

    Gas optimization on average function of Math.sol (#2757)
    * change implementation to save gas
    
    * add average test with two max uni256 number

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -25,8 +25,8 @@ library Math {
      * zero.
      */
     function average(uint256 a, uint256 b) internal pure returns (uint256) {
-        // (a + b) / 2 can overflow, so we distribute.
-        return (a / 2) + (b / 2) + (((a % 2) + (b % 2)) / 2);
+        // (a + b) / 2 can overflow.
+        return (a & b) + (a ^ b) / 2;
     }
 
     /**
