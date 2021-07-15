90e06619270174b3462f75136c527ca61eae5167
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-07-15 15:52:42 +0200

    improve values() documentation

--- contracts/utils/structs/EnumerableSet.sol
+++ contracts/utils/structs/EnumerableSet.sol
@@ -131,7 +131,12 @@ library EnumerableSet {
     }
 
     /**
-     * @dev Return the entier set in an array
+     * @dev Return the entire set in an array
+     *
+     * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
+     * to mostly be used by view accessors that are queried without any gas fees. Developers should keep in mind that
+     * this function has an unbounded cost, and using it as part of a state-changing function may render the function
+     * uncallable if the set grows to a point where copying to memory consumes too much gas to fit in a block.
      */
     function _values(Set storage set) private view returns (bytes32[] memory) {
         return set._values;
@@ -192,7 +197,12 @@ library EnumerableSet {
     }
 
     /**
-     * @dev Return the entier set in an array
+     * @dev Return the entire set in an array
+     *
+     * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
+     * to mostly be used by view accessors that are queried without any gas fees. Developers should keep in mind that
+     * this function has an unbounded cost, and using it as part of a state-changing function may render the function
+     * uncallable if the set grows to a point where copying to memory consumes too much gas to fit in a block.
      */
     function values(Bytes32Set storage set) internal view returns (bytes32[] memory) {
         return _values(set._inner);
@@ -253,7 +263,12 @@ library EnumerableSet {
     }
 
     /**
-     * @dev Return the entier set in an array
+     * @dev Return the entire set in an array
+     *
+     * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
+     * to mostly be used by view accessors that are queried without any gas fees. Developers should keep in mind that
+     * this function has an unbounded cost, and using it as part of a state-changing function may render the function
+     * uncallable if the set grows to a point where copying to memory consumes too much gas to fit in a block.
      */
     function values(AddressSet storage set) internal view returns (address[] memory) {
         bytes32[] memory store = _values(set._inner);
@@ -321,7 +336,12 @@ library EnumerableSet {
     }
 
     /**
-     * @dev Return the entier set in an array
+     * @dev Return the entire set in an array
+     *
+     * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
+     * to mostly be used by view accessors that are queried without any gas fees. Developers should keep in mind that
+     * this function has an unbounded cost, and using it as part of a state-changing function may render the function
+     * uncallable if the set grows to a point where copying to memory consumes too much gas to fit in a block.
      */
     function values(UintSet storage set) internal view returns (uint256[] memory) {
         bytes32[] memory store = _values(set._inner);
