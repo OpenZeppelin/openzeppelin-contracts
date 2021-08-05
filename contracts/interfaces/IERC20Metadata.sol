73979625f7cd4e8d9583abb18a7a4ca7eaf1a202
Francisco Giordano <frangio.1@gmail.com>
2021-08-04 23:20:30 -0300

    move interfaces back to original location

--- contracts/interfaces/IERC20Metadata.sol
+++ contracts/interfaces/IERC20Metadata.sol
@@ -2,21 +2,4 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC20.sol";
-
-interface IERC20Metadata is IERC20 {
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() external view returns (string memory);
-
-    /**
-     * @dev Returns the symbol of the token.
-     */
-    function symbol() external view returns (string memory);
-
-    /**
-     * @dev Returns the decimals places of the token.
-     */
-    function decimals() external view returns (uint8);
-}
+import "../token/ERC20/extensions/IERC20Metadata.sol";
--- contracts/token/ERC20/extensions/IERC20Metadata.sol
+++ contracts/token/ERC20/extensions/IERC20Metadata.sol
@@ -2,4 +2,26 @@
 
 pragma solidity ^0.8.0;
 
-import "../../../interfaces/IERC20Metadata.sol";
+import "../IERC20.sol";
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
