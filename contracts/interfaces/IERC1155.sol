18668870fd70a63b922e244833c5dbb5c131fb75
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-08-06 15:36:34 +0200

    Add an interface folder that lists common interfaces (#2517)
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

---
+++ contracts/interfaces/IERC1155.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../token/ERC1155/IERC1155.sol";
---
+++ contracts/interfaces/IERC721Receiver.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../token/ERC721/IERC721Receiver.sol";
