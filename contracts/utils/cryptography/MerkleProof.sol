ab803416f9f76fe6ad79f6939707d08b36afbef3
Anton Bukov <k06aaa@gmail.com>
2021-08-08 00:08:05 +0300

    Introduce verifyAndRecoverIndex

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -23,7 +23,23 @@ library MerkleProof {
         bytes32 root,
         bytes32 leaf
     ) internal pure returns (bool) {
+        (bool success, uint256 index) = verifyAndRecoverIndex(proof, root, leaf);
+        return success;
+    }
+    
+    /**
+     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, a `proof` must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Each
+     * pair of leaves and each pair of pre-images are assumed to be sorted.
+     */
+    function verifyAndRecoverIndex(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool, uint256) {
         bytes32 computedHash = leaf;
+        uint256 index = 0;
 
         for (uint256 i = 0; i < proof.length; i++) {
             bytes32 proofElement = proof[i];
@@ -34,11 +50,12 @@ library MerkleProof {
             } else {
                 // Hash(current element of the proof + current computed hash)
                 computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+                index |= 1 << i;
             }
         }
 
         // Check if the computed hash (root) is equal to the provided root
-        return computedHash == root;
+        return (computedHash == root, index);
     }
     
     /**
