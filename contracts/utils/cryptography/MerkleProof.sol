5b0233c1366bff59c0446a02c1aba33a5ddbc675
Anton Bukov <k06aaa@gmail.com>
2021-08-07 21:48:16 +0300

    Extend MerkleProof to support unsorted Merkle tree

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -40,4 +40,36 @@ library MerkleProof {
         // Check if the computed hash (root) is equal to the provided root
         return computedHash == root;
     }
+    
+    /**
+     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, a `proof` must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Each
+     * pair of leaves and each pair of pre-images are assumed to be sorted.
+     * Instead of assuming pair of leaves and pair of pre-images are being sorted,
+     * this method relies on leaf index to recover merkle path directions.
+     */
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf,
+        uint256 leadIndex
+    ) internal pure returns (bool) {
+        bytes32 computedHash = leaf;
+
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+
+            if ((leadIndex >> i) & 1 == 0) {
+                // Hash(current computed hash + current element of the proof)
+                computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
+            } else {
+                // Hash(current element of the proof + current computed hash)
+                computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
+            }
+        }
+
+        // Check if the computed hash (root) is equal to the provided root
+        return computedHash == root;
+    }
 }
