d10badc299ec7727d1ddb80f11c8cff95129c0ed
Anton Bukov <k06aaa@gmail.com>
2021-08-24 11:57:58 +0200

    Fix name typo

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -70,14 +70,14 @@ library MerkleProof {
         bytes32[] memory proof,
         bytes32 root,
         bytes32 leaf,
-        uint256 leadIndex
+        uint256 leafIndex
     ) internal pure returns (bool) {
         bytes32 computedHash = leaf;
 
         for (uint256 i = 0; i < proof.length; i++) {
             bytes32 proofElement = proof[i];
 
-            if ((leadIndex >> i) & 1 == 0) {
+            if ((leafIndex >> i) & 1 == 0) {
                 // Hash(current computed hash + current element of the proof)
                 computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
             } else {
