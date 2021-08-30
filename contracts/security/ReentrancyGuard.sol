9b4326d91eb7230562f4c3c4eb77909156adc7ee
Hasan Raza <49759922+hasanza@users.noreply.github.com>
2021-08-30 18:20:35 +0500

    Fixed a typo (#2835)

--- contracts/security/ReentrancyGuard.sol
+++ contracts/security/ReentrancyGuard.sol
@@ -43,7 +43,7 @@ abstract contract ReentrancyGuard {
      * @dev Prevents a contract from calling itself, directly or indirectly.
      * Calling a `nonReentrant` function from another `nonReentrant`
      * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and make it call a
+     * by making the `nonReentrant` function external, and making it call a
      * `private` function that does the actual work.
      */
     modifier nonReentrant() {
