3c29f6b7ff34ce9f0c1400a2b86bfe75a576bd3d
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-09-13 21:42:38 +0200

    refactor compound timelock module

--- contracts/governance/extensions/GovernorTimelockCompound.sol
+++ contracts/governance/extensions/GovernorTimelockCompound.sol
@@ -177,8 +177,9 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
     ) internal virtual override {
         uint256 eta = proposalEta(proposalId);
         require(eta > 0, "GovernorTimelockCompound: proposal not yet queued");
+        Address.sendValue(payable(_timelock), msg.value);
         for (uint256 i = 0; i < targets.length; ++i) {
-            _timelock.executeTransaction{value: i == 0 ? msg.value : 0}(targets[i], values[i], "", calldatas[i], eta);
+            _timelock.executeTransaction(targets[i], values[i], "", calldatas[i], eta);
         }
     }
 
