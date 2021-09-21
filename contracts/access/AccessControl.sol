6af4ed6169ee4165ce86457f98c0a568bd9d7491
Simon Perriard <simon.perriard@epfl.ch>
2021-09-21 10:16:14 +0200

    revokeRole spec mismatch (#2872)
    revokeRole emits {RoleRevoked} event if `account` has been revoked `role`
    
    Greetings from ChainSecurity!

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -150,7 +150,7 @@ abstract contract AccessControl is Context, IAccessControl, ERC165 {
      * purpose is to provide a mechanism for accounts to lose their privileges
      * if they are compromised (such as when a trusted device is misplaced).
      *
-     * If the calling account had been granted `role`, emits a {RoleRevoked}
+     * If the calling account had been revoked `role`, emits a {RoleRevoked}
      * event.
      *
      * Requirements:
