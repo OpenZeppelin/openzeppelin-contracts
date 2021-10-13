efafb348058dcef7d6aab76dd90edd4ab49a61b9
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-10-13 22:30:34 +0200

    improve coverage

--- contracts/mocks/GovernorMock.sol
+++ contracts/mocks/GovernorMock.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import "../governance/extensions/GovernorProposalThreshold.sol";
 import "../governance/extensions/GovernorSettings.sol";
 import "../governance/extensions/GovernorCountingSimple.sol";
 import "../governance/extensions/GovernorVotesQuorumFraction.sol";
 
-contract GovernorMock is GovernorSettings, GovernorVotesQuorumFraction, GovernorCountingSimple {
+contract GovernorMock is GovernorProposalThreshold, GovernorSettings, GovernorVotesQuorumFraction, GovernorCountingSimple {
     constructor(
         string memory name_,
         ERC20Votes token_,
@@ -41,5 +42,14 @@ contract GovernorMock is GovernorSettings, GovernorVotesQuorumFraction, Governor
 
     function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
         return super.proposalThreshold();
+    }
+
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) public virtual override(Governor, GovernorProposalThreshold) returns (uint256) {
+        return super.propose(targets, values, calldatas, description);
     }
 }
