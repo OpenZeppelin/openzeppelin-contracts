9a80138e125b829bdd3bf4b28b3c60e324b02eab
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-10-14 11:56:26 +0200

    improve doc

--- contracts/governance/extensions/GovernorProposalThreshold.sol
+++ contracts/governance/extensions/GovernorProposalThreshold.sol
@@ -8,6 +8,7 @@ import "../Governor.sol";
  * @dev Extension of {Governor} for proposal restriction to token holders with a minimum balance.
  *
  * _Available since v4.3._
+ * _Deprecated since v4.4._
  */
 abstract contract GovernorProposalThreshold is Governor {
     function propose(
