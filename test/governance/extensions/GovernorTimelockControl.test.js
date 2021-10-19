b12af48a7de4e74b4e06eed4ed5df6b382fb17ea
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-10-19 20:33:02 +0200

    Add Governor module for governance-settable parameters (#2904)
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- test/governance/extensions/GovernorTimelockControl.test.js
+++ test/governance/extensions/GovernorTimelockControl.test.js
@@ -45,6 +45,10 @@ contract('GovernorTimelockControl', function (accounts) {
     'GovernorTimelock',
   ]);
 
+  it('doesn\'t accept ether transfers', async function () {
+    await expectRevert.unspecified(web3.eth.sendTransaction({ from: voter, to: this.mock.address, value: 1 }));
+  });
+
   it('post deployment check', async function () {
     expect(await this.mock.name()).to.be.equal(name);
     expect(await this.mock.token()).to.be.equal(this.token.address);
