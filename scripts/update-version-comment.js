e4413d89b71e604fc743655185b69bf2ff8344d2
Francisco Giordano <frangio.1@gmail.com>
2021-10-19 17:56:20 -0300

    add script to update comment with package version

---
+++ scripts/update-version-comment.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const glob = require('glob');
+
+const { version } = require('../package.json');
+
+const files = glob.sync('contracts/!(mocks)/**/*.sol');
+
+for (const file of files) {
+  const current = fs.readFileSync(file, 'utf8');
+  const updated = current.replace(
+    /(\/\/ SPDX-License-Identifier:.*)$(\n\/\/ OpenZeppelin Contracts v.*$)?/m,
+    `$1\n// OpenZeppelin Contracts v${version} (${file.replace('contracts/', '')})`,
+  );
+  fs.writeFileSync(file, updated);
+}
