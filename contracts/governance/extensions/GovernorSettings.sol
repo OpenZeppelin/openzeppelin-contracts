a3ae78b6a776e62ff7ea5e8f0c28b4431e43823d
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-10-19 13:45:20 +0200

    add documentation to GovernorSettings

--- contracts/governance/extensions/GovernorSettings.sol
+++ contracts/governance/extensions/GovernorSettings.sol
@@ -18,6 +18,9 @@ abstract contract GovernorSettings is Governor {
     event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod);
     event ProposalThresholdSet(uint256 oldProposalThreshold, uint256 newProposalThreshold);
 
+    /**
+     * @dev Initialize the governance parameters.
+     */
     constructor(
         uint256 initialVotingDelay,
         uint256 initialVotingPeriod,
@@ -28,46 +31,84 @@ abstract contract GovernorSettings is Governor {
         _setProposalThreshold(initialProposalThreshold);
     }
 
+    /**
+     * @dev See {IGovernor-votingDelay}.
+     */
     function votingDelay() public view virtual override returns (uint256) {
         return _votingDelay;
     }
 
+    /**
+     * @dev See {IGovernor-votingPeriod}.
+     */
     function votingPeriod() public view virtual override returns (uint256) {
         return _votingPeriod;
     }
 
+    /**
+     * @dev See {Governor-proposalThreshold}.
+     */
     function proposalThreshold() public view virtual override returns (uint256) {
         return _proposalThreshold;
     }
 
+    /**
+     * @dev Update the voting delay. This operation can only be performed through a governance proposal.
+     *
+     * Emits a {VotingDelaySet} event.
+     */
     function setVotingDelay(uint256 newVotingDelay) public onlyGovernance {
         _setVotingDelay(newVotingDelay);
     }
 
+    /**
+     * @dev Update the voting period. This operation can only be performed through a governance proposal.
+     *
+     * Emits a {VotingPeriodSet} event.
+     */
     function setVotingPeriod(uint256 newVotingPeriod) public onlyGovernance {
         _setVotingPeriod(newVotingPeriod);
     }
 
+    /**
+     * @dev Update the proposal threshold. This operation can only be performed through a governance proposal.
+     *
+     * Emits a {ProposalThresholdSet} event.
+     */
     function setProposalThreshold(uint256 newProposalThreshold) public onlyGovernance {
         _setProposalThreshold(newProposalThreshold);
     }
 
+    /**
+     * @dev Internal setter for the the voting delay.
+     *
+     * Emits a {VotingDelaySet} event.
+     */
     function _setVotingDelay(uint256 newVotingDelay) internal virtual {
         emit VotingDelaySet(_votingDelay, newVotingDelay);
         _votingDelay = newVotingDelay;
     }
 
+    /**
+     * @dev Internal setter for the the voting period.
+     *
+     * Emits a {VotingPeriodSet} event.
+     */
     function _setVotingPeriod(uint256 newVotingPeriod) internal virtual {
         // voting period must be at least one block long
         require(newVotingPeriod > 0, "GovernorSettings: voting period too low");
-        uint256 oldVotingPeriod = _votingPeriod;
+
+        emit VotingPeriodSet(_votingPeriod, newVotingPeriod);
         _votingPeriod = newVotingPeriod;
-        emit VotingPeriodSet(oldVotingPeriod, newVotingPeriod);
     }
 
+    /**
+     * @dev Internal setter for the the proposal threshold.
+     *
+     * Emits a {ProposalThresholdSet} event.
+     */
     function _setProposalThreshold(uint256 newProposalThreshold) internal virtual {
-        uint256 oldProposalThreshold = _proposalThreshold;
+        emit ProposalThresholdSet(_proposalThreshold, newProposalThreshold);
         _proposalThreshold = newProposalThreshold;
-        emit ProposalThresholdSet(oldProposalThreshold, newProposalThreshold);
     }
 }
