1bb429a473df279880b23b4b6e66489d6a75d612
soulofmischief <hello@badsoft.co>
2021-10-19 23:13:14 -0500

    Fix typo

--- contracts/token/ERC1155/extensions/ERC1155Supply.sol
+++ contracts/token/ERC1155/extensions/ERC1155Supply.sol
@@ -24,7 +24,7 @@ abstract contract ERC1155Supply is ERC1155 {
     }
 
     /**
-     * @dev Indicates weither any token exist with a given id, or not.
+     * @dev Indicates whether any token exist with a given id, or not.
      */
     function exists(uint256 id) public view virtual returns (bool) {
         return ERC1155Supply.totalSupply(id) > 0;
