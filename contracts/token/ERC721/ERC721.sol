af71849eb223fa848413ddf0c461e2606254a4fa
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2021-10-29 22:12:07 +0530

    Update ERC721.sol

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (token/ERC721/ERC721.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0 <= 0.8.0;
 
 import "./IERC721.sol";
 import "./IERC721Receiver.sol";
