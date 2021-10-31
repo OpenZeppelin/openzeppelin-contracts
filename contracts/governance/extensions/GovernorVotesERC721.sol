ec2c44e104a75e25f98ffa32a96783691a430973
JulissaDantes <julissa@singularsound.com>
2021-10-30 20:49:29 -0400

    Initial contracts creation

--- contracts/governance/extensions/GovernorVotesERC721.sol
+++ contracts/governance/extensions/GovernorVotesERC721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.3.2 (governance/extensions/ERC721GovernorVotesERC721.sol)
+// OpenZeppelin Contracts v4.3.2 (governance/extensions/ERC72GovernorVotesERC721.sol)
 
 pragma solidity ^0.8.0;
 
