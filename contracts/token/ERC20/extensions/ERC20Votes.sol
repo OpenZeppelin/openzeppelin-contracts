897938f63d7168fab84781f1ec19a4757d4ad31f
JulissaDantes <julissa@singularsound.com>
2021-11-01 16:24:20 -0400

    Adding  _afterTokenTransfer to base ERC271 contract

--- contracts/token/ERC20/extensions/ERC20Votes.sol
+++ contracts/token/ERC20/extensions/ERC20Votes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.0 (token/ERC20/extensions/ERC20Votes.sol)
+// OpenZeppelin Contracts v4.3.2 (token/ERC20/extensions/ERC20Votes.sol)
 
 pragma solidity ^0.8.0;
 
@@ -7,6 +7,7 @@ import "./draft-ERC20Permit.sol";
 import "../../../utils/math/Math.sol";
 import "../../../utils/math/SafeCast.sol";
 import "../../../utils/cryptography/ECDSA.sol";
+
 /**
  * @dev Extension of ERC20 to support Compound-like voting and delegation. This version is more generic than Compound's,
  * and supports token supply up to 2^224^ - 1, while COMP is limited to 2^96^ - 1.
@@ -173,7 +174,7 @@ abstract contract ERC20Votes is ERC20Permit {
         super._mint(account, amount);
         require(totalSupply() <= _maxSupply(), "ERC20Votes: total supply risks overflowing votes");
 
-        _writeCheckpoint(_totalSupplyCheckpoints, _add, 1);
+        _writeCheckpoint(_totalSupplyCheckpoints, _add, amount);
     }
 
     /**
