364da56ab4755d895a2595c76b9372d1996be4ec
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2021-11-04 15:03:28 +0200

    quotes on var in msg

--- certora/scripts/Governor.sh
+++ certora/scripts/Governor.sh
@@ -2,8 +2,8 @@ certoraRun certora/harnesses/GovernorHarness.sol \
     --verify GovernorHarness:certora/specs/GovernorBase.spec \
     --solc solc8.0 \
     --staging \
-    --msg $1 \
-    --disableLocalTypeChecking \
     --optimistic_loop \
-    --settings -copyLoopUnroll=4
-    --rule sanityCheckVoteStart
+    --disableLocalTypeChecking \
+    --settings -copyLoopUnroll=4 \
+    --rule voteStartBeforeVoteEnd \
+    --msg "$1"
