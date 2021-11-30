de594921cc88dfde77f0743d8bd807ccbb8911a8
Michael M <91594326+MichaelMorami@users.noreply.github.com>
2021-11-30 18:10:20 +0200

    fix script

--- certora/scripts/WizardControlFirstPriority.sh
+++ certora/scripts/WizardControlFirstPriority.sh
@@ -1,10 +1,9 @@
 certoraRun certora/harnesses/ERC20VotesHarness.sol certora/harnesses/WizardControlFirstPriority.sol \
     --link WizardControlFirstPriority:token=ERC20VotesHarness \
-    --verify WizardFirstPriority:certora/specs/GovernorBase.spec \
+    --verify WizardControlFirstPriority:certora/specs/GovernorBase.spec \
     --solc solc8.2 \
-    --disableLocalTypeChecking \
     --staging shelly/forSasha \
     --optimistic_loop \
     --settings -copyLoopUnroll=4 \
-    --rule executedImplyStartAndEndDateNonZero \
+    --rule canVoteDuringVotingPeriod \
     --msg "$1"
