74b01f668bdb1824da584075e12ae0e71b802880
Dan Liu <liudaniel@qq.com>
2021-12-04 09:11:11 +0800

    Improve the calculation speed of function ceilDiv in library Math

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -38,7 +38,7 @@ library Math {
      */
     function ceilDiv(uint256 a, uint256 b) internal pure returns (uint256) {
         // (a + b - 1) / b can overflow on addition, so we distribute.
-        return a / b + (a % b == 0 ? 0 : 1);
+        return (a > 0) ? ((a - 1) / b + 1): 0;
     }
 
     /**
