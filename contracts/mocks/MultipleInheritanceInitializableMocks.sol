9eb1a842bb04b25564e96ccb8822084f2eec83f9
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-12-07 18:32:12 +0100

    fix lint

--- contracts/mocks/MultipleInheritanceInitializableMocks.sol
+++ contracts/mocks/MultipleInheritanceInitializableMocks.sol
@@ -25,11 +25,13 @@ contract SampleHuman is Initializable {
         __SampleHuman_init();
     }
 
-    function __SampleHuman_init() internal onlyInitializing() {
+    // solhint-disable-next-line func-name-mixedcase
+    function __SampleHuman_init() internal onlyInitializing {
         __SampleHuman_init_unchained();
     }
 
-    function __SampleHuman_init_unchained() internal onlyInitializing() {
+    // solhint-disable-next-line func-name-mixedcase
+    function __SampleHuman_init_unchained() internal onlyInitializing {
         isHuman = true;
     }
 }
@@ -44,11 +46,13 @@ contract SampleMother is Initializable, SampleHuman {
         __SampleMother_init(value);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleMother_init(uint256 value) internal onlyInitializing {
         __SampleHuman_init();
         __SampleMother_init_unchained(value);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleMother_init_unchained(uint256 value) internal onlyInitializing {
         mother = value;
     }
@@ -64,11 +68,13 @@ contract SampleGramps is Initializable, SampleHuman {
         __SampleGramps_init(value);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleGramps_init(string memory value) internal onlyInitializing {
         __SampleHuman_init();
         __SampleGramps_init_unchained(value);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleGramps_init_unchained(string memory value) internal onlyInitializing {
         gramps = value;
     }
@@ -84,11 +90,13 @@ contract SampleFather is Initializable, SampleGramps {
         __SampleFather_init(_gramps, _father);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleFather_init(string memory _gramps, uint256 _father) internal onlyInitializing {
         __SampleGramps_init(_gramps);
         __SampleFather_init_unchained(_father);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleFather_init_unchained(uint256 _father) internal onlyInitializing {
         father = _father;
     }
@@ -109,6 +117,7 @@ contract SampleChild is Initializable, SampleMother, SampleFather {
         __SampleChild_init(_mother, _gramps, _father, _child);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleChild_init(
         uint256 _mother,
         string memory _gramps,
@@ -120,6 +129,7 @@ contract SampleChild is Initializable, SampleMother, SampleFather {
         __SampleChild_init_unchained(_child);
     }
 
+    // solhint-disable-next-line func-name-mixedcase
     function __SampleChild_init_unchained(uint256 _child) internal onlyInitializing {
         child = _child;
     }
