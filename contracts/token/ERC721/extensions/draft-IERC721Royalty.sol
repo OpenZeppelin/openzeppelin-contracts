f4378c548c040e81436e4fc7058f4c8e2ce532d0
JulissaDantes <julissa@openzeppelin.com>
2021-12-13 19:14:42 -0400

    Update contracts/token/ERC721/extensions/draft-IERC721Royalty.sol
    Co-authored-by: Francisco Giordano <frangio.1@gmail.com>

--- contracts/token/ERC721/extensions/draft-IERC721Royalty.sol
+++ contracts/token/ERC721/extensions/draft-IERC721Royalty.sol
@@ -10,6 +10,7 @@ import "../../../interfaces/IERC165.sol";
  *
  * A standardized way to retrieve royalty payment information for non-fungible tokens (NFTs) to enable universal
  * support for royalty payments across all NFT marketplaces and ecosystem participants.
+ *
  * _Available since v4.5._
  */
 interface IERC721Royalty is IERC165 {
