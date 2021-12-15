1d58f7f5e5d7c195dd955d1a3038d96d50e0c44e
Anton Bukov <k06aaa@gmail.com>
2021-12-15 11:24:20 +0100

    Replace excodesize assembly with address.code.length (https://github.com/ethereum/solidity/releases/tag/v0.8.1)

--- contracts/utils/Address.sol
+++ contracts/utils/Address.sol
@@ -25,15 +25,11 @@ library Address {
      * ====
      */
     function isContract(address account) internal view returns (bool) {
-        // This method relies on extcodesize, which returns 0 for contracts in
-        // construction, since the code is only stored at the end of the
-        // constructor execution.
+        // This method relies on extcodesize/address.code.length, which returns 0
+        // for contracts in construction, since the code is only stored at the end
+        // of the constructor execution.
 
-        uint256 size;
-        assembly {
-            size := extcodesize(account)
-        }
-        return size > 0;
+        return account.code.length > 0;
     }
 
     /**
