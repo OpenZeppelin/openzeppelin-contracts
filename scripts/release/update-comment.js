42f69b1007582eafb4b2a345f5ad1d2cf963ee64
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-12-16 19:15:17 +0100

    Only update version number for file that changed since the last tag

--- scripts/release/update-comment.js
+++ scripts/release/update-comment.js
@@ -1,27 +1,49 @@
-#!/usr/bin/env node
-
 const fs = require('fs');
-const glob = require('glob');
-const proc = require('child_process');
-
-const gitStatus = proc.execFileSync('git', ['status', '--porcelain', '-uno', 'contracts/**/*.sol']);
-
-if (gitStatus.length > 0) {
-  console.error('Contracts directory is not clean');
-  process.exit(1);
-}
+const git = require('gift');
+const semver = require('semver');
 
 const { version } = require('../../package.json');
 
-const files = glob.sync('contracts/!(mocks)/**/*.sol');
-
-for (const file of files) {
-  const current = fs.readFileSync(file, 'utf8');
-  const updated = current.replace(
-    /(\/\/ SPDX-License-Identifier:.*)$(\n\/\/ OpenZeppelin Contracts v.*$)?/m,
-    `$1\n// OpenZeppelin Contracts v${version} (${file.replace('contracts/', '')})`,
-  );
-  fs.writeFileSync(file, updated);
+function check(error, message) {
+    if (error) {
+        console.error(message ?? error);
+        process.exit(1);
+    }
 }
 
-proc.execFileSync('git', ['add', '--update', 'contracts']);
+const repo = git('.');
+
+repo.git('status --porcelain -uno contracts/**/*.sol', (error, status) => {
+    check(error);
+    check(!!status, 'Contracts directory is not clean');
+
+    repo.tags((error, tags) => {
+        check(error);
+
+        const [ tag ] = tags
+            .map(({ name }) => name)
+            .filter(semver.valid)
+            .sort(semver.rcompare);
+
+        repo.diff('master', tag, (error, diffs) => {
+            check(error);
+
+            diffs
+                .filter(({ b_path: path }) => path.startsWith('contracts/'))
+                .filter(({ b_path: path }) => !path.startsWith('contracts/mocks'))
+                .filter(({ b_path: path }) => path.endsWith('.sol'))
+                .forEach(({ b_path: path }) => {
+                    const current = fs.readFileSync(path, 'utf8');
+                    const updated = current.replace(
+                        /(\/\/ SPDX-License-Identifier:.*)$(\n\/\/ OpenZeppelin Contracts v.*$)?/m,
+                        `$1\n// Last updated in OpenZeppelin Contracts v${version} (${path.replace('contracts/', '')})`,
+                    );
+                    fs.writeFileSync(path, updated);
+                });
+
+            repo.git('add --update contracts', (error) => {
+                check(error);
+            });
+        });
+    });
+});
