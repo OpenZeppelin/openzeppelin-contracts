da29846526297a176c0e9e15c9e0d5eeeb25b496
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-12-17 19:20:56 +0100

    address comments in PR

--- scripts/release/update-comment.js
+++ scripts/release/update-comment.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const fs = require('fs');
 const proc = require('child_process');
 const semver = require('semver');
@@ -18,7 +19,6 @@ const [ tag ] = run('git', 'tag')
   .sort(semver.rcompare);
 
 // Ordering tag → HEAD is important here.
-// Is it right to use HEAD ?
 const files = run('git', 'diff', tag, 'HEAD', '--name-only', 'contracts/**/*.sol')
   .split(/\r?\n/)
   .filter(file => !file.match(/mock/i));
@@ -27,7 +27,7 @@ for (const file of files) {
   const current = fs.readFileSync(file, 'utf8');
   const updated = current.replace(
     /(\/\/ SPDX-License-Identifier:.*)$(\n\/\/ OpenZeppelin Contracts v.*$)?/m,
-    `$1\n// Last updated in OpenZeppelin Contracts v${version} (${file.replace('contracts/', '')})`,
+    `$1\n// OpenZeppelin Contracts (last updated v${version}) (${file.replace('contracts/', '')})`,
   );
   fs.writeFileSync(file, updated);
 }
