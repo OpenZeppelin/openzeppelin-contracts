c1060ca493d30676973a83eadbff698accdc7ed3
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-12-21 15:34:43 +0100

    use mstore8 to improve memory accesses

--- contracts/utils/Base64.sol
+++ contracts/utils/Base64.sol
@@ -30,28 +30,19 @@ library Base64 {
         // - `data.length + 2`  -> Round up
         // - `/ 3`              -> Number of 3-bytes chunks
         // - `4 *`              -> 4 characters for each chunk
-        uint256 encodedLen = 4 * ((data.length + 2) / 3);
-
-        // Add some extra buffer at the end required for the writing
-        string memory result = new string(encodedLen + 32);
+        string memory result = new string(4 * ((data.length + 2) / 3));
 
         assembly {
-            // Store the actual result length in memory
-            mstore(result, encodedLen)
-
-            // Prepare the lookup table
+            // Prepare the lookup table (skip the first "length" byte)
             let tablePtr := add(table, 1)
-
-            // Prepare input pointer
-            let dataPtr := data
-            let endPtr := add(dataPtr, mload(data))
 
             // Prepare result pointer, jump over length
             let resultPtr := add(result, 32)
 
             // Run over the input, 3 bytes at a time
             for {
-
+                let dataPtr := data
+                let endPtr := add(data, mload(data))
             } lt(dataPtr, endPtr) {
 
             } {
@@ -67,16 +58,16 @@ library Base64 {
                 // and finally write it in the result pointer but with a left shift
                 // of 256 (1 byte) - 8 (1 ASCII char) = 248 bits
 
-                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
                 resultPtr := add(resultPtr, 1) // Advance
 
-                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
                 resultPtr := add(resultPtr, 1) // Advance
 
-                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(6, input), 0x3F)))))
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(6, input), 0x3F))))
                 resultPtr := add(resultPtr, 1) // Advance
 
-                mstore(resultPtr, shl(248, mload(add(tablePtr, and(input, 0x3F)))))
+                mstore8(resultPtr, mload(add(tablePtr, and(input, 0x3F))))
                 resultPtr := add(resultPtr, 1) // Advance
             }
 
@@ -84,10 +75,11 @@ library Base64 {
             // it is padded with `=` characters at the end
             switch mod(mload(data), 3)
             case 1 {
-                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+                mstore8(sub(resultPtr, 1), 0x3d)
+                mstore8(sub(resultPtr, 2), 0x3d)
             }
             case 2 {
-                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+                mstore8(sub(resultPtr, 1), 0x3d)
             }
         }
 
