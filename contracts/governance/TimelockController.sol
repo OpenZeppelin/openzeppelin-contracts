95d3653a5794efdcf688fd7c5eac4ebf451d990c
JulissaDantes <julissa@singularsound.com>
2022-01-05 10:25:40 -0400

    Add comment for TimelockController execute function

--- contracts/governance/TimelockController.sol
+++ contracts/governance/TimelockController.sol
@@ -261,6 +261,9 @@ contract TimelockController is AccessControl {
      *
      * - the caller must have the 'executor' role.
      */
+    //Reentrancy here is not a risk because the _beforeCall and _afterCall functions acts as guards to avoid
+    // _call to execute a proposal in the wrong status.
+    //slither-disable-next-line reentrancy-eth
     function execute(
         address target,
         uint256 value,
