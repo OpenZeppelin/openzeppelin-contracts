c39457e411816f112a49d61bfbd3abdf4d3ed9e0
Francisco Giordano <frangio.1@gmail.com>
2022-01-10 19:57:38 -0300

    add docs to IERC1822Proxiable

--- contracts/interfaces/draft-IERC1822.sol
+++ contracts/interfaces/draft-IERC1822.sol
@@ -3,6 +3,15 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @dev ERC1822: Universal Upgradeable Proxy Standard (UUPS) documents a method for upgradeability through a simplified
+ * proxy whose upgrades are fully controlled by the current implementation.
+ */
 interface IERC1822Proxiable {
+    /**
+     * @dev Returns the storage slot that the proxiable contract assumes is being used to store the implementation
+     * address. Must revert if invoked through delegatecall, i.e. through a proxy, otherwise risks bricking a contract
+     * that upgrades to it.
+     */
     function proxiableUUID() external view returns (bytes32);
 }
