828d7847cf5dea0bd09a7849de830d40d18b9f50
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-01-10 14:20:15 +0100

    minor wording change

--- contracts/proxy/ERC1967/ERC1967Upgrade.sol
+++ contracts/proxy/ERC1967/ERC1967Upgrade.sol
@@ -83,8 +83,8 @@ abstract contract ERC1967Upgrade {
         bytes memory data,
         bool forceCall
     ) internal {
-        // Upgrades from old implementations will perform a rollback test. This test requires the
-        // new implementation to upgrade to the old, non-ERC1822 compliant, implementation. Removing
+        // Upgrades from old implementations will perform a rollback test. This test requires the new
+        // implementation to upgrade back to the old, non-ERC1822 compliant, implementation. Removing
         // this special case will break upgrade paths from old UUPS implementation to new ones.
         if (StorageSlot.getBooleanSlot(_ROLLBACK_SLOT).value) {
             _setImplementation(newImplementation);
