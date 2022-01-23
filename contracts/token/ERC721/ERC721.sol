989ca5c4a65e8f9740f381ba3f528c5dc7f23e3f
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2022-01-23 10:18:16 +0500

    Update ERC721.sol

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -100,7 +100,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     /**
      * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
      * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
-     * by default, can be overriden in child contracts.
+     * by default, can be overridden in child contracts.
      */
     function _baseURI() internal view virtual returns (string memory) {
         return "";
