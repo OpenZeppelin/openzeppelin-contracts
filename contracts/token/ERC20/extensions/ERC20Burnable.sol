3df8fc3f97e117dfcac707284e91383d44d3e413
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-02-07 16:20:28 +0100

    add support for unlimited allowance to ERC20Burnable

--- contracts/token/ERC20/extensions/ERC20Burnable.sol
+++ contracts/token/ERC20/extensions/ERC20Burnable.sol
@@ -34,9 +34,11 @@ abstract contract ERC20Burnable is Context, ERC20 {
      */
     function burnFrom(address account, uint256 amount) public virtual {
         uint256 currentAllowance = allowance(account, _msgSender());
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-        unchecked {
-            _approve(account, _msgSender(), currentAllowance - amount);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+            unchecked {
+                _approve(account, _msgSender(), currentAllowance - amount);
+            }
         }
         _burn(account, amount);
     }
