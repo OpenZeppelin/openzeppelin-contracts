b7fb0960e48fbfd03b79cf5e23b4ec33432561fe
Francisco Giordano <frangio.1@gmail.com>
2022-02-11 18:54:06 -0300

    refactor tests and expand coverage

--- test/utils/structs/DoubleEndedQueue.test.js
+++ test/utils/structs/DoubleEndedQueue.test.js
@@ -3,9 +3,7 @@ const { expect } = require('chai');
 
 const Bytes32DequeMock = artifacts.require('Bytes32DequeMock');
 
-/**
- * Rebuild the content of the deque as a JS array.
- */
+/** Rebuild the content of the deque as a JS array. */
 async function getContent (deque) {
   const length = await deque.length().then(bn => bn.toNumber());
   const values = await Promise.all(Array(length).fill().map((_, i) => deque.at(i)));
@@ -13,79 +11,86 @@ async function getContent (deque) {
 }
 
 contract('DoubleEndedQueue', function (accounts) {
-  // Bytes32Vector
-  describe('EnumerableVector', function () {
-    const bytesA = '0xdeadbeef'.padEnd(66, '0');
-    const bytesB = '0x0123456789'.padEnd(66, '0');
-    const bytesC = '0x42424242'.padEnd(66, '0');
-    const bytesD = '0x171717'.padEnd(66, '0');
+  const bytesA = '0xdeadbeef'.padEnd(66, '0');
+  const bytesB = '0x0123456789'.padEnd(66, '0');
+  const bytesC = '0x42424242'.padEnd(66, '0');
+  const bytesD = '0x171717'.padEnd(66, '0');
 
-    beforeEach(async function () {
-      this.deque = await Bytes32DequeMock.new();
-    });
+  beforeEach(async function () {
+    this.deque = await Bytes32DequeMock.new();
+  });
 
-    it('empty deque', async function () {
+  describe('when empty', function () {
+    it('getters', async function () {
       expect(await this.deque.empty()).to.be.equal(true);
       expect(await getContent(this.deque)).to.have.ordered.members([]);
     });
 
-    describe('with content', function () {
-      beforeEach(async function () {
-        await this.deque.pushBack(bytesB);
-        await this.deque.pushFront(bytesA);
-        await this.deque.pushBack(bytesC);
-        this.content = [ bytesA, bytesB, bytesC ];
-      });
+    it('reverts on accesses', async function () {
+      await expectRevert.unspecified(this.deque.popBack());
+      await expectRevert.unspecified(this.deque.popFront());
+      await expectRevert.unspecified(this.deque.back());
+      await expectRevert.unspecified(this.deque.front());
+    });
+  });
 
-      it('content is valid', async function () {
-        expect(await this.deque.empty()).to.be.equal(false);
-        expect(await this.deque.front()).to.be.equal(bytesA);
-        expect(await this.deque.back()).to.be.equal(bytesC);
+  describe('when not empty', function () {
+    beforeEach(async function () {
+      await this.deque.pushBack(bytesB);
+      await this.deque.pushFront(bytesA);
+      await this.deque.pushBack(bytesC);
+      this.content = [ bytesA, bytesB, bytesC ];
+    });
+
+    it('getters', async function () {
+      expect(await this.deque.empty()).to.be.equal(false);
+      expect(await this.deque.length()).to.be.bignumber.equal(this.content.length.toString());
+      expect(await this.deque.front()).to.be.equal(this.content[0]);
+      expect(await this.deque.back()).to.be.equal(this.content[this.content.length - 1]);
+      expect(await getContent(this.deque)).to.have.ordered.members(this.content);
+    });
+
+    it('out of bounds access', async function () {
+      await expectRevert.unspecified(this.deque.at(this.content.length));
+    });
+
+    describe('push', function () {
+      it('front', async function () {
+        await this.deque.pushFront(bytesD);
+        this.content.unshift(bytesD); // add element at the begining
+
         expect(await getContent(this.deque)).to.have.ordered.members(this.content);
       });
 
-      it('out of bound access', async function () {
-        await expectRevert.unspecified(this.deque.at(10));
+      it('back', async function () {
+        await this.deque.pushBack(bytesD);
+        this.content.push(bytesD); // add element at the end
+
+        expect(await getContent(this.deque)).to.have.ordered.members(this.content);
+      });
+    });
+
+    describe('pop', function () {
+      it('front', async function () {
+        const value = this.content.shift(); // remove first element
+        expectEvent(await this.deque.popFront(), 'OperationResult', { value });
+
+        expect(await getContent(this.deque)).to.have.ordered.members(this.content);
       });
 
-      describe('push', function () {
-        it('front', async function () {
-          await this.deque.pushFront(bytesD);
-          this.content.unshift(bytesD); // add element at the begining
+      it('back', async function () {
+        const value = this.content.pop(); // remove last element
+        expectEvent(await this.deque.popBack(), 'OperationResult', { value });
 
-          expect(await getContent(this.deque)).to.have.ordered.members(this.content);
-        });
-
-        it('back', async function () {
-          await this.deque.pushBack(bytesD);
-          this.content.push(bytesD); // add element at the end
-
-          expect(await getContent(this.deque)).to.have.ordered.members(this.content);
-        });
+        expect(await getContent(this.deque)).to.have.ordered.members(this.content);
       });
+    });
 
-      describe('pop', function () {
-        it('front', async function () {
-          expectEvent(await this.deque.popFront(), 'OperationResult', { value: bytesA });
-          this.content.shift(); // remove first element
+    it('clear', async function () {
+      await this.deque.clear();
 
-          expect(await getContent(this.deque)).to.have.ordered.members(this.content);
-        });
-
-        it('back', async function () {
-          expectEvent(await this.deque.popBack(), 'OperationResult', { value: bytesC });
-          this.content.pop(); // remove last element
-
-          expect(await getContent(this.deque)).to.have.ordered.members(this.content);
-        });
-      });
-
-      it('clear', async function () {
-        await this.deque.clear();
-
-        expect(await this.deque.empty()).to.be.equal(true);
-        expect(await getContent(this.deque)).to.have.ordered.members([]);
-      });
+      expect(await this.deque.empty()).to.be.equal(true);
+      expect(await getContent(this.deque)).to.have.ordered.members([]);
     });
   });
 });
