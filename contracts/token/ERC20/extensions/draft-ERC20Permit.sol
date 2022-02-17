fa5db25d7d06a606bb1817bdd94c9dea47b7de4e
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2022-02-17 10:36:29 +0100

    replace `immutable` with `constant` for _PERMIT_TYPEHASH
    This commit is related to the following issue discussion: https://github.com/OpenZeppelin/contracts-wizard/issues/89#issuecomment-1042391318
    
    Since Solidity version `0.6.12` the `keccak256` of string literals is treated specially and the hash is evaluated at compile time. Since the OpenZeppelin Wizard also uses `constant` for OpenZeppelin's AccessControl's roles declarations, it's good practice to make this consistent.

--- contracts/token/ERC20/extensions/draft-ERC20Permit.sol
+++ contracts/token/ERC20/extensions/draft-ERC20Permit.sol
@@ -25,7 +25,7 @@ abstract contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
     mapping(address => Counters.Counter) private _nonces;
 
     // solhint-disable-next-line var-name-mixedcase
-    bytes32 private immutable _PERMIT_TYPEHASH =
+    bytes32 private constant _PERMIT_TYPEHASH =
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     /**
