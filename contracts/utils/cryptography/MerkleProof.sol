259651c5dfc2d027b6da0a870cb1ea94ff03a4f2
Troy Salem <troy.salem.work@gmail.com>
2022-02-18 17:24:04 -0500

    Update for backwards compatibility

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.5.0) (utils/cryptography/MerkleProof.sol)
 
@@ -25,11 +26,28 @@ library MerkleProof {
      * pair of leaves and each pair of pre-images are assumed to be sorted.
      */
     function verify(
-        bytes32[] calldata proof,
+        bytes32[] memory proof,
         bytes32 root,
         bytes32 leaf
     ) internal pure returns (bool) {
         return processProof(proof, leaf) == root;
+    }
+
+    /**
+     * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
+     * defined by `root`. For this, a `proof` must be provided, containing
+     * sibling hashes on the branch from the leaf to the root of the tree. Each
+     * pair of leaves and each pair of pre-images are assumed to be sorted.
+     *
+     * Updated to use calldata
+     *
+     */
+    function verifyCall(
+        bytes32[] calldata proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
+        return processProofCall(proof, leaf) == root;
     }
 
     /**
@@ -40,7 +58,32 @@ library MerkleProof {
      *
      * _Available since v4.4._
      */
-    function processProof(bytes32[] calldata proof, bytes32 leaf) internal pure returns (bytes32) {
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+            if (computedHash <= proofElement) {
+                // Hash(current computed hash + current element of the proof)
+                computedHash = _efficientHash(computedHash, proofElement);
+            } else {
+                // Hash(current element of the proof + current computed hash)
+                computedHash = _efficientHash(proofElement, computedHash);
+            }
+        }
+        return computedHash;
+    }
+
+    /**
+     * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
+     * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
+     * hash matches the root of the tree. When processing the proof, the pairs
+     * of leafs & pre-images are assumed to be sorted.
+     *
+     * Updated to use calldata
+     *
+     * _Available since v4.4._
+     */
+    function processProofCall(bytes32[] calldata proof, bytes32 leaf) internal pure returns (bytes32) {
         bytes32 computedHash = leaf;
         for (uint256 i = 0; i < proof.length; i++) {
             bytes32 proofElement = proof[i];
