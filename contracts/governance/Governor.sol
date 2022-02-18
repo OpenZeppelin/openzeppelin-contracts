ee1a34baa38d64c026ed9cdd63cd2d79e41c6de9
Francisco Giordano <frangio.1@gmail.com>
2022-02-18 15:21:44 -0300

    write more extended docs for onlyGovernance

--- contracts/governance/Governor.sol
+++ contracts/governance/Governor.sol
@@ -49,8 +49,14 @@ abstract contract Governor is Context, ERC165, EIP712, IGovernor {
     DoubleEndedQueue.Bytes32Deque private _governanceCall;
 
     /**
-     * @dev Restrict access to governor executing address. Some module might override the _executor function to make
-     * sure this modifier is consistant with the execution model.
+     * @dev Restricts a function so it can only be executed through governance proposals. For example, governance
+     * parameter setters in {GovernorSettings} are protected using this modifier.
+     *
+     * The governance executing address may be different from the Governor's own address, for example it could be a
+     * timelock. This can be customized by modules by overriding {_executor}. The executor is only able to invoke these
+     * functions during the execution of the governor's {execute} function, and not under any other circumstances. Thus,
+     * for example, additional timelock proposers are not able to change governance parameters without going through the
+     * governance protocol (since v4.6).
      */
     modifier onlyGovernance() {
         require(_msgSender() == _executor(), "Governor: onlyGovernance");
