80ec9e27369b53355c064e7085821bc929221a86
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-02-22 22:29:16 +0100

    rename params for clarity

--- contracts/crosschain/arbitrum/CrossChainEnabledArbitrumL1.sol
+++ contracts/crosschain/arbitrum/CrossChainEnabledArbitrumL1.sol
@@ -5,17 +5,17 @@ import "../CrossChainEnabled.sol";
 import "./LibArbitrumL1.sol";
 
 abstract contract CrossChainEnabledArbitrumL1 is CrossChainEnabled {
-    address private immutable _bridge;
+    address private immutable _inbox;
 
-    constructor(address bridge) {
-        _bridge = bridge;
+    constructor(address inbox) {
+        _inbox = inbox;
     }
 
     function _isCrossChain() internal view virtual override returns (bool) {
-        return LibArbitrumL1.isCrossChain(_bridge);
+        return LibArbitrumL1.isCrossChain(_inbox);
     }
 
     function _crossChainSender() internal view virtual override onlyCrossChain returns (address) {
-        return LibArbitrumL1.crossChainSender(_bridge);
+        return LibArbitrumL1.crossChainSender(_inbox);
     }
 }
