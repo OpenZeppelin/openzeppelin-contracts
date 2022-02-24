94e75d8979466ee5759bcc8e37f0c736e74fca2e
jayceftg <jayceftg@gmail.com>
2022-02-24 10:01:22 +0800

    Update ECDSA.sol
    To make the function more intuitive to some readers

--- contracts/utils/cryptography/ECDSA.sol
+++ contracts/utils/cryptography/ECDSA.sol
@@ -22,7 +22,7 @@ library ECDSA {
 
     function _throwError(RecoverError error) private pure {
         if (error == RecoverError.NoError) {
-            return; // no error: do nothing
+           // no error: do nothing
         } else if (error == RecoverError.InvalidSignature) {
             revert("ECDSA: invalid signature");
         } else if (error == RecoverError.InvalidSignatureLength) {
