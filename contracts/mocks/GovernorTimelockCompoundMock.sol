f796e23f5e57f66b5ccebd43c03771be9efdad0b
anant asthana <anant.asty@gmail.com>
2022-02-27 07:27:54 +0530

    Update GovernorTimelockCompoundMock.sol

--- contracts/mocks/GovernorTimelockCompoundMock.sol
+++ contracts/mocks/GovernorTimelockCompoundMock.sol
@@ -16,8 +16,8 @@ contract GovernorTimelockCompoundMock is
     constructor(
         string memory name_,
         IVotes token_,
-        uint256 votingDelay_,
-        uint256 votingPeriod_,
+        uint64 votingDelay_,
+        uint64 votingPeriod_,
         ICompoundTimelock timelock_,
         uint256 quorumNumerator_
     )
