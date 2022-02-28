d12157466804e694254f628601290aad2ad68ed3
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-02-28 11:52:36 +0100

    use _spendAllowance in ERC20FlashMint

--- contracts/token/ERC20/extensions/ERC20FlashMint.sol
+++ contracts/token/ERC20/extensions/ERC20FlashMint.sol
@@ -72,9 +72,7 @@ abstract contract ERC20FlashMint is ERC20, IERC3156FlashLender {
             receiver.onFlashLoan(msg.sender, token, amount, fee, data) == _RETURN_VALUE,
             "ERC20FlashMint: invalid return value"
         );
-        uint256 currentAllowance = allowance(address(receiver), address(this));
-        require(currentAllowance >= amount + fee, "ERC20FlashMint: allowance does not allow refund");
-        _approve(address(receiver), address(this), currentAllowance - amount - fee);
+        _spendAllowance(address(receiver), address(this), amount + fee);
         _burn(address(receiver), amount + fee);
         return true;
     }
