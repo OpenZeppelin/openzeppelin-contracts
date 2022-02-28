e47f8605a46b8d30c503b18201a78cee4422e383
CJ42 <cavallerajean@gmail.com>
2022-02-28 20:25:00 +0000

    docs: add natspec docs for IERC777 events definitions

--- contracts/token/ERC777/IERC777.sol
+++ contracts/token/ERC777/IERC777.sol
@@ -14,12 +14,28 @@ pragma solidity ^0.8.0;
  */
 interface IERC777 {
     
+    /**
+     * @dev Emitted when `amount` tokens are created by `operator` and assigned to `to`.
+     *
+     * Note that some additional user `data` and `operatorData` can be logged in the event.
+     */
     event Minted(address indexed operator, address indexed to, uint256 amount, bytes data, bytes operatorData);
 
+    /**
+     * @dev Emitted when `operator` destroys `amount` tokens from `account`.
+     *
+     * Note that some additional user `data` and `operatorData` can be logged in the event.
+     */
     event Burned(address indexed operator, address indexed from, uint256 amount, bytes data, bytes operatorData);
 
+    /**
+     * @dev Emitted when `operator` is made operator for `tokenHolder`
+     */    
     event AuthorizedOperator(address indexed operator, address indexed tokenHolder);
 
+    /**
+     * @dev Emitted when `operator` is revoked its operator status for `tokenHolder`
+     */
     event RevokedOperator(address indexed operator, address indexed tokenHolder);
 
     /**
