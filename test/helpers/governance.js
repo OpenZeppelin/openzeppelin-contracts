1aaa4e7144920f6ab794fc25462297a197d36f6a
Francisco Giordano <frangio.1@gmail.com>
2022-03-09 23:46:51 -0300

    remove unused proposal argument

--- test/helpers/governance.js
+++ test/helpers/governance.js
@@ -31,149 +31,147 @@ class GovernorHelper {
     ]);
   }
 
-  propose (opts = null, proposal = undefined) {
-    const details = this.setProposal(proposal);
+  propose (opts = null) {
+    const proposal = this.currentProposal;
 
     return this.governor.methods[
-      details.useCompatibilityInterface
+      proposal.useCompatibilityInterface
         ? 'propose(address[],uint256[],string[],bytes[],string)'
         : 'propose(address[],uint256[],bytes[],string)'
-    ](...concatOpts(details.proposal, opts));
+    ](...concatOpts(proposal.proposal, opts));
   }
 
-  queue (opts = null, proposal = undefined) {
-    const details = this.setProposal(proposal);
+  queue (opts = null) {
+    const proposal = this.currentProposal;
 
-    return details.useCompatibilityInterface
+    return proposal.useCompatibilityInterface
       ? this.governor.methods['queue(uint256)'](...concatOpts(
-        [ details.id ],
+        [ proposal.id ],
         opts,
       ))
       : this.governor.methods['queue(address[],uint256[],bytes[],bytes32)'](...concatOpts(
-        details.shortProposal,
+        proposal.shortProposal,
         opts,
       ));
   }
 
-  execute (opts = null, proposal = undefined) {
-    const details = this.setProposal(proposal);
+  execute (opts = null) {
+    const proposal = this.currentProposal;
 
-    return details.useCompatibilityInterface
+    return proposal.useCompatibilityInterface
       ? this.governor.methods['execute(uint256)'](...concatOpts(
-        [ details.id ],
+        [ proposal.id ],
         opts,
       ))
       : this.governor.methods['execute(address[],uint256[],bytes[],bytes32)'](...concatOpts(
-        details.shortProposal,
+        proposal.shortProposal,
         opts,
       ));
   }
 
-  cancel (opts = null, proposal = undefined) {
-    const details = this.setProposal(proposal);
+  cancel (opts = null) {
+    const proposal = this.currentProposal;
 
-    return details.useCompatibilityInterface
+    return proposal.useCompatibilityInterface
       ? this.governor.methods['cancel(uint256)'](...concatOpts(
-        [ details.id ],
+        [ proposal.id ],
         opts,
       ))
       : this.governor.methods['cancel(address[],uint256[],bytes[],bytes32)'](...concatOpts(
-        details.shortProposal,
+        proposal.shortProposal,
         opts,
       ));
   }
 
-  vote (vote = {}, opts = null, proposal = undefined) {
-    const details = this.setProposal(proposal);
+  vote (vote = {}, opts = null) {
+    const proposal = this.currentProposal;
 
     return vote.signature
       // if signature, and either params or reason →
       ? vote.params || vote.reason
         ? vote.signature({
-          proposalId: details.id,
+          proposalId: proposal.id,
           support: vote.support,
           reason: vote.reason || '',
           params: vote.params || '',
         }).then(({ v, r, s }) => this.governor.castVoteWithReasonAndParamsBySig(...concatOpts(
-          [ details.id, vote.support, vote.reason || '', vote.params || '', v, r, s ],
+          [ proposal.id, vote.support, vote.reason || '', vote.params || '', v, r, s ],
           opts,
         )))
         : vote.signature({
-          proposalId: details.id,
+          proposalId: proposal.id,
           support: vote.support,
         }).then(({ v, r, s }) => this.governor.castVoteBySig(...concatOpts(
-          [ details.id, vote.support, v, r, s ],
+          [ proposal.id, vote.support, v, r, s ],
           opts,
         )))
       : vote.params
         // otherwize if params
         ? this.governor.castVoteWithReasonAndParams(...concatOpts(
-          [ details.id, vote.support, vote.reason || '', vote.params ],
+          [ proposal.id, vote.support, vote.reason || '', vote.params ],
           opts,
         ))
         : vote.reason
           // otherwize if reason
           ? this.governor.castVoteWithReason(...concatOpts(
-            [ details.id, vote.support, vote.reason ],
+            [ proposal.id, vote.support, vote.reason ],
             opts,
           ))
           : this.governor.castVote(...concatOpts(
-            [ details.id, vote.support ],
+            [ proposal.id, vote.support ],
             opts,
           ));
   }
 
-  waitForSnapshot (offset = 0, proposal = undefined) {
-    const details = this.setProposal(proposal);
-    return this.governor.proposalSnapshot(details.id)
+  waitForSnapshot (offset = 0) {
+    const proposal = this.currentProposal;
+    return this.governor.proposalSnapshot(proposal.id)
       .then(blockNumber => time.advanceBlockTo(blockNumber.addn(offset)));
   }
 
-  waitForDeadline (offset = 0, proposal = undefined) {
-    const details = this.setProposal(proposal);
-    return this.governor.proposalDeadline(details.id)
+  waitForDeadline (offset = 0) {
+    const proposal = this.currentProposal;
+    return this.governor.proposalDeadline(proposal.id)
       .then(blockNumber => time.advanceBlockTo(blockNumber.addn(offset)));
   }
 
-  waitForEta (offset = 0, proposal = undefined) {
-    const details = this.setProposal(proposal);
-    return this.governor.proposalEta(details.id)
+  waitForEta (offset = 0) {
+    const proposal = this.currentProposal;
+    return this.governor.proposalEta(proposal.id)
       .then(timestamp => time.increaseTo(timestamp.addn(offset)));
   }
 
   setProposal (proposal) {
-    if (proposal) {
-      const useCompatibilityInterface = proposal.length === 5;
-      const description = proposal[proposal.length - 1];
-      const shortProposal = [
-        // targets
-        proposal[0],
-        // values
-        proposal[1],
-        // calldata (prefix selector if necessary)
-        useCompatibilityInterface
-          ? zip(
-            proposal[2].map(selector => selector && web3.eth.abi.encodeFunctionSignature(selector)),
-            proposal[3],
-          ).map(hexs => concatHex(...hexs))
-          : proposal[2],
-        // descriptionHash
-        web3.utils.keccak256(description),
-      ];
-      const id = web3.utils.toBN(web3.utils.keccak256(web3.eth.abi.encodeParameters(
-        [ 'address[]', 'uint256[]', 'bytes[]', 'bytes32' ],
-        shortProposal,
-      )));
+    const useCompatibilityInterface = proposal.length === 5;
+    const description = proposal[proposal.length - 1];
+    const shortProposal = [
+      // targets
+      proposal[0],
+      // values
+      proposal[1],
+      // calldata (prefix selector if necessary)
+      useCompatibilityInterface
+        ? zip(
+          proposal[2].map(selector => selector && web3.eth.abi.encodeFunctionSignature(selector)),
+          proposal[3],
+        ).map(hexs => concatHex(...hexs))
+        : proposal[2],
+      // descriptionHash
+      web3.utils.keccak256(description),
+    ];
+    const id = web3.utils.toBN(web3.utils.keccak256(web3.eth.abi.encodeParameters(
+      [ 'address[]', 'uint256[]', 'bytes[]', 'bytes32' ],
+      shortProposal,
+    )));
 
-      this.lastProposalDetails = {
-        id,
-        proposal,
-        shortProposal,
-        description,
-        useCompatibilityInterface,
-      };
-    }
-    return this.lastProposalDetails;
+    this.currentProposal = {
+      id,
+      proposal,
+      shortProposal,
+      description,
+      useCompatibilityInterface,
+    };
+    return this.currentProposal;
   }
 }
 
