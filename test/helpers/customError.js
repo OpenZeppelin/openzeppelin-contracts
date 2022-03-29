cc2dd0444ccece0656f0abdfd7d4b64b03ee9792
Francisco Giordano <frangio.1@gmail.com>
2022-03-29 20:58:40 -0300

    fix custom error tests when optimizations are on

--- test/helpers/customError.js
+++ test/helpers/customError.js
@@ -1,11 +1,20 @@
+const { config } = require('hardhat');
+
+const optimizationsEnabled = config.solidity.compilers.some(c => c.settings.optimizer.enabled);
+
 /** Revert handler that supports custom errors. */
-async function expectRevertCustomError(promise, reason) {
+async function expectRevertCustomError (promise, reason) {
   try {
     await promise;
     expect.fail('Expected promise to throw but it didn\'t');
   } catch (revert) {
     if (reason) {
-      expect(revert.message).to.include(reason);
+      if (optimizationsEnabled) {
+        // Optimizations currently mess with Hardhat's decoding of custom errors
+        expect(revert.message).to.include.oneOf([reason, 'unrecognized return data or custom error']);
+      } else {
+        expect(revert.message).to.include(reason);
+      }
     }
   }
 };
