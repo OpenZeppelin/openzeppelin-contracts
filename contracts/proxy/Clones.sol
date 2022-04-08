abd26795d5bbfe43b48e60369198322c9f85f98c
philogy <philippe@dumo.net>
2022-04-08 15:03:41 +0200

    Adjust length for new deployment code

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -28,7 +28,7 @@ library Clones {
             mstore(ptr, 0x602d8060093d393df3363d3d373d3d3d363d7300000000000000000000000000)
             mstore(add(ptr, 0x13), shl(0x60, implementation))
             mstore(add(ptr, 0x27), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            instance := create(0, ptr, 0x37)
+            instance := create(0, ptr, 0x36)
         }
         require(instance != address(0), "ERC1167: create failed");
     }
@@ -46,7 +46,7 @@ library Clones {
             mstore(ptr, 0x602d8060093d393df3363d3d373d3d3d363d7300000000000000000000000000)
             mstore(add(ptr, 0x13), shl(0x60, implementation))
             mstore(add(ptr, 0x27), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            instance := create2(0, ptr, 0x37, salt)
+            instance := create2(0, ptr, 0x36, salt)
         }
         require(instance != address(0), "ERC1167: create2 failed");
     }
@@ -66,8 +66,8 @@ library Clones {
             mstore(add(ptr, 0x27), 0x5af43d82803e903d91602b57fd5bf3ff00000000000000000000000000000000)
             mstore(add(ptr, 0x37), shl(0x60, deployer))
             mstore(add(ptr, 0x4b), salt)
-            mstore(add(ptr, 0x6c), keccak256(ptr, 0x37))
-            predicted := keccak256(add(ptr, 0x37), 0x55)
+            mstore(add(ptr, 0x6b), keccak256(ptr, 0x36))
+            predicted := keccak256(add(ptr, 0x36), 0x55)
         }
     }
 
