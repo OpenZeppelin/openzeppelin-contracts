c42f573d4d518bd7c78afbe9d2d8547d2418ce26
ashhanai <ashhab.naim@gmail.com>
2022-04-12 17:48:02 +0200

    Enable using ERC165 check for one supported interface directly

--- contracts/utils/introspection/ERC165Checker.sol
+++ contracts/utils/introspection/ERC165Checker.sol
@@ -104,7 +104,7 @@ library ERC165Checker {
      * with {supportsERC165}.
      * Interface identification is specified in ERC-165.
      */
-    function _supportsERC165Interface(address account, bytes4 interfaceId) private view returns (bool) {
+    function _supportsERC165Interface(address account, bytes4 interfaceId) internal view returns (bool) {
         bytes memory encodedParams = abi.encodeWithSelector(IERC165.supportsInterface.selector, interfaceId);
         (bool success, bytes memory result) = account.staticcall{gas: 30000}(encodedParams);
         if (result.length < 32) return false;
