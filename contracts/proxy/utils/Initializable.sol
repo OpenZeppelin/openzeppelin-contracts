2cf3ac4033e32e4892d596440d11e3b39768f687
Francisco Giordano <frangio.1@gmail.com>
2022-04-13 19:21:52 -0300

    Bump minimum Solidity version for Initializable.sol to 0.8.2 (#3328)
    (cherry picked from commit cb14ea3c5c72151f254ec79866793ddf65f4c727)

--- contracts/proxy/utils/Initializable.sol
+++ contracts/proxy/utils/Initializable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.6.0-rc.0) (proxy/utils/Initializable.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "../../utils/Address.sol";
 
