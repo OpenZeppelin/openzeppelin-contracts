22ef7d2674fda2ea7c5510de17f1120bbf84e46b
Hamoud121 <104237835+Hamoud121@users.noreply.github.com>
2022-04-23 03:58:44 +0300

    Create Bep-20.sol

---
+++ Bep-20.sol
@@ -0,0 +1,8 @@
+Pragma solidity ^0.8.0;
+
+
+Import " https://github.com/OpenZeppelin/openzeppelin-contracts/new/master"
+
+Contract Rose-Leaf is ERC20 {
+   Constructor(uint256 initialsupply) puplic ERC20 ("Fomotion", "Rose"{
+   _mint(msg.sender,initialsupply);
