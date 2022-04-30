993a17e75bf14663eff7982e954ad03468ac5df8
Nate <nate@Nates-MacBook-Pro.local>
2022-04-30 14:31:32 -0600

    fix: use comment instead of dialect string for now

--- contracts/mocks/SafeMathMock.sol
+++ contracts/mocks/SafeMathMock.sol
@@ -73,65 +73,75 @@ contract SafeMathMock {
 
     function addMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := mload(0x40)
         }
         for (uint256 i = 0; i < length; ++i) {
             SafeMath.add(1, 1);
         }
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := sub(mload(0x40), mem)
         }
     }
 
     function subMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := mload(0x40)
         }
         for (uint256 i = 0; i < length; ++i) {
             SafeMath.sub(1, 1);
         }
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := sub(mload(0x40), mem)
         }
     }
 
     function mulMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := mload(0x40)
         }
         for (uint256 i = 0; i < length; ++i) {
             SafeMath.mul(1, 1);
         }
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := sub(mload(0x40), mem)
         }
     }
 
     function divMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := mload(0x40)
         }
         for (uint256 i = 0; i < length; ++i) {
             SafeMath.div(1, 1);
         }
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := sub(mload(0x40), mem)
         }
     }
 
     function modMemoryCheck() public pure returns (uint256 mem) {
         uint256 length = 32;
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := mload(0x40)
         }
         for (uint256 i = 0; i < length; ++i) {
             SafeMath.mod(1, 1);
         }
-        assembly ("memory-safe") {
+        /// @solidity memory-safe-assembly
+        assembly {
             mem := sub(mload(0x40), mem)
         }
     }
