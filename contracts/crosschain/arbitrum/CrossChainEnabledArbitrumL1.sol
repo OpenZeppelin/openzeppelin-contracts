2ebd1899aea62d0f0e897446dd75b84fc79374e3
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-05-02 15:00:01 +0200

    Fix links in documentation

--- contracts/crosschain/arbitrum/CrossChainEnabledArbitrumL1.sol
+++ contracts/crosschain/arbitrum/CrossChainEnabledArbitrumL1.sol
@@ -7,7 +7,7 @@ import "../CrossChainEnabled.sol";
 import "./LibArbitrumL1.sol";
 
 /**
- * @dev [Arbitrum](https://arbitrum.io/) specialization or the
+ * @dev https://arbitrum.io/[Arbitrum] specialization or the
  * {CrossChainEnabled} abstraction the L1 side (mainnet).
  *
  * This version should only be deployed on L1 to process cross-chain messages
@@ -15,7 +15,7 @@ import "./LibArbitrumL1.sol";
  *
  * The bridge contract is provided and maintained by the arbitrum team. You can
  * find the address of this contract on the rinkeby testnet in
- * [Arbitrum's developer documentation](https://developer.offchainlabs.com/docs/useful_addresses).
+ * https://developer.offchainlabs.com/docs/useful_addresses[Arbitrum's developer documentation].
  *
  * _Available since v4.6._
  */
