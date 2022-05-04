a4750eba850ef06a3d5860e9056b604b91eea5bc
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-05-04 18:20:59 -0300

    Improve wording consistency in code/doc (#3365)
    (cherry picked from commit 07b1b472c0ac3e50963c8d52cd2dac6e7e05260c)

--- contracts/governance/IGovernor.sol
+++ contracts/governance/IGovernor.sol
@@ -237,7 +237,7 @@ abstract contract IGovernor is IERC165 {
     /**
      * @dev Cast a vote with a reason and additional encoded parameters
      *
-     * Emits a {VoteCast} event.
+     * Emits a {VoteCast} or {VoteCastWithParams} event depending on the length of params.
      */
     function castVoteWithReasonAndParams(
         uint256 proposalId,
@@ -262,7 +262,7 @@ abstract contract IGovernor is IERC165 {
     /**
      * @dev Cast a vote with a reason and additional encoded parameters using the user's cryptographic signature.
      *
-     * Emits a {VoteCast} event.
+     * Emits a {VoteCast} or {VoteCastWithParams} event depending on the length of params.
      */
     function castVoteWithReasonAndParamsBySig(
         uint256 proposalId,
