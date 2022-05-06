aafb14461be6d71d02afbe57a06457725595f9af
Michael George <mdgeorge@cs.cornell.edu>
2022-05-06 14:12:32 -0400

    made the spec run

--- certora/scripts/verifyERC1155Pausable.sh
+++ certora/scripts/verifyERC1155Pausable.sh
@@ -1,5 +1,5 @@
 certoraRun \
-    certora/harness/ERC1155/ERC1155PausableHarness.sol \
+    certora/harnesses/ERC1155/ERC1155PausableHarness.sol \
     --verify ERC1155PausableHarness:certora/specs/ERC1155Pausable.spec \
     --solc solc8.2 \
     --optimistic_loop \
