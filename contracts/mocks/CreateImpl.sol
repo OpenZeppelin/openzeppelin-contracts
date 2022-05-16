8be534c13932395532e336e5861938d1eb272dc4
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-05-16 09:19:28 +0200

    mock contract
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ contracts/mocks/CreateImpl.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "../utils/Create.sol";
+import "../utils/introspection/ERC1820Implementer.sol";
+
+contract CreateImpl {
+    function deploy(
+        uint256 value,
+        bytes memory code
+    ) public {
+        Create.deploy(value, code);
+    }
+
+    function deployERC1820Implementer(uint256 value) public {
+        Create.deploy(value, type(ERC1820Implementer).creationCode);
+    }
+
+    function computeAddress(address addr, uint256 nonce) public pure returns (address) {
+        return Create.computeAddress(addr, nonce);
+    }
+
+    receive() external payable {}
+}
