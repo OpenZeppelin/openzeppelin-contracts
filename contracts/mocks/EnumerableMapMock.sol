b9bc2c3e936429626a169b39899bb8926684610e
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-05-20 15:14:00 +0200

    Update EnumerableMapMock.sol

--- contracts/mocks/EnumerableMapMock.sol
+++ contracts/mocks/EnumerableMapMock.sol
@@ -4,49 +4,6 @@ pragma solidity ^0.8.0;
 
 import "../utils/structs/EnumerableMap.sol";
 
-// UintToUintMap
-contract UintToUintMapMock {
-    using EnumerableMap for EnumerableMap.UintToUintMap;
-
-    event OperationResult(bool result);
-
-    EnumerableMap.UintToUintMap private _map;
-
-    function contains(uint256 key) public view returns (bool) {
-        return _map.contains(key);
-    }
-
-    function set(uint256 key, uint256 value) public {
-        bool result = _map.set(key, value);
-        emit OperationResult(result);
-    }
-
-    function remove(uint256 key) public {
-        bool result = _map.remove(key);
-        emit OperationResult(result);
-    }
-
-    function length() public view returns (uint256) {
-        return _map.length();
-    }
-
-    function at(uint256 index) public view returns (uint256 key, uint256 value) {
-        return _map.at(index);
-    }
-
-    function tryGet(uint256 key) public view returns (bool, uint256) {
-        return _map.tryGet(key);
-    }
-
-    function get(uint256 key) public view returns (uint256) {
-        return _map.get(key);
-    }
-
-    function getWithMessage(uint256 key, string calldata errorMessage) public view returns (uint256) {
-        return _map.get(key, errorMessage);
-    }
-}
-
 // UintToAddressMap
 contract UintToAddressMapMock {
     using EnumerableMap for EnumerableMap.UintToAddressMap;
@@ -171,6 +128,49 @@ contract Bytes32ToBytes32MapMock {
     }
 
     function getWithMessage(bytes32 key, string calldata errorMessage) public view returns (bytes32) {
+        return _map.get(key, errorMessage);
+    }
+}
+
+// UintToUintMap
+contract UintToUintMapMock {
+    using EnumerableMap for EnumerableMap.UintToUintMap;
+
+    event OperationResult(bool result);
+
+    EnumerableMap.UintToUintMap private _map;
+
+    function contains(uint256 key) public view returns (bool) {
+        return _map.contains(key);
+    }
+
+    function set(uint256 key, uint256 value) public {
+        bool result = _map.set(key, value);
+        emit OperationResult(result);
+    }
+
+    function remove(uint256 key) public {
+        bool result = _map.remove(key);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _map.length();
+    }
+
+    function at(uint256 index) public view returns (uint256 key, uint256 value) {
+        return _map.at(index);
+    }
+
+    function tryGet(uint256 key) public view returns (bool, uint256) {
+        return _map.tryGet(key);
+    }
+
+    function get(uint256 key) public view returns (uint256) {
+        return _map.get(key);
+    }
+
+    function getWithMessage(uint256 key, string calldata errorMessage) public view returns (uint256) {
         return _map.get(key, errorMessage);
     }
 }
