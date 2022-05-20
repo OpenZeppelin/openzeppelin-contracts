c4f76cfa155ec28a66bdd9bf24890174fef5b976
ashhanai <ashhab.naim@gmail.com>
2022-05-20 14:31:53 +0100

    Add uint to uint enumerable map (#3338)

--- contracts/mocks/EnumerableMapMock.sol
+++ contracts/mocks/EnumerableMapMock.sol
@@ -131,3 +131,46 @@ contract Bytes32ToBytes32MapMock {
         return _map.get(key, errorMessage);
     }
 }
+
+// UintToUintMap
+contract UintToUintMapMock {
+    using EnumerableMap for EnumerableMap.UintToUintMap;
+
+    event OperationResult(bool result);
+
+    EnumerableMap.UintToUintMap private _map;
+
+    function contains(uint256 key) public view returns (bool) {
+        return _map.contains(key);
+    }
+
+    function set(uint256 key, uint256 value) public {
+        bool result = _map.set(key, value);
+        emit OperationResult(result);
+    }
+
+    function remove(uint256 key) public {
+        bool result = _map.remove(key);
+        emit OperationResult(result);
+    }
+
+    function length() public view returns (uint256) {
+        return _map.length();
+    }
+
+    function at(uint256 index) public view returns (uint256 key, uint256 value) {
+        return _map.at(index);
+    }
+
+    function tryGet(uint256 key) public view returns (bool, uint256) {
+        return _map.tryGet(key);
+    }
+
+    function get(uint256 key) public view returns (uint256) {
+        return _map.get(key);
+    }
+
+    function getWithMessage(uint256 key, string calldata errorMessage) public view returns (uint256) {
+        return _map.get(key, errorMessage);
+    }
+}
