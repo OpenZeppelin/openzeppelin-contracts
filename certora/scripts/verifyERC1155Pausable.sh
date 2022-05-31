018c58219fb25ceb516348d493cb50435ea77ad5
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-05-31 13:59:46 -0700

    Updated 1155 Pausable and Supply scripts to first run make munged

--- certora/scripts/verifyERC1155Pausable.sh
+++ certora/scripts/verifyERC1155Pausable.sh
@@ -1,3 +1,5 @@
+make -C certora munged
+
 certoraRun \
     certora/harnesses/ERC1155/ERC1155PausableHarness.sol \
     --verify ERC1155PausableHarness:certora/specs/ERC1155Pausable.spec \
--- certora/scripts/verifyERC1155Supply.sh
+++ certora/scripts/verifyERC1155Supply.sh
@@ -1,3 +1,5 @@
+make -C certora munged
+
 certoraRun \
     certora/harnesses/ERC1155/ERC1155SupplyHarness.sol \
     --verify ERC1155SupplyHarness:certora/specs/ERC1155Supply.spec \
