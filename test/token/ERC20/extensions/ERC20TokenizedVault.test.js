9a6ab42a1f4f6dc1be4e1bcef1b51833d6a33bcc
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-01 21:04:05 +0200

    fix lint

--- test/token/ERC20/extensions/ERC20TokenizedVault.test.js
+++ test/token/ERC20/extensions/ERC20TokenizedVault.test.js
@@ -225,7 +225,10 @@ contract('ERC20TokenizedVault', function (accounts) {
 
       // Cannot deposit more than 0
       await expectRevert.unspecified(this.vault.previewDeposit(parseToken(1)));
-      await expectRevert(this.vault.deposit(parseToken(1), recipient, { from: holder }), 'ERC20TokenizedVault: deposit more than max');
+      await expectRevert(
+				this.vault.deposit(parseToken(1), recipient, { from: holder }),
+				'ERC20TokenizedVault: deposit more than max',
+			);
     });
 
     it('mint', async function () {
