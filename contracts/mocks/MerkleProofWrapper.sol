e2ed8f89081eeea87ecfd7d833d12d532c4a454f
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-02 09:23:21 +0200

    Update MerkleProofWrapper.sol

--- contracts/mocks/MerkleProofWrapper.sol
+++ contracts/mocks/MerkleProofWrapper.sol
@@ -8,7 +8,7 @@ contract MerkleProofWrapper {
     function verify(
         bytes32[] memory proof,
         bytes32 root,
-        bytes32 leaves
+        bytes32 leaf
     ) public pure returns (bool) {
         return MerkleProof.verify(proof, root, leaf);
     }
