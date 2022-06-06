234b843c36aee4058b6c9a4e854870f9ec308996
Michael George <mdgeorge@cs.cornell.edu>
2022-06-06 15:44:07 -0400

    commented out failing rules and added solc version to ERC1155

--- certora/scripts/old/verifyERC1155.sh
+++ certora/scripts/old/verifyERC1155.sh
@@ -1,7 +1,7 @@
 certoraRun \
     certora/harnesses/ERC1155/ERC1155Harness.sol \
     --verify ERC1155Harness:certora/specs/ERC1155.spec \
-    --solc solc \
+    --solc solc8.2 \
     --optimistic_loop \
     --loop_iter 3 \
     --send_only \
