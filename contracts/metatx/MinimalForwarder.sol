0ef4b8a9cbfd315f460cc64b7c929600684eb8f1
Francisco Giordano <frangio.1@gmail.com>
2022-06-06 18:27:15 -0300

    formatting and small rewording

--- contracts/metatx/MinimalForwarder.sol
+++ contracts/metatx/MinimalForwarder.sol
@@ -8,7 +8,11 @@ import "../utils/cryptography/draft-EIP712.sol";
 
 /**
  * @dev Simple minimal forwarder to be used together with an ERC2771 compatible contract. See {ERC2771Context}.
- *  MinimalForwarder is missing features to be a good forwarder contract. This contract does not intend to have all the properties that are needed for a sound forwarding system. A fully functioning forwarding system with good properties requires more complexity. We suggest you look at other projects such as the GSN which do have the goal of building a system like that.
+ *
+ * MinimalForwarder is mainly meant for testing, as it is missing features to be a good production-ready forwarder. This
+ * contract does not intend to have all the properties that are needed for a sound forwarding system. A fully
+ * functioning forwarding system with good properties requires more complexity. We suggest you look at other projects
+ * such as the GSN which do have the goal of building a system like that.
  */
 contract MinimalForwarder is EIP712 {
     using ECDSA for bytes32;
