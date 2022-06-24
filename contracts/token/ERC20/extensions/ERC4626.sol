594c35e751937ce3e95b5207ad3f5143032ffdac
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-24 10:30:15 +0200

    Make ERC4626 _deposit and _withdraw internal virtual

--- contracts/token/ERC20/extensions/ERC4626.sol
+++ contracts/token/ERC20/extensions/ERC4626.sol
@@ -175,7 +175,7 @@ abstract contract ERC4626 is ERC20, IERC4626 {
         address receiver,
         uint256 assets,
         uint256 shares
-    ) private {
+    ) internal virtual {
         // If _asset is ERC777, `transferFrom` can trigger a reenterancy BEFORE the transfer happens through the
         // `tokensToSend` hook. On the other hand, the `tokenReceived` hook, that is triggered after the transfer,
         // calls the vault, which is assumed not malicious.
@@ -198,7 +198,7 @@ abstract contract ERC4626 is ERC20, IERC4626 {
         address owner,
         uint256 assets,
         uint256 shares
-    ) private {
+    ) internal virtual {
         if (caller != owner) {
             _spendAllowance(owner, caller, shares);
         }
