3dac7bbed7b4c0dbf504180c33e8ed8e350b93eb
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-06-29 12:34:30 +0200

    Merge v4.7 back into master (#3516)

--- contracts/finance/PaymentSplitter.sol
+++ contracts/finance/PaymentSplitter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.1 (finance/PaymentSplitter.sol)
+// OpenZeppelin Contracts (last updated v4.7.0) (finance/PaymentSplitter.sol)
 
 pragma solidity ^0.8.0;
 
