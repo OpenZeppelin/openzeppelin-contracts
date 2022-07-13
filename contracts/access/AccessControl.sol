1cfe674423d4d7621a68a387d2e66a441336dfc2
Pavan Ananth Sharma <86551444+PavanAnanthSharma@users.noreply.github.com>
2022-07-14 01:11:01 +0530

    Update AccessControl.sol
    Change in the abi encoding and the version of solidity

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (access/AccessControl.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "./IAccessControl.sol";
 import "../utils/Context.sol";
@@ -108,12 +108,14 @@ abstract contract AccessControl is Context, IAccessControl, ERC165 {
         if (!hasRole(role, account)) {
             revert(
                 string(
-                    abi.encodePacked(
-                        "AccessControl: account ",
-                        Strings.toHexString(account),
-                        " is missing role ",
-                        Strings.toHexString(uint256(role), 32)
-                    )
+                    keccak256(
+                        abi.encodePacked(
+                            "AccessControl: account ",
+                            Strings.toHexString(account),
+                            " is missing role ",
+                            Strings.toHexString(uint256(role), 32)
+                      )
+                   )
                 )
             );
         }
