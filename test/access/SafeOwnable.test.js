fbc33a89842d6ff2fb62d5fd65d8683a92d74255
Helder Sepulveda <heldersepu@gmail.com>
2022-08-16 09:40:34 -0400

    testing the new SafeOwnable contract

---
+++ test/access/SafeOwnable.test.js
@@ -0,0 +1,44 @@
+const { constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+
+const { expect } = require('chai');
+
+const SafeOwnable = artifacts.require('SafeOwnableMock');
+
+contract('SafeOwnable', function (accounts) {
+  const [ owner, accountA, accountB ] = accounts;
+
+  beforeEach(async function () {
+    this.safe_ownable = await SafeOwnable.new({ from: owner });
+  });
+
+  describe('transfer ownership', function () {
+    it('starting a transfer does not change owner', async function () {
+      const receipt = await this.safe_ownable.transferOwnership(accountA, { from: owner });
+      expectEvent(receipt, 'OwnershipTransferStarted');
+      expect(await this.safe_ownable.owner()).to.equal(owner);
+    });
+
+    it('guards transfer against invalid user', async function () {
+        await this.safe_ownable.transferOwnership(accountA, { from: owner });
+        await expectRevert(
+          this.safe_ownable.acceptOwnership({ from: accountB }),
+          'SafeOwnable: caller is not the new owner',
+        );
+      });
+
+    it('changes owner after transfer', async function () {
+        await this.safe_ownable.transferOwnership(accountA, { from: owner });
+        const receipt = await this.safe_ownable.acceptOwnership({ from: accountA });
+        expectEvent(receipt, 'OwnershipTransferred');
+        expect(await this.safe_ownable.owner()).to.equal(accountA);
+    });
+
+    it('guards ownership against stuck state', async function () {
+      await expectRevert(
+        this.safe_ownable.transferOwnership(ZERO_ADDRESS, { from: owner }),
+        'SafeOwnable: new owner is the zero address',
+      );
+    });
+  });
+});
