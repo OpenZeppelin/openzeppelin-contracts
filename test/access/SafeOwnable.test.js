6c7325d66ff0b13a50f2c8b189c957bf7485da58
Helder Sepulveda <heldersepu@gmail.com>
2022-08-16 10:28:36 -0400

    Fix error 'safe_ownable' is not in camel case

--- test/access/SafeOwnable.test.js
+++ test/access/SafeOwnable.test.js
@@ -9,34 +9,34 @@ contract('SafeOwnable', function (accounts) {
   const [ owner, accountA, accountB ] = accounts;
 
   beforeEach(async function () {
-    this.safe_ownable = await SafeOwnable.new({ from: owner });
+    this.safeOwnable = await SafeOwnable.new({ from: owner });
   });
 
   describe('transfer ownership', function () {
     it('starting a transfer does not change owner', async function () {
-      const receipt = await this.safe_ownable.transferOwnership(accountA, { from: owner });
+      const receipt = await this.safeOwnable.transferOwnership(accountA, { from: owner });
       expectEvent(receipt, 'OwnershipTransferStarted');
-      expect(await this.safe_ownable.owner()).to.equal(owner);
+      expect(await this.safeOwnable.owner()).to.equal(owner);
     });
 
     it('changes owner after transfer', async function () {
-      await this.safe_ownable.transferOwnership(accountA, { from: owner });
-      const receipt = await this.safe_ownable.acceptOwnership({ from: accountA });
+      await this.safeOwnable.transferOwnership(accountA, { from: owner });
+      const receipt = await this.safeOwnable.acceptOwnership({ from: accountA });
       expectEvent(receipt, 'OwnershipTransferred');
-      expect(await this.safe_ownable.owner()).to.equal(accountA);
+      expect(await this.safeOwnable.owner()).to.equal(accountA);
     });
 
     it('guards transfer against invalid user', async function () {
-      await this.safe_ownable.transferOwnership(accountA, { from: owner });
+      await this.safeOwnable.transferOwnership(accountA, { from: owner });
       await expectRevert(
-        this.safe_ownable.acceptOwnership({ from: accountB }),
+        this.safeOwnable.acceptOwnership({ from: accountB }),
         'SafeOwnable: caller is not the new owner',
       );
     });
 
     it('guards ownership against stuck state', async function () {
       await expectRevert(
-        this.safe_ownable.transferOwnership(ZERO_ADDRESS, { from: owner }),
+        this.safeOwnable.transferOwnership(ZERO_ADDRESS, { from: owner }),
         'SafeOwnable: new owner is the zero address',
       );
     });
