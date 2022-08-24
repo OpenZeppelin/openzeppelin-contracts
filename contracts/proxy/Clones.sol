f09b4d00a430fc780e9dae453db9d5d8ad74fa85
Vectorized <webby1111@hotmail.com>
2022-08-24 16:56:41 +0800

    Revert "Update contracts/proxy/Clones.sol"
    This reverts commit 75e577044c5926f73162c60e647d392011578796.

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -25,11 +25,12 @@ library Clones {
     function clone(address implementation) internal returns (address instance) {
         /// @solidity memory-safe-assembly
         assembly {
-             // Packs the size 3 bytes of `implementation` with the bytecode before the address. 
-            mstore(0x00, or(shr(0x88, implementation), 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000))
-             // Packs the remaining 17 bytes of `implementation` with the bytecode after the address.             
-             mstore(0x20, or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3)) 
-            instance := create(0, 0x00, 0x37)
+            // Cleans the upper 96 bits of the `implementation` word, then packs the first 3 bytes
+            // of the `implementation` address with the bytecode before the address.
+            mstore(0x00, or(0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000, shr(0xe8, shl(0x60, implementation))))
+            // Packs the remaining 17 bytes of `implementation` with the bytecode after the address.
+            mstore(0x20, or(shl(0x78, implementation), 0x5af43d82803e903d91602b57fd5bf3))
+            instance := create(0, 0x09, 0x37)
         }
         require(instance != address(0), "ERC1167: create failed");
     }
