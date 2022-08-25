b9a4ae1c70bf188a5e702dba6fdef79f1f17a806
CodeSandwich <igor.zuk@protonmail.com>
2022-08-25 17:09:00 +0200

    Less assembly

--- contracts/utils/Strings.sol
+++ contracts/utils/Strings.sol
@@ -49,21 +49,19 @@ library Strings {
             // now, length is log10(value) + 1
 
             string memory buffer = new string(length);
+            uint ptr;
             /// @solidity memory-safe-assembly
             assembly {
-                let ptr := add(buffer, add(32, length))
-                for {
-
-                } 1 {
-
-                } {
-                    ptr := sub(ptr, 1)
+                ptr := add(buffer, add(32, length))
+            }
+            while(true) {
+                ptr--;
+                /// @solidity memory-safe-assembly
+                assembly {
                     mstore8(ptr, byte(mod(value, 10), _SYMBOLS))
-                    value := div(value, 10)
-                    if iszero(value) {
-                        break
-                    }
                 }
+                value /= 10;
+                if (value == 0) break;
             }
             return buffer;
         }
