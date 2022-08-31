b36ceef9cfe3dd10139d0950f0ccd2ecfdab8d43
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-08-31 15:57:10 +0200

    Remove Introspection mocks

--- test/utils/introspection/ERC165Storage.test.js
+++ test/utils/introspection/ERC165Storage.test.js
@@ -2,21 +2,21 @@ const { expectRevert } = require('@openzeppelin/test-helpers');
 
 const { shouldSupportInterfaces } = require('./SupportsInterface.behavior');
 
-const ERC165Mock = artifacts.require('ERC165StorageMock');
+const ERC165Storage = artifacts.require('$ERC165Storage');
 
 contract('ERC165Storage', function (accounts) {
   beforeEach(async function () {
-    this.mock = await ERC165Mock.new();
+    this.mock = await ERC165Storage.new();
   });
 
   it('register interface', async function () {
     expect(await this.mock.supportsInterface('0x00000001')).to.be.equal(false);
-    await this.mock.registerInterface('0x00000001');
+    await this.mock.$_registerInterface('0x00000001');
     expect(await this.mock.supportsInterface('0x00000001')).to.be.equal(true);
   });
 
   it('does not allow 0xffffffff', async function () {
-    await expectRevert(this.mock.registerInterface('0xffffffff'), 'ERC165: invalid interface id');
+    await expectRevert(this.mock.$_registerInterface('0xffffffff'), 'ERC165: invalid interface id');
   });
 
   shouldSupportInterfaces([
