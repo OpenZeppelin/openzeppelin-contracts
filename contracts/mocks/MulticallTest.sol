4a511fab815026722992f96c1712328d9fb92133
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-08-31 19:28:38 +0200

    fix multicall token test

--- contracts/mocks/MulticallTest.sol
+++ contracts/mocks/MulticallTest.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.0;
 
-import "./MulticallTokenMock.sol";
+import "./ERC20/ERC20MulticallMock.sol";
 
 contract MulticallTest {
     function testReturnValues(
-        MulticallTokenMock multicallToken,
+        ERC20MulticallMock multicallToken,
         address[] calldata recipients,
         uint256[] calldata amounts
     ) external {
--- contracts/mocks/MulticallTokenMock.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-import "../token/ERC20/ERC20.sol";
-import "../utils/Multicall.sol";
-
-abstract contract MulticallTokenMock is ERC20, Multicall {}
