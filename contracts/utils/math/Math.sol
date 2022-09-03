b8a9d4b5505a72517386fb8f1c3312634349449a
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-03 21:58:55 +0200

    unchecked arithmetics

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -190,8 +190,10 @@ library Math {
      */
     function sqrt(uint256 a, Rounding rounding) internal pure returns (uint256) {
         uint256 result = sqrt(a);
-        if (rounding == Rounding.Up && result * result < a) {
-            result += 1;
+        unchecked {
+            if (rounding == Rounding.Up && result * result < a) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -202,36 +204,38 @@ library Math {
      */
     function log2(uint256 value) internal pure returns (uint256) {
         uint256 result = 0;
-        if (value >> 128 > 0) {
-            value >>= 128;
-            result += 128;
-        }
-        if (value >> 64 > 0) {
-            value >>= 64;
-            result += 64;
-        }
-        if (value >> 32 > 0) {
-            value >>= 32;
-            result += 32;
-        }
-        if (value >> 16 > 0) {
-            value >>= 16;
-            result += 16;
-        }
-        if (value >> 8 > 0) {
-            value >>= 8;
-            result += 8;
-        }
-        if (value >> 4 > 0) {
-            value >>= 4;
-            result += 4;
-        }
-        if (value >> 2 > 0) {
-            value >>= 2;
-            result += 2;
-        }
-        if (value >> 1 > 0) {
-            result += 1;
+        unchecked {
+            if (value >> 128 > 0) {
+                value >>= 128;
+                result += 128;
+            }
+            if (value >> 64 > 0) {
+                value >>= 64;
+                result += 64;
+            }
+            if (value >> 32 > 0) {
+                value >>= 32;
+                result += 32;
+            }
+            if (value >> 16 > 0) {
+                value >>= 16;
+                result += 16;
+            }
+            if (value >> 8 > 0) {
+                value >>= 8;
+                result += 8;
+            }
+            if (value >> 4 > 0) {
+                value >>= 4;
+                result += 4;
+            }
+            if (value >> 2 > 0) {
+                value >>= 2;
+                result += 2;
+            }
+            if (value >> 1 > 0) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -242,8 +246,10 @@ library Math {
      */
     function log2(uint256 value, Rounding rounding) internal pure returns (uint256) {
         uint256 result = log2(value);
-        if (rounding == Rounding.Up && 1 << result < value) {
-            result += 1;
+        unchecked {
+            if (rounding == Rounding.Up && 1 << result < value) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -254,32 +260,34 @@ library Math {
      */
     function log10(uint256 value) internal pure returns (uint256) {
         uint256 result = 0;
-        if (value >= 10**64) {
-            value /= 10**64;
-            result += 64;
-        }
-        if (value >= 10**32) {
-            value /= 10**32;
-            result += 32;
-        }
-        if (value >= 10**16) {
-            value /= 10**16;
-            result += 16;
-        }
-        if (value >= 10**8) {
-            value /= 10**8;
-            result += 8;
-        }
-        if (value >= 10**4) {
-            value /= 10**4;
-            result += 4;
-        }
-        if (value >= 10**2) {
-            value /= 10**2;
-            result += 2;
-        }
-        if (value >= 10**1) {
-            result += 1;
+        unchecked {
+            if (value >= 10**64) {
+                value /= 10**64;
+                result += 64;
+            }
+            if (value >= 10**32) {
+                value /= 10**32;
+                result += 32;
+            }
+            if (value >= 10**16) {
+                value /= 10**16;
+                result += 16;
+            }
+            if (value >= 10**8) {
+                value /= 10**8;
+                result += 8;
+            }
+            if (value >= 10**4) {
+                value /= 10**4;
+                result += 4;
+            }
+            if (value >= 10**2) {
+                value /= 10**2;
+                result += 2;
+            }
+            if (value >= 10**1) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -290,8 +298,10 @@ library Math {
      */
     function log10(uint256 value, Rounding rounding) internal pure returns (uint256) {
         uint256 result = log10(value);
-        if (rounding == Rounding.Up && 10**result < value) {
-            result += 1;
+        unchecked {
+            if (rounding == Rounding.Up && 10**result < value) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -304,24 +314,26 @@ library Math {
      */
     function log256(uint256 value) internal pure returns (uint256) {
         uint256 result = 0;
-        if (value >> 128 > 0) {
-            value >>= 128;
-            result += 16;
-        }
-        if (value >> 64 > 0) {
-            value >>= 64;
-            result += 8;
-        }
-        if (value >> 32 > 0) {
-            value >>= 32;
-            result += 4;
-        }
-        if (value >> 16 > 0) {
-            value >>= 16;
-            result += 2;
-        }
-        if (value >> 8 > 0) {
-            result += 1;
+        unchecked {
+            if (value >> 128 > 0) {
+                value >>= 128;
+                result += 16;
+            }
+            if (value >> 64 > 0) {
+                value >>= 64;
+                result += 8;
+            }
+            if (value >> 32 > 0) {
+                value >>= 32;
+                result += 4;
+            }
+            if (value >> 16 > 0) {
+                value >>= 16;
+                result += 2;
+            }
+            if (value >> 8 > 0) {
+                result += 1;
+            }
         }
         return result;
     }
@@ -332,8 +344,10 @@ library Math {
      */
     function log256(uint256 value, Rounding rounding) internal pure returns (uint256) {
         uint256 result = log256(value);
-        if (rounding == Rounding.Up && 1 << (result * 8) < value) {
-            result += 1;
+        unchecked {
+            if (rounding == Rounding.Up && 1 << (result * 8) < value) {
+                result += 1;
+            }
         }
         return result;
     }
