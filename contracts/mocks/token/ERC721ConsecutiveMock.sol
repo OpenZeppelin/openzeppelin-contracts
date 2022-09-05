44e7271262fbd336be9fd44e92d6e3ead14baa48
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-05 23:59:52 +0200

    refactor ERC721Consecutive tests

--- contracts/mocks/token/ERC721ConsecutiveMock.sol
+++ contracts/mocks/token/ERC721ConsecutiveMock.sol
@@ -2,16 +2,15 @@
 
 pragma solidity ^0.8.0;
 
-import "../token/ERC721/extensions/ERC721Burnable.sol";
-import "../token/ERC721/extensions/ERC721Consecutive.sol";
-import "../token/ERC721/extensions/ERC721Enumerable.sol";
-import "../token/ERC721/extensions/ERC721Pausable.sol";
-import "../token/ERC721/extensions/draft-ERC721Votes.sol";
+import "../../token/ERC721/extensions/ERC721Consecutive.sol";
+import "../../token/ERC721/extensions/ERC721Enumerable.sol";
+import "../../token/ERC721/extensions/ERC721Pausable.sol";
+import "../../token/ERC721/extensions/draft-ERC721Votes.sol";
 
 /**
  * @title ERC721ConsecutiveMock
  */
-contract ERC721ConsecutiveMock is ERC721Burnable, ERC721Consecutive, ERC721Pausable, ERC721Votes {
+contract ERC721ConsecutiveMock is ERC721Consecutive, ERC721Pausable, ERC721Votes {
     constructor(
         string memory name,
         string memory symbol,
@@ -26,30 +25,6 @@ contract ERC721ConsecutiveMock is ERC721Burnable, ERC721Consecutive, ERC721Pausa
         for (uint256 i = 0; i < receivers.length; ++i) {
             _mintConsecutive(receivers[i], amounts[i]);
         }
-    }
-
-    function pause() external {
-        _pause();
-    }
-
-    function unpause() external {
-        _unpause();
-    }
-
-    function exists(uint256 tokenId) public view returns (bool) {
-        return _exists(tokenId);
-    }
-
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
-    }
-
-    function mintConsecutive(address to, uint96 amount) public {
-        _mintConsecutive(to, amount);
-    }
-
-    function safeMint(address to, uint256 tokenId) public {
-        _safeMint(to, tokenId);
     }
 
     function _ownerOf(uint256 tokenId) internal view virtual override(ERC721, ERC721Consecutive) returns (address) {
