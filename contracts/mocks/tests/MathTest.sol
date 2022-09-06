43da0ef633be726251e93b9783aeb073bd0a1d5f
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-06 22:13:51 +0200

    fix lint

--- contracts/mocks/tests/MathTest.sol
+++ contracts/mocks/tests/MathTest.sol
@@ -15,14 +15,12 @@ contract MathTest is Test {
         uint256 result = Math.sqrt(input, rounding);
 
         // square of result is bigger than input
-        if (_squareBigger(result, input))
-        {
+        if (_squareBigger(result, input)) {
             assertTrue(rounding == Math.Rounding.Up);
             assertTrue(_squareSmaller(result - 1, input));
         }
         // square of result is smaller than input
-        else if (_squareSmaller(result, input))
-        {
+        else if (_squareSmaller(result, input)) {
             assertFalse(rounding == Math.Rounding.Up);
             assertTrue(_squareBigger(result + 1, input));
         }
@@ -45,24 +43,21 @@ contract MathTest is Test {
 
         if (input == 0) {
             assertEq(result, 0);
-        } else if (_powerOf2Bigger(result, input))
-        {
+        } else if (_powerOf2Bigger(result, input)) {
             assertTrue(rounding == Math.Rounding.Up);
             assertTrue(_powerOf2Smaller(result - 1, input));
-        }
-        else if (_powerOf2Smaller(result, input))
-        {
+        } else if (_powerOf2Smaller(result, input)) {
             assertFalse(rounding == Math.Rounding.Up);
             assertTrue(_powerOf2Bigger(result + 1, input));
         }
     }
 
     function _powerOf2Bigger(uint256 value, uint256 ref) private pure returns (bool) {
-        return value >= 256 || 2 ** value > ref; // 2**256 overflows uint256
+        return value >= 256 || 2**value > ref; // 2**256 overflows uint256
     }
 
     function _powerOf2Smaller(uint256 value, uint256 ref) private pure returns (bool) {
-        return 2 ** value < ref;
+        return 2**value < ref;
     }
 
     // LOG10
@@ -73,52 +68,46 @@ contract MathTest is Test {
 
         if (input == 0) {
             assertEq(result, 0);
-        } else if (_powerOf10Bigger(result, input))
-        {
+        } else if (_powerOf10Bigger(result, input)) {
             assertTrue(rounding == Math.Rounding.Up);
             assertTrue(_powerOf10Smaller(result - 1, input));
-        }
-        else if (_powerOf10Smaller(result, input))
-        {
+        } else if (_powerOf10Smaller(result, input)) {
             assertFalse(rounding == Math.Rounding.Up);
             assertTrue(_powerOf10Bigger(result + 1, input));
         }
     }
 
     function _powerOf10Bigger(uint256 value, uint256 ref) private pure returns (bool) {
-        return value >= 78 || 10 ** value > ref; // 10**78 overflows uint256
+        return value >= 78 || 10**value > ref; // 10**78 overflows uint256
     }
 
     function _powerOf10Smaller(uint256 value, uint256 ref) private pure returns (bool) {
-        return 10 ** value < ref;
+        return 10**value < ref;
     }
 
     // LOG256
-        function testLog256(uint256 input, uint8 r) public {
+    function testLog256(uint256 input, uint8 r) public {
         Math.Rounding rounding = _asRounding(r);
 
         uint256 result = Math.log256(input, rounding);
 
         if (input == 0) {
             assertEq(result, 0);
-        } else if (_powerOf256Bigger(result, input))
-        {
+        } else if (_powerOf256Bigger(result, input)) {
             assertTrue(rounding == Math.Rounding.Up);
             assertTrue(_powerOf256Smaller(result - 1, input));
-        }
-        else if (_powerOf256Smaller(result, input))
-        {
+        } else if (_powerOf256Smaller(result, input)) {
             assertFalse(rounding == Math.Rounding.Up);
             assertTrue(_powerOf256Bigger(result + 1, input));
         }
     }
 
     function _powerOf256Bigger(uint256 value, uint256 ref) private pure returns (bool) {
-        return value >= 32 || 256 ** value > ref; // 256**32 overflows uint256
+        return value >= 32 || 256**value > ref; // 256**32 overflows uint256
     }
 
     function _powerOf256Smaller(uint256 value, uint256 ref) private pure returns (bool) {
-        return 256 ** value < ref;
+        return 256**value < ref;
     }
 
     // Helpers
