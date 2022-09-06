61d84815478daf9901d17c1389845d7ba0177060
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-06 22:19:43 +0200

    skip foundry test contract in hardhat compile

---
+++ hardhat/skip-foundry-tests.js
@@ -0,0 +1,5 @@
+const { subtask } = require('hardhat/config');
+const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require('hardhat/builtin-tasks/task-names');
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
+  .setAction((_, __, runSuper) => runSuper().then(paths => paths.filter(p => !p.endsWith('.test.sol'))));
