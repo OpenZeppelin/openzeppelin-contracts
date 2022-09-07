f203f1507ba9fa439e0f934597b2d6f7c70bd917
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-07 16:06:23 +0200

    move foundry tests to the ./test folder

--- hardhat/skip-foundry-tests.js
+++
@@ -1,5 +0,0 @@
-const { subtask } = require('hardhat/config');
-const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require('hardhat/builtin-tasks/task-names');
-
-subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
-  .setAction((_, __, runSuper) => runSuper().then(paths => paths.filter(p => !p.endsWith('.test.sol'))));
