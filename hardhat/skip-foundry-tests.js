904fc309f50781cdbb2534b7dcd42de4ee895481
Francisco Giordano <frangio.1@gmail.com>
2022-09-07 14:12:00 -0300

    use async instead of .then

--- hardhat/skip-foundry-tests.js
+++ hardhat/skip-foundry-tests.js
@@ -2,4 +2,6 @@ const { subtask } = require('hardhat/config');
 const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require('hardhat/builtin-tasks/task-names');
 
 subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
-  .setAction((_, __, runSuper) => runSuper().then(paths => paths.filter(path => !path.endsWith('.t.sol'))));
+  .setAction(async (_, __, runSuper) =>
+    (await runSuper()).filter((path) => !path.endsWith('.t.sol')),
+  );
