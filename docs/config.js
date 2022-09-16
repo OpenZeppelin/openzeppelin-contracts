1631dce59746593461507f46382d3c4e4b02fb9e
Francisco Giordano <frangio.1@gmail.com>
2022-09-16 20:50:54 -0300

    add explanation of config.pages

--- docs/config.js
+++ docs/config.js
@@ -8,6 +8,7 @@ module.exports = {
   exclude: ['mocks'],
   pageExtension: '.adoc',
   pages: (_, file, config) => {
+    // For each contract file, find the closest README.adoc and return its location as the output page path.
     const sourcesDir = path.resolve(config.root, config.sourcesDir);
     let dir = path.resolve(config.root, file.absolutePath);
     while (dir.startsWith(sourcesDir)) {
