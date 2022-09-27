7e205de58e185a4ff60733d60e161591a6394f72
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-09-27 21:25:19 +0200

    Update contracts/vendor/arbitrum/IArbSys.sol
    Co-authored-by: Francisco <frangio.1@gmail.com>

--- contracts/vendor/arbitrum/IArbSys.sol
+++ contracts/vendor/arbitrum/IArbSys.sol
@@ -1,5 +1,5 @@
 // Copyright 2021-2022, Offchain Labs, Inc.
-// For license information, see https://github.com/nitro/blob/master/LICENSE
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE
 // SPDX-License-Identifier: BUSL-1.1
 
 pragma solidity >=0.4.21 <0.9.0;
