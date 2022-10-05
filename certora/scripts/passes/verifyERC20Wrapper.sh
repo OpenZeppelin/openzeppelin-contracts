a73d7ab57b3190d7be82b2baec5cdd562187e394
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-10-05 15:30:03 +0200

    get rid of certora/helpers

--- certora/scripts/passes/verifyERC20Wrapper.sh
+++ certora/scripts/passes/verifyERC20Wrapper.sh
@@ -2,9 +2,8 @@
 
 set -euxo pipefail
 
-    # --link ERC20WrapperHarness:underlying=DummyERC20A \
 certoraRun \
-    certora/harnesses/ERC20WrapperHarness.sol certora/helpers/DummyERC20A.sol certora/helpers/DummyERC20B.sol \
+    certora/harnesses/ERC20WrapperHarness.sol certora/harnesses/ERC20PermitHarness.sol \
     --verify ERC20WrapperHarness:certora/specs/ERC20Wrapper.spec \
     --solc solc \
     --optimistic_loop \
