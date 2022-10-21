49abefd36866afb2ce93b92a9664ee36490b5f24
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2022-10-21 15:29:10 +0100

    Add toString(int256) function (OpenZeppelin#3758)

--- contracts/utils/Strings.sol
+++ contracts/utils/Strings.sol
@@ -41,7 +41,7 @@ library Strings {
     /**
      * @dev Converts a `int256` to its ASCII `string` decimal representation.
      */
-    function toString(int256 value) public pure returns (string memory) {
+    function toString(int256 value) internal pure returns (string memory) {
         return string(abi.encodePacked(value < 0 ? "-" : "",toString(SignedMath.abs(value))));       
     }
 
