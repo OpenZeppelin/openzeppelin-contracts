16fa1834e576764ed3901bafad6d9fc86661649d
Francisco Giordano <frangio.1@gmail.com>
2022-11-01 15:22:47 -0300

    Fix MerkleProof natspec
    (cherry picked from commit 7d01facdb8fc8f7b9f0584c6d9a655a3c44f23f6)

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -6,8 +6,8 @@ pragma solidity ^0.8.0;
 /**
  * @dev These functions deal with verification of Merkle Tree proofs.
  *
- * The proofs can be generated using our JavaScript library
- * https://github.com/OpenZeppelin/merkle-tree[`@openzeppelin/merkle-tree`].
+ * The tree and the proofs can be generated using our
+ * https://github.com/OpenZeppelin/merkle-tree[JavaScript library].
  * You will find a quickstart guide in the readme.
  *
  * WARNING: You should avoid using leaf values that are 64 bytes long prior to
