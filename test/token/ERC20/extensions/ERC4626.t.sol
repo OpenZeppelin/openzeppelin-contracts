2c18893393ca264c4fd1ceae98c18fe29cfda723
Daejun Park <daejunpark@gmail.com>
2022-11-02 10:27:37 -0700

    Revert "Update test/token/ERC20/extensions/ERC4626.t.sol"
    This reverts commit 99a220c6c7f4e708393f2f04f229ee68bc100b84.

--- test/token/ERC20/extensions/ERC4626.t.sol
+++ test/token/ERC20/extensions/ERC4626.t.sol
@@ -8,7 +8,7 @@ import {ERC4626Mock, IERC20Metadata} from "../../../../contracts/mocks/ERC4626Mo
 
 contract ERC4626StdTest is ERC4626Test {
     function setUp() public override {
-        _underlying_ = address(new ERC20Mock("MockERC20", "MockERC20", address(0), 0));
+        _underlying_ = address(new ERC20Mock("MockERC20", "MockERC20", address(this), 0));
         _vault_ = address(new ERC4626Mock(IERC20Metadata(_underlying_), "MockERC4626", "MockERC4626"));
         _delta_ = 0;
         _vaultMayBeEmpty = false;
