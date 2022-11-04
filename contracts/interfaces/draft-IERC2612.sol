d87dc42af8783550d7c0c52e423eab3297581ad1
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-11-04 10:31:17 +0100

    Apply suggestions from code review
    Co-authored-by: Francisco <frangio.1@gmail.com>

--- contracts/interfaces/draft-IERC2612.sol
+++ contracts/interfaces/draft-IERC2612.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// IERC2612 was marked as draft due to the EIP-2612 dependency.
 // EIP-2612 is Final as of 2022-11-01. This file is deprecated.
 
 import "./IERC2612.sol";
--- contracts/token/ERC20/extensions/draft-ERC20Permit.sol
+++ contracts/token/ERC20/extensions/draft-ERC20Permit.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-// IERC20Permit was marked as draft due to the EIP-2612 dependency.
 // EIP-2612 is Final as of 2022-11-01. This file is deprecated.
 
 import "./ERC20Permit.sol";
