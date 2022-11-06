5646b62df4fafc380fdda8d939a9b6648e3b0225
eugenioclrc <eugenioclrc@gmail.com>
2022-11-06 15:22:22 -0300

    run lint

--- test/access/Ownable.t.sol
+++ test/access/Ownable.t.sol
@@ -12,47 +12,47 @@ contract OwnableTest is Test {
     OwnableMock public ownable;
 
     function setUp() public {
-      vm.prank(_owner);
-      ownable = new OwnableMock();
+        vm.prank(_owner);
+        ownable = new OwnableMock();
     }
 
     function testHasOwner() public {
-      assertEq(ownable.owner(), _owner);
+        assertEq(ownable.owner(), _owner);
     }
 
     ///@dev changes owner after transfer
     function testTransferOwnership() public {
-      vm.prank(_owner);
-      // vm.expectEmit(true, true, false, true, address(ownable));
-      ownable.transferOwnership(_other);
-      
-      assertEq(ownable.owner(), _other);
+        vm.prank(_owner);
+        // vm.expectEmit(true, true, false, true, address(ownable));
+        ownable.transferOwnership(_other);
+
+        assertEq(ownable.owner(), _other);
     }
 
     /// @dev prevents non-owners from transferring
     function testTransferOwnershipNonOwner() public {
-      vm.prank(_other);
-      vm.expectRevert('Ownable: caller is not the owner');
-      ownable.transferOwnership(_owner);
+        vm.prank(_other);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ownable.transferOwnership(_owner);
     }
 
     /// @dev guards ownership against stuck state
     function testTransferCantStuck() public {
-      vm.prank(_owner);
-      vm.expectRevert('Ownable: new owner is the zero address');
-      ownable.transferOwnership(address(0));
+        vm.prank(_owner);
+        vm.expectRevert("Ownable: new owner is the zero address");
+        ownable.transferOwnership(address(0));
     }
 
     /// @dev loses owner after renouncement
     function testRenounceOwnership() public {
-      vm.prank(_other);
-      vm.expectRevert('Ownable: caller is not the owner');
-      ownable.renounceOwnership();
+        vm.prank(_other);
+        vm.expectRevert("Ownable: caller is not the owner");
+        ownable.renounceOwnership();
 
-      vm.prank(_owner);
-      // vm.expectEmit(true, true, false, true, address(ownable));
-      ownable.renounceOwnership();
+        vm.prank(_owner);
+        // vm.expectEmit(true, true, false, true, address(ownable));
+        ownable.renounceOwnership();
 
-      assertEq(ownable.owner(), address(0));      
+        assertEq(ownable.owner(), address(0));
     }
 }
