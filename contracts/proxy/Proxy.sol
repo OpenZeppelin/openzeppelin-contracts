b9d504c09b7a4bae90a86145006bc7ab52136807
wiasliaw <wiasliaw@protonmail.com>
2022-11-23 15:13:13 +0800

    perf(proxy): add memory safe

--- contracts/proxy/Proxy.sol
+++ contracts/proxy/Proxy.sol
@@ -20,6 +20,7 @@ abstract contract Proxy {
      * This function does not return to its internal call site, it will return directly to the external caller.
      */
     function _delegate(address implementation) internal virtual {
+        /// @solidity memory-safe-assembly
         assembly {
             // Copy msg.data. We take full control of memory in this inline assembly
             // block because it will not return to Solidity code. We overwrite the
