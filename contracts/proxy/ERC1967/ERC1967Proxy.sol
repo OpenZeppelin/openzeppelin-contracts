69b966218eeab5805fcf18bba274f21150988cf1
Francisco <frangio.1@gmail.com>
2022-11-25 13:09:43 -0300

    remove extra space

--- contracts/proxy/ERC1967/ERC1967Proxy.sol
+++ contracts/proxy/ERC1967/ERC1967Proxy.sol
@@ -29,7 +29,6 @@ contract ERC1967Proxy is Proxy, ERC1967Upgrade {
      * TIP: To get this value clients can read directly from the storage slot shown below (specified by EIP1967) using the
      * https://eth.wiki/json-rpc/API#eth_getstorageat[`eth_getStorageAt`] RPC call.
      * `0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc`
-     *
      */
     function _implementation() internal view virtual override returns (address impl) {
         return ERC1967Upgrade._getImplementation();
