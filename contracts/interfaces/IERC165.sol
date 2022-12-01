b6bc1889ca0face4d22182eb75c3469450bc4b1d
Must479 <67345599+must479@users.noreply.github.com>
2022-12-01 22:17:19 +0700

    Update IERC165.sol

--- contracts/interfaces/IERC165.sol
+++ contracts/interfaces/IERC165.sol
@@ -3,27 +3,23 @@
 
 pragma solidity ^0.8.0;
 
-import "./IERC165.sol";
-
 /**
- * @dev Implementation of the {IERC165} interface.
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
  *
- * Contracts that want to implement ERC165 should inherit from this contract and override {supportsInterface} to check
- * for the additional interface id that will be supported. For example:
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
  *
- * ```solidity
- * function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
- *     return interfaceId == type(MyInterface).interfaceId || super.supportsInterface(interfaceId);
- * }
- * ```
- *
- * Alternatively, {ERC165Storage} provides an easier to use but more expensive implementation.
+ * For an implementation, see {ERC165}.
  */
-abstract contract ERC165 is IERC165 {
+interface IERC165 {
     /**
-     * @dev See {IERC165-supportsInterface}.
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
      */
-    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
-        return interfaceId == type(IERC165).interfaceId;
-    }
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
 }
