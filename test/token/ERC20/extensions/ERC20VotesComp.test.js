206a2394481ec1af16d0e0acf216bbffedde405b
Hagen Hübel <hhuebel@itinance.com>
2022-12-05 21:25:53 +0100

    Remove unused variables in tests (#3852)

--- test/token/ERC20/extensions/ERC20VotesComp.test.js
+++ test/token/ERC20/extensions/ERC20VotesComp.test.js
@@ -2,7 +2,7 @@
 
 const { BN, constants, expectEvent, expectRevert, time } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
-const { MAX_UINT256, ZERO_ADDRESS, ZERO_BYTES32 } = constants;
+const { MAX_UINT256, ZERO_ADDRESS } = constants;
 
 const { fromRpcSig } = require('ethereumjs-util');
 const ethSigUtil = require('eth-sig-util');
@@ -20,7 +20,7 @@ const Delegation = [
 ];
 
 contract('ERC20VotesComp', function (accounts) {
-  const [ holder, recipient, holderDelegatee, recipientDelegatee, other1, other2 ] = accounts;
+  const [ holder, recipient, holderDelegatee, other1, other2 ] = accounts;
 
   const name = 'My Token';
   const symbol = 'MTKN';
