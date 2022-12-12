ed66c580397a2f87ee778bde58db918c42661f82
JulissaDantes <julissadcj@gmail.com>
2022-12-12 13:43:17 -0400

    update test

--- test/token/ERC20/ERC20.behavior.js
+++ test/token/ERC20/ERC20.behavior.js
@@ -158,8 +158,9 @@ function shouldBehaveLikeERC20 (errorPrefix, initialSupply, initialHolder, recip
         });
 
         it('reverts', async function () {
-          await expectRevert(this.token.transferFrom(
-            tokenOwner, to, amount, { from: spender }), `${errorPrefix}: transfer to the zero address`,
+          await expectRevert(
+            this.token.transferFrom(tokenOwner, to, amount, { from: spender }),
+            `${errorPrefix}: transfer to the zero address`,
           );
         });
       });
