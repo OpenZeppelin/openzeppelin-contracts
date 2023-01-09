5d9fc173745fd77566bd4ee2468fbbafd01ae9e8
Francisco Giordano <frangio.1@gmail.com>
2023-01-09 19:07:57 -0300

    lint

--- contracts/mocks/ERC20Mock.sol
+++ contracts/mocks/ERC20Mock.sol
@@ -23,28 +23,16 @@ contract ERC20Mock is ERC20 {
         _burn(account, amount);
     }
 
-    function transferInternal(
-        address from,
-        address to,
-        uint256 value
-    ) public {
+    function transferInternal(address from, address to, uint256 value) public {
         _transfer(from, to, value);
     }
 
-    function approveInternal(
-        address owner,
-        address spender,
-        uint256 value
-    ) public {
+    function approveInternal(address owner, address spender, uint256 value) public {
         _approve(owner, spender, value);
     }
 
     // Exposed for testing purposes
-    function update(
-        address from,
-        address to,
-        uint256 amount
-    ) public {
+    function update(address from, address to, uint256 amount) public {
         _update(from, to, amount);
     }
 }
