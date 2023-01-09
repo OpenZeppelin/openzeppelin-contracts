706dab4848e3e74118f930b6dda62d3447e8fa89
youssef houmazen <31320884+Chainmaven@users.noreply.github.com>
2023-01-09 12:30:49 +0100

    🌗 Support for old solidity versions and new solidity versions.
    There is a new version of solidity that I wish to use with Openzeppelin support.

--- contracts/security/ReentrancyGuard.sol
+++ contracts/security/ReentrancyGuard.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.8.0) (security/ReentrancyGuard.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.7.0 <0.9.0;
 
 /**
  * @dev Contract module that helps prevent reentrant calls to a function.
