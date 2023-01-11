eac65075eaf87d710612e694e84c2a873d125189
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-11 10:40:14 +0100

    fix lint

--- test/helpers/BNmath.js
+++ test/helpers/BNmath.js
@@ -1,9 +1,9 @@
 function sum (inputs) {
-    let acc = web3.utils.toBN(0);
-    for (const n of inputs) {
-        acc = acc.add(n);
-    }
-    return acc;
+  let acc = web3.utils.toBN(0);
+  for (const n of inputs) {
+    acc = acc.add(n);
+  }
+  return acc;
 }
 
 module.exports = {
