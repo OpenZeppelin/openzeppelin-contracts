66bd4a0117655387c62ff5446882a879c0a4c3c4
JulissaDantes <julissadcj@gmail.com>
2023-01-16 14:56:35 -0400

    update multicall contract

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -14,6 +14,7 @@ abstract contract Multicall {
     /**
      * @dev Receives and executes a batch of function calls on this contract.
      */
+    /// @custom:oz-upgrades-unsafe-allow delegatecall
     function multicall(bytes[] calldata data) external virtual returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
