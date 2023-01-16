4515398d18d766f4937f43373c7dd2ef316d1271
JulissaDantes <julissadcj@gmail.com>
2023-01-16 11:41:52 -0400

    Update docs/templates/helpers.js
    Co-authored-by: Francisco <frangio.1@gmail.com>

--- docs/templates/helpers.js
+++ docs/templates/helpers.js
@@ -9,7 +9,6 @@ module.exports['readme-path'] = opts => {
 module.exports.names = params => params.map(p => p.name).join(', ');
 
 module.exports['typed-params'] = params => {
-  console.log(params);
   if (params == undefined) return [];
   return params.map(p => `${p.type}${p.indexed ? ' indexed' : ''}${p.name ? ' ' + p.name : ''}`).join(', ');
 };
