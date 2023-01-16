16bdf8e6b97ae3f8d68ccadf8be73949cefdd274
ernestognw <ernestognw@gmail.com>
2023-01-16 11:34:19 -0600

    Improved RegExp for `format-changelog.js`

--- scripts/release/format-changelog.js
+++ scripts/release/format-changelog.js
@@ -6,40 +6,24 @@
 const fs = require('fs');
 const changelog = fs.readFileSync('CHANGELOG.md', 'utf8');
 
-const PR_NUMBER = `\\[#(\\d+)\\]`;
-const REPO = '[A-z|-]+';
-const GITHUB_USERNAME = `(?:[A-z\\d](?:[A-z\\d]|-(?=[A-z\\d])){0,38})`;
-const GITHUB_URL = `https://github\\.com/`;
-const REPO_URL = `${GITHUB_URL}${GITHUB_USERNAME}/${REPO}/`;
-const SHORT_SHA = '[0-9a-f]{7}';
-const FULL_SHA = '[0-9a-f]{40}';
-
 // Groups:
-//  - 1: Pull Request Number
-//  - 2: Pull Request URL
-//  - 3: Changeset entry
-const RELEASE_LINE_REGEX = new RegExp(
-  `- (?:${PR_NUMBER}\\((${REPO_URL}pull/\\d+)\\) )?\\[\`${SHORT_SHA}\`\\]\\(${REPO_URL}commit/${FULL_SHA}\\) Thanks \\[@${GITHUB_USERNAME}\\]\\(${GITHUB_URL}(?:apps/)?${GITHUB_USERNAME}\\)! - (.*)`,
-  'g',
-);
+//  - 1: Pull Request Number and URL
+//  - 2: Changeset entry
+const RELEASE_LINE_REGEX = /^- (\[#.*?\]\(.*?\))?.*?! - (.*)$/gm;
 
 // Captures vX.Y.Z or vX.Y.Z-rc.W
-const VERSION_TITLE_REGEX = /\n## (\d+\.\d+\.\d+(-rc\.\d+)?)\n/g;
+const VERSION_TITLE_REGEX = /^## (\d+\.\d+\.\d+(-rc\.\d+)?)$/gm;
 
 const formatted = changelog
   // Remove extra whitespace between items
-  .replace(/\n- (\[.*)/g, '- $1')
+  .replace(/^(- \[.*\n)\n(?=[-|#])/gm, '$1')
   // Format each release line
-  .replace(RELEASE_LINE_REGEX, (_, PRNumber, PRUrl, title) => {
-    const replaced = `- ${title}`;
-    if (PRNumber && PRUrl) return `${replaced} ([#${PRNumber}](${PRUrl}))`;
-    return replaced;
-  })
+  .replace(RELEASE_LINE_REGEX, (_, pr, entry) => (pr ? `- ${entry} (${pr})` : `- ${entry}`))
   // Add date to new version
-  .replace(VERSION_TITLE_REGEX, `\n## $1 (${new Date().toISOString().split('T')[0]})\n\n`)
+  .replace(VERSION_TITLE_REGEX, `\n## $1 (${new Date().toISOString().split('T')[0]})`)
   // Remove titles
-  .replace(/\n### Major Changes\n/g, '')
-  .replace(/\n### Minor Changes\n/g, '')
-  .replace(/\n### Patch Changes\n/g, '');
+  .replace(/^### Major Changes\n\n/gm, '')
+  .replace(/^### Minor Changes\n\n/gm, '')
+  .replace(/^### Patch Changes\n\n/gm, '');
 
 fs.writeFileSync('CHANGELOG.md', formatted);
