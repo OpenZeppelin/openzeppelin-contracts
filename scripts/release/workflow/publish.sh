8e860492a037a786eddde31132d6785c67f24383
ernestognw <ernestognw@gmail.com>
2023-01-16 16:38:41 -0600

    Use correct npm tags

--- scripts/release/workflow/publish.sh
+++ scripts/release/workflow/publish.sh
@@ -2,11 +2,46 @@
 
 set -o errexit
 
+package_name() {
+  echo $(node --print --eval "require('./package.json').name")
+}
+
+latest_npm_version() { 
+  echo $(npm info "$package_name" version)
+}
+
+package_json_version() {
+  echo $(node --print --eval "require('./package.json').name")
+}
+
+dist_tag() {
+  if [ $PRERELEASE = "true" ]; then
+    echo "next"
+  else
+    if [ `npx semver -r ">$package_json_version" $latest_npm_version` = "" ]; then
+      echo "latest"
+    else
+      # This is a patch for an older version
+      # npm can't publish without a tag
+      echo "tmp"
+    fi
+  fi
+}
+
 publish() {
   cd contracts
-  ## Intentionally escape \$ to avoid interpolation and to write the token to disk/
-  echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc 
-  npm publish $TARBALL
+
+  ## Intentionally escape $ to avoid interpolation and to write the token to disk
+  echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+  # Actual publish
+  npm publish $TARBALL --tag "$dist_tag"
+
+  if [ "$dist_tag" = "tmp" ]; then
+    # Remove tmp tag
+    npm dist-tag rm "$package_name" "$dist_tag"
+  fi
+
   cd ..
 }
 
