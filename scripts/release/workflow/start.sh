7054e6df4f5af778886a7ea8b9106c12f9b82e67
ernestognw <ernestognw@gmail.com>
2023-01-17 10:33:57 -0600

    Fix `start` script

--- scripts/release/workflow/start.sh
+++ scripts/release/workflow/start.sh
@@ -4,25 +4,19 @@ set -euo pipefail
 
 # Set changeset status location
 # This is needed because `changeset status --output` only works with relative routes
-CHANGESETS_STATUS_JSON="$(node -p -e "require('path').relative(__dirname, '$RUNNER_TEMP/status.json')")"
+CHANGESETS_STATUS_JSON="$(realpath --relative-to=. "$RUNNER_TEMP/status.json")"
 
 # Save changeset status to temp file
 npx changeset status --output="$CHANGESETS_STATUS_JSON"
 
-# Get the list of releases from the status
-RELEASES="$(node -p -e "require('$CHANGESETS_STATUS_JSON').releases")"
-
 # Defensive assertion. SHOULD NOT BE REACHED
-if [ "$(node -p -e "$RELEASES.length")" != 1 ]; then
+if [ "$(jq '.releases | length' "$CHANGESETS_STATUS_JSON")" != 1 ]; then
   echo "::error file=$CHANGESETS_STATUS_JSON::The status doesn't contain only 1 release"
   exit 1;
 fi;
 
-# Get the next version
-NEW_VERSION="$(node -p -e "($RELEASES)[0].newVersion")"
-
 # Create branch
-BRANCH_SUFFIX="$(echo $NEW_VERSION | awk -F'.' '{ print $1"."$2 }')"
+BRANCH_SUFFIX="$(jq -r '.releases[0].newVersion | gsub("\\.\\d+$"; "")')"
 RELEASE_BRANCH=release-v$BRANCH_SUFFIX
 git checkout -b $RELEASE_BRANCH
 
