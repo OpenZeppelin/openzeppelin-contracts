8f30a87e12ed65c7e3080ec014cc37c1608a75f9
Francisco Giordano <frangio.1@gmail.com>
2023-01-18 17:41:49 -0300

    move annotation into existing comment

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -13,8 +13,8 @@ import "./Address.sol";
 abstract contract Multicall {
     /**
      * @dev Receives and executes a batch of function calls on this contract.
+     * @custom:oz-upgrades-unsafe-allow-reachable delegatecall
      */
-    /// @custom:oz-upgrades-unsafe-allow-reachable delegatecall
     function multicall(bytes[] calldata data) external virtual returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
