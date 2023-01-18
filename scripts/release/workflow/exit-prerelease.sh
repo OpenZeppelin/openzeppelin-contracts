06b9cc83caa3fc260b54dba290f5df4cf5ab3992
ernestognw <ernestognw@gmail.com>
2023-01-18 10:38:53 -0600

    Make exit prerelease condition to be in `release-cycle.yml`

--- scripts/release/workflow/exit-prerelease.sh
+++ scripts/release/workflow/exit-prerelease.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-if [ $PRERELEASE != "false" ]; then
-  npx changeset pre exit rc
-  git add .
-  git commit -m "Exit release candidate"
-  git push --all origin
-fi
+npx changeset pre exit rc
+git add .
+git commit -m "Exit release candidate"
+git push --all origin
