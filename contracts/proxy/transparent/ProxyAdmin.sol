39a4b59e85e67fde8078fb91063616701577a322
sunn1day <0v4rl0r5@gmail.com>
2023-01-19 13:31:52 +0100

    better require check

--- contracts/proxy/transparent/ProxyAdmin.sol
+++ contracts/proxy/transparent/ProxyAdmin.sol
@@ -22,7 +22,7 @@ contract ProxyAdmin is Ownable {
         // We need to manually run the static call since the getter cannot be flagged as view
         // bytes4(keccak256("implementation()")) == 0x5c60da1b
         (bool success, bytes memory returndata) = address(proxy).staticcall(hex"5c60da1b");
-        require(success);
+        require(success && returndata.length == 32);
         return abi.decode(returndata, (address));
     }
 
@@ -37,7 +37,7 @@ contract ProxyAdmin is Ownable {
         // We need to manually run the static call since the getter cannot be flagged as view
         // bytes4(keccak256("admin()")) == 0xf851a440
         (bool success, bytes memory returndata) = address(proxy).staticcall(hex"f851a440");
-        require(success);
+        require(success && returndata.length == 32);
         return abi.decode(returndata, (address));
     }
 
