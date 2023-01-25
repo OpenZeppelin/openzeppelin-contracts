82403e15fcccfc96de7af0f5448f85a0765355b5
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-25 18:27:11 +0100

    remove extra assumption in the foundry tests

--- test/token/ERC20/extensions/ERC4626.t.sol
+++ test/token/ERC20/extensions/ERC4626.t.sol
@@ -15,22 +15,4 @@ contract ERC4626StdTest is ERC4626Test {
         _vaultMayBeEmpty = false;
         _unlimitedAmount = true;
     }
-
-    // solhint-disable-next-line func-name-mixedcase
-    function test_RT_mint_withdraw(ERC4626Test.Init memory init, uint256 shares) public override {
-        // There is an edge case where we currently behave different than the property tests,
-        // when all assets are lost to negative yield.
-
-        // Sum all initially deposited assets.
-        int256 initAssets = 0;
-        for (uint256 i = 0; i < init.share.length; i++) {
-            vm.assume(init.share[i] <= uint256(type(int256).max - initAssets));
-            initAssets += SafeCast.toInt256(init.share[i]);
-        }
-
-        // Reject tests where the yield loses all assets from the vault.
-        vm.assume(init.yield > -initAssets);
-
-        super.test_RT_mint_withdraw(init, shares);
-    }
 }
