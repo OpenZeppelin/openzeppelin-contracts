c17b32662662f3974b1455f15c7772c5dac46b6e
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-27 15:19:43 +0100

    add string and bytes support to the StorageSlots library

--- contracts/utils/StorageSlot.sol
+++ contracts/utils/StorageSlot.sol
@@ -27,7 +27,8 @@ pragma solidity ^0.8.0;
  * }
  * ```
  *
- * _Available since v4.1 for `address`, `bool`, `bytes32`, and `uint256`._
+ * _Available since v4.1 for `address`, `bool`, `bytes32` and `uint256`._
+ * _Available since v4.9 for `string` and `bytes`._
  */
 library StorageSlot {
     struct AddressSlot {
@@ -44,6 +45,14 @@ library StorageSlot {
 
     struct Uint256Slot {
         uint256 value;
+    }
+
+    struct StringSlot {
+        string value;
+    }
+
+    struct BytesSlot {
+        bytes value;
     }
 
     /**
@@ -83,6 +92,46 @@ library StorageSlot {
         /// @solidity memory-safe-assembly
         assembly {
             r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `StringSlot` with member `value` located at `slot`.
+     */
+    function getStringSlot(bytes32 slot) internal pure returns (StringSlot storage r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `StringSlot` representation of the string storage pointer `store`.
+     */
+    function getStringSlot(string storage store) internal pure returns (StringSlot storage r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            r.slot := store.slot
+        }
+    }
+
+    /**
+     * @dev Returns an `BytesSlot` with member `value` located at `slot`.
+     */
+    function getBytesSlot(bytes32 slot) internal pure returns (BytesSlot storage r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            r.slot := slot
+        }
+    }
+
+    /**
+     * @dev Returns an `BytesSlot` representation of the bytes storage pointer `store`.
+     */
+    function getBytesSlot(bytes storage store) internal pure returns (BytesSlot storage r) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            r.slot := store.slot
         }
     }
 }
