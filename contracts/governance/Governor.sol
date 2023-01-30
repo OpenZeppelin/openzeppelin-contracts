7c9d08f0de38d47e2d3fb6927324e5ac506ca93f
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-30 10:40:29 +0100

    fix

--- contracts/governance/Governor.sol
+++ contracts/governance/Governor.sol
@@ -39,7 +39,6 @@ abstract contract Governor is Context, ERC165, EIP712, IGovernor, IERC721Receive
         address proposer;
         bytes4 __gap_unused0;
         // --- start retyped from Timers.BlockNumber at offset 0x20 ---
-        // start retyped from Timers.BlockNumber at offset 0x20
         uint64 voteEnd;
         bytes24 __gap_unused1;
         // --- Remaining fields starting at offset 0x40 ---------------
