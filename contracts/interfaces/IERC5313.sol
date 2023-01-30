5cc449d4b5838c0e2086130256e76512e4bde8fc
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-30 14:37:13 +0100

    Add IERC5313.sol

---
+++ contracts/interfaces/IERC5313.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @title EIP-5313 Light Contract Ownership Standard
+interface IERC5313 {
+    /// @notice Get the address of the owner
+    /// @return The address of the owner
+    function owner() view external returns(address);
+}
