773c4c065fec689d41e08c3709984a8f73e16659
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-02-06 10:49:07 +0100

    Update contracts/governance/extensions/GovernorVotesQuorumFraction.sol
    Co-authored-by: Francisco <fg@frang.io>

--- contracts/governance/extensions/GovernorVotesQuorumFraction.sol
+++ contracts/governance/extensions/GovernorVotesQuorumFraction.sol
@@ -70,7 +70,7 @@ abstract contract GovernorVotesQuorumFraction is GovernorVotes {
     }
 
     /**
-     * @dev Returns the quorum for a block number, in terms of number of votes: `supply * numerator / denominator`.
+     * @dev Returns the quorum for a timepoint, in terms of number of votes: `supply * numerator / denominator`.
      */
     function quorum(uint256 timepoint) public view virtual override returns (uint256) {
         return (token.getPastTotalSupply(timepoint) * quorumNumerator(timepoint)) / quorumDenominator();
