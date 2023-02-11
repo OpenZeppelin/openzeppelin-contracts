23d02a208d60ca1a9b8f800db6a3929515cc4a74
Francisco Giordano <fg@frang.io>
2023-02-11 19:32:53 -0300

    lint

--- hardhat/env-artifacts.js
+++ hardhat/env-artifacts.js
@@ -3,7 +3,7 @@ const { HardhatError } = require('hardhat/internal/core/errors');
 extendEnvironment(env => {
   const artifactsRequire = env.artifacts.require;
 
-  env.artifacts.require = (name) => {
+  env.artifacts.require = name => {
     for (const suffix of ['UpgradeableWithInit', 'Upgradeable', '']) {
       try {
         return artifactsRequire(name + suffix);
