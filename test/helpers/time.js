d79c2ecb6d31f82650fe71fef08364c208a4abea
Francisco Giordano <fg@frang.io>
2023-02-14 17:53:49 -0300

    Fix flaky timestamp tests

--- test/helpers/time.js
+++ test/helpers/time.js
@@ -1,16 +1,16 @@
-const { time } = require('@openzeppelin/test-helpers');
+const helpers = require('@nomicfoundation/hardhat-network-helpers');
 
 module.exports = {
   clock: {
-    blocknumber: () => web3.eth.getBlock('latest').then(block => block.number),
-    timestamp: () => web3.eth.getBlock('latest').then(block => block.timestamp),
+    blocknumber: () => helpers.latestBlock(),
+    timestamp: () => helpers.time.latest(),
   },
   clockFromReceipt: {
     blocknumber: receipt => Promise.resolve(receipt.blockNumber),
     timestamp: receipt => web3.eth.getBlock(receipt.blockNumber).then(block => block.timestamp),
   },
   forward: {
-    blocknumber: time.advanceBlockTo,
-    timestamp: time.increaseTo,
+    blocknumber: helpers.mineUpTo,
+    timestamp: helpers.time.increaseTo,
   },
 };
