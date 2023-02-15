413d8b3cec22ee98ba5c118b5ac0b2f0296cffa9
ernestognw <ernestognw@gmail.com>
2023-02-14 20:33:59 -0600

    Moar fixes

--- scripts/release/workflow/integrity-check.sh
+++ scripts/release/workflow/integrity-check.sh
@@ -3,12 +3,15 @@
 set -euo pipefail
 
 CHECKSUMS="$RUNNER_TEMP/checksums.txt"
-PACKAGE="$RUNNER_TEMP/package"
 
 # Extract tarball content into a tmp directory
 tar xf "$TARBALL" -C "$RUNNER_TEMP"
 
-cd "$PACKAGE"
+# Move to tmp directory and rename to contracts 
+# so the shasum check matches the absolute paths
+cd "$RUNNER_TEMP"
+mv package contracts
+cd contracts
 
 # Checksum extracted package contracts
 find . -type f -name "*.sol" | xargs shasum > "$CHECKSUMS"
