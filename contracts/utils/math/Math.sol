3f5e31824b76026cbf761e89e9c2c43a84b822a2
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2023-02-16 15:13:34 +0100

    Comment on unchecked arithmetic in `Math.sol`

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -67,6 +67,8 @@ library Math {
 
             // Handle non-overflow cases, 256 by 256 division.
             if (prod1 == 0) {
+                // Will revert if denominator == 0. It is not possible to disable the check for division by zero using the unchecked block.
+                // See https://docs.soliditylang.org/en/latest/control-structures.html#checked-or-unchecked-arithmetic.
                 return prod0 / denominator;
             }
 
