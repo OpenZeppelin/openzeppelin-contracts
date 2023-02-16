d3526941c74c12cc6513a630c738ed9f7345c434
Ernesto García <ernestognw@gmail.com>
2023-02-16 17:24:31 -0600

    Update contracts/access/AccessControlAdminRules.sol
    Co-authored-by: Francisco <fg@frang.io>

--- contracts/access/AccessControlAdminRules.sol
+++ contracts/access/AccessControlAdminRules.sol
@@ -174,7 +174,7 @@ abstract contract AccessControlAdminRules is IAccessControlAdminRules, IERC5313,
     function revokeRole(
         bytes32 role,
         address account
-    ) public virtual override(IAccessControl, AccessControl) onlyRole(getRoleAdmin(role)) {
+    ) public virtual override(IAccessControl, AccessControl) {
         require(role != DEFAULT_ADMIN_ROLE, "AccessControl: can't directly revoke defaultAdmin role");
         super.revokeRole(role, account);
     }
