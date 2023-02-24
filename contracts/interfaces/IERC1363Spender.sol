3f3774c5df70a7d2399810872056bb5338a09b4c
Vittorio Minacori <vittoriominacori@gmail.com>
2023-02-24 22:28:37 +0100

    Improve ERC1363 documentation (#3993)
    Co-authored-by: Francisco <fg@frang.io>

--- contracts/interfaces/IERC1363Spender.sol
+++ contracts/interfaces/IERC1363Spender.sol
@@ -3,6 +3,10 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @dev Interface for any contract that wants to support {IERC1363-approveAndCall}
+ * from {ERC1363} token contracts.
+ */
 interface IERC1363Spender {
     /*
      * Note: the ERC-165 identifier for this interface is 0x7b04a2d0.
@@ -17,10 +21,9 @@ interface IERC1363Spender {
      * transaction being reverted.
      * Note: the token contract address is always the message sender.
      * @param owner address The address which called `approveAndCall` function
-     * @param value uint256 The amount of tokens to be spent
+     * @param amount uint256 The amount of tokens to be spent
      * @param data bytes Additional data with no specified format
-     * @return `bytes4(keccak256("onApprovalReceived(address,uint256,bytes)"))`
-     *  unless throwing
+     * @return `bytes4(keccak256("onApprovalReceived(address,uint256,bytes)"))`unless throwing
      */
-    function onApprovalReceived(address owner, uint256 value, bytes memory data) external returns (bytes4);
+    function onApprovalReceived(address owner, uint256 amount, bytes memory data) external returns (bytes4);
 }
