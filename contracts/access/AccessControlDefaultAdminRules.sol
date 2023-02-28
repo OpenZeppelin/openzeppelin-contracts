2727779113c3ed3bbdc8238338f5d27b331dd3a9
ernestognw <ernestognw@gmail.com>
2023-02-27 19:55:52 -0700

    Fix typo

--- contracts/access/AccessControlDefaultAdminRules.sol
+++ contracts/access/AccessControlDefaultAdminRules.sol
@@ -232,7 +232,7 @@ abstract contract AccessControlDefaultAdminRules is IAccessControlDefaultAdminRu
         // Schedules the current delay if the delay is increased.
         // Otherwise, wait the difference between current and new delay to guarantee the delay change schedule + a default admin change
         // is effectively the current delay. For example, if delay is reduced from 10 days to 3 days, it's needed to wait 7 days
-        // before starting the new 5 days delayed transfer suming up to 10 days, which is the current delay.
+        // before starting the new 3 days delayed transfer suming up to 10 days, which is the current delay.
         uint48 changeDelay = newDefaultAdminDelay >= currentDefaultAdminDelay
             ? currentDefaultAdminDelay
             : currentDefaultAdminDelay - newDefaultAdminDelay; // This is 1 in worst-case scenario. Can't be 0.
