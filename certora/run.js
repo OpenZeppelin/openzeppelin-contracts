3e8ac224e526227a5a1f8dec7ae51ee58d0fad53
Francisco Giordano <fg@frang.io>
2023-03-03 18:33:47 -0300

    refactor formal verification into single script

---
+++ certora/run.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+const MAX_PARALLEL = 4;
+
+const specs = require(__dirname + '/specs.json');
+
+const fs = require('fs');
+const proc = require('child_process');
+const { PassThrough } = require('stream');
+const events = require('events');
+const limit = require('p-limit')(MAX_PARALLEL);
+
+for (const [spec, { files, contract }] of Object.entries(specs)) {
+  limit(run, files, contract, spec);
+}
+
+// Run certora, aggregate the output and print it at the end
+async function run(files, contract, spec, args = []) {
+  const child = proc.spawn('echo', [
+    ...files,
+    '--verify',
+    `${contract}:${spec}`,
+    '--optimistic-loop',
+    ...args,
+  ]);
+  const stream = new PassThrough();
+  const output = collect(stream);
+  child.stdout.pipe(stream, { end: false });
+  child.stderr.pipe(stream, { end: false });
+  const [code, signal] = await events.once(child, 'exit');
+  if (code || signal) {
+    console.error(`Specification ${spec} exited with error ${code || signal}`);
+  }
+  stream.end();
+  console.log(await output);
+}
+
+// Collects stream data into a string
+async function collect(stream) {
+  const buffers = [];
+  for await (const data of stream) {
+    const buf = Buffer.isBuffer(data) ? data : Buffer.from(data);
+    buffers.push(buf);
+  }
+  return Buffer.concat(buffers).toString('utf8');
+}
+
--- certora/scripts/verifyAccessControl.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-certoraRun \
-    certora/harnesses/AccessControlHarness.sol \
-    --verify AccessControlHarness:certora/specs/AccessControl.spec \
-    --optimistic_loop \
-    $@
