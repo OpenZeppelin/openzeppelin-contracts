b1e0f080e53090c103d1b2e6c22705d22636fc2a
ernestognw <ernestognw@gmail.com>
2023-03-21 00:33:58 -0600

    Revert local coverage changes

--- .solcover.js
+++ .solcover.js
@@ -6,7 +6,6 @@ module.exports = {
   providerOptions: {
     default_balance_ether: '10000000000000000000000000',
   },
-  istanbulReporter: ['html'],
   mocha: {
     fgrep: '[skip-on-coverage]',
     invert: true,
