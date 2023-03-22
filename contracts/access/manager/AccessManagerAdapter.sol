6e3da6621d7f8e5ff893b73b90b41ba2a049d42b
Francisco Giordano <fg@frang.io>
2023-03-22 14:22:51 -0300

    remove unused import

--- contracts/access/manager/AccessManagerAdapter.sol
+++ contracts/access/manager/AccessManagerAdapter.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "./AccessManager.sol";
 import "./AccessManaged.sol";
-import "../../utils/Address.sol";
 
 /**
  * @dev This contract can be used to migrate existing {Ownable} or {AccessControl} contracts into an {AccessManager}
