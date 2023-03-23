8f239021c01458027a6c0f2dcd84413445f75251
Simon Perriard <simon.perriard@chainsecurity.com>
2023-03-23 12:02:47 +0100

    gas optimization

--- contracts/security/ReentrancyGuard.sol
+++ contracts/security/ReentrancyGuard.sol
@@ -32,7 +32,7 @@ abstract contract ReentrancyGuard {
     // transaction's gas, it is best to keep them low in cases like this one, to
     // increase the likelihood of the full refund coming into effect.
     uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
+    uint256 private constant _ENTERED = 0;
 
     uint256 private _status;
 
@@ -55,7 +55,7 @@ abstract contract ReentrancyGuard {
 
     function _nonReentrantBefore() private {
         // On the first call to nonReentrant, _status will be _NOT_ENTERED
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        require(_status > _ENTERED, "ReentrancyGuard: reentrant call");
 
         // Any calls to nonReentrant after this point will fail
         _status = _ENTERED;
