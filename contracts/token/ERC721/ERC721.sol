45149c43193902df9f88974a82fa27fb7e163539
ViharGandhi <97725643+ViharGandhi@users.noreply.github.com>
2023-03-29 09:30:43 +0530

    Update contracts/token/ERC721/ERC721.sol
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- contracts/token/ERC721/ERC721.sol
+++ contracts/token/ERC721/ERC721.sol
@@ -364,9 +364,10 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
      * Emits an {Approval} event.
      */
     function _approve(address to, uint256 tokenId) internal virtual {
+        address owner = ERC721.ownerOf(tokenId);
         require(to != owner, "ERC721: approval to current owner");
         _tokenApprovals[tokenId] = to;
-        emit Approval(ERC721.ownerOf(tokenId), to, tokenId);
+        emit Approval(owner, to, tokenId);
     }
 
     /**
