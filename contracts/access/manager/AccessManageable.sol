c21f44679cca243dd2ca95c247b47ca8055b0472
ernestognw <ernestognw@gmail.com>
2023-04-11 19:02:17 +0200

    Improve writing

--- contracts/access/manager/AccessManageable.sol
+++ contracts/access/manager/AccessManageable.sol
@@ -33,13 +33,13 @@ contract AccessManageable is Context {
      * 
      * [NOTE]
      * ====
-     * Although a selector collision is prevented by scoping permissions per contract, consider that an authorized selector
-     * may clash with unintended functions in the following cases:
+     * Although a selector collision is prevented by scoping permissions per contract, consider a selector with access granted
+     * to a group may clash with unintended functions in the following cases:
      * 
      * * If the https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function[`receive()`] function is restricted,
-     * any other function with a `bytes4(0)` selector will share permissions.
-     * * Similarly, if there's no `receive()` function but a `fallback()` instead, the fallback might be called with empy `calldata`, sharing
-     * the `bytes4(0)` selector permissons.
+     * any other function with a `bytes4(0)` selector will share permissions with `receive()`.
+     * * Similarly, if there's no `receive()` function but a `fallback()` instead, the fallback might be called with empty `calldata`, 
+     * sharing the `bytes4(0)` selector permissions as well.
      * * For any other selector, if the restricted function is set on an upgradeable contract, an upgrade may remove the restricted 
      * function and replace it with a new method whose selector replaces the last one, keeping the previous permissions.
      * ====
