a3598e4af0b866ad1631dcfa3f8b4a7dc12cc8dd
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-04-26 11:55:10 -0300

    Benchmark commit

--- contracts/token/ERC1155/ERC1155.sol
+++ contracts/token/ERC1155/ERC1155.sol
@@ -271,8 +271,9 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
      */
     function _mint(address to, uint256 id, uint256 amount, bytes memory data) internal {
         require(to != address(0), "ERC1155: mint to the zero address");
-        uint256[] memory ids = _asSingletonArray(id);
-        uint256[] memory amounts = _asSingletonArray(amount);
+        uint256[] memory ids;
+        uint256[] memory amounts;
+        (ids, amounts) = _asSingletonArrays(id, amount);
         _update(address(0), to, ids, amounts, data);
     }
 
@@ -304,8 +305,9 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
      */
     function _burn(address from, uint256 id, uint256 amount) internal {
         require(from != address(0), "ERC1155: burn from the zero address");
-        uint256[] memory ids = _asSingletonArray(id);
-        uint256[] memory amounts = _asSingletonArray(amount);
+        uint256[] memory ids;
+        uint256[] memory amounts;
+        (ids, amounts) = _asSingletonArrays(id, amount);
         _update(from, address(0), ids, amounts, "");
     }
 
@@ -376,13 +378,6 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
                 revert("ERC1155: transfer to non-ERC1155Receiver implementer");
             }
         }
-    }
-
-    function _asSingletonArray(uint256 element) private pure returns (uint256[] memory) {
-        uint256[] memory array = new uint256[](1);
-        array[0] = element;
-
-        return array;
     }
 
     function _asSingletonArrays(uint256 element1, uint256 element2) private pure returns (uint256[] memory, uint256[] memory) {
