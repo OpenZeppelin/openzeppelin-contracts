2c6cbff2edcf9665d81602d4e960b0d7cff6e48c
MollyHe5 <molly.cn.mail@gmail.com>
2023-05-02 16:11:45 -0400

    uncheck plus one operation of big integer to save gas

--- contracts/utils/cryptography/MerkleProof.sol
+++ contracts/utils/cryptography/MerkleProof.sol
@@ -47,8 +47,11 @@ library MerkleProof {
      */
     function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
         bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
+        for (uint256 i = 0; i < proof.length; ) {
             computedHash = _hashPair(computedHash, proof[i]);
+            unchecked {
+                i++;
+            }
         }
         return computedHash;
     }
@@ -60,8 +63,11 @@ library MerkleProof {
      */
     function processProofCalldata(bytes32[] calldata proof, bytes32 leaf) internal pure returns (bytes32) {
         bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
+        for (uint256 i = 0; i < proof.length; ) {
             computedHash = _hashPair(computedHash, proof[i]);
+            unchecked {
+                i++;
+            }
         }
         return computedHash;
     }
@@ -137,12 +143,15 @@ library MerkleProof {
         //   get the next hash.
         // - depending on the flag, either another value from the "main queue" (merging branches) or an element from the
         //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
+        for (uint256 i = 0; i < totalHashes; ) {
             bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
             bytes32 b = proofFlags[i]
                 ? (leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++])
                 : proof[proofPos++];
             hashes[i] = _hashPair(a, b);
+            unchecked {
+                i++;
+            }
         }
 
         if (totalHashes > 0) {
@@ -189,12 +198,15 @@ library MerkleProof {
         //   get the next hash.
         // - depending on the flag, either another value from the "main queue" (merging branches) or an element from the
         //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
+        for (uint256 i = 0; i < totalHashes; ) {
             bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
             bytes32 b = proofFlags[i]
                 ? (leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++])
                 : proof[proofPos++];
             hashes[i] = _hashPair(a, b);
+            unchecked {
+                i++;
+            }
         }
 
         if (totalHashes > 0) {
