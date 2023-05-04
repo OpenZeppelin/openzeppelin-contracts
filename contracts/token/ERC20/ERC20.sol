9ff11f26728b2c914ad703004c85fbfdca19a485
Francisco Giordano <fg@frang.io>
2023-05-04 16:56:34 -0300

    add code change

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -60,6 +60,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * @dev Returns the name of the token.
      */
     function name() public view virtual override returns (string memory) {
+        // testing
         return _name;
     }
 
