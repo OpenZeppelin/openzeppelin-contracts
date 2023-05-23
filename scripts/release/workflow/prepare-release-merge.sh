95ed609ff4d4674971b4f9a2ef98f597bce94923
Francisco <fg@frang.io>
2023-05-23 18:20:54 -0300

    Apply suggestions from code review
    Co-authored-by: Ernesto García <ernestognw@gmail.com>

--- scripts/release/workflow/prepare-release-merge.sh
+++ scripts/release/workflow/prepare-release-merge.sh
@@ -9,6 +9,7 @@ MERGE_BRANCH=merge/$GITHUB_REF_NAME
 git checkout -B "$MERGE_BRANCH" "$GITHUB_REF_NAME"
 
 # Get deleted changesets in this branch that might conflict with master
+# --diff-filter=D - Only deleted files
 readarray -t DELETED_CHANGESETS < <(git diff origin/master --diff-filter=D --name-only -- '.changeset/*.md')
 
 # Merge master, which will take those files cherry-picked. Auto-resolve conflicts favoring master.
@@ -18,6 +19,7 @@ git merge origin/master -m "Merge master to $GITHUB_REF_NAME" -X theirs || true
 # Remove the originally deleted changesets to correctly sync with master
 rm -f "${DELETED_CHANGESETS[@]}"
 
+# Only git add deleted files
 git ls-files --deleted .changeset/ | xargs git add
 
 # Allow empty here since there may be no changes if `rm -f` failed for all changesets
