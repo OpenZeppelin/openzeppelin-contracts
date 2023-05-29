7e32fe5906c0010b871551ced56d0483cd31c3dc
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-05-29 21:23:36 +0200

    remove Counters.sol

--- contracts/utils/Nonces.sol
+++ contracts/utils/Nonces.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./Counters.sol";
-
 /**
  * @dev Provides tracking nonces for addresses. Nonces will only increment.
  */
 abstract contract Nonces {
-    using Counters for Counters.Counter;
-
-    mapping(address => Counters.Counter) private _nonces;
+    mapping(address => uint256) private _nonces;
 
     /**
      * @dev Returns an address nonce.
      */
     function nonces(address owner) public view virtual returns (uint256) {
-        return _nonces[owner].current();
+        return _nonces[owner];
     }
 
     /**
@@ -23,9 +19,10 @@ abstract contract Nonces {
      *
      * Returns the current value and increments nonce.
      */
-    function _useNonce(address owner) internal virtual returns (uint256 current) {
-        Counters.Counter storage nonce = _nonces[owner];
-        current = nonce.current();
-        nonce.increment();
+    function _useNonce(address owner) internal virtual returns (uint256) {
+        unchecked {
+            // It is important to do x++ and not ++x here.
+            return _nonces[owner]++;
+        }
     }
 }
