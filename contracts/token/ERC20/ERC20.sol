4fd03c7ccdc90df450f2f5277c2129da7f6f73db
Alexander Herranz <alexherranz89@gmail.com>
2023-06-03 11:26:57 +0200

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -39,7 +39,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    uint256 private _totalSupply;
+    uint256 internal _totalSupply;
 
     string private _name;
     string private _symbol;
