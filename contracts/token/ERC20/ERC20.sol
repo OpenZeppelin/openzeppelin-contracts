b497b6668700340ee6378a2569b35d385a30353d
Alexander Herranz <alexherranz89@gmail.com>
2023-06-03 11:05:19 +0200

    Update to enable _transfer overriding when inheriting.
    Updated "_balances" mapping's visibility from private, to internal, in order to enable _transfer function overriding when inheriting. 
    Ie. to apply taxes, burning, etc. in each transfer.

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -35,7 +35,7 @@ import "../../utils/Context.sol";
  * allowances. See {IERC20-approve}.
  */
 contract ERC20 is Context, IERC20, IERC20Metadata {
-    mapping(address => uint256) private _balances;
+    mapping(address => uint256) internal _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
 
