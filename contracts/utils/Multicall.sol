93e3c9372d571c75a1e3950687a21ee2239ac842
Nazar Khatsko <58341930+nazarkhatsko@users.noreply.github.com>
2023-06-04 10:17:46 +0300

    Minor update Multicall.sol

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -15,8 +15,8 @@ abstract contract Multicall {
      * @dev Receives and executes a batch of function calls on this contract.
      * @custom:oz-upgrades-unsafe-allow-reachable delegatecall
      */
-    function multicall(bytes[] calldata data) external virtual returns (bytes[] memory results) {
-        results = new bytes[](data.length);
+    function multicall(bytes[] calldata data) external virtual returns (bytes[] memory) {
+        bytes[] memory results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
             results[i] = Address.functionDelegateCall(address(this), data[i]);
         }
