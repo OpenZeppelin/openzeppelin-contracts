d1663aa1f737e3acab17a5cf7ef80ec38a522984
balajipachai <balaji.pachai08@gmail.com>
2023-06-07 23:38:36 +0530

    removes context from contracts/metatx contracts

--- contracts/metatx/ERC2771Context.sol
+++ contracts/metatx/ERC2771Context.sol
@@ -3,12 +3,10 @@
 
 pragma solidity ^0.8.19;
 
-import "../utils/Context.sol";
-
 /**
  * @dev Context variant with ERC2771 support.
  */
-abstract contract ERC2771Context is Context {
+abstract contract ERC2771Context {
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
     address private immutable _trustedForwarder;
 
@@ -21,7 +19,7 @@ abstract contract ERC2771Context is Context {
         return forwarder == _trustedForwarder;
     }
 
-    function _msgSender() internal view virtual override returns (address sender) {
+    function _msgSender() internal view virtual returns (address sender) {
         if (isTrustedForwarder(msg.sender)) {
             // The assembly code is more direct than the Solidity version using `abi.decode`.
             /// @solidity memory-safe-assembly
@@ -29,15 +27,15 @@ abstract contract ERC2771Context is Context {
                 sender := shr(96, calldataload(sub(calldatasize(), 20)))
             }
         } else {
-            return super._msgSender();
+            return msg.sender;
         }
     }
 
-    function _msgData() internal view virtual override returns (bytes calldata) {
+    function _msgData() internal view virtual returns (bytes calldata) {
         if (isTrustedForwarder(msg.sender)) {
             return msg.data[:msg.data.length - 20];
         } else {
-            return super._msgData();
+            return msg.data;
         }
     }
 }
