55433e7134fb0ba5457997395d482f55877882f4
ernestognw <ernestognw@gmail.com>
2023-06-06 18:05:09 -0600

    Improved custom error matcher

--- test/helpers/customError.js
+++ test/helpers/customError.js
@@ -17,12 +17,30 @@ async function expectRevertCustomError(promise, expectedErrorName, args) {
         // Optimizations currently mess with Hardhat's decoding of custom errors
         expect(revert.message).to.include.oneOf([expectedErrorName, 'unrecognized return data or custom error']);
       } else {
+        // The revert message for custom errors looks like:
+        // VM Exception while processing transaction:
+        // reverted with custom error 'InvalidAccountNonce("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", 0)'
+
+        // We trim out anything inside the single quotes as comma-separated values
         const [, error] = revert.message.match(/'(.*)'/);
-        if (!/([A-Z])\w+\(.*\)/g.test(error)) {
+
+        // Attempt to parse as an error
+        if (!/\w+\(.*\)/g.test(error)) {
           expect.fail(`Couldn't parse "${error}" as a custom error`);
         }
+
+        // Extract the error name
         const [, errorName] = error.match(/(\w+)\(.*\)/);
-        const argMatches = [...error.replace(errorName, '').matchAll(/(0x[0-9A-Fa-f]+|-?\d+|\w+)/g)];
+
+        const argMatches = [
+          ...error
+            // Remove the error name and leave the parameters
+            .replace(errorName, '')
+            // Capture the remaining parameters
+            .matchAll(/(0x[0-9A-Fa-f]+|-?\d+|\w+)/g),
+        ];
+
+        // Assert error name
         expect(errorName).to.be.equal(
           expectedErrorName,
           `Unexpected custom error name (with found args: [${argMatches.map(([a]) => a)}])`,
@@ -31,6 +49,8 @@ async function expectRevertCustomError(promise, expectedErrorName, args) {
         // Coerce to string for comparison since `arg` can be either a number or hex.
         const sanitizedExpected = args.map(arg => arg.toString().toLowerCase());
         const sanitizedActual = argMatches.map(([arg]) => arg.toString().toLowerCase());
+
+        // Assert argument equality
         expect(sanitizedActual).to.have.members(sanitizedExpected, `Unexpected ${errorName} arguments`);
       }
     }
