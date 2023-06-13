59b854d56968a2d46acc56025ec23770f6c57b37
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-06-13 16:34:38 +0200

    fix lint

--- contracts/proxy/ERC1967/ERC1967Upgrade.sol
+++ contracts/proxy/ERC1967/ERC1967Upgrade.sol
@@ -41,6 +41,7 @@ library ERC1967Upgrade {
      * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1, and is
      * validated in the constructor.
      */
+    // solhint-disable-next-line private-vars-leading-underscore
     bytes32 internal constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     /**
@@ -131,6 +132,7 @@ library ERC1967Upgrade {
      * This is the keccak-256 hash of "eip1967.proxy.admin" subtracted by 1, and is
      * validated in the constructor.
      */
+    // solhint-disable-next-line private-vars-leading-underscore
     bytes32 internal constant ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
 
     /**
@@ -168,6 +170,7 @@ library ERC1967Upgrade {
      * @dev The storage slot of the UpgradeableBeacon contract which defines the implementation for this proxy.
      * This is bytes32(uint256(keccak256('eip1967.proxy.beacon')) - 1)) and is validated in the constructor.
      */
+    // solhint-disable-next-line private-vars-leading-underscore
     bytes32 internal constant BEACON_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
 
     /**
