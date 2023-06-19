c450ec754438166ca1c1083fa67143757d2c6e7a
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2023-06-19 14:56:03 +0200

    Update Ownable.sol
    Fix NatSpec comment regarding the initial owner

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -10,8 +10,8 @@ import "../utils/Context.sol";
  * there is an account (an owner) that can be granted exclusive access to
  * specific functions.
  *
- * By default, the owner account will be the one that deploys the contract. This
- * can later be changed with {transferOwnership}.
+ * The initial owner is set to the address provided by the deployer. This can
+ * later be changed with {transferOwnership}.
  *
  * This module is used through inheritance. It will make available the modifier
  * `onlyOwner`, which can be applied to your functions to restrict their use to
