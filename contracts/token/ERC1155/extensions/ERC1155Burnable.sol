42d3eea76cab5600503c27617290404ae659b9a2
ernestognw <ernestognw@gmail.com>
2023-06-22 15:51:53 -0600

    Rename `ERC1155InsufficientApprovalForAll` to `ERC1155MissingApprovalForAll`

--- contracts/token/ERC1155/extensions/ERC1155Burnable.sol
+++ contracts/token/ERC1155/extensions/ERC1155Burnable.sol
@@ -14,7 +14,7 @@ import "../ERC1155.sol";
 abstract contract ERC1155Burnable is ERC1155 {
     function burn(address account, uint256 id, uint256 value) public virtual {
         if (account != _msgSender() && !isApprovedForAll(account, _msgSender())) {
-            revert ERC1155InsufficientApprovalForAll(_msgSender(), account);
+            revert ERC1155MissingApprovalForAll(_msgSender(), account);
         }
 
         _burn(account, id, value);
@@ -22,7 +22,7 @@ abstract contract ERC1155Burnable is ERC1155 {
 
     function burnBatch(address account, uint256[] memory ids, uint256[] memory values) public virtual {
         if (account != _msgSender() && !isApprovedForAll(account, _msgSender())) {
-            revert ERC1155InsufficientApprovalForAll(_msgSender(), account);
+            revert ERC1155MissingApprovalForAll(_msgSender(), account);
         }
 
         _burnBatch(account, ids, values);
