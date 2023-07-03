1b597e37fced6efbc70131d4f232c4d191def48c
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-07-03 15:27:40 +0200

    up

--- contracts/governance/extensions/GovernorTimelock.sol
+++ contracts/governance/extensions/GovernorTimelock.sol
@@ -19,9 +19,8 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         uint32 delay;
     }
 
-    mapping(uint256 => ExecutionDetail[]) private _executionPlan;
+    mapping(uint256 => ExecutionDetail[]) private _executionDetails;
     mapping(uint256 => uint256) private _proposalEta;
-
 
     /**
      * @dev Overridden version of the {Governor-state} function with added support for the `Queued` and `Expired` state.
@@ -43,15 +42,13 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         return _proposalEta[proposalId];
     }
 
+    function proposalExecutionDetails(uint256 proposalId) public view virtual returns (ExecutionDetail[] memory) {
+        return _executionDetails[proposalId];
+    }
 
-
-
-
-
-
-
-
-
+    /**
+     * @dev See {IGovernor-propose}
+     */
     function propose(
         address[] memory targets,
         uint256[] memory values,
@@ -60,7 +57,7 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
     ) public virtual override(IGovernor, Governor) returns (uint256) {
         uint256 proposalId = super.propose(targets, values, calldatas, description);
 
-        ExecutionDetail[] storage details = _executionPlan[proposalId];
+        ExecutionDetail[] storage details = _executionDetails[proposalId];
         for (uint256 i = 0; i < targets.length; ++i) {
             details.push(_detectExecutionDetails(targets[i], bytes4(calldatas[i])));
         }
@@ -68,6 +65,12 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         return proposalId;
     }
 
+    /**
+     * @dev Function to queue a proposal to the timelock.
+     *
+     * NOTE: execution delay is estimated based on the delay information retreived in {proposal}. This value may be
+     * off if the delay were updated during the vote.
+     */
     function queue(
         address[] memory targets,
         uint256[] memory values,
@@ -87,13 +90,12 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
 
         uint32 setback = 0;
 
-        ExecutionDetail[] storage details = _executionPlan[proposalId];
+        ExecutionDetail[] memory details = _executionDetails[proposalId];
         for (uint256 i = 0; i < targets.length; ++i) {
-            ExecutionDetail memory detail = details[i];
-            if (detail.manager != address(0)) {
-                IAccessManager(detail.manager).schedule(targets[i], calldatas[i]);
+            if (details[i].manager != address(0)) {
+                IAccessManager(details[i].manager).schedule(targets[i], calldatas[i]);
             }
-            setback = uint32(Math.max(setback, detail.delay)); // cast is safe, both parameters are uint32
+            setback = uint32(Math.max(setback, details[i].delay)); // cast is safe, both parameters are uint32
         }
 
         uint256 eta = block.timestamp + setback;
@@ -102,6 +104,9 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         return eta;
     }
 
+    /**
+     * @dev See {IGovernor-_execute}
+     */
     function _execute(
         uint256 proposalId,
         address[] memory targets,
@@ -109,21 +114,23 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         bytes[] memory calldatas,
         bytes32 /*descriptionHash*/
     ) internal virtual override {
-        ExecutionDetail[] storage details = _executionPlan[proposalId];
+        ExecutionDetail[] memory details = _executionDetails[proposalId];
 
         for (uint256 i = 0; i < targets.length; ++i) {
-            ExecutionDetail memory detail = details[i];
-            if (detail.manager != address(0)) {
-                IAccessManager(detail.manager).relay{value: values[i]}(targets[i], calldatas[i]);
+            if (details[i].manager != address(0)) {
+                IAccessManager(details[i].manager).relay{value: values[i]}(targets[i], calldatas[i]);
             } else {
                 (bool success, bytes memory returndata) = targets[i].call{value: values[i]}(calldatas[i]);
                 Address.verifyCallResult(success, returndata);
             }
         }
 
-        delete _executionPlan[proposalId];
+        delete _executionDetails[proposalId];
     }
 
+    /**
+     * @dev See {IGovernor-_cancel}
+     */
     function _cancel(
         address[] memory targets,
         uint256[] memory values,
@@ -131,7 +138,7 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
         bytes32 descriptionHash
     ) internal virtual override returns (uint256) {
         uint256 proposalId = super._cancel(targets, values, calldatas, descriptionHash);
-        ExecutionDetail[] storage details = _executionPlan[proposalId];
+        ExecutionDetail[] storage details = _executionDetails[proposalId];
 
         for (uint256 i = 0; i < targets.length; ++i) {
             ExecutionDetail memory detail = details[i];
@@ -140,16 +147,14 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
             }
         }
 
-        delete _executionPlan[proposalId];
+        delete _executionDetails[proposalId];
 
         return proposalId;
     }
 
-
-
-
-
-
+    function _defaultDelay() internal view virtual returns (uint32) {
+        return 0;
+    }
 
     function _detectExecutionDetails(address target, bytes4 selector) private view returns (ExecutionDetail memory) {
         // If target is not a contract, skip
@@ -166,11 +171,5 @@ abstract contract GovernorTimelockCompound is IGovernorTimelock, Governor {
             } catch {}
         }
         return ExecutionDetail({ manager: address(0), delay: _defaultDelay() });
-    }
-
-
-
-    function _defaultDelay() internal view virtual returns (uint32) {
-        return 0;
     }
 }
