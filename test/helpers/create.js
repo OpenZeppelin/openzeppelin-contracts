0f479c644902ab5d28a7f76d11a56def67db0621
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-07-08 10:07:34 +0200

    Update create.js

--- test/helpers/create.js
+++ test/helpers/create.js
@@ -1,23 +1,18 @@
 const { rlp } = require('ethereumjs-util');
 
-function computeCreate2Address(saltHex, bytecode, deployer) {
-  return web3.utils.toChecksumAddress(
-    `0x${web3.utils
-      .sha3(`0x${['ff', deployer, saltHex, web3.utils.soliditySha3(bytecode)].map(x => x.replace(/0x/, '')).join('')}`)
-      .slice(-40)}`,
-  );
+function computeCreateAddress(creator, nonce) {
+  return web3.utils.toChecksumAddress(web3.utils.sha3(RLP.encode([deployer.address ?? deployer, nonce])).slice(-40));
 }
 
-function computeCreateAddress(creator, nonce) {
+function computeCreate2Address(saltHex, bytecode, deployer) {
   return web3.utils.toChecksumAddress(
     web3.utils
-      .sha3(rlp.encode([creator, nonce]))
-      .slice(12)
-      .substring(14),
+      .sha3(`0x${['ff', deployer, saltHex, web3.utils.soliditySha3(bytecode)].map(x => x.replace(/0x/, '')).join('')}`)
+      .slice(-40),
   );
 }
 
 module.exports = {
-  computeCreate2Address,
   computeCreateAddress,
+  computeCreate2Address,
 };
