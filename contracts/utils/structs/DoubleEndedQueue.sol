53ac10df9aaf4627fe502d0d8ba385371bfd03ed
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-07-26 17:44:57 +0200

    fix lint

--- contracts/utils/structs/DoubleEndedQueue.sol
+++ contracts/utils/structs/DoubleEndedQueue.sol
@@ -133,7 +133,7 @@ library DoubleEndedQueue {
      */
     function at(Bytes32Deque storage deque, uint256 index) internal view returns (bytes32 value) {
         if (index >= length(deque)) revert QueueOutOfBounds();
-				// by construction, length is a uint128, so the check enshures that index can safelly be casted to a uint128.
+        // by construction, length is a uint128, so the check enshures that index can safelly be casted to a uint128.
         unchecked {
             return deque._data[deque._begin + uint128(index)];
         }
