a1bc0fae292ce56854f89ad2e49f587598cfda68
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-07-28 14:59:58 +0200

    reduce changes & fix conflict

--- contracts/proxy/ERC1967/ERC1967Utils.sol
+++ contracts/proxy/ERC1967/ERC1967Utils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.0) (proxy/ERC1967/ERC1967Utils.sol)
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import {IBeacon} from "../beacon/IBeacon.sol";
 import {Address} from "../../utils/Address.sol";
