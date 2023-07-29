aed5720a0177ddd36e2923ecedb1759205ee6014
Ernesto García <ernestognw@gmail.com>
2023-07-28 18:58:30 -0600

    Avoid `returndatacopy` in ERC2771Forwarder by calling via assembly (#4458)

--- contracts/metatx/ERC2771Context.sol
+++ contracts/metatx/ERC2771Context.sol
@@ -7,6 +7,12 @@ import {Context} from "../utils/Context.sol";
 
 /**
  * @dev Context variant with ERC2771 support.
+ *
+ * WARNING: Avoid using this pattern in contracts that rely in a specific calldata length as they'll
+ * be affected by any forwarder whose `msg.data` is suffixed with the `from` address according to the ERC2771
+ * specification adding the address size in bytes (20) to the calldata size. An example of an unexpected
+ * behavior could be an unintended fallback (or another function) invocation while trying to invoke the `receive`
+ * function only accessible if `msg.data.length == 0`.
  */
 abstract contract ERC2771Context is Context {
     /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
