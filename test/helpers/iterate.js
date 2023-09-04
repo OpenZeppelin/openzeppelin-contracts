f37e440661a24ef63cfb68a86f97cdf22d00df95
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-09-04 17:27:27 +0200

    spelling

--- test/helpers/iterate.js
+++ test/helpers/iterate.js
@@ -5,7 +5,7 @@ const mapValues = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([k, v
 const max = (...values) => values.slice(1).reduce((x, y) => (x > y ? x : y), values[0]);
 const min = (...values) => values.slice(1).reduce((x, y) => (x < y ? x : y), values[0]);
 
-// Carthesian product of a list of arrays
+// Cartesian product of a list of arrays
 const product = (...arrays) => arrays.reduce((a, b) => a.flatMap(ai => b.map(bi => [...ai, bi])), [[]]);
 
 module.exports = {
