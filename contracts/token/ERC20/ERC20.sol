e1d2a608ce4a14000dcdcd89c6214230cd574502
Francisco <fg@frang.io>
2023-09-13 16:21:24 -0300

    Update contracts/token/ERC20/ERC20.sol
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -356,7 +356,7 @@ abstract contract ERC20 is Context, IERC20, IERC20Metadata, IERC20Errors {
      * Does not update the allowance value in case of infinite allowance.
      * Revert if not enough allowance is available.
      *
-     * Won't emit an {Approval} event.
+     * Does not emit an {Approval} event.
      */
     function _spendAllowance(address owner, address spender, uint256 value) internal virtual {
         uint256 currentAllowance = allowance(owner, spender);
