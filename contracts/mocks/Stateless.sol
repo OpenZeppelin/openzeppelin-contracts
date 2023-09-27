448d8f6d3b605b0c831497ef01fa3549c559695a
Francisco Giordano <fg@frang.io>
2023-09-27 19:55:21 -0300

    add Stateless.sol

---
+++ contracts/mocks/Stateless.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+// We keep these imports just to we can run the test suite after transpilation.
+
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
