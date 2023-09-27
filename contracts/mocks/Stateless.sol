7246dbb430dc1a5f4ce540b9df7da9d0c4960b29
Francisco Giordano <fg@frang.io>
2023-09-27 20:19:33 -0300

    add dummy contract in stateless file

--- contracts/mocks/Stateless.sol
+++ contracts/mocks/Stateless.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-// We keep these imports just to we can run the test suite after transpilation.
+// We keep these imports and a dummy contract just to we can run the test suite after transpilation.
+contract Dummy1234 {}
 
 import {Clones} from "../proxy/Clones.sol";
