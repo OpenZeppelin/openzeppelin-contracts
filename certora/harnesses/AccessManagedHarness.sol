7259e7f49f3e904065b93df14b7a104fe6e052a6
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-09-27 17:50:44 +0200

    fix the AccessManager test the ahcky way

--- certora/harnesses/AccessManagedHarness.sol
+++ certora/harnesses/AccessManagedHarness.sol
@@ -6,9 +6,20 @@ import "../patched/access/manager/IAccessManager.sol";
 import "../patched/access/manager/AccessManaged.sol";
 
 contract AccessManagedHarness is AccessManaged {
+    bytes internal SOME_FUNCTION_CALLDATA = abi.encodeCall(this.someFunction, ());
+
     constructor(address initialAuthority) AccessManaged(initialAuthority) {}
 
-    function someFunction() public restricted() {}
+    function someFunction() public restricted() {
+        // Sanity for FV: the msg.data when calling this function should be the same as the data used when checking
+        // the schedule. This is a reformulation of `msg.data == SOME_FUNCTION_CALLDATA` that focuses on the operation
+        // hash for this call.
+        require(
+            IAccessManager(authority()).hashOperation(_msgSender(), address(this), msg.data)
+            ==
+            IAccessManager(authority()).hashOperation(_msgSender(), address(this), SOME_FUNCTION_CALLDATA)
+        );
+    }
 
     function authority_canCall_1(address caller) public view returns (bool result) {
         (result,) = AuthorityUtils.canCallWithDelay(authority(), caller, address(this), this.someFunction.selector);
@@ -20,6 +31,6 @@ contract AccessManagedHarness is AccessManaged {
 
     function authority_getSchedule(address caller) public view returns (uint48) {
         IAccessManager manager = IAccessManager(authority());
-        return manager.getSchedule(manager.hashOperation(caller, address(this), abi.encodeCall(this.someFunction, ())));
+        return manager.getSchedule(manager.hashOperation(caller, address(this), SOME_FUNCTION_CALLDATA));
     }
 }
