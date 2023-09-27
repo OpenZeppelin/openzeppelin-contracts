b4a9a76318ca6651ea2957cef8941e6670549d3f
Francisco Giordano <fg@frang.io>
2023-09-27 00:26:18 -0300

    refactor prepare/prepack scripts

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -4,9 +4,24 @@ set -euo pipefail
 shopt -s globstar
 
 # cross platform `mkdir -p`
-node -e 'fs.mkdirSync("build/contracts", { recursive: true })'
+mkdirp() {
+  node -e "fs.mkdirSync('$1', { recursive: true })"
+}
 
+# cd to the root of the repo
+cd "$(git rev-parse --show-toplevel)"
+
+npm run clean
+
+env COMPILE_MODE=production npm run compile
+
+mkdirp build/contracts
 cp artifacts/contracts/**/*.json build/contracts
 rm build/contracts/*.dbg.json
 
 node scripts/remove-ignored-artifacts.js
+
+cp README.md contracts/
+
+mkdirp contracts/build/contracts
+cp -r build/contracts/*.json contracts/build/contracts
