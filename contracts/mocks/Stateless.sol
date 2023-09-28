e16c0ae42bb437c542fed1582b758a370febacf4
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-09-28 12:05:57 +0200

    mark some contracts as stateless

--- contracts/mocks/Stateless.sol
+++ contracts/mocks/Stateless.sol
@@ -4,30 +4,35 @@ pragma solidity ^0.8.20;
 
 // We keep these imports and a dummy contract just to we can run the test suite after transpilation.
 
-import {Time} from "../utils/types/Time.sol";
-import {Strings} from "../utils/Strings.sol";
-import {SafeCast} from "../utils/math/SafeCast.sol";
-import {EnumerableMap} from "../utils/structs/EnumerableMap.sol";
-import {BitMaps} from "../utils/structs/BitMaps.sol";
 import {Address} from "../utils/Address.sol";
-import {SignatureChecker} from "../utils/cryptography/SignatureChecker.sol";
-import {Clones} from "../proxy/Clones.sol";
-import {Checkpoints} from "../utils/structs/Checkpoints.sol";
-import {ECDSA} from "../utils/cryptography/ECDSA.sol";
-import {Create2} from "../utils/Create2.sol";
 import {Arrays} from "../utils/Arrays.sol";
-import {ShortStrings} from "../utils/ShortStrings.sol";
-import {MerkleProof} from "../utils/cryptography/MerkleProof.sol";
+import {AuthorityUtils} from "../access/manager/AuthorityUtils.sol";
+import {Base64} from "../utils/Base64.sol";
+import {BitMaps} from "../utils/structs/BitMaps.sol";
+import {Checkpoints} from "../utils/structs/Checkpoints.sol";
+import {Clones} from "../proxy/Clones.sol";
+import {Context} from "../utils/Context.sol";
+import {Create2} from "../utils/Create2.sol";
+import {DoubleEndedQueue} from "../utils/structs/DoubleEndedQueue.sol";
+import {ECDSA} from "../utils/cryptography/ECDSA.sol";
+import {ERC165} from "../utils/introspection/ERC165.sol";
+import {ERC721Holder} from "../tokens/ERC721/utils/ERC721Holder.sol";
+import {ERC1155Holder} from "../tokens/ERC1155/utils/ERC1155Holder.sol";
+import {EnumerableMap} from "../utils/structs/EnumerableMap.sol";
+import {EnumerableSet} from "../utils/structs/EnumerableSet.sol";
+import {ERC165Checker} from "../utils/introspection/ERC165Checker.sol";
 import {ERC1967Utils} from "../proxy/ERC1967/ERC1967Utils.sol";
 import {Math} from "../utils/math/Math.sol";
-import {DoubleEndedQueue} from "../utils/structs/DoubleEndedQueue.sol";
-import {StorageSlot} from "../utils/StorageSlot.sol";
-import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "../utils/cryptography/MerkleProof.sol";
 import {MessageHashUtils} from "../utils/cryptography/MessageHashUtils.sol";
-import {AuthorityUtils} from "../access/manager/AuthorityUtils.sol";
-import {ERC165Checker} from "../utils/introspection/ERC165Checker.sol";
-import {EnumerableSet} from "../utils/structs/EnumerableSet.sol";
+import {Multicall} from "../utils/Multicall.sol";
+import {SafeCast} from "../utils/math/SafeCast.sol";
+import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
+import {ShortStrings} from "../utils/ShortStrings.sol";
+import {SignatureChecker} from "../utils/cryptography/SignatureChecker.sol";
 import {SignedMath} from "../utils/math/SignedMath.sol";
-import {Base64} from "../utils/Base64.sol";
+import {StorageSlot} from "../utils/StorageSlot.sol";
+import {Strings} from "../utils/Strings.sol";
+import {Time} from "../utils/types/Time.sol";
 
 contract Dummy1234 {}
