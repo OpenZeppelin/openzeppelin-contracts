004408068fe33cefece56b2d7600a7201779a7de
Francisco Giordano <fg@frang.io>
2023-10-04 19:22:18 -0300

    Reset Hardhat Network before each test suite

--- hardhat/env-contract.js
+++ hardhat/env-contract.js
@@ -2,9 +2,23 @@ extendEnvironment(env => {
   const { contract } = env;
 
   env.contract = function (name, body) {
+    const { takeSnapshot } = require('@nomicfoundation/hardhat-network-helpers');
+
     // remove the default account from the accounts list used in tests, in order
     // to protect tests against accidentally passing due to the contract
     // deployer being used subsequently as function caller
-    contract(name, accounts => body(accounts.slice(1)));
+    contract(name, accounts => {
+      let snapshot;
+
+      before(async function () {
+        snapshot = await takeSnapshot();
+      });
+
+      after(async function () {
+        await snapshot.restore();
+      });
+
+      body(accounts.slice(1));
+    });
   };
 });
