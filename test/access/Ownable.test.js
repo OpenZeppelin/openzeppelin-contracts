673ff7984a2d88c861d07b73c342659f3dac7b27
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-10-05 13:43:04 +0200

    fix lint

--- test/access/Ownable.test.js
+++ test/access/Ownable.test.js
@@ -5,11 +5,11 @@ const { deploy, getFactory } = require('../helpers/deploy');
 
 async function fixture() {
   const accounts = await ethers.getSigners(); // this is slow :/
-  const owner    = accounts.shift();
-  const other    = accounts.shift();
-  const ownable  = await deploy('$Ownable', [ owner.address ]);
+  const owner = accounts.shift();
+  const other = accounts.shift();
+  const ownable = await deploy('$Ownable', [owner.address]);
   return { accounts, owner, other, ownable };
-};
+}
 
 describe('Ownable', function () {
   beforeEach(async function () {
@@ -21,9 +21,9 @@ describe('Ownable', function () {
     // we can get it from the contract factory
     const { interface } = await getFactory('$Ownable');
 
-    await expect(deploy('$Ownable', [ ethers.ZeroAddress ]))
-    .to.be.revertedWithCustomError({ interface }, 'OwnableInvalidOwner')
-    .withArgs(ethers.ZeroAddress);
+    await expect(deploy('$Ownable', [ethers.ZeroAddress]))
+      .to.be.revertedWithCustomError({ interface }, 'OwnableInvalidOwner')
+      .withArgs(ethers.ZeroAddress);
   });
 
   it('has an owner', async function () {
@@ -33,46 +33,46 @@ describe('Ownable', function () {
   describe('transfer ownership', function () {
     it('changes owner after transfer', async function () {
       await expect(this.ownable.connect(this.owner).transferOwnership(this.other.address))
-      .to.emit(this.ownable, 'OwnershipTransferred')
-      .withArgs(this.owner.address, this.other.address);
+        .to.emit(this.ownable, 'OwnershipTransferred')
+        .withArgs(this.owner.address, this.other.address);
 
       expect(await this.ownable.owner()).to.equal(this.other.address);
     });
 
     it('prevents non-owners from transferring', async function () {
       await expect(this.ownable.connect(this.other).transferOwnership(this.other.address))
-      .to.be.revertedWithCustomError(this.ownable, 'OwnableUnauthorizedAccount')
-      .withArgs(this.other.address);
+        .to.be.revertedWithCustomError(this.ownable, 'OwnableUnauthorizedAccount')
+        .withArgs(this.other.address);
     });
 
     it('guards ownership against stuck state', async function () {
       await expect(this.ownable.connect(this.owner).transferOwnership(ethers.ZeroAddress))
-      .to.be.revertedWithCustomError(this.ownable, 'OwnableInvalidOwner')
-      .withArgs(ethers.ZeroAddress);
+        .to.be.revertedWithCustomError(this.ownable, 'OwnableInvalidOwner')
+        .withArgs(ethers.ZeroAddress);
     });
   });
 
   describe('renounce ownership', function () {
     it('loses ownership after renouncement', async function () {
       await expect(this.ownable.connect(this.owner).renounceOwnership())
-      .to.emit(this.ownable, 'OwnershipTransferred')
-      .withArgs(this.owner.address, ethers.ZeroAddress);
+        .to.emit(this.ownable, 'OwnershipTransferred')
+        .withArgs(this.owner.address, ethers.ZeroAddress);
 
       expect(await this.ownable.owner()).to.equal(ethers.ZeroAddress);
     });
 
     it('prevents non-owners from renouncement', async function () {
       await expect(this.ownable.connect(this.other).renounceOwnership())
-      .to.be.revertedWithCustomError(this.ownable, 'OwnableUnauthorizedAccount')
-      .withArgs(this.other.address);
+        .to.be.revertedWithCustomError(this.ownable, 'OwnableUnauthorizedAccount')
+        .withArgs(this.other.address);
     });
 
     it('allows to recover access using the internal _transferOwnership', async function () {
       await this.ownable.connect(this.owner).renounceOwnership();
 
       await expect(this.ownable.$_transferOwnership(this.other.address))
-      .to.emit(this.ownable, 'OwnershipTransferred')
-      .withArgs(ethers.ZeroAddress, this.other.address);
+        .to.emit(this.ownable, 'OwnershipTransferred')
+        .withArgs(ethers.ZeroAddress, this.other.address);
 
       expect(await this.ownable.owner()).to.equal(this.other.address);
     });
--- test/helpers/deploy.js
+++ test/helpers/deploy.js
@@ -1,20 +1,25 @@
 const { ethers } = require('hardhat');
 
 async function getFactory(name, opts = {}) {
-    return ethers.getContractFactory(name).then(contract => contract.connect(opts.signer || contract.runner));
+  return ethers.getContractFactory(name).then(contract => contract.connect(opts.signer || contract.runner));
 }
 
 function attach(name, address, opts = {}) {
-    return getFactory(name, opts).then(factory => factory.attach(address));
+  return getFactory(name, opts).then(factory => factory.attach(address));
 }
 
 function deploy(name, args = [], opts = {}) {
-    if (!Array.isArray(args)) { opts = args; args = []; }
-    return getFactory(name, opts).then(factory => factory.deploy(...args)).then(contract => contract.waitForDeployment());
+  if (!Array.isArray(args)) {
+    opts = args;
+    args = [];
+  }
+  return getFactory(name, opts)
+    .then(factory => factory.deploy(...args))
+    .then(contract => contract.waitForDeployment());
 }
 
 module.exports = {
-    getFactory,
-    attach,
-    deploy,
+  getFactory,
+  attach,
+  deploy,
 };
