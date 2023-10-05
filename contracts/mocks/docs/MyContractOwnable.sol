bd25a0a26f452e475c8e3e30a94ad4bc4966a76e
Francisco <fg@frang.io>
2023-10-04 22:37:11 -0300

    Fix guides for 5.0 (#4654)
    Co-authored-by: Ernesto García <ernestognw@gmail.com>
    (cherry picked from commit e12511b53eb6ec99d86acec76b0923acfa5b8da2)

---
+++ contracts/mocks/docs/MyContractOwnable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Ownable} from "../../access/Ownable.sol";
+
+contract MyContract is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function normalThing() public {
+        // anyone can call this normalThing()
+    }
+
+    function specialThing() public onlyOwner {
+        // only the owner can call specialThing()!
+    }
+}
