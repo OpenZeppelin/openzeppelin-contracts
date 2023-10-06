0e6e84cb95032f70a6f04b5a494511942e5422ac
Vittorio Minacori <vittoriominacori@gmail.com>
2023-10-06 15:14:29 +0200

    Update pragma to 0.8.20

--- contracts/mocks/token/ERC1363ReceiverMock.sol
+++ contracts/mocks/token/ERC1363ReceiverMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {IERC1363Receiver} from "../../interfaces/IERC1363Receiver.sol";
 
--- contracts/mocks/token/ERC1363SpenderMock.sol
+++ contracts/mocks/token/ERC1363SpenderMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {IERC1363Spender} from "../../interfaces/IERC1363Spender.sol";
 
