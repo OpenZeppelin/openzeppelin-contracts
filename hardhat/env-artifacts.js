b24fec46deebe7086c1ff8719c39817deda567f8
ernestognw <ernestognw@gmail.com>
2023-10-10 13:37:50 -0600

    Fix upgradeable tests

--- hardhat/env-artifacts.js
+++ hardhat/env-artifacts.js
@@ -1,15 +1,11 @@
 const { HardhatError } = require('hardhat/internal/core/errors');
 
-// Modifies `artifacts.require(X)` so that instead of X it loads the XUpgradeable contract.
-// This allows us to run the same test suite on both the original and the transpiled and renamed Upgradeable contracts.
-
-extendEnvironment(env => {
-  const artifactsRequire = env.artifacts.require;
-
-  env.artifacts.require = name => {
+// Extends a require artifact function to try with the Upgradeable variants.
+function tryRequireUpgradableVariantsWith(originalRequire) {
+  return function (name) {
     for (const suffix of ['UpgradeableWithInit', 'Upgradeable', '']) {
       try {
-        return artifactsRequire(name + suffix);
+        return originalRequire.call(this, name + suffix);
       } catch (e) {
         // HH700: Artifact not found - from https://hardhat.org/hardhat-runner/docs/errors#HH700
         if (HardhatError.isHardhatError(e) && e.number === 700 && suffix !== '') {
@@ -21,4 +17,15 @@ extendEnvironment(env => {
     }
     throw new Error('Unreachable');
   };
+}
+
+// Modifies the artifact require functions so that instead of X it loads the XUpgradeable contract.
+// This allows us to run the same test suite on both the original and the transpiled and renamed Upgradeable contracts.
+extendEnvironment(env => {
+  for (const require of [
+    'require', // Truffle (Deprecated)
+    'readArtifact', // Ethers
+  ]) {
+    env.artifacts[require] = tryRequireUpgradableVariantsWith(env.artifacts[require]);
+  }
 });
