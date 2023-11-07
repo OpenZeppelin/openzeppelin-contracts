74016c376ad7c1a902a7bef6803090f68f2433fb
Ernesto García <ernestognw@gmail.com>
2023-11-07 16:35:58 +0000

    Update docs for Ownable2Step (#4721)
    Co-authored-by: Nicolás Venturo <nicolas.venturo@gmail.com>

--- contracts/access/Ownable2Step.sol
+++ contracts/access/Ownable2Step.sol
@@ -10,6 +10,12 @@ import {Ownable} from "./Ownable.sol";
  * there is an account (an owner) that can be granted exclusive access to
  * specific functions.
  *
+ * This extension of the {Ownable} contract includes a two-step mechanism to transfer
+ * ownership, where the new owner must call {acceptOwnership} in order to replace the
+ * old one. This can help prevent common mistakes, such as transfers of ownership to
+ * incorrect accounts, or to contracts that are unable to interact with the
+ * permission system.
+ *
  * The initial owner is specified at deployment time in the constructor for `Ownable`. This
  * can later be changed with {transferOwnership} and {acceptOwnership}.
  *
