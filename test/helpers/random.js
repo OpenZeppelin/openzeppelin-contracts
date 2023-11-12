d78ae17d576ea9619202932110484145a5850ae7
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-11-12 16:29:01 +0000

    Migrate 'arrays'

---
+++ test/helpers/random.js
@@ -0,0 +1,7 @@
+const { ethers } = require("hardhat");
+
+function randomHex(length) {
+    return `0x${Buffer.from(ethers.randomBytes(length)).toString('hex')}`
+}
+
+module.exports = { randomHex }
