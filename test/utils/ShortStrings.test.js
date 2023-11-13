c78e8f960b5e576f578e55ffe17c8fd67df3f9f2
Renan Souza <renan.rodrigues.souza1@gmail.com>
2023-11-13 13:36:49 +0000

    Migrate 'ShortString' (#12)

--- test/utils/ShortStrings.test.js
+++ test/utils/ShortStrings.test.js
@@ -1,7 +1,8 @@
+const { ethers } = require('hardhat');
 const { expect } = require('chai');
-const { expectRevertCustomError } = require('../helpers/customError');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
-const ShortStrings = artifacts.require('$ShortStrings');
+const ShortStrings = '$ShortStrings';
 
 function length(sstr) {
   return parseInt(sstr.slice(64), 16);
@@ -11,9 +12,14 @@ function decode(sstr) {
   return web3.utils.toUtf8(sstr).slice(0, length(sstr));
 }
 
-contract('ShortStrings', function () {
-  before(async function () {
-    this.mock = await ShortStrings.new();
+async function fixture() {
+  const mock = await ethers.deployContract(ShortStrings);
+  return { mock };
+}
+
+describe('ShortStrings', function () {
+  beforeEach(async function () {
+    Object.assign(this, await loadFixture(fixture));
   });
 
   for (const str of [0, 1, 16, 31, 32, 64, 1024].map(length => 'a'.repeat(length))) {
@@ -24,28 +30,31 @@ contract('ShortStrings', function () {
           expect(decode(encoded)).to.be.equal(str);
 
           const length = await this.mock.$byteLength(encoded);
-          expect(length.toNumber()).to.be.equal(str.length);
+          expect(length).to.be.equal(str.length);
 
           const decoded = await this.mock.$toString(encoded);
           expect(decoded).to.be.equal(str);
         } else {
-          await expectRevertCustomError(this.mock.$toShortString(str), 'StringTooLong', [str]);
+          await expect(this.mock.$toShortString(str))
+            .to.be.revertedWithCustomError(this.mock, 'StringTooLong')
+            .withArgs(str);
         }
       });
 
       it('set / get with fallback', async function () {
-        const { logs } = await this.mock.$toShortStringWithFallback(str, 0);
-        const { ret0 } = logs.find(({ event }) => event == 'return$toShortStringWithFallback').args;
+        const ret0 = await this.mock.$toShortStringWithFallback.staticCall(str, 0);
+        const tx = await this.mock.$toShortStringWithFallback(str, 0);
+        await expect(tx).to.emit(this.mock, 'return$toShortStringWithFallback').withArgs(ret0);
 
         const promise = this.mock.$toString(ret0);
         if (str.length < 32) {
           expect(await promise).to.be.equal(str);
         } else {
-          await expectRevertCustomError(promise, 'InvalidShortString', []);
+          await expect(promise).to.be.revertedWithCustomError(this.mock, 'InvalidShortString');
         }
 
         const length = await this.mock.$byteLengthWithFallback(ret0, 0);
-        expect(length.toNumber()).to.be.equal(str.length);
+        expect(length).to.be.equal(str.length);
 
         const recovered = await this.mock.$toStringWithFallback(ret0, 0);
         expect(recovered).to.be.equal(str);
