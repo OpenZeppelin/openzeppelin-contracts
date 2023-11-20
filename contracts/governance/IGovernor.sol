dc1988c3916e9c40de497a4f315d39b413646904
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-11-20 15:30:00 +0000

    Rename EIP6372 to ERC6372 in references

--- contracts/governance/IGovernor.sol
+++ contracts/governance/IGovernor.sol
@@ -254,7 +254,7 @@ interface IGovernor is IERC165, IERC6372 {
     /**
      * @notice module:user-config
      * @dev Delay, between the proposal is created and the vote starts. The unit this duration is expressed in depends
-     * on the clock (see EIP-6372) this contract uses.
+     * on the clock (see ERC-6372) this contract uses.
      *
      * This can be increased to leave time for users to buy voting power, or delegate it, before the voting of a
      * proposal starts.
@@ -267,7 +267,7 @@ interface IGovernor is IERC165, IERC6372 {
     /**
      * @notice module:user-config
      * @dev Delay between the vote start and vote end. The unit this duration is expressed in depends on the clock
-     * (see EIP-6372) this contract uses.
+     * (see ERC-6372) this contract uses.
      *
      * NOTE: The {votingDelay} can delay the start of the vote. This must be considered when setting the voting
      * duration compared to the voting delay.
