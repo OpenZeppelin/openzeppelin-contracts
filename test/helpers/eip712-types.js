8e2ee4bab41cb548b290f05234574d0653c807f4
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-11-22 09:24:37 +0100

    minor improvements

---
+++ test/helpers/eip712-types.js
@@ -0,0 +1,16 @@
+module.exports = {
+  EIP712Domain: [
+    { name: 'name', type: 'string' },
+    { name: 'version', type: 'string' },
+    { name: 'chainId', type: 'uint256' },
+    { name: 'verifyingContract', type: 'address' },
+    { name: 'salt', type: 'bytes32' },
+  ],
+  Permit: [
+    { name: 'owner', type: 'address' },
+    { name: 'spender', type: 'address' },
+    { name: 'value', type: 'uint256' },
+    { name: 'nonce', type: 'uint256' },
+    { name: 'deadline', type: 'uint256' },
+  ],
+};
