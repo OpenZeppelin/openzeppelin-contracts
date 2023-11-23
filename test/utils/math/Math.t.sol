e5fb718d4071b4be5e8dc980a3733616605c570a
carter-ya <liukefeng2008@hotmail.com>
2023-11-23 23:31:14 +0800

    Optimized gas costs in `ceilDiv` (#4553)

--- test/utils/math/Math.t.sol
+++ test/utils/math/Math.t.sol
@@ -16,10 +16,11 @@ contract MathTest is Test {
         if (result == 0) {
             assertEq(a, 0);
         } else {
-            uint256 maxdiv = UINT256_MAX / b;
-            bool overflow = maxdiv * b < a;
-            assertTrue(a > b * (result - 1));
-            assertTrue(overflow ? result == maxdiv + 1 : a <= b * result);
+            uint256 expect = a / b;
+            if (expect * b < a) {
+                expect += 1;
+            }
+            assertEq(result, expect);
         }
     }
 
