b3198c53f8bd10f56520026e4445088ddef64a58
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-11-29 15:48:33 +0100

    up

--- test/helpers/iterate.js
+++ test/helpers/iterate.js
@@ -2,9 +2,9 @@
 const mapValues = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]));
 
 // Array of number or bigint
-const max = (...values) => values.slice(1).reduce((x, y) => (x > y ? x : y), values[0]);
-const min = (...values) => values.slice(1).reduce((x, y) => (x < y ? x : y), values[0]);
-const sum = (...values) => values.slice(1).reduce((x, y) => x + y, values[0]);
+const max = (...values) => values.slice(1).reduce((x, y) => (x > y ? x : y), values.at(0));
+const min = (...values) => values.slice(1).reduce((x, y) => (x < y ? x : y), values.at(0));
+const sum = (...values) => values.slice(1).reduce((x, y) => x + y, values.at(0));
 
 // Cartesian product of a list of arrays
 const product = (...arrays) => arrays.reduce((a, b) => a.flatMap(ai => b.map(bi => [...ai, bi])), [[]]);
