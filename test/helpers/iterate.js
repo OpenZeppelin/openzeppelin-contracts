2a1841187ac486861765ec0bf5c5f3498f9ad2b0
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-11-29 15:47:01 +0100

    remove duplicate helpers

--- test/helpers/iterate.js
+++ test/helpers/iterate.js
@@ -4,6 +4,7 @@ const mapValues = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([k, v
 // Array of number or bigint
 const max = (...values) => values.slice(1).reduce((x, y) => (x > y ? x : y), values[0]);
 const min = (...values) => values.slice(1).reduce((x, y) => (x < y ? x : y), values[0]);
+const sum = (...values) => values.slice(1).reduce((x, y) => x + y, values[0]);
 
 // Cartesian product of a list of arrays
 const product = (...arrays) => arrays.reduce((a, b) => a.flatMap(ai => b.map(bi => [...ai, bi])), [[]]);
@@ -12,5 +13,6 @@ module.exports = {
   mapValues,
   max,
   min,
+  sum,
   product,
 };
--- test/helpers/math.js
+++ test/helpers/math.js
@@ -1,12 +1,10 @@
+const { min, max, sum } = require('./iterate');
+
 module.exports = {
-  // sum of integer / bignumber
-  sum: (...args) => args.reduce((acc, n) => acc + n, 0),
-  bigintSum: (...args) => args.reduce((acc, n) => acc + n, 0n),
+  // re-export min, max & sum of integer / bignumber
+  min,
+  max,
+  sum,
+  // deprecated: BN version of sum
   BNsum: (...args) => args.reduce((acc, n) => acc.add(n), web3.utils.toBN(0)),
-  // min of integer / bignumber
-  min: (...args) => args.slice(1).reduce((x, y) => (x < y ? x : y), args[0]),
-  BNmin: (...args) => args.slice(1).reduce((x, y) => (x.lt(y) ? x : y), args[0]),
-  // max of integer / bignumber
-  max: (...args) => args.slice(1).reduce((x, y) => (x > y ? x : y), args[0]),
-  BNmax: (...args) => args.slice(1).reduce((x, y) => (x.gt(y) ? x : y), args[0]),
 };
