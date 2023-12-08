88ac712e06832bce73b41e8166cded2729e25205
ernestognw <ernestognw@gmail.com>
2023-12-08 08:49:17 -0600

    Replace double `functionDelegateCall`

--- contracts/utils/Multicall.sol
+++ contracts/utils/Multicall.sol
@@ -32,7 +32,6 @@ abstract contract Multicall is Context {
 
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
-            results[i] = Address.functionDelegateCall(address(this), data[i]);
             results[i] = Address.functionDelegateCall(address(this), bytes.concat(data[i], context));
         }
         return results;
