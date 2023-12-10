c6ac69e3f4318bce177ac404f54f55f9027e9f1e
BCJdevelopment <bcj@bcjdevelopment.com>
2023-12-09 23:08:19 -0700

    removed context from ownable contracts

--- contracts/access/Ownable2Step.sol
+++ contracts/access/Ownable2Step.sol
@@ -56,7 +56,7 @@ abstract contract Ownable2Step is Ownable {
      * @dev The new owner accepts the ownership transfer.
      */
     function acceptOwnership() public virtual {
-        address sender = _msgSender();
+        address sender = msg.sender;
         if (pendingOwner() != sender) {
             revert OwnableUnauthorizedAccount(sender);
         }
