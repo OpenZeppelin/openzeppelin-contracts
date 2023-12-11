343b82299de04a1639d8a3dde12d0caad97301b0
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-12-11 13:16:31 +0100

    start erc721 test migration

--- test/token/ERC721/ERC721.test.js
+++ test/token/ERC721/ERC721.test.js
@@ -1,15 +1,23 @@
+const { ethers } = require('hardhat');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
 const { shouldBehaveLikeERC721, shouldBehaveLikeERC721Metadata } = require('./ERC721.behavior');
 
-const ERC721 = artifacts.require('$ERC721');
+const name = 'Non Fungible Token';
+const symbol = 'NFT';
 
-contract('ERC721', function (accounts) {
-  const name = 'Non Fungible Token';
-  const symbol = 'NFT';
+async function fixture() {
+  return {
+    accounts: await ethers.getSigners(),
+    token: await ethers.deployContract('$ERC721', [name, symbol]),
+  };
+}
 
+describe('ERC721', function () {
   beforeEach(async function () {
-    this.token = await ERC721.new(name, symbol);
+    Object.assign(this, await loadFixture(fixture));
   });
 
-  shouldBehaveLikeERC721(...accounts);
-  shouldBehaveLikeERC721Metadata(name, symbol, ...accounts);
+  shouldBehaveLikeERC721();
+  shouldBehaveLikeERC721Metadata(name, symbol);
 });
