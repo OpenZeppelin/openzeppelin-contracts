fa99e423b488d1646ea304640bca227e6920f0e0
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-12-11 17:16:21 +0100

    improve support interface behavior

--- test/helpers/methods.js
+++ test/helpers/methods.js
@@ -1,5 +1,16 @@
 const { ethers } = require('hardhat');
 
+const selector = signature => ethers.FunctionFragment.from(signature).selector;
+
+const interfaceId = signatures =>
+  ethers.toBeHex(
+    ethers.Interface.from(signatures.map(fn => `function ${fn}`))
+      .fragments.filter(f => f.type === 'function')
+      .reduce((acc, { selector }) => acc ^ ethers.toBigInt(selector), 0n),
+    4,
+  );
+
 module.exports = {
-  selector: signature => ethers.FunctionFragment.from(signature).selector,
+  selector,
+  interfaceId,
 };
