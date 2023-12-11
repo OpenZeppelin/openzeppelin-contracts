23080f3084f31f2f5ec0c7dcb73604563262b9ab
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-12-11 17:19:40 +0100

    up

--- test/helpers/methods.js
+++ test/helpers/methods.js
@@ -4,9 +4,7 @@ const selector = signature => ethers.FunctionFragment.from(signature).selector;
 
 const interfaceId = signatures =>
   ethers.toBeHex(
-    ethers.Interface.from(signatures.map(fn => `function ${fn}`))
-      .fragments.filter(f => f.type === 'function')
-      .reduce((acc, { selector }) => acc ^ ethers.toBigInt(selector), 0n),
+    signatures.reduce((acc, signature) => acc ^ ethers.toBigInt(selector(signature)), 0n),
     4,
   );
 
