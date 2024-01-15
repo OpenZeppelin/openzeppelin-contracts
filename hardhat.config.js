7b4b82de94ffb3cb492df8c1cb89a38bbeb1d6b5
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-15 09:29:43 +0100

    refactor hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,62 +11,67 @@ const fs = require('fs');
 const path = require('path');
 const proc = require('child_process');
 
-const argv = require('yargs/yargs')()
+const cmdOptions = {
+  // Compilation settings
+  compiler: {
+    alias: 'compileVersion',
+    type: 'string',
+    default: '0.8.20',
+  },
+  src: {
+    alias: 'source',
+    type: 'string',
+    default: 'contracts',
+  },
+  mode: {
+    alias: 'compileMode',
+    type: 'string',
+    choices: ['production', 'development'],
+    default: 'development',
+  },
+  ir: {
+    alias: 'enableIR',
+    type: 'boolean',
+    default: false,
+  },
+  // Extra modules
+  coverage: {
+    type: 'boolean',
+    default: false,
+  },
+  gas: {
+    alias: 'enableGasReport',
+    type: 'boolean',
+    default: false,
+  },
+  foundry: {
+    alias: 'hasFoundry',
+    type: 'boolean',
+    default: undefined, // default depends on other options
+  },
+  coinmarketcap: {
+    alias: 'coinmarketcapApiKey',
+    type: 'string',
+  },
+};
+
+const { argv } = require('yargs/yargs')()
   .env('')
-  .options({
-    // Compilation settings
-    compiler: {
-      alias: 'compileVersion',
-      type: 'string',
-      default: '0.8.20',
-    },
-    src: {
-      alias: 'source',
-      type: 'string',
-      default: undefined,
-    },
-    mode: {
-      alias: 'compileMode',
-      type: 'string',
-      choices: ['production', 'development'],
-      default: 'development',
-    },
-    ir: {
-      alias: 'enableIR',
-      type: 'boolean',
-      default: false,
-    },
-    // Extra modules
-    coverage: {
-      type: 'boolean',
-      default: false,
-    },
-    gas: {
-      alias: 'enableGasReport',
-      type: 'boolean',
-      default: false,
-    },
-    foundry: {
-      alias: 'hasFoundry',
-      type: 'boolean',
-      default: undefined, // default depends on other options
-    },
-    coinmarketcap: {
-      alias: 'coinmarketcapApiKey',
-      type: 'string',
-    },
-  })
-  .check(argv => {
-    if (argv.foundry && argv.src)
-      return 'Custom source is incompatible with Foundry. Disable with `FOUNDRY=false` in the environment';
-    if (argv.foundry && argv.coverage)
-      return 'Coverage analysis is incompatible with Foundry. Disable with `FOUNDRY=false` in the environment';
-    return true;
-  }).argv;
+  .options(cmdOptions)
+  .check(argv =>
+    argv.foundry && argv.src != cmdOptions.src.default
+      ? 'Custom source is incompatible with Foundry. Disable with `FOUNDRY=false` in the environment'
+      : true,
+  )
+  .check(argv =>
+    argv.foundry && argv.coverage
+      ? 'Coverage analysis is incompatible with Foundry. Disable with `FOUNDRY=false` in the environment'
+      : true,
+  );
 
 // if no value was specified for "foundry", and if "src" and "coverage" don't conflict with it,
 // then check if foundry is available, and enable it if that is the case.
-if (argv.foundry == undefined && !argv.src && !argv.coverage) {
+if (argv.foundry == cmdOptions.foundry.default && argv.src == cmdOptions.src.default && argv.coverage) {
   argv.foundry = hasFoundry();
 }
 
