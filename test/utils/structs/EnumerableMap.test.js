9677b6b755efff7cf68d2629edf5761cdf271566
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-18 11:33:50 +0100

    refactor

--- test/utils/structs/EnumerableMap.test.js
+++ test/utils/structs/EnumerableMap.test.js
@@ -7,14 +7,6 @@ const { TYPES, formatType } = require('../../../scripts/generate/templates/Enume
 
 const { shouldBehaveLikeMap } = require('./EnumerableMap.behavior');
 
-const getMethods = (mock, fnSigs) =>
-  mapValues(
-    fnSigs,
-    fnSig =>
-      (...args) =>
-        mock.getFunction(fnSig)(0, ...args),
-  );
-
 // Add Bytes32ToBytes32Map that must be tested but is not part of the generated types.
 TYPES.unshift(formatType('bytes32', 'bytes32'));
 
@@ -28,16 +20,21 @@ async function fixture() {
         keys: randomArray(generators[keyType]),
         values: randomArray(generators[valueType]),
         zeroValue: generators[valueType].zero,
-        methods: getMethods(mock, {
-          set: `$set(uint256,${keyType},${valueType})`,
-          get: `$get_EnumerableMap_${name}(uint256,${keyType})`,
-          tryGet: `$tryGet_EnumerableMap_${name}(uint256,${keyType})`,
-          remove: `$remove_EnumerableMap_${name}(uint256,${keyType})`,
-          length: `$length_EnumerableMap_${name}(uint256)`,
-          at: `$at_EnumerableMap_${name}(uint256,uint256)`,
-          contains: `$contains_EnumerableMap_${name}(uint256,${keyType})`,
-          keys: `$keys_EnumerableMap_${name}(uint256)`,
-        }),
+        methods: mapValues(
+          {
+            set: `$set(uint256,${keyType},${valueType})`,
+            get: `$get_EnumerableMap_${name}(uint256,${keyType})`,
+            tryGet: `$tryGet_EnumerableMap_${name}(uint256,${keyType})`,
+            remove: `$remove_EnumerableMap_${name}(uint256,${keyType})`,
+            length: `$length_EnumerableMap_${name}(uint256)`,
+            at: `$at_EnumerableMap_${name}(uint256,uint256)`,
+            contains: `$contains_EnumerableMap_${name}(uint256,${keyType})`,
+            keys: `$keys_EnumerableMap_${name}(uint256)`,
+          },
+          fnSig =>
+            (...args) =>
+              mock.getFunction(fnSig)(0, ...args),
+        ),
         events: {
           setReturn: `return$set_EnumerableMap_${name}_${keyType}_${valueType}`,
           removeReturn: `return$remove_EnumerableMap_${name}_${keyType}`,
