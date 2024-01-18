373e2474a523aec63ecf69fe50561a1618b48f96
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-18 11:27:58 +0100

    Add new enumerable map types

--- test/utils/structs/EnumerableMap.test.js
+++ test/utils/structs/EnumerableMap.test.js
@@ -7,60 +7,37 @@ const { TYPES, formatType } = require('../../../scripts/generate/templates/Enume
 
 const { shouldBehaveLikeMap } = require('./EnumerableMap.behavior');
 
-const getMethods = (mock, fnSigs) => {
-  return mapValues(
+const getMethods = (mock, fnSigs) =>
+  mapValues(
     fnSigs,
     fnSig =>
       (...args) =>
         mock.getFunction(fnSig)(0, ...args),
   );
-};
 
-const testTypes = [formatType('bytes32', 'bytes32'), ...TYPES];
+// Add Bytes32ToBytes32Map that must be tested but is not part of the generated types.
+TYPES.unshift(formatType('bytes32', 'bytes32'));
 
 async function fixture() {
   const mock = await ethers.deployContract('$EnumerableMap');
-
-  const zeroValue = {
-    uint256: 0n,
-    address: ethers.ZeroAddress,
-    bytes32: ethers.ZeroHash,
-  };
-
   const env = Object.fromEntries(
-    testTypes.map(({ name, keyType, valueType }) => [
+    TYPES.map(({ name, keyType, valueType }) => [
       name,
       {
         keyType,
         keys: randomArray(generators[keyType]),
         values: randomArray(generators[valueType]),
-
-        methods: getMethods(
-          mock,
-          testTypes.filter(t => keyType == t.keyType).length == 1
-            ? {
-                set: `$set(uint256,${keyType},${valueType})`,
-                get: `$get(uint256,${keyType})`,
-                tryGet: `$tryGet(uint256,${keyType})`,
-                remove: `$remove(uint256,${keyType})`,
-                length: `$length_EnumerableMap_${name}(uint256)`,
-                at: `$at_EnumerableMap_${name}(uint256,uint256)`,
-                contains: `$contains(uint256,${keyType})`,
-                keys: `$keys_EnumerableMap_${name}(uint256)`,
-              }
-            : {
-                set: `$set(uint256,${keyType},${valueType})`,
-                get: `$get_EnumerableMap_${name}(uint256,${keyType})`,
-                tryGet: `$tryGet_EnumerableMap_${name}(uint256,${keyType})`,
-                remove: `$remove_EnumerableMap_${name}(uint256,${keyType})`,
-                length: `$length_EnumerableMap_${name}(uint256)`,
-                at: `$at_EnumerableMap_${name}(uint256,uint256)`,
-                contains: `$contains_EnumerableMap_${name}(uint256,${keyType})`,
-                keys: `$keys_EnumerableMap_${name}(uint256)`,
-              },
-        ),
-
-        zeroValue: zeroValue[valueType],
+        zeroValue: generators[valueType].zero,
+        methods: getMethods(mock, {
+          set: `$set(uint256,${keyType},${valueType})`,
+          get: `$get_EnumerableMap_${name}(uint256,${keyType})`,
+          tryGet: `$tryGet_EnumerableMap_${name}(uint256,${keyType})`,
+          remove: `$remove_EnumerableMap_${name}(uint256,${keyType})`,
+          length: `$length_EnumerableMap_${name}(uint256)`,
+          at: `$at_EnumerableMap_${name}(uint256,uint256)`,
+          contains: `$contains_EnumerableMap_${name}(uint256,${keyType})`,
+          keys: `$keys_EnumerableMap_${name}(uint256)`,
+        }),
         events: {
           setReturn: `return$set_EnumerableMap_${name}_${keyType}_${valueType}`,
           removeReturn: `return$remove_EnumerableMap_${name}_${keyType}`,
@@ -78,7 +55,7 @@ describe('EnumerableMap', function () {
   });
 
   // UintToAddressMap
-  for (const { name } of testTypes) {
+  for (const { name } of TYPES) {
     describe(name, function () {
       beforeEach(async function () {
         Object.assign(this, this.env[name]);
