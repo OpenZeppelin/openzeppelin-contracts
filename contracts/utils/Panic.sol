d149ea683a1bf34557073b113e4860f12afe997c
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-02-01 17:20:17 +0100

    mark constants as internal

--- contracts/utils/Panic.sol
+++ contracts/utils/Panic.sol
@@ -6,15 +6,15 @@ pragma solidity ^0.8.20;
  * @dev Helper library for emitting standardized panic codes.
  */
 library Panic {
-    uint256 constant ASSERTION_ERROR = 0x1;
-    uint256 constant ARITHMETIC_UNDER_OR_OVERFLOW = 0x11;
-    uint256 constant DIVISION_BY_ZERO = 0x12;
-    uint256 constant ENUM_CONVERSION_OUT_OF_BOUNDS = 0x21;
-    uint256 constant INCORRECTLY_ENCODED_STORAGE_BYTE_ARRAY = 0x22;
-    uint256 constant POP_ON_EMPTY_ARRAY = 0x31;
-    uint256 constant ARRAY_ACCESS_OUT_OF_BOUNDS = 0x32;
-    uint256 constant TOO_MUCH_MEMORY_ALLOCATED = 0x41;
-    uint256 constant ZERO_INITIALIZED_VARIABLE = 0x51;
+    uint256 internal constant ASSERTION_ERROR = 0x1;
+    uint256 internal constant ARITHMETIC_UNDER_OR_OVERFLOW = 0x11;
+    uint256 internal constant DIVISION_BY_ZERO = 0x12;
+    uint256 internal constant ENUM_CONVERSION_OUT_OF_BOUNDS = 0x21;
+    uint256 internal constant INCORRECTLY_ENCODED_STORAGE_BYTE_ARRAY = 0x22;
+    uint256 internal constant POP_ON_EMPTY_ARRAY = 0x31;
+    uint256 internal constant ARRAY_ACCESS_OUT_OF_BOUNDS = 0x32;
+    uint256 internal constant TOO_MUCH_MEMORY_ALLOCATED = 0x41;
+    uint256 internal constant ZERO_INITIALIZED_VARIABLE = 0x51;
 
     function panic(uint256 code) internal pure {
         /// @solidity memory-safe-assembly
