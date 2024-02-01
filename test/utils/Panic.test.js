f3526812566ce8fd3cd70288308a450bf39c0cde
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-02-01 17:47:31 +0100

    add unit testing for Panic

---
+++ test/utils/Panic.test.js
@@ -0,0 +1,26 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { PANIC_CODES } = require('@nomicfoundation/hardhat-chai-matchers/panic');
+
+async function fixture() {
+  return { mock: await ethers.deployContract('$Panic') };
+}
+
+describe('Panic', function () {
+  beforeEach(async function () {
+    Object.assign(this, await loadFixture(fixture));
+  });
+
+  for (const [ name, code ] of Object.entries(PANIC_CODES)) {
+    describe(name, function () {
+      it(`exposes panic code as constant`, async function () {
+        expect(await this.mock.getFunction(`$${name}`)()).to.equal(code);
+      });
+
+      it(`throw panic code ${ethers.toBeHex(code)}`, async function () {
+        await expect(this.mock.$panic(code)).to.be.revertedWithPanic(code);
+      });
+    });
+  }
+});
