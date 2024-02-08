c7bb7d5109ec4a3fb43089b9d29de878b6aa9fad
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-02-08 11:32:28 +0100

    get build-info file through args

--- scripts/checks/artifacts-size.js
+++ scripts/checks/artifacts-size.js
@@ -1,50 +1,49 @@
 #!/usr/bin/env node
 
-const fs = require("fs")
-const path = require("path")
+const path = require('path');
+const { _: artifacts } = require('yargs').argv;
 
 // Constants from EIP-3860
 const MAX_CODE_SIZE = 24576;
 const MAX_INITCODE_SIZE = 2 * MAX_CODE_SIZE;
 
-// Folder containing compilation artifacts
-const BUILD_FOLDER = path.join(__dirname, "../../artifacts/build-info");
-
 // Get all compiled contracts from compilation artifacts
 const contracts = Object.fromEntries(
-    fs.readdirSync(BUILD_FOLDER)
-        .filter(buildFile => path.extname(buildFile) === '.json')
-        .map(buildFile => require(path.join(BUILD_FOLDER, buildFile)))
-        .flatMap(buildInfo => Object.entries(buildInfo.output.contracts)
-            .flatMap(([ file, contracts ]) => Object.entries(contracts)
-                .map(([ name, details ]) => [ `${file}:${name}`, Object.assign(details, { extra: { file, name, compilerSettings: buildInfo.input.settings }}) ])
-            )
-        )
+  artifacts
+    .map(artifact => require(path.resolve(__dirname, '../..', artifact)))
+    .flatMap(buildInfo =>
+      Object.entries(buildInfo.output.contracts).flatMap(([file, contracts]) =>
+        Object.entries(contracts).map(([name, details]) => [
+          `${file}:${name}`,
+          Object.assign(details, { extra: { file, name, compilerSettings: buildInfo.input.settings } }),
+        ]),
+      ),
+    ),
 );
 
-for (const [ id, details ] of Object.entries(contracts)) {
-    // Skip "exposed" contracts
-    if (details.extra.name.startsWith('$')) {
-        continue;
-    }
+for (const [id, details] of Object.entries(contracts)) {
+  // Skip "exposed" contracts
+  if (details.extra.name.startsWith('$')) {
+    continue;
+  }
 
-    // Skip (with warning) contracts that were not compiled with optimizations
-    if (!details.extra.compilerSettings.optimizer.enabled) {
-        console.warn(`[Warning] ${id} was compiled without optimizations. Skipped.`);
-        continue;
-    }
+  // Skip (with warning) contracts that were not compiled with optimizations
+  if (!details.extra.compilerSettings.optimizer.enabled) {
+    console.warn(`[Warning] ${id} was compiled without optimizations. Skipped.`);
+    continue;
+  }
 
-    // Check bytecode length is within limits
-    const { length: bytecodeLength } = Buffer.from(details.evm.bytecode.object, "hex");
-    if (bytecodeLength > MAX_INITCODE_SIZE) {
-        console.log("[bytecodeLength over limit]", id, bytecodeLength);
-        process.exitCode = 1;
-    }
+  // Check bytecode length is within limits
+  const { length: bytecodeLength } = Buffer.from(details.evm.bytecode.object, 'hex');
+  if (bytecodeLength > MAX_INITCODE_SIZE) {
+    console.log('[bytecodeLength over limit]', id, bytecodeLength);
+    process.exitCode = 1;
+  }
 
-    // Check deployed bytecode length is within limits
-    const { length: deployedBytecodeLength } = Buffer.from(details.evm.deployedBytecode.object, "hex");
-    if (deployedBytecodeLength > MAX_CODE_SIZE ) {
-        console.log("[deployedBytecode over limit]", id, deployedBytecodeLength);
-        process.exitCode = 1;
-    }
+  // Check deployed bytecode length is within limits
+  const { length: deployedBytecodeLength } = Buffer.from(details.evm.deployedBytecode.object, 'hex');
+  if (deployedBytecodeLength > MAX_CODE_SIZE) {
+    console.log('[deployedBytecode over limit]', id, deployedBytecodeLength);
+    process.exitCode = 1;
+  }
 }
