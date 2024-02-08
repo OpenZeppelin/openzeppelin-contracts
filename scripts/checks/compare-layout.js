3fb4cbc06f30833cc43e52db032e6fb0f54c5501
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-02-08 11:26:27 +0100

    check compiled contracts are within EIP-3860 limits

--- scripts/checks/compare-layout.js
+++ scripts/checks/compare-layout.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const { getStorageUpgradeReport } = require('@openzeppelin/upgrades-core/dist/storage');
 
--- scripts/checks/extract-layout.js
+++ scripts/checks/extract-layout.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const { findAll, astDereferencer, srcDecoder } = require('solidity-ast/utils');
 const { extractStorageLayout } = require('@openzeppelin/upgrades-core/dist/storage/extract');
