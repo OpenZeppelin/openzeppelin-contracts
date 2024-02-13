60a56ba9fa8a8103600ff3a633914e40d338bdda
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-02-13 20:55:02 +0100

    Deduplicate code in ./scripts/helpers and ./test/helpers/iterate

--- test/helpers/iterate.js
+++ test/helpers/iterate.js
@@ -1,15 +1,1 @@
-// Map values in an object
-const mapValues = (obj, fn) => Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]));
-
-// Cartesian product of a list of arrays
-const product = (...arrays) => arrays.reduce((a, b) => a.flatMap(ai => b.map(bi => [...ai, bi])), [[]]);
-const unique = (...array) => array.filter((obj, i) => array.indexOf(obj) === i);
-const zip = (...args) =>
-  Array.from({ length: Math.max(...args.map(array => array.length)) }, (_, i) => args.map(array => array[i]));
-
-module.exports = {
-  mapValues,
-  product,
-  unique,
-  zip,
-};
+module.exports = require('../../scripts/helpers');
