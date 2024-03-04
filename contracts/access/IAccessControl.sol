6dc3bbeed0b665eb15bd181f887d40ed583803ea
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-03-04 17:05:27 +0100

    fix lint

--- contracts/access/IAccessControl.sol
+++ contracts/access/IAccessControl.sol
@@ -30,7 +30,7 @@ interface IAccessControl {
     /**
      * @dev Emitted when `account` is granted `role`.
      *
-     * `sender` is the account that originated the contract call. This account bears the admin role (for the granted 
+     * `sender` is the account that originated the contract call. This account bears the admin role (for the granted
      * role), expect in cases where the role was granted using the internal {AccessControl-_grantRole}.
      */
     event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
