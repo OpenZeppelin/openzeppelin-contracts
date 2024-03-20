ea73fae8ffb11da17b23bfb53c44445e3ad20b51
ernestognw <ernestognw@gmail.com>
2024-03-20 14:08:43 +0000

    Replace imports

--- contracts/mocks/docs/utilities/Base64NFT.sol
+++ contracts/mocks/docs/utilities/Base64NFT.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.20;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+import {ERC721} from "../../../token/ERC721/ERC721.sol";
+import {Strings} from "../../../utils/Strings.sol";
+import {Base64} from "../../../utils/Base64.sol";
 
 contract Base64NFT is ERC721 {
     using Strings for uint256;
