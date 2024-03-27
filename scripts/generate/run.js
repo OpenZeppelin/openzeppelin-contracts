5e3ba29b088f77483d1284b69d5c5927593ab457
Renan Souza <renan.rodrigues.souza1@gmail.com>
2024-03-27 10:17:19 -0300

    Procedurally generate Arrays.sol (#4859)
    Co-authored-by: ernestognw <ernestognw@gmail.com>
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- scripts/generate/run.js
+++ scripts/generate/run.js
@@ -37,6 +37,7 @@ for (const [file, template] of Object.entries({
   'utils/structs/EnumerableMap.sol': './templates/EnumerableMap.js',
   'utils/structs/Checkpoints.sol': './templates/Checkpoints.js',
   'utils/StorageSlot.sol': './templates/StorageSlot.js',
+  'utils/Arrays.sol': './templates/Arrays.js',
 })) {
   generateFromTemplate(file, template, './contracts/');
 }
