0f45e02675c8d2ccb9ffd7dd88019fd37dc9f263
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2024-03-28 14:49:14 -0300

    Remove unnecessary return label

--- contracts/utils/math/Math.sol
+++ contracts/utils/math/Math.sol
@@ -93,14 +93,14 @@ library Math {
     /**
      * @dev Returns the largest of two numbers.
      */
-    function max(uint256 a, uint256 b) internal pure returns (uint256 result) {
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
         return choice(a > b, a, b);
     }
 
     /**
      * @dev Returns the smallest of two numbers.
      */
-    function min(uint256 a, uint256 b) internal pure returns (uint256 result) {
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return choice(a < b, a, b);
     }
 
