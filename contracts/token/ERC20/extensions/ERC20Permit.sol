a5cdda62e2ccf52da166a9f32561c8277d1f1e5f
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-04-02 12:41:30 +0300

    docs: update link to EIP-2612

--- contracts/token/ERC20/extensions/ERC20Permit.sol
+++ contracts/token/ERC20/extensions/ERC20Permit.sol
@@ -11,7 +11,7 @@ import {Nonces} from "../../../utils/Nonces.sol";
 
 /**
  * @dev Implementation of the ERC-20 Permit extension allowing approvals to be made via signatures, as defined in
- * https://eips.ethereum.org/EIPS/eip-2612[ERC-2612].
+ * https://eips.ethereum.org/EIPS/eip-2612.
  *
  * Adds the {permit} method, which can be used to change an account's ERC-20 allowance (see {IERC20-allowance}) by
  * presenting a message signed by the account. By not relying on `{IERC20-approve}`, the token holder account doesn't
