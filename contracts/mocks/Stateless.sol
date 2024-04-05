f8c2e1035e1cb7f511f7037df850a29c3a1f6955
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-04-05 14:59:18 +0200

    Add Packing library (#4992)
    Co-authored-by: ernestognw <ernestognw@gmail.com>

--- contracts/mocks/Stateless.sol
+++ contracts/mocks/Stateless.sol
@@ -24,6 +24,7 @@ import {ERC721Holder} from "../token/ERC721/utils/ERC721Holder.sol";
 import {Math} from "../utils/math/Math.sol";
 import {MerkleProof} from "../utils/cryptography/MerkleProof.sol";
 import {MessageHashUtils} from "../utils/cryptography/MessageHashUtils.sol";
+import {Packing} from "../utils/Packing.sol";
 import {SafeCast} from "../utils/math/SafeCast.sol";
 import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
 import {ShortStrings} from "../utils/ShortStrings.sol";
