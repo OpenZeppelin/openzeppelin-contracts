00e0eea76d5049f30b5b7be368e5c69c6d67a396
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-04-05 16:31:37 +0200

    reorder UserOperationUtils.sol

--- contracts/abstraction/UserOperationUtils.sol
+++ contracts/abstraction/UserOperationUtils.sol
@@ -9,61 +9,8 @@ import {Packing} from "../utils/Packing.sol";
 library UserOperationUtils {
     using Packing for *;
 
-    uint256 internal constant PAYMASTER_VALIDATION_GAS_OFFSET = 20;
-    uint256 internal constant PAYMASTER_POSTOP_GAS_OFFSET = 36;
-    uint256 internal constant PAYMASTER_DATA_OFFSET = 52;
-
-    // Need to fuzz this against `userOp.sender`
-    function getSender(PackedUserOperation calldata userOp) internal pure returns (address) {
-        address data;
-        assembly {
-            data := calldataload(userOp)
-        }
-        return address(uint160(data));
-    }
-
-    function getMaxPriorityFeePerGas(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return userOp.gasFees.asUint128x2().high();
-    }
-
-    function getMaxFeePerGas(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return userOp.gasFees.asUint128x2().low();
-    }
-
-    function getGasPrice(PackedUserOperation calldata userOp) internal view returns (uint256) {
-        unchecked {
-            (uint256 maxPriorityFeePerGas, uint256 maxFeePerGas) = userOp.gasFees.asUint128x2().split();
-            return
-                maxFeePerGas == maxPriorityFeePerGas
-                    ? maxFeePerGas
-                    : Math.min(maxFeePerGas, maxPriorityFeePerGas + block.basefee);
-        }
-    }
-
-    function getVerificationGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return userOp.accountGasLimits.asUint128x2().high();
-    }
-
-    function getCallGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return userOp.accountGasLimits.asUint128x2().low();
-    }
-
-    function getPaymasterVerificationGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return uint128(bytes16(userOp.paymasterAndData[PAYMASTER_VALIDATION_GAS_OFFSET:PAYMASTER_POSTOP_GAS_OFFSET]));
-    }
-
-    function getPostOpGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
-        return uint128(bytes16(userOp.paymasterAndData[PAYMASTER_POSTOP_GAS_OFFSET:PAYMASTER_DATA_OFFSET]));
-    }
-
-    function getPaymasterStaticFields(
-        bytes calldata paymasterAndData
-    ) internal pure returns (address paymaster, uint256 validationGasLimit, uint256 postOpGasLimit) {
-        return (
-            address(bytes20(paymasterAndData[:PAYMASTER_VALIDATION_GAS_OFFSET])),
-            uint128(bytes16(paymasterAndData[PAYMASTER_VALIDATION_GAS_OFFSET:PAYMASTER_POSTOP_GAS_OFFSET])),
-            uint128(bytes16(paymasterAndData[PAYMASTER_POSTOP_GAS_OFFSET:PAYMASTER_DATA_OFFSET]))
-        );
+    function hash(PackedUserOperation calldata userOp) internal pure returns (bytes32) {
+        return keccak256(encode(userOp));
     }
 
     function encode(PackedUserOperation calldata userOp) internal pure returns (bytes memory ret) {
@@ -71,20 +18,61 @@ library UserOperationUtils {
             abi.encode(
                 userOp.sender,
                 userOp.nonce,
-                calldataKeccak(userOp.initCode),
-                calldataKeccak(userOp.callData),
+                _keccak(userOp.initCode),
+                _keccak(userOp.callData),
                 userOp.accountGasLimits,
                 userOp.preVerificationGas,
                 userOp.gasFees,
-                calldataKeccak(userOp.paymasterAndData)
+                _keccak(userOp.paymasterAndData)
             );
     }
 
-    function hash(PackedUserOperation calldata userOp) internal pure returns (bytes32) {
-        return keccak256(encode(userOp));
+    // Need to fuzz this against `userOp.sender`
+    function sender(PackedUserOperation calldata userOp) internal pure returns (address) {
+        address data;
+        assembly {
+            data := calldataload(userOp)
+        }
+        return address(uint160(data));
     }
 
-    function calldataKeccak(bytes calldata data) private pure returns (bytes32 ret) {
+    function verificationGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return userOp.accountGasLimits.asUint128x2().first();
+    }
+
+    function callGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return userOp.accountGasLimits.asUint128x2().second();
+    }
+
+    function maxPriorityFeePerGas(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return userOp.gasFees.asUint128x2().first();
+    }
+
+    function maxFeePerGas(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return userOp.gasFees.asUint128x2().second();
+    }
+
+    function gasPrice(PackedUserOperation calldata userOp) internal view returns (uint256) {
+        unchecked {
+            // Following values are "per gas"
+            (uint256 maxPriorityFee, uint256 maxFee) = userOp.gasFees.asUint128x2().split();
+            return maxFee == maxPriorityFee ? maxFee : Math.min(maxFee, maxPriorityFee + block.basefee);
+        }
+    }
+
+    function paymaster(PackedUserOperation calldata userOp) internal pure returns (address) {
+        return address(bytes20(userOp.paymasterAndData[0:20]));
+    }
+
+    function paymasterVerificationGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return uint128(bytes16(userOp.paymasterAndData[20:36]));
+    }
+
+    function paymasterPostOpGasLimit(PackedUserOperation calldata userOp) internal pure returns (uint256) {
+        return uint128(bytes16(userOp.paymasterAndData[36:52]));
+    }
+
+    function _keccak(bytes calldata data) private pure returns (bytes32 ret) {
         assembly ("memory-safe") {
             let ptr := mload(0x40)
             let len := data.length
