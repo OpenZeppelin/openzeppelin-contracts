df954a976eef8e205b930a6e8fc25a9027bfedf4
Ernesto García <ernestognw@gmail.com>
2024-04-08 15:11:59 -0600

    Update scripts/upgradeable/transpile.sh
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- scripts/upgradeable/transpile.sh
+++ scripts/upgradeable/transpile.sh
@@ -38,7 +38,6 @@ npx @openzeppelin/upgrade-safe-transpiler -D \
   -x '!contracts/proxy/ERC1967/ERC1967Utils.sol' \
   -x '!contracts/proxy/utils/UUPSUpgradeable.sol' \
   -x '!contracts/proxy/beacon/IBeacon.sol' \
-  -p 'contracts/**/presets/**/*' \
   -p 'contracts/access/manager/AccessManager.sol' \
   -p 'contracts/finance/VestingWallet.sol' \
   -p 'contracts/governance/TimelockController.sol' \
