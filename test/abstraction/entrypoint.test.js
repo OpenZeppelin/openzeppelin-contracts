a129a45b7ccd0cf1eac47e88b2b9fad477d80b09
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-04-30 15:36:00 +0200

    improve tests

--- test/abstraction/entrypoint.test.js
+++ test/abstraction/entrypoint.test.js
@@ -133,15 +133,17 @@ describe('EntryPoint', function () {
   });
 
   describe('execute operation', function () {
-    describe('sender not reployed yet', function () {
-      it('success: deploy and call', async function () {
-        await this.user.sendTransaction({ to: this.sender, value: ethers.parseEther('1') });
+    beforeEach('fund account', async function () {
+      await this.user.sendTransaction({ to: this.sender, value: ethers.parseEther('1') });
+    });
 
+    describe('account not deployed yet', function () {
+      it('success: deploy and call', async function () {
         const operation = await this.sender
           .createOp({
             callData: this.sender.interface.encodeFunctionData('execute', [
               this.target.target,
-              42,
+              17,
               this.target.interface.encodeFunctionData('mockFunctionExtra'),
             ]),
           })
@@ -151,6 +153,28 @@ describe('EntryPoint', function () {
         await expect(this.entrypoint.handleOps([operation.packed], this.beneficiary))
           .to.emit(this.entrypoint, 'AccountDeployed')
           .withArgs(operation.hash, this.sender, this.factory, ethers.ZeroAddress)
+          .to.emit(this.target, 'MockFunctionCalledExtra')
+          .withArgs(this.sender, 17);
+      });
+    });
+
+    describe('account already deployed', function () {
+      beforeEach(async function () {
+        await this.sender.deploy();
+      });
+
+      it('success: deploy and call', async function () {
+        const operation = await this.sender
+          .createOp({
+            callData: this.sender.interface.encodeFunctionData('execute', [
+              this.target.target,
+              42,
+              this.target.interface.encodeFunctionData('mockFunctionExtra'),
+            ]),
+          })
+          .then(op => op.sign());
+
+        await expect(this.entrypoint.handleOps([operation.packed], this.beneficiary))
           .to.emit(this.target, 'MockFunctionCalledExtra')
           .withArgs(this.sender, 42);
       });
