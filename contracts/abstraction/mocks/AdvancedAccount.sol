612cadc1213c255f5147414b21b8dc68ce2ffdb1
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-21 10:13:52 +0200

    AccountCommon

--- contracts/abstraction/mocks/AdvancedAccount.sol
+++ contracts/abstraction/mocks/AdvancedAccount.sol
@@ -2,42 +2,36 @@
 
 pragma solidity ^0.8.20;
 
-import {PackedUserOperation, IEntryPoint} from "../../interfaces/IERC4337.sol";
+import {IEntryPoint} from "../../interfaces/IERC4337.sol";
 import {AccessControl} from "../../access/AccessControl.sol";
-import {ERC721Holder} from "../../token/ERC721/utils/ERC721Holder.sol";
 import {ERC1155Holder} from "../../token/ERC1155/utils/ERC1155Holder.sol";
-import {Address} from "../../utils/Address.sol";
 import {Account} from "../account/Account.sol";
+import {AccountCommon} from "../account/AccountCommon.sol";
 import {AccountMultisig} from "../account/modules/AccountMultisig.sol";
 import {AccountECDSA} from "../account/modules/AccountECDSA.sol";
 import {AccountP256} from "../account/modules/AccountP256.sol";
 
-abstract contract AdvancedAccount is AccountMultisig, AccessControl, ERC721Holder, ERC1155Holder {
+contract AdvancedAccountECDSA is AccessControl, AccountCommon, AccountECDSA, AccountMultisig {
     bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
-
-    IEntryPoint private immutable _entryPoint;
     uint256 private _requiredSignatures;
 
-    constructor(IEntryPoint entryPoint_, address admin_, address[] memory signers_, uint256 requiredSignatures_) {
+    constructor(
+        IEntryPoint entryPoint_,
+        address admin_,
+        address[] memory signers_,
+        uint256 requiredSignatures_
+    ) AccountCommon(entryPoint_) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin_);
         for (uint256 i = 0; i < signers_.length; ++i) {
             _grantRole(SIGNER_ROLE, signers_[i]);
         }
-
-        _entryPoint = entryPoint_;
         _requiredSignatures = requiredSignatures_;
     }
-
-    receive() external payable {}
 
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual override(AccessControl, ERC1155Holder) returns (bool) {
         return super.supportsInterface(interfaceId);
-    }
-
-    function entryPoint() public view virtual override returns (IEntryPoint) {
-        return _entryPoint;
     }
 
     function requiredSignatures() public view virtual override returns (uint256) {
@@ -48,38 +42,6 @@ abstract contract AdvancedAccount is AccountMultisig, AccessControl, ERC721Holde
         return hasRole(SIGNER_ROLE, user);
     }
 
-    function execute(address target, uint256 value, bytes calldata data) public virtual onlyEntryPoint {
-        _call(target, value, data);
-    }
-
-    function executeBatch(
-        address[] calldata targets,
-        uint256[] calldata values,
-        bytes[] calldata calldatas
-    ) public virtual onlyEntryPoint {
-        if (targets.length != calldatas.length || (values.length != 0 && values.length != targets.length)) {
-            revert AccountInvalidBatchLength();
-        }
-
-        for (uint256 i = 0; i < targets.length; ++i) {
-            _call(targets[i], (values.length == 0 ? 0 : values[i]), calldatas[i]);
-        }
-    }
-
-    function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory returndata) = target.call{value: value}(data);
-        Address.verifyCallResult(success, returndata);
-    }
-}
-
-contract AdvancedAccountECDSA is AdvancedAccount, AccountECDSA {
-    constructor(
-        IEntryPoint entryPoint_,
-        address admin_,
-        address[] memory signers_,
-        uint256 requiredSignatures_
-    ) AdvancedAccount(entryPoint_, admin_, signers_, requiredSignatures_) {}
-
     function _processSignature(
         bytes memory signature,
         bytes32 userOpHash
@@ -88,13 +50,36 @@ contract AdvancedAccountECDSA is AdvancedAccount, AccountECDSA {
     }
 }
 
-contract AdvancedAccountP256 is AdvancedAccount, AccountP256 {
+contract AdvancedAccountP256 is AccessControl, AccountCommon, AccountP256, AccountMultisig {
+    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+    uint256 private _requiredSignatures;
+
     constructor(
         IEntryPoint entryPoint_,
         address admin_,
         address[] memory signers_,
         uint256 requiredSignatures_
-    ) AdvancedAccount(entryPoint_, admin_, signers_, requiredSignatures_) {}
+    ) AccountCommon(entryPoint_) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin_);
+        for (uint256 i = 0; i < signers_.length; ++i) {
+            _grantRole(SIGNER_ROLE, signers_[i]);
+        }
+        _requiredSignatures = requiredSignatures_;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(AccessControl, ERC1155Holder) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function requiredSignatures() public view virtual override returns (uint256) {
+        return _requiredSignatures;
+    }
+
+    function _isAuthorized(address user) internal view virtual override returns (bool) {
+        return hasRole(SIGNER_ROLE, user);
+    }
 
     function _processSignature(
         bytes memory signature,
--- contracts/abstraction/mocks/SimpleAccount.sol
+++ contracts/abstraction/mocks/SimpleAccount.sol
@@ -2,69 +2,24 @@
 
 pragma solidity ^0.8.20;
 
-import {PackedUserOperation, IEntryPoint} from "../../interfaces/IERC4337.sol";
+import {IEntryPoint} from "../../interfaces/IERC4337.sol";
 import {Ownable} from "../../access/Ownable.sol";
-import {ERC721Holder} from "../../token/ERC721/utils/ERC721Holder.sol";
-import {ERC1155Holder} from "../../token/ERC1155/utils/ERC1155Holder.sol";
-import {Address} from "../../utils/Address.sol";
-import {Account} from "../account/Account.sol";
+import {AccountCommon} from "../account/AccountCommon.sol";
 import {AccountECDSA} from "../account/modules/AccountECDSA.sol";
 import {AccountP256} from "../account/modules/AccountP256.sol";
 
-abstract contract SimpleAccount is Account, Ownable, ERC721Holder, ERC1155Holder {
-    IEntryPoint private immutable _entryPoint;
-
-    error AccountUserRestricted();
-
-    modifier onlyOwnerOrEntryPoint() {
-        if (msg.sender != address(entryPoint()) && msg.sender != owner()) {
-            revert AccountUserRestricted();
-        }
-        _;
-    }
-
-    constructor(IEntryPoint entryPoint_, address owner_) Ownable(owner_) {
-        _entryPoint = entryPoint_;
-    }
-
-    receive() external payable {}
-
-    function entryPoint() public view virtual override returns (IEntryPoint) {
-        return _entryPoint;
-    }
+contract SimpleAccountECDSA is Ownable, AccountCommon, AccountECDSA {
+    constructor(IEntryPoint entryPoint_, address owner_) AccountCommon(entryPoint_) Ownable(owner_) {}
 
     function _isAuthorized(address user) internal view virtual override returns (bool) {
         return user == owner();
     }
-
-    function execute(address target, uint256 value, bytes calldata data) public virtual onlyOwnerOrEntryPoint {
-        _call(target, value, data);
-    }
-
-    function executeBatch(
-        address[] calldata targets,
-        uint256[] calldata values,
-        bytes[] calldata calldatas
-    ) public virtual onlyOwnerOrEntryPoint {
-        if (targets.length != calldatas.length || (values.length != 0 && values.length != targets.length)) {
-            revert AccountInvalidBatchLength();
-        }
-
-        for (uint256 i = 0; i < targets.length; ++i) {
-            _call(targets[i], (values.length == 0 ? 0 : values[i]), calldatas[i]);
-        }
-    }
-
-    function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory returndata) = target.call{value: value}(data);
-        Address.verifyCallResult(success, returndata);
-    }
 }
 
-contract SimpleAccountECDSA is SimpleAccount, AccountECDSA {
-    constructor(IEntryPoint entryPoint_, address owner_) SimpleAccount(entryPoint_, owner_) {}
-}
+contract SimpleAccountP256 is Ownable, AccountCommon, AccountP256 {
+    constructor(IEntryPoint entryPoint_, address owner_) AccountCommon(entryPoint_) Ownable(owner_) {}
 
-contract SimpleAccountP256 is SimpleAccount, AccountP256 {
-    constructor(IEntryPoint entryPoint_, address owner_) SimpleAccount(entryPoint_, owner_) {}
+    function _isAuthorized(address user) internal view virtual override returns (bool) {
+        return user == owner();
+    }
 }
