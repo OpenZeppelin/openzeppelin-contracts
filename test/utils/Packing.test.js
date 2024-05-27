dedb3894624f2b32b9f6a61237d95afae27dce23
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-27 15:20:50 +0200

    refactor tests

--- test/utils/Packing.test.js
+++ test/utils/Packing.test.js
@@ -18,32 +18,19 @@ describe('Packing', function () {
 
   describe('casting', function () {
     for (const { size, bytes, uint } of TYPES) {
-      const valueAsBytes = ethers.Typed[bytes](ethers.hexlify(ethers.randomBytes(size)));
-      const valueAsUint = ethers.Typed[uint](ethers.toBigInt(valueAsBytes.value));
+      const valueBytes = ethers.Typed[bytes](ethers.hexlify(ethers.randomBytes(size)));
+      const valueUint = ethers.Typed[uint](ethers.toBigInt(valueBytes.value));
 
       it(`Packed${capitalize(bytes)}`, async function () {
-        this.values = [
-          {
-            promise: this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAsBytes),
-            expected: valueAsBytes.value,
-          },
-          {
-            promise: this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAsUint),
-            expected: valueAsBytes.value,
-          },
-          { promise: this.mock.getFunction(`$as${capitalize(bytes)}`)(valueAsBytes), expected: valueAsBytes.value },
-          { promise: this.mock.getFunction(`$as${capitalize(uint)}`)(valueAsBytes), expected: valueAsUint.value },
-        ];
+        expect(await this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueBytes)).to.equal(valueBytes.value);
+        expect(await this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueUint)).to.equal(valueBytes.value);
+        expect(await this.mock.getFunction(`$as${capitalize(bytes)}`)(valueBytes)).to.equal(valueBytes.value);
+        expect(await this.mock.getFunction(`$as${capitalize(uint)}`)(valueBytes)).to.equal(valueUint.value);
 
         if (size == 20) {
-          const valueAsAddress = ethers.Typed.address(valueAsBytes.value);
-          this.values.push(
-            {
-              promise: this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAsAddress),
-              expected: valueAsBytes.value,
-            },
-            { promise: this.mock.getFunction('$asAddress')(valueAsBytes), expected: valueAsAddress.value },
-          );
+          const valueAddress = ethers.Typed.address(ethers.getAddress(valueBytes.value));
+          expect(await this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAddress)).to.equal(valueBytes.value);
+          expect(await this.mock.getFunction('$asAddress')(valueBytes)).to.equal(valueAddress.value);
         }
       });
     }
@@ -59,11 +46,9 @@ describe('Packing', function () {
       const packed = ethers.Typed[t3.bytes](ethers.concat([left.value, right.value]));
 
       it(`${t1.bytes} + ${t2.bytes} <> ${t3.bytes}`, async function () {
-        this.values = [
-          { promise: this.mock.getFunction('$pack')(left, right), expected: packed.value },
-          { promise: this.mock.getFunction(`$extract${t1.size}`)(packed, 0), expected: left.value },
-          { promise: this.mock.getFunction(`$extract${t2.size}`)(packed, t1.size), expected: right.value },
-        ];
+        expect(await this.mock.getFunction('$pack')(left, right)).to.equal(packed.value);
+        expect(await this.mock.getFunction(`$extract${t1.size}`)(packed, 0)).to.equal(left.value);
+        expect(await this.mock.getFunction(`$extract${t2.size}`)(packed, t1.size)).to.equal(right.value);
       });
 
       it(`out of range extraction`, async function () {
@@ -73,11 +58,5 @@ describe('Packing', function () {
         );
       });
     }
-  });
-
-  afterEach(async function () {
-    expect(await Promise.all((this.values ?? []).map(({ promise }) => promise))).to.deep.equal(
-      (this.values ?? []).map(({ expected }) => expected),
-    );
   });
 });
