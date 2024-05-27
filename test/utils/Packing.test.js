9f1c8910da5904b3fe69ace353739fa28bb2b7f2
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-27 13:50:47 +0200

    unit tests for coverage

--- test/utils/Packing.test.js
+++ test/utils/Packing.test.js
@@ -1,7 +1,11 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
 const { forceDeployCode } = require('../helpers/deploy');
+const { product } = require('../helpers/iterate');
+const { capitalize } = require('../helpers/strings');
+const { TYPES } = require('../../scripts/generate/templates/Packing.opts');
 
 async function fixture() {
   return { mock: await forceDeployCode('$Packing') };
@@ -12,18 +16,41 @@ describe('Packing', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  it('bytes16 x2 <=> bytes32', async function () {
-    const first = ethers.hexlify(ethers.randomBytes(16));
-    const second = ethers.hexlify(ethers.randomBytes(16));
-    const packed = ethers.hexlify(ethers.toBeArray((ethers.toBigInt(first) << 128n) | ethers.toBigInt(second)));
+  describe('casting', function () {
+    for (const { size, bytes, uint } of TYPES) {
+      it(`Packed${capitalize(bytes)}`, async function () {
+        const valueAsBytes = ethers.Typed[bytes](ethers.hexlify(ethers.randomBytes(size)));
+        const valueAsUint = ethers.Typed[uint](ethers.toBigInt(valueAsBytes.value));
 
-    expect(await this.mock.$asPackedBytes32(ethers.Typed.bytes32(packed))).to.equal(packed);
-    expect(await this.mock.$asPackedBytes32(ethers.Typed.uint256(packed))).to.equal(packed);
-    expect(await this.mock.$asBytes32(packed)).to.equal(packed);
-    expect(await this.mock.$asUint256(packed)).to.equal(packed);
+        expect(
+          await Promise.all([
+            this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAsBytes),
+            this.mock.getFunction(`$asPacked${capitalize(bytes)}`)(valueAsUint),
+            this.mock.getFunction(`$as${capitalize(bytes)}`)(valueAsBytes),
+            this.mock.getFunction(`$as${capitalize(uint)}`)(valueAsBytes),
+          ]),
+        ).to.deep.equal([valueAsBytes.value, valueAsBytes.value, valueAsBytes.value, valueAsUint.value]);
+      });
+    }
+  });
 
-    expect(await this.mock.$pack(ethers.Typed.bytes16(first), ethers.Typed.bytes16(second))).to.equal(packed);
-    expect(await this.mock.$extract16(ethers.Typed.bytes32(packed), 0x00)).to.deep.equal(first);
-    expect(await this.mock.$extract16(ethers.Typed.bytes32(packed), 0x10)).to.deep.equal(second);
+  describe('pack and extract', function () {
+    for (const [t1, t2] of product(TYPES, TYPES).filter(([t1, t2]) => t1.size + t2.size <= 32)) {
+      const t3 = TYPES.find(t => t.size == t1.size + t2.size);
+
+      it(`${t1.bytes} + ${t2.bytes} <> ${t3.bytes}`, async function () {
+        const left = ethers.Typed[t1.bytes](ethers.hexlify(ethers.randomBytes(t1.size)));
+        const right = ethers.Typed[t2.bytes](ethers.hexlify(ethers.randomBytes(t2.size)));
+        const packed = ethers.Typed[t3.bytes](ethers.concat([left.value, right.value]));
+
+        expect(
+          await Promise.all([
+            this.mock.getFunction('$pack')(left, right),
+            this.mock.getFunction(`$extract${t1.size}`)(packed, 0),
+            this.mock.getFunction(`$extract${t2.size}`)(packed, t1.size),
+          ]),
+        ).to.deep.equal([packed.value, left.value, right.value]);
+      });
+    }
   });
 });
