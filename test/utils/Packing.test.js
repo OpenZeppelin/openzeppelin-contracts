bb680060a8f20628a8d21f064d16a4aec02cf8d6
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-30 15:20:00 +0200

    unit testing / coverage

--- test/utils/Packing.test.js
+++ test/utils/Packing.test.js
@@ -15,25 +15,55 @@ describe('Packing', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  describe('pack and extract', function () {
-    for (const [s1, s2] of product(SIZES, SIZES)) {
-      if (!SIZES.includes(s1 + s2)) continue;
+  describe('pack', function () {
+    for (const [size1, size2] of product(SIZES, SIZES).filter(([size1, size2]) => SIZES.includes(size1 + size2))) {
+      const value1 = ethers.hexlify(ethers.randomBytes(size1));
+      const value2 = ethers.hexlify(ethers.randomBytes(size2));
+      const packed = ethers.concat([value1, value2]);
 
-      const left = ethers.Typed[`bytes${s1}`](ethers.hexlify(ethers.randomBytes(s1)));
-      const right = ethers.Typed[`bytes${s2}`](ethers.hexlify(ethers.randomBytes(s2)));
-      const packed = ethers.Typed[`bytes${s1 + s2}`](ethers.concat([left.value, right.value]));
-
-      it(`bytes${s1} + bytes${s2} <> bytes${s1 + s2}`, async function () {
-        expect(await this.mock.getFunction(`$pack_${s1}_${s2}`)(left, right)).to.equal(packed.value);
-        expect(await this.mock.getFunction(`$extract_${s1 + s2}_${s1}`)(packed, 0)).to.equal(left.value);
-        expect(await this.mock.getFunction(`$extract_${s1 + s2}_${s2}`)(packed, s1)).to.equal(right.value);
+      it(`pack bytes${size1} + bytes${size2} => bytes${size1 + size2}`, async function () {
+        expect(await this.mock[`$pack_${size1}_${size2}`](value1, value2)).to.equal(packed);
+        expect(await this.mock[`$extract_${size1 + size2}_${size1}`](packed, 0)).to.equal(value1);
+        expect(await this.mock[`$extract_${size1 + size2}_${size2}`](packed, size1)).to.equal(value2);
       });
+    }
+  });
 
-      it(`out of range extraction`, async function () {
-        await expect(this.mock.getFunction(`$extract_${s1 + s2}_${s2}`)(packed, s1 + 1)).to.be.revertedWithCustomError(
+  describe('extract / replace', function () {
+    for (const [size1, size2] of product(SIZES, SIZES).filter(([size1, size2]) => size1 > size2)) {
+      const MAX_OFFSET = size1 - size2;
+      const offset = ethers.toNumber(ethers.randomBytes(1)) % (MAX_OFFSET + 1);
+      const outer = ethers.randomBytes(size1);
+      const value = ethers.randomBytes(size2);
+
+      it(`extract bytes${size2} from bytes${size1}`, async function () {
+        expect(await this.mock[`$extract_${size1}_${size2}`](outer, offset)).to.equal(
+          ethers.hexlify(outer.slice(offset, offset + size2)),
+        );
+
+        await expect(this.mock[`$extract_${size1}_${size2}`](outer, MAX_OFFSET)).to.not.be.revertedWithCustomError(
           this.mock,
           'OutOfRangeAccess',
         );
+
+        await expect(this.mock[`$extract_${size1}_${size2}`](outer, MAX_OFFSET + 1)).to.be.revertedWithCustomError(
+          this.mock,
+          'OutOfRangeAccess',
+        );
+      });
+
+      it(`replace bytes${size2} from bytes${size1}`, async function () {
+        expect(await this.mock[`$replace_${size1}_${size2}`](outer, value, offset)).to.equal(
+          ethers.concat([outer.slice(0, offset), value, outer.slice(offset + size2)]),
+        );
+
+        await expect(
+          this.mock[`$replace_${size1}_${size2}`](outer, value, MAX_OFFSET),
+        ).to.not.be.revertedWithCustomError(this.mock, 'OutOfRangeAccess');
+
+        await expect(
+          this.mock[`$replace_${size1}_${size2}`](outer, value, MAX_OFFSET + 1),
+        ).to.be.revertedWithCustomError(this.mock, 'OutOfRangeAccess');
       });
     }
   });
