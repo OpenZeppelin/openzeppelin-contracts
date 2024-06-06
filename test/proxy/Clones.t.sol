101331b801a29daf7960c616b7eebb50356eb1c4
ernestognw <ernestognw@gmail.com>
2024-06-06 11:44:01 -0600

    Disable func-name-mixedcase

--- test/proxy/Clones.t.sol
+++ test/proxy/Clones.t.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.20;
 
+// solhint-disable func-name-mixedcase
+
 import {Test, console2} from "forge-std/Test.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 
