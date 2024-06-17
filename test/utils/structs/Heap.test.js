df82b15633358e4f47b2a7d59a36eb929c8fc2c7
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-06-17 20:56:37 +0200

    testing

--- test/utils/structs/Heap.test.js
+++ test/utils/structs/Heap.test.js
@@ -3,6 +3,8 @@ const { expect } = require('chai');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { PANIC_CODES } = require('@nomicfoundation/hardhat-chai-matchers/panic');
 
+const { TYPES } = require('../../../scripts/generate/templates/Heap.opts');
+
 async function fixture() {
   const mock = await ethers.deployContract('$Heap');
   return { mock };
@@ -13,85 +15,103 @@ describe('Heap', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  it('starts empty', async function () {
-    await expect(this.mock.$top(0)).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
-    expect(await this.mock.$length(0)).to.equal(0n);
-  });
+  for (const { struct, valueType } of TYPES) {
+    describe(struct, function () {
+      const returnEvent = `return$pop_Heap_${struct}`;
 
-  it('pop from empty', async function () {
-    await expect(this.mock.$pop(0)).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
-  });
+      beforeEach(async function () {
+        this.helper = {
+          clear: (...args) => this.mock[`$clear_Heap_${struct}`](0, ...args),
+          insert: (...args) => this.mock[`$insert(uint256,${valueType})`](0, ...args),
+          length: (...args) => this.mock[`$length_Heap_${struct}`](0, ...args),
+          pop: (...args) => this.mock[`$pop_Heap_${struct}`](0, ...args),
+          top: (...args) => this.mock[`$top_Heap_${struct}`](0, ...args),
+        };
+      });
 
-  it('clear', async function () {
-    await this.mock.$insert(0, 42n);
-    expect(await this.mock.$length(0)).to.equal(1n);
-    expect(await this.mock.$top(0)).to.equal(42n);
+      it('starts empty', async function () {
+        await expect(this.helper.top()).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
+        expect(await this.helper.length()).to.equal(0n);
+      });
 
-    await this.mock.$clear(0);
-    expect(await this.mock.$length(0)).to.equal(0n);
-    await expect(this.mock.$top(0)).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
-  });
+      it('pop from empty', async function () {
+        await expect(this.helper.pop()).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
+      });
 
-  it('support duplicated items', async function () {
-    expect(await this.mock.$length(0)).to.equal(0n);
+      it('clear', async function () {
+        await this.helper.insert(42n);
 
-    // insert 5 times
-    await this.mock.$insert(0, 42n);
-    await this.mock.$insert(0, 42n);
-    await this.mock.$insert(0, 42n);
-    await this.mock.$insert(0, 42n);
-    await this.mock.$insert(0, 42n);
+        expect(await this.helper.length()).to.equal(1n);
+        expect(await this.helper.top()).to.equal(42n);
 
-    // pop 5 times
-    await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(42n);
-    await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(42n);
-    await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(42n);
-    await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(42n);
-    await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(42n);
+        await this.helper.clear();
 
-    // popping a 6th time panics
-    await expect(this.mock.$pop(0)).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
-  });
+        expect(await this.helper.length()).to.equal(0n);
+        await expect(this.helper.top()).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
+      });
 
-  it('insert and pop', async function () {
-    const heap = [];
-    for (const { op, value } of [
-      { op: 'insert', value: 712 }, // [712]
-      { op: 'insert', value: 20 }, // [20, 712]
-      { op: 'insert', value: 4337 }, // [20, 712, 4437]
-      { op: 'pop' }, // 20, [712, 4437]
-      { op: 'insert', value: 1559 }, // [712, 1159, 4437]
-      { op: 'insert', value: 155 }, // [155, 712, 1159, 4437]
-      { op: 'insert', value: 7702 }, // [155, 712, 1159, 4437, 7702]
-      { op: 'pop' }, // 155, [712, 1159, 4437, 7702]
-      { op: 'insert', value: 721 }, // [712, 721, 1159, 4437, 7702]
-      { op: 'pop' }, // 712, [721, 1159, 4437, 7702]
-      { op: 'pop' }, // 721, [1159, 4437, 7702]
-      { op: 'pop' }, // 1159, [4437, 7702]
-      { op: 'pop' }, // 4437, [7702]
-      { op: 'pop' }, // 7702, []
-      { op: 'pop' }, // panic
-    ]) {
-      switch (op) {
-        case 'insert':
-          await this.mock.$insert(0, value);
-          heap.push(value);
-          heap.sort((a, b) => a - b);
-          break;
-        case 'pop':
-          if (heap.length == 0) {
-            await expect(this.mock.$pop(0)).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
-          } else {
-            await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(heap.shift());
+      it('support duplicated items', async function () {
+        expect(await this.helper.length()).to.equal(0n);
+
+        // insert 5 times
+        await this.helper.insert(42n);
+        await this.helper.insert(42n);
+        await this.helper.insert(42n);
+        await this.helper.insert(42n);
+        await this.helper.insert(42n);
+
+        // pop 5 times
+        await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(42n);
+        await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(42n);
+        await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(42n);
+        await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(42n);
+        await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(42n);
+
+        // popping a 6th time panics
+        await expect(this.helper.pop()).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
+      });
+
+      it('insert and pop', async function () {
+        const heap = [];
+        for (const { op, value } of [
+          { op: 'insert', value: 712 }, // [712]
+          { op: 'insert', value: 20 }, // [20, 712]
+          { op: 'insert', value: 4337 }, // [20, 712, 4437]
+          { op: 'pop' }, // 20, [712, 4437]
+          { op: 'insert', value: 1559 }, // [712, 1159, 4437]
+          { op: 'insert', value: 155 }, // [155, 712, 1159, 4437]
+          { op: 'insert', value: 7702 }, // [155, 712, 1159, 4437, 7702]
+          { op: 'pop' }, // 155, [712, 1159, 4437, 7702]
+          { op: 'insert', value: 721 }, // [712, 721, 1159, 4437, 7702]
+          { op: 'pop' }, // 712, [721, 1159, 4437, 7702]
+          { op: 'pop' }, // 721, [1159, 4437, 7702]
+          { op: 'pop' }, // 1159, [4437, 7702]
+          { op: 'pop' }, // 4437, [7702]
+          { op: 'pop' }, // 7702, []
+          { op: 'pop' }, // panic
+        ]) {
+          switch (op) {
+            case 'insert':
+              await this.helper.insert(value);
+              heap.push(value);
+              heap.sort((a, b) => a - b);
+              break;
+            case 'pop':
+              if (heap.length == 0) {
+                await expect(this.helper.pop()).to.be.revertedWithPanic(PANIC_CODES.POP_ON_EMPTY_ARRAY);
+              } else {
+                await expect(this.helper.pop()).to.emit(this.mock, returnEvent).withArgs(heap.shift());
+              }
+              break;
           }
-          break;
-      }
-      expect(await this.mock.$length(0)).to.equal(heap.length);
-      if (heap.length == 0) {
-        await expect(this.mock.$top(0)).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
-      } else {
-        expect(await this.mock.$top(0)).to.equal(heap[0]);
-      }
-    }
-  });
+          expect(await this.helper.length()).to.equal(heap.length);
+          if (heap.length == 0) {
+            await expect(this.helper.top()).to.be.revertedWithPanic(PANIC_CODES.ARRAY_ACCESS_OUT_OF_BOUNDS);
+          } else {
+            expect(await this.helper.top()).to.equal(heap[0]);
+          }
+        }
+      });
+    });
+  }
 });
