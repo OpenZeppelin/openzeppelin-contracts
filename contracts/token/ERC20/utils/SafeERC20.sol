2fcf803f1d2e00072d4733d9b3147e8959349e96
Ernesto García <ernestognw@gmail.com>
2024-06-24 18:30:51 -0600

    Update contracts/token/ERC20/utils/SafeERC20.sol

--- contracts/token/ERC20/utils/SafeERC20.sol
+++ contracts/token/ERC20/utils/SafeERC20.sol
@@ -150,9 +150,8 @@ library SafeERC20 {
             let success := call(gas(), token, 0, add(data, 0x20), mload(data), 0, 0x20)
             // bubble errors
             if iszero(success) {
-                let ptr := mload(0x40)
-                returndatacopy(ptr, 0, returndatasize())
-                revert(ptr, returndatasize())
+                returndatacopy(0x00, 0, returndatasize())
+                revert(0x00, returndatasize())
             }
             returnSize := returndatasize()
             returnValue := mload(0)
