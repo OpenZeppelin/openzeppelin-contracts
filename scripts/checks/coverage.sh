9fe61deb30ab22e6ae3526a3644aa0d541a2fc3b
cairo <cairoeth@protonmail.com>
2024-06-28 10:55:42 +0200

    apply suggestions from code review
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- scripts/checks/coverage.sh
+++ scripts/checks/coverage.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 export COVERAGE=true
 export FOUNDRY_FUZZ_RUNS=10
 
 # Hardhat coverage
-npx hardhat coverage
+hardhat coverage
 
-# Foundry coverage
-forge coverage --report lcov
-# Remove test and mock data
-lcov --rc derive_function_end_line=0 -o lcov.info --remove lcov.info 'test/*' 'contracts/mocks/*'
-# Remove zero hits
-awk '!/,0/' lcov.info > temp && mv temp lcov.info
+if [ "${CI:-"false"}" == "true" ]; then
+  # Foundry coverage
+  forge coverage --report lcov
+  # Remove test and mock data
+  lcov --rc derive_function_end_line=0 -o lcov.info --remove lcov.info 'test/*' 'contracts/mocks/*'
+  # Remove zero hits
+  sed -i '/,0/d' lcov.info
+fi
 
 # Reports are then uploaded to Codecov automatically by workflow, and merged.
