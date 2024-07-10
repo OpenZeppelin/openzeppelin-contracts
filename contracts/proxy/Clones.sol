3696b7d598f592b233d3d3a86c9899c5ac66dd21
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-07-11 00:40:26 +0200

    Add clone variant with instance parameters stored in 'immutable storage'

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -17,6 +17,8 @@ import {Errors} from "../utils/Errors.sol";
  * deterministic method.
  */
 library Clones {
+    error ImmutableArgsTooLarge();
+
     /**
      * @dev Deploys and returns the address of a clone that mimics the behaviour of `implementation`.
      *
@@ -120,5 +122,142 @@ library Clones {
         bytes32 salt
     ) internal view returns (address predicted) {
         return predictDeterministicAddress(implementation, salt, address(this));
+    }
+
+    function cloneWithImmutableArgs(address implementation, bytes memory args) internal returns (address instance) {
+        return cloneWithImmutableArgs(implementation, args, 0);
+    }
+
+    function cloneWithImmutableArgs(
+        address implementation,
+        bytes memory args,
+        uint256 value
+    ) internal returns (address instance) {
+        if (address(this).balance < value) {
+            revert Errors.InsufficientBalance(address(this).balance, value);
+        }
+
+        uint256 extraLength = args.length;
+        uint256 codeLength = 0x2d + extraLength;
+        uint256 initLength = 0x38 + extraLength;
+        if (codeLength > 0xffff) revert ImmutableArgsTooLarge();
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            // [ptr + 0x43] ......................................................................................................................................<ARGS> // args
+            // [ptr + 0x23] ......................................................................00000000000000000000000000000000005af43d82803e903d91602b57fd5bf3...... // suffix
+            // [ptr + 0x14] ........................................000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.................................... // implementation
+            // [ptr + 0x00] 00000000000000000000003d61000080600a3d3981f3363d3d373d3d3d363d73............................................................................ // prefix
+            // [ptr + 0x0d] ..........................XX................................................................................................................ // length (part 1)
+            // [ptr + 0x0e] ............................XX.............................................................................................................. // length (part 2)
+            let ptr := mload(0x40)
+            mcopy(add(ptr, 0x43), add(args, 0x20), extraLength)
+            mstore(add(ptr, 0x23), 0x5af43d82803e903d91602b57fd5bf3)
+            mstore(add(ptr, 0x14), implementation)
+            mstore(add(ptr, 0x00), 0x3d61000080600b3d3981f3363d3d373d3d3d363d73)
+            mstore8(add(ptr, 0x0d), shr(8, codeLength))
+            mstore8(add(ptr, 0x0e), shr(0, codeLength))
+            instance := create(value, add(ptr, 0x0b), initLength)
+        }
+
+        if (instance == address(0)) {
+            revert Errors.FailedDeployment();
+        }
+    }
+
+    function cloneWithImmutableArgsDeterministic(
+        address implementation,
+        bytes memory args,
+        bytes32 salt
+    ) internal returns (address instance) {
+        return cloneWithImmutableArgsDeterministic(implementation, args, salt, 0);
+    }
+
+    function cloneWithImmutableArgsDeterministic(
+        address implementation,
+        bytes memory args,
+        bytes32 salt,
+        uint256 value
+    ) internal returns (address instance) {
+        if (address(this).balance < value) {
+            revert Errors.InsufficientBalance(address(this).balance, value);
+        }
+
+        uint256 extraLength = args.length;
+        uint256 codeLength = 0x2d + extraLength;
+        uint256 initLength = 0x38 + extraLength;
+        if (codeLength > 0xffff) revert ImmutableArgsTooLarge();
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            // [ptr + 0x43] ......................................................................................................................................<ARGS> // args
+            // [ptr + 0x23] ......................................................................00000000000000000000000000000000005af43d82803e903d91602b57fd5bf3...... // suffix
+            // [ptr + 0x14] ........................................000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.................................... // implementation
+            // [ptr + 0x00] 00000000000000000000003d61000080600a3d3981f3363d3d373d3d3d363d73............................................................................ // prefix
+            // [ptr + 0x0d] ..........................XX................................................................................................................ // length (part 1)
+            // [ptr + 0x0e] ............................XX.............................................................................................................. // length (part 2)
+            let ptr := mload(0x40)
+            mcopy(add(ptr, 0x43), add(args, 0x20), extraLength)
+            mstore(add(ptr, 0x23), 0x5af43d82803e903d91602b57fd5bf3)
+            mstore(add(ptr, 0x14), implementation)
+            mstore(add(ptr, 0x00), 0x3d61000080600b3d3981f3363d3d373d3d3d363d73)
+            mstore8(add(ptr, 0x0d), shr(8, codeLength))
+            mstore8(add(ptr, 0x0e), shr(0, codeLength))
+            instance := create2(value, add(ptr, 0x0b), initLength, salt)
+        }
+        if (instance == address(0)) {
+            revert Errors.FailedDeployment();
+        }
+    }
+
+    function predictWithImmutableArgsDeterministicAddress(
+        address implementation,
+        bytes memory args,
+        bytes32 salt,
+        address deployer
+    ) internal pure returns (address predicted) {
+        uint256 extraLength = args.length;
+        uint256 codeLength = 0x2d + extraLength;
+        uint256 initLength = 0x38 + extraLength;
+        if (codeLength > 0xffff) revert ImmutableArgsTooLarge();
+
+        /// @solidity memory-safe-assembly
+        assembly {
+            let ptr := mload(0x40)
+            mstore(add(ptr, add(0x58, extraLength)), salt)
+            mstore(add(ptr, add(0x38, extraLength)), deployer)
+            mstore8(add(ptr, add(0x43, extraLength)), 0xff)
+            mcopy(add(ptr, 0x43), add(args, 0x20), extraLength)
+            mstore(add(ptr, 0x23), 0x5af43d82803e903d91602b57fd5bf3)
+            mstore(add(ptr, 0x14), implementation)
+            mstore(add(ptr, 0x00), 0x3d61000080600b3d3981f3363d3d373d3d3d363d73)
+            mstore8(add(ptr, 0x0d), shr(8, codeLength))
+            mstore8(add(ptr, 0x0e), shr(0, codeLength))
+            mstore(add(ptr, add(0x78, extraLength)), keccak256(add(ptr, 0x0b), initLength))
+            predicted := and(
+                keccak256(add(ptr, add(0x43, extraLength)), 0x55),
+                0xffffffffffffffffffffffffffffffffffffffff
+            )
+        }
+    }
+
+    function predictWithImmutableArgsDeterministicAddress(
+        address implementation,
+        bytes memory args,
+        bytes32 salt
+    ) internal view returns (address predicted) {
+        return predictWithImmutableArgsDeterministicAddress(implementation, args, salt, address(this));
+    }
+
+    function fetchCloneArgs(address instance) internal view returns (bytes memory result) {
+        uint256 argsLength = instance.code.length - 0x2d; // revert if length is too short
+        assembly {
+            // reserve space
+            result := mload(0x40)
+            mstore(0x40, add(result, add(0x20, argsLength)))
+            // load
+            mstore(result, argsLength)
+            extcodecopy(instance, add(result, 0x20), 0x2d, argsLength)
+        }
     }
 }
