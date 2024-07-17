17f34104972780bb75b4fb025dba411e0d5fbed1
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-07-17 18:15:06 +0200

    Apply suggestions from code review
    Co-authored-by: cairo <cairoeth@protonmail.com>

--- .changeset/serious-carrots-provide.md
+++ .changeset/serious-carrots-provide.md
@@ -2,4 +2,4 @@
 'openzeppelin-solidity': minor
 ---
 
-`ERC20TemporaryApproval`: Add an ERC-20 extension that implements temporary approval using transient storage (draft).
+`ERC20TemporaryApproval`: Add an ERC-20 extension that implements temporary approval using transient storage, based on ERC7674 (draft).
--- contracts/interfaces/draft-IERC7674.sol
+++ contracts/interfaces/draft-IERC7674.sol
@@ -7,7 +7,7 @@ pragma solidity ^0.8.20;
  */
 interface IERC7674 {
     /**
-     * @dev Set the temporary allowance, allowing allows `spender` to withdraw (within the same transaction) assets
+     * @dev Set the temporary allowance, allowing `spender` to withdraw (within the same transaction) assets
      * held by the caller.
      */
     function temporaryApprove(address spender, uint256 value) external returns (bool success);
