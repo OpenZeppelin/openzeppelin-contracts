757a25fac827feed9c19cc867c01b26bd09aeb54
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-07-30 17:11:11 +0200

    Update ERC4626.sol

--- contracts/token/ERC20/extensions/ERC4626.sol
+++ contracts/token/ERC20/extensions/ERC4626.sol
@@ -97,8 +97,6 @@ abstract contract ERC4626 is ERC20, IERC4626 {
     }
 
     /**
-     * @inheritdoc IERC20Metadata
-     *
      * @dev Decimals are computed by adding the decimal offset on top of the underlying asset's decimals. This
      * "original" value is cached during construction of the vault contract. If this read operation fails (e.g., the
      * asset has not been created yet), a default of 18 is used to represent the underlying asset's decimals.
