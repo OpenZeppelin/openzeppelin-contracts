83f954d8c46da85990f6a16b6a77b80904c1225c
Ernesto García <ernestognw@gmail.com>
2024-08-30 05:44:05 -0600

    Add memory side effects notes when using function pointers (#5174)

--- scripts/generate/templates/Arrays.js
+++ scripts/generate/templates/Arrays.js
@@ -26,6 +26,8 @@ const sort = type => `\
  * array. Using it in view functions that are executed through \`eth_call\` is safe, but one should be very careful
  * when executing this as part of a transaction. If the array being sorted is too large, the sort operation may
  * consume more gas than is available in a block, leading to potential DoS.
+ *
+ * IMPORTANT: Consider memory side-effects when using custom comparator functions that access memory in an unsafe way.
  */
 function sort(
     ${type}[] memory array,
