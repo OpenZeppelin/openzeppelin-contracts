89341402330100256fe25576a557bad0d0b9073c
cairo <cairoeth@protonmail.com>
2024-09-02 11:18:36 +0200

    Reduce optimizations due to stack too deep

--- contracts/utils/cryptography/P256.sol
+++ contracts/utils/cryptography/P256.sol
@@ -192,19 +192,24 @@ library P256 {
             let p := P
             let z1 := mload(add(p1, 0x40))
             let zz1 := mulmod(z1, z1, p) // zz1 = z1²
-            let zz2 := mulmod(z2, z2, p) // zz2 = z2²
-            let s1 := mulmod(mload(add(p1, 0x20)), mulmod(zz2, z2, p), p) // s1 = y1*z2³
+            let s1 := mulmod(mload(add(p1, 0x20)), mulmod(mulmod(z2, z2, p), z2, p), p) // s1 = y1*z2³
             let r := addmod(mulmod(y2, mulmod(zz1, z1, p), p), sub(p, s1), p) // r = s2-s1
-            let u1 := mulmod(mload(p1), zz2, p) // u1 = x1*z2²
+            let u1 := mulmod(mload(p1), mulmod(z2, z2, p), p) // u1 = x1*z2²
             let h := addmod(mulmod(x2, zz1, p), sub(p, u1), p) // h = u2-u1
             let hh := mulmod(h, h, p) // h²
-            let hhh := mulmod(h, hh, p) // h³
-            let v := mulmod(u1, hh, p) // v = u1*h²
 
             // x' = r²-h³-2*u1*h²
-            rx := addmod(addmod(mulmod(r, r, p), sub(p, hhh), p), sub(p, mulmod(2, v, p)), p)
+            rx := addmod(
+                addmod(mulmod(r, r, p), sub(p, mulmod(h, hh, p)), p),
+                sub(p, mulmod(2, mulmod(u1, hh, p), p)),
+                p
+            )
             // y' = r*(u1*h²-x')-s1*h³
-            ry := addmod(mulmod(r, addmod(v, sub(p, rx), p), p), sub(p, mulmod(s1, hhh, p)), p)
+            ry := addmod(
+                mulmod(r, addmod(mulmod(u1, hh, p), sub(p, rx), p), p),
+                sub(p, mulmod(s1, mulmod(h, hh, p), p)),
+                p
+            )
             // z' = h*z1*z2
             rz := mulmod(h, mulmod(z1, z2, p), p)
         }
