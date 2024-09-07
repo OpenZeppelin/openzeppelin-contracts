2b315d063514dc23a9c7ac473b1d54f40e420988
cairo <cairoeth@protonmail.com>
2024-09-07 16:55:45 +0200

    Bump version to transient minimum

--- contracts/token/ERC20/extensions/draft-ERC20TemporaryApproval.sol
+++ contracts/token/ERC20/extensions/draft-ERC20TemporaryApproval.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {IERC20, ERC20} from "../ERC20.sol";
 import {IERC7674} from "../../../interfaces/draft-IERC7674.sol";
