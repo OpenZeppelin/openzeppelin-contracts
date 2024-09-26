867d8deb27e3a1a3d2100963e1da72eb3cb5da47
ernestognw <ernestognw@gmail.com>
2024-09-26 11:51:02 -0600

    Revert "Run coverage with viaIR"
    This reverts commit dbb212f0fc58816fa702b270b8308c09499bc16b.

--- hardhat.config.js
+++ hardhat.config.js
@@ -80,7 +80,7 @@ module.exports = {
         runs: argv.runs,
       },
       evmVersion: argv.evm,
-      viaIR: argv.ir || argv.coverage,
+      viaIR: argv.ir,
       outputSelection: { '*': { '*': ['storageLayout'] } },
     },
   },
