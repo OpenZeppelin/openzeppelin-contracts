178a917d60c0e13594bdbb8fe4fe88c64965b840
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-10-07 18:12:22 +0200

    Update TransientSlot.test.js

--- test/utils/TransientSlot.test.js
+++ test/utils/TransientSlot.test.js
@@ -6,14 +6,13 @@ const { generators } = require('../helpers/random');
 const slot = ethers.id('some.storage.slot');
 const otherSlot = ethers.id('some.other.storage.slot');
 
+// Non-value types are not supported by the `TransientSlot` library.
 const TYPES = [
-  { name: 'Boolean', type: 'bool', value: true, isValueType: true, zero: false },
-  { name: 'Address', type: 'address', value: generators.address(), isValueType: true, zero: generators.address.zero },
-  { name: 'Bytes32', type: 'bytes32', value: generators.bytes32(), isValueType: true, zero: generators.bytes32.zero },
-  { name: 'Uint256', type: 'uint256', value: generators.uint256(), isValueType: true, zero: generators.uint256.zero },
-  { name: 'Int256', type: 'int256', value: generators.int256(), isValueType: true, zero: generators.int256.zero },
-  { name: 'Bytes', type: 'bytes', value: generators.hexBytes(128), isValueType: false, zero: generators.hexBytes.zero },
-  { name: 'String', type: 'string', value: 'lorem ipsum', isValueType: false, zero: '' },
+  { name: 'Boolean', type: 'bool', value: true, zero: false },
+  { name: 'Address', type: 'address', value: generators.address(), zero: generators.address.zero },
+  { name: 'Bytes32', type: 'bytes32', value: generators.bytes32(), zero: generators.bytes32.zero },
+  { name: 'Uint256', type: 'uint256', value: generators.uint256(), zero: generators.uint256.zero },
+  { name: 'Int256', type: 'int256', value: generators.int256(), zero: generators.int256.zero },
 ];
 
 async function fixture() {
@@ -25,7 +24,7 @@ describe('TransientSlot', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  for (const { name, type, value, zero } of TYPES.filter(type => type.isValueType)) {
+  for (const { name, type, value, zero } of TYPES) {
     describe(`${type} transient slot`, function () {
       const load = `tload${name}(bytes32)`;
       const store = `tstore(bytes32,${type})`;
