12adf001c81d189966fe6c429b41c74b5c86bf02
Ernesto García <ernestognw@gmail.com>
2024-10-10 23:06:36 -0600

    Fix compilation

--- contracts/account/draft-AccountEIP7702.sol
+++ contracts/account/draft-AccountEIP7702.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.20;
 
-import {AccountECDSA} from "./AccountECDSA.sol";
+import {AccountECDSA} from "./draft-AccountECDSA.sol";
 import {EIP712} from "../utils/cryptography/EIP712.sol";
 
 contract AccountEIP7702 is AccountECDSA {
