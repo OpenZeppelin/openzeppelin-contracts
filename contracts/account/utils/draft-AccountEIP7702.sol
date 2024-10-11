114ed90aaeb02e114cc59d14c70036d7a87e76d8
Ernesto García <ernestognw@gmail.com>
2024-10-10 23:05:37 -0600

    Add AccountEIP7702

---
+++ contracts/account/utils/draft-AccountEIP7702.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {AccountECDSA} from "./AccountECDSA.sol";
+import {EIP712} from "../utils/cryptography/EIP712.sol";
+
+contract AccountEIP7702 is AccountECDSA {
+    constructor(string memory name, string memory version) AccountECDSA(address(this)) EIP712(name, version) {}
+}
