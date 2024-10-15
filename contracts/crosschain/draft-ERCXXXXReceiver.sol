1dfff54067a8462ade2dfcbba21ee0d114f56b1c
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-10-15 18:22:37 +0200

    doc

--- contracts/crosschain/draft-ERCXXXXReceiver.sol
+++ contracts/crosschain/draft-ERCXXXXReceiver.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.0;
 
 import {IERCXXXXGatewayDestinationPassive, IERCXXXXReceiver} from "../interfaces/draft-IERCXXXX.sol";
 
+/**
+ * @dev Base implementation of an ERC-XXXX compliant cross-chain message receiver.
+ *
+ * This abstract contract exposes the `receiveMessage` function that is used in both active and passive mode for
+ * communication with (on or multiple) destination gateways. This contract leaves two function unimplemented:
+ *
+ * * `_isKnownGateway(address)`: an internal getter used to verify weither an address is recognised by the contract as
+ *   a valid ERC-XXXX gateway. One or multiple gateway can be supported. Note that any malicious address for which
+ *   this function returns true would be able to impersonnate any account on any other chain sending any message.
+ * * `_processMessage`: the internal function that will be called with any message that has been validated.
+ */
 abstract contract ERCXXXXReceiver is IERCXXXXReceiver {
     error ERCXXXXReceiverInvalidGateway(address gateway);
 
+    /// @inheritdoc IERCXXXXReceiver
     function receiveMessage(
         address gateway,
         bytes calldata gatewayMessageKey,
@@ -33,8 +45,10 @@ abstract contract ERCXXXXReceiver is IERCXXXXReceiver {
         _processMessage(gateway, source, sender, payload, attributes);
     }
 
+    /// @dev Virtual getter that returns weither an address in a valid ERC-XXXX gateway.
     function _isKnownGateway(address instance) internal view virtual returns (bool);
 
+    /// @dev Virtual function that should contain the logic to execute when a crosschain message is received.
     function _processMessage(
         address gateway,
         string calldata source,
