29f40597739707296e27d01ff368e0833029fc67
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-10-22 23:01:26 +0200

    Bump pragma of files that depend on mcopy (#5276)

--- contracts/mocks/Stateless.sol
+++ contracts/mocks/Stateless.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
 // We keep these imports and a dummy contract just to we can run the test suite after transpilation.
 
