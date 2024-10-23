2fa4d103fe374335b15f9c54e7eec32e36ea89b4
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-10-23 09:16:10 +0200

    Add NoncesKeyed variant (#5272)

--- test/utils/NoncesKeyed.test.js
+++ test/utils/NoncesKeyed.test.js
@@ -1,16 +1,17 @@
 const { ethers } = require('hardhat');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { shouldBehaveLikeNonces } = require('./Nonces.behavior');
+const { shouldBehaveLikeNonces, shouldBehaveLikeNoncesKeyed } = require('./Nonces.behavior');
 
 async function fixture() {
-  const mock = await ethers.deployContract('$Nonces');
+  const mock = await ethers.deployContract('$NoncesKeyed');
   return { mock };
 }
 
-describe('Nonces', function () {
+describe('NoncesKeyed', function () {
   beforeEach(async function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
   shouldBehaveLikeNonces();
+  shouldBehaveLikeNoncesKeyed();
 });
