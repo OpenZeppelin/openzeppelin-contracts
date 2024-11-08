08eae45091762714db2b9877751e1aef2368ae60
Jeroen Offerijns <jeroen@offerijns.com>
2024-11-08 10:02:12 +0100

    Fix comments

--- contracts/token/ERC20/extensions/ERC7540.sol
+++ contracts/token/ERC20/extensions/ERC7540.sol
@@ -33,6 +33,9 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         return totalAssets() - _totalPendingDepositAssets;
     }
 
+    /**
+     * @dev See {IERC4626-maxDeposit}.
+     */
     function maxDeposit(address controller)
         public
         view
@@ -43,10 +46,16 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         return BaseERC7540Deposit.maxDeposit(controller);
     }
 
+    /**
+     * @dev See {IERC4626-previewDeposit}.
+     */
     function previewDeposit(uint256) public pure virtual override(BaseERC7540Deposit, ERC4626) returns (uint256) {
-        revert("ERC7540Vault/async-flow");
+        revert("async-flow");
     }
 
+    /**
+     * @dev See {IERC4626-deposit}.
+     */
     function deposit(uint256 assets, address receiver)
         public
         virtual
@@ -56,14 +65,23 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         shares = BaseERC7540Deposit.deposit(assets, receiver, receiver);
     }
 
+    /**
+     * @dev See {IERC4626-maxMint}.
+     */
     function maxMint(address controller) public view virtual override(BaseERC7540Deposit, ERC4626) returns (uint256) {
         return BaseERC7540Deposit.maxMint(controller);
     }
 
+    /**
+     * @dev See {IERC4626-previewMint}.
+     */
     function previewMint(uint256) public pure virtual override(BaseERC7540Deposit, ERC4626) returns (uint256) {
-        revert("ERC7540Vault/async-flow");
+        revert("async-flow");
     }
 
+    /**
+     * @dev See {IERC4626-mint}.
+     */
     function mint(uint256 shares, address receiver)
         public
         virtual
@@ -73,6 +91,9 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         assets = BaseERC7540Deposit.mint(shares, receiver, receiver);
     }
 
+    /**
+     * @dev See {IERC4626-maxWithdraw}.
+     */
     function maxWithdraw(address controller)
         public
         view
@@ -83,10 +104,16 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         return BaseERC7540Redeem.maxWithdraw(controller);
     }
 
+    /**
+     * @dev See {IERC4626-previewWithdraw}.
+     */
     function previewWithdraw(uint256) public pure virtual override(BaseERC7540Redeem, ERC4626) returns (uint256) {
-        revert("ERC7540Vault/async-flow");
+        revert("async-flow");
     }
 
+    /**
+     * @dev See {IERC4626-withdraw}.
+     */
     function withdraw(uint256 assets, address receiver, address controller)
         public
         virtual
@@ -96,14 +123,23 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         shares = BaseERC7540Redeem.withdraw(assets, receiver, controller);
     }
 
+    /**
+     * @dev See {IERC4626-maxRedeem}.
+     */
     function maxRedeem(address controller) public view virtual override(BaseERC7540Redeem, ERC4626) returns (uint256) {
         return BaseERC7540Redeem.maxRedeem(controller);
     }
 
+    /**
+     * @dev See {IERC4626-previewRedeem}.
+     */
     function previewRedeem(uint256) public pure virtual override(BaseERC7540Redeem, ERC4626) returns (uint256) {
-        revert("ERC7540Vault/async-flow");
+        revert("async-flow");
     }
 
+    /**
+     * @dev See {IERC4626-redeem}.
+     */
     function redeem(uint256 shares, address receiver, address controller)
         public
         virtual
@@ -113,6 +149,9 @@ contract ERC7540 is BaseERC7540Deposit, BaseERC7540Redeem {
         assets = BaseERC7540Redeem.redeem(shares, receiver, controller);
     }
 
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
     function supportsInterface(bytes4 interfaceId)
         public
         pure
