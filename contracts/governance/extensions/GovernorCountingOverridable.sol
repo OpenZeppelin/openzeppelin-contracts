0513853ca54b35eed39bd787bdc80ca29073d774
Ernesto García <ernestognw@gmail.com>
2024-11-25 11:28:59 -0600

    Rephrase `VoteReduced` event docs (#5318)

--- contracts/governance/extensions/GovernorCountingOverridable.sol
+++ contracts/governance/extensions/GovernorCountingOverridable.sol
@@ -35,7 +35,7 @@ abstract contract GovernorCountingOverridable is GovernorVotes {
         mapping(address voter => VoteReceipt) voteReceipt;
     }
 
-    /// @dev The vote was reduced by `weight` after an override over the `delegate` by the original token holder
+    /// @dev The votes casted by `delegate` were reduced by `weight` after an override vote was casted by the original token holder
     event VoteReduced(address indexed delegate, uint256 proposalId, uint8 support, uint256 weight);
 
     /// @dev A delegated vote on `proposalId` was overridden by `weight`
