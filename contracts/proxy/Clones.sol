8e0e13b79db3901cb955bffd37f0c054f32c2b43
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-11-27 18:05:20 +0100

    Update contracts/proxy/Clones.sol

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -229,7 +229,7 @@ library Clones {
     function fetchCloneArgs(address instance) internal view returns (bytes memory) {
         bytes memory result = new bytes(instance.code.length - 0x2d); // revert if length is too short
         assembly ("memory-safe") {
-            extcodecopy(instance, add(result, 32), 0x2d, mload(result))
+            extcodecopy(instance, add(result, 0x20), 0x2d, mload(result))
         }
         return result;
     }
