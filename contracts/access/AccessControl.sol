e50cae8bcbe5e5b6375cdae9543f89562dfc5bb5
Simka <0xsimka@gmail.com>
2024-12-02 18:38:14 +0300

    Fix Syntax Errors in Mapping Definitions

--- contracts/access/AccessControl.sol
+++ contracts/access/AccessControl.sol
@@ -48,11 +48,11 @@ import {ERC165} from "../utils/introspection/ERC165.sol";
  */
 abstract contract AccessControl is Context, IAccessControl, ERC165 {
     struct RoleData {
-        mapping(address account => bool) hasRole;
+        mapping(address => bool) hasRole;
         bytes32 adminRole;
     }
 
-    mapping(bytes32 role => RoleData) private _roles;
+    mapping(bytes32 => RoleData) private _roles;
 
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
 
