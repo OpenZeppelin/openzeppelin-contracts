195cb47b06778defe3a4fa0dbd050040d768c891
Arr00 <13561405+arr00@users.noreply.github.com>
2024-12-10 13:33:57 -0500

    add file to interfaces dir

---
+++ contracts/interfaces/draft-IERC6909.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (interfaces/draft-IERC6909.sol)
+
+pragma solidity ^0.8.20;
+
+import {IERC6909} from "../token/ERC6909/draft-IERC6909.sol";
