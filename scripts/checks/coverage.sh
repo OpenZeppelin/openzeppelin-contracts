0abdf442e9a66481e1736a2f3aee94271278ab11
Arr00 <13561405+arr00@users.noreply.github.com>
2024-12-18 14:33:49 -0500

    only update max old space size if not set

--- scripts/checks/coverage.sh
+++ scripts/checks/coverage.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 export COVERAGE=true
 export FOUNDRY_FUZZ_RUNS=10
+export NODE_OPTIONS="${NODE_OPTIONS:-}"
+
+if [[ $NODE_OPTIONS != *"--max-old-space-size"* ]]; then
+  export NODE_OPTIONS="${NODE_OPTIONS} --max-old-space-size=8192"
+fi
 
 # Hardhat coverage
 hardhat coverage
