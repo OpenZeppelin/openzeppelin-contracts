9ae0429fcf00bfc1ed4c0fa2840ddbf9bc7b5040
Arr00 <13561405+arr00@users.noreply.github.com>
2024-12-20 10:32:49 -0500

    add more tests

--- test/token/ERC6909/ERC6909.behavior.js
+++ test/token/ERC6909/ERC6909.behavior.js
@@ -96,8 +96,65 @@ function shouldBehaveLikeERC6909() {
         await expect(this.token.connect(this.alice).transfer(ethers.ZeroAddress, firstTokenId, firstTokenAmount))
           .to.emit(this.token, 'Transfer')
           .withArgs(this.alice, this.alice, ethers.ZeroAddress, firstTokenId, firstTokenAmount);
+
+        // expect(this.token.balanceOf(ethers.ZeroAddress, firstTokenId)).to.eventually.equal(firstTokenAmount); TODO: fix
+        expect(this.token.balanceOf(this.alice, firstTokenId)).to.eventually.equal(0);
+      });
+
+      it('reverts when insufficient balance', async function () {
+        await expect(this.token.connect(this.alice).transfer(this.bruce, firstTokenId, firstTokenAmount + 1n))
+          .to.be.revertedWithCustomError(this.token, 'ERC6909InsufficientBalance')
+          .withArgs(this.alice, firstTokenAmount, firstTokenAmount + 1n, firstTokenId);
       });
     });
+
+    describe('transferFrom', function () {
+      beforeEach(async function () {
+        await this.token.$_mint(this.alice, firstTokenId, firstTokenAmount);
+        await this.token.$_mint(this.bruce, secondTokenId, secondTokenAmount);
+      });
+
+      it('transfer from self', async function () {
+        await this.token.connect(this.alice).transferFrom(this.alice, this.bruce, firstTokenId, firstTokenAmount);
+        expect(this.token.balanceOf(this.alice, firstTokenId)).to.eventually.equal(0);
+        expect(this.token.balanceOf(this.bruce, firstTokenId)).to.eventually.equal(firstTokenAmount);
+      });
+
+      describe('with approval', async function () {
+        beforeEach(async function () {
+          await this.token.connect(this.alice).approve(this.operator, firstTokenId, firstTokenAmount - 1n);
+          this.tx = await this.token
+            .connect(this.operator)
+            .transferFrom(this.alice, this.bruce, firstTokenId, firstTokenAmount - 1n);
+        });
+
+        it('reverts when insufficient allowance', async function () {
+          await expect(this.token.connect(this.operator).transferFrom(this.alice, this.bruce, firstTokenId, 1))
+            .to.be.revertedWithCustomError(this.token, 'ERC6909InsufficientAllowance')
+            .withArgs(this.operator, 0, 1, firstTokenId);
+        });
+
+        it('should emit transfer event', async function () {
+          expect(this.tx)
+            .to.emit(this.token, 'Transfer')
+            .withArgs(this.operator, this.alice, this.bruce, firstTokenId, firstTokenAmount - 1n);
+        });
+
+        it('should update approval', async function () {
+          expect(this.token.allowance(this.alice, this.operator, firstTokenId)).to.eventually.equal(0);
+        });
+
+        it("shouldn't reduce allowance when infinite", async function () {
+          await this.token.connect(this.bruce).approve(this.operator, secondTokenId, ethers.MaxUint256);
+          await this.token
+            .connect(this.operator)
+            .transferFrom(this.bruce, this.alice, secondTokenId, secondTokenAmount);
+          expect(this.token.allowance(this.bruce, this.operator, secondTokenId)).to.eventually.equal(ethers.MaxUint256);
+        });
+      });
+    });
+
+    describe('with operator approval', function () {});
 
     shouldSupportInterfaces(['ERC6909']);
   });
