e711da686426e4f4781869a3644d43ef4c00bceb
Arr00 <13561405+arr00@users.noreply.github.com>
2025-01-17 09:40:52 -1000

    update docs

--- contracts/mocks/docs/token/ERC6909/GameItems.sol
+++ contracts/mocks/docs/token/ERC6909/GameItems.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC6909} from "../../../../token/ERC6909/draft-ERC6909.sol";
+import {ERC6909Metadata} from "../../../../token/ERC6909/extensions/draft-ERC6909Metadata.sol";
 
-contract GameItems is ERC6909 {
+contract GameItems is ERC6909Metadata {
     uint256 public constant GOLD = 0;
     uint256 public constant SILVER = 1;
     uint256 public constant THORS_HAMMER = 2;
@@ -11,8 +11,14 @@ contract GameItems is ERC6909 {
     uint256 public constant SHIELD = 4;
 
     constructor() {
+        _setDecimals(GOLD, 18);
+        _setDecimals(SILVER, 18);
+        // Default decimals is 0
+        _setDecimals(SWORD, 9);
+        _setDecimals(SHIELD, 9);
+
         _mint(msg.sender, GOLD, 10 ** 18);
-        _mint(msg.sender, SILVER, 10 ** 27);
+        _mint(msg.sender, SILVER, 10_000 ** 18);
         _mint(msg.sender, THORS_HAMMER, 1);
         _mint(msg.sender, SWORD, 10 ** 9);
         _mint(msg.sender, SHIELD, 10 ** 9);
