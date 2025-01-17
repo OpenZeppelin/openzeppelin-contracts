78bf584c8e31da061c1cce9b36ba3dff449150eb
Arr00 <13561405+arr00@users.noreply.github.com>
2025-01-17 12:14:43 -1000

    emit events on uri changes

--- contracts/token/ERC6909/extensions/draft-ERC6909ContentURI.sol
+++ contracts/token/ERC6909/extensions/draft-ERC6909ContentURI.sol
@@ -12,6 +12,16 @@ contract ERC6909ContentURI is ERC6909, IERC6909ContentURI {
     string private _contractURI;
     mapping(uint256 id => string) private _tokenURIs;
 
+    /**
+     * @dev Event emitted when the contract URI is changed. See https://eips.ethereum.org/EIPS/eip-7572[ERC-7572] for details.
+     */
+    event ContractURIUpdated();
+
+    /**
+     * @dev See {IERC4906-MetadataUpdate}. This contract does not inherit {IERC4906} as it requires {IERC721}.
+     */
+    event MetadataUpdate(uint256 _tokenId);
+
     /// @inheritdoc IERC6909ContentURI
     function contractURI() external view virtual override returns (string memory) {
         return _contractURI;
@@ -24,9 +34,13 @@ contract ERC6909ContentURI is ERC6909, IERC6909ContentURI {
 
     function _setContractURI(string memory newContractURI) internal {
         _contractURI = newContractURI;
+
+        emit ContractURIUpdated();
     }
 
     function _setTokenURI(uint256 id, string memory newTokenURI) internal {
         _tokenURIs[id] = newTokenURI;
+
+        emit MetadataUpdate(id);
     }
 }
