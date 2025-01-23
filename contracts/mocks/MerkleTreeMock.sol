13804e53b6a322f1dc38118039d9f05e951a1633
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-01-23 17:15:22 +0100

    Update MerkleTreeMock.sol

--- contracts/mocks/MerkleTreeMock.sol
+++ contracts/mocks/MerkleTreeMock.sol
@@ -28,7 +28,7 @@ contract MerkleTreeMock {
 
     function update(uint256 index, bytes32 oldValue, bytes32 newValue, bytes32[] memory proof) public {
         (bytes32 oldRoot, bytes32 newRoot) = _tree.update(index, oldValue, newValue, proof);
-        require(oldRoot == root, "invalid proof");
+        if (oldRoot != root) revert MerkleTreeUpdateInvalidProof();
         emit LeafUpdated(oldValue, newValue, index, newRoot);
         root = newRoot;
     }
