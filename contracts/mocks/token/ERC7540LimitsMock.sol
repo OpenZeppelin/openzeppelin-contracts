ff2d631301f241c7c9e5c909543d4a3d8e019aca
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2025-01-26 02:44:01 +0100

    ERC-7540 - Add ERC7540 - add ERC7540LimitsMock.sol

---
+++ contracts/mocks/token/ERC7540LimitsMock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {IERC20} from "../../token/ERC20/IERC20.sol";
+import {IERC4626} from "../../interfaces/IERC4626.sol";
+import {ERC7540} from "../../token/ERC20/extensions/ERC7540.sol";
+import {ERC4626} from "../../token/ERC20/extensions/ERC4626.sol";
+
+abstract contract ERC7540LimitsMock is ERC7540 {
+    uint256 private immutable _maxDeposit;
+    uint256 private immutable _maxMint;
+
+    constructor(IERC20 asset, uint256 maxDepositValue, uint256 maxMintValue) ERC7540(asset) {
+        _maxDeposit = maxDepositValue;
+        _maxMint = maxMintValue;
+    }
+
+    function maxDeposit(address) public view override(IERC4626, ERC4626) returns (uint256) {
+        return _maxDeposit;
+    }
+
+    function maxMint(address) public view override(IERC4626, ERC4626) returns (uint256) {
+        return _maxMint;
+    }
+}
