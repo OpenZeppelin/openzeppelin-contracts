0c78de50785d87d29ea9b82657b9aab559fd8f6b
Arr00 <13561405+arr00@users.noreply.github.com>
2025-03-04 21:17:53 -0500

    optimize and add tests

--- contracts/utils/structs/LinkedList.sol
+++ contracts/utils/structs/LinkedList.sol
@@ -1,25 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * Library for managing linked lists. The linked lists support introspection, addition, and deletion.
+ * Order is maintained while adding and removing elements from the list does not require a full traversal.
+ *
+ * The linked lists are signly linked. They are optimized for manipulation at or near the tail of the list. This is
+ * done by having a "reversed" linked list.
+ */
 library LinkedList {
+    error LinkListEmpty();
+
     struct Bytes32LinkedList {
         mapping(bytes32 => bytes32) _list;
-        bytes32 _tail;
         uint256 _size;
     }
 
     bytes32 private constant SENTINEL = keccak256(abi.encode("SENTINEL"));
 
-    // O(1)
-    function head(Bytes32LinkedList storage self) internal view returns (bytes32) {
+    /// @dev Returns the last element in the linked list. O(1)
+    function peek(Bytes32LinkedList storage self) internal view returns (bytes32) {
         bytes32 val = self._list[SENTINEL];
         return val;
     }
 
-    // O(1)
-    function insert(Bytes32LinkedList storage self, bytes32 prev, bytes32 value) internal returns (bool) {
-        if (prev == 0) {
-            prev = SENTINEL;
+    /// @dev Insert a `value` into the linked list before the `next` element. O(1)
+    function insert(Bytes32LinkedList storage self, bytes32 next, bytes32 value) internal returns (bool) {
+        if (next == 0) {
+            next = SENTINEL;
         }
 
         if (value == 0 || value == SENTINEL) {
@@ -30,115 +38,132 @@ library LinkedList {
             return false;
         }
 
-        bytes32 next = self._list[prev];
-        self._list[value] = next;
-        self._list[prev] = value;
-
-        if (next == 0) {
-            self._tail = value;
-        }
+        bytes32 prev = self._list[next];
+        self._list[value] = prev == 0 ? SENTINEL : prev;
+        self._list[next] = value;
         self._size++;
 
         return true;
     }
 
-    // Inefficient version of insert to insert at an index, O(n)
+    /// @dev Insert a `value` into the linked list at index `index`. O(n)
     function insert(Bytes32LinkedList storage self, uint256 index, bytes32 value) internal returns (bool) {
-        if (index > self._size) {
+        uint256 size_ = self._size;
+        if (index > size_) {
             return false;
         }
 
-        uint256 currentIndex = 0;
-        bytes32 prev = SENTINEL;
+        uint256 currentIndex = size_;
+        bytes32 next = self._list[SENTINEL];
 
         while (currentIndex != index) {
-            prev = self._list[prev];
-            currentIndex++;
+            next = self._list[next];
+            currentIndex--;
         }
 
-        return insert(self, prev, value);
+        return insert(self, next, value);
     }
 
-    // O(1)
+    /// @dev Push a `value` onto the end of the linked list. O(1)
     function push(Bytes32LinkedList storage self, bytes32 value) internal {
-        insert(self, self._tail, value);
+        insert(self, SENTINEL, value);
     }
 
-    // O(1). requires previous value of one to remove
-    function removeEfficient(Bytes32LinkedList storage self, bytes32 prev) internal returns (bool) {
-        if (prev == 0) {
-            prev = SENTINEL;
+    /// @dev Removes the element before `next` in the linked list. Returns true on success, false otherwise. O(1)
+    function removeEfficient(Bytes32LinkedList storage self, bytes32 next) internal returns (bool) {
+        if (next == 0) {
+            next = SENTINEL;
         }
 
-        bytes32 next = self._list[prev];
-        self._list[prev] = next;
-        if (next == 0) {
-            self._tail = prev;
+        bytes32 val = self._list[next];
+        if (val == SENTINEL) {
+            return false;
         }
-        delete self._list[next];
+        bytes32 prev = self._list[val];
+
+        self._list[next] = prev;
+        delete self._list[val];
         self._size--;
 
         return true;
     }
 
-    // Inefficient version of remove if prev is not know. O(n)
+    /// @dev Remove a give `value` from the linked list. Returns true on success, false otherwise. O(n)
     function remove(Bytes32LinkedList storage self, bytes32 value) internal returns (bool) {
-        bytes32 prev = SENTINEL;
+        if (self._list[value] == 0) {
+            return false;
+        }
 
-        while (self._list[prev] != value) {
-            prev = self._list[prev];
-            if (prev == 0) {
+        bytes32 next = SENTINEL;
+        while (self._list[next] != value) {
+            next = self._list[next];
+            if (next == SENTINEL) {
                 return false;
             }
         }
 
-        return removeEfficient(self, prev);
+        return removeEfficient(self, next);
     }
 
-    // O(n)
-    function removeAt(Bytes32LinkedList storage self, uint256 index) internal returns (bool) {
+    /// @dev Remove the element at `index` from the linked list. Optimized for high indices. O(n)
+    function removeAt(Bytes32LinkedList storage self, uint256 index) internal returns (bool success, bytes32 value) {
         if (index >= self._size) {
-            return false;
+            return (false, bytes32(0));
         }
 
-        bytes32 prev = SENTINEL;
-        uint256 currentIndex = 0;
+        bytes32 next = SENTINEL;
+        uint256 currentIndex = self._size - 1;
 
         while (currentIndex != index) {
-            prev = self._list[prev];
-            currentIndex++;
+            next = self._list[next];
+            currentIndex--;
         }
 
-        return remove(self, prev);
+        bytes32 returnVal = self._list[next];
+        return (removeEfficient(self, next), returnVal);
     }
 
-    // O(1)
+    /// @dev Remove the last element from the linked list and return it. O(1)
+    function pop(Bytes32LinkedList storage self) internal returns (bytes32) {
+        bytes32 removedVal = self._list[SENTINEL];
+        if (!removeEfficient(self, SENTINEL)) revert LinkListEmpty();
+
+        return removedVal;
+    }
+
+    /// @dev Check if `value` is contained in the linked list. O(1)
     function contains(Bytes32LinkedList storage self, bytes32 value) internal view returns (bool) {
         return self._list[value] != 0;
     }
 
-    // O(n)
+    /// @dev Removes all elements from the linked list. O(n)
     function clear(Bytes32LinkedList storage self) internal {
-        bytes32 next = SENTINEL;
-        delete self._tail;
+        bytes32 prev = SENTINEL;
         delete self._size;
 
-        while (next != 0) {
-            next = self._list[next];
-            delete self._list[next];
+        while (prev != 0) {
+            bytes32 newPrev = self._list[prev];
+            delete self._list[prev];
+            prev = newPrev;
         }
     }
 
-    // O(n)
+    /// @dev Get all elements from the linked list. O(n)
     function values(Bytes32LinkedList storage self) internal view returns (bytes32[] memory) {
-        bytes32[] memory res = new bytes32[](self._size);
+        uint256 size_ = self._size;
+        bytes32[] memory res = new bytes32[](size_);
 
-        bytes32 next = SENTINEL;
-        for (uint256 i = 0; i < self._size; i++) {
-            next = self._list[next];
-            res[i] = next;
+        bytes32 prev = SENTINEL;
+        for (uint256 i = size_; i > 0; --i) {
+            prev = self._list[prev];
+            res[i - 1] = prev;
         }
 
         return res;
+    }
+
+    /// @dev Returns the number of elements in the linked list.
+    function size(Bytes32LinkedList storage self) internal view returns (uint256) {
+        return self._size;
     }
 }
---
+++ test/utils/structs/LinkedList.test.js
@@ -0,0 +1,129 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+async function fixture() {
+  const mock = await ethers.deployContract('$LinkedList');
+  return { mock };
+}
+
+describe.only('LinkedList', function () {
+  beforeEach(async function () {
+    Object.assign(this, await loadFixture(fixture));
+  });
+
+  it('starts empty', async function () {
+    expect(await this.mock.$size(0)).to.equal(0n);
+  });
+
+  describe('add', function () {
+    beforeEach(async function () {
+      const vals = ['0x02'].map(a => ethers.zeroPadValue(a, 32));
+      await this.mock.$push(0, ethers.zeroPadValue(vals[0], 32));
+      this.values = vals;
+    });
+
+    it('via insert with next', async function () {
+      const val = ethers.zeroPadValue('0x01', 32);
+      await this.mock['$insert(uint256,bytes32,bytes32)'](0, ethers.ZeroHash, val);
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values.concat([val]));
+    });
+
+    it('via insert with index', async function () {
+      const val = ethers.zeroPadValue('0x01', 32);
+      await this.mock['$insert(uint256,uint256,bytes32)'](0, 0, val);
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values.concat([val]));
+    });
+
+    it('via push', async function () {
+      const val = ethers.zeroPadValue('0x01', 32);
+      await this.mock.$push(0, val);
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values.concat([val]));
+    });
+  });
+
+  describe('remove', function () {
+    beforeEach(async function () {
+      const vals = ['0x01', '0x02', '0x03'].map(a => ethers.zeroPadValue(a, 32));
+      this.values = vals;
+
+      for (const val of vals) {
+        await this.mock.$push(0, val);
+      }
+    });
+
+    it('last element via pop', async function () {
+      await expect(this.mock.$pop(0)).to.emit(this.mock, 'return$pop').withArgs(this.values.pop());
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values);
+    });
+
+    it('first element via index', async function () {
+      await expect(this.mock.$removeAt(0, 0)).to.emit(this.mock, 'return$removeAt').withArgs(true, this.values.shift());
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values);
+    });
+
+    it('second element via next', async function () {
+      await expect(this.mock.$removeEfficient(0, this.values[2]))
+        .to.emit(this.mock, 'return$removeEfficient')
+        .withArgs(true);
+      await expect(this.mock.$values(0)).to.eventually.deep.equal([this.values[0], this.values[2]]);
+    });
+
+    it('via value', async function () {
+      await expect(this.mock.$remove(0, this.values[1])).to.emit(this.mock, 'return$remove').withArgs(true);
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(this.values.toSpliced(1, 1));
+    });
+
+    it('non-existent element', async function () {
+      await expect(this.mock.$removeAt(0, 3)).to.emit(this.mock, 'return$removeAt').withArgs(false, ethers.ZeroHash);
+    });
+  });
+
+  it('contains', async function () {
+    const vals = ['0x01', '0x02', '0x03'].map(a => ethers.zeroPadValue(a, 32));
+    for (const val of vals) {
+      await expect(this.mock.$contains(0, val)).to.eventually.be.false;
+      await this.mock.$push(0, val);
+    }
+
+    for (const val of vals) {
+      await expect(this.mock.$contains(0, val)).to.eventually.be.true;
+    }
+  });
+
+  describe('clear', function () {
+    it('empty linked list', async function () {
+      await this.mock.$clear(0);
+      expect(await this.mock.$size(0)).to.equal(0n);
+    });
+
+    it('non-empty linked list', async function () {
+      const vals = ['0x01', '0x02', '0x03'].map(a => ethers.zeroPadValue(a, 32));
+      for (let i = 0; i < vals.length; i++) {
+        await this.mock.$push(0, vals[i]);
+        await expect(this.mock.$size(0)).to.eventually.equal(i + 1);
+      }
+
+      await this.mock.$clear(0);
+      expect(await this.mock.$size(0)).to.equal(0n);
+    });
+  });
+
+  describe('introspection', function () {
+    it('via peek', async function () {
+      await this.mock.$push(0, ethers.zeroPadValue('0x01', 32));
+      await this.mock.$push(0, ethers.zeroPadValue('0x02', 32));
+
+      await expect(this.mock.$peek(0)).to.eventually.equal(ethers.zeroPadValue('0x02', 32));
+    });
+
+    it('via values', async function () {
+      const vals = ['0x01', '0x02', '0x03'].map(a => ethers.zeroPadValue(a, 32));
+      for (const val of vals) {
+        await this.mock.$push(0, val);
+      }
+
+      await expect(this.mock.$values(0)).to.eventually.deep.equal(vals);
+    });
+  });
+});
