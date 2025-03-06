d514606a9cba7c1fc081427b5d7dd19017f26f9c
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-03-06 17:14:59 +0100

    fix tests

--- test/utils/Memory.t.sol
+++ test/utils/Memory.t.sol
@@ -8,7 +8,11 @@ import {Memory} from "@openzeppelin/contracts/utils/Memory.sol";
 contract MemoryTest is Test {
     using Memory for *;
 
-    function testSymbolicGetSetFreePointer(bytes32 ptr) public {
+    function testSymbolicGetSetFreePointer(uint256 seed) public pure {
+        // - first 0x80 bytes are reserved (scratch + FMP + zero)
+        // - moving the free memory pointer to far causes OOG errors
+        bytes32 ptr = bytes32(bound(seed, 0x80, type(uint24).max));
+
         Memory.setFreePointer(ptr.asPointer());
         assertEq(Memory.getFreePointer().asBytes32(), ptr);
     }
