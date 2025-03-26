dbd74f9200721ed224f8a63e9104923eb4ad7706
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-03-26 12:05:32 +0100

    Update EIP7702Utils.sol

--- contracts/account/utils/EIP7702Utils.sol
+++ contracts/account/utils/EIP7702Utils.sol
@@ -10,10 +10,13 @@ pragma solidity ^0.8.20;
 library EIP7702Utils {
     bytes3 internal constant EIP7702_PREFIX = 0xef0100;
 
-    function fetchDelegate(address account) internal view returns (bool isEIP7702, address delegate) {
+    /**
+     * @dev Returns the address of the delegate if `account` as an EIP-7702 delegation setup, or address(0) otherwise.
+     */
+    function fetchDelegate(address account) internal view returns (address) {
         bytes23 delegation = bytes23(account.code);
-
-        isEIP7702 = bytes3(delegation) == EIP7702_PREFIX;
-        delegate = isEIP7702 ? address(bytes20(delegation << 24)) : address(0);
+        return bytes3(delegation) == EIP7702_PREFIX
+            ? address(bytes20(delegation << 24))
+            : address(0);
     }
 }
