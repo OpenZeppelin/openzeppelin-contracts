1bc3f030948a0ca3596cbd7700a31501626e47f4
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-04-09 15:14:30 +0200

    update relayer call to revert if calldata is too short

--- contracts/utils/RelayedCall.sol
+++ contracts/utils/RelayedCall.sol
@@ -40,46 +40,55 @@ abstract contract RelayedCall {
     }
 
     function _deployRelayer() private returns (address addr) {
-        // deployment prefix: 3d602480600a3d3981f3
-        // deployed bytecode: 6014360360145f375f5f601436035f345f3560601c5af13d5f5f3e5f3d91602257fd5bf3
+        // deployment prefix: 3d602f80600a3d3981f3
+        // deployed bytecode: 60133611600a575f5ffd5b6014360360145f375f5f601436035f345f3560601c5af13d5f5f3e5f3d91602d57fd5bf3
         //
         // offset | bytecode | opcode         | stack
         // -------|----------|----------------|--------
-        // 0x0000 | 6014     | push1 0x14     | 0x14
-        // 0x0002 | 36       | calldatasize   | cds 0x14
-        // 0x0003 | 03       | sub            | (cds-0x14)
-        // 0x0004 | 6014     | push1 0x14     | 0x14 (cds-0x14)
-        // 0x0006 | 5f       | push0          | 0 0x14 (cds-0x14)
-        // 0x0007 | 37       | calldatacopy   |
-        // 0x0008 | 5f       | push0          | 0
-        // 0x0009 | 5f       | push0          | 0 0
-        // 0x000a | 6014     | push1 0x14     | 0x14 0 0
-        // 0x000c | 36       | calldatasize   | cds 0x14 0 0
-        // 0x000d | 03       | sub            | (cds-0x14) 0 0
-        // 0x000e | 5f       | push0          | 0 (cds-0x14) 0 0
-        // 0x000f | 34       | callvalue      | value 0 (cds-0x14) 0 0
-        // 0x0010 | 5f       | push0          | 0 value 0 (cds-0x14) 0 0
-        // 0x0011 | 35       | calldataload   | cd[0] value 0 (cds-0x14) 0 0
-        // 0x0012 | 6060     | push1 0x60     | 0x60 cd[0] value 0 (cds-0x14) 0 0
-        // 0x0014 | 1c       | shr            | target value 0 (cds-0x14) 0 0
-        // 0x0015 | 5a       | gas            | gas target value 0 (cds-0x14) 0 0
-        // 0x0016 | f1       | call           | suc
-        // 0x0017 | 3d       | returndatasize | rds suc
-        // 0x0018 | 5f       | push0          | 0 rds suc
-        // 0x0019 | 5f       | push0          | 0 0 rds suc
-        // 0x001a | 3e       | returndatacopy | suc
-        // 0x001b | 5f       | push0          | 0 suc
-        // 0x001c | 3d       | returndatasize | rds 0 suc
-        // 0x001d | 91       | swap2          | suc 0 rds
-        // 0x001e | 6022     | push1 0x22     | 0x22 suc 0 rds
-        // 0x0020 | 57       | jumpi          | 0 rds
-        // 0x0021 | fd       | revert         |
-        // 0x0022 | 5b       | jumpdest       | 0 rds
-        // 0x0023 | f3       | return         |
+        // 0x0000 | 6013     | push1 0x13     | 0x13
+        // 0x0002 | 36       | calldatasize   | cds 0x13
+        // 0x0003 | 11       | gt             | (cds>0x13)
+        // 0x0004 | 600a     | push1 0x0a     | 0x0a (cds>0x13)
+        // 0x0006 | 57       | jumpi          | 0x0a (cds>0x13)
+        // 0x0007 | 5f       | push0          | 0
+        // 0x0008 | 5f       | push0          | 0 0
+        // 0x0009 | fd       | revert         |
+        // 0x000a | 5b       | jumpdest       |
+        // 0x000b | 6014     | push1 0x14     | 0x14
+        // 0x000d | 36       | calldatasize   | cds 0x14
+        // 0x000e | 03       | sub            | (cds-0x14)
+        // 0x000f | 6014     | push1 0x14     | 0x14 (cds-0x14)
+        // 0x0011 | 5f       | push0          | 0 0x14 (cds-0x14)
+        // 0x0012 | 37       | calldatacopy   |
+        // 0x0013 | 5f       | push0          | 0
+        // 0x0014 | 5f       | push0          | 0 0
+        // 0x0015 | 6014     | push1 0x14     | 0x14 0 0
+        // 0x0017 | 36       | calldatasize   | cds 0x14 0 0
+        // 0x0018 | 03       | sub            | (cds-0x14) 0 0
+        // 0x0019 | 5f       | push0          | 0 (cds-0x14) 0 0
+        // 0x001a | 34       | callvalue      | value 0 (cds-0x14) 0 0
+        // 0x001b | 5f       | push0          | 0 value 0 (cds-0x14) 0 0
+        // 0x001c | 35       | calldataload   | cd[0] value 0 (cds-0x14) 0 0
+        // 0x001d | 6060     | push1 0x60     | 0x60 cd[0] value 0 (cds-0x14) 0 0
+        // 0x001f | 1c       | shr            | target value 0 (cds-0x14) 0 0
+        // 0x0020 | 5a       | gas            | gas target value 0 (cds-0x14) 0 0
+        // 0x0021 | f1       | call           | suc
+        // 0x0022 | 3d       | returndatasize | rds suc
+        // 0x0023 | 5f       | push0          | 0 rds suc
+        // 0x0024 | 5f       | push0          | 0 0 rds suc
+        // 0x0025 | 3e       | returndatacopy | suc
+        // 0x0026 | 5f       | push0          | 0 suc
+        // 0x0027 | 3d       | returndatasize | rds 0 suc
+        // 0x0028 | 91       | swap2          | suc 0 rds
+        // 0x0029 | 602d     | push1 0x2d     | 0x2d suc 0 rds
+        // 0x002b | 57       | jumpi          | 0 rds
+        // 0x002c | fd       | revert         |
+        // 0x002d | 5b       | jumpdest       | 0 rds
+        // 0x002e | f3       | return         |
         assembly ("memory-safe") {
-            mstore(0x0e, 0xf13d5f5f3e5f3d91602257fd5bf3)
-            mstore(0x00, 0x3d602480600a3d3981f36014360360145f375f5f601436035f345f3560601c5a)
-            addr := create2(0, 0, 0x2f, 0)
+            mstore(0x19, 0x1436035f345f3560601c5af13d5f5f3e5f3d91602d57fd5bf3)
+            mstore(0x00, 0x3d602f80600a3d3981f360133611600a575f5ffd5b6014360360145f375f5f60)
+            addr := create2(0, 0, 0x39, 0)
             if iszero(addr) {
                 let ptr := mload(0x40)
                 returndatacopy(ptr, 0, returndatasize())
--- test/utils/RelayedCall.test.js
+++ test/utils/RelayedCall.test.js
@@ -19,27 +19,44 @@ describe('RelayedCall', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  it('call success', async function () {
-    await expect(
-      this.mock.$_relayedCallStrict(this.target, this.target.interface.encodeFunctionData('fnUnrestricted', [])),
-    )
-      .to.emit(this.target, 'CalledUnrestricted')
-      .withArgs(this.relayer);
+  describe('relayed call', function () {
+    it('target success', async function () {
+      await expect(
+        this.mock.$_relayedCallStrict(this.target, this.target.interface.encodeFunctionData('fnUnrestricted', [])),
+      )
+        .to.emit(this.target, 'CalledUnrestricted')
+        .withArgs(this.relayer);
+    });
+
+    it('target success (with value)', async function () {
+      const value = 42n;
+      await expect(this.mock.$_relayedCallStrict(this.receiver, value, '0x', { value })).to.changeEtherBalances(
+        [this.mock, this.relayer, this.receiver],
+        [0n, 0n, value],
+      );
+    });
+
+    it('target revert', async function () {
+      await expect(
+        this.mock.$_relayedCallStrict(this.target, this.target.interface.encodeFunctionData('fnRestricted', [])),
+      )
+        .to.be.revertedWithCustomError(this.target, 'AccessManagedUnauthorized')
+        .withArgs(this.relayer);
+    });
   });
 
-  it('call success with value', async function () {
-    const value = 42n;
-    await expect(this.mock.$_relayedCallStrict(this.receiver, value, '0x', { value })).to.changeEtherBalances(
-      [this.mock, this.relayer, this.receiver],
-      [0n, 0n, value],
-    );
-  });
-
-  it('call failure', async function () {
+  it('direct call to the relayer', async function () {
+    // 20 bytes (address + empty data) - OK
     await expect(
-      this.mock.$_relayedCallStrict(this.target, this.target.interface.encodeFunctionData('fnRestricted', [])),
-    )
-      .to.be.revertedWithCustomError(this.target, 'AccessManagedUnauthorized')
-      .withArgs(this.relayer);
+      this.mock.runner.sendTransaction({ to: this.relayer, data: '0x7859821024E633C5dC8a4FcF86fC52e7720Ce525' }),
+    ).to.not.be.reverted;
+
+    // 19 bytes (not enough for an address) - REVERT
+    await expect(
+      this.mock.runner.sendTransaction({ to: this.relayer, data: '0x7859821024E633C5dC8a4FcF86fC52e7720Ce5' }),
+    ).to.be.revertedWithoutReason();
+
+    // 0 bytes (not enough for an address) - REVERT
+    await expect(this.mock.runner.sendTransaction({ to: this.relayer, data: '0x' })).to.be.revertedWithoutReason();
   });
 });
