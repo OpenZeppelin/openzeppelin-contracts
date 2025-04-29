e8b16c598c523d9ccfd874b59633556187f754f9
Arr00 <13561405+arr00@users.noreply.github.com>
2025-04-29 17:23:52 -0400

    fix pragma

--- contracts/utils/Blockhash.sol
+++ contracts/utils/Blockhash.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
 /**
  * @dev Library for accessing historical block hashes beyond the standard 256 block limit.
