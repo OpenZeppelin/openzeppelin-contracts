be6875359dc9329c524f60e528686d0d1e83902d
ernestognw <ernestognw@gmail.com>
2025-05-02 15:57:04 -0600

    up

--- contracts/mocks/account/AccountMock.sol
+++ contracts/mocks/account/AccountMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
 import {Account} from "../../account/Account.sol";
 import {AccountERC7579} from "../../account/extensions/AccountERC7579.sol";
