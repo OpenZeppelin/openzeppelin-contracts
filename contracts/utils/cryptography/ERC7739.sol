087a844adc3e0e30d059ee6b271fe4cd26663971
ernestognw <ernestognw@gmail.com>
2025-05-02 00:43:29 -0600

    up

--- contracts/utils/cryptography/ERC7739.sol
+++ contracts/utils/cryptography/ERC7739.sol
@@ -18,8 +18,8 @@ import {ERC7739Utils} from "./ERC7739Utils.sol";
  * This contract requires implementing the {_rawSignatureValidation} function, which passes the wrapped message hash,
  * which may be either an typed data or a personal sign nested type.
  *
- * NOTE: xref:api:utils#EIP712[EIP-712] uses xref:api:utils#ShortStrings[ShortStrings] to optimize gas 
- * costs for short strings (up to 31 characters). Consider that strings longer than that will use storage, 
+ * NOTE: xref:api:utils#EIP712[EIP-712] uses xref:api:utils#ShortStrings[ShortStrings] to optimize gas
+ * costs for short strings (up to 31 characters). Consider that strings longer than that will use storage,
  * which may limit the ability of the signer to be used within the ERC-4337 validation phase (due to
  * https://eips.ethereum.org/EIPS/eip-7562#storage-rules[ERC-7562 storage access rules]).
  */
