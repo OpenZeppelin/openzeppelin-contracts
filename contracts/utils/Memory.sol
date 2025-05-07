14fa04ef86a0c78af70fae316d7cee2cae71304c
Ernesto García <ernestognw@gmail.com>
2025-05-07 12:30:10 -0600

    Update contracts/utils/Memory.sol
    Co-authored-by: Arr00 <13561405+arr00@users.noreply.github.com>

--- contracts/utils/Memory.sol
+++ contracts/utils/Memory.sol
@@ -2,13 +2,15 @@
 
 pragma solidity ^0.8.20;
 
-/// @dev Utilities to manipulate memory.
-///
-/// Memory is a contiguous and dynamic byte array in which Solidity stores non-primitive types.
-/// This library provides functions to manipulate pointers to this dynamic array.
-///
-/// WARNING: When manipulating memory, make sure to follow the Solidity documentation
-/// guidelines for https://docs.soliditylang.org/en/v0.8.20/assembly.html#memory-safety[Memory Safety].
+/**
+ *  @dev Utilities to manipulate memory.
+ *
+ * Memory is a contiguous and dynamic byte array in which Solidity stores non-primitive types.
+ * This library provides functions to manipulate pointers to this dynamic array.
+ *
+ * WARNING: When manipulating memory, make sure to follow the Solidity documentation
+ * guidelines for https://docs.soliditylang.org/en/v0.8.20/assembly.html#memory-safety[Memory Safety].
+ */
 library Memory {
     type Pointer is bytes32;
 
