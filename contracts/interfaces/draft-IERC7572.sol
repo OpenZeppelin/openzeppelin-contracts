bf047f82cdf32fc180ad48300201375802b0e3ad
Coffee☕️ <coffee@coffeebot.dev>
2025-05-13 23:48:49 -0400

    lint

--- contracts/interfaces/draft-IERC7572.sol
+++ contracts/interfaces/draft-IERC7572.sol
@@ -7,13 +7,12 @@ pragma solidity ^0.8.20;
 /// @dev Required interface of an ERC-7572 compliant contract, as defined in the
 /// https://eips.ethereum.org/EIPS/eip-7572[ERC].
 interface IERC7572 {
+    /// @dev This event should be emitted on updates to the contract metadata
+    /// to indicate to offchain indexers that they should query the contract
+    /// for the latest URI.
+    event ContractURIUpdated();
 
-  /// @dev This event should be emitted on updates to the contract metadata 
-  /// to indicate to offchain indexers that they should query the contract
-  /// for the latest URI.
-  event ContractURIUpdated();
-
-  /// @dev Returns an offchain resource for contract metadata or onchain JSON
-  /// data string (data:application/json;utf8,{}).
-  function contractURI() external view returns (string memory);
+    /// @dev Returns an offchain resource for contract metadata or onchain JSON
+    /// data string (data:application/json;utf8,{}).
+    function contractURI() external view returns (string memory);
 }
