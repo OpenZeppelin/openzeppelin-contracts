b12ca61a09acabae77d2747c21cb2d2b3d2b553d
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-02 10:42:23 +0200

    Merge Enumerable{Set,Map}Extended into Enumerable{Set,Map}

--- scripts/generate/templates/Enumerable.opts.js
+++ scripts/generate/templates/Enumerable.opts.js
@@ -1,23 +1,6 @@
 const { capitalize, mapValues } = require('../../helpers');
 
-const mapType = str => (str == 'uint256' ? 'Uint' : capitalize(str));
-
-const formatSetType = type => ({ name: `${mapType(type)}Set`, type });
-
-const SET_TYPES = ['bytes32', 'address', 'uint256'].map(formatSetType);
-
-const formatMapType = (keyType, valueType) => ({
-  name: `${mapType(keyType)}To${mapType(valueType)}Map`,
-  keyType,
-  valueType,
-});
-
-const MAP_TYPES = ['uint256', 'address', 'bytes32']
-  .flatMap((key, _, array) => array.map(value => [key, value]))
-  .slice(0, -1) // remove bytes32 → byte32 (last one) that is already defined
-  .map(args => formatMapType(...args));
-
-const extendedTypeDescr = ({ type, size = 0, memory = false }) => {
+const typeDescr = ({ type, size = 0, memory = false }) => {
   memory |= size > 0;
 
   const name = [type == 'uint256' ? 'Uint' : capitalize(type), size].filter(Boolean).join('x');
@@ -27,38 +10,46 @@ const extendedTypeDescr = ({ type, size = 0, memory = false }) => {
   return { name, type: typeFull, typeLoc, base, size, memory };
 };
 
-const toExtendedSetTypeDescr = value => ({ name: value.name + 'Set', value });
+const toSetTypeDescr = value => ({
+  name: value.name + 'Set',
+  value,
+});
 
-const toExtendedMapTypeDescr = ({ key, value }) => ({
+const toMapTypeDescr = ({ key, value }) => ({
   name: `${key.name}To${value.name}Map`,
-  keySet: toExtendedSetTypeDescr(key),
+  keySet: toSetTypeDescr(key),
   key,
   value,
 });
 
-const EXTENDED_SET_TYPES = [
+const SET_TYPES = [
+  { type: 'bytes32' },
+  { type: 'address' },
+  { type: 'uint256' },
   { type: 'bytes32', size: 2 },
   { type: 'string', memory: true },
   { type: 'bytes', memory: true },
 ]
-  .map(extendedTypeDescr)
-  .map(toExtendedSetTypeDescr);
+  .map(typeDescr)
+  .map(toSetTypeDescr);
 
-const EXTENDED_MAP_TYPES = [
-  { key: { type: 'bytes', memory: true }, value: { type: 'uint256' } },
-  { key: { type: 'string', memory: true }, value: { type: 'string', memory: true } },
-]
-  .map(entry => mapValues(entry, extendedTypeDescr))
-  .map(toExtendedMapTypeDescr);
+const MAP_TYPES = []
+  .concat(
+    // value type maps
+    ['uint256', 'address', 'bytes32']
+      .flatMap((keyType, _, array) => array.map(valueType => ({ key: { type: keyType }, value: { type: valueType } })))
+      .slice(0, -1), // remove bytes32 → byte32 (last one) that is already defined
+    // non-value type maps
+    { key: { type: 'bytes', memory: true }, value: { type: 'uint256' } },
+    { key: { type: 'string', memory: true }, value: { type: 'string', memory: true } },
+  )
+  .map(entry => mapValues(entry, typeDescr))
+  .map(toMapTypeDescr);
 
 module.exports = {
   SET_TYPES,
   MAP_TYPES,
-  EXTENDED_SET_TYPES,
-  EXTENDED_MAP_TYPES,
-  formatSetType,
-  formatMapType,
-  extendedTypeDescr,
-  toExtendedSetTypeDescr,
-  toExtendedMapTypeDescr,
+  typeDescr,
+  toSetTypeDescr,
+  toMapTypeDescr,
 };
