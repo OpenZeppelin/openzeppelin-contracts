9a321354660fd654866d8f25a48ae05e5d960d82
ernestognw <ernestognw@gmail.com>
2025-06-03 23:11:01 -0600

    Increase SignatureChecker minimum pragma to 0.8.24

--- contracts/governance/extensions/GovernorSuperQuorum.sol
+++ contracts/governance/extensions/GovernorSuperQuorum.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.3.0) (governance/extensions/GovernorSuperQuorum.sol)
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {Governor} from "../Governor.sol";
 
