856f98e55bbccd20a388cc9e688d53c04ddb2a23
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-06 09:41:31 +0200

    refactor for readability

--- scripts/minimize-pragma.js
+++ scripts/minimize-pragma.js
@@ -2,13 +2,14 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const glob = require('glob');
 const graphlib = require('graphlib');
+const match = require('micromatch');
 const path = require('path');
 const semver = require('semver');
 
 const { range, unique } = require('./helpers');
 
-const pattern = 'contracts/**/*.sol';
-const exclude = ['contracts/mocks/'];
+const pattern = 'contracts/*.sol';
+const exclude = ['contracts/mocks/**'];
 
 const solcVersionsMaxPatch = ['0.4.26', '0.5.16', '0.6.12', '0.7.6', '0.8.30'];
 const minVersionForContracts = '0.8.20';
@@ -16,6 +17,10 @@ const minVersionForInterfaces = '0.0.0';
 const allSolcVersions = solcVersionsMaxPatch
   .map(semver.parse)
   .flatMap(({ major, minor, patch }) => range(patch + 1).map(p => `${major}.${minor}.${p}`));
+
+/********************************************************************************************************************
+ *                                                     HELPERS                                                      *
+ ********************************************************************************************************************/
 
 /**
  * Updates the pragma in the given file to the newPragma version.
@@ -66,57 +71,69 @@ const getMinimalApplicablePragma = (file, candidates = allSolcVersions) =>
     }
   });
 
-// MAIN
+/**
+ * Get the minimum applicable pragmas for a given file, and update the file to use it.
+ * @param {*} file Absolute path to the file to compile.
+ * @param {*} candidates List of solc version to test. (ex: ['0.8.4','0.8.5'])
+ * @param {*} prefix Prefix to use when building the pragma (ex: '^')
+ * @returns {Promise<string>} Version that was used and set in the file
+ */
+const setMinimalApplicablePragma = (file, candidates = allSolcVersions, prefix = '>=') =>
+  getMinimalApplicablePragma(file, candidates)
+    .then(version => `${prefix}${version}`)
+    .then(pragma => {
+      updatePragma(file, pragma);
+      return pragma;
+    });
+
+/********************************************************************************************************************
+ *                                                       MAIN                                                       *
+ ********************************************************************************************************************/
+
+// extract metadata for all source files
+const files = glob.sync(pattern).filter(file => !match.any(file, exclude));
+
+const metadata = Object.fromEntries(
+  files.map(file => {
+    // Get all objects (contracts, libraries, interfaces) defined in the file
+    const items = glob.sync(`out/${path.basename(file)}/*`);
+    // Get the (deduplicated) sources for each of these object. If we don't filter, we get some 'lib/forge-std' stuff we don't want.
+    const sources = unique(
+      items.flatMap(path => Object.keys(JSON.parse(fs.readFileSync(path)).metadata.sources)),
+    ).filter(item => item !== file && files.includes(item));
+    // Check if all objects are interfaces.
+    const isInterface = items.every(item => path.basename(item).match(/^I[A-Z]/));
+
+    return [file, { sources, isInterface }];
+  }),
+);
+
+// Build dependency graph
+const graph = new graphlib.Graph({ directed: true });
+Object.keys(metadata).forEach(file => {
+  graph.setNode(file);
+  metadata[file].sources.forEach(dep => graph.setEdge(dep, file));
+});
+
+// Weaken all pragma to allow exploration
+files.forEach(file => updatePragma(file, '>=0.0.0'));
+
+// Do a topological traversal of the dependency graph, minimizing pragma for each file we encounter
 (async () => {
-  // extract metadata for all source files
-  const files = glob.sync(pattern).filter(file => exclude.every(e => path.relative(e, file).startsWith('..')));
-  const metadata = Object.fromEntries(
-    files.map(file => {
-      // Get all objects (contracts, libraries, interfaces) defined in the file
-      const items = glob.sync(`out/${path.basename(file)}/*`);
-      // Get the (deduplicated) sources for each of these object. If we don't filter, we get some 'lib/forge-std' stuff we don't want.
-      const sources = unique(
-        items.flatMap(path => Object.keys(JSON.parse(fs.readFileSync(path)).metadata.sources)),
-      ).filter(item => item !== file && files.includes(item));
-      // Check if all objects are interfaces.
-      const isInterface = items.every(item => path.basename(item).match(/^I[A-Z]/));
+  const queue = graph.sources();
+  const minimized = {};
+  while (queue.length) {
+    const file = queue.shift();
+    if (!Object.hasOwn(minimized, file)) {
+      const minVersion = metadata[file].isInterface ? minVersionForInterfaces : minVersionForContracts;
+      const candidates = allSolcVersions.filter(v => semver.gte(v, minVersion));
+      const pragmaPrefix = metadata[file].isInterface ? '>=' : '^';
 
-      return [file, { sources, isInterface }];
-    }),
-  );
+      process.stdout.write(`Searching minimal version for ${file} ... `);
+      await setMinimalApplicablePragma(file, candidates, pragmaPrefix).then(console.log);
 
-  // Build dependency graph
-  const graph = new graphlib.Graph({ directed: true });
-  Object.keys(metadata).forEach(file => {
-    graph.setNode(file);
-    metadata[file].sources.forEach(dep => graph.setEdge(dep, file));
-  });
-  // break cycles :/
-  graphlib.alg.findCycles(graph).forEach(([a, b]) => graph.removeEdge(a, b));
-
-  // Build a topological order of the graph
-  const visited = {};
-  const ordered = [];
-  const visit = node => {
-    if (!Object.hasOwn(visited, node)) {
-      visited[node] = true;
-      graph.predecessors(node).forEach(visit);
-      ordered.push(node);
+      minimized[file] = true;
+      queue.push(...graph.successors(file));
     }
-  };
-  graph.sinks().forEach(visit);
-
-  // Weaken all pragma to allow exploration
-  ordered.forEach(file => updatePragma(file, '>=0.0.0'));
-
-  // Find minimal pragma for each file
-  for (const file of ordered) {
-    const minVersion = metadata[file].isInterface ? minVersionForInterfaces : minVersionForContracts;
-    const candidates = allSolcVersions.filter(v => semver.gte(v, minVersion));
-
-    process.stdout.write(`Searching minimal pragma for ${file} ... `);
-    const pragma = await getMinimalApplicablePragma(file, candidates);
-    updatePragma(file, metadata[file].isInterface ? `>=${pragma}` : `^${pragma}`);
-    console.log(pragma);
   }
 })();
