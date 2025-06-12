529454739aa9714c2720920ca6a8ba9c41392e4e
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-06-12 10:16:37 +0200

    Add script to automatically minimize pragma

--- contracts/mocks/DummyImplementation.sol
+++ contracts/mocks/DummyImplementation.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.21;
 
 import {ERC1967Utils} from "../proxy/ERC1967/ERC1967Utils.sol";
 import {StorageSlot} from "../utils/StorageSlot.sol";
