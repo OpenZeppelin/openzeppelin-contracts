7bd43fd57450f71c1569aedf6e430d59139ec784
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-07-02 21:28:59 +0200

    Update SafeERC20.sol
    Co-authored-by: Arr00 <13561405+arr00@users.noreply.github.com>

--- contracts/token/ERC20/utils/SafeERC20.sol
+++ contracts/token/ERC20/utils/SafeERC20.sol
@@ -239,7 +239,7 @@ library SafeERC20 {
                 // - we got returndata (in that case mload(0x00) is a real false)
                 // - the token address doesn't have code
                 if and(success, or(gt(returndatasize(), 0), iszero(extcodesize(token)))) {
-                    // than the transfer is a failure
+                    // then the transfer is a failure
                     success := 0
                 }
             }
