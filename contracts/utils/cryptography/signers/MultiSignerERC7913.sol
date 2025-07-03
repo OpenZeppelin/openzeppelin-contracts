7303972a5c863bf48ab2d3f850def9bdfccf2884
ernestognw <ernestognw@gmail.com>
2025-07-03 16:43:55 -0600

    Address 5.4 audit documentation improvements

--- contracts/utils/cryptography/signers/MultiSignerERC7913.sol
+++ contracts/utils/cryptography/signers/MultiSignerERC7913.sol
@@ -225,7 +225,7 @@ abstract contract MultiSignerERC7913 is AbstractSigner {
      *
      * Requirements:
      *
-     * * The `signatures` arrays must be at least as large as the `signers` arrays. Panics otherwise.
+     * * The `signatures` arrays must be equal in length to the `signers` arrays. Panics otherwise.
      */
     function _validateSignatures(
         bytes32 hash,
