0aaa23e57df7eb0949a1d9ae90742c85f771a7cf
Ernesto García <ernestognw@gmail.com>
2025-07-03 13:51:56 -0600

    Minimize pragma for MultiSignerERC7913Weighted.sol (#5778)

--- contracts/utils/cryptography/signers/MultiSignerERC7913Weighted.sol
+++ contracts/utils/cryptography/signers/MultiSignerERC7913Weighted.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.26;
 
 import {SafeCast} from "../../math/SafeCast.sol";
 import {MultiSignerERC7913} from "./MultiSignerERC7913.sol";
