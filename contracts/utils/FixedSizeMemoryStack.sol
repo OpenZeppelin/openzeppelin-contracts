47db11b5e36512ff9c7cce79bb9a1930e50de115
Wael Almattar <waelsy123@gmail.com>
2025-07-04 09:36:33 +0200

    docs: changeset

--- contracts/utils/FixedSizeMemoryStack.sol
+++ contracts/utils/FixedSizeMemoryStack.sol
@@ -1,4 +1,4 @@
-// SPDX‑License‑Identifier: MIT
+// SPDX-License-Identifier:  MIT
 pragma solidity ^0.8.24;
 
 /// @title FixedSizeMemoryStack
--- test/utils/FixedSizeMemoryStack.t.sol
+++ test/utils/FixedSizeMemoryStack.t.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier:  MIT
 pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
