340e94ccd5d6076e4dc7852f96cc040e5f9ddb33
Ernesto García <ernestognw@gmail.com>
2025-07-09 13:44:20 -0600

    Update Memory.sol
    Co-authored-by: Arr00 <13561405+arr00@users.noreply.github.com>

--- contracts/utils/Memory.sol
+++ contracts/utils/Memory.sol
@@ -21,9 +21,11 @@ library Memory {
         }
     }
 
-    /// @dev Sets the free `Pointer` to a specific value.
-    ///
-    /// WARNING: Everything after the pointer may be overwritten.
+    /**
+     * @dev Sets the free `Pointer` to a specific value.
+     *
+     * WARNING: Everything after the pointer may be overwritten.
+     **/
     function setFreeMemoryPointer(Pointer ptr) internal pure {
         assembly ("memory-safe") {
             mstore(0x40, ptr)
