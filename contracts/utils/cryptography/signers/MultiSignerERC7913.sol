86c2fa9c61eb2d50deec20d2258616c6c40fb3a2
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-07-09 09:31:46 +0200

    up

--- contracts/utils/cryptography/signers/MultiSignerERC7913.sol
+++ contracts/utils/cryptography/signers/MultiSignerERC7913.sol
@@ -151,7 +151,8 @@ abstract contract MultiSignerERC7913 is AbstractSigner {
      *
      * Requirements:
      *
-     * * The {getSignerCount}'s length must be `>=` to the {threshold}. Throws {MultiSignerERC7913UnreachableThreshold} if not.
+     * * The {getSignerCount} must be greater or equal than to the {threshold}. Throws
+     * {MultiSignerERC7913UnreachableThreshold} if not.
      */
     function _validateReachableThreshold() internal view virtual {
         uint256 signersLength = _signers.length();
