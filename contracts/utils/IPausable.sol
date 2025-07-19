2081a1011c553f63c769fa599648f1722d5cecbd
mukku <mukeshvermagbd20@gmail.com>
2025-07-19 15:22:12 +0000

    refactor: move Paused and Unpaused events to IPausable interface (#5791)

---
+++ contracts/utils/IPausable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @title IPausable
+/// @dev Interface declaring Paused and Unpaused events
+interface IPausable {
+    event Paused(address account);
+    event Unpaused(address account);
+}
