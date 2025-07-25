56da3b57f35787d895f4fece78aa614fbaacfd1d
Ernesto García <ernestognw@gmail.com>
2025-07-25 07:50:36 -0600

    Update contracts/utils/Bytes.sol
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>

--- contracts/utils/Bytes.sol
+++ contracts/utils/Bytes.sol
@@ -188,7 +188,7 @@ library Bytes {
     }
 
     /**
-     * @dev Counts the number of leading zeros in bits a bytes array. Returns `8 * buffer.length`
+     * @dev Counts the number of leading zero bits a bytes array. Returns `8 * buffer.length`
      * if the buffer is all zeros.
      */
     function clz(bytes memory buffer) internal pure returns (uint256) {
