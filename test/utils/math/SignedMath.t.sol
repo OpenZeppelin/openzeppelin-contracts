060e5b096cb5ee8240cba304f6e0961c6b556787
Daejun Park <daejunpark@gmail.com>
2025-07-25 16:14:16 -0700

    test: enable some math tests to be run by halmos

--- test/utils/math/SignedMath.t.sol
+++ test/utils/math/SignedMath.t.sol
@@ -48,7 +48,8 @@ contract SignedMathTest is Test {
     }
 
     // 2. more complex test, full int256 range
-    function testAverage2(int256 a, int256 b) public pure {
+    /// @custom:halmos --solver-timeout-assertion 0
+    function testSymbolicAverage2(int256 a, int256 b) public pure {
         (int256 result, int256 min, int256 max) = (
             SignedMath.average(a, b),
             SignedMath.min(a, b),
