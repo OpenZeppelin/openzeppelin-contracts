8098fb209a6838fb6c9a25bb39cd8c6675200bdc
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-08-27 17:36:06 +0200

    Update test/utils/Base58.t.sol

--- test/utils/Base58.t.sol
+++ test/utils/Base58.t.sol
@@ -7,7 +7,7 @@ import {Base58} from "@openzeppelin/contracts/utils/Base58.sol";
 
 contract Base58Test is Test {
     function testEncodeDecodeEmpty() external pure {
-        assertEq(Base58.decode(Base58.encode("")), "");
+        assertEq(Base58.decode(Base58.encode(hex"")), hex"");
     }
 
     function testEncodeDecodeZeros() external pure {
