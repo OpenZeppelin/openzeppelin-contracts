959d0aa97af1d089c32d2ecf9d34192e8d6c8af9
ernestognw <ernestognw@gmail.com>
2025-10-03 14:45:57 -0600

    up

--- contracts/token/ERC1155/extensions/ERC1155Burnable.sol
+++ contracts/token/ERC1155/extensions/ERC1155Burnable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (token/ERC1155/extensions/ERC1155Burnable.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {ERC1155} from "../ERC1155.sol";
 
