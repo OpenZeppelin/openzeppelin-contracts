5c1d1fdd836e7339acdd6858540549bb1a6a9c8e
Ernesto García <ernestognw@gmail.com>
2025-10-20 15:35:56 -0600

    Update test/utils/Bytes.test.js

--- test/utils/Bytes.test.js
+++ test/utils/Bytes.test.js
@@ -130,7 +130,7 @@ describe('Bytes', function () {
       });
 
       it('replacement longer than buffer', async function () {
-        const longReplacement = ethers.toUtf8Bytes('THIS IS A VERY LONG REPLACEMENT THAT EXCEEDS THE BUFFER SIZE');
+        const longReplacement = ethers.toUtf8Bytes('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi placerat elit non felis scelerisque faucibus. Donec eget blandit.');
         const buffer = new Uint8Array(lorem);
         const expected = new Uint8Array(buffer);
         expected.set(longReplacement.slice(0, Math.min(longReplacement.length, buffer.length)), 0);
