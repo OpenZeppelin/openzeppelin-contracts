aa1b25a7238a62a1f6a2decd3e4be32362c27c54
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2025-10-31 14:14:05 +0100

    Check upgradeable separately

--- scripts/release/workflow/github-release-upgradeable.sh
+++ scripts/release/workflow/github-release-upgradeable.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+ADDITIONAL_OPTION_IF_PRERELEASE=""
+if [ "$PRERELEASE" = "true" ]; then
+  ADDITIONAL_OPTION_IF_PRERELEASE="--prerelease"
+fi
 gh release create "${GIT_TAG}" \
   --repo="${UPGRADEABLE_REPO}" \
   --title="${GIT_TAG}" \
+  --target="${RELEASE_COMMIT}" \
   --notes="$(gh release view "${OLD_GIT_TAG}" --repo="${VANILLA_REPO}" --json body -q .body)" `# TODO: Update tag before merging` \
   "${ADDITIONAL_OPTION_IF_PRERELEASE}"
--- scripts/release/workflow/publish-upgradeable.sh
+++ scripts/release/workflow/publish-upgradeable.sh
@@ -1,33 +1,9 @@
 #!/usr/bin/env bash
 
-cd $UPGRADEABLE_DIR
+set -euo pipefail
 
-if ! git log -1 --pretty=%B | grep -q "Transpile ${VANILLA_COMMIT}"; then
-  echo "Expected 'Transpile ${VANILLA_COMMIT}' but found '$(git log -1 --pretty=%B)'"
-  exit 1
-fi
-VERSION="$(jq -r .version contracts/package.json)"
-GIT_TAG="v${VERSION}"
-NPM_TAG="tmp"
-ADDITIONAL_OPTION_IF_PRERELEASE="--prerelease"
-if [[ "${GIT_TAG}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  NPM_TAG="dev"
-  ADDITIONAL_OPTION_IF_PRERELEASE=""
-elif [[ "${GIT_TAG}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+-rc.[0-9]+$ ]]; then
-  NPM_TAG="next"
-fi
-echo "additional_option_if_prerelease=${ADDITIONAL_OPTION_IF_PRERELEASE}" >> "$GITHUB_OUTPUT"
-### [START BLOCK] TODO: Remove block before merging
-TIMESTAMPED_VERSION="${VERSION}-$(date +%s)"
-echo "OLD_GIT_TAG=${GIT_TAG}" >> "$GITHUB_ENV"
-GIT_TAG="${GIT_TAG}-$(date +%s)" # incremental git tag for testing
-sed -i'' -e 's/openzeppelin\/contracts-upgradeable/james-toussaint\/contracts-upgradeable/g' contracts/package.json # custom scope for testing
-sed -i'' -e "s/${VERSION}/${TIMESTAMPED_VERSION}/g" contracts/package.json && head contracts/package.json # incremental npm package version for testing
-### [END BLOCK]
+eval $GO_TO_UPGRADEABLE_DIR
 sed -i'' -e 's/OpenZeppelin\/openzeppelin-contracts-upgradeable/james-toussaint\/openzeppelin-contracts/g' contracts/package.json # repository.url for provenance (TODO: Update and try keep upgradeable url)
-git tag -m {,}"${GIT_TAG}"
-CI=true git push origin tag "${GIT_TAG}"
 npm ci
 cd "contracts/"
 npm publish --tag "${NPM_TAG}"
-echo "git_tag=${GIT_TAG}" >> "$GITHUB_OUTPUT"
