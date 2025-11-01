d3f9d42aec425c1bf1d3340ed15970ed9c097918
MozirDmitriy <dmitriymozir@gmail.com>
2025-11-01 13:54:59 +0300

    remove unused ERC1155InvalidApprover error

--- contracts/interfaces/draft-IERC6093.sol
+++ contracts/interfaces/draft-IERC6093.sol
@@ -141,13 +141,6 @@ interface IERC1155Errors {
     error ERC1155MissingApprovalForAll(address operator, address owner);
 
     /**
-     * @dev Non-standard for ERC-1155. ERC-1155 only defines operator-wide approvals via {setApprovalForAll}.
-     * @deprecated Non-standard for ERC-1155; use {ERC1155InvalidOperator} or {ERC1155MissingApprovalForAll} instead.
-     * @param approver Address initiating an approval operation.
-     */
-    error ERC1155InvalidApprover(address approver);
-
-    /**
      * @dev Indicates a failure with the `operator` to be approved. Used in approvals.
      * @param operator Address that may be allowed to operate on tokens without being their owner.
      */
