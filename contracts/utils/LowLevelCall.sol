b7f1901d574a8e5533caf690c6499d5d93baec40
Micke <155267459+reallesee@users.noreply.github.com>
2025-11-06 13:44:04 +0100

    fix: correct LowLevelCall overload reference

--- contracts/utils/LowLevelCall.sol
+++ contracts/utils/LowLevelCall.sol
@@ -34,7 +34,7 @@ library LowLevelCall {
         return callReturn64Bytes(target, 0, data);
     }
 
-    /// @dev Same as {callReturnBytes32Pair}, but allows to specify the value to be sent in the call.
+    /// @dev Same as the {callReturn64Bytes(address,bytes)} overload, but allows to specify the value to be sent in the call.
     function callReturn64Bytes(
         address target,
         uint256 value,
