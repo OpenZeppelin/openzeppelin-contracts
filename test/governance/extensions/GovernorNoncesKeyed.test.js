509b91dbbea9ab89e76e3296d515521308ac2c49
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-11-06 15:47:47 +0100

    Refactor getNonce function for cleaner syntax

--- test/governance/extensions/GovernorNoncesKeyed.test.js
+++ test/governance/extensions/GovernorNoncesKeyed.test.js
@@ -91,11 +91,10 @@ describe('GovernorNoncesKeyed', function () {
 
           const maskedProposalId = BigInt(this.helper.id) & (2n ** 192n - 1n);
 
-          this.getNonce = async address => {
-            return nonceType === 'default'
+          this.getNonce = address =>
+            nonceType === 'default'
               ? this.mock.nonces(address)
               : this.mock['nonces(address,uint192)'](address, maskedProposalId);
-          };
         });
 
         it('votes with an EOA signature', async function () {
