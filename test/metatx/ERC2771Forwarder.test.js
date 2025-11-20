8925666450beb98cb11ea9f645e2b48cd14d73eb
Forostovec <ilonaforostovec22@gmail.com>
2025-11-20 15:33:04 +0200

    fix(test): use this.value instead of undefined this.amount

--- test/metatx/ERC2771Forwarder.test.js
+++ test/metatx/ERC2771Forwarder.test.js
@@ -268,7 +268,7 @@ describe('ERC2771Forwarder', function () {
             this.accounts[1],
           );
 
-          await expect(this.forwarder.executeBatch(this.requests, this.refundReceiver, { value: this.amount }))
+          await expect(this.forwarder.executeBatch(this.requests, this.refundReceiver, { value: this.value }))
             .to.be.revertedWithCustomError(this.forwarder, 'ERC2771ForwarderExpiredRequest')
             .withArgs(this.requests[idx].deadline);
         });
