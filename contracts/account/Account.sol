ff23994aa55fdda4d1af4363b9dbe3303851ab34
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-11-22 05:04:54 +0100

    Bump ERC-4337 entrypoint to version 0.9

--- contracts/account/Account.sol
+++ contracts/account/Account.sol
@@ -50,7 +50,7 @@ abstract contract Account is AbstractSigner, IAccount {
      * @dev Canonical entry point for the account that forwards and validates user operations.
      */
     function entryPoint() public view virtual returns (IEntryPoint) {
-        return ERC4337Utils.ENTRYPOINT_V08;
+        return ERC4337Utils.ENTRYPOINT_V09;
     }
 
     /**
