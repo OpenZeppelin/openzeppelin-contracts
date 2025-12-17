d96e92a71e7018d2b6d2481edbe551bf380c071d
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-12-17 11:55:24 +0100

    Update github-release-upgradeable.sh

--- scripts/release/workflow/github-release-upgradeable.sh
+++ scripts/release/workflow/github-release-upgradeable.sh
@@ -6,5 +6,5 @@ gh release create "${GIT_TAG}" \
   --repo="${UPGRADEABLE_REPO}" \
   --title="${GIT_TAG}" \
   --target="${RELEASE_COMMIT}" \
-  --notes="$(gh release view "${OLD_GIT_TAG}" --repo="${VANILLA_REPO}" --json body -q .body)" `# TODO: Update tag before merging` \
+  --notes="$(gh release view "${OLD_GIT_TAG}" --repo="${REFERENCE_REPO}" --json body -q .body)" `# TODO: Update tag before merging` \
   --prerelease="${PRERELEASE}"
