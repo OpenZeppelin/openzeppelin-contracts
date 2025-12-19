1b1d274c9a5dc9ba2fd7749c63e6a2a6c8b6c96a
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-12-19 15:01:31 +0100

    Update check-upgradeable.sh

--- scripts/release/workflow/check-upgradeable.sh
+++ scripts/release/workflow/check-upgradeable.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+# Get commit hash (short) of the reference commit (non-upgradeable version)
 cd lib/openzeppelin-contracts
 REFERENCE_COMMIT="$(git rev-parse --short HEAD)"
 
+# Check that the commit message of the local commit (upgradeable version) matches the reference commit
 cd ../..
 if ! git log -1 --pretty=%B | grep -q "Transpile ${REFERENCE_COMMIT}"; then
   echo "Expected 'Transpile ${REFERENCE_COMMIT}' but found '$(git log -1 --pretty=%B)'"
   exit 1
 fi
 
+# Read the version from the package.json, and check whether that corresponds to a pre-release
 VERSION="$(jq -r .version contracts/package.json)"
 if [[ "${VERSION}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   PRERELEASE="false"
