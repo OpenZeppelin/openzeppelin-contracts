f0d4989a30db55f096c82182ed744d2391030453
Arr00 <13561405+arr00@users.noreply.github.com>
2025-12-31 21:04:29 +0200

    correct pragmas

--- contracts/token/ERC20/extensions/draft-ERC7246.sol
+++ contracts/token/ERC20/extensions/draft-ERC7246.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import {ERC20} from "../ERC20.sol";
 import {IERC7246} from "../../../interfaces/draft-IERC7246.sol";
