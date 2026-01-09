a1a0a67a2050f5b0edac2bb64ba679cb07a88943
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-01-09 17:42:20 +0100

    Target Osaka EVM during tests (#6217)
    Co-authored-by: ernestognw <ernestognw@gmail.com>

--- test/utils/cryptography/P256.t.sol
+++ test/utils/cryptography/P256.t.sol
@@ -16,6 +16,7 @@ contract P256Test is Test {
         (bytes32 r, bytes32 s) = vm.signP256(privateKey, digest);
         s = _ensureLowerS(s);
         assertTrue(P256.verify(digest, r, s, bytes32(x), bytes32(y)));
+        assertTrue(P256.verifyNative(digest, r, s, bytes32(x), bytes32(y)));
         assertTrue(P256.verifySolidity(digest, r, s, bytes32(x), bytes32(y)));
     }
 
@@ -31,22 +32,6 @@ contract P256Test is Test {
         assertTrue((qx0 == bytes32(x) && qy0 == bytes32(y)) || (qx1 == bytes32(x) && qy1 == bytes32(y)));
     }
 
-    function testVerifyNativeUnsupportedRIP7212(bytes32 digest, uint256 seed) public {
-        // By default, the precompile at address 0x100 is not supported.
-
-        uint256 privateKey = _asPrivateKey(seed);
-
-        (uint256 x, uint256 y) = vm.publicKeyP256(privateKey);
-        (bytes32 r, bytes32 s) = vm.signP256(privateKey, digest);
-        s = _ensureLowerS(s);
-
-        (bool success, bytes memory returndata) = address(this).call(
-            abi.encodeCall(P256Test.verifyNative, (digest, r, s, bytes32(x), bytes32(y)))
-        );
-        assertFalse(success);
-        assertEq(returndata, abi.encodeWithSelector(Errors.MissingPrecompile.selector, address(0x100)));
-    }
-
     function _asPrivateKey(uint256 seed) private pure returns (uint256) {
         return bound(seed, 1, P256.N - 1);
     }
@@ -56,10 +41,5 @@ contract P256Test is Test {
         unchecked {
             return _s > P256.N / 2 ? bytes32(P256.N - _s) : s;
         }
-    }
-
-    // See https://github.com/foundry-rs/foundry/issues/10237
-    function verifyNative(bytes32 digest, bytes32 r, bytes32 s, bytes32 x, bytes32 y) external view {
-        P256.verifyNative(digest, r, s, x, y);
     }
 }
