6158e567a925ef4733ef74947404674d2ca1c9c6
Abhiram Satpute <abhiram.satpute@gmail.com>
2026-01-18 14:28:09 +0530

    lint fix in Arrays.js

--- scripts/generate/templates/Arrays.js
+++ scripts/generate/templates/Arrays.js
@@ -85,7 +85,7 @@ function _quickSort(uint256 begin, uint256 end, function(uint256, uint256) pure 
             }
 
             _swap(begin, pos); // Swap pivot into place
-            
+
             // Sort the smaller partition recursively, and the larger partition iteratively
             // This reduces maximum recursion depth from O(n) to O(log n)
             if (pos - begin < end - (pos + 0x20)) {
