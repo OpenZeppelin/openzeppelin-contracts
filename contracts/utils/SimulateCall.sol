eee8a28fa8ef3a98d6c12e0204358668f4afb7d6
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-01-22 09:36:49 +0100

    Refactor SimulateCall to delegatecall into a single relayer (no more salt)

--- contracts/utils/SimulateCall.sol
+++ contracts/utils/SimulateCall.sol
@@ -12,7 +12,7 @@ pragma solidity ^0.8.20;
 library SimulateCall {
     /// @dev Simulates a call to the target contract through a dynamically deployed simulator.
     function simulateCall(address target, bytes memory data) internal returns (bool success, bytes memory retData) {
-        return simulateCall(target, 0, data, bytes32(0));
+        return simulateCall(target, 0, data);
     }
 
     /// @dev Same as {simulateCall-address-bytes} but with a value.
@@ -21,112 +21,77 @@ library SimulateCall {
         uint256 value,
         bytes memory data
     ) internal returns (bool success, bytes memory retData) {
-        return simulateCall(target, value, data, bytes32(0));
+        (success, retData) = getSimulator().delegatecall(abi.encodePacked(target, value, data));
+        success = !success;
     }
 
-    /// @dev Same as {simulateCall-address-bytes} but with a salt for deterministic simulator address.
-    function simulateCall(
-        address target,
-        bytes memory data,
-        bytes32 salt
-    ) internal returns (bool success, bytes memory retData) {
-        return simulateCall(target, 0, data, salt);
-    }
-
-    /// @dev Same as {simulateCall-address-bytes} but with a salt and a value.
-    function simulateCall(
-        address target,
-        uint256 value,
-        bytes memory data,
-        bytes32 salt
-    ) internal returns (bool success, bytes memory retData) {
-        // Calls to a simulator always revert. No need to check the success flag.
-        (, retData) = getSimulator(salt).call{value: value}(abi.encodePacked(target, data));
-
-        assembly ("memory-safe") {
-            // length of the returned buffer (result/reason + success byte)
-            let length := mload(retData)
-
-            // extract the success byte (last byte of the returned buffer)
-            success := and(mload(add(retData, length)), 0xff)
-
-            // shrink retData to exclude the success byte
-            mstore(retData, sub(length, 1))
-        }
-    }
-
-    /// @dev Same as {getSimulator} but with a `bytes32(0)` default salt.
-    function getSimulator() internal returns (address) {
-        return getSimulator(bytes32(0));
-    }
-
-    /// @dev Returns the simulator address for a given salt.
-    function getSimulator(bytes32 salt) internal returns (address simulator) {
+    /// @dev Returns the simulator address.
+    function getSimulator() internal returns (address instance) {
         // [Simulator details]
-        //
-        // deployment prefix: 602e5f8160095f39f3
-        // deployed bytecode: 60133611600a575f5ffd5b6014360360145f375f5f601436035f345f3560601c5af13d5f5f3e3d533d6001015ffd
+        // deployment prefix: 60315f8160095f39f3
+        // deployed bytecode: 60333611600a575f5ffd5b6034360360345f375f5f603436035f6014355f3560601c5af13d5f5f3e5f3d91602f57f35bfd
         //
         // offset | bytecode    | opcode         | stack
         // -------|-------------|----------------|--------
-        // 0x0000 | 6013        | push1 0x13     | 0x13
-        // 0x0002 | 36          | calldatasize   | cds 0x13
-        // 0x0003 | 11          | gt             | (cds>0x13)
-        // 0x0004 | 600a        | push1 0x0a     | 0x0a (cds>0x13)
+        // 0x0000 | 6033        | push1 0x33     | 0x33
+        // 0x0002 | 36          | calldatasize   | cds 0x33
+        // 0x0003 | 11          | gt             | (cds>0x33)
+        // 0x0004 | 600a        | push1 0x0a     | 0x0a (cds>0x33)
         // 0x0006 | 57          | jumpi          |
         // 0x0007 | 5f          | push0          | 0
         // 0x0008 | 5f          | push0          | 0 0
         // 0x0009 | fd          | revert         |
         // 0x000a | 5b          | jumpdest       |
-        // 0x000b | 6014        | push1 0x14     | 0x14
-        // 0x000d | 36          | calldatasize   | cds 0x14
-        // 0x000e | 03          | sub            | (cds-0x14)
-        // 0x000f | 6014        | push1 0x14     | 0x14 (cds-0x14)
-        // 0x0011 | 5f          | push0          | 0 0x14 (cds-0x14)
+        // 0x000b | 6034        | push1 0x34     | 0x34
+        // 0x000d | 36          | calldatasize   | cds 0x34
+        // 0x000e | 03          | sub            | (cds-0x34)
+        // 0x000f | 6034        | push1 0x34     | 0x34 (cds-0x34)
+        // 0x0011 | 5f          | push0          | 0 0x34 (cds-0x34)
         // 0x0012 | 37          | calldatacopy   |
         // 0x0013 | 5f          | push0          | 0
         // 0x0014 | 5f          | push0          | 0 0
-        // 0x0015 | 6014        | push1 0x14     | 0x14 0 0
-        // 0x0017 | 36          | calldatasize   | cds 0x14 0 0
-        // 0x0018 | 03          | sub            | (cds-0x14) 0 0
-        // 0x0019 | 5f          | push0          | 0 (cds-0x14) 0 0
-        // 0x001a | 34          | callvalue      | value 0 (cds-0x14) 0 0
-        // 0x001b | 5f          | push0          | 0 value 0 (cds-0x14) 0 0
-        // 0x001c | 35          | calldataload   | cd[0] value 0 (cds-0x14) 0 0
-        // 0x001d | 6060        | push1 0x60     | 0x60 cd[0] value 0 (cds-0x14) 0 0
-        // 0x001f | 1c          | shr            | target value 0 (cds-0x14) 0 0
-        // 0x0020 | 5a          | gas            | gas target value 0 (cds-0x14) 0 0
-        // 0x0021 | f1          | call           | suc
-        // 0x0022 | 3d          | returndatasize | rds suc
-        // 0x0023 | 5f          | push0          | 0 rds suc
-        // 0x0024 | 5f          | push0          | 0 0 rds suc
-        // 0x0025 | 3e          | returndatacopy | suc
-        // 0x0026 | 3d          | returndatasize | rds suc
-        // 0x0027 | 53          | mstore8        |
-        // 0x0028 | 3d          | returndatasize | rds
-        // 0x0029 | 6001        | push1 0x01     | 0x01 rds
-        // 0x002b | 01          | add            | (rds+0x01)
-        // 0x002c | 5f          | push0          | 0 (rds+0x01)
-        // 0x002d | fd          | revert         |
-
+        // 0x0015 | 6034        | push1 0x34     | 0x34 0 0
+        // 0x0017 | 36          | calldatasize   | cds 0x34 0 0
+        // 0x0018 | 03          | sub            | (cds-0x34) 0 0
+        // 0x0019 | 5f          | push0          | 0 (cds-0x34) 0 0
+        // 0x001a | 6014        | push1 0x14     | 0x14 0 (cds-0x34) 0 0
+        // 0x001c | 35          | calldataload   | cd[0x14] 0 (cds-0x34) 0 0
+        // 0x001d | 5f          | push0          | 0 cd[0x14] 0 (cds-0x34) 0 0
+        // 0x001e | 35          | calldataload   | cd[0] cd[0x14] 0 (cds-0x34) 0 0
+        // 0x001f | 6060        | push1 0x60     | 0x60 cd[0] cd[0x14] 0 (cds-0x34) 0 0
+        // 0x0021 | 1c          | shr            | target cd[0x14] 0 (cds-0x34) 0 0
+        // 0x0022 | 5a          | gas            | gas target cd[0x14] 0 (cds-0x34) 0 0
+        // 0x0023 | f1          | call           | suc
+        // 0x0024 | 3d          | returndatasize | rds suc
+        // 0x0025 | 5f          | push0          | 0 rds suc
+        // 0x0026 | 5f          | push0          | 0 0 rds suc
+        // 0x0027 | 3e          | returndatacopy | suc
+        // 0x0028 | 5f          | push0          | 0 suc
+        // 0x0029 | 3d          | returndatasize | rds 0 suc
+        // 0x002a | 91          | swap2          | suc 0 rds
+        // 0x002b | 602f        | push1 0x2f     | 0x2f suc 0 rds
+        // 0x002d | 57          | jumpi          | 0 rds
+        // 0x002e | f3          | return         |
+        // 0x002f | 5b          | jumpdest       | 0 rds
+        // 0x0030 | fd          | revert         |
         assembly ("memory-safe") {
             // build initcode at scratch space
-            mstore(0x20, 0x0360145f375f5f601436035f345f3560601c5af13d5f5f3e3d533d6001015ffd)
-            mstore(0x00, 0x602e5f8160095f39f360133611600a575f5ffd5b601436)
-            let initcodehash := keccak256(0x09, 0x37)
+            mstore(0x20, 0x5f375f5f603436035f6014355f3560601c5af13d5f5f3e5f3d91602f57f35bfd)
+            mstore(0x00, 0x60315f8160095f39f360333611600a575f5ffd5b603436036034)
+            let initcodehash := keccak256(0x06, 0x3a)
 
             let fmp := mload(0x40) // cache free memory pointer
 
             // compute create2 address
             mstore(add(fmp, 0x40), initcodehash)
-            mstore(add(fmp, 0x20), salt)
+            mstore(add(fmp, 0x20), 0)
             mstore(add(fmp, 0x00), address())
             mstore8(add(fmp, 0x0b), 0xff)
-            simulator := and(keccak256(add(fmp, 0x0b), 0x55), shr(96, not(0)))
+            instance := and(keccak256(add(fmp, 0x0b), 0x55), shr(96, not(0)))
 
             // if simulator not yet deployed, deploy it
-            if iszero(extcodesize(simulator)) {
-                if iszero(create2(0, 0x09, 0x37, salt)) {
+            if iszero(extcodesize(instance)) {
+                if iszero(create2(0, 0x06, 0x3a, 0)) {
                     returndatacopy(fmp, 0x00, returndatasize())
                     revert(fmp, returndatasize())
                 }
--- test/utils/SimulatedCall.test.js
+++ test/utils/SimulatedCall.test.js
@@ -6,21 +6,20 @@ async function fixture() {
   const [receiver, other] = await ethers.getSigners();
 
   const mock = await ethers.deployContract('$SimulateCall');
-  const computeSimulatorAddress = (salt = ethers.ZeroHash) =>
-    ethers.getCreate2Address(
-      mock.target,
-      salt,
-      ethers.keccak256(
-        ethers.concat([
-          '0x602e5f8160095f39f3',
-          '0x60133611600a575f5ffd5b6014360360145f375f5f601436035f345f3560601c5af13d5f5f3e3d533d6001015ffd',
-        ]),
-      ),
-    );
+  const simulator = ethers.getCreate2Address(
+    mock.target,
+    ethers.ZeroHash,
+    ethers.keccak256(
+      ethers.concat([
+        '0x60315f8160095f39f3',
+        '0x60333611600a575f5ffd5b6034360360345f375f5f603436035f6014355f3560601c5af13d5f5f3e5f3d91602f57f35bfd',
+      ]),
+    ),
+  );
 
   const target = await ethers.deployContract('$CallReceiverMock');
 
-  return { mock, target, receiver, other, computeSimulatorAddress };
+  return { mock, target, receiver, other, simulator };
 }
 
 describe('SimulateCall', function () {
@@ -28,129 +27,62 @@ describe('SimulateCall', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  describe('default (zero) salt', function () {
-    beforeEach(async function () {
-      this.simulator = await this.computeSimulatorAddress();
-    });
+  it('automatic simulator deployment', async function () {
+    await expect(ethers.provider.getCode(this.simulator)).to.eventually.equal('0x');
 
-    it('automatic simulator deployment', async function () {
-      await expect(ethers.provider.getCode(this.simulator)).to.eventually.equal('0x');
+    // First call performs deployment
+    await expect(this.mock.$getSimulator()).to.emit(this.mock, 'return$getSimulator').withArgs(this.simulator);
 
-      // First call performs deployment
-      await expect(this.mock.$getSimulator()).to.emit(this.mock, 'return$getSimulator').withArgs(this.simulator);
+    await expect(ethers.provider.getCode(this.simulator)).to.eventually.not.equal('0x');
 
-      await expect(ethers.provider.getCode(this.simulator)).to.eventually.not.equal('0x');
-
-      // Following calls use the same simulator
-      await expect(this.mock.$getSimulator()).to.emit(this.mock, 'return$getSimulator').withArgs(this.simulator);
-    });
-
-    describe('simulated call', function () {
-      it('target success', async function () {
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.target),
-          ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionWithArgsReturn', [10, 20])),
-        );
-        await expect(tx)
-          .to.emit(this.mock, 'return$simulateCall_address_bytes')
-          .withArgs(true, ethers.AbiCoder.defaultAbiCoder().encode(['uint256', 'uint256'], [10, 20]))
-          .to.not.emit(this.target, 'MockFunctionCalledWithArgs');
-      });
-
-      it('target success (with value)', async function () {
-        const value = 42n;
-
-        // fund the mock
-        await this.other.sendTransaction({ to: this.mock.target, value });
-
-        // perform simulated call
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.receiver),
-          ethers.Typed.uint256(value),
-          ethers.Typed.bytes('0x'),
-        );
-
-        await expect(tx).to.changeEtherBalances([this.mock, this.simulator, this.receiver], [0n, 0n, 0n]);
-        await expect(tx).to.emit(this.mock, 'return$simulateCall_address_uint256_bytes').withArgs(true, '0x');
-      });
-
-      it('target revert', async function () {
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.target),
-          ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionRevertsReason', [])),
-        );
-
-        await expect(tx)
-          .to.emit(this.mock, 'return$simulateCall_address_bytes')
-          .withArgs(false, this.target.interface.encodeErrorResult('Error', ['CallReceiverMock: reverting']));
-      });
-    });
+    // Following calls use the same simulator
+    await expect(this.mock.$getSimulator()).to.emit(this.mock, 'return$getSimulator').withArgs(this.simulator);
   });
 
-  describe('random salt', function () {
-    beforeEach(async function () {
-      this.salt = ethers.hexlify(ethers.randomBytes(32));
-      this.simulator = await this.computeSimulatorAddress(this.salt);
+  describe('simulated call', function () {
+    it('target success', async function () {
+      const txPromise = this.mock.$simulateCall(
+        ethers.Typed.address(this.target),
+        ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionWithArgsReturn', [10, 20])),
+      );
+
+      await expect(txPromise).to.changeEtherBalances([this.mock, this.simulator, this.target], [0n, 0n, 0n]);
+      await expect(txPromise)
+        .to.emit(this.mock, 'return$simulateCall_address_bytes')
+        .withArgs(true, ethers.AbiCoder.defaultAbiCoder().encode(['uint256', 'uint256'], [10, 20]))
+        .to.not.emit(this.target, 'MockFunctionCalledWithArgs');
     });
 
-    it('automatic simulator deployment', async function () {
-      await expect(ethers.provider.getCode(this.simulator)).to.eventually.equal('0x');
+    it('target success (with value)', async function () {
+      const value = 42n;
 
-      // First call performs deployment
-      await expect(this.mock.$getSimulator(ethers.Typed.bytes32(this.salt)))
-        .to.emit(this.mock, 'return$getSimulator_bytes32')
-        .withArgs(this.simulator);
+      // fund the mock
+      await this.other.sendTransaction({ to: this.mock.target, value });
 
-      await expect(ethers.provider.getCode(this.simulator)).to.eventually.not.equal('0x');
+      // perform simulated call
+      const txPromise = this.mock.$simulateCall(
+        ethers.Typed.address(this.target),
+        ethers.Typed.uint256(value),
+        ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionExtra')),
+      );
 
-      // Following calls use the same simulator
-      await expect(this.mock.$getSimulator(ethers.Typed.bytes32(this.salt)))
-        .to.emit(this.mock, 'return$getSimulator_bytes32')
-        .withArgs(this.simulator);
+      await expect(txPromise).to.changeEtherBalances([this.mock, this.simulator, this.target], [0n, 0n, 0n]);
+      await expect(txPromise)
+        .to.emit(this.mock, 'return$simulateCall_address_uint256_bytes')
+        .withArgs(true, ethers.AbiCoder.defaultAbiCoder().encode(['address', 'uint256'], [this.mock.target, value]))
+        .to.not.emit(this.target, 'MockFunctionCalledExtra');
     });
 
-    describe('simulated call', function () {
-      it('target success', async function () {
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.target),
-          ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionWithArgsReturn', [10, 20])),
-          ethers.Typed.bytes32(this.salt),
-        );
-        await expect(tx)
-          .to.emit(this.mock, 'return$simulateCall_address_bytes_bytes32')
-          .withArgs(true, ethers.AbiCoder.defaultAbiCoder().encode(['uint256', 'uint256'], [10, 20]))
-          .to.not.emit(this.target, 'MockFunctionCalledWithArgs');
-      });
+    it('target revert', async function () {
+      const txPromise = this.mock.$simulateCall(
+        ethers.Typed.address(this.target),
+        ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionRevertsReason')),
+      );
 
-      it('target success (with value)', async function () {
-        const value = 42n;
-
-        // fund the mock
-        await this.other.sendTransaction({ to: this.mock.target, value });
-
-        // perform simulated call
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.receiver),
-          ethers.Typed.uint256(value),
-          ethers.Typed.bytes('0x'),
-          ethers.Typed.bytes32(this.salt),
-        );
-
-        await expect(tx).to.changeEtherBalances([this.mock, this.simulator, this.receiver], [0n, 0n, 0n]);
-        await expect(tx).to.emit(this.mock, 'return$simulateCall_address_uint256_bytes_bytes32').withArgs(true, '0x');
-      });
-
-      it('target revert', async function () {
-        const tx = this.mock.$simulateCall(
-          ethers.Typed.address(this.target),
-          ethers.Typed.bytes(this.target.interface.encodeFunctionData('mockFunctionRevertsReason', [])),
-          ethers.Typed.bytes32(this.salt),
-        );
-
-        await expect(tx)
-          .to.emit(this.mock, 'return$simulateCall_address_bytes_bytes32')
-          .withArgs(false, this.target.interface.encodeErrorResult('Error', ['CallReceiverMock: reverting']));
-      });
+      await expect(txPromise).to.changeEtherBalances([this.mock, this.simulator, this.target], [0n, 0n, 0n]);
+      await expect(txPromise)
+        .to.emit(this.mock, 'return$simulateCall_address_bytes')
+        .withArgs(false, this.target.interface.encodeErrorResult('Error', ['CallReceiverMock: reverting']));
     });
   });
 });
