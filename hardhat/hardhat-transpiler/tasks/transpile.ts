de13540af23f906d874c8ed8bc481b7310ca14df
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-02-02 18:25:27 +0100

    Refactor transpile

--- hardhat/hardhat-transpiler/tasks/transpile.ts
+++ hardhat/hardhat-transpiler/tasks/transpile.ts
@@ -34,58 +34,66 @@ export default async function ({ settings }: { settings?: string }, hre: Hardhat
   assert(settings, 'Transpile settings file must be provided');
   const options = await loadSettings(hre, settings);
 
-  // Clean compilation (should result in a single build info)
-  await hre.tasks.getTask('clean').run();
-  await hre.tasks.getTask('compile').run({ noTests: true });
+  const { contractRootPaths } = await hre.tasks.getTask('compile').run({ noTests: true });
+  const compilationJobs = await hre.solidity.getCompilationJobs(contractRootPaths);
+  assert('cacheHits' in compilationJobs, 'Compilation jobs not found');
 
-  const [buildId, ...rest] = await hre.artifacts.getAllBuildInfoIds();
-  assert(buildId && rest.length == 0, 'No build info ids found');
+  const buildIds: Set<string> = new Set();
+  for (const { buildId } of compilationJobs.cacheHits.values()) {
+    buildIds.add(buildId);
+  }
 
-  // Load build info
-  const { input, solcVersion } = await hre.artifacts
-    .getBuildInfoPath(buildId)
-    .then(file => fs.readFile(file!, 'utf-8'))
-    .then(JSON.parse);
-  const { output } = await hre.artifacts
-    .getBuildInfoOutputPath(buildId)
-    .then(file => fs.readFile(file!, 'utf-8'))
-    .then(JSON.parse);
+  const keep: Set<string> = new Set();
+  const seen: Set<string> = new Set();
 
-  // Run transpilation on the first source folder
-  const transpiled = await transpile(
-    input,
-    output,
-    { root: hre.config.paths.root, sources: hre.config.paths.sources.solidity.at(0)! },
-    { ...options, solcVersion },
-  );
+  for (const buildId of buildIds) {
+    // Load build info
+    const { input, solcVersion } = await hre.artifacts
+      .getBuildInfoPath(buildId)
+      .then(file => fs.readFile(file!, 'utf-8'))
+      .then(JSON.parse);
+    const { output } = await hre.artifacts
+      .getBuildInfoOutputPath(buildId)
+      .then(file => fs.readFile(file!, 'utf-8'))
+      .then(JSON.parse);
 
-  // Write transpiled files to disk
-  await Promise.all(
-    transpiled.map(async t => {
-      const outputPath = path.join(hre.config.paths.root, t.path.replace(/^project\//, ''));
-      await fs.mkdir(path.dirname(outputPath), { recursive: true });
-      await fs.writeFile(outputPath, t.source);
-    }),
-  );
+    // Run transpilation on the first source folder
+    const transpiled = await transpile(
+      input,
+      output,
+      { root: hre.config.paths.root, sources: hre.config.paths.sources.solidity.at(0)! },
+      { ...options, solcVersion },
+    );
 
-  // Delete originals files
-  if (options.deleteOriginals) {
-    const keep = new Set(
+    // Write transpiled files to disk
+    await Promise.all(
+      transpiled.map(async t => {
+        const outputPath = path.join(hre.config.paths.root, t.path.replace(/^project\//, ''));
+        await fs.mkdir(path.dirname(outputPath), { recursive: true });
+        await fs.writeFile(outputPath, t.source);
+      }),
+    );
+
+    // Delete originals files
+    if (options.deleteOriginals) {
       ([] as string[])
         .concat(
           transpiled.map(t => t.path),
           findAlreadyInitializable(output, options.initializablePath),
         )
-        .map(p => path.join(hre.config.paths.root, p.replace(/^project\//, ''))),
-    );
-    if (options.initializablePath && options.peerProject === undefined) {
-      keep.add(path.join(hre.config.paths.root, options.initializablePath.replace(/^project\//, '')));
-    }
-    await Promise.all(
+        .map(p => path.join(hre.config.paths.root, p.replace(/^project\//, '')))
+        .forEach(p => keep.add(p));
+
+      if (options.initializablePath && options.peerProject === undefined) {
+        keep.add(path.join(hre.config.paths.root, options.initializablePath.replace(/^project\//, '')));
+      }
+
       Object.keys(output.sources)
         .map(s => path.join(hre.config.paths.root, s.replace(/^project\//, '')))
-        .filter(p => !keep.has(p))
-        .map(p => fs.unlink(p).catch(() => {})),
-    );
+        .forEach(p => seen.add(p));
+    }
   }
+
+  // Perform delete
+  await Promise.all([...seen].filter(p => !keep.has(p)).map(p => fs.unlink(p).catch(() => {})));
 }
