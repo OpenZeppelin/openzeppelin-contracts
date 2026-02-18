c83f9721a1d0f4d69c1a6b460745151b6bc6beab
ernestognw <ernestognw@gmail.com>
2026-02-18 10:06:09 -0600

    Skip SignatureChecker for r,s,v functions

--- contracts/token/ERC20/extensions/ERC20TransferAuthorization.sol
+++ contracts/token/ERC20/extensions/ERC20TransferAuthorization.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "../ERC20.sol";
 import {EIP712} from "../../../utils/cryptography/EIP712.sol";
 import {SignatureChecker} from "../../../utils/cryptography/SignatureChecker.sol";
+import {ECDSA} from "../../../utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "../../../utils/cryptography/MessageHashUtils.sol";
 import {IERC3009, IERC3009Cancel} from "../../../interfaces/draft-IERC3009.sol";
 
@@ -76,7 +77,12 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         bytes32 r,
         bytes32 s
     ) public virtual {
-        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce, abi.encodePacked(r, s, v));
+        bytes32 hash = _hashTypedDataV4(
+            keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
+        );
+        (address recovered, ECDSA.RecoverError err, ) = ECDSA.tryRecover(hash, v, r, s);
+        require(err == ECDSA.RecoverError.NoError && recovered == from, ERC3009InvalidSignature());
+        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce);
     }
 
     /// @dev Same as {transferWithAuthorization} but with a bytes signature.
@@ -89,7 +95,11 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         bytes32 nonce,
         bytes memory signature
     ) public virtual {
-        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce, signature);
+        bytes32 hash = _hashTypedDataV4(
+            keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
+        );
+        require(SignatureChecker.isValidSignatureNow(from, hash, signature), ERC3009InvalidSignature());
+        _transferWithAuthorization(from, to, value, validAfter, validBefore, nonce);
     }
 
     /// @inheritdoc IERC3009
@@ -104,7 +114,12 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         bytes32 r,
         bytes32 s
     ) public virtual {
-        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce, abi.encodePacked(r, s, v));
+        bytes32 hash = _hashTypedDataV4(
+            keccak256(abi.encode(RECEIVE_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
+        );
+        (address recovered, ECDSA.RecoverError err, ) = ECDSA.tryRecover(hash, v, r, s);
+        require(err == ECDSA.RecoverError.NoError && recovered == from, ERC3009InvalidSignature());
+        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce);
     }
 
     /// @dev Same as {receiveWithAuthorization} but with a bytes signature.
@@ -117,17 +132,26 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         bytes32 nonce,
         bytes memory signature
     ) public virtual {
-        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce, signature);
+        bytes32 hash = _hashTypedDataV4(
+            keccak256(abi.encode(RECEIVE_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
+        );
+        require(SignatureChecker.isValidSignatureNow(from, hash, signature), ERC3009InvalidSignature());
+        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce);
     }
 
     /// @inheritdoc IERC3009Cancel
     function cancelAuthorization(address authorizer, bytes32 nonce, uint8 v, bytes32 r, bytes32 s) public virtual {
-        _cancelAuthorization(authorizer, nonce, abi.encodePacked(r, s, v));
+        bytes32 hash = _hashTypedDataV4(keccak256(abi.encode(CANCEL_AUTHORIZATION_TYPEHASH, authorizer, nonce)));
+        (address recovered, ECDSA.RecoverError err, ) = ECDSA.tryRecover(hash, v, r, s);
+        require(err == ECDSA.RecoverError.NoError && recovered == authorizer, ERC3009InvalidSignature());
+        _cancelAuthorization(authorizer, nonce);
     }
 
     /// @dev Same as {cancelAuthorization} but with a bytes signature.
     function cancelAuthorization(address authorizer, bytes32 nonce, bytes memory signature) public virtual {
-        _cancelAuthorization(authorizer, nonce, signature);
+        bytes32 hash = _hashTypedDataV4(keccak256(abi.encode(CANCEL_AUTHORIZATION_TYPEHASH, authorizer, nonce)));
+        require(SignatureChecker.isValidSignatureNow(authorizer, hash, signature), ERC3009InvalidSignature());
+        _cancelAuthorization(authorizer, nonce);
     }
 
     /// @dev Internal version of {transferWithAuthorization} that accepts a bytes signature.
@@ -137,19 +161,13 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         uint256 value,
         uint256 validAfter,
         uint256 validBefore,
-        bytes32 nonce,
-        bytes memory signature
+        bytes32 nonce
     ) internal virtual {
         require(
             block.timestamp > validAfter && block.timestamp < validBefore,
             ERC3009InvalidAuthorizationTime(validAfter, validBefore)
         );
         require(!authorizationState(from, nonce), ERC3009ConsumedAuthorization(from, nonce));
-        bytes32 hash = _hashTypedDataV4(
-            keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
-        );
-        require(SignatureChecker.isValidSignatureNow(from, hash, signature), ERC3009InvalidSignature());
-
         _consumed[from][nonce] = true;
         emit AuthorizationUsed(from, nonce);
         _transfer(from, to, value);
@@ -162,8 +180,7 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
         uint256 value,
         uint256 validAfter,
         uint256 validBefore,
-        bytes32 nonce,
-        bytes memory signature
+        bytes32 nonce
     ) internal virtual {
         require(to == _msgSender(), ERC20InvalidReceiver(to));
         require(
@@ -171,22 +188,14 @@ abstract contract ERC20TransferAuthorization is ERC20, EIP712, IERC3009, IERC300
             ERC3009InvalidAuthorizationTime(validAfter, validBefore)
         );
         require(!authorizationState(from, nonce), ERC3009ConsumedAuthorization(from, nonce));
-        bytes32 hash = _hashTypedDataV4(
-            keccak256(abi.encode(RECEIVE_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce))
-        );
-        require(SignatureChecker.isValidSignatureNow(from, hash, signature), ERC3009InvalidSignature());
-
         _consumed[from][nonce] = true;
         emit AuthorizationUsed(from, nonce);
         _transfer(from, to, value);
     }
 
     /// @dev Internal version of {cancelAuthorization} that accepts a bytes signature.
-    function _cancelAuthorization(address authorizer, bytes32 nonce, bytes memory signature) internal virtual {
+    function _cancelAuthorization(address authorizer, bytes32 nonce) internal virtual {
         require(!authorizationState(authorizer, nonce), ERC3009ConsumedAuthorization(authorizer, nonce));
-        bytes32 hash = _hashTypedDataV4(keccak256(abi.encode(CANCEL_AUTHORIZATION_TYPEHASH, authorizer, nonce)));
-        require(SignatureChecker.isValidSignatureNow(authorizer, hash, signature), ERC3009InvalidSignature());
-
         _consumed[authorizer][nonce] = true;
         emit AuthorizationCanceled(authorizer, nonce);
     }
