9e21544d96e8938f48704393e05bdb6b13557205
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-02-19 21:12:58 +0100

    pragma

--- contracts/governance/extensions/GovernorCrosschain.sol
+++ contracts/governance/extensions/GovernorCrosschain.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import {Governor} from "../Governor.sol";
 import {Mode} from "../../account/utils/draft-ERC7579Utils.sol";
