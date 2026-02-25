39799af33508af8eff50158b724ee2ff5cc3bbed
ernestognw <ernestognw@gmail.com>
2026-02-25 13:16:29 -0600

    up

--- contracts/mocks/docs/account/paymaster/PaymasterECDSASigner.sol
+++ contracts/mocks/docs/account/paymaster/PaymasterECDSASigner.sol
@@ -2,9 +2,9 @@
 
 pragma solidity ^0.8.20;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {PackedUserOperation} from "@openzeppelin/contracts/interfaces/draft-IERC4337.sol";
-import {SignerECDSA} from "@openzeppelin/contracts/utils/cryptography/signers/SignerECDSA.sol";
+import {Ownable} from "../../../../access/Ownable.sol";
+import {PackedUserOperation} from "../../../../interfaces/draft-IERC4337.sol";
+import {SignerECDSA} from "../../../../utils/cryptography/signers/SignerECDSA.sol";
 import {PaymasterSigner, EIP712} from "../../../../account/paymaster/PaymasterSigner.sol";
 
 contract PaymasterECDSASigner is PaymasterSigner, SignerECDSA, Ownable {
