7e1f23d6930f384e0898803bc4a500182e09ab4e
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-03-03 15:52:52 +0100

    remove executor registration

--- test/governance/extensions/GovernorCrosschain.test.js
+++ test/governance/extensions/GovernorCrosschain.test.js
@@ -68,7 +68,7 @@ describe('GovernorCrosschain', function () {
     Object.assign(this, await loadFixture(fixture));
   });
 
-  it('execute with unregistered executor', async function () {
+  it('execute with executor', async function () {
     this.helper.setProposal(
       [
         {
@@ -93,44 +93,15 @@ describe('GovernorCrosschain', function () {
     await expect(this.helper.execute()).to.emit(this.receiver, 'MockFunctionCalledExtra').withArgs(this.executor, 0n);
   });
 
-  it('register executor and execute with registered executor', async function () {
-    this.helper.setProposal(
-      [
-        {
-          target: this.governor.target,
-          data: this.governor.interface.encodeFunctionData('registerRemoteExecutor', [
-            this.gateway.target,
-            this.chain.toErc7930(this.executor),
-          ]),
-        },
-        {
-          target: this.governor.target,
-          data: this.governor.interface.encodeFunctionData('relayCrosschain(bytes,bytes32,bytes)', [
-            this.chain.erc7930,
-            encodeMode({ callType: CALL_TYPE_SINGLE }),
-            encodeSingle(this.receiver, 0n, this.receiver.interface.encodeFunctionData('mockFunctionExtra')),
-          ]),
-        },
-      ],
-      '<proposal description>',
-    );
-
-    await this.helper.propose();
-    await this.helper.waitForSnapshot();
-    await this.helper.connect(this.voter1).vote({ support: VoteType.For });
-    await this.helper.connect(this.voter2).vote({ support: VoteType.For });
-    await this.helper.waitForDeadline();
-
-    await expect(this.helper.execute())
-      .to.emit(this.governor, 'RemoteExecutorRegistered')
-      .withArgs(this.gateway, this.chain.toErc7930(this.executor))
-      .to.emit(this.receiver, 'MockFunctionCalledExtra')
-      .withArgs(this.executor, 0n);
-
-    await expect(this.governor.getRemoteExecutor(this.chain.erc7930)).to.eventually.deep.equal([
-      this.gateway.target,
-      this.chain.toErc7930(this.executor),
-    ]);
+  it('relayCrosschain is onlyGovernance', async function () {
+    await expect(
+      this.governor.getFunction('relayCrosschain(address,bytes,bytes32,bytes)')(
+        this.gateway,
+        this.chain.toErc7930(this.executor),
+        encodeMode({ callType: CALL_TYPE_SINGLE }),
+        encodeSingle(this.receiver, 0n, this.receiver.interface.encodeFunctionData('mockFunctionExtra')),
+      ),
+    ).to.be.revertedWithCustomError(this.governor, 'GovernorOnlyExecutor');
   });
 
   it('reconfigure executor', async function () {
@@ -183,34 +154,5 @@ describe('GovernorCrosschain', function () {
     // After reconfiguration
     await expect(this.executor.gateway()).to.eventually.equal(this.gateway);
     await expect(this.executor.controller()).to.eventually.equal(this.chain.toErc7930(newGovernor));
-  });
-
-  describe('restricted', function () {
-    it('relayCrosschain with unregistered executor is onlyGovernance', async function () {
-      await expect(
-        this.governor.getFunction('relayCrosschain(address,bytes,bytes32,bytes)')(
-          this.gateway,
-          this.chain.toErc7930(this.executor),
-          encodeMode({ callType: CALL_TYPE_SINGLE }),
-          encodeSingle(this.receiver, 0n, this.receiver.interface.encodeFunctionData('mockFunctionExtra')),
-        ),
-      ).to.be.revertedWithCustomError(this.governor, 'GovernorOnlyExecutor');
-    });
-
-    it('relayCrosschain with registered executor is onlyGovernance', async function () {
-      await expect(
-        this.governor.getFunction('relayCrosschain(bytes,bytes32,bytes)')(
-          this.chain.erc7930,
-          encodeMode({ callType: CALL_TYPE_SINGLE }),
-          encodeSingle(this.receiver, 0n, this.receiver.interface.encodeFunctionData('mockFunctionExtra')),
-        ),
-      ).to.be.revertedWithCustomError(this.governor, 'GovernorOnlyExecutor');
-    });
-
-    it('registerRemoteExecutor is onlyGovernance', async function () {
-      await expect(
-        this.governor.registerRemoteExecutor(this.gateway, this.chain.toErc7930(this.executor)),
-      ).to.be.revertedWithCustomError(this.governor, 'GovernorOnlyExecutor');
-    });
   });
 });
