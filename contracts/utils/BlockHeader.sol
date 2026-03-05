c4e86a8a3c0d36352ea5ddd95240eb84f15ddb91
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-03-05 20:14:22 +0100

    update

--- contracts/utils/BlockHeader.sol
+++ contracts/utils/BlockHeader.sol
@@ -69,126 +69,123 @@ library BlockHeader {
 
     /// @dev Extract the parent hash from the block header RLP.
     function getParentHash(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.ParentHash)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.ParentHash, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the ommers hash from the block header RLP. This is constant to keccak256(rlp([])) since EIP-3675 (Paris)
     function getOmmersHash(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.OmmersHash)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.OmmersHash, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the coinbase (a.k.a. beneficiary or miner) address from the block header RLP.
     function getCoinbase(bytes memory headerRLP) internal pure returns (address) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.Coinbase)].readAddress();
+        return _parseHeader(headerRLP, HeaderField.Coinbase, Hardforks.Frontier).readAddress();
     }
 
     /// @dev Extract the state root from the block header RLP.
     function getStateRoot(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.StateRoot)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.StateRoot, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the transactions root from the block header RLP.
     function getTransactionsRoot(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.TransactionsRoot)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.TransactionsRoot, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the receipts root from the block header RLP.
     function getReceiptsRoot(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.ReceiptsRoot)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.ReceiptsRoot, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the logs bloom from the block header RLP.
     function getLogsBloom(bytes memory headerRLP) internal pure returns (bytes memory) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.LogsBloom)].readBytes();
+        return _parseHeader(headerRLP, HeaderField.LogsBloom, Hardforks.Frontier).readBytes();
     }
 
     /// @dev Extract the difficulty from the block header RLP. This is constant to 0 since EIP-3675 (Paris)
     function getDifficulty(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.Difficulty)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.Difficulty, Hardforks.Frontier).readUint256();
     }
 
     /// @dev Extract the block number from the block header RLP.
     function getNumber(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.Number)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.Number, Hardforks.Frontier).readUint256();
     }
 
     /// @dev Extract the gas used from the block header RLP.
     function getGasUsed(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.GasUsed)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.GasUsed, Hardforks.Frontier).readUint256();
     }
 
     /// @dev Extract the gas limit from the block header RLP.
     function getGasLimit(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.GasLimit)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.GasLimit, Hardforks.Frontier).readUint256();
     }
 
     /// @dev Extract the timestamp from the block header RLP.
     function getTimestamp(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.Timestamp)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.Timestamp, Hardforks.Frontier).readUint256();
     }
 
     /// @dev Extract the extra data from the block header RLP.
     function getExtraData(bytes memory headerRLP) internal pure returns (bytes memory) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.ExtraData)].readBytes();
+        return _parseHeader(headerRLP, HeaderField.ExtraData, Hardforks.Frontier).readBytes();
     }
 
     /// @dev Extract the prevRandao (a.k.a. mixHash before Paris) from the block header RLP.
     function getPrevRandao(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.PrevRandao)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.PrevRandao, Hardforks.Frontier).readBytes32();
     }
 
     /// @dev Extract the nonce from the block header RLP. This is constant to 0 since EIP-3675 (Paris)
     function getNonce(bytes memory headerRLP) internal pure returns (bytes8) {
-        return bytes8(_parseHeader(headerRLP, Hardforks.Frontier)[uint8(HeaderField.Nonce)].readUint256().toUint64());
+        return bytes8(_parseHeader(headerRLP, HeaderField.Nonce, Hardforks.Frontier).readUint256().toUint64());
     }
 
     /// @dev Extract the base fee per gas from the block header RLP. This was introduced in London.
     function getBaseFeePerGas(bytes memory headerRLP) internal pure returns (uint256) {
-        return _parseHeader(headerRLP, Hardforks.London)[uint8(HeaderField.BaseFeePerGas)].readUint256();
+        return _parseHeader(headerRLP, HeaderField.BaseFeePerGas, Hardforks.London).readUint256();
     }
 
     /// @dev Extract the withdrawals root from the block header RLP. This was introduced in Shanghai.
     function getWithdrawalsRoot(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Shanghai)[uint8(HeaderField.WithdrawalsRoot)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.WithdrawalsRoot, Hardforks.Shanghai).readBytes32();
     }
 
     /// @dev Extract the blob gas used from the block header RLP. This was introduced in Cancun.
     function getBlobGasUsed(bytes memory headerRLP) internal pure returns (uint64) {
-        return _parseHeader(headerRLP, Hardforks.Cancun)[uint8(HeaderField.BlobGasUsed)].readUint256().toUint64();
+        return _parseHeader(headerRLP, HeaderField.BlobGasUsed, Hardforks.Cancun).readUint256().toUint64();
     }
 
     /// @dev Extract the excess blob gas from the block header RLP. This was introduced in Cancun.
     function getExcessBlobGas(bytes memory headerRLP) internal pure returns (uint64) {
-        return _parseHeader(headerRLP, Hardforks.Cancun)[uint8(HeaderField.ExcessBlobGas)].readUint256().toUint64();
+        return _parseHeader(headerRLP, HeaderField.ExcessBlobGas, Hardforks.Cancun).readUint256().toUint64();
     }
 
     /// @dev Extract the parent beacon block root from the block header RLP. This was introduced in Cancun.
     function getParentBeaconBlockRoot(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Cancun)[uint8(HeaderField.ParentBeaconBlockRoot)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.ParentBeaconBlockRoot, Hardforks.Cancun).readBytes32();
     }
 
     /// @dev Extract the requests hash from the block header RLP. This was introduced in Prague.
     function getRequestsHash(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Prague)[uint8(HeaderField.RequestsHash)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.RequestsHash, Hardforks.Prague).readBytes32();
     }
 
     /// @dev Extract the block access list hash from the block header RLP. This will be introduced in Amsterdam.
     function getBlockAccessListHash(bytes memory headerRLP) internal pure returns (bytes32) {
-        return _parseHeader(headerRLP, Hardforks.Amsterdam)[uint8(HeaderField.BlockAccessListHash)].readBytes32();
+        return _parseHeader(headerRLP, HeaderField.BlockAccessListHash, Hardforks.Amsterdam).readBytes32();
     }
 
     /// @dev Internal function to parse the block header RLP and return the list of fields as memory slices.
     /// It also checks that the number of fields is at least the expected number for the given hardfork version.
-    function _parseHeader(bytes memory headerRLP, Hardforks version) private pure returns (Memory.Slice[] memory) {
+    function _parseHeader(
+        bytes memory headerRLP,
+        HeaderField field,
+        Hardforks version
+    ) private pure returns (Memory.Slice) {
         Memory.Slice[] memory fields = RLP.decodeList(headerRLP);
-        require(fields.length >= _expectedHeadersLength(version), InvalidBlockHeader(version));
-        return fields;
-    }
-
-    /// @dev Internal function to return the expected number of fields in the block header RLP for a given hardfork version.
-    function _expectedHeadersLength(Hardforks version) private pure returns (uint8 count) {
-        assembly ("memory-safe") {
-            count := byte(version, 0x0F0F0F0F0F0F0F0F0F0F10101114151516000000000000000000000000000000)
-        }
+        require(uint8(field) < fields.length, InvalidBlockHeader(version));
+        return fields[uint8(field)];
     }
 }
