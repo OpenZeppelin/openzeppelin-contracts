c93b1ee2cdb02e7c5ad5609ba1223786dbcbdab4
ernestognw <ernestognw@gmail.com>
2026-04-01 15:42:50 +0200

    Fix transpilation?

--- contracts/mocks/token/ERC4626Mock.sol
+++ contracts/mocks/token/ERC4626Mock.sol
@@ -5,7 +5,7 @@ import {IERC20, ERC20} from "../../token/ERC20/ERC20.sol";
 import {ERC4626} from "../../token/ERC20/extensions/ERC4626.sol";
 
 contract ERC4626Mock is ERC4626 {
-    constructor(address underlying) ERC20("ERC4626Mock", "E4626M") ERC4626(IERC20(underlying)) {}
+    constructor(IERC20 underlying) ERC20("ERC4626Mock", "E4626M") ERC4626(underlying) {}
 
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
