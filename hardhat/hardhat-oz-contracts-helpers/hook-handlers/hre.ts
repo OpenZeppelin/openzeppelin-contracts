f76b4ffcbf4934db51b2d8fd8b254c2fb74384a0
Patricio Palladino <email@patriciopalladino.com>
2026-04-21 09:50:54 -0300

    Optimize the `hre.artifacts.readArtifact` overload
    This method used to call readArtifact with a try/catch to check if an
    artifact existed. This is slower than calling
    `hre.artifacts.artifactExists` as the errors thrown by `readArtifact`
    compute the list of similar artifact names.
    
    This commit changes the overload to check if an artifact exists using
    `artifactExists` first, and only calling `readArtifact` if that's
    positive.

--- hardhat/hardhat-oz-contracts-helpers/hook-handlers/hre.ts
+++ hardhat/hardhat-oz-contracts-helpers/hook-handlers/hre.ts
@@ -3,31 +3,26 @@ import type { HardhatRuntimeEnvironmentHooks, HookContext } from 'hardhat/types/
 import type { HardhatRuntimeEnvironment } from 'hardhat/types/hre';
 import type { ArtifactManager } from 'hardhat/types/artifacts';
 
-const suffixes = ['UpgradeableWithInit', 'Upgradeable', ''];
-
-function isExpectedError(e: unknown, suffix: string): boolean {
-  return HardhatError.isHardhatError(e) && e.number === 1000 && suffix !== '';
-}
+const suffixes = ['UpgradeableWithInit', 'Upgradeable'];
 
 const overrideReadArtifact =
-  (runSuper: ArtifactManager['readArtifact']) =>
+  (artifactExists: ArtifactManager['artifactExists'], runSuper: ArtifactManager['readArtifact']) =>
   async <ContractNameT extends string>(contractNameOrFullyQualifiedName: ContractNameT) => {
     for (const suffix of suffixes) {
-      try {
+      const artifactWithSuffix = contractNameOrFullyQualifiedName + suffix;
+      if (await artifactExists(artifactWithSuffix)) {
         return await runSuper((contractNameOrFullyQualifiedName + suffix) as ContractNameT);
-      } catch (e) {
-        if (isExpectedError(e, suffix)) {
-          continue;
-        } else {
-          throw e;
-        }
       }
     }
-    throw new Error('Unreachable');
+
+    return await runSuper(contractNameOrFullyQualifiedName as ContractNameT);
   };
 
 export default async (): Promise<Partial<HardhatRuntimeEnvironmentHooks>> => ({
   created: async (context: HookContext, hre: HardhatRuntimeEnvironment): Promise<void> => {
-    hre.artifacts.readArtifact = overrideReadArtifact(hre.artifacts.readArtifact.bind(hre.artifacts));
+    hre.artifacts.readArtifact = overrideReadArtifact(
+      hre.artifacts.artifactExists.bind(hre.artifacts),
+      hre.artifacts.readArtifact.bind(hre.artifacts),
+    );
   },
 });
