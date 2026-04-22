c63b602564016ddc58b7c3907ab62c83185a3aed
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-04-22 15:17:08 +0200

    add variants

--- contracts/token/ERC20/utils/ERC6372Utils.sol
+++ contracts/token/ERC20/utils/ERC6372Utils.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.24;
 
+import {IERC6372} from "../../../interfaces/IERC6372.sol";
 import {Time} from "../../../utils/types/Time.sol";
 
 /**
@@ -11,19 +12,39 @@ library ERC6372Utils {
     /// @dev The clock was incorrectly modified.
     error ERC6372InconsistentClock();
 
-    /// @dev Block number clock mode. Checks that the current `clock` was not modified.
+    /// @dev Variant of {blockNumberClockMode-uint48-} that checks against an IERC6372 instance
+    function blockNumberClockMode(IERC6372 instance) internal view returns (string memory) {
+        return blockNumberClockMode(instance.clock());
+    }
+
+    /// @dev Variant of {blockNumberClockMode-uint48-} that checks against the clock function.
     function blockNumberClockMode(function() view returns (uint48) clock) internal view returns (string memory) {
+        return blockNumberClockMode(clock());
+    }
+
+    /// @dev Block number clock mode. Checks that the current `clock` was not modified.
+    function blockNumberClockMode(uint48 clock) internal view returns (string memory) {
         // Check that the clock was not modified
-        if (clock() != Time.blockNumber()) {
+        if (clock != Time.blockNumber()) {
             revert ERC6372InconsistentClock();
         }
         return "mode=blocknumber&from=default";
     }
 
-    /// @dev Timestamp clock mode. Checks that the current `clock` was not modified.
+    /// @dev Variant of {timestampClockMode-uint48-} that checks against an IERC6372 instance
+    function timestampClockMode(IERC6372 instance) internal view returns (string memory) {
+        return timestampClockMode(instance.clock());
+    }
+
+    /// @dev Variant of {timestampClockMode-uint48-} that checks against the clock function.
     function timestampClockMode(function() view returns (uint48) clock) internal view returns (string memory) {
+        return timestampClockMode(clock());
+    }
+
+    /// @dev Timestamp clock mode. Checks that the current `clock` was not modified.
+    function timestampClockMode(uint48 clock) internal view returns (string memory) {
         // Check that the clock was not modified
-        if (clock() != Time.timestamp()) {
+        if (clock != Time.timestamp()) {
             revert ERC6372InconsistentClock();
         }
         return "mode=timestamp";
