df17c50a816d39e34f20bf3781d99fce34a1dff1
Dnreikronos <dnreikronos2@gmail.com>
2026-04-24 15:04:28 -0300

    Bump VestingWallet pragma to ^0.8.24 for ERC6372Utils compatibility

--- contracts/finance/VestingWallet.sol
+++ contracts/finance/VestingWallet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.6.0) (finance/VestingWallet.sol)
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {IERC20} from "../token/ERC20/IERC20.sol";
 import {SafeERC20} from "../token/ERC20/utils/SafeERC20.sol";
