a1aa44ebf34cf2c54edbf25acdcadd037923f009
TenHam3 <simbaandbella3@gmail.com>
2026-05-10 20:57:47 -0500

    Wrapped data struct in contract block and added events

--- contracts/finance/VestingWalletFactory.sol
+++ contracts/finance/VestingWalletFactory.sol
@@ -3,14 +3,19 @@
 
 pragma solidity ^0.8.20;
 
-struct VestingSchedule {
-    address beneficiary;
-    address token;
-    uint64  start;
-    uint64  duration;
-    uint256 totalAllocation;
-    uint256 released;
-}
+contract VestingWalletFactory {
+    struct VestingSchedule {
+        address beneficiary;
+        address token;
+        uint64  start;
+        uint64  duration;
+        uint256 totalAllocation;
+        uint256 released;
+    }
 
-mapping(uint256 scheduleId => VestingSchedule) private _schedules;
-uint256 private _scheduleCount;
+    mapping(uint256 scheduleId => VestingSchedule) private _schedules;
+    uint256 private _scheduleCount;
+
+    event VestingScheduleCreated(uint256 indexed scheduleId, address indexed beneficiary, address indexed token, uint64 start, uint64 duration, uint256 amount);
+    event ERC20Released(uint256 indexed scheduleId, address indexed token, uint256 amount);
+}
