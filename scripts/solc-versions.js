620d143e4b782450725d96597f7bff46534cd561
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-05-22 21:24:49 +0200

    use execFile to avoid script injection

--- scripts/solc-versions.js
+++ scripts/solc-versions.js
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { execFile } from 'child_process';
 import semver from 'semver';
 import { range } from './helpers.js';
 
@@ -8,7 +8,7 @@ export const versions = ['0.4.26', '0.5.16', '0.6.12', '0.7.6', '0.8.33']
 
 export const compile = (source, version) =>
   new Promise((resolve, reject) =>
-    exec(`forge build ${source} --use ${version} --out out/solc-${version}`, error =>
+    execFile('forge', ['build', source, '--use', version, '--out', `out/solc-${version}`], error =>
       error ? reject(error) : resolve(),
     ),
   );
