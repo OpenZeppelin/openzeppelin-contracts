d954f39823a4cb07805602af42af71bcfc216fd2
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-06-04 17:04:07 +0200

    Revert "reorder prefundContext structure"
    This reverts commit b9fea85f0b2938850a1a845661e0f798dcd40896.

--- test/account/paymaster/PaymasterERC20Guarantor.test.js
+++ test/account/paymaster/PaymasterERC20Guarantor.test.js
@@ -6,7 +6,6 @@ const { anyValue } = require('@nomicfoundation/hardhat-chai-matchers/withArgs');
 const { getDomain, formatType, PackedUserOperation } = require('../../helpers/eip712');
 const { ERC4337Helper } = require('../../helpers/erc4337');
 const { encodeBatch, encodeMode, CALL_TYPE_BATCH } = require('../../helpers/erc7579');
-const { generators } = require('../../helpers/random');
 
 const { shouldBehaveLikePaymaster } = require('./Paymaster.behavior');
 
@@ -370,7 +369,10 @@ describe('PaymasterERC20Guarantor', function () {
       await this.token.$_approve(this.guarantor, this.paymaster, ethers.MaxUint256);
       await this.token.$_mint(this.paymaster, value); // fund the refund leg
 
-      const prefundContext = ethers.solidityPacked(['address', 'bytes'], [this.guarantor.address, generators.bytes()]);
+      const prefundContext = ethers.solidityPacked(
+        ['bytes32', 'address'],
+        [ethers.id('extra subclass data'), this.guarantor.address],
+      );
 
       await expect(
         // prefunder=other ≠ userOpSender=guarantor (read from tail) so `_refund` enters the
