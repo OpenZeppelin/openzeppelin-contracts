cfe2f2f608dd86e8458297919de93b0b03e3da22
Akshay Tarpara <akpatel0618@gmail.com>
2026-06-11 00:51:28 +0530

    ⚡Optimize Create3 Lib (#6564)
    Co-authored-by: Hadrien Croubois <hadrien.croubois@gmail.com>
    Co-authored-by: ernestognw <ernestognw@gmail.com>

--- test/utils/Create3.test.js
+++ test/utils/Create3.test.js
@@ -5,7 +5,7 @@ const { PANIC_CODES } = require('@nomicfoundation/hardhat-chai-matchers/panic');
 
 const { RevertType } = require('../helpers/enums');
 
-const PROXY_INITCODE_HASH = '0xd61bbde0460e6c48ddd99fb8b7e1ad36529d2ec79cbac1db0300b3d26ddcdc2a';
+const PROXY_INITCODE_HASH = '0x57a34f6e879358dd76825d6700df87013ad6a3fb43c0d0c602f70a8772c153bd';
 const getCreate3Address = (deployer, salt) =>
   ethers.getCreateAddress({ from: ethers.getCreate2Address(deployer, salt, PROXY_INITCODE_HASH), nonce: 1 });
 
