7cb61784596f742a96bc552d5120764c45e361d7
Ernesto García <ernestognw@gmail.com>
2026-06-10 11:29:38 -0600

    Remove `draft-` prefixes from ERC-4337 related files (#6581)

--- contracts/account/utils/ERC4337Utils.sol
+++ contracts/account/utils/ERC4337Utils.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.6.0) (account/utils/draft-ERC4337Utils.sol)
+// OpenZeppelin Contracts (last updated v5.6.0) (account/utils/ERC4337Utils.sol)
 
 pragma solidity ^0.8.20;
 
-import {IEntryPoint, PackedUserOperation} from "../../interfaces/draft-IERC4337.sol";
+import {IEntryPoint, PackedUserOperation} from "../../interfaces/IERC4337.sol";
 import {Math} from "../../utils/math/Math.sol";
 import {Calldata} from "../../utils/Calldata.sol";
 import {Packing} from "../../utils/Packing.sol";
