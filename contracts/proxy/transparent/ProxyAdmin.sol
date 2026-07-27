d98fd6fc8cc2ce5a39ea4ec9f6fbaeef5da9c0a2
Tino Martínez Molina <tino@martinezmolina.es>
2026-07-27 15:07:55 -0700

    Lower `ProxyAdmin` `pragma`

--- contracts/proxy/transparent/ProxyAdmin.sol
+++ contracts/proxy/transparent/ProxyAdmin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.2.0) (proxy/transparent/ProxyAdmin.sol)
 
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.20;
 
 import {ITransparentUpgradeableProxy} from "./ITransparentUpgradeableProxy.sol";
 import {Ownable} from "../../access/Ownable.sol";
