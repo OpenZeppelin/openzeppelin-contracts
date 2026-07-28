e2d8c3939d0b92fe4e717b113ea6921f85547197
Hadrien Croubois <hadrien.croubois@gmail.com>
2026-07-28 10:32:16 +0200

    Validate non-empty address in crosschain bridge transfers

--- test/crosschain/BridgeERC20.behavior.js
+++ test/crosschain/BridgeERC20.behavior.js
@@ -73,6 +73,19 @@ function shouldBehaveLikeBridgeERC20({ chainAIsCustodial = false, chainBIsCustod
         .withArgs(chainAIsCustodial ? this.bridgeA : ethers.ZeroAddress, chris, amount);
     });
 
+    describe('invalid transfer', function () {
+      it('reverts if the address part of the interoperable address is empty', async function () {
+        const [alice] = this.accounts;
+
+        await this.tokenA.$_mint(alice, amount);
+        await this.tokenA.connect(alice).approve(this.bridgeA, ethers.MaxUint256);
+
+        await expect(
+          this.bridgeA.connect(alice).crosschainTransfer(this.chain.toErc7930(undefined), amount), // No address
+        ).to.be.revertedWithCustomError(this.bridgeA, 'CrosschainFungibleEmptyAddress');
+      });
+    });
+
     describe('restrictions', function () {
       beforeEach(async function () {
         await this.tokenA.$_mint(this.bridgeA, 1_000_000_000n);
