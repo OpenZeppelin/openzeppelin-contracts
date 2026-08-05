842833a12ec983675483bdbaeddb8afad61597ec
Luiz <luiz.vasconcelos@openzeppelin.com>
2026-08-05 17:05:14 -0300

    use assembly instead of caching address.code

--- contracts/account/utils/EIP7702Utils.sol
+++ contracts/account/utils/EIP7702Utils.sol
@@ -13,9 +13,17 @@ library EIP7702Utils {
 
     /**
      * @dev Returns the address of the delegate if `account` has an EIP-7702 delegation setup, or address(0) otherwise.
+     *
+     * NOTE: Only the first word of `account`'s code is read, so the cost does not depend on the size of that code.
      */
     function fetchDelegate(address account) internal view returns (address) {
-        bytes23 delegation = bytes23(account.code);
+        bytes32 word;
+        assembly ("memory-safe") {
+            extcodecopy(account, 0x00, 0x00, 0x20)
+            word := mload(0x00)
+        }
+        // A delegation designator is 23 bytes. `extcodecopy` zero-pads, so shorter code can't match the prefix.
+        bytes23 delegation = bytes23(word);
         return bytes3(delegation) == EIP7702_PREFIX ? address(bytes20(delegation << 24)) : address(0);
     }
 }
