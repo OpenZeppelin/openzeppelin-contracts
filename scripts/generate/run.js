7eaa7a62447d43ab51471765f8c07a95815f9fb0
James Toussaint <33313130+james-toussaint@users.noreply.github.com>
2026-09-01 09:45:33 +0200

    Inject shared data in all templates

--- scripts/generate/run.js
+++ scripts/generate/run.js
@@ -5,28 +5,15 @@ import path from 'path';
 import prettier from 'prettier';
 import { Eta } from 'eta';
 
-// Shared data + helpers injected into the `.sol.eta` templates.
+// Shared data + helpers. The full set is injected into every `.sol.eta` template (e.g. it.SLOT_TYPES, it.sanitize).
+import * as data from './data.js';
 import * as sanitize from './helpers/sanitize.js';
 import { capitalize, product } from '../helpers.js';
 import { toBytes32, fromBytes32 } from './helpers/conversion.js';
-import {
-  SLOT_TYPES,
-  SLOT_VALUE_TYPES,
-  SET_TYPES,
-  MAP_TYPES,
-  CHECKPOINTS_OPTS,
-  PACKING_SIZES,
-  ARRAYS_TYPES,
-  ARRAYS_VALUE_TYPES,
-  ARRAYS_CAST_TYPES,
-  ARRAYS_SORT_TYPES,
-  ARRAYS_COMPARATOR_TYPES,
-  SAFECAST_LENGTHS,
-  MERKLEPROOF_OPTS,
-  MERKLEPROOF_DEFAULT_HASH,
-} from './data.js';
 
 const args = (...parts) => parts.filter(Boolean).join(', ');
+// Everything a template can reference
+const context = { ...data, sanitize, capitalize, product, toBytes32, fromBytes32, args };
 const repoRoot = path.join(import.meta.dirname, '../..');
 const templatesDir = path.join(import.meta.dirname, 'templates');
 const eta = new Eta({ views: templatesDir, autoEscape: false, autoTrim: false, defaultExtension: '' });
@@ -62,37 +49,26 @@ async function generate(file, context, outputPrefix) {
   fs.writeFileSync(output, await prettier.format(content, { ...prettierConfig, filepath: output }));
 }
 
-// output path (relative to the prefix) -> template context
-const contracts = {
-  'mocks/StorageSlotMock.sol': { TYPES: SLOT_TYPES, VALUE_TYPES: SLOT_VALUE_TYPES },
-  'mocks/TransientSlotMock.sol': { TYPES: SLOT_TYPES, VALUE_TYPES: SLOT_VALUE_TYPES },
-  'utils/Arrays.sol': {
-    TYPES: ARRAYS_TYPES,
-    VALUE_TYPES: ARRAYS_VALUE_TYPES,
-    CAST_TYPES: ARRAYS_CAST_TYPES,
-    SORT_TYPES: ARRAYS_SORT_TYPES,
-    COMPARATOR_TYPES: ARRAYS_COMPARATOR_TYPES,
-    capitalize,
-  },
-  'utils/Packing.sol': { SIZES: PACKING_SIZES, sanitize, product },
-  'utils/SlotDerivation.sol': { TYPES: SLOT_TYPES, sanitize },
-  'utils/StorageSlot.sol': { TYPES: SLOT_TYPES },
-  'utils/TransientSlot.sol': { TYPES: SLOT_TYPES, VALUE_TYPES: SLOT_VALUE_TYPES },
-  'utils/cryptography/MerkleProof.sol': { OPTS: MERKLEPROOF_OPTS, DEFAULT_HASH: MERKLEPROOF_DEFAULT_HASH, args },
-  'utils/math/SafeCast.sol': { LENGTHS: SAFECAST_LENGTHS },
-  'utils/structs/Checkpoints.sol': { OPTS: CHECKPOINTS_OPTS },
-  'utils/structs/EnumerableMap.sol': { MAP_TYPES, toBytes32, fromBytes32 },
-  'utils/structs/EnumerableSet.sol': { SET_TYPES, toBytes32, fromBytes32 },
+const all = {
+  './contracts/': [
+    'mocks/StorageSlotMock.sol',
+    'mocks/TransientSlotMock.sol',
+    'utils/Arrays.sol',
+    'utils/Packing.sol',
+    'utils/SlotDerivation.sol',
+    'utils/StorageSlot.sol',
+    'utils/TransientSlot.sol',
+    'utils/cryptography/MerkleProof.sol',
+    'utils/math/SafeCast.sol',
+    'utils/structs/Checkpoints.sol',
+    'utils/structs/EnumerableMap.sol',
+    'utils/structs/EnumerableSet.sol',
+  ],
+  './test/': ['utils/Packing.t.sol', 'utils/SlotDerivation.t.sol', 'utils/structs/Checkpoints.t.sol'],
 };
 
-const tests = {
-  'utils/Packing.t.sol': { SIZES: PACKING_SIZES, product },
-  'utils/SlotDerivation.t.sol': { TYPES: SLOT_TYPES, capitalize },
-  'utils/structs/Checkpoints.t.sol': { OPTS: CHECKPOINTS_OPTS, capitalize },
-};
-
-for (const [prefix, entries] of Object.entries({ './contracts/': contracts, './test/': tests })) {
-  for (const [file, context] of Object.entries(entries)) {
+for (const [prefix, files] of Object.entries(all)) {
+  for (const file of files) {
     await generate(file, context, prefix);
   }
 }
